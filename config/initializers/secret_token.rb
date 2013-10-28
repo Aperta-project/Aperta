868252bdbc6faba155963a251b06fe5603e49ff9
Mike Mazur & Rizwan Reza <pair+mike+riz@neo.com>
2013-10-28 13:46:45 -0700

    Initial import of rails app with rspec and devise views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tahi::Application.config.secret_key_base = '22e1dfc5ca6d879aaa7e06d5a149fd65dbbb775859b546e8163f400cb91eb2edfea11a8c6a8ad7b5a378c7f68c881c4f4250501b84a85622eb8a49651b27050d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tahi::Application.config.session_store :cookie_store, key: '_tahi_session'

98c9f8cd2c97938b7050cca70c65eda538a63432
Mike Mazur & Rizwan Reza <pair+mike+riz@neo.com>
2013-10-29 10:56:38 -0700

    Add staging environment with sendgrid config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,5 +33,5 @@ Tahi::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-  config.action_mailer.default_url_options = { :host => 'www.example.com' }
+  config.action_mailer.default_url_options = { host: 'www.example.com' }
 end

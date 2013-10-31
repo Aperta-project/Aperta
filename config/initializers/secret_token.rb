da1f754ba2c57bb7647045bbe8992bbf66303acf
Mike Mazur & Rizwan Reza <pair+mike+riz@neo.com>
2013-10-31 15:41:52 -0700

    Read secret token from environment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tahi::Application.config.secret_key_base = '22e1dfc5ca6d879aaa7e06d5a149fd65dbbb775859b546e8163f400cb91eb2edfea11a8c6a8ad7b5a378c7f68c881c4f4250501b84a85622eb8a49651b27050d'
+Tahi::Application.config.secret_key_base = ENV['RAILS_SECRET_TOKEN']

beec642b4bbb81756c12ca687356d60a95f4f372
Mike Mazur <pair+mike@neo.com>
2013-11-08 12:28:14 -0800

    Paper has many declarations
    [#59692782]

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,9 +1,19 @@
 class Paper < ActiveRecord::Base
   PAPER_TYPES = %w(research front_matter)
 
-  after_initialize ->(p) { p.paper_type = 'research' }, unless: :paper_type
+  after_initialize :initialize_defaults
 
   belongs_to :user
+  has_many :declarations
+
+  accepts_nested_attributes_for :declarations
 
   validates :paper_type, inclusion: { in: PAPER_TYPES }
+
+  private
+
+  def initialize_defaults
+    self.paper_type = 'research' if paper_type.blank?
+    self.declarations = Declaration.default_declarations if declarations.blank?
+  end
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -9,9 +9,37 @@ describe Paper do
         end
       end
 
+      context "when paper_type is the empty string" do
+        it "defaults to research" do
+          expect(Paper.new(paper_type: '').paper_type).to eq 'research'
+        end
+      end
+
       context "when paper_type is specified" do
         it "uses specified paper_type" do
           expect(Paper.new(paper_type: 'foobar').paper_type).to eq 'foobar'
+        end
+      end
+    end
+
+    describe "declarations" do
+      it "initializes default declarations" do
+        default_declarations = [
+          Declaration.new(question: 'Question 1'),
+          Declaration.new(question: 'Question 2'),
+          Declaration.new(question: 'Question 3')
+        ]
+        Declaration.stub(:default_declarations).and_return default_declarations
+
+        paper = Paper.new
+        expect(paper.declarations).to match_array default_declarations
+      end
+
+      context "when declarations are specified" do
+        it "uses provided declarations" do
+          declarations = [Declaration.new(question: 'Question')]
+          paper = Paper.new declarations: declarations
+          expect(paper.declarations).to match_array declarations
         end
       end
     end

e3239e5f13cf3d8ea9cb41dbf2ef20e26d822866
Mike Mazur <pair+mike@neo.com>
2013-11-07 17:52:39 -0800

    Run jasmine in CI

--- bin/ci.sh
+++ bin/ci.sh
@@ -21,4 +21,14 @@ bundle install
 export RAILS_SECRET_TOKEN='abc'
 RAILS_ENV=test rake db:test:load
 
+set +e
+
+rake jasmine:ci
+jasmine_result=$?
+
 rspec spec --format=documentation
+rspec_result=$?
+
+if [ $jasmine_result != '0' -o $rspec_result != '0' ]; then
+  exit 1;
+fi

1bb0b38a0d936b028ee553c939b70ef64eea8ccd
Mike Mazur <pair+mike@neo.com>
2014-01-22 11:29:04 -0800

    Don't require basic auth in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: "tahi", password: "tahi3000", if: -> { %w(development production staging).include? Rails.env }
+  http_basic_authenticate_with name: "tahi", password: "tahi3000", if: -> { %w(production staging).include? Rails.env }
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 

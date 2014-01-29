4afb005d9927b587c112742cc86b0db6a6b5a266
Mike Mazur & Rizwan Reza <pair+mike+riz@neo.com>
2014-01-29 12:23:30 -0800

    Remove paper type from new page as well
    [Finishes #64429672]

--- spec/features/author_submits_paper_spec.rb
+++ spec/features/author_submits_paper_spec.rb
@@ -18,7 +18,7 @@ feature "Paper Submission" do
 
     dashboard_page = DashboardPage.visit
     new_submission_page = dashboard_page.new_submission
-    edit_submission_page = new_submission_page.create_submission 'This is a short title', journal: 'PLOS One', paper_type: 'Front matter'
+    edit_submission_page = new_submission_page.create_submission 'This is a short title', journal: 'PLOS One'
 
     expect(edit_submission_page.journal).to eq('PLOS One')
     dashboard_page = edit_submission_page.visit_dashboard
--- spec/support/pages/new_submission_page.rb
+++ spec/support/pages/new_submission_page.rb
@@ -1,10 +1,9 @@
 class NewSubmissionPage < Page
   path :new_paper
 
-  def create_submission short_title, journal: Journal.first.name, paper_type: nil
+  def create_submission short_title, journal: Journal.first.name
     fill_in 'Short title', with: short_title
     select journal, from: 'Journal'
-    select paper_type, from: 'Paper type' if paper_type.present?
     click_on 'Create'
     EditPaperPage.new
   end

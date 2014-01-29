b8c31c2d155038e12dad57a7dec1231124f5d649
Mike Mazur & Rizwan Reza <pair+mike+riz@neo.com>
2014-01-29 10:21:57 -0800

    Dashboard logic for displaying help messages

--- spec/views/dashboards/index.html.haml_spec.rb
+++ spec/views/dashboards/index.html.haml_spec.rb
@@ -2,23 +2,59 @@ require 'spec_helper'
 
 describe "dashboards/index" do
   let(:all_submitted_papers) { [] }
+  let(:papers) { [] }
+  let(:paper_tasks) { [] }
 
   before do
     allow(view).to receive(:current_user).and_return(mock_model User)
-    assign(:papers, [])
+    assign(:papers, papers)
     assign(:all_submitted_papers, all_submitted_papers)
-    assign(:paper_tasks, [])
+    assign(:paper_tasks, paper_tasks)
   end
 
   subject { render; Capybara.string(rendered) }
 
-  it { should_not have_text 'submitted paper' }
+  describe "my-tasks" do
+    context "when there are paper tasks" do
+      let(:paper) { mock_model(Paper, title: "My paper") }
+      let(:paper_tasks) { {paper => [mock_model(Task, title: 'My huge task', completed?: true, paper: paper)]} }
 
-  context "when there are submitted papers" do
-    let(:all_submitted_papers) do
-      [mock_model(Paper, short_title: 'submitted paper')]
+      it { should have_text 'My huge task' }
     end
 
-    it { should have_text 'submitted paper' }
+    context "when there are no paper tasks" do
+      it { should_not have_text 'My huge task' }
+      it { should_not have_text 'Your Tasks' }
+    end
+  end
+
+  describe "my-submissions" do
+    context "when there are no papers belonging to the user" do
+      let(:papers) { [] }
+
+      it { should have_text 'Your scientific paper submissions will appear here.' }
+    end
+
+    context "when there is a paper belonging to the user" do
+      let!(:papers) do
+        [mock_model(Paper, short_title: 'my paper')]
+      end
+
+      it { should have_text 'my paper' }
+      it { should_not have_text 'Your scientific paper submissions will appear here.' }
+    end
+  end
+
+  describe "all submitted papers" do
+
+    it { should_not have_text 'submitted paper' }
+
+    context "when there are submitted papers" do
+      let(:all_submitted_papers) do
+        [mock_model(Paper, short_title: 'submitted paper')]
+      end
+
+      it { should have_text 'submitted paper' }
+    end
   end
 end

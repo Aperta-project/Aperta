6442d16fb103ce767c4fe8fc260cc23b9d24844d
Andi Plantenberg & Mike Mazur <pair+andi+mike@neo.com>
2014-01-29 16:13:50 -0800

    Style create submission page

--- spec/support/pages/new_submission_page.rb
+++ spec/support/pages/new_submission_page.rb
@@ -2,7 +2,7 @@ class NewSubmissionPage < Page
   path :new_paper
 
   def create_submission short_title, journal: Journal.first.name
-    fill_in 'Short title', with: short_title
+    fill_in 'paper_short_title', with: short_title
     select journal, from: 'Journal'
     click_on 'Create'
     EditPaperPage.new

f5ddb7a00b68af0eb21c39e52ee66c1f9a2d8437
Chris Westra & Ryan Stocker <pair+chris.westra+ryan.stocker@neo.com>
2014-02-21 11:29:01 -0500

    Only load flow manager items for json request [finish #66203108]

--- app/controllers/flow_managers_controller.rb
+++ app/controllers/flow_managers_controller.rb
@@ -3,19 +3,24 @@ class FlowManagersController < ApplicationController
   before_action :verify_admin!
 
   def show
-    incomplete_tasks = Task.assigned_to(current_user).incomplete.group_by { |t| t.paper }.to_a
-    complete_tasks = Task.assigned_to(current_user).completed.map do |task|
-      [task.paper, [task]]
+    respond_to do |format|
+      format.json do
+        incomplete_tasks = Task.assigned_to(current_user).incomplete.group_by { |t| t.paper }.to_a
+        complete_tasks = Task.assigned_to(current_user).completed.map do |task|
+          [task.paper, [task]]
+        end
+        paper_admin_tasks = PaperAdminTask.assigned_to(current_user).map do |task|
+          [task.paper, []]
+        end
+        unassigned_papers = PaperAdminTask.where(assignee_id: nil).map do |task|
+          [task.paper, [task]] if User.admins_for(task.paper.journal).include? current_user
+        end.compact
+        @flows = [["Up for grabs", unassigned_papers],
+                  ["My Tasks", incomplete_tasks],
+                  ["My Papers", paper_admin_tasks],
+                  ["Done", complete_tasks]]
+      end
+      format.html
     end
-    paper_admin_tasks = PaperAdminTask.assigned_to(current_user).map do |task|
-      [task.paper, []]
-    end
-    unassigned_papers = PaperAdminTask.where(assignee_id: nil).map do |task|
-      [task.paper, [task]] if User.admins_for(task.paper.journal).include? current_user
-    end.compact
-    @flows = [["Up for grabs", unassigned_papers],
-              ["My Tasks", incomplete_tasks],
-              ["My Papers", paper_admin_tasks],
-              ["Done", complete_tasks]]
   end
 end
--- spec/controllers/flow_managers_controller_spec.rb
+++ spec/controllers/flow_managers_controller_spec.rb
@@ -9,6 +9,7 @@ describe FlowManagersController do
 
   describe "GET 'show'" do
     subject(:do_request) { get :show }
+    subject(:do_request_json) { get :show, format: :json }
 
     specify { expect(do_request).to be_success }
     specify { expect(do_request).to render_template 'show' }
@@ -26,13 +27,13 @@ describe FlowManagersController do
       end
 
       it "includes papers with Assigned Admin tasks with no assignee" do
-        do_request
+        do_request_json
         unassigned_papers = assigns(:flows).first.last.map &:first
         expect(unassigned_papers).to include paper1
       end
 
       it "does not include unassigned papers from journals the user is not an admin for" do
-        do_request
+        do_request_json
         unassigned_papers = assigns(:flows).first.last.map &:first
         expect(unassigned_papers).not_to include paper2
       end

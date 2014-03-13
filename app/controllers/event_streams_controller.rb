b6cad45fe92481455255d4fc08967574c19883ba
Stuart Nelson <stuartnelson3@gmail.com>
2014-03-13 16:54:14 -0700

    Add some untested basic auth to streams controller

--- app/controllers/event_streams_controller.rb
+++ app/controllers/event_streams_controller.rb
@@ -1,5 +1,11 @@
 class EventStreamsController < ApplicationController
+  before_action :authenticate_user!
   def show
-    render json: EventStream.connection_info(params[:id]).to_json
+    pp = PaperPolicy.new(params[:id], current_user)
+    if pp.paper
+      render json: EventStream.connection_info(params[:id]).to_json
+    else
+      head :forbidden
+    end
   end
 end

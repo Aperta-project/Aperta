9908347e3e3ffcc81adcfe6fb14078e262b49bb7
Ryan Stocker & Stuart Nelson <pair+ryan.stocker+stuart@neo.com>
2014-03-24 11:22:28 -0400

    Add db/seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#
+
+case Rails.env
+when 'development'
+  # create admin user
+  mike = User.where(email: 'mikedoel@neo.com').first_or_create(
+    first_name: 'Mike',
+    last_name: 'Doel',
+    password: 'skyline1',
+    username: 'mikedoel',
+    affiliation: 'skyline',
+    admin: true)
+
+
+  # create journal
+  plos_journal = Journal.where(name: 'PLOS Yeti').first_or_create(logo: '')
+
+  Paper.where(
+    user_id: mike.id, 
+    journal_id: plos_journal.id,
+    short_title: "The great scientific paper of 2014"
+  ).first_or_create(
+    title: "The most scrumtrulescent scientific paper of 2014.",
+    abstract: "We've discovered the rain in spain tends to stay in the plain",
+    body: "The quick man bear pig jumped over the fox"
+  )
+
+  mike.journal_roles.create(admin: true, journal_id: plos_journal.id)
+
+end

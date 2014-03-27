96a346df41ea02cf831c11a19617cc0f1f913e28
Ryan Stocker & Stuart Nelson <pair+ryan.stocker+stuart@neo.com>
2014-03-27 14:29:16 -0400

    Assign admin to paper

--- app/serializers/paper_serializer.rb
+++ app/serializers/paper_serializer.rb
@@ -1,10 +1,15 @@
 class PaperSerializer < ActiveModel::Serializer
   attributes :id, :short_title, :title, :decision, :decision_letter, :authors
+  attributes :admin_id
 
   %i!phases assignees declarations figures reviewers!.each do |relation|
     has_many relation, embed: :ids, include: true
   end
   has_one :journal, embed: :ids, include: true
+
+  def admin_id
+    object.admin.id
+  end
 
   def authors
     object.authors

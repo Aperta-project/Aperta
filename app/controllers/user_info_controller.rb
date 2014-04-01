7fc8f02f3a6597f3150400f70f6b8269d6129247
Chris Westra <chris.westra@neo.com>
2014-04-01 08:28:13 -0400

    Show message tasks on dashboard.

--- app/controllers/user_info_controller.rb
+++ app/controllers/user_info_controller.rb
@@ -8,14 +8,19 @@ class UserInfoController < ApplicationController
   end
 
   def dashboard
-    assigned_tasks = current_user.tasks.includes(:paper)
-    @tasks = assigned_tasks.group_by { |t| t.paper.id }.map do |id, tasks|
-      {id: id, tasks: tasks}
+    assigned_tasks = current_user.tasks.includes(:paper, :assignee)
+    @tasks = assigned_tasks.group_by { |t| t.paper }.map do |paper, tasks|
+      {id: paper.id, tasks: serialize_tasks(paper.message_tasks + tasks)}
     end
     @task_papers = assigned_tasks.map(&:paper).uniq
     @user_papers = current_user.papers
     @all_submitted_papers = Paper.submitted if current_user.admin?
 
     render json: { user: current_user, submissions: @user_papers, task_papers: @task_papers, assigned_tasks: @tasks}
+  end
+
+  private
+  def serialize_tasks(tasks)
+    ActiveModel::ArraySerializer.new(tasks, each_serializer: DashboardTaskSerializer)
   end
 end

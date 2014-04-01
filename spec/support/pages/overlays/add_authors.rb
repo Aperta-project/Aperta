1bb98988ac9f3e3442e2cb064a767bec1f283eb1
Chris Westra <chris.westra@neo.com>
2014-04-01 18:00:20 -0400

    Fixup authors overlay spec.

--- spec/support/pages/overlays/add_authors.rb
+++ spec/support/pages/overlays/add_authors.rb
@@ -1,10 +1,12 @@
 class AddAuthorsOverlay < CardOverlay
   def add_author author
+    binding.pry
     find('.btn-xs', text: "Add new").click
     fill_in "First name", with: author[:first_name]
     fill_in "Last name", with: author[:last_name]
     fill_in "Email", with: author[:email]
     fill_in "Affiliation", with: author[:affiliation]
-    click_on "done"
+    find('.add-author-form button.secondary-button').click
+    expect(page).to have_no_css('.add-author-form')
   end
 end

8e60005c655b0a1ee35cb7b70ce18255b4217f16
Chris Westra <chris.westra@neo.com>
2014-04-01 08:34:33 -0400

    Add a few includes.

--- app/controllers/user_info_controller.rb
+++ app/controllers/user_info_controller.rb
@@ -8,13 +8,13 @@ class UserInfoController < ApplicationController
   end
 
   def dashboard
-    assigned_tasks = current_user.tasks.includes(:paper, :assignee)
+    assigned_tasks = current_user.tasks.includes({paper: :message_tasks}, :assignee)
     @tasks = assigned_tasks.group_by { |t| t.paper }.map do |paper, tasks|
       {id: paper.id, tasks: serialize_tasks(paper.message_tasks + tasks)}
     end
     @task_papers = assigned_tasks.map(&:paper).uniq
-    @user_papers = current_user.papers
-    @all_submitted_papers = Paper.submitted if current_user.admin?
+    @user_papers = current_user.papers.includes(task_manager: :phases)
+    @all_submitted_papers = Paper.submitted.includes(task_manager: :phases) if current_user.admin?
 
     render json: { user: current_user, submissions: @user_papers, task_papers: @task_papers, assigned_tasks: @tasks}
   end

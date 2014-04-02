d12449456252abcb825eb6d6419d03b71b4abc8f
Ryan Stocker & Stuart Nelson <pair+ryan.stocker+stuart@neo.com>
2014-04-02 14:00:56 -0400

    Fix updating papers on flow_manager

--- app/serializers/flow_paper_serializer.rb
+++ app/serializers/flow_paper_serializer.rb
@@ -2,6 +2,10 @@ class FlowPaperSerializer < ActiveModel::Serializer
   attributes :id, :short_title, :title, :decision, :decision_letter, :authors
   has_many :phases, embed: :ids, include: true
   has_many :tasks, embed: :ids
+  # Flow manager page makes a request for the journal when updating the paper,
+  # but we just need the id..? Would prefer to not have to include journal to
+  # avoid this error.
+  has_one :journal, embed: :id, include: true
 
   def authors
     object.authors

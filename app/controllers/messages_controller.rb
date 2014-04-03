a417d34e5c0fab20e4ad3f020f66ea953f055c2f
Feifan Wang & Mike Mazur <pair+feifan+mike@neo.com>
2014-04-02 17:25:33 -0700

    removed outdated #create method.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,26 +4,6 @@ class MessageTasksController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
-  def create
-    # Parameters: {
-    # "message_task"=>{"title"=>"title", "type"=>"MessageTask",
-    # "completed"=>false, "role"=>nil, "body"=>"body", "phase_id"=>"17",
-    # "assignee_id"=>nil}
-    # }
-    message_task = params[:message_task]
-    # then put in the appropriate shiat.
-    if current_user.admin
-      paper = Paper.find(params[:paper_id])
-    else
-      paper = current_user.managed_papers.find(params[:paper_id])
-    end
-    phase = paper.phases.find(params[:phase_id])
-    message_task = MessageTaskCreator.with_initial_comment(phase,
-                                                           message_task_params,
-                                                           current_user)
-    render json: message_task
-  end
-
   def update_participants
     task = Task.find(params[:id])
     p = PaperPolicy.new task.paper, current_user
@@ -40,15 +20,9 @@ class MessageTasksController < ApplicationController
 
  private
 
-  def message_task_params
-    params.require(:message_task).permit([:message_body, :message_subject, {participant_ids: []}])
-  end
-
   def update_participants_params
     params.require(:task).permit({participant_ids: []})
   end
-
-
 
   def render_404
     head 404

8dac3b63e4d5c608efd5d10d8db1fb3979d71aae
Chris Westra <chris.westra@neo.com>
2014-04-03 15:51:16 -0400

    Add dependent: :destroy to message task stuff.

--- app/models/message_task.rb
+++ app/models/message_task.rb
@@ -4,8 +4,8 @@ class MessageTask < Task
 
   PERMITTED_ATTRIBUTES = [:body, {participant_ids: []}]
 
-  has_many :comments, inverse_of: :message_task, foreign_key: 'task_id'
-  has_many :message_participants, inverse_of: :message_task, foreign_key: 'task_id'
+  has_many :comments, inverse_of: :message_task, foreign_key: 'task_id', dependent: :destroy
+  has_many :message_participants, inverse_of: :message_task, foreign_key: 'task_id', dependent: :destroy
   has_many :participants, through: :message_participants
 
   validates :participants, length: {minimum: 1}

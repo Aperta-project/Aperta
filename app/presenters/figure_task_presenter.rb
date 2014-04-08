64c18abd534c581fc65703d8888af33f61119f39
Chris Westra <chris.westra@neo.com>
2014-04-08 09:55:11 -0400

    Remove FigureTaskPresenter and spec.

--- app/presenters/figure_task_presenter.rb
+++
@@ -1,8 +0,0 @@
-class FigureTaskPresenter < TaskPresenter
-  def data_attributes
-    super.merge({
-      'figures' => task.figure_access_details,
-      'figuresPath' => paper_figures_path(task.paper)
-    })
-  end
-end

fd35290425399982a80e96367d57229d1b4c0502
Aaron and Chandu <aaron@neo.com>
2014-04-08 15:22:18 -0400

    Revert "Remove test.  add_authors html view is going away soon."
    This reverts commit 91235ed5de4284bb471613d143a63f00695071af.

---
+++ spec/features/add_authors_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+feature "Add contributing authors", js: true do
+  let(:author) { FactoryGirl.create :user }
+  let(:paper) { author.papers.create! short_title: 'foo bar', journal: Journal.create! }
+
+  before do
+    sign_in_page = SignInPage.visit
+    sign_in_page.sign_in author.email
+  end
+
+  scenario "Author specifies contributing authors" do
+    pending
+    edit_paper = EditPaperPage.visit paper
+
+    edit_paper.view_card 'Add Authors' do |overlay|
+      overlay.add_author first_name: 'Neils', last_name: 'Bohr', affiliation: 'University of Copenhagen', email: 'neils@bohr.com'
+      overlay.add_author first_name: 'Nikola', last_name: 'Tesla', affiliation: 'Wardenclyffe'
+      overlay.mark_as_complete
+      expect(overlay).to be_completed
+    end
+
+    expect(edit_paper.authors).to eq "Neils Bohr, Nikola Tesla"
+
+    edit_paper.reload
+
+    expect(edit_paper.authors).to eq "Neils Bohr, Nikola Tesla"
+
+    edit_paper.view_card 'Add Authors' do |overlay|
+      expect(overlay).to be_completed
+    end
+  end
+end

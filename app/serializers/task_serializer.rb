7f7afba182b261aaf82e6db54650a64e2c568c9e
Jerry Nummi <jerry@neo.com>
2014-04-09 14:08:47 -0400

    Fix message card specs.
    Don't embed a task's phase when serializing.

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,6 +1,6 @@
 class TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :type, :completed, :parent_type, :body, :paper_title, :role
-  has_one :phase, embed: :id, include: true
+  has_one :phase, embed: :id
   has_many :assignees, embed: :ids, include: true, root: :users
   has_one :assignee, embed: :ids, include: true, root: :users
 

8cd93732af01318b9a99b8fa7676c86b957a6124
Aaron and Chandu <aaron@neo.com>
2014-04-11 14:49:22 -0400

    Fix incorrect comment to message_task relationship

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :message_task, inverse_of: :comments
+  belongs_to :message_task, inverse_of: :comments, foreign_key: :task_id
   belongs_to :commenter, class_name: 'User'
 end

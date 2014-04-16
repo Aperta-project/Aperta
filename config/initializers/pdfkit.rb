dbdac3eaeb821e0143f76fc69ea0d9fdf81d15b3
Feifan Wang & Rizwan Reza <pair+feifan+riz@neo.com>
2014-04-16 10:50:27 -0700

    Use newly added wkhtmltopdf binary for heroku

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,10 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = Rails.root.join("bin", "wkhtmltopdf").to_s
+  config.wkhtmltopdf = if Rails.env.development?
+                         Rails.root.join("bin", "wkhtmltopdf").to_s
+                       else
+                         Rails.root.join("bin", "wkhtmltopdf-linux-amd64").to_s
+                       end
+
   config.default_options = {
     :page_size => 'Letter'
   }

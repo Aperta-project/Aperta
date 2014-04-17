ecd5bd1d5282f61b6768a97d34f7674c91d54aa7
Feifan Wang & Rizwan Reza <pair+feifan+riz@neo.com>
2014-04-17 16:12:15 -0700

    [#67438120] The api controller properly recognizes json-api PATCH body.

--- app/controllers/api/papers_controller.rb
+++ app/controllers/api/papers_controller.rb
@@ -8,4 +8,8 @@ class Api::PapersController < ApplicationController
     @paper = Paper.find params[:id]
     render json: [@paper], each_serializer: Api::PaperSerializer
   end
+
+  def update
+    head :no_content
+  end
 end
--- spec/requests/api/papers_spec.rb
+++ spec/requests/api/papers_spec.rb
@@ -16,7 +16,7 @@ describe Api::PapersController do
                                        title: "Second paper") }
 
     it "user can get a list of papers" do
-      get :index
+      get api_papers_path
 
       expect(JSON.parse(response.body)).to eq(
         {
@@ -33,7 +33,7 @@ describe Api::PapersController do
 
   describe "GET 'show'" do
     it "user can get a single paper" do
-      get :show, { id: paper1.id }
+      get api_paper_path(paper1.id)
 
       data = JSON.parse response.body
       expect(data['papers'].length).to eq 1
@@ -46,6 +46,18 @@ describe Api::PapersController do
           ]
         }.with_indifferent_access
       )
+    end
+  end
+
+  describe "PATCH 'published_at'" do
+    it "updates the published_at attribute for a paper" do
+      patch_params = '[{ "op": "replace", "path": "/papers/0/publishedAt", "value": "2014-03-21" }]'
+      patch api_paper_path(paper1.id), patch_params, { 'CONTENT_TYPE' => "application/json-patch+json",
+                                                         'ACCEPT' => "application/vnd.api+json" }
+
+      expect(response.body).to_not be_nil
+      expect(response.status).to eq 204
+      expect(paper1.published_at).to eq("2014-03-21")
     end
   end
 end

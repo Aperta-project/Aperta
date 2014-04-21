3b3d4f6915891c3bb8236c18425590334d9e1a0f
Chris Westra <chris.westra@neo.com>
2014-04-21 09:04:42 -0400

    Remove unused parent_type attributes from models.

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,14 +1,10 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :title, :type, :completed, :parent_type, :body, :paper_title, :role
+  attributes :id, :title, :type, :completed, :body, :paper_title, :role
   has_one :phase, embed: :id
   has_many :assignees, embed: :ids, include: true, root: :users
   has_one :assignee, embed: :ids, include: true, root: :users
 
   self.root = :task
-
-  def parent_type
-    'task'
-  end
 
   def paper_title
     object.paper.display_title

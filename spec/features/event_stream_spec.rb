1d0acf1b6168562e6371e92b4faacfc356c10bb4
Stuart Nelson <stuartnelson3@gmail.com>
2014-04-28 13:47:51 -0400

    Add edit page spec and sleep for race condition.

--- spec/features/event_stream_spec.rb
+++ spec/features/event_stream_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature "Event streaming", js: true do
   let!(:author) { FactoryGirl.create :user }
   let!(:paper) { author.papers.create! short_title: 'foo bar', journal: Journal.create! }
+  let(:upload_task) { author.papers.first.tasks_for_type(UploadManuscriptTask).first }
 
   before do
     sign_in_page = SignInPage.visit
@@ -10,10 +11,19 @@ feature "Event streaming", js: true do
   end
 
   scenario "On the dashboard page" do
+    # Weird race condition if this test doesn't run first.
+    sleep 0.3
     expect(page).to have_no_selector(".completed")
-    t = author.papers.first.tasks_for_type(UploadManuscriptTask).first
-    t.completed = true
-    t.save
+    upload_task.completed = true
+    upload_task.save
+    expect(page).to have_css(".card-completed", count: 1)
+  end
+
+  scenario "On the edit paper page" do
+    EditPaperPage.visit paper
+    expect(page).to have_no_selector(".completed")
+    upload_task.completed = true
+    upload_task.save
     expect(page).to have_css(".card-completed", count: 1)
   end
 

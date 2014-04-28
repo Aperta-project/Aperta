e99c85cacae13b6d59de9df08069eae52f45df65
Stuart Nelson <stuartnelson3@gmail.com>
2014-04-28 15:39:44 -0400

    Add message task creation and completion spec.

--- spec/features/event_stream_spec.rb
+++ spec/features/event_stream_spec.rb
@@ -22,12 +22,26 @@ feature "Event streaming", js: true do
     expect(page).to have_css(".card-completed", count: 1)
   end
 
-  describe "tasks" do
-    scenario "creating a new task" do
+  describe "manuscript manager" do
+    before do
       edit_paper = EditPaperPage.visit paper
       edit_paper.navigate_to_task_manager
+    end
 
-      submission_phase = paper.phases.find_by_name("Submission Data")
+    let(:submission_phase) { paper.phases.find_by_name("Submission Data") }
+
+    scenario "creating a new message task" do
+      mt = submission_phase.tasks.new title: "Wicked Message Card", type: "MessageTask", body: "Hi there!", role: "user"
+      mt.participants << author
+      mt.save!
+
+      phase = all('.column').detect {|p| p.find('h2').text == "Submission Data" }
+      within phase do
+        expect(page).to have_content "Wicked Message Card"
+      end
+    end
+
+    scenario "creating a new task" do
       submission_phase.tasks.create title: "Wicked Awesome Card", type: "Task", body: "Hi there!", role: "admin"
 
       phase = all('.column').detect {|p| p.find('h2').text == "Submission Data" }
@@ -35,7 +49,30 @@ feature "Event streaming", js: true do
         expect(page).to have_content "Wicked Awesome Card"
       end
     end
+  end
 
+  describe "message tasks" do
+    before do
+      edit_paper = EditPaperPage.visit paper
+      edit_paper.navigate_to_task_manager
+      submission_phase = paper.phases.find_by_name("Submission Data")
+      @mt = submission_phase.tasks.new title: "Wicked Message Card", type: "MessageTask", body: "Hi there!", role: "user"
+      @mt.participants << author
+      @mt.save!
+      find('.card-content', text: "Wicked Message Card").click
+    end
+
+    scenario "marking complete" do
+      checkbox = find("#task_completed")
+      expect(checkbox).to_not be_checked
+      @mt.completed = true
+      @mt.save
+      sleep 0.3
+      expect(checkbox).to be_checked
+    end
+  end
+
+  describe "tasks" do
     scenario "enter declarations" do
       edit_paper = EditPaperPage.visit paper
       edit_paper.view_card('Enter Declarations')

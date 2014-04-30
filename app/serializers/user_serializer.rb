61e50e58f53c605be5e8d6c978970161082b396e
Chris Camacho and Rizwan Reza <pair+chris+riz@neo.com>
2014-04-30 16:55:42 -0700

    Whitespace fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name, :image_url, :username, :affiliation, :email
+  attributes :id,
+    :full_name,
+    :image_url,
+    :username,
+    :affiliation,
+    :email
 end

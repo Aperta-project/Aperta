9ca6f354fdfdda3f5f52cc1ceb3fdefdec6ab51a
Feifan Wang & Rizwan Reza <pair+feifan+riz@neo.com>
2014-05-08 11:29:28 -0700

    [#70765796] Update responder now properly responds.

--- app/assets/javascripts/controllers/overlays/paper_reviewer_overlay_controller.js.coffee
+++ app/assets/javascripts/controllers/overlays/paper_reviewer_overlay_controller.js.coffee
@@ -1,6 +1,1 @@
-ETahi.PaperReviewerOverlayController = ETahi.TaskController.extend
-  reveiwersChanged: (->
-    # @get('model.paper').then (paper) =>
-    #   paper.get('tasks').then (tasks) =>
-    #     tasks.findBy('title', 'Reviewer Report').deleteRecord()
-  ).observes('reviewers.@each').on('isDeleted') #this doesnt work
+ETahi.PaperReviewerOverlayController = ETahi.TaskController.extend()
--- app/services/update_responders/paper_reviewer_task.rb
+++ app/services/update_responders/paper_reviewer_task.rb
@@ -6,14 +6,9 @@ module UpdateResponders
     end
 
     def content
-      paper_reviewer_tasks = @task.paper.tasks.where(type: ReviewerReportTask)
-      # phases = @task.paper.phases.where(id: paper_reviewer_tasks.pluck(:phase_id).push(@task.phase_id))
-      # phases = @task.paper.phases.where(id: [186, 187, 188, 189, 190])
-      # phases = @task.paper.phases.where(id: [188, 189])
-      phases = @task.paper.phases.where(id: [187, 188, 189])
-      # phases = @task.paper.phases
-
-      json = ActiveModel::ArraySerializer.new(phases, root: :phases).as_json
+      json = ActiveModel::ArraySerializer.new(@task.paper.phases, root: :phases).as_json
+      json[:tasks].unshift(@task)
+      json[:tasks] = json[:tasks].uniq { |task| task[:id] }
       json
     end
   end

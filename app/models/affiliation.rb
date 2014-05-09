358f31be9706967081bfd2d6a3778bf04a3b711f
Aaron Christy <aaron@phad.com>
2014-05-09 10:17:38 -0400

    add email validation to affiliation

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -2,6 +2,7 @@ class Affiliation < ActiveRecord::Base
   belongs_to :user, inverse_of: :affiliations
 
   validates :user, :name, presence: true
+  validates :email, format: Devise.email_regexp, allow_blank: true
   validates_with AffiliationDateValidator
 
   scope :by_date, -> { order(end_date: :desc, start_date: :asc) }

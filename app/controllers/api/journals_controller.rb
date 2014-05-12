fe56cc2ba57435737fb50457a3b5120330534821
Feifan Wang & Rizwan Reza <pair+feifan+riz@neo.com>
2014-05-12 14:35:13 -0700

    journal index route + spec

---
+++ app/controllers/api/journals_controller.rb
@@ -0,0 +1,7 @@
+class Api::JournalsController < ApplicationController
+  def index
+    @journals = Journal.all
+    render json: @journals, each_serializer: Api::JournalSerializer
+  end
+
+end
---
+++ app/serializers/api/journal_serializer.rb
@@ -0,0 +1,3 @@
+class Api::JournalSerializer < ActiveModel::Serializer
+  attributes :id, :name
+end

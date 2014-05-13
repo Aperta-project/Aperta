b94b7b98b0a15613562c5688b9df1670dec2142e
Aaron Christy <aaron@phad.com>
2014-05-13 13:04:48 -0400

    Simplify the pattern matching for event notifications

---
+++ app/services/tahi_notifier.rb
@@ -0,0 +1,14 @@
+class TahiNotifier
+  def self.subscribe(*listeners)
+    listeners.flatten.each do |listener|
+      ActiveSupport::Notifications.subscribe(to_regexp(listener)) do |name, start, finish, id, payload|
+        yield(name, start, finish, id, payload)
+      end
+    end
+  end
+
+  def self.to_regexp(name = nil)
+    n = name.gsub(/\*$/, '')
+    %r{^#{Regexp.escape(n)}}
+  end
+end
--- config/initializers/event_stream_subscriptions.rb
+++ config/initializers/event_stream_subscriptions.rb
@@ -1,28 +1,23 @@
-#TODO: refactor so that subscribe() takes array
-#TODO: refactor so that subscribe() can take a splat such as 'comment:*'
-update_listeners = ["task:created", "task:updated", "comment:created", "comment:updated", "comment:destroyed", "survey:created", "survey:updated", "survey:destroyed"]
-update_listeners.each do |ns|
-  ActiveSupport::Notifications.subscribe(ns) do |name, start, finish, id, payload|
-    action     = payload[:action]
-    task_id    = payload[:task_id]
-    journal_id = payload[:journal_id]
+TahiNotifier.subscribe("task:created", "task:updated", "comment:*", "survey:*") do |name, start, finish, id, payload|
+  action     = payload[:action]
+  task_id    = payload[:task_id]
+  journal_id = payload[:journal_id]
 
-    task = Task.find(task_id)
-    serializer = task.active_model_serializer.new(task)
-    EventStream.post_event(
-      journal_id,
-      serializer.to_json
-    )
-  end
+  task = Task.find(task_id)
+  serializer = task.active_model_serializer.new(task)
+  EventStream.post_event(
+    journal_id,
+    serializer.to_json
+  )
 end
 
-ActiveSupport::Notifications.subscribe('task:destroyed') do |name, start, finish, id, payload|
-    action     = payload[:action]
-    task_id    = payload[:task_id]
-    journal_id = payload[:journal_id]
+TahiNotifier.subscribe("task:destroyed") do |name, start, finish, id, payload|
+  action     = payload[:action]
+  task_id    = payload[:task_id]
+  journal_id = payload[:journal_id]
 
-    EventStream.post_event(
-      journal_id,
-      { action: "destroy", task_ids: [task_id] }.to_json
-    )
+  EventStream.post_event(
+    journal_id,
+    { action: "destroy", task_ids: [task_id] }.to_json
+  )
 end

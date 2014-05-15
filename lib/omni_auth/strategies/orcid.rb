26a927ce85bebb76914c581346a348dbfd203dfb
Aaron Christy <aaron@phad.com>
2014-05-15 13:37:06 -0400

    Strategy supports multiple environments

--- lib/omni_auth/strategies/orcid.rb
+++ lib/omni_auth/strategies/orcid.rb
@@ -6,29 +6,43 @@ module OmniAuth
 
       DEFAULT_SCOPE = '/orcid-bio/read-limited'
 
-      option :client_options, {
-        :site => 'http://api.sandbox.orcid.org',
-        :authorize_url => 'http://sandbox.orcid.org/oauth/authorize',
-        :token_url => 'https://api.sandbox.orcid.org/oauth/token',
-        :scope => '/orcid-profile/read-limited',
-        :response_type => 'code',
-        :mode => :header
-      }
-
-
-      # Pull out unique ID for the user in the external system
-      uid {  access_token.params["orcid"] }
-
-      info do{} end
+      def self.options
+        { site:        "http://#{api_host}",
+        authorize_url: "http://#{site_host}/oauth/authorize",
+        token_url:     "https://#{api_host}/oauth/token",
+        scope:         "/orcid-profile/read-limited",
+        response_type: "code",
+        mode:          :header }
+      end
 
       # Customize the parameters passed to the OAuth provider in the authorization phase
       def authorize_params
-        # Trick shamelessly borrowed from the omniauth-facebook gem! 
         super.tap do |params|
           %w[scope].each { |v| params[v.to_sym] = request.params[v] if request.params[v] }
           params[:scope] ||= DEFAULT_SCOPE # ensure that we're always request *some* default scope
         end
       end
+
+      def self.api_host
+        if Rails.env.production?
+          "api.orcid.org"
+        else
+          "api.sandbox.orcid.org"
+        end
+      end
+
+      def self.site_host
+        if Rails.env.production?
+          "orcid.org"
+        else
+          "sandbox.orcid.org"
+        end
+      end
+
+      option :client_options, options
+      uid {  access_token.params["orcid"] }
+      info { Hash.new }
+
     end
   end
 end

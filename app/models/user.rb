7685591887e640a95fceb63fa4837e209e6404e7
Aaron Christy + James Martinez <pairing+ac+jm@edgecase.com>
2014-05-16 11:26:35 -0400

    comment purpose of devise override method
    - moved it to bottom of file.  not my preference for a class method,
    but it is clutter.

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,16 +22,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          authentication_keys: [:login]
 
-
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
-    else
-      where(conditions).first
-    end
-  end
-
   def self.admins
     where(admin: true)
   end
@@ -42,6 +32,16 @@ class User < ActiveRecord::Base
 
   def image_url
     avatar.url.presence || "/images/profile-no-image.png"
+  end
+
+  # allow devise login using either email or username
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    else
+      where(conditions).first
+    end
   end
 
   private

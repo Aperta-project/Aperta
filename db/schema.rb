b390570db6da816ce96fe581766c5855cf64e6d9
Aaron Christy + James Martinez <pairing+ac+jm@edgecase.com>
2014-05-19 16:38:10 -0400

    Forgotten schema change due to master merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140516173923) do
-=======
-ActiveRecord::Schema.define(version: 20140514174824) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -226,6 +222,8 @@ ActiveRecord::Schema.define(version: 20140514174824) do
     t.string   "username"
     t.boolean  "admin",                  default: false, null: false
     t.string   "avatar"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

c958f1bd0a3be5c9c49689fc11ae5ec66cc2781d
Aaron Christy <aaron@phad.com>
2014-05-19 16:43:43 -0400

    Add inline comments explanation to hash extension

--- config/initializers/core_ext.rb
+++ config/initializers/core_ext.rb
@@ -1,3 +1,9 @@
+#
+# Allows pulling nested values from a hash without worrying about key existence
+# examples:
+#   { h: { a: "1", b: "2" } }.get(:h, :b)     # "2"
+#   { h: { a: "1", b: "2" } }.get(:h, :z)     # nil
+#
 Hash.class_eval do
   def get(key, *keys)
     if keys.empty?

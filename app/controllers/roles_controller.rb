fb13effcb4a7c8c24c6a5444464a24d0823b3b9c
John Andrews + Chris Westra <pairing+ja+cw@edgecase.com>
2014-05-19 11:58:34 -0400

    Roles controller

---
+++ app/controllers/roles_controller.rb
@@ -0,0 +1,25 @@
+class RolesController < ApplicationController
+  before_action :authenticate_user!
+  respond_to :json
+
+  def create
+    role = Role.create(role_params)
+    respond_with role
+  end
+
+  def update
+    role = Role.find(params[:id])
+    role.update_attributes(role_params)
+    respond_with role
+  end
+
+  def destroy
+    role = Role.find(params[:id])
+    role.destroy
+    respond_with role
+  end
+
+  def role_params
+    params.require(:role).permit(:name, :admin, :editor, :reviewer, :journal_id)
+  end
+end

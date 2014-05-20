6ec2b10b5b1eb1c0f49281d3483bb8bdf47745ab
Aaron Christy + James Martinez <pairing+ac+jm@edgecase.com>
2014-05-20 13:48:34 -0400

    Make affiliation model polymorphic to associate to authors or users

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,7 @@
 class Author < ActiveRecord::Base
+  has_many :affiliations, inverse_of: :affiliable, as: :affiliable
+  # belongs_to :paper, inverse_of: :authors
+
   validates :first_name, :middle_initial, :last_name, :title, :department, presence: true
   validates :email, format: Devise.email_regexp
-  # belongs_to :paper, inverse_of: :authors
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520152711) do
+ActiveRecord::Schema.define(version: 20140520173418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "affiliations", force: true do |t|
-    t.integer  "user_id"
+    t.integer  "affiliable_id"
     t.string   "name"
     t.date     "start_date"
     t.date     "end_date"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.string   "affiliable_type"
   end
 
-  add_index "affiliations", ["user_id"], name: "index_affiliations_on_user_id", using: :btree
+  add_index "affiliations", ["affiliable_id"], name: "index_affiliations_on_affiliable_id", using: :btree
 
   create_table "authors", force: true do |t|
     t.string   "first_name"

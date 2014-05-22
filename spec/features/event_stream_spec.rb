b95b8c9f1b0358862bf23fb35eb01faf3d84e9c1
Feifan Wang & Rizwan Reza <pair+feifan+riz@neo.com>
2014-05-22 14:30:30 -0700

    fixed inconsistency in event stream spec

--- spec/features/event_stream_spec.rb
+++ spec/features/event_stream_spec.rb
@@ -4,7 +4,7 @@ feature "Event streaming", js: true do
   let!(:author) { FactoryGirl.create :user, :admin }
   let!(:journal) { FactoryGirl.create :journal, :with_default_template }
   let!(:paper) { FactoryGirl.create :paper, :with_tasks, user: author, journal: journal }
-  let(:upload_task) { paper.tasks_for_type(UploadManuscriptTask).first }
+  let(:upload_task) { paper.tasks_for_type(UploadManuscript::Task).first }
 
   before do
     sign_in_page = SignInPage.visit

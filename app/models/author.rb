8542e2a8e649c709785b7e70124ee77cc1ab2208
James Martinez + Stuart Nelson + Chandu Tennety <pairing+jm+sn+ct@edgecase.com>
2014-05-29 09:47:00 -0400

    Use acts_as_list for authors.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ActiveRecord::Base
   belongs_to :author_group, inverse_of: :authors
+  acts_as_list scope: :author_group
 
   # validates :first_name, :middle_initial, :last_name, :title, :department, presence: true
   # validates :email, format: Devise.email_regexp
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522222621) do
+ActiveRecord::Schema.define(version: 20140529134609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20140522222621) do
     t.integer  "author_group_id"
     t.string   "affiliation"
     t.string   "secondary_affiliation"
+    t.integer  "position"
   end
 
   create_table "comment_looks", force: true do |t|

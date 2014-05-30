a54d529d3b5e8bfd0d5f135f7d1d4c7203f4026d
James Martinez + Stuart Nelson + Chandu Tennety <pairing+jm+sn+ct@edgecase.com>
2014-05-30 11:21:44 -0400

    Validate position field on author.

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,8 @@ class Author < ActiveRecord::Base
   belongs_to :author_group, inverse_of: :authors
   acts_as_list scope: :author_group
 
+  validates :position, presence: true
+
   # validates :first_name, :middle_initial, :last_name, :title, :department, presence: true
   # validates :email, format: Devise.email_regexp
 end

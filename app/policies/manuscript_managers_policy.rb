85b09bfee7f7fdeae6d04fd021c69f0baa4c1217
John Andrews + Aaron Christy <pairing+ja+ac@edgecase.com>
2014-06-02 09:55:02 -0400

    change to interrogation methods

--- app/policies/manuscript_managers_policy.rb
+++ app/policies/manuscript_managers_policy.rb
@@ -3,7 +3,7 @@ class ManuscriptManagersPolicy < ApplicationPolicy
   require_params :paper
 
   def show?
-    super_admin? || can_manage_any_manuscript || can_manage_this_manuscript
+    super_admin? || can_manage_any_manuscript? || can_manage_this_manuscript?
   end
 
   private
@@ -12,11 +12,11 @@ class ManuscriptManagersPolicy < ApplicationPolicy
     current_user.roles.where(journal: paper.journal)
   end
 
-  def can_manage_any_manuscript
+  def can_manage_any_manuscript?
     roles.merge(Role.can_view_all_manuscript_managers).exists?
   end
 
-  def can_manage_this_manuscript
+  def can_manage_this_manuscript?
     paper.tasks.assigned_to(current_user).exists? &&
     roles.merge(Role.can_view_assigned_manuscript_managers).exists?
   end

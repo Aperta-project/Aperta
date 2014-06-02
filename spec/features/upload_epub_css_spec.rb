92682bba6baffdeb812ecd726a951f9e07b179ca
Feifan Wang & Rizwan Reza <pair+feifan+riz@neo.com>
2014-06-02 15:59:20 -0700

    moved admin journal code into partial. initial feature spec

--- spec/features/upload_epub_css_spec.rb
+++ spec/features/upload_epub_css_spec.rb
@@ -1,20 +1,31 @@
 require 'spec_helper'
 
-feature 'Upload default ePub CSS to journal' do
+feature 'Upload default ePub CSS to journal', js: true do
   let(:admin) { create :user, :admin }
   let!(:journal) { create :journal }
-  let(:admin_page) { AdminDashboardPage.visit }
-  let(:journal_page) { admin_page.visit_journal(journal) }
 
   before do
     sign_in_page = SignInPage.visit
     sign_in_page.sign_in admin
   end
 
-  scenario 'uploading an ePub CSS source' do
-    journal_page
-    journal_page.upload_css
-    expect(journal_page.epub_cover).to eq('yeti.jpg')
+  let(:admin_page) { AdminDashboardPage.visit }
+  let!(:journal_page) { admin_page.visit_journal(journal) }
 
+  scenario 'uploading an ePub CSS source' do
+    journal_page.view_card 'EDIT EPUB CSS' do |overlay|
+      overlay.css = 'body: { background-color: red; }'
+      overlay.save
+    end
+
+    journal_page.view_card 'Edit EPUB CSS' do |overlay|
+      expect(overlay.css).to eq('body: { background-color: red; }')
+    end
+
+    journal_page.reload
+
+    journal_page.view_card 'Edit EPUB CSS' do |overlay|
+      expect(overlay.css).to eq('body: { background-color: red; }')
+    end
   end
 end

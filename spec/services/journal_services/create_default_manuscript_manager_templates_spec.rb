e1899556e1d9870d3dc7f8a35f8783a961ae43e8
John Andrews + Aaron Christy <pairing+ja+ac@edgecase.com>
2014-06-02 10:02:14 -0400

    Better spec message

--- spec/services/journal_services/create_default_manuscript_manager_templates_spec.rb
+++ spec/services/journal_services/create_default_manuscript_manager_templates_spec.rb
@@ -7,7 +7,7 @@ describe JournalServices::CreateDefaultManuscriptManagerTemplates do
     allow_any_instance_of(Journal).to receive(:setup_defaults)
   end
 
-  it "does what it says" do
+  it "creates default manager templates" do
     expect do
       JournalServices::CreateDefaultManuscriptManagerTemplates.call(journal)
     end.to change { journal.manuscript_manager_templates.count } .by(1)

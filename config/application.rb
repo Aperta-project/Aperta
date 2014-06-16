8c3f75c24c647e21f3fc5be8840bdeb30916d2d6
Feifan Wang & Rizwan Reza <pair+feifan+riz@neo.com>
2014-06-16 16:39:05 -0700

    Add gif for precompile

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ Bundler.require(:default, Rails.env)
 module Tahi
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf|gif)$/
 
     config.s3_bucket = :not_set
     config.carrierwave_storage = :file

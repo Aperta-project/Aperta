2cdb6f0d32cc9c8acf6ad2d81d59c7a9612306ab
Ryan Stocker <ryan.stocker@neo.com>
2014-06-18 15:16:57 -0400

    switch figure controller specs to use xhr

--- spec/controllers/figures_controller_spec.rb
+++ spec/controllers/figures_controller_spec.rb
@@ -25,7 +25,7 @@ describe FiguresController do
     let(:url) { "http://someawesomeurl.com" }
     it "causes the creation of the figure" do
       expect(DownloadFigure).to receive(:call).with(kind_of(Figure), url).and_return(Figure.new)
-      post :create, format: "json", paper_id: paper.to_param, url: url
+      xhr :post, :create, format: "json", paper_id: paper.to_param, url: url
       expect(response).to be_success
     end
   end
@@ -35,13 +35,13 @@ describe FiguresController do
     let(:figure) { paper.figures.create! }
     it "calls DownloadFigure" do
       expect(DownloadFigure).to receive(:call).with(figure, url).and_return(figure)
-      put :update_attachment, format: "json", id: figure.id, url: url
+      xhr :put, :update_attachment, format: "json", id: figure.id, url: url
       expect(response).to be_success
     end
   end
 
   describe "PUT 'update'" do
-    subject(:do_request) { patch :update, id: paper.figures.last.id, paper_id: paper.id, figure: {title: "new title", caption: "new caption"}, format: :json }
+    subject(:do_request) { xhr :patch, :update, id: paper.figures.last.id, paper_id: paper.id, figure: {title: "new title", caption: "new caption"}, format: :json }
     before(:each) do
       paper.figures.create!
     end

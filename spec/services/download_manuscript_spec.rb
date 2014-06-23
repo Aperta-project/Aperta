94fb1ad249fd8cae231875686456c234c9b6da40
John Andrews + Chris Westra <pairing+ja+cw@edgecase.com>
2014-06-23 16:41:39 -0400

    Fix download manuscript specs

--- spec/services/download_manuscript_spec.rb
+++ spec/services/download_manuscript_spec.rb
@@ -4,16 +4,20 @@ describe DownloadManuscript do
   let(:paper) { FactoryGirl.create(:paper) }
   let(:url) { "https://tahi-development.s3.amazonaws.com/temp/about_equations.docx" }
 
+  before do
+    paper.create_manuscript!
+  end
+
   it "downloads the attachment" do
     with_aws_cassette('manuscript') do
-      manuscript = DownloadManuscript.enqueue(paper.id, url)
-      expect(manuscript.source.filename).to be_present
+      DownloadManuscript.enqueue(paper.manuscript.id, url)
+      expect(paper.manuscript.reload.source.url).to match(%r{manuscript/source\.docx})
     end
   end
 
   it "updates the paper title" do
     with_aws_cassette('manuscript') do
-      DownloadManuscript.enqueue(paper.id, url)
+      DownloadManuscript.enqueue(paper.manuscript.id, url)
       expect(paper.reload.title).to eq("Technical Writing Information Sheets")
     end
   end

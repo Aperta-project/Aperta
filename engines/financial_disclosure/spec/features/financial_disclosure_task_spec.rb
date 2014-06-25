6c99136da341d0ee27566b47a89d89c396ca4bf8
Ryan Stocker + Chandu Tennety <pairing+rs+ct@neo.com>
2014-06-25 13:37:15 -0400

    Remove unnecessary pending test.

--- engines/financial_disclosure/spec/features/financial_disclosure_task_spec.rb
+++ engines/financial_disclosure/spec/features/financial_disclosure_task_spec.rb
@@ -21,17 +21,4 @@ feature "Financial Disclosures", js: true do
       expect(overlay.dataset).to be_visible
     end
   end
-
-  scenario "navigate to authors card and back" do
-    pending "Waiting on discussion about how to visually implement adding authors to funding sources"
-    edit_paper = EditPaperPage.visit paper
-
-    edit_paper.view_card 'Financial Disclosure' do |overlay|
-      overlay.received_funding.click
-      find('a', text: 'Add author').click
-      expect(page).to have_content 'Add Authors'
-      all('.overlay-close-button').first.click
-      expect(page).to have_content 'Financial Disclosures'
-    end
-  end
 end

255776be2357086a97bf21016f3219443bc2e8be
John Andrews + Chris Westra <pairing+ja+cw@edgecase.com>
2014-06-25 13:06:22 -0400

    Update message participants with new comment.
    Attempting to update the message's participants after saving the comment
    can cause a race condition with the event stream.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,6 +12,12 @@ describe Comment do
       expect(comment.comment_looks.count).to eq(1)
     end
 
+    it 'adds the commenter as a participant if they are not already' do
+      message_task = create(:message_task, participants: [participant], phase_id: 1)
+      message_task.comments.create_with_comment_look(message_task, {body: "Halo", task: message_task, commenter: commenter})
+      expect(message_task.participants).to match_array([commenter, participant])
+    end
+
     it 'creates comment look records for each participant except commenter' do
       expect(comment.comment_looks.map(&:user)).to match_array [participant]
     end

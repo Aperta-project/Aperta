23f86686f94e792b31481855286f3d5f8b71abc1
Rizwan Reza <rizwanreza@gmail.com>
2014-06-26 16:16:28 -0700

    Use AdminJournalSerializer after create too

--- app/controllers/admin/journals_controller.rb
+++ app/controllers/admin/journals_controller.rb
@@ -9,7 +9,7 @@ class Admin::JournalsController < ApplicationController
   end
 
   def create
-    respond_with Journal.create journal_params
+    respond_with Journal.create(journal_params), serializer: AdminJournalSerializer, root: 'admin_journals'
   end
 
   def update

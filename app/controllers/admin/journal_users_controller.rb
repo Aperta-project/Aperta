a53c71850ef2ca67aab1a4181702e7edd6526e14
Chandu Tennety + Chris Westra <pairing+ct+cw@edgecase.com>
2014-06-30 16:16:16 -0400

    Now even more sanitary.

--- app/controllers/admin/journal_users_controller.rb
+++ app/controllers/admin/journal_users_controller.rb
@@ -2,7 +2,7 @@ class Admin::JournalUsersController < ApplicationController
   respond_to :json
 
   def index
-    users = User.search_users(params[:query] || "")
+    users = User.search_users(params[:query])
     respond_with users, each_serializer: AdminJournalUserSerializer, root: 'admin_journal_users'
   end
 

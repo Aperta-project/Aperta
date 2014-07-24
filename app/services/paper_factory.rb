9c1756977a7b3f8fb07dd1cc8b937dad6e7350fb
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2014-07-24 14:26:42 -0400

    Move task author assignee into error begin block

--- app/services/paper_factory.rb
+++ app/services/paper_factory.rb
@@ -42,14 +42,13 @@ class PaperFactory
     task = nil
     begin
       task = task_klass.constantize.new(phase: phase)
+      if task.role == 'author'
+        task.assignee = author
+      end
+      task.save!
     rescue NameError => e
       Rails.logger.error "Task #{task_klass} does not exist. ManuscriptManagerTemplate will need to be updated"
     end
-
-    if task.role == 'author'
-      task.assignee = author
-    end
-    task.save!
   end
 
   def template

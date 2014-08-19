adba3a65c2f8cb06f0269bbd455b9bc53e49a4fc
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2014-08-19 15:07:39 -0400

    Use SQL for all the things

--- db/migrate/20140818152233_default_value_for_task_body.rb
+++ db/migrate/20140818152233_default_value_for_task_body.rb
@@ -1,13 +1,13 @@
 class DefaultValueForTaskBody < ActiveRecord::Migration
   def up
     execute "ALTER TABLE tasks ALTER COLUMN body SET DEFAULT '[]'::JSON"
-    Task.where(body: nil).update_all(body: '[]')
+    execute "UPDATE tasks SET body = '[]' WHERE body IS NULL"
     change_column :tasks, :body, :json, null: false
   end
 
   def down
     execute "ALTER TABLE tasks ALTER COLUMN body DROP DEFAULT"
     change_column :tasks, :body, :json, null: true
-    Task.where("body::text = '[]'::text").update_all(body: nil)
+    execute "UPDATE tasks SET body = NULL WHERE body::text = '[]'::text"
   end
 end

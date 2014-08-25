712274f4a783e68344330d471ebbe2326733d14c
John Andrews + Chris Westra <pairing+ja+cw@edgecase.com>
2014-08-25 17:54:52 -0400

    Replace unread_comments_count attr on MessageTask.

--- app/serializers/comment_look_serializer.rb
+++ app/serializers/comment_look_serializer.rb
@@ -1,3 +1,4 @@
 class CommentLookSerializer < ActiveModel::Serializer
   attributes :id, :read_at
+  has_one :comment, embed: :id
 end
--- app/serializers/message_task_serializer.rb
+++ app/serializers/message_task_serializer.rb
@@ -1,6 +1,5 @@
 class MessageTaskSerializer < TaskSerializer
   embed :ids
-  attributes :unread_comments_count
   has_many :comments, include: true
   has_many :participants, serializer: UserSerializer, include: true, root: :users
 
@@ -8,11 +7,4 @@ class MessageTaskSerializer < TaskSerializer
     object.participants.includes(:affiliations)
   end
 
-  def unread_comments_count
-    if (defined? current_user) && current_user
-      CommentLook.where(user_id: current_user.id,
-                        comment_id: object.comments.pluck(:id),
-                        read_at: nil).count
-    end
-  end
 end

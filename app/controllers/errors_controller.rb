7143d3d69bfd8c1e89c92b740fea93c5624b82f3
Chris Westra <chris.westra@neo.com>
2014-09-08 10:18:03 -0400

    Add current user id to js error logging.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,6 +2,7 @@ class ErrorsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    logger.warn "JS error for user #{current_user.id}"
     logger.warn params[:stack]
     head 204
   end

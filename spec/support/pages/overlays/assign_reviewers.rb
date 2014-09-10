d305188032d1138571befdcdcf0cd2e2afac31a8
Chandu Tennety + Chris Westra <pairing+ct+cw@edgecase.com>
2014-09-10 10:16:30 -0400

    slow down selection of reviewers to give event stream time to work

--- spec/support/pages/overlays/assign_reviewers.rb
+++ spec/support/pages/overlays/assign_reviewers.rb
@@ -1,6 +1,14 @@
 class AssignReviewersOverlay < CardOverlay
   def paper_reviewers=(names)
-    names.each { |name| select_from_chosen name, skip_synchronize: true, class: 'reviewers-select' }
+    names.each do |name|
+      select_from_chosen(name, skip_synchronize: false, class: 'reviewers-select')
+      # When you select more than 1 new reviewer in rapid succession (like capybara),
+      # the event-stream update from the first selection will return
+      # before the debounced second selection has posted to the server.
+      # If you watch the server logs you'll see the same set of attributes POSTed twice in a row.
+      # So we wait before making more selections.
+      sleep 0.3 unless name == names.last
+    end
   end
 
   def paper_reviewers

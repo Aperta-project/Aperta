958e40396660e064681117833444a55a4883c52f
Aaron Christy <aaron@phad.com>
2014-09-11 15:05:03 -0400

    hound

--- config/application.rb
+++ config/application.rb
@@ -17,13 +17,13 @@ module Tahi
     config.action_mailer.default_url_options = { host: ENV.fetch('DEFAULT_MAILER_URL') }
 
     ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
+      address: 'smtp.sendgrid.net',
+      port: '587',
+      authentication: :plain,
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: 'heroku.com',
+      enable_starttls_auto: true
     }
   end
 end

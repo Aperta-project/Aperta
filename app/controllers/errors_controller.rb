7cf74e9ab86c1f3e59daf7c505b91cbc950e6afa
Felix FLores + Chris Westra <chris.westra+ff+cw@neo.com>
2014-09-18 10:32:58 -0400

    Put error messages on stdout.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,6 +4,8 @@ class ErrorsController < ApplicationController
   def create
     logger.warn "JS error for user #{current_user.id}"
     logger.warn params[:message]
+    puts "****************JS error ********************************************"
+    puts params[:message]
     head 204
   end
 end

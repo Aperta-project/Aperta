7f635fb9a840c07b5bd1ecd85496e7f288bf22c3
Felix FLores + Chris Westra <chris.westra+ff+cw@neo.com>
2014-09-18 17:48:24 -0400

    wip keep only one commentLook per comment client-side.
    [ci skip]

--- app/services/comment_look_manager.rb
+++ app/services/comment_look_manager.rb
@@ -1,8 +1,9 @@
 class CommentLookManager
-  def self.comment_looks(comment)
-    comment.participants.map do |participant|
-      read_at = Time.now if comment.created_by?(participant)
-      comment.comment_looks.where(user_id: participant.id).first_or_create!(read_at: read_at)
+  def self.comment_look(user, comment)
+    participation = user.participations.find_by_task_id(comment.task_id)
+    if participation && comment.created_at >= participation.created_at
+      read_at = Time.now if comment.created_by?(user)
+      comment.comment_looks.where(user_id: user.id).first_or_create!(read_at: read_at)
     end
   end
 end

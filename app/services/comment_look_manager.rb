e94902210caa408f69f86d0fc0883e0142642c66
Chris Westra <chris.westra@neo.com>
2014-09-22 10:35:51 -0400

    Return gracefully if no user.
    We know this is going to happen when comment looks are serlialized
    in the event stream.

--- app/services/comment_look_manager.rb
+++ app/services/comment_look_manager.rb
@@ -1,5 +1,7 @@
 class CommentLookManager
   def self.comment_look(user, comment)
+    return unless user
+
     participation = user.participations.find_by_task_id(comment.task_id)
     if participation && comment.created_at >= participation.created_at
       read_at = Time.now if comment.created_by?(user)

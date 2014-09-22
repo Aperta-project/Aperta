dd2522ec2da25547916d59c8154ead18919be04f
Chris Westra <chris.westra@neo.com>
2014-09-22 10:50:56 -0400

    Update spec for comment look manager.

--- spec/services/comment_look_manger_spec.rb
+++ spec/services/comment_look_manger_spec.rb
@@ -1,43 +1,54 @@
 require 'spec_helper'
 
 describe CommentLookManager do
-  it "creates a comment look for any participant" do
+  it "creates a comment look if the user was already a participant" do
     user = FactoryGirl.create(:user)
     participant = FactoryGirl.create(:user)
-    other_participant = FactoryGirl.create(:user)
 
-    task = FactoryGirl.create(:task, participants: [participant, other_participant])
+    task = FactoryGirl.create(:task, participants: [participant])
     comment = FactoryGirl.create(:comment, commenter: user, task: task)
 
-    looks = CommentLookManager.comment_looks(comment)
+    look = CommentLookManager.comment_look(participant, comment)
 
-    expect(looks.count).to eq(2)
-    expect(looks.first.read_at).to be_nil
+    expect(look).to_not be_nil
+    expect(look.read_at).to be_nil
   end
 
-  it "does not create new comment look records if they already exist" do
+  it "doesn't create a comment look if the comment was created before the user became a participant" do
+    user = FactoryGirl.create(:user)
+    task = FactoryGirl.create(:task)
+    comment = FactoryGirl.create(:comment, commenter: user, task: task)
+
+    participant = FactoryGirl.create(:user)
+    task.participants << participant
+
+    look = CommentLookManager.comment_look(participant, comment)
+
+    expect(look).to be_nil
+  end
+
+  it "does not create a new comment look records if it already exists" do
     user = FactoryGirl.create(:user)
 
     task = FactoryGirl.create(:task, participants: [user])
     comment = FactoryGirl.create(:comment, commenter: user, task: task)
 
-    looks = CommentLookManager.comment_looks(comment)
-    expect(looks.count).to eq(1)
+    look = CommentLookManager.comment_look(user, comment)
+    expect(look).to_not be_nil
 
-    more_looks = CommentLookManager.comment_looks(comment)
-    expect(more_looks.count).to eq(1)
+    another_look = CommentLookManager.comment_look(user, comment)
+    expect(look).to eq(another_look)
   end
 
-  it "doesn't make comment looks for non-participants even if they comment" do
+  it "doesn't make comment look for non-participants even if they comment" do
     participant = FactoryGirl.create(:user)
     commenter = FactoryGirl.create(:user)
 
     task = FactoryGirl.create(:task, participants: [participant])
     comment = FactoryGirl.create(:comment, commenter: commenter, task: task)
 
-    looks = CommentLookManager.comment_looks(comment)
-    expect(looks.count).to eq(1)
-    expect(looks.first.user_id).to eq(participant.id)
+    look = CommentLookManager.comment_look(commenter, comment)
+    expect(look).to be_nil
   end
 
   it "creates a comment_look on comment for commenter and sets the read_at to the current time" do
@@ -46,7 +57,7 @@ describe CommentLookManager do
     task = FactoryGirl.create(:task, participants: [commenter])
     comment = FactoryGirl.create(:comment, commenter: commenter, task: task)
 
-    looks = CommentLookManager.comment_looks(comment)
-    expect(looks.first.read_at).to_not be_nil
+    look = CommentLookManager.comment_look(commenter, comment)
+    expect(look.read_at).to_not be_nil
   end
 end

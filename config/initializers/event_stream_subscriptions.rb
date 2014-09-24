332957c7c7ca321e939bd6db2f94d35a9955feb4
Aaron Christy <aaron@neo.com>
2014-09-24 15:39:05 -0400

    clean up

--- config/initializers/event_stream_subscriptions.rb
+++ config/initializers/event_stream_subscriptions.rb
@@ -1,4 +1,4 @@
-TahiNotifier.subscribe("paper_role:created") do |name, start, finish, id, payload|
+TahiNotifier.subscribe("paper_role:created") do |payload|
   user_id  = payload[:user_id]
   paper_id = payload[:paper_id]
   action   = payload[:action]
@@ -8,13 +8,13 @@ TahiNotifier.subscribe("paper_role:created") do |name, start, finish, id, payloa
   serializer = LitePaperSerializer.new(paper, user: User.find(user_id))
 
   EventStream.post_event(
-    user_id,
     User,
+    user_id,
     serializer.as_json.merge(action: action, meta: meta).to_json
   )
 end
 
-TahiNotifier.subscribe("task:created", "task:updated", "comment:*") do |name, start, finish, id, payload|
+TahiNotifier.subscribe("task:created", "task:updated", "comment:*") do |payload|
   action     = payload[:action]
   task_id    = payload[:task_id]
   paper_id   = payload[:paper_id]
@@ -23,13 +23,13 @@ TahiNotifier.subscribe("task:created", "task:updated", "comment:*") do |name, st
   task = Task.find(task_id)
   serializer = task.active_model_serializer.new(task)
   EventStream.post_event(
-    paper_id,
     Paper,
+    paper_id,
     serializer.as_json.merge(action: action, meta: meta).to_json
   )
 end
 
-TahiNotifier.subscribe("supporting_information/file:*", "figure:*", "paper:*", "paper_role:created", "question_attachment:*") do |name, start, finish, id, payload|
+TahiNotifier.subscribe("supporting_information/file:*", "figure:*", "paper:*", "paper_role:created", "question_attachment:*") do |payload|
   action     = payload[:action]
   id         = payload[:id]
   paper_id   = payload[:paper_id]
@@ -39,31 +39,31 @@ TahiNotifier.subscribe("supporting_information/file:*", "figure:*", "paper:*", "
   record = klass.find(id)
   serializer = record.event_stream_serializer.new(record)
   EventStream.post_event(
-    paper_id,
     Paper,
+    paper_id,
     serializer.as_json.merge(action: action, meta: meta).to_json
   )
 end
 
-TahiNotifier.subscribe("task:destroyed") do |name, start, finish, id, payload|
+TahiNotifier.subscribe("task:destroyed") do |payload|
   action     = payload[:action]
   task_id    = payload[:task_id]
   paper_id   = payload[:paper_id]
 
   EventStream.post_event(
-    paper_id,
     Paper,
+    paper_id,
     { action: "destroy", tasks: [task_id] }.to_json
   )
 end
 
-TahiNotifier.subscribe("paper_role:destroyed") do |name, start, finish, id, payload|
+TahiNotifier.subscribe("paper_role:destroyed") do |payload|
   user_id  = payload[:user_id]
   paper_id = payload[:paper_id]
 
   EventStream.post_event(
-    user_id,
     User,
+    user_id,
     { action: "destroy", lite_papers: [paper_id] }.to_json
   )
 end

19242b7d51c25365dabbc5e9b30269c19589670e
Felix FLores + Chris Westra <chris.westra+ff+cw@neo.com>
2014-09-25 11:32:23 -0400

    Working on it... [ci skip]

--- spec/services/s3_form_configurator_spec.rb
+++ spec/services/s3_form_configurator_spec.rb
@@ -6,27 +6,45 @@ class S3FormConfigurator
   end
 end
 
+# To post to s3 from a web form we need to give the client some information via a JSON payload.
+# here's how it should look:
+# {url: <s3-url>
+#  access_key_id: <our-aws-access-key-id>
+#  acl: 'public-read' // the permissions that this file will have
+#  policy: <s3-policy> // this is a Base64 encoded JSON object with newlines and carriage returns removed.
+#  signature: <s3-signature> // this is a Base64 encoded HMAC digest generated using our aws key and the <s3-policy>
+#  key: 'some/directory/path' // this is where the file will go inside the bucket.  It should be unique.
+#  }
+#
+# The s3 policy should look as follows before it's Base64 encoded:
+# {expiration: 30.minutes.from_now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z'),
+# conditions: [
+#   { bucket: <s3-bucket-name> },
+#   { acl: 'public-read' },
+#   ["starts-with", "$key", "<key-root-path>"], //in the example shown here, this would be 'some/'
+#   ["eq", "$Content-Type", <content-type>],
+#   { success_action_status: '201' }
+# }
 describe S3FormConfigurator do
 
   describe "form_json" do
     let(:result) do
-      post_params = {s3:
-                {url: "testUrl",
-                 bucket_name: "aBucket",
-                 path_prefix: "aPath"}
-      }
+      s3_params =
+         {url: "testUrl",
+          bucket_name: "aBucket",
+          upload_root: "pending",
+          upload_path: "files/pictures",
+          content_type: "jpeg"}
 
-      result = S3FormConfigurator.form_json(test_url,
-                                            aws_key,
+      result = S3FormConfigurator.form_json(aws_key,
                                             aws_secret,
-                                            user_id,
-                                            bucket_name,
-                                            path_prefix)
+                                            s3_params
+                                           )
       JSON.parse(result)
     end
 
     it "contains the specified url" do
-      expect(result["url"]).to eq("foo")
+      expect(result["url"]).to eq("testUrl")
     end
 
     it "has the aws key" do

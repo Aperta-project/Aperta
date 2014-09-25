c57eada32a67546650ea23590eb34930ab65d9e3
Aaron Christy <aaron@phad.com>
2014-09-25 14:03:50 -0400

    Fix incorrect singular inverse_of declaration

--- app/models/comment_look.rb
+++ app/models/comment_look.rb
@@ -2,7 +2,7 @@ class CommentLook < ActiveRecord::Base
   include EventStreamNotifier
 
   belongs_to :user, inverse_of: :comment_looks
-  belongs_to :comment, inverse_of: :comment_look
+  belongs_to :comment, inverse_of: :comment_looks
   has_one :task, through: :comment
 
   private

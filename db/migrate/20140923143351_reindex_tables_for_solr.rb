e994e0f316ec5550f0786d035e03d9465c646c5b
Aaron Specht Baker + Felix Flores <pairing+asb+ff@edgecase.com>
2014-10-01 09:54:19 -0400

    [#78944478] stop reindex on migration

--- db/migrate/20140923143351_reindex_tables_for_solr.rb
+++
@@ -1,8 +0,0 @@
-class ReindexTablesForSolr < ActiveRecord::Migration
-  def up
-    if Rails.env.development?
-      Rake::Task["sunspot:solr:start"].invoke
-      Rake::Task["sunspot:reindex"].invoke
-    end
-  end
-end

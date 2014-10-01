a572e5948fce81b08c9b01950979c833fb8a0dca
Aaron Specht Baker + Felix Flores <pairing+asb+ff@edgecase.com>
2014-10-01 10:57:57 -0400

    [#79361732] go back to using role kind instead of role name

--- app/services/journal_services/create_default_task_types.rb
+++ app/services/journal_services/create_default_task_types.rb
@@ -6,7 +6,7 @@ module JournalServices
         task_types = TaskType.all
         task_types.each do |task_type|
           jtt = journal.journal_task_types.where(task_type_id: task_type.id).first_or_create
-          jtt.role ||= Role.find_by(kind: task_type.default_role).try(:name)
+          jtt.role ||= task_type.default_role
           jtt.title ||= task_type.default_title
           jtt.save
         end

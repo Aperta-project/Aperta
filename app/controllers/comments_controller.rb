7937fd72879ca0660f37eb0924a74177d571d690
Ryan Wold <pair+ryan@neo.com>
2014-10-02 14:39:52 -0700

    remove code that now lives in comment.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,6 @@ class CommentsController < ApplicationController
   def create
     task = Task.find(params[:comment][:task_id])
     comment = task.comments.create(comment_params)
-    mail_mentioned(comment)
 
     respond_with comment
   end
@@ -19,18 +18,6 @@ class CommentsController < ApplicationController
 
 
   private
-
-  def mail_mentioned(comment)
-    # TODO
-    # read comment body and parse out names
-    # handle duplicate names
-    # send email to each user
-    people_mentioned = [User.first] # Change me
-
-    people_mentioned.each do |mentionee|
-      UserMailer.delay.mention_collaborator(comment.task.assignee.id, mentionee.id, comment.id)
-    end
-  end
 
   def comment_params
     params.require(:comment).permit(:commenter_id, :body)

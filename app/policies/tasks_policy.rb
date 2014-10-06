3a29d4abd555797e19bd0bd09cd90e0527d25f70
Chris Westra chris.westra@neo.com <pairing>
2014-10-06 11:38:04 -0400

    Remove unused tasks polity methods.

--- app/policies/tasks_policy.rb
+++ app/policies/tasks_policy.rb
@@ -18,14 +18,6 @@ class TasksPolicy < ApplicationPolicy
     current_user.admin? || task_owner? || metadata_task_collaborator? || can_view_all_manuscript_managers_for_journal? || can_view_manuscript_manager_for_paper?
   end
 
-  def collaborators?
-    current_user.admin? || task_owner? || metadata_task_collaborator? || has_sufficient_role?
-  end
-
-  def non_collaborators?
-    current_user.admin? || task_owner? || metadata_task_collaborator? || has_sufficient_role?
-  end
-
   def destroy?
     current_user.admin? || task_owner? || metadata_task_collaborator? || can_view_all_manuscript_managers_for_journal? || can_view_manuscript_manager_for_paper?
   end

ed99ee0067a6b55b1d1ff1ea325c16515928e46c
Aaron Christy <aaron@neo.com>
2014-10-07 09:28:31 -0400

    Add id to serializer

--- app/serializers/participation_serializer.rb
+++ app/serializers/participation_serializer.rb
@@ -1,4 +1,5 @@
 class ParticipationSerializer < ActiveModel::Serializer
+  attributes :id
   has_one :participant, serializer: UserSerializer, embed: :ids, include: true, root: :users
   has_one :task, embed: :id, polymorphic: true
 end

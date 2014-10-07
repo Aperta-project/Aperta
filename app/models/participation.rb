a380984d9e592f1af39a09f8a2724f42ac5cb4b0
Aaron Christy <aaron@neo.com>
2014-10-07 09:28:16 -0400

    Add validations on participation

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -4,6 +4,8 @@ class Participation < ActiveRecord::Base
   belongs_to :task, inverse_of: :participations
   belongs_to :participant, class_name: 'User', inverse_of: :participations
 
+  validates :participant_id, presence: true
+
   private
 
   def notifier_payload

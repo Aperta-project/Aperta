857f4288956fd0f9d0f62fc2dd3415295989392c
Rizwan Reza <rizwanreza@gmail.com>
2014-10-08 17:09:43 -0700

    Use ihat's webhook feature

--- app/workers/download_manuscript_worker.rb
+++ app/workers/download_manuscript_worker.rb
@@ -9,12 +9,21 @@ class DownloadManuscriptWorker
 
     epub = EpubConverter.new manuscript.paper, User.first, true
 
-    response = RestClient.post(
-      ENV['IHAT_URL'] + "convert/docx",
-      epub: epub.epub_stream.string,
-      multipart: true
-    )
+    tempfile = Tempfile.new 'epub'
+    tempfile.binmode
+    tempfile.write epub.epub_stream.string
+    tempfile.rewind
 
-    manuscript.paper.update JSON.parse(response.body).symbolize_keys!
+    response = RestClient.post(
+      "http://localhost:3000/jobs", #ihat
+      epub: tempfile,
+      multipart: true,
+      callback_url: "http://localhost:3001/ihat_jobs/:id" #tahi
+    )
+    response_attributes = JSON.parse(response.body).symbolize_keys!
+    IhatJob.create! paper: manuscript.paper, job_id: response_attributes[:jobs]["id"]
+
+  ensure
+    tempfile.unlink
   end
 end

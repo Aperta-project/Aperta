4aa235367a4ae70f4f670c9327cb4e1d7d75119f
Rizwan Reza <rizwanreza@gmail.com>
2014-10-08 17:07:23 -0700

    Use :null_session strategy for CSRF protection

--- app/controllers/ihat_jobs_controller.rb
+++ app/controllers/ihat_jobs_controller.rb
@@ -1,4 +1,6 @@
 class IhatJobsController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def update
     job = IhatJob.find_by(job_id: params[:id])
     PaperUpdateWorker.perform_async(job_id: params[:id])

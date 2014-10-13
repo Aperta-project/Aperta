e85bb722cc39d73f28c6925a201cb6e528d0ce85
Aaron Specht Baker + Aaron Christy <pairing+asb+ac@edgecase.com>
2014-10-13 08:50:44 -0400

    Let prior task assignees fall on the floor

--- db/migrate/20141009132903_move_assignments_to_participations.rb
+++ db/migrate/20141009132903_move_assignments_to_participations.rb
@@ -1,13 +1,5 @@
 class MoveAssignmentsToParticipations < ActiveRecord::Migration
   def up
-    Task.where.not(assignee_id: nil).find_each do |task|
-      unless task.participants.include? task.assignee
-        task.participants << task.assignee
-        task.update_attribute(:assignee, nil)
-        CommentLookManager.sync_task(task)
-      end
-    end
-
     remove_column :tasks, :assignee_id
   end
 

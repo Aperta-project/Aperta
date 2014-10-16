d9c07b59d41ae02757038b4ebf254eef558c5073
Chandu Tennety + Chris Westra <Neo@michaels-imac.neo>
2014-10-16 16:12:20 -0400

    Add another check around assigning reviewers just in case.

--- engines/standard_tasks/spec/features/assign_reviewer_task_spec.rb
+++ engines/standard_tasks/spec/features/assign_reviewer_task_spec.rb
@@ -25,6 +25,7 @@ feature "Assigns Reviewer", js: true do
     manuscript_page = dashboard_page.view_submitted_paper paper
     manuscript_page.view_card task.title do |overlay|
       overlay.paper_reviewers = [albert.full_name, neil.full_name]
+      expect(overlay).to have_no_application_error
       expect(overlay).to have_reviewers(albert, neil)
       # the debounce in the reviewers overlay is causing a race condition between the
       # delayed save and the database truncation during test cleanup.  This will fix it for now.

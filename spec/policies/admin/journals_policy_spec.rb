31121918dfb0bf5adcb698615d98370f1ceb6647
Chandu Tennety + Chris Westra <Neo@michaels-imac.neo>
2014-10-16 12:10:36 -0400

    Correct names in policy spec.

--- spec/policies/admin/journals_policy_spec.rb
+++ spec/policies/admin/journals_policy_spec.rb
@@ -7,7 +7,7 @@ describe Admin::JournalsPolicy do
   context "admin" do
     let(:user) { FactoryGirl.create(:user, :admin) }
 
-    it { expect(policy.authorized?).to be(true) }
+    it { expect(policy.authorization?).to be(true) }
     it { expect(policy.index?).to be(true) }
     it { expect(policy.update?).to be(true) }
   end
@@ -15,7 +15,7 @@ describe Admin::JournalsPolicy do
   context "non admin who does not administer the journal" do
     let(:user) { FactoryGirl.create(:user) }
 
-    it { expect(policy.authorized?).to be(false) }
+    it { expect(policy.authorization?).to be(false) }
     it { expect(policy.index?).to be(false) }
     it { expect(policy.update?).to be(false) }
   end
@@ -27,7 +27,7 @@ describe Admin::JournalsPolicy do
       assign_journal_role(journal, user, :admin)
     end
 
-    it { expect(policy.authorized?).to be(true) }
+    it { expect(policy.authorization?).to be(true) }
     it { expect(policy.index?).to be(true) }
     it { expect(policy.update?).to be(true) }
   end

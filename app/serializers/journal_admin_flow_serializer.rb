fdd399b41932a05cfaebed607fd3e4a80411a1c4
Chandu Tennety + Chris Westra <Neo@michaels-imac.neo>
2014-10-16 12:00:02 -0400

    JournalAdminFlowSerializer < FlowSerializer

--- app/serializers/journal_admin_flow_serializer.rb
+++ app/serializers/journal_admin_flow_serializer.rb
@@ -1,28 +1,8 @@
-class JournalAdminFlowSerializer < ActiveModel::Serializer
-  attributes :id, :title, :empty_text
-  has_many :lite_papers, embed: :ids, include: true, serializer: LitePaperSerializer
-  has_many :tasks, embed: :ids, include: true, root: :card_thumbnails, serializer: CardThumbnailSerializer
-
-  def tasks
-    @tasks ||= flow_map[object.title]
-  end
-
-  def lite_papers
-    Paper.joins(:tasks).includes(:paper_roles).where("tasks.id" => tasks).uniq
-  end
-
+class JournalAdminFlowSerializer < FlowSerializer
   def cached_tasks
     @cached_tasks ||= Task.joins(paper: :journal)
       .assigned_to(current_user).includes(:paper)
       .where(journals: {id: current_user.roles.pluck(:journal_id).uniq })
-  end
-
-  def incomplete_tasks
-    cached_tasks.incomplete
-  end
-
-  def complete_tasks
-    cached_tasks.completed
   end
 
   def paper_admin_tasks
@@ -34,19 +14,10 @@ class JournalAdminFlowSerializer < ActiveModel::Serializer
   end
 
   def unassigned_tasks
-    Task.joins(paper: :journal).includes(:paper)
+    Task.joins(paper: :journal)
       .includes(:paper)
       .incomplete.unassigned
       .where(type: "StandardTasks::PaperAdminTask")
       .where(journals: {id: current_user.roles.pluck(:journal_id).uniq })
-  end
-
-  def flow_map
-    {
-      'Up for grabs' => unassigned_tasks,
-      'My tasks' => incomplete_tasks,
-      'My papers' => paper_admin_tasks,
-      'Done' => complete_tasks
-    }
   end
 end

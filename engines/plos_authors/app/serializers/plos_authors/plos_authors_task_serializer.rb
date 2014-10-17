fe47c1e8bd57f776108f96a4f4a95f6eef39fa06
Aaron Christy <aaron@neo.com>
2014-10-17 15:47:25 -0400

    Move plos_authors subscriptions to the engine; create plos_authors whenever a new author is created

--- engines/plos_authors/app/serializers/plos_authors/plos_authors_task_serializer.rb
+++ engines/plos_authors/app/serializers/plos_authors/plos_authors_task_serializer.rb
@@ -3,9 +3,6 @@ module PlosAuthors
     has_many :plos_authors, embed: :ids, include: true
 
     def plos_authors
-      # generic Authors may have been created in a different task, so
-      # convert them to PlosAuthors before serializing
-      object.convert_generic_authors!
       object.plos_authors
     end
   end

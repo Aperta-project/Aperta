c6ae435a123612b5b63819b95bcba9c1477d2064
Rizwan Reza <rizwanreza@gmail.com>
2014-10-20 13:33:39 -0700

    Set headers in spec/controllers/ihat_jobs_controller_spec

--- spec/controllers/ihat_jobs_controller_spec.rb
+++ spec/controllers/ihat_jobs_controller_spec.rb
@@ -4,6 +4,11 @@ describe IhatJobsController, :type => :controller do
 
   describe "PUT update" do
     subject(:job) { IhatJob.create! job_id: "blah-blah", paper: FactoryGirl.create(:paper) }
+    let(:api_token) { ApiKey.generate! }
+
+    before do
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(api_token)
+    end
 
     it "returns http success" do
       put :update, id: job.job_id

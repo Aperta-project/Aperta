a970c373023bbb4b0b54aaf441f5a5fae64df52c
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2014-10-21 10:34:39 -0400

    Add proper email address format validation

--- engines/plos_authors/app/models/plos_authors/plos_author.rb
+++ engines/plos_authors/app/models/plos_authors/plos_author.rb
@@ -8,6 +8,8 @@ module PlosAuthors
     belongs_to :plos_authors_task, inverse_of: :plos_authors
 
     validates :affiliation, :department, :title, :email, presence: true, if: :task_completed?
+    validates :email, format: { with: Devise.email_regexp, message: "needs to be a valid email address" }, if: :task_completed?
+
 
     def self.for_paper(paper)
       where(paper_id: paper)

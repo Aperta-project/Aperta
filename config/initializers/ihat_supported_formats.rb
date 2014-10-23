6624ca286aed480cdd1f2dbab70a5f465523dbf1
Faun <faun@riseup.net>
2014-10-23 09:04:35 -0700

    WIP

--- config/initializers/ihat_supported_formats.rb
+++ config/initializers/ihat_supported_formats.rb
@@ -1,11 +1,9 @@
-unless Rails.env.test?
+if ENV['IHAT_URL']
   begin
     Tahi::Application.config.ihat_supported_formats = Faraday.get(ENV['IHAT_URL']).body
   rescue Faraday::ConnectionFailed
-    if ENV['IHAT_URL']
-      Rails.logger.warn "Unable to connect to #{ENV['IHAT_URL']}"
-    else
-      Rails.logger.warn "ENV['IHAT_URL'] Not set, falling back to default document types…"
-    end
+    Rails.logger.warn "Unable to connect to #{ENV['IHAT_URL']}"
   end
+else
+  Rails.logger.warn "ENV['IHAT_URL'] Not set, falling back to default document types…"
 end

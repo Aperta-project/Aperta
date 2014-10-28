8a04072ee11c3e7002a3c248e4ac1c70731d8a94
Chris Westra chris.westra@neo.com <pairing>
2014-10-28 06:48:43 -0400

    Rename tasks policy methods

--- app/policies/tasks_policy.rb
+++ app/policies/tasks_policy.rb
@@ -3,32 +3,32 @@ class TasksPolicy < ApplicationPolicy
   include TaskAccessCriteria
 
   def show?
-    authorized_to_modify?
+    authorized_to_modify_task?
   end
 
   def create?
-    authorized_to_create?
+    authorized_to_create_task?
   end
 
   def update?
-    authorized_to_modify?
+    authorized_to_modify_task?
   end
 
   def upload?
-    authorized_to_modify?
+    authorized_to_modify_task?
   end
 
   def destroy?
-    authorized_to_modify?
+    authorized_to_modify_task?
   end
 
   private
-  def authorized_to_modify?
+  def authorized_to_modify_task?
     current_user.site_admin? || can_view_all_manuscript_managers_for_journal? || can_view_manuscript_manager_for_paper? ||
     allowed_manuscript_information_task? || allowed_reviewer_task? || task_participant?
   end
 
-  def authorized_to_create?
+  def authorized_to_create_task?
     current_user.site_admin? || can_view_all_manuscript_managers_for_journal? || can_view_manuscript_manager_for_paper?
   end
 end

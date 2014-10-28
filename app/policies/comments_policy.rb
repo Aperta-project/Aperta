06a0b0d5320a1deff33b35056878637491f5d8ef
Chris Westra chris.westra@neo.com <pairing>
2014-10-28 06:53:34 -0400

    Move authorized methods to mixin

--- app/policies/comments_policy.rb
+++ app/policies/comments_policy.rb
@@ -9,10 +9,4 @@ class CommentsPolicy < ApplicationPolicy
   def create?
     authorized_to_modify_task?
   end
-
-  private
-  def authorized_to_modify_task?
-    current_user.site_admin? || can_view_all_manuscript_managers_for_journal? || can_view_manuscript_manager_for_paper? ||
-    allowed_manuscript_information_task? || metadata_task_collaborator? || allowed_reviewer_task? || task_participant?
-  end
 end
--- app/policies/tasks_policy.rb
+++ app/policies/tasks_policy.rb
@@ -21,14 +21,4 @@ class TasksPolicy < ApplicationPolicy
   def destroy?
     authorized_to_modify_task?
   end
-
-  private
-  def authorized_to_modify_task?
-    current_user.site_admin? || can_view_all_manuscript_managers_for_journal? || can_view_manuscript_manager_for_paper? ||
-    allowed_manuscript_information_task? || metadata_task_collaborator? || allowed_reviewer_task? || task_participant?
-  end
-
-  def authorized_to_create_task?
-    current_user.site_admin? || can_view_all_manuscript_managers_for_journal? || can_view_manuscript_manager_for_paper?
-  end
 end

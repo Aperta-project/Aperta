97d78a287719dae7d039c0b4aee4b47d885c0817
Chris Westra chris.westra@neo.com <pairing>
2014-10-30 07:34:03 -0400

    Use hash condition in where.

--- app/mailers/adhoc_mailer.rb
+++ app/mailers/adhoc_mailer.rb
@@ -3,7 +3,7 @@ class AdhocMailer < ActionMailer::Base
   default from: ENV.fetch('FROM_EMAIL')
 
   def send_adhoc_email(subject, body, user_ids)
-    users = User.where("id IN (?)", user_ids)
+    users = User.where(id: user_ids)
 
     users.each do |user|
       mail(to: user.email, subject: subject, body: body)

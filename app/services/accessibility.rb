5ac0a8455db2186b1be49b160372a88fcfc36cdb
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2014-11-03 16:40:11 -0500

    Accessibility class that will return all users associated to a given resource (i.e., paper/task/etc)

---
+++ app/services/accessibility.rb
@@ -0,0 +1,39 @@
+class Accessibility
+
+  attr_accessor :resource, :action
+
+  def initialize(resource, action=:show)
+    @resource = resource
+    @action = action
+  end
+
+  def users
+    action.present? ? filtered_users(action) : connected_users
+  end
+
+
+  private
+
+  def filtered_users(filter)
+    connected_users.select do |user|
+      policy(user).send("#{action}?")
+    end.uniq
+  end
+
+  def policy(user=nil)
+    #TODO: instead of hardcoding paper:, it should send necessary policy attributes
+    policy_klass.new(current_user: user, paper: resource)
+  end
+
+  def connected_users
+    policy.connected_users
+  end
+
+  def policy_klass
+    klass_name = "#{resource.class.name.pluralize}Policy"
+    klass_name.constantize
+  rescue NameError
+    raise ApplicationPolicy::ApplicationPolicyNotFound, "Could not find #{klass_name} for #{resource}"
+  end
+
+end

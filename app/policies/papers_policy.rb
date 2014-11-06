7651b66929b64ebce9fb0173f5e1b9f6e27a52cf
Aaron Christy <aaron@phad.com>
2014-11-06 16:57:03 -0500

    Enforce primary resource for paper policy

--- app/policies/papers_policy.rb
+++ app/policies/papers_policy.rb
@@ -1,5 +1,5 @@
 class PapersPolicy < ApplicationPolicy
-  require_params :paper
+  primary_resource :paper
 
   def connected_users
     #TODO: consolidate all of these into paper roles, to make this a very efficient query (see pivotal #81129650)

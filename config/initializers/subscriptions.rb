328648d94a09272978c44cdf2d5703ccd4a9e4e3
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2014-11-11 15:12:55 -0500

    Change event_stream_subscriptions -> subscriptions

--- config/initializers/subscriptions.rb
+++ config/initializers/subscriptions.rb
@@ -3,7 +3,6 @@ TahiNotifier.subscribe(
   "comment:created", "comment:updated",
   "task:created", "task:updated",
   "paper:created", "paper:updated",
-  "paper_role:created", "paper_role:updated",
   "author:created", "author:updated",
   "figure:created", "figure:updated",
   "question_attachment:created", "question_attachment:updated") do |subscription_name, payload|
@@ -24,23 +23,4 @@ TahiNotifier.subscribe(
   id         = payload[:id]
 
   EventStream.new(action, klass, id, subscription_name).destroy
-end
-
-
-TahiNotifier.subscribe("paper_role:destroyed") do |subscription_name, payload|
-  user_id  = payload[:user_id]
-  paper_id = payload[:paper_id]
-
-  # only remove paper if the user has no other access to it
-  unless PaperRole.for_user(user_id).pluck(:paper_id).include?(paper_id)
-    EventStream.post_user_event(
-      user_id,
-      { action: "destroyed", lite_papers: [paper_id], subscription_name: subscription_name }.to_json
-    )
-
-    EventStream.post_user_event(
-      user_id,
-      { action: "updateStreams", subscription_name: subscription_name }.to_json
-    )
-  end
 end

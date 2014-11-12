60a78aa2eae72eab83a08a92ef5638a612029c1e
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2014-11-12 13:55:25 -0500

    Update todo to point to open pivotal card

--- app/serializers/lite_paper_serializer.rb
+++ app/serializers/lite_paper_serializer.rb
@@ -5,8 +5,7 @@ class LitePaperSerializer < ActiveModel::Serializer
     scoped_user.paper_roles.where(paper: object).order(created_at: :desc).pluck(:created_at).first
   end
 
-  # TODO: should we modify this to show new task participants on their dashboard?
-  # it only looks at paper_roles right now
+  #TODO: this method does not include a tooltip if user is related to paper by just being a task participant (see pivotal #82588944)
   def roles
     # rocking this in memory because eager-loading
     roles = object.paper_roles.select { |role|

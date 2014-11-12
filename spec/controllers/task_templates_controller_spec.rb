46567caf7101cc0313f78890fb1c550fb126a594
Felix Flores + Jerry Nummi <felixflores+ff+jn@gmail.com>
2014-11-12 13:58:04 -0500

    Fix broken test setup

--- spec/controllers/task_templates_controller_spec.rb
+++ spec/controllers/task_templates_controller_spec.rb
@@ -4,20 +4,28 @@ describe TaskTemplatesController do
   let(:user) { create :user, :site_admin }
   before { sign_in user }
 
-  let(:phase_template) { FactoryGirl.create(:phase_template) }
-  let(:task_template) { FactoryGirl.create(:task_template, phase_template: phase_template) }
   let(:journal) { FactoryGirl.create(:journal) }
+  let(:manuscript_manager_template) { FactoryGirl.create(:manuscript_manager_template,
+                                                         journal: journal) }
+  let(:phase_template) { FactoryGirl.create(:phase_template,
+                                            manuscript_manager_template: manuscript_manager_template) }
+  let(:task_template) { FactoryGirl.create(:task_template,
+                                          phase_template: phase_template,
+                                          journal_task_type: journal.journal_task_types.first) }
   let(:journal_task_type) { journal.journal_task_types.first }
 
   expect_policy_enforcement
 
   it "creates a record" do
-    post :create, format: :json, task_template: { phase_template_id: phase_template.id, journal_task_type_id: journal_task_type.id, title: "Valid Title" }
+    post :create, format: :json, task_template: { phase_template_id: phase_template.id,
+                                                  journal_task_type_id: journal_task_type.id,
+                                                  title: "Valid Title" }
     expect(response.status).to eq(201)
   end
 
   it "updates a record" do
-    put :update, format: :json, id: task_template.id, task_template: { phase_template_id: phase_template.id, journal_task_type_id: journal_task_type.id }
+    put :update, format: :json, id: task_template.id, task_template: { phase_template_id: phase_template.id,
+                                                                       journal_task_type_id: journal_task_type.id }
     expect(response.status).to eq(204)
   end
 

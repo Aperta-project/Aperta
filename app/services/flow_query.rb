8d5344f8c0e73cddb03bdd6df57a23395cb297d4
Chris Westra <chris.westra@neo.com>
2014-11-14 18:44:37 -0500

    Clean up flow query logic.

--- app/services/flow_query.rb
+++ app/services/flow_query.rb
@@ -12,45 +12,10 @@ class FlowQuery
   end
 
   def lite_papers
-    @lite_papers ||= Paper.joins(:tasks).includes(:paper_roles).where("tasks.id" => tasks).uniq
-  end
-
-  def assigned_tasks
-    Task.assigned_to(user).includes(:paper)
-  end
-
-  def paper_admin_tasks_for_user
-    Task.joins(paper: :assigned_users)
-      .includes(:paper)
-      .merge(PaperRole.admins.for_user(user))
-      .where(type: "StandardTasks::PaperAdminTask")
-  end
-
-  def unassigned_tasks
-    scope_to_journals ? unassigned_tasks_for_journals : all_unassigned_tasks
-  end
-
-  def unassigned_tasks_for_journals
-    Task.joins(paper: :journal)
-      .includes(:paper)
-      .incomplete.unassigned
-      .where(type: "StandardTasks::PaperAdminTask")
-      .where(journals: { id: attached_journal_ids })
-  end
-
-  def all_unassigned_tasks
-    Task.joins(:paper)
-      .includes(:paper)
-      .incomplete.unassigned
-      .where(type: "StandardTasks::PaperAdminTask")
-  end
-
-  def attached_journal_ids
-    @ids ||= user.roles.pluck(:journal_id).uniq 
-  end
-
-  def task_includes_paper
-    Task.includes(:paper)
+    @lite_papers ||= Paper.joins(:tasks)
+      .includes(:paper_roles)
+      .where("tasks.id" => tasks)
+      .uniq
   end
 
   private
@@ -63,4 +28,41 @@ class FlowQuery
       'Done' => assigned_tasks.completed
     }
   end
+
+  def assigned_tasks
+    base_query.assigned_to(user)
+  end
+
+  def paper_admin_tasks_for_user
+    admin_tasks
+      .joins(paper: :assigned_users)
+      .merge(PaperRole.admins.for_user(user))
+  end
+
+  def unassigned_tasks
+    scope_to_journals ? unassigned_tasks_for_journals : all_unassigned_tasks
+  end
+
+  def unassigned_tasks_for_journals
+    all_unassigned_tasks
+      .joins(paper: :journal)
+      .where(journals: { id: attached_journal_ids })
+  end
+
+  def all_unassigned_tasks
+    admin_tasks.incomplete.unassigned
+  end
+
+  def admin_tasks
+    base_query.where(type: "StandardTasks::PaperAdminTask")
+  end
+
+  def base_query
+    Task.includes(:paper)
+  end
+
+  def attached_journal_ids
+    @attached_journal_ids ||= user.roles.pluck(:journal_id).uniq 
+  end
+
 end

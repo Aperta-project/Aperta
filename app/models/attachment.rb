f39e8c609296501b4683600821793140f28ea485
Felix Flores + Jerry Nummi <felixflores+ff+jn@gmail.com>
2014-11-17 11:31:11 -0500

    Restrict accepted filetypes

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,9 +2,13 @@ class Attachment < ActiveRecord::Base
   belongs_to :attachable, polymorphic: true
   mount_uploader :file, AdhocAttachmentUploader
 
-  IMAGE_TYPES = %w{jpg jpeg tiff tif gif png eps}
+  IMAGE_TYPES = %w{.jpg jpeg tiff tif gif png eps docx odt epub pdf tif}
 
   def image?
-    IMAGE_TYPES.include? file.file.extension
+    if file.file
+      IMAGE_TYPES.include? file.file.extension
+    else
+      false
+    end
   end
 end

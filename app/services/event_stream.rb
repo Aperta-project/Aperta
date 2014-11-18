54c4943302bf245703b4b2054fb0a9e7367a5145
Aaron Christy + Tony Schneider <pairing+ac+ts@edgecase.com>
2014-11-18 15:47:46 -0500

    extract common

--- app/services/event_stream.rb
+++ app/services/event_stream.rb
@@ -18,25 +18,13 @@ class EventStream
 
   def destroy
     channel = EventStreamConnection::SYSTEM_CHANNEL_NAME
-    payload = {
-      action: "destroyed",
-      type: record.class.base_class.name.demodulize.tableize,
-      ids: [record.id],
-      subscription_name: subscription_name
-    }.to_json
-    EventStreamConnection.post_event(channel, payload)
+    EventStreamConnection.post_event(channel, destroyed_payload)
   end
 
   def destroy_for(user)
     if Accessibility.new(record).disconnected?(user)
       channel = EventStreamConnection.channel_name(User, user.id)
-      payload = {
-        action: "destroyed",
-        type: record.class.base_class.name.demodulize.tableize,
-        ids: [record.id],
-        subscription_name: subscription_name
-      }.to_json
-      EventStreamConnection.post_event(channel, payload)
+      EventStreamConnection.post_event(channel, destroyed_payload)
     end
   end
 
@@ -45,5 +33,13 @@ class EventStream
   def payload_for(user)
     serializer = record.event_stream_serializer(user)
     serializer.as_json.merge(action: action, subscription_name: subscription_name).to_json
+  end
+
+  def destroyed_payload
+    { action: "destroyed",
+      type: record.class.base_class.name.demodulize.tableize,
+      ids: [record.id],
+      subscription_name: subscription_name
+    }.to_json
   end
 end

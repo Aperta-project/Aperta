1d73e5658732829b57c3d38a5c3b0a2c6c44a935
Felix Flores <felixflores@gmail.com>
2014-12-08 16:32:27 -0500

    add test coverage to user_flow_controller

--- spec/controllers/user_flows_controller_spec.rb
+++ spec/controllers/user_flows_controller_spec.rb
@@ -7,18 +7,39 @@ describe UserFlowsController do
   let(:user) { FactoryGirl.create(:user) }
   before { sign_in user }
 
-  describe "#update" do
+  describe "#index" do
     let!(:flow) { FactoryGirl.create(:flow, title: "My tasks") }
     let!(:user_flow) { FactoryGirl.create(:user_flow, user: user, flow_id: flow.id) }
 
-    context "title does not map to a template" do
+    context "returns a list of user_flows" do
       let(:new_title) { "Something that does not match" }
 
-      it "returns head 200" do
-        put :update, { format: 'json', id: flow.id, user_flow: { title: new_title } }
-        expect(response.status).to eq(200)
-        expect(Flow.find(flow.id).title).to eq(new_title)
+      before do
+        get :index, format: :json
       end
+
+      it "returns head 200" do
+        expect(response.status).to eq(200)
+      end
+
+      it "appends the meta information for flows" do
+        expect(JSON.parse(response.body)["meta"]["flows"][0]["title"]).to eq("My tasks")
+      end
+    end
+  end
+
+  describe "#create" do
+    let!(:flow) { FactoryGirl.create(:flow, title: "My tasks") }
+    let!(:user_flow) { FactoryGirl.create(:user_flow, user: user, flow_id: flow.id) }
+
+    it "returns a serialized user_flow" do
+      post :create, user_flow: {flow_id: flow.id}
+      expect(JSON.parse(response.body)["user_flows"]).to_not be_nil
+    end
+
+    it "associates a flow with the current_user" do
+      post :create, user_flow: {flow_id: flow.id}
+      expect(user.flows).to include(flow)
     end
   end
 end

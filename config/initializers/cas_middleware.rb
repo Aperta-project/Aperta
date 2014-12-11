9a4cbc6959010f59df32ec37df00e72ca2766040
Erik Hetzner <ehetzner@plos.org>
2014-12-11 15:07:42 -0800

    allow configuration of CAS via envvar

--- config/initializers/cas_middleware.rb
+++ config/initializers/cas_middleware.rb
@@ -1,4 +1,19 @@
-CAS_CONFIG = YAML.load_file("#{Rails.root}/config/cas.yml")[Rails.env]
+CAS_CONFIG = if ENV['CAS_HOST']
+               retval = {
+                 # booleans
+                 'ssl'                      => ENV['CAS_SSL'].present?,
+                 'disable_ssl_verification' => ENV['CAS_DISABLE_SSL_VERIFICATION'].present?
+               }
+               %w(host port service_validate_url callback_url logout_url
+                  login_url uid_field ca_path).each do |key|
+                 val = ENV["CAS_#{key.upcase}"]
+                 retval[key] = val if val
+               end
+               retval
+             else
+               YAML.load_file(File.join(Rails.root, 'config', 'cas.yml'))[Rails.env]
+             end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cas, CAS_CONFIG
 end

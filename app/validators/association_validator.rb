49c9a12e65972f7ba676e69a75a1458b1a01bc52
Faun <github@faun.me>
2014-12-18 14:07:28 -0800

    Refactor AssociationValidator

--- app/validators/association_validator.rb
+++ app/validators/association_validator.rb
@@ -1,23 +1,22 @@
 class AssociationValidator < ActiveModel::Validator
+  attr_reader :record
+
   def validate(record)
-    record.errors[association].clear # remove generic "is invalid" messages
+    @record = record
 
-    errors = record.send(association).each_with_object({}) do |associated, errors|
-      if associated.invalid?
-        errors[associated.id] = associated.errors
-      end
-    end
-
-    if errors.any?
-      record.errors.set(association, errors)
-      record.send(failure_callback) if failure_callback.present?
-    end
-
-    errors.empty?
+    remove_invalid_messages
+    record.errors.set(association, association_errors) if association_errors.any?
+    record.send(failure_callback) if failure_callback.present?
+    association_errors.empty?
   end
 
-
   private
+
+  def association_errors
+    @association_errors ||= record.send(association).each_with_object({}) { |associated, errors|
+      errors[associated.id] = associated.errors if associated.invalid?
+    }
+  end
 
   def association
     options[:association]
@@ -25,5 +24,9 @@ class AssociationValidator < ActiveModel::Validator
 
   def failure_callback
     options[:fail]
+  end
+
+  def remove_invalid_messages
+    record.errors[association].clear # remove generic "is invalid" messages
   end
 end

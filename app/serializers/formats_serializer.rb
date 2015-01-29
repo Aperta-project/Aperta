15dab37439bea72118886dbbf58605d135daa4f2
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2015-01-29 18:02:35 -0500

    Simplify supported formats

--- app/serializers/formats_serializer.rb
+++ app/serializers/formats_serializer.rb
@@ -1,31 +1,8 @@
 class FormatsSerializer < ActiveModel::Serializer
-  attr_accessor :export_formats, :import_formats
   def attributes
-    if Tahi::Application.config.ihat_supported_formats
-      {
-        export_formats: export_formats,
-        import_formats: import_formats
-      }
-    else
-      nil
-    end
-  end
-
-  private
-
-  def formats
-    JSON.parse Tahi::Application.config.ihat_supported_formats
-  end
-
-  def export_formats
-    pluck_format formats['export_formats']
-  end
-
-  def import_formats
-    pluck_format formats['import_formats']
-  end
-
-  def pluck_format(formats_hash)
-    formats_hash.map { |entry| { format: entry['format'] } }
+    {
+      import_formats: Tahi::Application.config.ihat_supported_import_formats,
+      export_formats: Tahi::Application.config.ihat_supported_export_formats
+    }
   end
 end

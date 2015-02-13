4f24d2c8d7c881706f01cc5f0dad85546e5282a2
Anna Petry & Rizwan Reza <pair+anna.petry+riz@neo.com>
2015-02-12 17:02:38 -0800

    Refactor benchmark_spec

--- spec/requests/performance/flow_manager/benchmark_spec.rb
+++ spec/requests/performance/flow_manager/benchmark_spec.rb
@@ -3,197 +3,72 @@ require 'benchmark'
 
 TEST_NAME = "flow_manager"
 
-describe UserFlowsController, performance: true do
-  let(:path) { BenchmarkSuite.path(TEST_NAME) }
+shared_examples "a batch of papers" do |num_papers|
+  describe UserFlowsController do
+    let(:path) { BenchmarkSuite.path(TEST_NAME) }
 
-  before(:all) { VCR.turn_off! && WebMock.allow_net_connect! }
-  after(:all)  { VCR.turn_on! && WebMock.disable_net_connect! }
+    before(:all) { VCR.turn_off! && WebMock.allow_net_connect! }
+    after(:all)  { VCR.turn_on! && WebMock.disable_net_connect! }
 
-  context "performance of 100 papers" do
-    before(:all) do
-      @num_papers = 100
-      @bench = BenchmarkSuite::FlowManager.new num_papers: @num_papers
-    end
-
-    after(:all) do
-      BenchmarkSuite::Emailer.call(TEST_NAME)
-    end
-
-    context "the user is a site admin" do
-      before do
-        post user_session_path, user: {:login => @bench.site_admin.email, :password => 'password'}
+    context "performance of 100 papers" do
+      before(:all) do
+        @bench = BenchmarkSuite::FlowManager.new num_papers: num_papers
       end
 
-      it "takes time" do
-        time = Benchmark.realtime {
-          get '/user_flows.json'
-        }
-        BenchmarkSuite::Results.new(test_name: TEST_NAME,
-                                    duration: time,
-                                    unit: :sec,
-                                    title: "#{@num_papers}, 100%").write
-        expect(time).to be < 1.5
-      end
-    end
+      let(:time) { Benchmark.realtime { get '/user_flows.json' } }
 
-    context "when the user is an admin of big journal" do
-      before do
-        post user_session_path, user: {:login => @bench.big_admin.email, :password => 'password'}
-      end
+      context "the user is a site admin" do
+        before do
+          post user_session_path, user: {:login => @bench.site_admin.email, :password => 'password'}
+        end
 
-      it "takes time" do
-        time = Benchmark.realtime {
-          get '/user_flows.json'
-        }
-        BenchmarkSuite::Results.new(test_name: TEST_NAME,
-                                    duration: time,
-                                    unit: :sec,
-                                    title: "#{@num_papers}, 80%").write
-        expect(time).to be < 1.5
-      end
-    end
 
-    context "when the user is an admin of small journal" do
-      before do
-        post user_session_path, user: {:login => @bench.small_admin.email, :password => 'password'}
+        it "takes time" do
+          BenchmarkSuite::Results.new(test_name: TEST_NAME,
+                                      duration: time,
+                                      unit: :sec,
+                                      title: "#{num_papers}, 100%").write
+          expect(time).to be < 1.5
+        end
       end
 
-      it "takes time" do
-        time = Benchmark.realtime {
-          get '/user_flows.json'
-        }
-        BenchmarkSuite::Results.new(test_name: TEST_NAME,
-                                    duration: time,
-                                    unit: :sec,
-                                    title: "#{@num_papers}, 20%").write
-        expect(time).to be < 1.5
+      context "when the user is an admin of big journal" do
+        before do
+          post user_session_path, user: {:login => @bench.big_admin.email, :password => 'password'}
+        end
+
+        it "takes time" do
+          BenchmarkSuite::Results.new(test_name: TEST_NAME,
+                                      duration: time,
+                                      unit: :sec,
+                                      title: "#{num_papers}, 80%").write
+          expect(time).to be < 1.5
+        end
+      end
+
+      context "when the user is an admin of small journal" do
+        before do
+          post user_session_path, user: {:login => @bench.small_admin.email, :password => 'password'}
+        end
+
+        it "takes time" do
+          BenchmarkSuite::Results.new(test_name: TEST_NAME,
+                                      duration: time,
+                                      unit: :sec,
+                                      title: "#{num_papers}, 20%").write
+          expect(time).to be < 1.5
+        end
       end
     end
   end
+end
 
+describe "batch of papers" do
+  it_should_behave_like "a batch of papers", 5
+  it_should_behave_like "a batch of papers", 1
+  it_should_behave_like "a batch of papers", 10
 
-  context "performance of 1_000 papers" do
-    before(:all) do
-      @num_papers = 1_000
-      @bench = BenchmarkSuite::FlowManager.new num_papers: @num_papers
-    end
-
-    after(:all) do
-      BenchmarkSuite::Emailer.call(TEST_NAME)
-    end
-
-    context "the user is a site admin" do
-      before do
-        post user_session_path, user: {:login => @bench.site_admin.email, :password => 'password'}
-      end
-
-      it "takes time" do
-        time = Benchmark.realtime {
-          get '/user_flows.json'
-        }
-        BenchmarkSuite::Results.new(test_name: TEST_NAME,
-                                    duration: time,
-                                    unit: :sec,
-                                    title: "#{@num_papers}, 100%").write
-        expect(time).to be < 1.5
-      end
-    end
-
-    context "when the user is an admin of big journal" do
-      before do
-        post user_session_path, user: {:login => @bench.big_admin.email, :password => 'password'}
-      end
-
-      it "takes time" do
-        time = Benchmark.realtime {
-          get '/user_flows.json'
-        }
-        BenchmarkSuite::Results.new(test_name: TEST_NAME,
-                                    duration: time,
-                                    unit: :sec,
-                                    title: "#{@num_papers}, 80%").write
-        expect(time).to be < 1.5
-      end
-    end
-
-    context "when the user is an admin of small journal" do
-      before do
-        post user_session_path, user: {:login => @bench.small_admin.email, :password => 'password'}
-      end
-
-      it "takes time" do
-        time = Benchmark.realtime {
-          get '/user_flows.json'
-        }
-        BenchmarkSuite::Results.new(test_name: TEST_NAME,
-                                    duration: time,
-                                    unit: :sec,
-                                    title: "#{@num_papers}, 20%").write
-        expect(time).to be < 1.5
-      end
-    end
+  after(:all) do
+    BenchmarkSuite::Emailer.call(TEST_NAME)
   end
-
-  context "performance of 10_000 papers" do
-    before(:all) do
-      @num_papers = 10_000
-      @bench = BenchmarkSuite::FlowManager.new num_papers: @num_papers
-    end
-
-    after(:all) do
-      BenchmarkSuite::Emailer.call(TEST_NAME)
-    end
-
-    context "the user is a site admin" do
-      before do
-        post user_session_path, user: {:login => @bench.site_admin.email, :password => 'password'}
-      end
-
-      it "takes time" do
-        time = Benchmark.realtime {
-          get '/user_flows.json'
-        }
-        BenchmarkSuite::Results.new(test_name: TEST_NAME,
-                                    duration: time,
-                                    unit: :sec,
-                                    title: "#{@num_papers}, 100%").write
-        expect(time).to be < 1.5
-      end
-    end
-
-    context "when the user is an admin of big journal" do
-      before do
-        post user_session_path, user: {:login => @bench.big_admin.email, :password => 'password'}
-      end
-
-      it "takes time" do
-        time = Benchmark.realtime {
-          get '/user_flows.json'
-        }
-        BenchmarkSuite::Results.new(test_name: TEST_NAME,
-                                    duration: time,
-                                    unit: :sec,
-                                    title: "#{@num_papers}, 80%").write
-        expect(time).to be < 1.5
-      end
-    end
-
-    context "when the user is an admin of small journal" do
-      before do
-        post user_session_path, user: {:login => @bench.small_admin.email, :password => 'password'}
-      end
-
-      it "takes time" do
-        time = Benchmark.realtime {
-          get '/user_flows.json'
-        }
-        BenchmarkSuite::Results.new(test_name: TEST_NAME,
-                                    duration: time,
-                                    unit: :sec,
-                                    title: "#{@num_papers}, 20%").write
-        expect(time).to be < 1.5
-      end
-    end
-  end
-
 end

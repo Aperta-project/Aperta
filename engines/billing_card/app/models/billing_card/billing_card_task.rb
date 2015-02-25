2b1a3307f96d3d37eadeb596d52ab53f5348c76f
David Trejo & Faun Winter <pair+david.trejo+faun.winter@neo.com>
2015-02-25 11:32:59 -0800

    billing card: update name to `Billing`

--- engines/billing_card/app/models/billing_card/billing_card_task.rb
+++ engines/billing_card/app/models/billing_card/billing_card_task.rb
@@ -3,7 +3,7 @@ module BillingCard
     # uncomment the following line if you want to enable event streaming for this model
     # include EventStreamNotifier
 
-    register_task default_title: "BillingCard Task", default_role: "author"
+    register_task default_title: "Billing", default_role: "author"
 
     def active_model_serializer
       TaskSerializer

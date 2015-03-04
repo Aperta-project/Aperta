1f5b244a44decbb39c171d550dfb899ae0e8ca20
Feifan Wang and Ryan Wold <pair+feifan+ryan.wold@neo.com>
2015-03-03 16:19:20 -0800

    load correct section on page load

--- client/lib/billing/app/views/overlays/billing.js
+++ client/lib/billing/app/views/overlays/billing.js
@@ -3,5 +3,10 @@ import OverlayView from 'tahi/views/overlay';
 export default OverlayView.extend({
   templateName: 'overlays/billing',
   layoutName:   'layouts/overlay',
-  cardName: 'billing'
+  cardName: 'billing',
+
+  didInsertElement: function() {
+    var choice = $(".payment-method .select2-container").select2("val");
+    this.get("controller").set("selectedPaymentMethod", choice);
+  }
 });

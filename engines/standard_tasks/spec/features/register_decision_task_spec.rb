482aee98f7d4acaca841494d86c4cecfd559c720
Ryan Wold and Tad Park <pair+ryan.wold+tadp@neo.com>
2015-03-09 16:58:46 -0700

    WIP: failing test

--- engines/standard_tasks/spec/features/register_decision_task_spec.rb
+++ engines/standard_tasks/spec/features/register_decision_task_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature "Register Decision", js: true do
+feature "Register Decision", js: true, selenium: true do
 
   let(:user) { FactoryGirl.create(:user) }
   let(:task) { FactoryGirl.create(:register_decision_task) }
@@ -24,6 +24,22 @@ feature "Register Decision", js: true do
       overlay.decision_letter = "Accepting this because I can"
       overlay.mark_as_complete
       expect(overlay).to be_completed
+    end
+  end
+
+  scenario "Completed card cannot be modified" do
+    dashboard_page = DashboardPage.new
+    manuscript_page = dashboard_page.view_submitted_paper paper
+    manuscript_page.view_card 'Register Decision' do |overlay|
+      overlay.register_decision = "Accepted"
+      overlay.decision_letter = "Accepting this because I can"
+      overlay.mark_as_complete
+      check "Completed"
+      expect(overlay).to be_completed
+
+      # assert the overlay is not editable
+      overlay.register_decision = "Rejected" # should not be able to select
+      expect(find(".decision-label input[value='accepted']")[:checked]).to eq "true"
     end
   end
 end

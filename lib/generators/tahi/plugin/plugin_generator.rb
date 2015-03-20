5629627a1094ca5d7fcb167784d3790d2dd1de8f
Rizwan Reza <rizwanreza@gmail.com>
2015-03-19 17:40:57 -0700

    Refactor plugin generator

--- lib/generators/tahi/plugin/plugin_generator.rb
+++ lib/generators/tahi/plugin/plugin_generator.rb
@@ -4,17 +4,21 @@ module Tahi
     argument :path, type: :string, required: false
 
     def generate
-      plugin_dir = path || File.join(Rails.root, 'engines', name.underscore)
+      system("rails plugin new #{plugin_path} --full --mountable --skip-test-unit --skip-gemfile-entry")
 
-      system("rails plugin new #{plugin_dir} --full --mountable --skip-test-unit --skip-gemfile-entry")
-      # might need to cleanup a few excess, non-used files
-
-      client_dir = File.join(plugin_dir, 'client')
-      template 'index.js', File.join(client_dir, 'index.js')
-      template 'package.json', File.join(client_dir, 'package.json')
+      template 'index.js', File.join(client_path, 'index.js')
+      template 'package.json', File.join(client_path, 'package.json')
     end
 
     private
+
+    def client_path
+      plugin_path + "/client"
+    end
+
+    def plugin_path
+      path || Rails.root.join('engines', name.underscore)
+    end
 
     def dasherized_module_name
       name.dasherize

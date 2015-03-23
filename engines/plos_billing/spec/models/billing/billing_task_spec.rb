4343a86f2e662bc6ea843d6ea3e90b73ce40c6fe
Rizwan Reza and Ryan Wold <pair+riz+ryan.wold@neo.com>
2015-03-23 13:42:06 -0700

    Align and remove whitespace

--- engines/plos_billing/spec/models/billing/billing_task_spec.rb
+++ engines/plos_billing/spec/models/billing/billing_task_spec.rb
@@ -5,21 +5,21 @@ module PlosBilling
     let(:paper) { FactoryGirl.create(:paper, :with_tasks) }
     let(:billing_task) do
       ::PlosBilling::BillingTask.create! completed: true,
-        phase: paper.phases.first,
-        title: "Billing",
-        role: "author"
+                                         phase: paper.phases.first,
+                                         title: "Billing",
+                                         role: "author"
     end
+
     describe '.create' do
       it "creates it" do
         expect(billing_task).to_not be_nil
       end
-
     end
+
     describe '#active_model_serializer' do
       it 'has the proper serializer' do
         expect(billing_task.active_model_serializer).to eq PlosBilling::TaskSerializer
       end
     end
-
   end
 end

13b7380b1717082a580cc09c88601c3c375452ba
Rizwan Reza and Ryan Wold <pair+riz+ryan.wold@neo.com>
2015-03-23 13:35:14 -0700

    :dog: silence!

--- client/app/pods/components/question-radio/component.js
+++ client/app/pods/components/question-radio/component.js
@@ -1,10 +1,10 @@
-import Ember from 'ember';
-import QuestionComponent from 'tahi/pods/components/question/component';
+import Ember from "ember";
+import QuestionComponent from "tahi/pods/components/question/component";
 
 export default QuestionComponent.extend({
-  displayContent: Ember.computed.oneWay('selectedYes'),
+  displayContent: Ember.computed.oneWay("selectedYes"),
   yesLabel: "Yes",
   noLabel: "No",
-  selectedYes: Ember.computed.equal('model.answer', 'Yes'),
-  selectedNo:  Ember.computed.equal('model.answer', 'No')
+  selectedYes: Ember.computed.equal("model.answer", "Yes"),
+  selectedNo:  Ember.computed.equal("model.answer", "No")
 });

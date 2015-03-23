aafed03b24beeb42eb2cf3936db6df7d76bdb607
Rizwan Reza and Ryan Wold <pair+riz+ryan.wold@neo.com>
2015-03-23 16:43:57 -0700

    Change flaky spec to actually be run as flaky

--- engines/tahi_upload_manuscript/spec/features/upload_paper_spec.rb
+++ engines/tahi_upload_manuscript/spec/features/upload_paper_spec.rb
@@ -10,7 +10,7 @@ feature "Upload paper", js: true, selenium: true do
     sign_in_page.sign_in author
   end
 
-  skip "Author uploads paper in Word format" do
+  scenario "Author uploads paper in Word format", flaky: true do
     expect(DownloadManuscriptWorker).to receive(:perform_async) do |manuscript_id, url|
       paper.manuscript.update(status: "done")
       paper.update(title: "This is a Title About Turtles", body: "And this is my subtitle")

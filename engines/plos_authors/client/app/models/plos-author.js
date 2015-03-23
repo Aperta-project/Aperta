27ac2cab24716d5ce520cbd0f2ce69a380d75f24
Rizwan Reza and Ryan Wold <pair+riz+ryan.wold@neo.com>
2015-03-23 15:35:57 -0700

    merge PlosAuthors ember-cli addon into PlosAuthors engine

---
+++ engines/plos_authors/client/app/models/plos-author.js
@@ -0,0 +1,26 @@
+import DS from 'ember-data';
+
+var a = DS.attr;
+
+export default DS.Model.extend({
+  paper: DS.belongsTo('paper'),
+  plosAuthorsTask: DS.belongsTo('plosAuthorsTask'),
+
+  qualifiedType: 'PlosAuthors::PlosAuthor',
+
+  firstName: a('string'),
+  middleInitial: a('string'),
+  lastName: a('string'),
+  email: a('string'),
+  title: a('string'),
+  department: a('string'),
+  affiliation: a('string'),
+  secondaryAffiliation: a('string'),
+  corresponding: a('boolean'),
+  deceased: a('boolean'),
+  position: a('number'),
+
+  fullName: function() {
+    return [this.get('firstName'), this.get('middleInitial'), this.get('lastName')].compact().join(' ');
+  }.property('firstName', 'middleInitial', 'lastName')
+});
---
+++ engines/plos_authors/client/app/models/plos-authors-task.js
@@ -0,0 +1,8 @@
+import DS   from 'ember-data';
+import Task from 'tahi/models/task';
+
+export default Task.extend({
+  plosAuthors: DS.hasMany('plosAuthor'),
+  qualifiedType: 'StandardTasks::PlosAuthorsTask',
+  isMetadataTask: true
+});

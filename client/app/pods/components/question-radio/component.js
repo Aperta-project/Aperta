89e9366109ce6bab7163d254d67d41d7e5ed89fd
Rizwan Reza and Ryan Wold <pair+riz+ryan.wold@neo.com>
2015-03-23 13:32:09 -0700

    add yes/no label support for question-radio

--- client/app/pods/components/question-radio/component.js
+++ client/app/pods/components/question-radio/component.js
@@ -3,6 +3,8 @@ import QuestionComponent from 'tahi/pods/components/question/component';
 
 export default QuestionComponent.extend({
   displayContent: Ember.computed.oneWay('selectedYes'),
+  yesLabel: "Yes",
+  noLabel: "No",
   selectedYes: Ember.computed.equal('model.answer', 'Yes'),
   selectedNo:  Ember.computed.equal('model.answer', 'No')
 });

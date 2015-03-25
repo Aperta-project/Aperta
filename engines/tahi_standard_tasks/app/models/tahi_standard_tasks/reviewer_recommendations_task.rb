d89c6d01bc2606b5738f1a01a4951a0a6546598e
Feifan Wang <feifanw@gmail.com>
2015-03-25 11:27:58 -0700

    update default task name

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/reviewer_recommendations_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/reviewer_recommendations_task.rb
@@ -3,7 +3,7 @@ module TahiStandardTasks
     # uncomment the following line if you want to enable event streaming for this model
     # include EventStreamNotifier
 
-    register_task default_title: "ReviewerRecommendations Task", default_role: "author"
+    register_task default_title: "Reviewer Recommendations", default_role: "author"
 
     def active_model_serializer
       TaskSerializer

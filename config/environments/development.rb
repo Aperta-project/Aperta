a9ac2ff6da9ed81fd8f444b8f87fd89ccc7005da
Hildebrando Rueda <harq.soft@gmail.com>
2015-03-25 22:00:12 -0700

    Move production smtp settings to production.rb file
    Also remove the delivery_method in development.rb file since is the
    default value

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,6 @@ Tahi::Application.configure do
   config.assets.debug = false
 
   # Mailcatcher configuration
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
 
   # defaults to local storage
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,15 @@ Tahi::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.carrierwave_storage = :fog
+
+  # Sendgrid Configuration
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
 end

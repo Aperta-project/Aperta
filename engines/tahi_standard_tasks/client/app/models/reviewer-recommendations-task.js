a419fee7c7d3c8c37545fb526d5131d279283b25
Feifan Wang <feifanw@gmail.com>
2015-03-26 14:53:33 -0700

    fetch the correct institutions

--- engines/tahi_standard_tasks/client/app/models/reviewer-recommendations-task.js
+++ engines/tahi_standard_tasks/client/app/models/reviewer-recommendations-task.js
@@ -1,4 +1,11 @@
 import DS   from 'ember-data';
 import Task from 'tahi/models/task';
 
-export default Task.extend({});
+export default Task.extend({
+  institutionNames: DS.attr('string'),
+  institutions: function() {
+    return (this.get('institutionNames').split(',')).map(function(institution) {
+      return { id: institution, text: institution };
+    });
+  }.property('institutionNames')
+});

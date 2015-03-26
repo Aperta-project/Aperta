bf6944f4e68950fead5abc73588c700bf99245a3
Hildebrando Rueda <harq.soft@gmail.com>
2015-03-25 23:42:11 -0700

    raise exception is ENV keys are not set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Tahi::Application.configure do
     address: 'smtp.sendgrid.net',
     port: '587',
     authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: ENV.fetch('SENDGRID_USERNAME'),
+    password: ENV.fetch('SENDGRID_PASSWORD'),
     domain: 'heroku.com',
     enable_starttls_auto: true
   }

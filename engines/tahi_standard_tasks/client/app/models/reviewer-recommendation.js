fad851dd7f4c85bdc695069444fce9dac04b64e1
Feifan Wang <feifanw@gmail.com>
2015-03-31 11:55:27 -0700

    ember model for reviewer rec

---
+++ engines/tahi_standard_tasks/client/app/models/reviewer-recommendation.js
@@ -0,0 +1,20 @@
+import DS from 'ember-data';
+
+var a = DS.attr;
+
+export default DS.Model.extend({
+  // qualifiedType: 'TahiStandardTask::ReviewerRecommendation',
+
+  firstName: a('string'),
+  middleInitial: a('string'),
+  lastName: a('string'),
+  email: a('string'),
+  title: a('string'),
+  department: a('string'),
+  affiliation: a('string'),
+  recommendOrOppose: a('string'),
+
+  fullName: function() {
+    return [this.get('firstName'), this.get('middleInitial'), this.get('lastName')].compact().join(' ');
+  }.property('firstName', 'middleInitial', 'lastName')
+});
--- engines/tahi_standard_tasks/client/app/models/reviewer-recommendations-task.js
+++ engines/tahi_standard_tasks/client/app/models/reviewer-recommendations-task.js
@@ -2,6 +2,7 @@ import DS   from 'ember-data';
 import Task from 'tahi/models/task';
 
 export default Task.extend({
+  reviewerRecommendations: DS.hasMany('reviewerRecommendation'),
   institutionNames: DS.attr('string'),
   institutions: function() {
     return (this.get('institutionNames').split(',')).map(function(institution) {

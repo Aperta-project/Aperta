1181212b549eda7d505a184dd5fd94e9f8e26c0b
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2015-04-01 15:07:55 -0400

    schema.rb did not get checked in?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325175514) do
+ActiveRecord::Schema.define(version: 20150401180824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -271,6 +271,7 @@ ActiveRecord::Schema.define(version: 20150325175514) do
     t.string   "secondary_affiliation", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "contributions"
   end
 
   create_table "question_attachments", force: :cascade do |t|

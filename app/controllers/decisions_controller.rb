f5d47587dd5289ead3975b61636dc6cd7de66de6
Rizwan Reza and Ryan Wold <pair+riz+ryan.wold@neo.com>
2015-04-02 17:50:16 -0700

    add DecisionController specs

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -1,15 +1,15 @@
 class DecisionsController < ApplicationController
 
   def create
-    paper = Paper.find(params[:decision][:paper_id])
-    decision = paper.decisions.create!(decision_params)
-    render json: decision, serializer: DecisionSerializer, root: 'decision'
+    @paper = Paper.find(params[:decision][:paper_id])
+    @decision = @paper.decisions.create!(decision_params)
+    render json: @decision, serializer: DecisionSerializer, root: 'decision'
   end
 
   def update
-    decision = Decision.find(params[:id])
-    decision.update! decision_params
-    render json: decision, serializer: DecisionSerializer, root: 'decision'
+    @decision = Decision.find(params[:id])
+    @decision.update! decision_params
+    render json: @decision, serializer: DecisionSerializer, root: 'decision'
   end
 
   def decision_params

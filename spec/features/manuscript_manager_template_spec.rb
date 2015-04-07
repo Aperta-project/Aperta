47ac3744bedefc7e70228275523f5c5b202e1870
James Martinez + Tony Schneider <pairing+jm+ts@edgecase.com>
2015-04-07 12:08:21 -0400

    Rollup mmt manage features into one scenario [ci skip]

--- spec/features/manuscript_manager_template_spec.rb
+++ spec/features/manuscript_manager_template_spec.rb
@@ -16,47 +16,36 @@ feature "Manuscript Manager Templates", js: true, selenium: true do
     end
   end
 
-  describe "Adding phases" do
-    scenario "Adding a phase" do
-      journal_page = JournalPage.new
-      mmt_page = journal_page.add_new_template
-      expect(mmt_page).to have_phase_names('Phase 1', 'Phase 2', 'Phase 3')
+  scenario "Managing the manuscript manager template" do
+    journal_page = JournalPage.new
+    mmt_page = journal_page.add_new_template
+    mmt_page.paper_type = "Test Type"
+    expect(mmt_page).to have_phase_names('Phase 1', 'Phase 2', 'Phase 3')
 
-      phase = mmt_page.find_phase 'Phase 1'
-      phase.rename 'F1rst Ph4ze'
-      expect { phase.add_phase }.to change { mmt_page.phases.count }.by(1)
-    end
-  end
+    # adding a phase template
+    phase = mmt_page.find_phase 'Phase 1'
+    phase.rename 'F1rst Ph4ze'
+    expect { phase.add_phase }.to change { mmt_page.phases.count }.by(1)
 
-  describe "Adding cards" do
-    scenario "Adding a card" do
-      journal_page = JournalPage.new
-      mmt_page = journal_page.add_new_template
-      mmt_page.paper_type = "Test Type"
-      phase = mmt_page.find_phase 'Phase 1'
-      phase.new_card overlay: ChooseCardTypeOverlay, card_type: "Reviewer Report"
-      expect(phase).to have_card("Reviewer Report")
-      expect(mmt_page).to have_content("SAVE TEMPLATE")
-      mmt_page.save
-      expect(mmt_page).to have_no_content("SAVE TEMPLATE")
-      expect(page.current_url).to match(%r{/admin/journals/\d+/manuscript_manager_templates/\d+/edit})
-      expect(mmt_page).to have_no_application_error
-    end
+    # adding a card template
+    phase = mmt_page.find_phase 'Phase 2'
+    phase.new_card overlay: ChooseCardTypeOverlay, card_type: "Reviewer Report"
+    expect(phase).to have_card("Reviewer Report")
+    expect(mmt_page).to have_content("SAVE TEMPLATE")
+    mmt_page.save
+    expect(mmt_page).to have_no_content("SAVE TEMPLATE")
+    expect(page.current_url).to match(%r{/admin/journals/\d+/manuscript_manager_templates/\d+/edit})
+    expect(mmt_page).to have_no_application_error
 
-    scenario "Adding a card without saving" do
-      journal_page = JournalPage.new
-      mmt_page = journal_page.add_new_template
-      mmt_page.paper_type = "Test Type"
-      phase = mmt_page.find_phase 'Phase 1'
-      phase.new_card overlay: ChooseCardTypeOverlay, card_type: "Reviewer Report"
+    # adding a card to an existing mmt is autosaved
+    mmt_page = ManuscriptManagerTemplatePage.new
+    phase = mmt_page.find_phase 'Phase 2'
+    phase.new_card overlay: ChooseCardTypeOverlay, card_type: "Assign Admin"
+    click_link "Admin"
+    find('.journal-thumbnail').click
+    find('.blue-box', text: 'Test Type').hover
+    find('.blue-box .glyphicon-pencil', visible: true).click
 
-      click_link 'Admin'
-
-      find('.journal-thumbnail').click
-      find('.blue-box', text: 'Test Type').hover
-      find('.blue-box .glyphicon-pencil', visible: true).click
-
-      expect(find('.card-content')).to have_text('Reviewer Report')
-    end
+    expect(find('.card-content', text: "Assign Admin")).to be_present
   end
 end

44b49f78c1c943b0d4e76eeb571376c0e3f59572
Aaron Christy <aaron@phad.com>
2015-04-08 14:36:19 -0400

    Add tables controller

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Tahi::Application.routes.draw do
   devise_scope :user do
     get "users/sign_out" => "devise/sessions#destroy"
   end
+
   authenticate :user, ->(u) { u.site_admin? } do
     mount Sidekiq::Web => "/sidekiq"
   end
@@ -58,6 +59,7 @@ Tahi::Application.routes.draw do
     resources :figures, only: [:destroy, :update] do
       put :update_attachment, on: :member
     end
+    resources :tables, only: [:create, :update, :destroy]
     resources :filtered_users do
       collection do
         get "admins/:paper_id", to: "filtered_users#admins"
---
+++ spec/controllers/tables_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe TablesController do
+  let(:user)  { create :user }
+  let(:paper) { FactoryGirl.create(:paper, creator: user) }
+
+  before { sign_in user }
+
+  describe "#create" do
+    let(:table_attributes) { FactoryGirl.attributes_for(:table) }
+
+    it "creates a new figure record" do
+      expect {
+        post :create, paper_id: paper.id, table: table_attributes, format: :json
+      }.to change{ paper.tables.reload.count }.by(1)
+    end
+  end
+
+  describe "#update" do
+    let(:table) { FactoryGirl.create(:table, paper: paper) }
+    let(:body) { "<table><tr><td>new</td></tr></table>" }
+
+    it "updates an existing figure record" do
+      put :update, id: table.id, paper_id: paper.id, table: { body: body }, format: :json
+      expect(table.reload.body).to eq(body)
+    end
+  end
+
+  describe "#destroy" do
+    let!(:table) { FactoryGirl.create(:table, paper: paper) }
+
+    it "destroys the figure record" do
+      expect {
+        delete :destroy, id: table.id, paper_id: paper.id, format: :json
+      }.to change{ paper.tables.reload.count }.by(-1)
+    end
+  end
+end

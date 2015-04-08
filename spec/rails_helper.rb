87654ad2f7ee905c7ab8b20c2701f232dcc575cc
James Martinez + Tony Schneider <pairing+jm+ts@edgecase.com>
2015-04-08 17:36:25 -0400

    Don’t install ff ember inspector unless EMBER_DEBUG

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,9 @@ end
 
 Capybara.register_driver :selenium do |app|
   profile = Selenium::WebDriver::Firefox::Profile.new
-  profile.add_extension("#{File.dirname(__FILE__)}/support/lib/ember_inspector-1.3.1-fx.xpi")
+  if ENV['EMBER_DEBUG']
+    profile.add_extension("#{File.dirname(__FILE__)}/support/lib/ember_inspector-1.3.1-fx.xpi")
+  end
   Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
 end
 

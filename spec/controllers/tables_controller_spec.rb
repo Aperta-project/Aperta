d792883f61e167d2c6c421dda81d790b12d8d1db
Aaron Christy <aaron@phad.com>
2015-04-08 20:07:50 -0400

    Fix typos

--- spec/controllers/tables_controller_spec.rb
+++ spec/controllers/tables_controller_spec.rb
@@ -9,7 +9,7 @@ describe TablesController do
   describe "#create" do
     let(:table_attributes) { FactoryGirl.attributes_for(:table) }
 
-    it "creates a new figure record" do
+    it "creates a new table record" do
       expect {
         post :create, paper_id: paper.id, table: table_attributes, format: :json
       }.to change{ paper.tables.reload.count }.by(1)
@@ -20,7 +20,7 @@ describe TablesController do
     let(:table) { FactoryGirl.create(:table, paper: paper) }
     let(:body) { "<table><tr><td>new</td></tr></table>" }
 
-    it "updates an existing figure record" do
+    it "updates an existing table record" do
       put :update, id: table.id, paper_id: paper.id, table: { body: body }, format: :json
       expect(table.reload.body).to eq(body)
     end
@@ -29,7 +29,7 @@ describe TablesController do
   describe "#destroy" do
     let!(:table) { FactoryGirl.create(:table, paper: paper) }
 
-    it "destroys the figure record" do
+    it "destroys the table record" do
       expect {
         delete :destroy, id: table.id, paper_id: paper.id, format: :json
       }.to change{ paper.tables.reload.count }.by(-1)

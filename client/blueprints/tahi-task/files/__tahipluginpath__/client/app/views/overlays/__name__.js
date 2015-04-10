59eb26405088be0df84c8c5ba964900bcc76e257
Erik Hetzner <ehetzner@plos.org>
2015-04-10 15:39:00 -0700

    fix task generator for tahi namespace
    rails generate tahi:task foo engines/bar was not working for the new
    Tahi:: module convention.

--- client/blueprints/tahi-task/files/__tahipluginpath__/client/app/views/overlays/__name__.js
+++ client/blueprints/tahi-task/files/__tahipluginpath__/client/app/views/overlays/__name__.js
@@ -2,6 +2,6 @@ import OverlayView from 'tahi/views/overlay';
 
 export default OverlayView.extend({
   templateName: 'overlays/<%= dasherizedModuleName %>',
-  layoutName:   'layouts/overlay',
+  layoutName: 'layouts/overlay',
   cardName: '<%= dasherizedModuleName %>'
 });

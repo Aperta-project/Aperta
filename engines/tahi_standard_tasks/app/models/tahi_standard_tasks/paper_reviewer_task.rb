1cfa1b4fcccd13e79480cd3a1d51df7af7717d24
Rizwan Reza <rizwanreza@gmail.com>
2015-04-10 16:01:30 -0700

    make a reviewer report task when the reviewer accepts invitation

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/paper_reviewer_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/paper_reviewer_task.rb
@@ -1,21 +1,21 @@
 module TahiStandardTasks
   class PaperReviewerTask < ::Task
-    def self.permitted_attributes
-      super + [{ reviewer_ids: [] }]
-    end
-
     register_task default_title: "Invite Reviewers", default_role: "reviewer"
 
     include Invitable
 
     def invitation_invited(invitation)
-      PaperReviewerMailer.delay.notify_invited({
-        invitation_id: invitation.id
-      })
+      PaperReviewerMailer.delay.notify_invited invitation_id: invitation.id
     end
 
     def invitation_accepted(invitation)
-      TaskRoleUpdater.new(self, invitation.invitee_id, PaperRole::REVIEWER).update
+      transaction do
+        TaskRoleUpdater.new(self, invitation.invitee_id, PaperRole::REVIEWER).update
+        task = TahiStandardTasks::ReviewerReportTask.create!(phase: reviewer_report_task_phase,
+                                                      role: journal_task_type.role,
+                                                      title: "Review by #{invitation.invitee.full_name}")
+        ParticipationFactory.create(task, invitation.invitee)
+      end
     end
 
     def invitation_rejected(invitation)
@@ -29,13 +29,8 @@ module TahiStandardTasks
       super + [:reviewer_ids]
     end
 
-    def reviewer_ids=(user_ids)
-      differences = Array.compare(paper.reviewers.pluck(:user_id), user_ids.map(&:to_i))
-      differences[:added].each do |id|
-        add_reviewer(User.find(id))
-      end
-      PaperRole.reviewers_for(paper).where(user_id: differences[:removed]).destroy_all
-      differences[:added]
+    def self.permitted_attributes
+      super + [{ reviewer_ids: [] }]
     end
 
     def update_responder
@@ -44,18 +39,6 @@ module TahiStandardTasks
 
     private
 
-    def add_reviewer(user)
-      transaction do
-        PaperRole.reviewers_for(paper).where(user: user).create!
-        task = TahiStandardTasks::ReviewerReportTask.create!(phase: reviewer_report_task_phase,
-                                                         role: journal_task_type.role,
-                                                         title: "Review by #{user.full_name}")
-        ParticipationFactory.create(task, user)
-        UserMailer.delay.add_reviewer(user.id, paper.id)
-      end
-    end
-
-    # TODO: remove need for hardcoded phase name across the application
     def reviewer_report_task_phase
       get_reviews_phase = paper.phases.where(name: 'Get Reviews').first
       get_reviews_phase || phase

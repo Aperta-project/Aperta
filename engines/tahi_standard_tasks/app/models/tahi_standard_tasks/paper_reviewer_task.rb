53319bdd1297e397a628962a745c79bc13b27d91
Rizwan Reza <rizwanreza@gmail.com>
2015-04-10 16:49:07 -0700

    :dog:

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/paper_reviewer_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/paper_reviewer_task.rb
@@ -12,8 +12,8 @@ module TahiStandardTasks
       transaction do
         TaskRoleUpdater.new(self, invitation.invitee_id, PaperRole::REVIEWER).update
         task = TahiStandardTasks::ReviewerReportTask.create!(phase: reviewer_report_task_phase,
-                                                      role: journal_task_type.role,
-                                                      title: "Review by #{invitation.invitee.full_name}")
+                                                             role: journal_task_type.role,
+                                                             title: "Review by #{invitation.invitee.full_name}")
         ParticipationFactory.create(task, invitation.invitee)
       end
     end

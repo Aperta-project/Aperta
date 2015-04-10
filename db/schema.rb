adbdacce43321ae67a5b2efa54e0f36d919f9c65
Anna Petry <hello@annapetry.com>
2015-04-10 12:08:11 -0700

    check in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407233310) do
+ActiveRecord::Schema.define(version: 20150408182120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -441,4 +441,5 @@ ActiveRecord::Schema.define(version: 20150407233310) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
+  add_foreign_key "decisions", "papers"
 end

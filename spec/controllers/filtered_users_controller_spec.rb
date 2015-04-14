41d9501f4ea90e7137547605c3e08afbe30a284f
Ryan Wold <pair+ryan.wold@neo.com>
2015-04-13 21:31:46 -0700

    filter users by query string in the api
    plus specs

--- spec/controllers/filtered_users_controller_spec.rb
+++ spec/controllers/filtered_users_controller_spec.rb
@@ -17,14 +17,12 @@ end
 
 describe FilteredUsersController do
   let(:user) { create :user }
-  let(:user2) { create :user }
   let(:journal) { FactoryGirl.create(:journal) }
   let(:role) { FactoryGirl.create(:role, :reviewer, journal: journal) }
   let(:paper) { FactoryGirl.create(:paper, journal: journal) }
 
   before do
     assign_journal_role(journal, user, :reviewer)
-    assign_journal_role(journal, user2, :reviewer)
     sign_in(user)
   end
 
@@ -91,5 +89,74 @@ describe FilteredUsersController do
         expect(res_body["filtered_users"].first["email"]).to eq user.email
       end
     end
+
+    # Create a total of:
+    #  4 Reviewers
+    #  3 Editors
+    #  2 Admins
+    context "when searching from select2 dropdowns" do
+      let(:reviewer2) { FactoryGirl.create :user, email: "reviewer@example.com" }
+      let(:reviewer3) { FactoryGirl.create :user }
+      let(:reviewer4) { FactoryGirl.create :user }
+      let(:editor1)   { FactoryGirl.create :user, email: "editor@example.com" }
+      let(:editor2)   { FactoryGirl.create :user }
+      let(:editor3)   { FactoryGirl.create :user }
+      let(:admin1)    { FactoryGirl.create :user, email: "admin@example.com" }
+      let(:admin2)    { FactoryGirl.create :user }
+
+      before do
+        assign_journal_role(journal, reviewer2, :reviewer)
+        assign_journal_role(journal, reviewer3, :reviewer)
+        assign_journal_role(journal, reviewer4, :reviewer)
+        assign_journal_role(journal, editor1, :editor)
+        assign_journal_role(journal, editor2, :editor)
+        assign_journal_role(journal, editor3, :editor)
+        assign_journal_role(journal, admin1, :admin)
+        assign_journal_role(journal, admin2, :admin)
+      end
+
+      describe "#editors" do
+        it "returns editors" do
+          get :editors, paper_id: paper.id, format: :json
+          expect(res_body["filtered_users"].count).to eq 3
+        end
+
+        it "filters editors by query string" do
+          get :editors, paper_id: paper.id, query: "editor", format: :json
+          expect(res_body["filtered_users"].count).to eq 1
+          expect(res_body["filtered_users"].first["id"]).to eq editor1.id
+          expect(res_body["filtered_users"].first["email"]).to eq editor1.email
+        end
+      end
+
+      describe "#admins" do
+        it "returns admins" do
+          get :admins, paper_id: paper.id, format: :json
+          expect(res_body["filtered_users"].count).to eq 2
+        end
+
+        it "filters admins by query string" do
+          get :admins, paper_id: paper.id, query: "admin", format: :json
+          expect(res_body["filtered_users"].count).to eq 1
+          expect(res_body["filtered_users"].first["id"]).to eq admin1.id
+          expect(res_body["filtered_users"].first["email"]).to eq admin1.email
+        end
+      end
+
+      describe "#reviewers" do
+        it "returns reviewers" do
+          get :reviewers, paper_id: paper.id, format: :json
+          expect(res_body["filtered_users"].count).to eq 4
+        end
+
+        it "returns reviewers by query string" do
+          get :reviewers, paper_id: paper.id, query: "reviewer", format: :json
+          expect(res_body["filtered_users"].count).to eq 1
+          expect(res_body["filtered_users"].first["id"]).to eq reviewer2.id
+          expect(res_body["filtered_users"].first["email"]).to eq reviewer2.email
+        end
+      end
+    end
+
   end
 end

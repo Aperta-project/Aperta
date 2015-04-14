9764933fdb8c203cf637a79d2a8bc681db1c8c08
Pairing <pairing@edgecase.com>
2015-04-14 09:39:09 -0400

    Make revise task a metadata task
    * This is to fix a "bug", but really we need to sit down and specify
    * what the sidebar should display

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/revise_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/revise_task.rb
@@ -1,6 +1,7 @@
 module TahiStandardTasks
   class ReviseTask < Task
     # uncomment the following line if you want to enable event streaming for this model
+    include MetadataTask
     include EventStreamNotifier
 
     register_task default_title: "Revise Task", default_role: "author"

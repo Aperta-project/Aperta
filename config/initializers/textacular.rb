f7d88d08f729c1e11f9036e53f8367a429354b8a
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2015-04-20 09:39:35 -0400

    remove textacular initializer

--- config/initializers/textacular.rb
+++
@@ -1,6 +0,0 @@
-# fuzzy searches are subject to a similarity threshold imposed by the 
-# pg_trgm module.  The default is 0.3, meaning that at least 30% of the
-# total string must match your search content.
-
-PG_SIMILARLITY = 0.1
-ActiveRecord::Base.connection.execute("SELECT set_limit(#{PG_SIMILARLITY});")

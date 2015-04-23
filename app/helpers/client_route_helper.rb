60f372a6aa3e5b7b8be827fc12305e0cd3bf74de
Anna Petry and Feifan Wang <pair+anna.petry+feifan@neo.com>
2015-04-23 14:42:59 -0700

    remove application route helpers

--- app/helpers/client_route_helper.rb
+++ app/helpers/client_route_helper.rb
@@ -1,5 +1,4 @@
 module ClientRouteHelper
-  include Rails.application.routes.url_helpers
 
   def client_paper_task_url(paper, task)
     "#{root_url}papers/#{paper.to_param}/tasks/#{task.to_param}"

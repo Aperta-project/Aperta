1006950e087dfd21b6ab7c932c924e7c91fa7b58
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2015-04-29 12:04:07 -0400

    Add asset_host to all non-dev / non-test environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,11 +55,8 @@ Tahi::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( auth.css )
+  # config.action_controller.asset_host = "//assets.example.com"
+  config.action_controller.asset_host = ENV.fetch("RAILS_ASSET_HOST")
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,6 +2,9 @@ require_relative 'production'
 
 Tahi::Application.configure do
   config.force_ssl = false if ENV["DISABLE_SSL"]
-
   config.basic_auth_required = true
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # config.action_controller.asset_host = "//assets.example.com"
+  config.action_controller.asset_host = ENV.fetch("RAILS_ASSET_HOST")
 end

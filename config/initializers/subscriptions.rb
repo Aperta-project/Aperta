68233905231660743a25545de1937a44f33cdc01
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2015-05-04 10:09:23 -0400

    EventStream -> EventStream::Broadcaster

--- config/initializers/subscriptions.rb
+++ config/initializers/subscriptions.rb
@@ -2,33 +2,33 @@ TahiNotifier.subscribe("paper:*") do |payload|
   action = payload[:action]
   record = payload[:record]
 
-  EventStream.new(record).post(action: action, channel_scope: record)
+  EventStream::Broadcaster.new(record).post(action: action, channel_scope: record)
 end
 
 TahiNotifier.subscribe("task:*", "author:*", "figure:*", "invitation:*", "supporting_information_file:*") do |payload|
   action = payload[:action]
   record = payload[:record]
 
-  EventStream.new(record).post(action: action, channel_scope: record.paper)
+  EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.paper)
 end
 
 TahiNotifier.subscribe("comment:*", "participation:*") do |payload|
   action = payload[:action]
   record = payload[:record]
 
-  EventStream.new(record).post(action: action, channel_scope: record.task.paper)
+  EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.task.paper)
 end
 
 TahiNotifier.subscribe("question_attachment:*") do |payload|
   action = payload[:action]
   record = payload[:record]
 
-  EventStream.new(record).post(action: action, channel_scope: record.question.task.paper)
+  EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.question.task.paper)
 end
 
 # TahiNotifier.subscribe("paper_role:*") do |payload|
 #   action = payload[:action]
 #   record = payload[:record]
 # 
-#   EventStream.new(record).post(action: action, channel_scope: record.paper)
+#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.paper)
 # end

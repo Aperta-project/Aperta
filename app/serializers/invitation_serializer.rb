05ac45939455fcdf4b872d8219442c55c41ef93e
Pairing <pairing@edgecase.com>
2015-05-05 11:10:24 -0400

    mv dashboard spec to invitations controller spec
    * lighten up the invitation serializers
    * For example, only the index view needs the paper data
    
    I think we should consider having a concept of “pending papers” as
    opposed to “invitations”. Will suggest this to designers.

--- app/serializers/invitation_serializer.rb
+++ app/serializers/invitation_serializer.rb
@@ -1,24 +1,9 @@
 class InvitationSerializer < ActiveModel::Serializer
   attributes :id,
              :state,
-             :title,
-             :abstract,
              :email,
-             :invitation_type,
              :created_at,
              :updated_at
 
   has_one :invitee, serializer: UserSerializer, embed: :id, root: :users, include: true
-
-  def title
-    object.paper.title
-  end
-
-  def abstract
-    object.paper.abstract
-  end
-
-  def invitation_type
-    object.task.invitee_role.capitalize
-  end
 end

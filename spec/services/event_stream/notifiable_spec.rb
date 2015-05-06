7364cd7ee2246b0be43bd8e613d7b32395efca5c
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2015-05-06 16:53:10 -0400

    Add notifiable specs

--- spec/services/event_stream/notifiable_spec.rb
+++ spec/services/event_stream/notifiable_spec.rb
@@ -3,15 +3,46 @@ require "rails_helper"
 describe EventStream::Notifiable do
 
   # a model with an event_stream_notifier included
-  let(:model) { FactoryGirl.build(:paper, id: 4) }
+  let(:model) { FactoryGirl.create(:paper) }
 
-  describe "#event_stream_payload" do
-    let(:payload) { model.event_stream_payload }
+  describe "#event_payload" do
+    let(:payload) do
+      model.event_payload
+    end
 
     it "contains the serialized model" do
       expect(payload[:record]).to eq(model)
     end
 
+    context "when created" do
+      it "sends the 'created' action" do
+        expect(payload[:action]).to eq("created")
+      end
+    end
+
+    context "when updated" do
+      let(:payload) do
+        paper = Paper.find(model.id)
+        paper.touch
+        paper.event_payload
+      end
+
+      it "sends the 'updated' action" do
+        expect(payload[:action]).to eq("updated")
+      end
+    end
+
+    context "when destroyed" do
+      let(:payload) do
+        paper = Paper.find(model.id)
+        paper.destroy
+        paper.event_payload
+      end
+
+      it "sends the 'destroyed' action" do
+        expect(payload[:action]).to eq("destroyed")
+      end
+    end
   end
 
 end

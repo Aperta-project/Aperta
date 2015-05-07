325af10167fba43b78dc2db8fb3dc60a5b5c221c
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2015-05-07 09:51:46 -0400

    Enable paper_role pusher serialization via user channel

--- config/initializers/subscriptions.rb
+++ config/initializers/subscriptions.rb
@@ -2,6 +2,7 @@ TahiNotifier.subscribe("paper:*") do |payload|
   action = payload[:action]
   record = payload[:record]
 
+  # serialize the paper down the paper channel
   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record)
 end
 
@@ -9,6 +10,7 @@ TahiNotifier.subscribe("task:*", "author:*", "figure:*", "invitation:*", "suppor
   action = payload[:action]
   record = payload[:record]
 
+  # serialize the respective model down the paper channel
   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.paper)
 end
 
@@ -16,6 +18,7 @@ TahiNotifier.subscribe("comment:*", "participation:*") do |payload|
   action = payload[:action]
   record = payload[:record]
 
+  # serialize the comment or participation down the paper channel
   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.task.paper)
 end
 
@@ -23,12 +26,14 @@ TahiNotifier.subscribe("question_attachment:*") do |payload|
   action = payload[:action]
   record = payload[:record]
 
+  # serialize the question_attachment down the paper channel
   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.question.task.paper)
 end
 
-# TahiNotifier.subscribe("paper_role:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-# 
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.paper)
-# end
+TahiNotifier.subscribe("paper_role:*") do |payload|
+  action = payload[:action]
+  record = payload[:record]
+
+  # serialize the paper down the user channel
+  EventStream::Broadcaster.new(record.paper).post(action: action, channel_scope: record.user)
+end

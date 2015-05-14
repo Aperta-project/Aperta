d77518b279aaff74d8fed95598155ef7839f20ac
James Martinez + Jerry Nummi <pairing+jm+jn@edgecase.com>
2015-05-14 17:25:36 -0400

    WIP paper new chosen to select2

--- client/app/controllers/overlays/paper-new.js
+++ client/app/controllers/overlays/paper-new.js
@@ -1,9 +1,36 @@
 import Ember from 'ember';
+import Utils from 'tahi/services/utils';
 
 export default Ember.Controller.extend({
   overlayClass: 'overlay--fullscreen paper-new-overlay',
   journals: null,
   noJournalSelected: Ember.computed.not('model.journal'),
+
+  journalProxies: function() {
+    return this.get('journals').map(function(journal) {
+      return {
+        id: journal.get('id'),
+        text: journal.get('name')
+      };
+    });
+  }.property('journal.@each'),
+
+  paperTypeProxies: function() {
+    return this.get('model.journal.paperTypes').map(function(paperType) {
+      return {
+        id: Utils.generateUUID(),
+        text: paperType
+      };
+    });
+  }.property('model.journal.paperTypes.@each'),
+
+  selectedJournal: function() {
+    let journal = this.get('model.journal');
+    if(Ember.isEmpty(journal)) { return; }
+
+    return { id:   journal.get('id'),
+             text: journal.get('name') };
+  }.property('model.journal'),
 
   journalDidChange: function() {
     this.set('model.paperType', this.get('model.journal.paperTypes.firstObject'));
@@ -17,6 +44,16 @@ export default Ember.Controller.extend({
       }, (response)=> {
         this.flash.displayErrorMessagesFromResponse(response);
       });
+    },
+
+    selectJournal(journalProxy) {
+      let journal = this.get('journals').findBy('id', journalProxy.id);
+      this.set('model.journal', journal);
+    },
+
+    selectPaperType(paperTypeProxy) {
+      let paperType = this.get('model.journal.paperTypes').findBy('id', paperTypeProxy.id);
+      this.set('model.paperType', paperType);
     }
   }
 });

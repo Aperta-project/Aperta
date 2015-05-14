a25d2840b124dd5b212013e07dd8de70c42e5de3
Feifan Wang <feifanw@gmail.com>
2015-05-14 15:03:47 -0700

    minor refactoring

--- client/app/controllers/overlays/invitations.js
+++ client/app/controllers/overlays/invitations.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   overlayClass: 'overlay--fullscreen invitations-overlay',
 
+  pendingInvitations: function(){
+
+  }.property('state'),
+
   didCompleteAllInvitations: function() {
     if(Ember.isEmpty(this.get('model'))) {
       this.send('closeOverlay');

c9ce32389bc0184cbb97c22a14aa95596ed4d4a6
James Martinez + Jerry Nummi <pairing+jm+jn@edgecase.com>
2015-05-14 16:36:48 -0400

    Journal Task type edit select2 from chosen

--- client/app/pods/components/journal-task-type-edit/component.js
+++ client/app/pods/components/journal-task-type-edit/component.js
@@ -4,11 +4,30 @@ export default Ember.Component.extend({
   classNames: ['individual-task-type'],
 
   roles: null,
+  selectedRole: Ember.computed('model.role', function() {
+    let role = this.get('availableTaskRoles').findBy('name', this.get('model.role'));
+    if(Ember.isEmpty(role)) { return null; }
+
+    return {
+      id: role.get('id'),
+      text: role.get('name')
+    };
+  }),
+
   journalRoleSort: ['name: asc'],
   availableTaskRoles: Ember.computed.sort('roles', 'journalRoleSort'),
+  formattedTaskRoles: function() {
+    return this.get('availableTaskRoles').map(function(taskRole) {
+      return {
+        id: taskRole.get('id'),
+        text: taskRole.get('name')
+      };
+    });
+  }.property('availableTaskRoles.@each'),
 
   actions: {
-    save() {
+    save(roleProxy) {
+      this.set('model.role', roleProxy.text);
       this.get('model').save().then(function(){}, function() {});
     }
   }

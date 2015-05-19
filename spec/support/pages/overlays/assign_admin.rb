f0e7ec464c383e2419d1a5e7b78a7afcb63709d3
James Martinez + Jerry Nummi <pairing+jm+jn@edgecase.com>
2015-05-19 11:18:27 -0400

    New Capybara select2 helper

--- spec/support/pages/overlays/assign_admin.rb
+++ spec/support/pages/overlays/assign_admin.rb
@@ -2,6 +2,6 @@ class AssignAdminOverlay < CardOverlay
   text_assertions :admin, '.admin-select2 .select2-chosen'
 
   def admin=(user)
-    pick_from_select2_single user.email, user.email, class: 'admin-select2'
+    select2 user.email, css: '.admin-select2'
   end
 end

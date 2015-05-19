23ee380a0fe2ab8744f6866f91c5eb1dc59afbf5
James Martinez + Jerry Nummi <pairing+jm+jn@edgecase.com>
2015-05-19 11:49:11 -0400

    Fix assign admin spec

--- spec/support/pages/overlays/assign_admin.rb
+++ spec/support/pages/overlays/assign_admin.rb
@@ -2,6 +2,6 @@ class AssignAdminOverlay < CardOverlay
   text_assertions :admin, '.admin-select2 .select2-chosen'
 
   def admin=(user)
-    select2 user.email, css: '.admin-select2'
+    select2 user.email, css: '.admin-select2', search: true
   end
 end

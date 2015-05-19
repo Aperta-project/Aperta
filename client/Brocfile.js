acdb2a018d71212ddad1f72788fb8f67c832c83a
Aaron Christy + Chandu Tennety <pairing+ac+ct@edgecase.com>
2015-05-19 10:30:20 -0400

    No longer include event-source js in broccoli

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -45,10 +45,7 @@ app.import('bower_components/bootstrap/js/tooltip.js');
 app.import('bower_components/bootstrap-datepicker/css/datepicker3.css');
 app.import('bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js');
 
-
-if (app.env === 'production') {
-  app.import('bower_components/event-source-polyfill/eventsource.js');
-} else {
+if (app.env !== 'production') {
   app.import('bower_components/sinon/index.js');
   app.import('bower_components/ember/ember-template-compiler.js');
 }

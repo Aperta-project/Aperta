bc600c5add9b14c7c95898bfccd685464dc2872f
Jerry Nummi <jerry@neo.com>
2015-05-22 16:35:58 -0400

    WIP

---
+++ client/app/pods/components/inline-edit-h1/component.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  editing: Ember.computed.oneWay('editOnOpen'),
+  snapshot: null,
+
+  createSnapshot() {
+    this.set('snapshot', Ember.copy(this.get('title')));
+  },
+
+  hasContent: Ember.computed.notEmpty('title'),
+
+  focusOnEdit: function() {
+    if (this.get('editing')) {
+      Ember.run.schedule('afterRender', this, function() {
+        this.$('input[type=text]').focus().select();
+      });
+    }
+  }.observes('editing').on('didInsertElement'),
+
+  actions: {
+    toggleEdit() {
+      this.createSnapshot();
+
+      if (this.get('editing')) {
+        this.sendAction('setTitle', this.get('snapshot'));
+      }
+
+      this.toggleProperty('editing');
+    },
+
+    save() {
+      if (this.get('hasContent')) {
+        this.sendAction('setTitle', this.get('title'));
+        this.toggleProperty('editing');
+      }
+    }
+  }
+});

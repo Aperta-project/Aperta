7daadd9505da41c6951f93060ddac03230ea15ed
Erik Hetzner <ehetzner@plos.org>
2015-05-22 10:37:35 -0700

    Allow use of the pusher service as well as slanger

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,9 +1,13 @@
 if TahiPusher::Config.enabled?
-  Pusher.app_id = ENV.fetch("EVENT_STREAM_APP_ID")
-  Pusher.key    = ENV.fetch("EVENT_STREAM_KEY")
-  Pusher.secret = ENV.fetch("EVENT_STREAM_SECRET")
-  Pusher.host   = ENV.fetch("EVENT_STREAM_API_HOST")
-  Pusher.port   = ENV.fetch("EVENT_STREAM_API_PORT").to_i
+  if ENV.key?('PUSHER_URL')
+    Pusher.url = ENV.fetch('PUSHER_URL')
+  else
+    Pusher.app_id = ENV.fetch("EVENT_STREAM_APP_ID")
+    Pusher.key    = ENV.fetch("EVENT_STREAM_KEY")
+    Pusher.secret = ENV.fetch("EVENT_STREAM_SECRET")
+    Pusher.host   = ENV.fetch("EVENT_STREAM_API_HOST")
+    Pusher.port   = ENV.fetch("EVENT_STREAM_API_PORT").to_i
+  end
   Pusher.logger = Rails.logger
 
   # SSL enabled for non-development

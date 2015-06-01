6e5f7336eba75c23f74fe82e834adafa44f67798
Jerry Nummi <jerry@neo.com>
2015-06-01 16:44:28 -0400

    WIP

--- client/app/pods/paper/edit/latex-editor/controller.js
+++ client/app/pods/paper/edit/latex-editor/controller.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 import PaperBaseMixin from 'tahi/mixins/controllers/paper-base';
 import PaperEditMixin from 'tahi/mixins/controllers/paper-edit';
+import { ClientEvents, Bindings } from 'ember-pusher';
 
-export default Ember.Controller.extend(PaperBaseMixin, PaperEditMixin, {
+export default Ember.Controller.extend(PaperBaseMixin, PaperEditMixin, ClientEvents, Bindings, {
   startEditing() {
     this.set('model.lockedBy', this.currentUser);
     this.get('model').save().then(()=> {
@@ -12,6 +13,11 @@ export default Ember.Controller.extend(PaperBaseMixin, PaperEditMixin, {
   },
 
   stopEditing() {
+    this.pusher.connection.send_event('client-body_updated', {
+      paper_id: this.get('model.id'),
+      body: this.get('model.body')
+    }, 'private-latex_paper');
+
     this.set('model.lockedBy', null);
     this.send('stopEditing');
     this.get('model').save().then(()=> {

ee2e6e474442bcfe07177db8a76f0a765593b264
Anna Petry <hello@annapetry.com>
2015-06-01 10:51:05 -0700

    fix indentation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
   def resource_name
-   :user
+    :user
   end
 
   def resource
-   @resource ||= User.new
+    @resource ||= User.new
   end
 
   def devise_mapping
-   @devise_mapping ||= Devise.mappings[:user]
+    @devise_mapping ||= Devise.mappings[:user]
   end
 
   def resource_class

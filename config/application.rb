5acbbb1aefd42e4032c23dd602e4c44499d6a9a1
Anna Petry <hello@annapetry.com>
2015-06-02 17:17:20 -0700

    dont access ENV vars in Devise initializer

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,6 @@ module Tahi
 
     config.orcid_key = ENV.fetch('ORCID_KEY', false)
     config.orcid_secret = ENV.fetch('ORCID_SECRET', false)
-    config.orcid_enabled = config.orcid_key && config.orcid_secret
+    config.orcid_enabled = !!(config.orcid_key && config.orcid_secret)
   end
 end

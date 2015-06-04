2efe53e3f96f2d53c77f4b0ac8d8f058e3b117c5
Max Cantor <max@maxcantor.net>
2015-06-04 13:25:53 -0400

    Re-implement name check with legacy skips.

--- lib/generators/tahi/task/task_generator.rb
+++ lib/generators/tahi/task/task_generator.rb
@@ -1,10 +1,23 @@
 module Tahi
+
+  # These plugins are excluded from the 'tahi-' prefix requirement,
+  # and will be removed from the array as they are updated.
+  LEGACY_PLUGINS = [
+    'tahi_standard_tasks',
+    'tahi_upload_manuscript',
+    'plos_authors',
+    'plos_billing',
+    'plos_bio_tech_check'
+  ]
+
   class TaskGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
     argument :plugin, type: :string, required: true
 
     def generate
+      name_check plugin
+
       @task_name = camel_space(class_name) + " Task"
       @plugin_module = plugin.camelize
 
@@ -23,6 +36,14 @@ module Tahi
 
     private
 
+    def name_check(plugin)
+      if LEGACY_PLUGINS.include? plugin
+        puts 'Skipping prefix check for legacy plugin'
+      elsif !plugin.match /^tahi-/
+        die "Plugins must be prefixed with 'tahi-'."
+      end
+    end
+
     # "CamelCase" -> "Camel Space"
     def camel_space(token)
       token.split(/(?=[A-Z])/).join(' ')
@@ -34,10 +55,16 @@ module Tahi
         s.name == gem_name
       end
       if !source
-        fail Bundler::GemNotFound, "Could not find local gem '#{gem_name}' in "\
-          " current bundle. Please ensure that it is a gem with a :path source."
+        die "Could not find local gem '#{gem_name}' in current bundle. Please"\
+            " ensure that it is a gem with a :path source."
       end
       Pathname.new(source.path.to_s).expand_path
     end
+
+    def die(msg)
+      puts "\033[31m#{msg}\033[0m"
+      exit 1
+    end
   end
+
 end

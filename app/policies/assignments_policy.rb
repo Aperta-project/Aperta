2a3c700a5a1385e05a747db176000098dece2ef9
Rizwan Reza <rizwanreza@gmail.com>
2015-06-09 14:21:05 -0700

    Add assignments policy

---
+++ app/policies/assignments_policy.rb
@@ -0,0 +1,11 @@
+class AssignmentsPolicy < ApplicationPolicy
+  require_params :paper
+
+  def index?
+    can_administer_journal? paper.journal
+  end
+
+  def create?
+    can_administer_journal? paper.journal
+  end
+end

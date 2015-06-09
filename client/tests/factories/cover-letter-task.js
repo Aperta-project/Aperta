9dcb5728b4bd511b497a81a3fe809dfd496e0594
Willa Wang <willa@neo.com>
2015-06-09 00:31:28 -0700

    Cleaning up unused factory and gitignore.

--- client/tests/factories/cover-letter-task.js
+++
@@ -1,9 +0,0 @@
-import FactoryGuy from "ember-data-factory-guy";
-
-FactoryGuy.define('paper-reviewer-task', {
-  default: {
-    title: 'Invite Reviewers',
-    type: 'PaperReviewerTask',
-    completed: false,
-  }
-});

9a2d84b27b734dc458e6112637ab2807201ecc8b
Rizwan Reza <rizwanreza@gmail.com>
2015-06-09 17:23:19 -0700

    Include created_at and user

--- app/serializers/assignment_serializer.rb
+++ app/serializers/assignment_serializer.rb
@@ -1,6 +1,6 @@
 class AssignmentSerializer < ActiveModel::Serializer
-  attributes :id, :role
+  attributes :id, :role, :created_at
 
   has_one :paper, embed: :id
-  has_one :user, embed: :id
+  has_one :user, embed: :id, include: true
 end
--- client/app/models/assignment.js
+++ client/app/models/assignment.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   user: DS.belongsTo('user'),
   paper: DS.belongsTo('paper'),
-  role: DS.attr('string')
+  role: DS.attr('string'),
+  createdAt: DS.attr('date')
 });

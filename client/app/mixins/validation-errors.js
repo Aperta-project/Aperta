2ba194254fe668f7ce4111c5b6296aa87ad03ae3
Rizwan Reza <rizwanreza@gmail.com>
2015-06-10 14:45:30 -0700

    The error text now contains field name

--- client/app/mixins/validation-errors.js
+++ client/app/mixins/validation-errors.js
@@ -59,7 +59,11 @@ export default Ember.Mixin.create({
   */
 
   _prepareResponseErrors: function(errors) {
-    return Utils.deepJoinArrays(Utils.deepCamelizeKeys(errors));
+    var errorsObject = Utils.deepJoinArrays(Utils.deepCamelizeKeys(errors));
+    for(var key in errorsObject) {
+      errorsObject[key] = `${key.capitalize()} ${errors[key]}`
+    };
+    return errorsObject;
   },
 
   /**

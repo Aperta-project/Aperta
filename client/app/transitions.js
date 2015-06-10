54ca5c9f0e210f07c5055ef67bd53c4f7e4060c4
Jerry Nummi <jerry@neo.com>
2015-06-09 21:05:44 -0400

    Cleanup discussion route animations

--- client/app/transitions.js
+++ client/app/transitions.js
@@ -1,22 +1,13 @@
 export default function() {
   this.transition(
-    this.fromRoute('paper.workflow.discussions.index'),
     this.toRoute('paper.workflow.discussions.show'),
-    this.use('toLeft',      { duration: 600, easing: [300, 25] }),
-    this.reverse('toRight', { duration: 600, easing: [300, 25] })
+    this.use('slideToLeft',      { duration: 600, easing: [300, 25] }),
+    this.reverse('slideToRight', { duration: 600, easing: [300, 25] })
   );
 
   this.transition(
-    this.fromRoute('paper.workflow.discussions.index'),
     this.toRoute('paper.workflow.discussions.new'),
-    this.use('toLeft',      { duration: 600, easing: [300, 25] }),
-    this.reverse('toRight', { duration: 600, easing: [300, 25] })
-  );
-
-  this.transition(
-    this.fromRoute('paper.workflow.discussions.new'),
-    this.toRoute('paper.workflow.discussions.show'),
-    this.use('toLeft',      { duration: 600, easing: [300, 25] }),
-    this.reverse('toRight', { duration: 600, easing: [300, 25] })
+    this.use('slideToLeft',      { duration: 600, easing: [300, 25] }),
+    this.reverse('slideToRight', { duration: 600, easing: [300, 25] })
   );
 }

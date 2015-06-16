1200a91f933414ce1182420ecb328ceb5c1605c6
Jerry Nummi <jerry@neo.com>
2015-06-16 13:30:18 -0400

    Properly format DiscussionReply
    – Keep line breaks
    – Hyperlinks for URLs

--- client/app/helpers/display-line-breaks.js
+++ client/app/helpers/display-line-breaks.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 import lineBreakToTag from 'tahi/lib/line-break-to-tag';
 
 export default Ember.Handlebars.makeBoundHelper(function(text) {
-  return new Ember.Handlebars.SafeString(lineBreakToTag(text));
+  // `text` could be String or Object from Handlebars subexpression
+  let string = Ember.typeOf(text) === 'string' ? text : text.string;
+  return new Ember.Handlebars.SafeString(lineBreakToTag(string));
 });
--- client/app/helpers/url-to-href.js
+++ client/app/helpers/url-to-href.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 import urlToHref from 'tahi/lib/url-to-href';
 
 export default Ember.Handlebars.makeBoundHelper(function(text) {
-  return new Ember.Handlebars.SafeString(urlToHref(text, true));
+  // `text` could be String or Object from Handlebars subexpression
+  let string = Ember.typeOf(text) === 'string' ? text : text.string;
+  return new Ember.Handlebars.SafeString(urlToHref(string, true));
 });

032edcf21b6fc7d02655c389ff162220ddf962aa
Jerry Nummi <jerry@neo.com>
2015-06-17 15:23:23 -0400

    Test discussion show

--- client/tests/factories/discussion-topic.js
+++ client/tests/factories/discussion-topic.js
@@ -3,5 +3,9 @@ import FactoryGuy from 'ember-data-factory-guy';
 FactoryGuy.define('discussion-topic', {
   default: {
     title: 'Tech Check Discussion'
+  },
+
+  topic_with_replies: {
+    discussionReplies: FactoryGuy.hasMany('discussion-reply', 2)
   }
 });
--- client/tests/integration/discussions-test.js
+++ client/tests/integration/discussions-test.js
@@ -20,7 +20,7 @@ module('Integration: Discussions', {
     TestHelper.setup(App);
 
     paper = make('paper', { phases: [], tasks: [] });
-    topic = make('discussion-topic', { paperId: paper.id, title: 'Hipster Ipsum Dolor' });
+    topic = make('topic_with_replies', { paperId: paper.id, title: 'Hipster Ipsum Dolor' });
 
     TestHelper.handleFind(paper);
     TestHelper.handleFindAll('discussion-topic', 1);
@@ -32,28 +32,23 @@ test('can see a list of topics', function(assert) {
     visit('/papers/' + paper.id + '/workflow/discussions/');
 
     andThen(function() {
-      assert.ok(find('.discussions-index-topic').length, 'Topic is found');
+      let topic = find('.discussions-index-topic:first');
+      assert.ok(topic.length, 'Topic is found: ' + topic.text());
     });
   });
 });
 
-// test('can add a new topic', function(assert) {
-//   Ember.run(function() {
-//     let topicTitle   = 'Tech Check Discussion';
-//     let firstComment = 'We need to talk about this, yous guys.';
-// 
-//     TestHelper.handleFindAll('discussion-topic', 1);
-//     TestHelper.handleCreate('discussion-topic');
-// 
-//     visit('/papers/' + paper.id + '/workflow/discussions/');
-//     click('a:contains("Create New Topic")');
-//     fillIn('.discussion-topic-title-field', topicTitle);
-//     fillIn('.discussion-topic-comment-field', firstComment);
-//     click('button:contains("Create Topic")');
-// 
-//     andThen(function() {
-//       assert.ok(find('.discussions-show-title:contains("' + topicTitle + '")').length, 'New topic is found');
-//       assert.ok(find('.message-comment:contains("' + firstComment + '")').length, 'First comment is found');
-//     });
-//   });
-// });
+test('can see a topic and a reply', function(assert) {
+  Ember.run(function() {
+    TestHelper.handleFind(topic);
+    visit('/papers/' + paper.id + '/workflow/discussions/' + topic.get('id'));
+
+    andThen(function() {
+      let titleText = find('.discussions-show-title input').val();
+      let replyText = find('.comment-body:first').text();
+
+      assert.equal(titleText, 'Hipster Ipsum Dolor', 'Topic title is found: ' + titleText);
+      assert.ok(replyText, 'Reply is found: ' + replyText);
+    });
+  });
+});

be5c125864b4fe3f8584357296507b035d9539a4
Jerry Nummi <jerry@neo.com>
2015-06-17 13:27:08 -0400

    Highlight new discussion-reply

--- client/app/pods/components/discussion-reply/component.js
+++ client/app/pods/components/discussion-reply/component.js
@@ -5,5 +5,18 @@ export default Ember.Component.extend({
   isNew: false,
 
   // passed into component
-  reply: null
+  reply: null,
+
+  checkAge: function() {
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      let createdAt = this.get('reply.createdAt');
+      if(Ember.isEmpty(createdAt)) { return; }
+
+      // if created in the last 2 seconds, set to isNew
+      let diff = Math.floor(Date.now() / 1000) - Math.floor(createdAt.getTime() / 1000)
+      if(diff < 2) {
+        this.set('isNew', true);
+      }
+    });
+  }.observes('reply.createdAt').on('didInsertElement'),
 });

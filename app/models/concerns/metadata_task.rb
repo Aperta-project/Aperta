ee73377334008d1c10692fd8d8a8830a39836952
Sam Bleckley <sam@mutuallyhuman.com>
2015-06-19 13:40:50 -0400

    Removes apparently unused authorize_update method.

--- app/models/concerns/metadata_task.rb
+++ app/models/concerns/metadata_task.rb
@@ -2,16 +2,6 @@ module MetadataTask
   extend ActiveSupport::Concern
   include SubmissionTask
 
-  def authorize_update?(params, user)
-    if user.site_admin?
-      true
-    else
-      # Is this actually related to editable?
-      # Can we post when not editable? Must we be able to post while editable?
-      paper.ongoing? || paper.in_revision? || paper.in_minor_revision?
-    end
-  end
-
   included do
     Task.metadata_types ||= Set.new
     Task.metadata_types.add name
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -19,61 +19,6 @@ describe Task do
     end
   end
 
-  describe "authorize_update?" do
-    let(:paper) { double('paper') }
-    let(:user)  { build(:user, site_admin: admin) }
-    let(:authorized) { task.authorize_update?(nil, user) }
-    before do
-      allow(task).to receive(:paper).and_return paper
-      allow(paper).to receive(:in_revision?).and_return false
-      allow(paper).to receive(:ongoing?).and_return ongoing
-    end
-
-    context "a non-metadata task with a submitted paper" do
-      let(:task) { Task.new(type: 'Task') }
-      let(:ongoing) { false }
-      let(:admin) { false }
-
-      it 'generally returns true' do
-        expect(authorized).to eq(true)
-      end
-    end
-
-    context "a metadata task" do
-      class AMetadataTask < Task
-        include MetadataTask
-      end
-
-      let(:task) { AMetadataTask.new(type: 'AMetadataTask') }
-
-      context 'the paper has been submitted' do
-        let(:ongoing) { false }
-
-        context "the user is an admin" do
-          let(:admin) { true }
-          it 'always allows admins' do
-            expect(authorized).to eq(true)
-          end
-        end
-
-        context "the user is not an admin" do
-          let(:admin) { false }
-          it "doesn't allow a regular user" do
-            expect(authorized).to eq(false)
-          end
-        end
-      end
-
-      context 'the paper has not been submitted' do
-        let(:ongoing) { true }
-        let(:admin) { false }
-        it "allows a regular user" do
-          expect(authorized).to eq(true)
-        end
-      end
-    end
-  end
-
   describe "#invitations" do
     let(:phase) { FactoryGirl.create :phase }
     let(:task) { FactoryGirl.create :invitable_task, phase: phase }

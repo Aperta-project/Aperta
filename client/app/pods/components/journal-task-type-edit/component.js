c3e94e64682cecaa8f08f0dda3d245cc38d3e086
Rizwan Reza <rizwanreza@gmail.com>
2015-06-21 02:51:58 -0700

    Make sure journal-task-type-edit component saves kind

--- client/app/pods/components/journal-task-type-edit/component.js
+++ client/app/pods/components/journal-task-type-edit/component.js
@@ -4,31 +4,35 @@ export default Ember.Component.extend({
   classNames: ['individual-task-type'],
 
   roles: null,
+
   selectedRole: Ember.computed('model.role', function() {
     let role = this.get('availableTaskRoles').findBy('kind', this.get('model.role'));
+
     if(Ember.isEmpty(role)) { return null; }
 
     return {
       id: role.get('id'),
-      text: role.get('kind')
+      text: role.get('name')
     };
   }),
 
-  journalRoleSort: ['kind: asc'],
+  journalRoleSort: ['name: asc'],
   availableTaskRoles: Ember.computed.sort('roles', 'journalRoleSort'),
+
   formattedTaskRoles: function() {
     return this.get('availableTaskRoles').map(function(taskRole) {
       return {
         id: taskRole.get('id'),
-        text: taskRole.get('kind')
+        text: taskRole.get('name')
       };
     });
   }.property('availableTaskRoles.@each'),
 
   actions: {
     save(roleProxy) {
-      this.set('model.role', roleProxy.text);
-      this.get('model').save().then(function(){}, function() {});
+      let role = this.get('availableTaskRoles').findBy('name', roleProxy.text);
+      this.set('model.role', role.get('kind'));
+      this.get('model').save();
     }
   }
 });

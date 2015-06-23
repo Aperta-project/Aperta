2198e6615459f6c5953eb22afe58f0ddd3cc72bd
Max Cantor <max@maxcantor.net>
2015-06-23 17:44:48 -0400

    Placate the hound.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 if ENV.has_key? 'AWS_ACCESS_KEY_ID'
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => ENV['AWS_REGION']
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION']
     }
     config.fog_directory  = Rails.application.config.s3_bucket
     config.fog_public     = false

a79dd7724975094be60671d3ac99436542c6ba64
Jerry Nummi <jerry@neo.com>
2015-06-23 12:15:48 -0400

    Simplify discussions route generation

--- client/app/mixins/discussions/route-paths.js
+++ client/app/mixins/discussions/route-paths.js
@@ -2,17 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   _setupTopicsPaths: Ember.on('init', function() {
-    // if not coming from subroute like edit or workflow: `paper`
-    // else: `paper.subroute`
-    let noSubRoute = Ember.isEmpty(this.get('subRouteName'));
-    let parentPath = 'paper' + (noSubRoute ? '' : '.' + this.get('subRouteName'));
-    let basePath   = parentPath + '.discussions';
+    let base = 'paper.' + this.get('subRouteName') + '.discussions.';
 
     this.setProperties({
-      topicsParentPath: parentPath,
-      topicsIndexPath:  basePath,
-      topicsNewPath:    basePath + '.new',
-      topicsShowPath:   basePath + '.show'
+      topicsParentPath: 'paper.' + this.get('subRouteName'),
+      topicsIndexPath:  base + 'index',
+      topicsNewPath:    base + 'new',
+      topicsShowPath:   base + 'show'
     });
   })
 });

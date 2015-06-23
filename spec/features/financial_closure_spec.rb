46c5a702a0100ab7aad5c2367a4b64e81fca6cc9
Willa Wang <willa@neo.com>
2015-06-23 14:27:54 -0400

    Added feature test.

--- spec/features/financial_closure_spec.rb
+++ spec/features/financial_closure_spec.rb
@@ -5,7 +5,7 @@ feature 'Financial Closure', js: true do
   let(:paper)  { create :paper, :with_tasks, :with_valid_plos_author, submitted: false, creator: admin }
   let(:letter_body)  { "Foo Bar, Hello World" }
 
-  scenario 'finishes the cover letter and save it', selenium: true do
+  scenario 'adds the financial closure card and view it', selenium: true do
     sign_in_page = SignInPage.visit
     edit_page = sign_in_page.sign_in admin
 
@@ -18,28 +18,6 @@ feature 'Financial Closure', js: true do
     within(".overlay-action-buttons") do
       find("button", text: "ADD").click
     end
-    find("div.card-content", text: 'Financial Disclosure').click
-
-    # first("div", text: "Financial Disclosure").click
-    # save_and_open_page
-    find('input', text: 'Yes').click
-    expect(page).to have_contentb 'Funder'
-    # expect(page).to have_content 'Cover Letter'
-    # find('.card-content', text: 'Cover Letter').click
-    # expect(page).to have_css('.edit-cover-letter')
-    #
-    # within '.edit-cover-letter' do
-    #   find('.taller-textarea').set(letter_body)
-    #   click_button 'Save'
-    # end
-    #
-    # expect(page).to_not have_css('.edit-cover-letter')
-    # expect(page).to have_css('.preview-cover-letter')
-    # within '.preview-cover-letter' do
-    #   expect(page).to have_content letter_body
-    #   click_button 'Make Changes'
-    # end
-    #
-    # expect(page).to have_css('.edit-cover-letter')
+    expect(page).to have_content 'Financial Disclosure'
   end
 end

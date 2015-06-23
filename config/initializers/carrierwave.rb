16d88ebafc0991058e8f901befe1f645805b1db6
Max Cantor <max@maxcantor.net>
2015-06-23 16:51:53 -0400

    Don't explode if CarrierWave can't be initialized.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,15 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => ENV['AWS_REGION']
-  }
-  config.fog_directory  = Rails.application.config.s3_bucket
-  config.fog_public     = false
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+if ENV.has_key? 'AWS_ACCESS_KEY_ID'
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => ENV['AWS_REGION']
+    }
+    config.fog_directory  = Rails.application.config.s3_bucket
+    config.fog_public     = false
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+  end
+else
+  Rails.logger.warn "AWS_ACCESS_KEY_ID not found in ENV; CarrierWave is disabled."
 end

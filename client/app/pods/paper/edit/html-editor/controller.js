c6613cdfbf3c0c615ec0e4e8b97599747503134e
Rizwan Reza <rizwanreza@gmail.com>
2015-06-23 15:37:47 -0700

    Remove controller.coffee as it reappeared due to merge

--- client/app/pods/paper/edit/html-editor/controller.js
+++ client/app/pods/paper/edit/html-editor/controller.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 import PaperBaseMixin from 'tahi/mixins/controllers/paper-base';
 import PaperEditMixin from 'tahi/mixins/controllers/paper-edit';
+import DiscussionsRoutePathsMixin from 'tahi/mixins/discussions/route-paths';
 
-var HtmlEditorController = Ember.Controller.extend(PaperBaseMixin, PaperEditMixin, {
+var HtmlEditorController = Ember.Controller.extend(PaperBaseMixin, PaperEditMixin, DiscussionsRoutePathsMixin, {
+  subRouteName: 'edit',
+
+  // initialized by paper/edit/view
+  toolbar: null,
+
+  // used to recover a selection when returning from another context (such as figures)
   isEditing: Ember.computed.alias('lockedByCurrentUser'),
   hasOverlay: false,
+
   editorComponent: 'tahi-editor-ve',
 
   paperBodyDidChange: function() {

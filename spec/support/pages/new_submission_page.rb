25c27230be4d044b6a5a33bae336eb6cc09a9397
Jerry Nummi <jerry@neo.com>
2015-06-24 08:58:01 -0400

    Fix new paper spec

--- spec/support/pages/new_submission_page.rb
+++ spec/support/pages/new_submission_page.rb
@@ -1,7 +1,8 @@
 class NewSubmissionPage < Page
-  def create_submission short_title, journal: Journal.first.name
+  def create_submission(short_title:, journal:, paper_type:)
     fill_in 'paper-short-title', with: short_title
     select2 journal, css: ".paper-new-journal-select"
+    select2 paper_type, css: ".paper-new-paper-type-select"
     click_on 'Create'
     expect(session).to have_css('#paper-title')
     EditPaperPage.new

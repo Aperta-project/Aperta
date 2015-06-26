4acfdbc4e994ef00050b3c03db74d0edc1e57deb
Rizwan Reza <rizwanreza@gmail.com>
2015-06-26 13:36:57 -0700

    Namespace ExternalReferencesController

--- spec/controllers/editor/external_references_controller_spec.rb
+++ spec/controllers/editor/external_references_controller_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
-describe ExternalReferencesController do
-  describe "GET /crossref" do
+describe Editor::ExternalReferencesController do
+  describe "GET /editor/crossref" do
     it "makes a request to Crossref with the given param" do
-      expect(RestClient).to receive(:get).with("http://search.crossref.org/dois?q=hello_world&sort=score").and_return('{}')
+      expect(RestClient).to receive(:get).
+        with("http://search.crossref.org/dois?q=hello_world&sort=score").
+        and_return('{}')
       get :crossref, query: "hello_world"
     end
   end
 
-  describe "GET /doi" do
+  describe "GET /editor/doi" do
     it "makes a request to DOI with the given param" do
       expect(RestClient).to receive(:get).
         with("http://dx.doi.org/hello_world/1/3/4", accept: "application/citeproc+json").

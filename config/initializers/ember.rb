1cc460190e8e9abf0048fbceb68155d3b84a13d7
Max Cantor <max@maxcantor.net>
2015-06-26 14:39:16 -0400

    Increase build timeout again, this time to 35s.
    Now I can finally get a first build without my browser timing out and
    the server console printing the "consider increasing your timeout"
    message.

--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -1,7 +1,7 @@
 EmberCLI.configure do |c|
   c.app(:tahi, {
     path: Rails.root.join('client'),
-    build_timeout: 22,
+    build_timeout: 35,
     enable: -> path { !%r{/(?:api|assets)/}.match(path) }
   })
 end

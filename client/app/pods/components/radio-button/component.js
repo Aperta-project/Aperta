43eef4afa7617cb85073cb32a125507f755d7da8
Ryan Wold <pair+ryan.wold@neo.com>
2015-06-26 20:50:56 -0700

    persist the register decision radio button

--- client/app/pods/components/radio-button/component.js
+++ client/app/pods/components/radio-button/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'input',
   type: 'radio',
-  attributeBindings: ['name', 'type', 'value', 'checked:checked:', 'disabled'],
+  attributeBindings: ['name', 'type', 'value', 'checked:checked', 'disabled'],
 
   checked: function() {
     return this.get('selection') === this.get('value');

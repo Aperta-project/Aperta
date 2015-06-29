8fe98e639a5ee7c4d10239971a032ac3e5dca2b1
Jerry Nummi <jerry@neo.com>
2015-06-29 10:36:53 -0400

    Store override to service

---
+++ client/app/services/store.js
@@ -0,0 +1,42 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.Store.extend({
+  push(type, data, _partial) {
+    let oldRecord;
+    let oldType = type;
+    let dataType = data.type;
+    let modelType = oldType;
+
+    if (dataType && (this.modelFor(oldType) !== this.modelFor(dataType))) {
+      modelType = dataType;
+      if (oldRecord = this.peekRecord(oldType, data.id)) {
+        this.dematerializeRecord(oldRecord);
+      }
+    }
+
+    return this._super(modelType, data, _partial);
+  },
+
+  findTask(id) {
+    let matchingTask = this.allTaskClasses().find(function(tm) {
+      return tm.idToRecord[id];
+    });
+
+    if (matchingTask) {
+      return matchingTask.idToRecord[id];
+    }
+  },
+
+  allTaskClasses() {
+    return Ember.keys(this.typeMaps).reduce((memo, key) => {
+      let typeMap = this.typeMaps[key];
+
+      if (typeMap.type.toString().match(/:.*task:/)) {
+        memo.addObject(typeMap);
+      }
+
+      return memo;
+    }, []);
+  }
+});

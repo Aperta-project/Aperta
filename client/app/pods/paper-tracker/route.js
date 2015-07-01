52f89792e1eade136052e8be93b1fc2d09605a52
Jerry Nummi <jerry@neo.com>
2015-07-01 10:20:39 -0400

    Adjust paper-tracker table styles
    — Remove badge-count Handlebars helper

--- client/app/pods/paper-tracker/route.js
+++ client/app/pods/paper-tracker/route.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return [{
-      title: 'Life Changing Science Right Here',
+      title: 'Life Changing Science Right Here Super Awesome Sauce Science Life Changing Science Right Here Super Awesome Sauce Science',
       id: 11111,
       dateSubmitted: new Date(),
       paperType: 'Research'

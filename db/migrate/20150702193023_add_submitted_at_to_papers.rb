1f92d30259379b4851bb9f9afab9fd58ceea33ca
Sam Bleckley <sam@mutuallyhuman.com>
2015-07-02 16:48:49 -0400

    Fix migration to support already submitted papers.

--- db/migrate/20150702193023_add_submitted_at_to_papers.rb
+++ db/migrate/20150702193023_add_submitted_at_to_papers.rb
@@ -1,5 +1,11 @@
 class AddSubmittedAtToPapers < ActiveRecord::Migration
-  def change
+  def up
     add_column :papers, :submitted_at, :datetime
+
+    execute "UPDATE papers SET submitted_at = created_at WHERE publishing_state <> 'unsubmitted'"
+  end
+
+  def down
+    remove_column :papers, :submitted_at
   end
 end

6700d2999202823eb2b73fe00f0c4ab98b1983a3
Jerry Nummi <jerry@neo.com>
2015-07-08 16:39:09 -0400

    Paper new test fix

--- client/tests/integration/paper-new-test.js
+++ client/tests/integration/paper-new-test.js
@@ -25,6 +25,8 @@ module('Integration: Create new paper', {
     TestHelper.handleFindAll('paper', 0);
     TestHelper.handleFindAll('invitation', 0);
     TestHelper.handleFindAll('comment-look', 0);
+    TestHelper.handleFindAll('discussion-topic', 0);
+    $.mockjax({url: /\/api\/papers\/\d+\/manuscript_manager/, status: 204});
   }
 });
 

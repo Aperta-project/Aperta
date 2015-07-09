b5c68c85e89b14dbfd97eda0bd8ad1642cdb0b20
Rizwan Reza <rizwanreza@gmail.com>
2015-07-09 16:40:14 -0700

    Captions don't need to limited to 255 characters

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702193023) do
+ActiveRecord::Schema.define(version: 20150709230305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,7 +167,7 @@ ActiveRecord::Schema.define(version: 20150702193023) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title",      limit: 255
-    t.string   "caption",    limit: 255
+    t.text     "caption"
     t.string   "status",     limit: 255, default: "processing"
   end
 

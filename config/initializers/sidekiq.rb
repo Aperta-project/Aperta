177acb738b21590ff0cca9aa97c150a8597de227
Edgecase Pairing <aaron@mutuallyhuman.com>
2015-07-09 16:10:30 -0400

    Configure Sidekiq Worker count without a deploy
    Heroku & friends enforce restrictive connection limits on applications
    as a way to force upgrades. Postgres and Redis aren't really designed to
    have such low connection limits. Most clients of both handle connection
    limit errors pretty poorly.
    
    This is super problematic when you start scaling your application
    horizontally and cause dramatic increases in the number of connections
    being made.
    
    A worked example with gem defaults:
    
    Let's say we have 2 Web dynos + 2 Sidekiq dyno.
    
    Each Web dyno runs 3 Puma processes (workers). Each Puma process
    allocates 16 threads. It knows to have Rails allocate a Postgres
    connection for each thread. Each process also allocates 5 Redis
    connections for Sidekiq (possibly more if we use Redis outside Sidekiq).
    
    Each Sidekiq dyno runs 1 Sidekiq process. Each Sidekiq process starts up
    25 threads. It knows to have Rails allocate a Postgres connection for
    each thread. It also allocates a Redis connection for each thread, plus
    2 additional Redis connections.
    
    If you're still playing along at home, that's a total of:
    
    Postgres: 2 * 3 * 16 + 2 * 25 = 146 connections
    Redis: 2 * 3 * 5 + 2 * 25 + 2 = 82 connections
    
    Let's say we see heavy load and decide to add another Web dyno.
    
    Postgres: 3 * 3 * 16 + 2 * 25 = 194 connections
    Redis: 3 * 3 * 5 + 2 * 25 + 2 = 97 connections
    
    Another Sidekiq dyno.
    
    Postgres: 3 * 3 * 16 + 3 * 25 = 219 connections
    Redis: 3 * 3 * 5 + 3 * 25 + 2 = 122 connections
    
    This is pretty crazy when you compare this with the absurdly low limits
    for the initial plans for Heroku's Postgres and Redis Cloud:
    
    Postgres:
    Free: 20 connections
    $9/mo: 20 connections
    $50/mo: 120 connections
    $200/mo: 400 connections
    
    Redis:
    Free: 30 connections
    $10/mo: 256 connections
    $52/mo: 512 connections
    
    This isn't so bad if you can configure things on the fly, but it is easy
    to see how things get out of hand quickly. We'll almost always use lower
    limits than the gem defaults, but it is most easily handled by ENV var
    configurations.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,19 @@ Sidekiq.configure_server do |config|
   config.redis = {
     namespace: "tahi_#{Rails.env}"
   }
+
+  # allow configuration of concurrency without redeploy
+  # controls the number of redis connections
+  sidekiq_workers = Integer(ENV['SIDEKIQ_CONCURRENCY'] || 25)
+  config.options[:concurrency] = sidekiq_workers
+
+  ActiveSupport.on_load(:active_record) do
+    ar_config = ActiveRecord::Base.configurations[Rails.env]
+    ar_config['pool'] = sidekiq_workers
+    ActiveRecord::Base.establish_connection(ar_config)
+  end
+
+  puts "Sidekiq started with WORKERS=#{sidekiq_workers}"
 end
 
 Sidekiq.configure_client do |config|

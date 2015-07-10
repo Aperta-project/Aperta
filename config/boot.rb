f34c77f16b0cbdf880da809d8079014397ebd8ae
Jerry Nummi <jerry@neo.com>
2015-07-10 15:53:53 -0400

    New question-radio block syntax

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,16 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+puts "\n"
+puts "\n"
+puts " 88888888888       888      d8b "
+puts "     888           888      Y8P "
+puts "     888           888          "
+puts "     888   8888b.  88888b.  888 "
+puts '     888      "88b 888 "88b 888 '
+puts "     888  .d888888 888  888 888 "
+puts "     888  888  888 888  888 888 "
+puts '     888  "Y888888 888  888 888 '
+puts "\n"
+puts "\n"

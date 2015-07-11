90b5307b0ad3667847a1282eec10186e731d5dc4
Jerry Nummi <jerry@neo.com>
2015-07-11 16:23:00 -0400

    AdditionalData test cleanup

--- client/tests/components/additional-datasets-test.js
+++ client/tests/components/additional-datasets-test.js
@@ -11,7 +11,7 @@ moduleForComponent('additional-datasets', 'AdditionalDatasets', {
 });
 
 test('with additional-datasets it renders them and a button to add more', function(assert) {
-  let fakeQuestion = Ember.Object.create({
+  let question = Ember.Object.create({
     ident: 'foo',
     additionalData: [{}, {}],
     question: 'Test Question',
@@ -20,7 +20,7 @@ test('with additional-datasets it renders them and a button to add more', functi
   });
 
   this.set('task', Ember.Object.create({
-    questions: [fakeQuestion]
+    questions: [question]
   }));
 
   this.render(hbs`
@@ -33,7 +33,7 @@ test('with additional-datasets it renders them and a button to add more', functi
   assert.ok(this.$("button:contains('Add Dataset')").length, 'Renders an Add Dataset button');
 });
 
-test('with additional-datasets it renders them and a button to add more', function(assert) {
+test('it uses dataset-* components to render attributes on additionalData', function(assert) {
    let additionalDataItem = {
      contact: "test contact",
      description: "test description",
@@ -43,7 +43,7 @@ test('with additional-datasets it renders them and a button to add more', functi
      url: "test url"
    };
 
-  let fakeQuestion = Ember.Object.create({
+  let question = Ember.Object.create({
     ident: 'foo',
     additionalData: [additionalDataItem],
     question: 'Test Question',
@@ -52,7 +52,7 @@ test('with additional-datasets it renders them and a button to add more', functi
   });
 
   this.set('task', Ember.Object.create({
-    questions: [fakeQuestion]
+    questions: [question]
   }));
 
   this.render(hbs`
@@ -68,66 +68,16 @@ test('with additional-datasets it renders them and a button to add more', functi
     {{/question-check}}
   `);
 
-  let assertValue = function(context, selector, value, message) {
-    return assert.equal(context.$(selector).val(), value, message);
-  };
-
-  assertValue(this, 'textarea[name="contact"]',  'test contact',     'contact is a textarea');
-  assertValue(this, 'input[name="description"]', 'test description', 'description is an input');
-  assertValue(this, 'input[name="accession"]',   'test doi',         'doi is an input');
-  assertValue(this, 'input[name="title"]',       'test title',       'title is an input');
-  assertValue(this, 'input[name="url"]',         'test url',         'url is an input');
-  assertValue(this, 'textarea[name="reasons"]',  'test reasons',     'reasons is a textarea');
+  assert.equal(this.$('textarea[name="contact"]').val(),  'test contact',     'contact is a textarea');
+  assert.equal(this.$('input[name="description"]').val(), 'test description', 'description is an input');
+  assert.equal(this.$('input[name="accession"]').val(),   'test doi',         'doi is an input');
+  assert.equal(this.$('input[name="title"]').val(),       'test title',       'title is an input');
+  assert.equal(this.$('input[name="url"]').val(),         'test url',         'url is an input');
+  assert.equal(this.$('textarea[name="reasons"]').val(),  'test reasons',     'reasons is a textarea');
 
   this.$('input[name="description"]').val('New description').change();
   this.$('textarea[name="contact"]').val('New contact').change();
+
   assert.equal(additionalDataItem.description, 'New description', 'additionalData syncs inputs to the value of the fields');
   assert.equal(additionalDataItem.contact,     'New contact',     'additionalData syncs to the textareas to the value of the fields');
 });
-
-// test('it uses dataset-* components to render attributes on additonalData', function() {
-//   var $component, additionalDataItem, assertValue, component, fakeQuestion, task, template;
-//   additionalDataItem = {
-//     contact: "test contact",
-//     description: "test description",
-//     accession: "test doi",
-//     reasons: "test reasons",
-//     title: "test title",
-//     url: "test url"
-//   };
-//   fakeQuestion = Ember.Object.create({
-//     ident: "foo",
-//     save: function() {
-//       return null;
-//     },
-//     additionalData: [additionalDataItem],
-//     question: "Test Question",
-//     answer: true
-//   });
-//   task = Ember.Object.create({
-//     questions: [fakeQuestion]
-//   });
-//   template = "{{#additional-datasets}} {{dataset-contact}} {{dataset-description}} {{dataset-doi}} {{dataset-reasons}} {{dataset-title}} {{dataset-url}} {{/additional-datasets}}";
-//   component = this.subject({
-//     ident: "foo",
-//     task: task,
-//     template: Ember.Handlebars.compile(template)
-//   });
-//   this.render();
-//   $component = this.subject().$();
-//   assertValue = function(component, selector, value, message) {
-//     return equal(component.find(selector).val(), value, message);
-//   };
-//   assertValue($component, "textarea[name='contact']", 'test contact', 'contact is a textarea');
-//   assertValue($component, "input[name='description']", 'test description', 'description is an input');
-//   assertValue($component, "input[name='accession']", 'test doi', 'doi is an input');
-//   equal($component.find("textarea[name='reasons']").val(), 'test reasons', 'reasons is a textarea');
-//   assertValue($component, "input[name='title']", 'test title', 'title is an input');
-//   assertValue($component, "input[name='url']", 'test url', 'url is an input');
-//   fillIn("input[name='description']", "New description");
-//   fillIn("textarea[name='contact']", "New contact");
-//   return andThen(function() {
-//     equal(additionalDataItem.description, 'New description', 'additionalData syncs inputs to the value of the fields');
-//     return equal(additionalDataItem.contact, 'New contact', 'additionalData syncs to the textareas to the value of the fields');
-//   });
-// });

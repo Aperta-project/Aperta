cc9eab57cefab93a1ea3cee2f2cee904336e3ae7
Edgecase Pairing <aaron@mutuallyhuman.com>
2015-07-15 09:32:59 -0400

    Add default puma support to deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,23 @@ set :linked_files, %w{config/database.yml}
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/assets', 'public/system')
 
-# Default value for keep_releases is 5
+# Puma config
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [16, 16]
+set :puma_workers, 2
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+set :puma_preload_app, true
+
 # npm
 set :npm_target_path, -> { "#{release_path}/client" }
 set :bower_target_path, -> { "#{release_path}/client" }

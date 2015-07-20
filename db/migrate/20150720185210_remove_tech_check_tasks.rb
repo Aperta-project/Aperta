f4b955bde2d82ed4519a3f26de0b40fc20015c49
Ryan Wold <wold@afomi.com>
2015-07-20 16:22:04 -0700

    also cleanup JournalTaskTypes

--- db/migrate/20150720185210_remove_tech_check_tasks.rb
+++ db/migrate/20150720185210_remove_tech_check_tasks.rb
@@ -3,5 +3,9 @@ class RemoveTechCheckTasks < ActiveRecord::Migration
     execute <<-SQL
       DELETE FROM "tasks" WHERE "tasks"."type" = 'TahiStandardTasks::TechCheckTask';
     SQL
+
+    execute <<-SQL
+      DELETE FROM "journal_task_types" WHERE "kind" = 'TahiStandardTasks::TechCheckTask';
+    SQL
   end
 end

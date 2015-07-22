1ec2f87a3f67aec6a2a1810a34603692fbdaae0c
Sam Bleckley <sam@mutuallyhuman.com>
2015-07-22 16:30:19 -0400

    Positioning cheat, spinner while searching.

--- client/app/pods/components/auto-suggest/component.js
+++ client/app/pods/components/auto-suggest/component.js
@@ -2,12 +2,15 @@ import Ember from 'ember';
 import RESTless from 'tahi/services/rest-less';
 
 export default Ember.Component.extend({
+  classNames: ['form-control', 'auto-suggest-border'],
+
   // attrs:
   endpoint: null,
   queryParameter: null,
   parseResponseFunction: null,
   itemDisplayTextFunction: null,
   placeholder: null,
+  debounce: 300,
 
   // props:
   highlightedItem: null,
@@ -15,8 +18,12 @@ export default Ember.Component.extend({
   searchAllowed: true,
   searchResults: null,
   selectedItem: null,
+  searching: 0,
 
   search() {
+    if (!this.get('resultText')) return;
+
+    this.incrementProperty('searching');
     let url = this.get('endpoint');
     let data = {};
     data[this.get('queryParameter')] = this.get('resultText');
@@ -24,19 +31,23 @@ export default Ember.Component.extend({
     RESTless.get(url, data).then((response) => {
       let results = this.get('parseResponseFunction')(response);
       this.set('searchResults',  results);
+      this.decrementProperty('searching');
+    },
+    (response) => {
+      this.decrementProperty('searching');
     });
   },
 
   _resultTextChanged: Ember.observer('resultText', function() {
     if(this.get('searchAllowed')) {
-      Ember.run.debounce(this, this.search, 150);
+      Ember.run.debounce(this, this.search, this.get('debounce'));
     }
 
     this.set('searchAllowed', true);
   }),
 
   _setupKeybindings: Ember.on('didInsertElement', function() {
-    $(document).on('keyup.autosuggest', (event)=> {
+    $(document).on('keyup.autosuggest', (event) => {
       if (event.which === 27) {
         this.set('highlightedItem', null);
       }

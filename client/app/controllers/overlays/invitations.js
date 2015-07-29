20a4a3ba5492349a2820195fd4c69e233c849925
Jerry Nummi <jerry@neo.com>
2015-07-29 16:26:18 -0400

    paper-base cleanup

--- client/app/controllers/overlays/invitations.js
+++ client/app/controllers/overlays/invitations.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  needs: ['dashboard/index'],
   overlayClass: 'overlay--fullscreen invitations-overlay',
 
   invitations: Ember.computed.reads('currentUser.invitedInvitations'),
--- client/app/mixins/controllers/paper-base.js
+++ client/app/mixins/controllers/paper-base.js
@@ -2,58 +2,58 @@ import Ember from 'ember';
 import DocumentDownload from 'tahi/services/document-download';
 import ENV from 'tahi/config/environment';
 
+let computed = Ember.computed;
+
 export default Ember.Mixin.create({
   needs: ['application', 'paper'],
   isAdmin: Ember.computed.alias('currentUser.siteAdmin'),
   canViewManuscriptManager: false,
 
-  supportedDownloadFormats: function() {
+  supportedDownloadFormats: computed(function() {
     return ENV.APP.iHatExportFormats.map(formatType => {
       return {format: formatType, icon: `svg/${formatType}-icon`};
     });
-  }.property(),
+  }),
 
-  downloadLink: function() {
+  downloadLink: computed('model.id', function() {
     return '/papers/' + this.get('model.id') + '/download';
-  }.property('model.id'),
+  }),
 
-  logoUrl: function() {
+  logoUrl: computed('model.journal.logoUrl', function() {
     let logoUrl = this.get('model.journal.logoUrl');
     return (/default-journal-logo/.test(logoUrl)) ? false : logoUrl;
-  }.property('model.journal.logoUrl'),
+  }),
 
-  pageContainerHTMLClass: function() {
+  pageContainerHTMLClass: computed('model.editorMode', function() {
     return 'paper-container-' + this.get('model.editorMode');
-  }.property('model.editorMode'),
+  }),
 
   // Tasks:
-  assignedTasks: function() {
+  assignedTasks: computed('model.tasks.@each', function() {
     let metadataTasks = this.get('metadataTasks');
-    var that = this;
 
     return this.get('model.tasks').filter((task) => {
-      return task.get('participations').mapBy('user').contains(that.get('currentUser'));
+      return task.get('participations').mapBy('user').contains(this.get('currentUser'));
     }).filter(function(t) {
       return !metadataTasks.contains(t);
     });
-  }.property('model.tasks.@each'),
+  }),
 
-  metadataTasks: function() {
+  metadataTasks: computed('model.tasks.@each.role', function() {
     return this.get('model.tasks').filter((task) => {
       return task.get('isMetadataTask');
     });
-  }.property('model.tasks.@each.role'),
+  }),
 
   taskSorting:         ['phase.position', 'position'],
   sortedMetadataTasks: Ember.computed.sort('metadataTasks',   'taskSorting'),
   sortedAssignedTasks: Ember.computed.sort('assignedTasks', 'taskSorting'),
 
-  noTasks: function() {
+  noTasks: computed('assignedTasks.@each', 'metadataTasks.@each', function() {
     return [this.get('assignedTasks'), this.get('metadataTasks')].every((taskGroup)=> {
       return Ember.isEmpty(taskGroup);
     });
-  }.property('assignedTasks.@each', 'metadataTasks.@each'),
-
+  }),
 
   actions: {
     'export': function(downloadType) {

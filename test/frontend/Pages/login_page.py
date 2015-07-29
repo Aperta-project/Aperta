0d2861d4ea89f1136ab4ebbd22da3b3c1a295753
Mr. Jeffrey Gray <jgray@plos.org>
2015-07-29 15:43:28 -0700

    Implemented test case for Login page and associated password reset page.

--- test/frontend/Pages/login_page.py
+++ test/frontend/Pages/login_page.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 from selenium.webdriver.common.by import By
-from ...Base.PlosPage import PlosPage
+from Base.PlosPage import PlosPage
 
 __author__ = 'jgray@plos.org'
 
@@ -13,7 +13,7 @@ class LoginPage(PlosPage):
   def __init__(self, driver):
     super(LoginPage, self).__init__(driver, '/users/sign_in')
 
-    #Locators - Instance members
+    # Locators - Instance members
     self._welcome_message = (By.TAG_NAME, 'h1')
     self._login_textbox = (By.CSS_SELECTOR, '#user_login')
     self._password_textbox = (By.CSS_SELECTOR, '#user_password')
@@ -24,12 +24,17 @@ class LoginPage(PlosPage):
     self._signup_link = (By.CLASS_NAME, 'auth-signup')
     self._alert_text = (By.CLASS_NAME, 'auth-flash--alert')
     self._notice_text = (By.CLASS_NAME, 'auth-flash--notice')
-    # target page locators
+    # dashboard page locators
     self._loggedin_nav_toggle = (By.CLASS_NAME, 'navigation-toggle')
     self._loggedin_signout_link = (By.CSS_SELECTOR, 'div.navigation > a')
+    # forgot pw page locators
+    self._fyp_title = (By.TAG_NAME, 'h1')
+    self._fyp_email_field = (By.ID, 'user_email')
+    self._fyp_send_reset_btn = (By.CSS_SELECTOR, 'input.button--green')
+    self._fyp_signin_btn = (By.CSS_SELECTOR, 'a.button--grey')
+    self._fyp_error = (By.CSS_SELECTOR, '#error_explanation ul li')
 
-
-  #POM Actions
+  # POM Actions
   def validate_initial_page_elements_styles(self):
     welcome_msg = self._get(self._welcome_message)
     assert welcome_msg.text == 'Welcome to Tahi'
@@ -45,6 +50,8 @@ class LoginPage(PlosPage):
     assert forgot_msg.value_of_css_property('font-weight') == '400'
     assert forgot_msg.value_of_css_property('line-height') == '20px'
     assert forgot_msg.value_of_css_property('color') == 'rgba(57, 163, 41, 1)'
+    remember_cb = self._get(self._remember_me_cb)
+    assert not remember_cb.is_selected()
 
     self._get(self._remember_me_cb)
     remember_msg = self._get(self._remember_me_label).text
@@ -73,17 +80,102 @@ class LoginPage(PlosPage):
     assert signout_msg.value_of_css_property('line-height') == '20px'
     assert signout_msg.value_of_css_property('color') == 'rgba(57, 163, 41, 1)'
     assert signout_msg.value_of_css_property('background-color') == 'rgba(234, 253, 231, 1)'
-    assert 'Signed out successfully.' in signout_msg.text  # where is there an extra span here?
+    assert 'Signed out successfully.' in signout_msg.text  # why is there an extra span here?
+
+  def validate_reset_pw_msg(self):
+    reset_msg = self._get(self._notice_text)
+    assert 'helvetica' in reset_msg.value_of_css_property('font-family')
+    assert reset_msg.value_of_css_property('font-size') == '14px'
+    assert reset_msg.value_of_css_property('font-weight') == '400'
+    assert reset_msg.value_of_css_property('line-height') == '20px'
+    assert reset_msg.value_of_css_property('color') == 'rgba(57, 163, 41, 1)'
+    assert reset_msg.value_of_css_property('background-color') == 'rgba(234, 253, 231, 1)'
+    assert 'You will receive an email with instructions about how to reset your password in a few minutes.' \
+           in reset_msg.text  # why is there an extra span here?
 
   def validate_invalid_login_attempt(self):
     alert_msg = self._get(self._alert_text)
-    assert 'Invalid email or password.' in alert_msg.text  # where is there an extra span here?
+    assert 'Invalid email or password.' in alert_msg.text  # why is there an extra span here?
     assert 'helvetica' in alert_msg.value_of_css_property('font-family')
     assert alert_msg.value_of_css_property('font-size') == '14px'
     assert alert_msg.value_of_css_property('font-weight') == '400'
     assert alert_msg.value_of_css_property('line-height') == '20px'
     assert alert_msg.value_of_css_property('color') == 'rgba(133, 63, 89, 1)'
     assert alert_msg.value_of_css_property('background-color') == 'rgba(246, 239, 232, 1)'
+
+  def enter_fyp_field(self, email):
+    """Enter email"""
+    self._get(self._fyp_email_field).clear()
+    self._get(self._fyp_email_field).send_keys(email)
+
+  def click_sri_button(self):
+    """Click Send Reset instructions button"""
+    self._get(self._fyp_send_reset_btn).click()
+
+  def open_fyp(self):
+    self._get(self._forgot_pw_link).click()
+
+  def close_fyp(self):
+    signin_btn = self._get(self._fyp_signin_btn)
+    signin_btn.click()
+
+  def validate_fyp_elements_styles_function(self):
+    pw_reset_ttl = self._get(self._fyp_title)
+    assert pw_reset_ttl.text == 'Forgot your password?'
+    assert 'helvetica' in pw_reset_ttl.value_of_css_property('font-family')
+    assert pw_reset_ttl.value_of_css_property('font-size') == '44px'
+    assert pw_reset_ttl.value_of_css_property('font-weight') == '400'
+    assert pw_reset_ttl.value_of_css_property('line-height') == '62.85px'
+    assert pw_reset_ttl.value_of_css_property('text-align') == 'center'
+    email_input = self._get(self._fyp_email_field)
+    assert email_input.get_attribute('placeholder') == 'Email'
+    assert email_input.value_of_css_property('width') == '205px'
+    assert email_input.value_of_css_property('line-height') == '18px'
+    send_reset_btn = self._get(self._fyp_send_reset_btn)
+    assert send_reset_btn.get_attribute('value') == 'Send reset instructions'
+    assert 'helvetica' in send_reset_btn.value_of_css_property('font-family')
+    assert send_reset_btn.value_of_css_property('font-size') == '14px'
+    assert send_reset_btn.value_of_css_property('font-weight') == '400'
+    assert send_reset_btn.value_of_css_property('line-height') == '20px'
+    assert send_reset_btn.value_of_css_property('color') == 'rgba(255, 255, 255, 1)'
+    assert send_reset_btn.value_of_css_property('text-align') == 'center'
+    assert send_reset_btn.value_of_css_property('text-transform') == 'uppercase'
+    send_reset_btn.click()
+    assert self._get(self._fyp_error).text == "Email can't be blank"
+    signin_btn = self._get(self._fyp_signin_btn)
+    assert signin_btn.text == 'Sign in'
+    assert 'helvetica' in signin_btn.value_of_css_property('font-family')
+    assert signin_btn.value_of_css_property('font-size') == '16px'
+    assert signin_btn.value_of_css_property('font-weight') == '600'
+    assert signin_btn.value_of_css_property('line-height') == '22.85px'
+    assert signin_btn.value_of_css_property('color') == 'rgba(255, 255, 255, 1)'
+    assert signin_btn.value_of_css_property('text-align') == 'center'
+    assert signin_btn.value_of_css_property('vertical-align') == 'middle'
+
+  def validate_fyp_email_fmt_error(self):
+    email_field = self._get(self._fyp_email_field)
+    assert email_field.value_of_css_property('border-top-color') == 'rgba(204, 204, 205, 1)'
+    assert email_field.value_of_css_property('border-bottom-color') == 'rgba(204, 204, 205, 1)'
+    assert email_field.value_of_css_property('border-left-color') == 'rgba(204, 204, 205, 1)'
+    assert email_field.value_of_css_property('border-right-color') == 'rgba(204, 204, 205, 1)'
+
+  def click_remember_me(self):
+    self._get(self._remember_me_cb).click()
+
+  def validate_remember_me(self, login, pw):
+    """
+    Validates the remember me function on the page insofar as the relevant cookie is set
+    :param login: valid login string
+    :param pw: valid pw string
+    """
+    cookie = self._driver.get_cookie('remember_user_token')
+    assert not cookie
+    self.enter_login_field(login)
+    self.enter_password_field(pw)
+    self.click_remember_me()
+    self.click_sign_in_button()
+    cookie = self._driver.get_cookie('remember_user_token')
+    assert cookie
 
   def sign_out(self):
     self._get(self._loggedin_nav_toggle).click()
--- test/frontend/test_login.py
+++ test/frontend/test_login.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-This test case validates the Aperta login page
+This test case validates the Aperta login page and associated forgot password page
 
 """
 __author__ = 'jgray@plos.org'
 
-from ..Base.Decorators import MultiBrowserFixture
-from ..Base.FrontEndTest import FrontEndTest
+from Base.Decorators import MultiBrowserFixture
+from Base.FrontEndTest import FrontEndTest
 from Pages.login_page import LoginPage
-import time
+from Base.Resources import login_valid_email, login_invalid_email, login_valid_uid, login_valid_pw, login_invalid_pw
 
 
 @MultiBrowserFixture
 class ApertaLoginTest(FrontEndTest):
+  """
+  Self imposed AC:
+     - validate page elements and styles for:
+         - login page
+         - forgot password page
+     - validate sign in and sign out and accompanying error messages
+     - validate remember me function (only by cookie validation)
+     - validate forgot password function (excludes email receipt validation)
+  """
   def test_validate_components_styles(self):
     """
     Validates the presence of the following provided elements:
@@ -21,30 +30,49 @@ class ApertaLoginTest(FrontEndTest):
     """
     login_page = LoginPage(self.getDriver())
     login_page.validate_initial_page_elements_styles()
+
     # Valid email, invalid pw
-    login_page.enter_login_field('jgray@plos.org')
-    login_page.enter_password_field('in|fury7')
+    login_page.enter_login_field(login_valid_email)
+    login_page.enter_password_field(login_invalid_pw)
     login_page.click_sign_in_button()
     login_page.validate_invalid_login_attempt()
 
     # Invalid email, invalid pw
-    login_page.enter_login_field('jgrey@plos.org')
-    login_page.enter_password_field('in|fury8')
+    login_page.enter_login_field(login_invalid_email)
+    login_page.enter_password_field(login_invalid_pw)
     login_page.click_sign_in_button()
     login_page.validate_invalid_login_attempt()
 
+    # Forgot password link - modal validation
+    login_page.open_fyp()
+    login_page.validate_fyp_elements_styles_function()
+    login_page.close_fyp()
+
     # valid email login
-    login_page.enter_login_field('jgray@plos.org')
-    login_page.enter_password_field('in|fury8')
+    login_page.enter_login_field(login_valid_email)
+    login_page.enter_password_field(login_valid_pw)
     login_page.click_sign_in_button()
     login_page.sign_out()
     login_page.validate_signed_out_msg()
 
     # valid uname login
-    login_page.enter_login_field('jgray')
-    login_page.enter_password_field('in|fury8')
+    login_page.enter_login_field(login_valid_uid)
+    login_page.enter_password_field(login_valid_pw)
     login_page.click_sign_in_button()
+    login_page.sign_out()
+    login_page.validate_signed_out_msg()
+
+    # forgotten password send email
+    login_page.open_fyp()
+    login_page.enter_fyp_field(login_valid_uid)
+    login_page.click_sri_button()
+    login_page.validate_fyp_email_fmt_error()
+    login_page.enter_fyp_field(login_valid_email)
+    login_page.click_sri_button()
+    login_page.validate_reset_pw_msg()
+
+    # Remember me function
+    login_page.validate_remember_me(login_valid_email, login_valid_pw)
 
 if __name__ == '__main__':
   FrontEndTest._run_tests_randomly()
-

a911636fa4cfb575841c1d3cd022f9438568ab29
Zach Dennis <zach.dennis@gmail.com>
2015-07-30 16:54:17 -0400

    Updating SidekiqHelperMethods#process_sidekiq_jobs to have a retry count of 5.
    * Allow for all worker jobs to be processed/drained
    * Allow for a worker job to queue up another worker job while processing (and to process that as well).
    
    This is to reduce the chance that the test fails because the Ruby thread running the tests executes entirely before the Ruby thread running the app is able to queue up jobs, process them, etc.

--- spec/support/sidekiq_helper_methods.rb
+++ spec/support/sidekiq_helper_methods.rb
@@ -1,15 +1,15 @@
 module SidekiqHelperMethods
+  class NoSidekiqJobError < ::StandardError ; end
 
-  def process_sidekiq_jobs
-    while Sidekiq::Worker.jobs.values.flatten.size > 0
-      Sidekiq::Worker.jobs.each_pair do |klass,job_data|
-        klass.drain
-      end
-
-      # Without this sleep Sidekiq may not see a job that was getting queued up
-      # during the processing the current job.
-      sleep 0.25
+  def process_sidekiq_jobs(tries = 5)
+    return if tries <= 0
+    loop do
+      raise NoSidekiqJobError if Sidekiq::Worker.jobs.values.flatten.empty?
+      Sidekiq::Worker.jobs.keys.map(&:drain)
     end
+  rescue NoSidekiqJobError
+    sleep 0.5
+    process_sidekiq_jobs tries - 1
   end
 
 end

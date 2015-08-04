e52426feb97389d1c7b4cde27612ce602d45546d
Edgecase Pairing <aaron@mutuallyhuman.com>
2015-08-04 12:01:33 -0400

    You can now hover, view details, and see the image on an ad-hoc card attachment.
    * already worked with the attachment-thumbnail emberjs component, just needed to supply the detail src
    * Updating the adhoc cards feature spec to ensure we're seeing the original preview image and the larger "big preview" image after clicking "view details"

--- app/serializers/attachment_serializer.rb
+++ app/serializers/attachment_serializer.rb
@@ -1,7 +1,7 @@
 class AttachmentSerializer < ActiveModel::Serializer
   has_one :task, embed: :id
 
-  attributes :id, :title, :caption, :kind, :src, :status, :preview_src, :filename
+  attributes :id, :title, :caption, :kind, :src, :status, :preview_src, :detail_src, :filename
 
   def src
     object.file.url
@@ -9,5 +9,9 @@ class AttachmentSerializer < ActiveModel::Serializer
 
   def preview_src
     object.file.preview.url if object.image?
+  end
+
+  def detail_src
+    object.file.url if object.image?
   end
 end
--- spec/support/upload_server/upload_server.rb
+++ spec/support/upload_server/upload_server.rb
@@ -1,6 +1,11 @@
 require 'sinatra/base'
+require "pry"
 
 class UploadServer < Sinatra::Base
+  def self.store
+    @@store ||= {}
+  end
+
   configure do
     set :server, :thin
   end
@@ -10,12 +15,19 @@ class UploadServer < Sinatra::Base
     response.headers['Access-Control-Allow-Method'] = 'PUT, POST'
   end
 
+  get "/:filename" do
+    send_file self.class.store[params[:filename]].path
+  end
+
   post '/' do
-    xml_response
+    filename, file = params[:file].values_at(:filename, :tempfile)
+    store(filename, file)
+    xml_response(filename)
   end
 
   put '/' do
-    xml_response
+    filename, file = params[:file].values_at(:filename, :tempfile)
+    xml_response(filename)
   end
 
   options '/' do
@@ -23,9 +35,17 @@ class UploadServer < Sinatra::Base
 
   private
 
-  def xml_response
-    content_type 'text/xml'
-    '<PostResponse><Location>http://localhost:31337/fake_s3/fake_file</Location></PostResponse>'
+  def store(key, value)
+    self.class.store[key] = value
+  end
+
+  def server_url
+    "http://#{Capybara.server_host}:#{Capybara.server_port}"
+  end
+
+  def xml_response(filename)
+    content_type 'application/xml'
+    "<PostResponse><Location>#{server_url}/fake_s3/#{filename}</Location></PostResponse>"
   end
 
   run! if app_file == $0

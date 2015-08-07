8901f7c369e43a558883cedf87a6c65275f40a61
Tad Park <tadp@users.noreply.github.com>
2015-08-07 15:26:02 -0700

    Make menu bar toggleable for versions

--- client/app/mixins/views/paper-edit.js
+++ client/app/mixins/views/paper-edit.js
@@ -59,26 +59,36 @@ export default Ember.Mixin.create(RedirectsIfEditable, {
     },
 
     hideSubNav() {
+      this.set('subNavVisible', false);
+      this.send('hideVisible');
+    },
+
+    hideVisible() {
       this.setProperties({
-        subNavVisible: false,
         contributorsVisible: false,
-        downloadsVisible: false
+        downloadsVisible: false,
+        versionsVisible: false
       });
     },
 
     showContributors() {
+      this.send('hideVisible');
       this.set('contributorsVisible', true);
-      this.set('downloadsVisible', false);
     },
 
     showDownloads() {
-      this.set('contributorsVisible', false);
+      this.send('hideVisible');
       this.set('downloadsVisible', true);
+    },
+
+    showVersions() {
+      this.send('hideVisible');
+      this.set('versionsVisible', true);
     },
 
     toggleVersioningMode() {
       this.set('controller.model.versioningMode', true)
-      this.send('showSubNav', 'version');
+      this.send('showSubNav', 'versions');
     }
   }
 });

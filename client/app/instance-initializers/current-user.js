bfde696739f68651a290fefe2f2b057f4c998482
Jerry Nummi <jerry@neo.com>
2015-08-07 15:04:44 -0400

    Current User initializer to instance-initializer

---
+++ client/app/instance-initializers/current-user.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'current-user',
+
+  initialize(instance) {
+    let data = window.currentUserData;
+    if($.isEmptyObject(data)) { return; }
+
+    delete data.affiliations;
+    delete data.user.affiliation_ids;
+
+    let store = instance.container.lookup('service:store');
+    store.push('user', data.user);
+
+    let user = store.getById('user', data.user.id);
+    instance.container.register('user:current', user, {
+      instantiate: false
+    });
+
+    instance.registry.injection('controller', 'currentUser', 'user:current');
+    instance.registry.injection('route', 'currentUser', 'user:current');
+  }
+};

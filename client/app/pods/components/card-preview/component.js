355eb0d5800cf794f1b1a0e4655d350ff4cad872
Jerry Nummi <jerry@neo.com>
2015-08-11 19:43:03 -0400

    [#100589444] right/ctrl click to open card in new tab
    Right now, the only way to open a card in another
    tab is to copy and paste the card URL into a new
    tab you open manually. You should be able to right
    click or ctrl click on a card, and have an option
    to open that card in a new tab.
    
    This is done by making the card-preview component
    an anchor element and generating the href.
    
    I didn't want to to hard wire the href so I ended
    up introducing a hack way of getting to the Router
    from a Component. I tried injecting the router into
    the card-preview component from an initializer
    but this ended up causing problems in the tests.
    
    Also slipped in a fix for a deprecation warning
    on the Discussion title input.

--- client/app/pods/components/card-preview/component.js
+++ client/app/pods/components/card-preview/component.js
@@ -1,6 +1,18 @@
+/*global Tahi:false */
+
 import Ember from 'ember';
+import ENV from 'tahi/config/environment';
+
+// `getRouter` and the `href` property are hacky but the way we're
+// handling routes + urls is un-ember-ish.
+
+let getRouter = function() {
+  return Tahi.__container__.lookup('router:main');
+};
 
 export default Ember.Component.extend({
+  tagName: 'a',
+  attributeBindings: ['href'],
   classNameBindings: [':card', 'task.completed:card--completed', 'classes'],
 
   // TODO: The templates always pass an attr of paper but it is never used
@@ -12,6 +24,14 @@ export default Ember.Component.extend({
   task: null,
   classes: '',
   canRemoveCard: false,
+
+  href: Ember.computed(function() {
+    // Getting access to the router from tests seems impossible, sorry
+    if(ENV.environment === 'test') { return '#'; }
+    let router = getRouter();
+    let args = ['paper.task', this.get('task.paper'), this.get('task')];
+    return router.generate.apply(router, args);
+  }),
 
   unreadCommentsCount: Ember.computed('task.commentLooks.@each', function() {
     // NOTE: this fn is also used for 'task-templates', who do

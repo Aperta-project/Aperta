23b1778a2031c59df6d2de482f3028f434ad8b30
Jerry Nummi <jerry@neo.com>
2015-08-13 19:13:14 -0400

    Add more options to position-near mixin
    - positionOver gives option to position over target, not directly below.
    - width option allows manual setting of width
    - also prevent element from shrinking smaller than target element

--- client/app/mixins/components/position-near.js
+++ client/app/mixins/components/position-near.js
@@ -6,13 +6,14 @@ export default Ember.Mixin.create({
   setMaxHeight: false,
   width: null,
   animationClass: null,
+  positionOver: false,
 
   // attrs:
   selector: null,
 
   position: on('didInsertElement', function() {
     let selector = this.get('selector');
-    if(Ember.isEmpty(selector)) { return; }
+    Ember.assert('position-near requires a selector property', selector);
 
     let target = Ember.$(selector);
     Ember.assert('position-near could not find target selector', target.length);
@@ -20,19 +21,41 @@ export default Ember.Mixin.create({
     let position = target.position();
     let offset   = target.offset();
     let targetHeight = target.outerHeight();
+    let windowHeight = $(window).height();
+
+    // css left
 
     let css = {
       position: 'absolute',
-      top: Math.round(position.top) + targetHeight,
       left: Math.round(position.left)
     };
 
-    if(this.get('matchWidth')) {
+    // css top
+
+    if(this.get('positionOver')) {
+      css.top = Math.round(position.top);
+    } else {
+      css.top = Math.round(position.top) + targetHeight;
+    }
+
+    // css width
+
+    if(this.get('width')) {
+      css.width = this.get('width');
+    } else if(this.get('matchWidth')) {
       css.width = target.outerWidth();
     }
 
+    // css maxHeight
+
     if(this.get('setMaxHeight')) {
-      css.maxHeight = $(window).height() - offset.top - targetHeight - 10;
+      let height = windowHeight - offset.top - targetHeight - 10;
+
+      if(height < targetHeight) {
+        height = targetHeight;
+      }
+
+      css.maxHeight = height;
     }
 
     this.$().css(css);

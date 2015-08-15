513268844c03e743bd51f53b5a1e86bdc7de8986
Jerry Nummi <jerry@neo.com>
2015-08-15 16:34:49 -0400

    Use select-box component in paper-new overlay

--- client/app/controllers/overlays/paper-new.js
+++ client/app/controllers/overlays/paper-new.js
@@ -5,44 +5,7 @@ export default Ember.Controller.extend(AnimateOverlay, {
   overlayClass: 'overlay--fullscreen paper-new-overlay',
   journals: null, // set on controller before rendering overlay
   paperSaving: false,
-
-  journalProxies: Ember.computed(function() {
-    return this.get('journals').map(function(journal) {
-      return {
-        id: journal.get('id'),
-        text: journal.get('name')
-      };
-    });
-  }),
-
-  // Select-2 requires data to be an object with an id key :\
-  paperTypeProxies: Ember.computed('model.journal.paperTypes.@each', function() {
-    let paperTypes = this.get('model.journal.paperTypes');
-    if(Ember.isEmpty(paperTypes)) { return []; }
-
-    return this.get('model.journal.paperTypes').map(function(paperType) {
-      return {
-        id: paperType,
-        text: paperType
-      };
-    });
-  }),
-
-  selectedJournal: Ember.computed('model.journal', function() {
-    let journal = this.get('model.journal');
-    if(Ember.isEmpty(journal)) { return; }
-
-    return { id:   journal.get('id'),
-             text: journal.get('name') };
-  }),
-
-  selectedPaperType: Ember.computed('model.paperType', function() {
-    let paperType = this.get('model.paperType');
-    if(Ember.isEmpty(paperType)) { paperType = ''; }
-
-    return { id:   paperType,
-             text: paperType };
-  }),
+  journalEmpty: Ember.computed.empty('model.journal'),
 
   actions: {
     createNewPaper() {
@@ -57,14 +20,22 @@ export default Ember.Controller.extend(AnimateOverlay, {
       });
     },
 
-    selectJournal(journalProxy) {
-      let journal = this.get('journals').findBy('id', journalProxy.id);
+    selectJournal(journal) {
       this.set('model.journal', journal);
       this.set('model.paperType', null);
     },
 
-    selectPaperType(paperTypeProxy) {
-      this.set('model.paperType', paperTypeProxy.text);
+    clearJournal() {
+      this.set('model.journal', null);
+      this.set('model.paperType', null);
+    },
+
+    selectPaperType(paperType) {
+      this.set('model.paperType', paperType);
+    },
+
+    clearPaperType() {
+      this.set('model.paperType', null);
     }
   }
 });

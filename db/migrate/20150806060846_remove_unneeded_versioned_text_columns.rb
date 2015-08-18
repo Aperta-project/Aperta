b9a4ee023b6603901ab08191b29e5b16f5fdb8ba
Erik Hetzner <ehetzner@plos.org>
2015-08-17 17:46:19 -0700

    Fix database rollback

--- db/migrate/20150806060846_remove_unneeded_versioned_text_columns.rb
+++ db/migrate/20150806060846_remove_unneeded_versioned_text_columns.rb
@@ -1,6 +1,6 @@
 class RemoveUnneededVersionedTextColumns < ActiveRecord::Migration
   def change
-    remove_column :versioned_texts, :active
-    remove_column :versioned_texts, :copy_on_edit
+    remove_column :versioned_texts, :active, :boolean, default: true
+    remove_column :versioned_texts, :copy_on_edit, :boolean, default: false
   end
 end

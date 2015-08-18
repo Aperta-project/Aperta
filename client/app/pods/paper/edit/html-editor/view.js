f9b12c2c3fd9fe19a91d527ab35fb49964d8438a
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-08-18 14:57:10 +0200

    Attach unlock click handler to an element which contains body and title.

--- client/app/pods/paper/edit/html-editor/view.js
+++ client/app/pods/paper/edit/html-editor/view.js
@@ -16,7 +16,7 @@ export default Ember.View.extend(PaperEditMixin, {
     let controller = this.get('controller');
     // When the paper is not locked we take a click
     // on the paper body to acquire the lock
-    this.$('.paper-body').on('click', (e)=>{
+    this.$('.manuscript-inner').on('click', (e)=>{
       if (!controller.get('model.lockedBy')) {
         e.preventDefault();
         e.stopPropagation();

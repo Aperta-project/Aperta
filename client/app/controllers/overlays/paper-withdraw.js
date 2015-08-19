d5bd4f636873f851cdfeddaaf49a8ccb4d0d04c6
Hildebrando Rueda <harq.soft@gmail.com>
2015-08-19 11:27:17 -0500

    Remove old code from paper-withdraw overlay

--- client/app/controllers/overlays/paper-withdraw.js
+++ client/app/controllers/overlays/paper-withdraw.js
@@ -1,37 +1,11 @@
 import Ember from 'ember';
-import RESTless from 'tahi/services/rest-less';
 
 export default Ember.Controller.extend({
   overlayClass: 'overlay--fullscreen overlay--green paper-submit-overlay',
-  paperSubmitted: false,
 
   actions: {
-    submit() {
-      RESTless.putUpdate(this.get('model'), '/submit').then(()=> {
-        this.set('paperSubmitted', true);
-      }, (arg)=> {
-        let status = arg.status;
-        let model  = arg.model;
-        let message;
-        switch (status) {
-          case 422:
-            message = model.get('errors.messages') + " You should probably reload.";
-            break;
-          case 403:
-            message = "You weren't authorized to do that";
-            break;
-          default:
-            message = 'There was a problem saving. Please reload.';
-        }
-
-        this.flash.displayMessage('error', message);
-      });
-    },
-
-    closeSuccessOverlay() {
-      this.send('closeOverlay');
-      this.set('paperSubmitted', false);
-      this.send('editableDidChange');
+    withdraw() {
+      alert('TBD');
     }
   }
 });

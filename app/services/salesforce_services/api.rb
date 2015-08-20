14c09e4b23fb6397470c779569e72f6d185b8fa9
Benjamin Moses Lieb <contact@pixelearth.net>
2015-08-20 15:02:49 -0700

    convert SalesforceService::API methods to class methods

--- app/services/salesforce_services/api.rb
+++ app/services/salesforce_services/api.rb
@@ -1,25 +1,26 @@
-require 'singleton'
-
 module SalesforceServices
   class API
-    include Singleton
     include ObjectTranslations
 
-    attr_accessor :client
-
-    def initialize
-      @client = Databasedotcom::Client.new(
+    def self.get_client
+      client = Databasedotcom::Client.new(
         host: Rails.configuration.salesforce_host,
         client_id: Rails.configuration.salesforce_client_id,
         client_secret: Rails.configuration.salesforce_client_secret
       )
-      @client.authenticate username: Rails.configuration.salesforce_username,
-                           password: Rails.configuration.salesforce_password
+
+      client.authenticate username: Rails.configuration.salesforce_username,
+                          password: Rails.configuration.salesforce_password
+
+      client
+    end
+    def self.client
+      @@client ||= self.get_client
     end
 
-    def create_manuscript(paper:)
-      mt = ManuscriptTranslator.new(user_id: client.user_id, paper: paper)
-      manuscript = client.materialize("Manuscript__c")
+    def self.create_manuscript(paper_id:)
+      mt = ManuscriptTranslator.new(user_id: client.user_id, paper: Paper.find(paper_id))
+      manuscript = self.client.materialize("Manuscript__c")
       manuscript.create(mt.paper_to_manuscript_hash)
     end
   end

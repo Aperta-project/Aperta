c3f188d717d18b38cde03dc9098b1960f06d6b40
Sam Bleckley <sam@mutuallyhuman.com>
2015-08-20 15:57:13 -0400

    Changed paper edited activity text.
    Acceptance criteria:
    Text: "Manuscript was edited"
    Action: Whenever anyone edits the MS

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -121,7 +121,7 @@ class Paper < ActiveRecord::Base
 
   feed_activities feed_names: ['manuscript', 'workflow'], subject: Proc.new { self } do
     activity :created, "Paper was created"
-    activity :edited, "Paper was edited"
+    activity :edited, "Manuscript was edited"
     activity :submitted, "Paper was submitted"
   end
 

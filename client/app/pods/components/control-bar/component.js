bddc85c427456427034108393b6b4478ee9ff374
Hildebrando Rueda <harq.soft@gmail.com>
2015-08-21 10:24:13 -0500

    Move logic from paper index view to component
    Styling refactored since was adding classess to the html tag.
    Render the versions div only if is active.
    Logic move to the control-bar component

--- client/app/pods/components/control-bar/component.js
+++ client/app/pods/components/control-bar/component.js
@@ -2,5 +2,43 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['control-bar'],
-  hasJournalLogo: Ember.computed.notEmpty('paper.journal.logoUrl')
+  classNameBindings: ['subNavVisible:control-bar-sub-nav-active'],
+  hasJournalLogo: Ember.computed.notEmpty('paper.journal.logoUrl'),
+  subNavVisible: false,
+  versionsVisible: false,
+
+  actions: {
+
+    showSubNav(sectionName) {
+      if (this.get('subNavVisible') && this.get(sectionName + 'Visible')) {
+        this.send('hideSubNav');
+      } else {
+        this.set('subNavVisible', true);
+        this.send('show' + (sectionName.capitalize()));
+      }
+    },
+
+    hideSubNav() {
+      this.set('subNavVisible', false);
+      this.send('hideVisible');
+    },
+
+    hideVisible() {
+      this.setProperties({
+        contributorsVisible: false,
+        downloadsVisible: false,
+        versionsVisible: false
+      });
+    },
+
+    toggleVersioningMode() {
+      this.toggleProperty('paper.versioningMode');
+      this.send('showSubNav', 'versions');
+    },
+
+    showVersions() {
+      this.send('hideVisible');
+      this.set('versionsVisible', true);
+    }
+  }
 });

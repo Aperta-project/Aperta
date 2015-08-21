ee689c1064b03a4a4ec369daa9d3da70b4d5e0ad
Hildebrando Rueda <harq.soft@gmail.com>
2015-08-21 11:29:10 -0500

    oops, missing comma

--- client/app/mixins/controllers/paper-base.js
+++ client/app/mixins/controllers/paper-base.js
@@ -13,7 +13,7 @@ export default Ember.Mixin.create({
     return ENV.APP.iHatExportFormats.map(formatType => {
       return {format: formatType, icon: `svg/${formatType}-icon`};
     });
-  })
+  }),
 
   pageContainerHTMLClass: computed('model.editorMode', function() {
     return 'paper-container-' + this.get('model.editorMode');

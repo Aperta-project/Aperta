cb46b629bdfa123dab1f780864486e124d879d2f
Zach Dennis <zach.dennis@gmail.com>
2015-08-21 10:39:11 -0400

    Renaming attach_and_upload_file => upload_attachment and replace_file => replace_attachment for AdhocOverlay.
    Trying to use more informative word pairs to communicate their discrete behavior.

--- spec/support/pages/overlays/adhoc_overlay.rb
+++ spec/support/pages/overlays/adhoc_overlay.rb
@@ -4,14 +4,14 @@ class AdhocOverlay < CardOverlay
     find('.adhoc-content-toolbar .fa-plus')
   end
 
-  def attach_and_upload_file(file_name)
+  def upload_attachment(file_name)
     add_content_button.click
     upload_file(element_id: "file_attachment",
                 file_name: file_name,
                 sentinel: Proc.new{ Attachment.count })
   end
 
-  def replace_file(file_name)
+  def replace_attachment(file_name)
     find(".thumbnail-preview").hover
     find(".replace").click
 

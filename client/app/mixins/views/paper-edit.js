c80160eb363774380d5e1f6c739ac0dac3550bcb
Hildebrando Rueda <harq.soft@gmail.com>
2015-08-21 11:14:43 -0500

    Replace partial with component in paper html-editor template
    Lots of duplication removed

--- client/app/mixins/views/paper-edit.js
+++ client/app/mixins/views/paper-edit.js
@@ -8,10 +8,6 @@ export default Ember.Mixin.create(RedirectsIfEditable, {
   editor: null,
   locked: Ember.computed.alias('controller.locked'),
   isEditing: Ember.computed.alias('controller.isEditing'),
-  subNavVisible: false,
-  downloadsVisible: false,
-  contributorsVisible: false,
-  versionsVisible: false,
 
   setBackgroundColor: on('didInsertElement', function() {
     $('html').addClass('matte');
@@ -33,62 +29,10 @@ export default Ember.Mixin.create(RedirectsIfEditable, {
     this.timeoutSave();
   }),
 
-  subNavVisibleDidChange: Ember.observer('subNavVisible', function() {
-    if (this.get('subNavVisible')) {
-      $('.paper-toolbar').css('top', '103px');
-      $('html').addClass('control-bar-sub-nav-active');
-    } else {
-      $('.paper-toolbar').css('top', '60px');
-      $('html').removeClass('control-bar-sub-nav-active');
-    }
-  }),
-
   actions: {
     submit() {
       this.saveEditorChanges();
       this.get('controller').send('confirmSubmitPaper');
-    },
-
-    showSubNav(sectionName) {
-      if (this.get('subNavVisible') && this.get(sectionName + 'Visible')) {
-        this.send('hideSubNav');
-      } else {
-        this.set('subNavVisible', true);
-        this.send('show' + (sectionName.capitalize()));
-      }
-    },
-
-    hideSubNav() {
-      this.set('subNavVisible', false);
-      this.send('hideVisible');
-    },
-
-    hideVisible() {
-      this.setProperties({
-        contributorsVisible: false,
-        downloadsVisible: false,
-        versionsVisible: false
-      });
-    },
-
-    showContributors() {
-      this.send('hideVisible');
-      this.set('contributorsVisible', true);
-    },
-
-    showDownloads() {
-      this.send('hideVisible');
-      this.set('downloadsVisible', true);
-    },
-
-    showVersions() {
-      this.send('hideVisible');
-      this.set('versionsVisible', true);
-    },
-
-    toggleVersioningMode() {
-      this.toggleProperty('controller.model.versioningMode');
-      this.send('showSubNav', 'versions');
     }
   }
 });

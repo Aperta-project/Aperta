61e16daf36d78c5fab239e60a3da648669f08a8b
Erik Hetzner <ehetzner@plos.org>
2015-08-24 15:38:57 -0700

    Fix versioned_texts required fields migration
    - Delete orphan versioned_texts
    - Create a new first versioned_text for all papers

--- db/migrate/20150803233256_require_fields_on_versioned_text.rb
+++ db/migrate/20150803233256_require_fields_on_versioned_text.rb
@@ -1,9 +1,27 @@
 class RequireFieldsOnVersionedText < ActiveRecord::Migration
   def change
+    reversible do |dir|
+      dir.up do
+        # delete orphan versioned_texts
+        execute "DELETE FROM versioned_texts where paper_id IS NULL;"
+      end
+    end
+
     change_column_null :versioned_texts, :paper_id, false
     change_column_null :versioned_texts, :minor_version, false
     change_column_default :versioned_texts, :minor_version, nil
     change_column_null :versioned_texts, :major_version, false
     change_column_default :versioned_texts, :major_version, nil
+
+    reversible do |dir|
+      dir.up do
+        # ensure all papers have a latest_version
+        Paper.all.each do |paper|
+          if paper.versioned_texts.empty?
+            paper.versioned_texts.create(major_version: 0, minor_version: 0, text: '')
+          end
+        end
+      end
+    end
   end
 end

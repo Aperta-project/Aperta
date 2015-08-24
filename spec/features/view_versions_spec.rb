e499093c24bfcd45e2cde9e3aef410cec9cfd8d7
Sam Bleckley <sam@mutuallyhuman.com>
2015-08-24 16:50:41 -0400

    Adds spec fo viewing versions.

--- spec/features/view_versions_spec.rb
+++ spec/features/view_versions_spec.rb
@@ -7,8 +7,8 @@ feature "Viewing Versions:", js: true do
     let(:paper) do
       FactoryGirl.create :paper,
                          :with_versions,
-                         first_version_body:  'OK first body',
-                         second_version_body: 'OK second body',
+                         first_version_body:  '<p>OK first body</p>',
+                         second_version_body: '<p>OK second body</p>',
                          creator: user
     end
 
@@ -33,6 +33,20 @@ feature "Viewing Versions:", js: true do
       wait_for_ajax
 
       expect(page.versioned_body).to include "OK first body"
+    end
+
+    scenario "the user views an old version of the paper.", selenium: true do
+      page = EditPaperPage.new
+      page.version_button.click
+      wait_for_ajax
+      select paper.versioned_texts.first.version_string, from: "view_version"
+
+
+      select paper.versioned_texts.last.version_string, from: "compare_version"
+      wait_for_ajax
+
+      expect(page.find("#paper-body .added").text).to include "OK second body"
+      expect(page.find("#paper-body .removed").text).to include "OK first body"
     end
   end
 end

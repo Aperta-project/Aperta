b2e6b425bda254cd26e32c8d791e27d8927cdf22
Jerry Nummi <jerry@neo.com>
2015-08-24 16:30:44 -0400

    Date rejiggering
    — Change default date format for `format-date` helper
    – Use `format-date` in place of `relative-date` for
    discussions and and task-comments

--- client/app/lib/format-date.js
+++ client/app/lib/format-date.js
@@ -1,5 +1,5 @@
 export default function(date, options) {
   let dateObj = moment(date);
   if (!dateObj.isValid()) { return date; }
-  return dateObj.format(options.format || 'LL');
+  return dateObj.format(options.format || 'MMMM D, YYYY h:mm A [GMT]');
 }
--- client/tests/unit/lib/format-date-test.js
+++ client/tests/unit/lib/format-date-test.js
@@ -8,7 +8,7 @@ test('default formatting', function(assert) {
   let date    = new Date('February 06, 1990');
   let result  = formatDate(date, options);
 
-  assert.equal(result, 'February 6, 1990', 'returns a human readable date');
+  assert.equal(result, 'February 6, 1990 12:00 AM GMT', 'returns a human readable date');
 });
 
 test('specify formatting', function(assert) {

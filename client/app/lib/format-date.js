69c39d3c7fe513a45b7713a976b08234df7c88a8
Jerry Nummi <jerry@neo.com>
2015-08-24 20:51:51 -0400

    Use 24-hour format for default date display

--- client/app/lib/format-date.js
+++ client/app/lib/format-date.js
@@ -1,5 +1,5 @@
 export default function(date, options) {
   let dateObj = moment(date);
   if (!dateObj.isValid()) { return date; }
-  return dateObj.format(options.format || 'MMMM D, YYYY h:mm A [GMT]');
+  return dateObj.format(options.format || 'MMMM D, YYYY HH:mm [GMT]');
 }

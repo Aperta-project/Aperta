3b79c49dafa34665858ae041d1a22aeac12870a8
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-08-25 14:09:15 -0400

    All task relationships should be polymorphic.  This is necessary so that the task `type` is sent with any serialized json.

--- app/serializers/attachment_serializer.rb
+++ app/serializers/attachment_serializer.rb
@@ -1,5 +1,5 @@
 class AttachmentSerializer < ActiveModel::Serializer
-  has_one :task, embed: :id
+  has_one :task, embed: :id, polymorphic: true
 
   attributes :id, :title, :caption, :kind, :src, :status, :preview_src, :detail_src, :filename
 

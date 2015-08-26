e317d3294d590f73cd1515830675a5df8d81775a
Ryan Wold <pair+ryan.wold@neo.com>
2015-08-26 15:02:41 -0700

    remove redundant .selfs and trailing whitespace

--- app/services/salesforce_services/api.rb
+++ app/services/salesforce_services/api.rb
@@ -3,9 +3,9 @@ module SalesforceServices
     include ObjectTranslations
 
     def self.get_client
-      unless self.has_valid_creds?
-        Rails.logger.warn "SalesForce credentials are not set. Information will NOT be synced to SalesForce" 
-        return 
+      unless has_valid_creds?
+        Rails.logger.warn "SalesForce credentials are not set. Information will NOT be synced to SalesForce"
+        return
       end
 
       begin
@@ -18,21 +18,21 @@ module SalesforceServices
         client.authenticate username: Rails.configuration.salesforce_username,
                             password: Rails.configuration.salesforce_password
       rescue
-        Rails.logger.warn("Failed authentication to SalesForce API") 
+        Rails.logger.warn("Failed authentication to SalesForce API")
         return
       end
       client
     end
 
     def self.client
-      @@client ||= self.get_client
+      @@client ||= get_client
     end
 
     def self.create_manuscript(paper_id:)
       paper = Paper.find(paper_id)
 
       mt = ManuscriptTranslator.new(user_id: client.user_id, paper: paper)
-      manuscript = self.client.materialize("Manuscript__c")
+      manuscript = client.materialize("Manuscript__c")
       sf_paper = manuscript.create(mt.paper_to_manuscript_hash)
 
       paper.update_attribute(:salesforce_manuscript_id, sf_paper.Id)
@@ -43,29 +43,29 @@ module SalesforceServices
       paper = Paper.find(paper_id)
 
       mt = ManuscriptTranslator.new(user_id: client.user_id, paper: paper)
-      manuscript = self.client.materialize("Manuscript__c")
+      manuscript = client.materialize("Manuscript__c")
       sf_paper = manuscript.find(paper.salesforce_manuscript_id)
       sf_paper.update_attributes mt.paper_to_manuscript_hash
       sf_paper
     end
 
     def self.find_or_create_manuscript(paper_id:)
-      unless self.client
-        Rails.logger.warn "No valid SalesForce client. Information will NOT be synced to SalesForce" 
+      unless client
+        Rails.logger.warn "No valid SalesForce client. Information will NOT be synced to SalesForce"
         return
       end
-       
+
       p = Paper.find(paper_id)
       if p.salesforce_manuscript_id
-        self.update_manuscript(paper_id: paper_id)
+        update_manuscript(paper_id: paper_id)
       else
-        self.create_manuscript(paper_id: paper_id)
+        create_manuscript(paper_id: paper_id)
       end
     end
-    
+
     def self.has_valid_creds?
       [ :salesforce_client_id, :salesforce_host, :salesforce_client_secret, :salesforce_username, :salesforce_password ].each do |key|
-        return false if !Rails.configuration.respond_to?(key) || Rails.configuration.send(key) == :not_set 
+        return false if !Rails.configuration.respond_to?(key) || Rails.configuration.send(key) == :not_set
       end
       true
     end

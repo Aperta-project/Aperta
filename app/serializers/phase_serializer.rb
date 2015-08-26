413cffaa02e9b10c1827d8fec5a2a03264418972
Erik Hetzner <ehetzner@plos.org>
2015-08-26 16:16:26 -0700

    Lazy load tasks in paper phases

--- app/serializers/phase_serializer.rb
+++ app/serializers/phase_serializer.rb
@@ -1,5 +1,5 @@
 class PhaseSerializer < ActiveModel::Serializer
   attributes :id, :name, :position
   has_one :paper, embed: :ids
-  has_many :tasks, embed: :ids, include: true, polymorphic: true
+  has_many :tasks, embed: :ids, polymorphic: true
 end

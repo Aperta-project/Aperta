4c71afb201ac4d63315bf2462a7214ea1314a419
Sam Bleckley <sam@mutuallyhuman.com>
2015-08-26 15:26:46 -0400

    Separates activity routes.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,8 @@
 class Activity < ActiveRecord::Base
   belongs_to :subject, polymorphic: true
   belongs_to :user
+
+  def self.feed_for(feed_names, subject)
+    where(feed_name: feed_names, subject_id: subject.id).order('created_at DESC')
+  end
 end

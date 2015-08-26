899471f88e27ec56ec9c913322f44e1e3fde998f
Jerry Nummi <jerry@neo.com>
2015-08-26 15:39:16 -0400

    Add service for loading countries
    — Includes caching of data

---
+++ client/app/services/countries.js
@@ -0,0 +1,57 @@
+import Ember from 'ember';
+import RESTless from 'tahi/services/rest-less';
+
+export default Ember.Service.extend({
+  loaded: false,
+  loading: false,
+  error: false,
+
+  _data: [],
+  data: Ember.computed({
+    get() {
+      let c = this.get('_data');
+      if(Ember.isEmpty(c)) { return this.fetch(); }
+
+      return c;
+    },
+
+    set(key, value) {
+      this.set('_data', value);
+    }
+  }),
+
+  fetch() {
+    this._didStartLoading();
+
+    RESTless.get('/api/countries').then((response)=> {
+      this.set('_data', response.countries);
+      this._didLoad();
+    }, ()=> {
+      this._didError();
+    });
+  },
+
+  _didStartLoading() {
+    this.setProperties({
+      loaded: false,
+      loading: true,
+      error: false
+    });
+  },
+
+  _didError() {
+    this.setProperties({
+      loaded: false,
+      loading: false,
+      error: true
+    });
+  },
+
+  _didLoad() {
+    this.setProperties({
+      loaded: true,
+      loading: false,
+      error: false
+    });
+  }
+});

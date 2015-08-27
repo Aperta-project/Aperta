578664a6316e395ded0b83be05bb37d9b383ebd8
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-08-27 09:06:42 -0400

    comment subscriber specs

---
+++ spec/subscribers/comment/destroyed/event_stream_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Comment::Destroyed::EventStream do
+  include EventStreamMatchers
+
+  let(:pusher_channel) { mock_delayed_class(TahiPusher::Channel) }
+  let(:comment) { FactoryGirl.build(:comment) }
+
+  it "serializes comment id down the system channel on destruction" do
+    expect(pusher_channel).to receive_push(serialize: :ids, down: 'system', on: 'destroyed')
+    described_class.call("tahi:comment:destroyed", { action: "destroyed", record: comment })
+  end
+
+end

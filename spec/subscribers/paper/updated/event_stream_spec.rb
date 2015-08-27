924971dbf65a26448544413a7ecbad3dd7244122
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-08-27 09:07:07 -0400

    paper subscriber specs

---
+++ spec/subscribers/paper/updated/event_stream_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Paper::Updated::EventStream do
+  include EventStreamMatchers
+
+  let(:pusher_channel) { mock_delayed_class(TahiPusher::Channel) }
+  let(:paper) { FactoryGirl.build(:paper, id: 44) } # PaperSerializer needs an id...
+
+  it "serializes paper down the paper channel on update" do
+    expect(pusher_channel).to receive_push(serialize: :paper, down: 'paper', on: 'updated')
+    described_class.call("tahi:paper:updated", { action: "updated", record: paper })
+  end
+
+end

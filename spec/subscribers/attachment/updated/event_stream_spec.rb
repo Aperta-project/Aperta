c70ba789649f361eb3556c145f1f021bebd52d5d
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-08-27 09:06:32 -0400

    attachment subscriber specs

---
+++ spec/subscribers/attachment/updated/event_stream_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Attachment::Updated::EventStream do
+  include EventStreamMatchers
+
+  let(:pusher_channel) { mock_delayed_class(TahiPusher::Channel) }
+  let(:attachment) { FactoryGirl.build(:attachment, :with_task) }
+
+  it "serializes attachment down the paper channel on update" do
+    expect(pusher_channel).to receive_push(serialize: :attachment, down: 'paper', on: 'updated')
+    described_class.call("tahi:attachment:updated", { action: "updated", record: attachment })
+  end
+
+end

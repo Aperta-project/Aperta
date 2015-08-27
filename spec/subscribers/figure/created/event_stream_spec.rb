7dd79ebd31a9f27bde0a1dc17db6306c1f4dabab
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-08-27 09:05:25 -0400

    figure subscriber specs

---
+++ spec/subscribers/figure/created/event_stream_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Figure::Created::EventStream do
+  include EventStreamMatchers
+
+  let(:pusher_channel) { mock_delayed_class(TahiPusher::Channel) }
+  let(:figure) { FactoryGirl.build(:figure) }
+
+  it "serializes attachment down the paper channel on creation" do
+    expect(pusher_channel).to receive_push(serialize: :figure, down: 'paper', on: 'created')
+    described_class.call("tahi:figure:created", { action: "created", record: figure })
+  end
+
+end

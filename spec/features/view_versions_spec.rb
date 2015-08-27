e6dfa18c7f6ccc895e1bd4f26f7b2529f681e6c2
Erik Hetzner <ehetzner@plos.org>
2015-08-26 19:53:26 -0700

    Fix view versioning specs

--- spec/features/view_versions_spec.rb
+++ spec/features/view_versions_spec.rb
@@ -27,26 +27,26 @@ feature "Viewing Versions:", js: true do
       wait_for_ajax
       select paper.versioned_texts.version_desc.first.version_string, from: "view_version"
 
-      expect(page.versioned_body).to include "OK second body"
+      expect(page.versioned_body).to have_content("OK second body")
 
       select paper.versioned_texts.version_desc.last.version_string, from: "view_version"
       wait_for_ajax
 
-      expect(page.versioned_body).to include "OK first body"
+      expect(page.versioned_body).to have_content("OK first body")
     end
 
     scenario "the user views an old version of the paper.", selenium: true do
       page = EditPaperPage.new
       page.version_button.click
       wait_for_ajax
-      select paper.versioned_texts.version_desc.first.version_string, from: "view_version"
+      select paper.versioned_texts.version_desc.last.version_string, from: "view_version"
 
 
-      select paper.versioned_texts.version_desc.last.version_string, from: "compare_version"
+      select paper.versioned_texts.version_desc.first.version_string, from: "compare_version"
       wait_for_ajax
 
-      expect(page.find("#paper-body .added").text).to include "OK second body"
-      expect(page.find("#paper-body .removed").text).to include "OK first body"
+      expect(page.find("#paper-body .added")).to have_content "OK first body"
+      expect(page.find("#paper-body .removed")).to have_content "OK second body"
     end
   end
 end

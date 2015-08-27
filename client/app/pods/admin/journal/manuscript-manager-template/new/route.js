48584130e52dd67c476dc6818435d9ec7ab7ab7e
Hildebrando Rueda <harq.soft@gmail.com>
2015-08-27 17:56:51 -0500

    Change mmt new route from coffee to js

---
+++ client/app/pods/admin/journal/manuscript-manager-template/new/route.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  controllerName: 'admin.journal.manuscript_manager_template.edit',
+
+  model() {
+
+    let journal = this.modelFor('admin.journal');
+
+    let newTemplate = this.store.createRecord('manuscriptManagerTemplate', {
+      journal: journal,
+      paperType: "Research"
+    });
+
+    newTemplate.get('phaseTemplates').pushObject(
+      this.store.createRecord('phaseTemplate', {name: "Phase 1", position: 1})
+    );
+
+    newTemplate.get('phaseTemplates').pushObject(
+      this.store.createRecord('phaseTemplate', {name: "Phase 2", position: 2})
+    );
+
+    newTemplate.get('phaseTemplates').pushObject(
+      this.store.createRecord('phaseTemplate', {name: "Phase 3", position: 3})
+    );
+
+    this.set('journal', journal);
+    this.set('newTemplate', newTemplate);
+
+    return newTemplate;
+  },
+
+  setupController(controller, model) {
+    controller.set('model', model);
+    controller.set('journal', this.modelFor('admin.journal'));
+  },
+
+  renderTemplate(){
+    this.render('admin/journal/manuscript_manager_template/edit');
+  },
+
+  actions: {
+
+    didRollBack(){
+      let newTemplate = this.get('newTemplate');
+      this.get('journal.manuscriptManagerTemplates').removeObject(newTemplate);
+      this.transitionTo('admin.journal');
+    }
+  }
+});

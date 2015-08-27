f87cef3da0f71f5a6729207af685057cbde4fcb3
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-08-27 09:06:04 -0400

    discussion related subscriber specs

---
+++ spec/subscribers/discussion_participant/created/notify_assignee_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe DiscussionParticipant::Created::NotifyAssignee do
+  include EventStreamMatchers
+
+  let(:pusher_channel) { mock_delayed_class(TahiPusher::Channel) }
+  let(:discussion_participant) { FactoryGirl.build(:discussion_participant) }
+
+  it "serializes discussion participtant down the user channel on creation" do
+    expect(pusher_channel).to receive_push(serialize: :discussion_participant, down: 'user', on: 'discussion-participant-created')
+    described_class.call("tahi:discussion_participtant:created", { action: "created", record: discussion_participant })
+  end
+
+end

b0008493c6f68116c42cc3b6522f4ecfd38916c5
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-08-27 16:14:00 -0400

    Fix broken revision_task spec

--- engines/tahi_standard_tasks/spec/subscribers/paper/resubmitted/reopen_revision_tasks_spec.rb
+++ engines/tahi_standard_tasks/spec/subscribers/paper/resubmitted/reopen_revision_tasks_spec.rb
@@ -5,15 +5,15 @@ describe Paper::Resubmitted::ReopenRevisionTasks do
 
   let(:mailer) { mock_delayed_class(UserMailer) }
   let(:paper) { FactoryGirl.create(:paper) }
-  let(:paper_reviewer_task) { FactoryGirl.create(:paper_reviewer_task, paper: paper, complete: true) }
-  let(:register_decision_task) { FactoryGirl.create(:register_decision_task, paper: paper, complete: true) }
+  let(:paper_reviewer_task) { FactoryGirl.create(:paper_reviewer_task, paper: paper, completed: true) }
+  let(:register_decision_task) { FactoryGirl.create(:register_decision_task, paper: paper, completed: true) }
 
   it "marks all revision tasks as incomplete" do
-    expect(paper_reviewer_task).to be_complete
-    expect(register_decision_task).to be_complete
+    expect(paper_reviewer_task).to be_completed
+    expect(register_decision_task).to be_completed
     described_class.call("tahi:paper:resubmitted", { paper: paper })
-    expect(paper_reviewer_task).to_not be_complete
-    expect(register_decision_task).to_not be_complete
+    expect(paper_reviewer_task.reload).to_not be_completed
+    expect(register_decision_task.reload).to_not be_completed
   end
 
 end

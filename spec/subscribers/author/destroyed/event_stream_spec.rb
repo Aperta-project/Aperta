788136c314734e7ae597d00cc21a1c0401ca78fd
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-08-27 09:04:57 -0400

    author subscriber specs

---
+++ spec/subscribers/author/destroyed/event_stream_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Author::Destroyed::EventStream do
+  include EventStreamMatchers
+
+  let(:pusher_channel) { mock_delayed_class(TahiPusher::Channel) }
+  let(:author) { FactoryGirl.build(:author, :with_paper) }
+
+  it "serializes author id down the system channel on destruction" do
+    expect(pusher_channel).to receive_push(serialize: :ids, down: 'system', on: 'destroyed')
+    described_class.call("tahi:author:destroyed", { action: "destroyed", record: author })
+  end
+
+end

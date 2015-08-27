78557496dfd15ad3115c29f82b1bf56180871e05
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-08-27 09:07:19 -0400

    participation subscriber specs

---
+++ spec/subscribers/participation/destroyed/event_stream_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Participation::Destroyed::EventStream do
+  include EventStreamMatchers
+
+  let(:pusher_channel) { mock_delayed_class(TahiPusher::Channel) }
+  let(:participation) { FactoryGirl.build(:participation) }
+
+  it "serializes comment id down the system channel on destruction" do
+    expect(pusher_channel).to receive_push(serialize: :ids, down: 'system', on: 'destroyed')
+    described_class.call("tahi:participation:destroyed", { action: "destroyed", record: participation })
+  end
+
+end

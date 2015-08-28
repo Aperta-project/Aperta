8ab05e47aab04ce8dc90afb4d16b6a292975d59f
Jerry Nummi <jerry@neo.com>
2015-08-28 09:07:34 -0400

    Remove old RESTless import

--- client/app/controllers/overlays/paper-withdraw.js
+++ client/app/controllers/overlays/paper-withdraw.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
-import RESTless from 'tahi/services/rest-less';
 
 export default Ember.Controller.extend({
+  restless: Ember.inject.service('restless'),
   overlayClass: 'overlay--fullscreen overlay--grey',
 
   actions: {
     withdraw() {
       var reason = this.get('model.withdrawalReason');
-      RESTless.putUpdate(this.get('model'), '/withdraw', {'reason': reason})
+      this.get('restless').putUpdate(this.get('model'), '/withdraw', {'reason': reason})
       .then(()=> {
         this.send('closeOverlay');
       });

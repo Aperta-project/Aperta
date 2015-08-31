c0b20bd24dfd1b8bdc15607c0007c13eb5b653a1
Hildebrando Rueda <harq.soft@gmail.com>
2015-08-31 10:46:48 -0500

    Fix bug in Ember Data with a workaround
    More info https://github.com/emberjs/data/issues/1829

---
+++ client/app/mixins/dependent-relationships.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  save: function() {
+    var recordsToDelete = [];
+    this.eachRelationship((name, descriptor) => {
+      if (descriptor.kind === 'hasMany') {
+        this.get(name).forEach((object) => {
+          if (object.get('isNew')) {
+            recordsToDelete.push(object);
+          }
+        });
+      }
+    });
+
+    var promise = this._super(...arguments);
+    promise.then(() => {
+      recordsToDelete.forEach((record) => {
+        record.deleteRecord();
+      });
+    });
+    return promise;
+  }
+});

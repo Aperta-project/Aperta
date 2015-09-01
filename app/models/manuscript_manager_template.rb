572a643b3eb7c46a4639c1475f1f8bba30294fbb
Hildebrando Rueda <harq.soft@gmail.com>
2015-08-31 21:46:03 -0500

    add destroy dependency to phase_templates association

--- app/models/manuscript_manager_template.rb
+++ app/models/manuscript_manager_template.rb
@@ -1,6 +1,8 @@
 class ManuscriptManagerTemplate < ActiveRecord::Base
   belongs_to :journal, inverse_of: :manuscript_manager_templates
-  has_many :phase_templates, -> { order("position asc") }, inverse_of: :manuscript_manager_template
+  has_many :phase_templates, -> { order("position asc") },
+                              inverse_of: :manuscript_manager_template,
+                              dependent: :destroy
 
   validates :paper_type, presence: true
   validates :paper_type, uniqueness: { scope: :journal_id }

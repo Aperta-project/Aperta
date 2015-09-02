b685f7804b632bd913d3d0ea195991ed62745e23
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-09-02 16:24:31 -0400

    Single source of truth for Subscriptions
    Rather than a single big hash listing out all the subscriptions, use a
    more intelligent object to keep track of event subscriptions. This
    should help greatly with inspection across gem/engine boundaries.
    Convert all the existing subscription files to the new dsl syntax.
    
    Added a `rake subscriptions` task to allow quick and easy inspection of
    subscribers.
    
    Move Notifier/Subscription related stuff to /lib. It isn't specific to
    the application anymore.

--- config/initializers/subscriptions.rb
+++ config/initializers/subscriptions.rb
@@ -1,167 +1,60 @@
-EVENTS = {
-  '.*' => [EventLogger],
+Subscriptions.configure do
+  add '.*', EventLogger
 
-  'paper:created' => [],
-  'paper:updated' => [Paper::Updated::EventStream],
-  'paper:destroyed' => [Paper::Destroyed::EventStream],
-  'paper:submitted' => [Paper::Submitted::EmailCreator, Paper::Submitted::EmailAdmins],
-  'paper:resubmitted' => [Paper::Resubmitted::EmailEditor],
+  add 'paper:updated', Paper::Updated::EventStream
+  add 'paper:destroyed', Paper::Destroyed::EventStream
+  add 'paper:submitted', Paper::Submitted::EmailCreator, Paper::Submitted::EmailAdmins
+  add 'paper:resubmitted', Paper::Resubmitted::EmailEditor
 
-  'paper_role:created' => [PaperRole::Created::EventStream::NotifyPaperMembers, PaperRole::Created::EventStream::NotifyAssignee],
-  'paper_role:destroyed' => [PaperRole::Destroyed::EventStream::NotifyPaperMembers, PaperRole::Destroyed::EventStream::NotifyAssignee],
+  add 'paper_role:created', PaperRole::Created::EventStream::NotifyPaperMembers, PaperRole::Created::EventStream::NotifyAssignee
+  add 'paper_role:destroyed', PaperRole::Destroyed::EventStream::NotifyPaperMembers, PaperRole::Destroyed::EventStream::NotifyAssignee
 
-  'task:created' => [Task::Created::EventStream],
-  'task:updated' => [Task::Updated::EventStream],
-  'task:destroyed' => [Task::Destroyed::EventStream],
+  add 'task:created', Task::Created::EventStream
+  # -1, when reordering on workflow
+  add 'task:updated', Task::Updated::EventStream
+  add 'task:destroyed', Task::Destroyed::EventStream
 
-  'question_attachment:created' => [QuestionAttachment::Created::EventStream],
-  'question_attachment:updated' => [QuestionAttachment::Updated::EventStream],
-  'question_attachment:destroyed' => [QuestionAttachment::Destroyed::EventStream],
+  add 'question_attachment:created', QuestionAttachment::Created::EventStream
+  add 'question_attachment:updated', QuestionAttachment::Updated::EventStream
+  add 'question_attachment:destroyed', QuestionAttachment::Destroyed::EventStream
 
-  'figure:created' => [Figure::Created::EventStream],
-  'figure:updated' => [Figure::Updated::EventStream],
-  'figure:destroyed' => [Figure::Destroyed::EventStream],
+  add 'figure:created', Figure::Created::EventStream
+  add 'figure:updated', Figure::Updated::EventStream
+  add 'figure:destroyed', Figure::Destroyed::EventStream
 
-  'supporting_information_file:created' => [SupportingInformationFile::Created::EventStream],
-  'supporting_information_file:updated' => [SupportingInformationFile::Updated::EventStream],
-  'supporting_information_file:destroyed' => [SupportingInformationFile::Destroyed::EventStream],
+  add 'supporting_information_file:created', SupportingInformationFile::Created::EventStream
+  add 'supporting_information_file:updated', SupportingInformationFile::Updated::EventStream
+  add 'supporting_information_file:destroyed', SupportingInformationFile::Destroyed::EventStream
 
-  'attachment:created' => [Attachment::Created::EventStream],
-  'attachment:updated' => [Attachment::Updated::EventStream],
-  'attachment:destroyed' => [Attachment::Destroyed::EventStream],
+  add 'attachment:created', Attachment::Created::EventStream
+  add 'attachment:updated', Attachment::Updated::EventStream
+  add 'attachment:destroyed', Attachment::Destroyed::EventStream
 
-  'invitation:created' => [Invitation::Created::EventStream::NotifyPaperMembers],
-  'invitation:updated' => [Invitation::Updated::EventStream::NotifyPaperMembers, Invitation::Updated::EventStream::NotifyInvitee],
-  'invitation:destroyed' => [Invitation::Destroyed::EventStream],
+  add 'invitation:created', Invitation::Created::EventStream::NotifyPaperMembers
+  add 'invitation:updated', Invitation::Updated::EventStream::NotifyPaperMembers, Invitation::Updated::EventStream::NotifyInvitee
+  add 'invitation:destroyed', Invitation::Destroyed::EventStream
 
-  'comment:created' => [Comment::Created::EventStream],
-  'comment:destroyed' => [Comment::Destroyed::EventStream],
+  add 'comment:created', Comment::Created::EventStream
+  add 'comment:destroyed', Comment::Destroyed::EventStream
 
-  'comment_look:created' => [CommentLook::Created::EventStream],
-  'comment_look:destroyed' => [CommentLook::Destroyed::EventStream],
-  'author:created' => [Author::Created::EventStream],
-  'author:updated' => [Author::Updated::EventStream],
-  'author:destroyed' => [Author::Destroyed::EventStream],
+  add 'comment_look:created', CommentLook::Created::EventStream
+  add 'comment_look:destroyed', CommentLook::Destroyed::EventStream
 
-  'participation:created' => [Participation::Created::EventStream],
-  'participation:destroyed' => [Participation::Destroyed::EventStream],
+  add 'author:created', Author::Created::EventStream
+  add 'author:updated', Author::Updated::EventStream
+  add 'author:destroyed', Author::Destroyed::EventStream
 
-  'discussion_topic:created' => [DiscussionTopic::Created::EventStream],
-  'discussion_topic:updated' => [DiscussionTopic::Updated::EventStream],
-  'discussion_topic:destroyed' => [DiscussionTopic::Destroyed::EventStream],
+  add 'participation:created', Participation::Created::EventStream
+  add 'participation:destroyed', Participation::Destroyed::EventStream
 
-  'discussion_participant:created' => [DiscussionParticipant::Created::EventStream::NotifyExistingParticipants, DiscussionParticipant::Created::EventStream::NotifyAssignee],
-  'discussion_participant:destroyed' => [DiscussionParticipant::Destroyed::EventStream],
+  add 'discussion_topic:created', DiscussionTopic::Created::EventStream
+  add 'discussion_topic:updated', DiscussionTopic::Updated::EventStream
+  add 'discussion_topic:destroyed', DiscussionTopic::Destroyed::EventStream
 
-  'discussion_reply:created' => [DiscussionReply::Created::EventStream],
-  'discussion_reply:updated' => [DiscussionReply::Updated::EventStream],
-  'discussion_reply:destroyed' => [DiscussionReply::Destroyed::EventStream],
+  add 'discussion_participant:created', DiscussionParticipant::Created::EventStream::NotifyExistingParticipants, DiscussionParticipant::Created::EventStream::NotifyAssignee
+  add 'discussion_participant:destroyed', DiscussionParticipant::Destroyed::EventStream
 
-}
-
-EVENTS.each do |event_name, subscriber_list|
-  Notifier.subscribe(event_name, subscriber_list)
+  add 'discussion_reply:created', DiscussionReply::Created::EventStream
+  add 'discussion_reply:updated', DiscussionReply::Updated::EventStream
+  add 'discussion_reply:destroyed', DiscussionReply::Destroyed::EventStream
 end
-
-# TahiNotifier.subscribe("paper:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the paper down the paper channel
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("task:*", "author:*", "figure:*", "invitation:*", "supporting_information_file:*", "attachment:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the respective model down the paper channel
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.paper, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("invitation:updated") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the invitation model down the user channel
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.invitee, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("comment:*", "participation:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the comment or participation down the paper channel
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.task.paper, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("question_attachment:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the question_attachment down the paper channel
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.question.task.paper, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("paper_role:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the paper down the paper channel
-#   EventStream::Broadcaster.new(record.paper).post(action: action, channel_scope: record.paper, excluded_socket_id: excluded_socket_id)
-#
-#   # serialize the paper down the user channel
-#   # this is necessary if the user is just now given access to the paper and have yet to subscribe to the paper channel
-#   EventStream::Broadcaster.new(record.paper).post(action: action, channel_scope: record.user, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("comment_look:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the comment_look down the user channel
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.user, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("discussion_topic:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the discussion_topic down the discussion_topic channel
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("discussion_participant:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the discussion_participant down the discussion_topic channel
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.discussion_topic, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("discussion_participant:created") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the discussion_participant down the user channel so they can subscribe to the topic
-#   EventStream::Broadcaster.new(record).post(action: "discussion-participant-created", channel_scope: record.user, excluded_socket_id: excluded_socket_id)
-# end
-#
-# TahiNotifier.subscribe("discussion_reply:*") do |payload|
-#   action = payload[:action]
-#   record = payload[:record]
-#   excluded_socket_id = payload[:requester_socket_id]
-#
-#   # serialize the discussion_reply down the discussion_topic channel
-#   EventStream::Broadcaster.new(record).post(action: action, channel_scope: record.discussion_topic, excluded_socket_id: excluded_socket_id)
-# end
--- engines/tahi_standard_tasks/config/initializers/subscriptions.rb
+++ engines/tahi_standard_tasks/config/initializers/subscriptions.rb
@@ -1,7 +1,3 @@
-STANDARD_TASK_EVENTS = {
-  'paper:resubmitted' => [Paper::Resubmitted::ReopenRevisionTasks],
-}
-
-STANDARD_TASK_EVENTS.each do |event_name, subscriber_list|
-  Notifier.subscribe(event_name, subscriber_list)
+Subscriptions.configure do
+  add 'paper:resubmitted', Paper::Resubmitted::ReopenRevisionTasks
 end

76f0e7692d51f522c0865e2c380b56e9f113fb14
Zach Dennis <zach.dennis@gmail.com>
2015-09-02 21:38:01 -0400

    WIP'ing up some publishing related questions.
    * styling is not quite right yet, but getting better

--- client/app/models/task-comparison-diff.js
+++ client/app/models/task-comparison-diff.js
@@ -6,20 +6,86 @@ export default Ember.Object.extend({
     date: "2015-08-01",
 
     properties: [
-      { type: "properties", name: "authors", children: [
-        { type: "properties", name: "author", children: [
-          { type: "text", name: "name", value: "Diana Smith"},
-          { type: "text", name: "email", value: "dsmith@example.com"},
-          { type: "text", name: "another.field", value: "Chupa chups pastry topping chocolate cake cholocate cake."},
-          { type: "text", name: "institution", value: "Arizona State Polytechnic Campus"}
-        ]},
-        { type: "properties", name: "author", children: [
-          { type: "text", name: "name", value: "Jane Goodall"},
-          { type: "text", name: "email", value: "jane.goodall@example.com"},
-          { type: "text", name: "another.field", value: "World's Foremost Authority on Chimpanzees - Primatology"},
-          { type: "text", name: "institution", value: "Arizona State Polytechnic Campus"}
-        ]}
-      ]}
+      { type: "properties", name: "publishing-related-questions", children: [
+        { type: "question", name: "question-1", value:
+          {
+            title: "Have the results, data, or figures in this manuscript been published elsewhere? Are they under consideration for publication elsewhere?",
+            answer: "Yes"
+          },
+          children: [
+            { type: "question", name: "question-1a",
+              value:  {
+                title: "Results, data or figures:",
+                answer: "Figure 2: Abico in singularis ut delenit sed mara volutpat os os dignissim. Illum luptatum quod capio augue ex abdo imputo probo in appellatio. Tincid-unt abbas velit quis convention. Luptatum distineo virtus feugait. Utrum neque decet. Et melior a ratis abdo cui nisl."
+              }
+            },
+            { type: "text", name: "attachment-file-name", value: "white-rodgers-thermostat_1F80.pdf" },
+            { type: "text", name: "attachment-title", value: "Singularis ut Delenit" },
+            { type: "text", name: "attachment-caption", value: "Illum luptatum quod capio augue ex abdo imputo probo in appellatio. Sed utrum duis vel refoveo interdico facilisi zelus. Utrum neque decet." },
+          ]
+        },
+
+        { type: "question", name: "question-2", value:
+          {
+            title: "Is this manuscript being submitted in conjunction with another submission?",
+            answer: "Yes"
+          },
+          children: [
+            { type: "question", name: "question-2a",
+              value:  {
+                title: "Title",
+                answer: "Miocene Odobenids from Mars"
+              }
+            },
+            { type: "question", name: "question-2b",
+              value:  {
+                title: "Corresponding Author",
+                answer: "Sir Walter W. Alerus"
+              }
+            }
+          ]
+        },
+
+        { type: "question", name: "question-3", value:
+          {
+            title: "Please indicate whether you have had any of the following previous interactions about this manuscript. Check all that apply.",
+            answer: ""
+          },
+          children: [
+            { type: "properties", name: "Responses:", children: [
+              { type: "question", name: "question-3b",
+                value:  {
+                  title: "One or more authors (including myself) curently serve, or have previously served, as an Academic Editor or Guest Editor for this journal.",
+                  answer: ""
+                }
+              },
+            ]
+          }]
+        },
+
+        { type: "question", name: "question-4", value:
+          {
+            title: "If your submission is intendd for a PLOS Collection, enter the name of the collection in the box below. Please also ensure the name of the collection is included in your cover letter.",
+            answer: ""
+          },
+          children: [
+            { type: "question", name: "question-4a",
+              value:  {
+                title: "Name of PLOS Collection",
+                answer: "Oceanic Critter Collection"
+              }
+            },
+          ]
+        },
+
+        { type: "question", name: "question-5", value:
+          {
+            title: "Are you or any of the contributing authors an employee of the United States Government",
+            answer: "Yes"
+          },
+        }
+      ]
+    }
     ]
   },
 
@@ -28,21 +94,80 @@ export default Ember.Object.extend({
     date: "2015-08-01",
 
     properties: [
-      { type: "properties", name: "authors", children: [
-        { type: "properties", name: "author", children: [
-          { type: "text", name: "name", value: "Diana Smith"},
-          { type: "text", name: "email", value: "dianasmith@gmail.com"},
-          { type: "text", name: "another.field", value: "Chupa chups pastry topping chocolate cake cholocate cake."},
-          { type: "text", name: "institution", value: "Arizona State Polytechnic Campus"}
-        ]},
-        { type: "properties", name: "author", children: [
-          { type: "text", name: "name", value: "Holly Something"},
-          { type: "text", name: "email", value: "hollysomething@example.come"},
-          { type: "text", name: "another.field", value: "Another description if required"},
-          { type: "text", name: "institution", value: "Washington State-Polytechnic Campus"}
-        ]}
+      { type: "properties", name: "publishing-related-questions", children: [
+        { type: "question", name: "question-1", value:
+          {
+            title: "Have the results, data, or figures in this manuscript been published elsewhere? Are they under consideration for publication elsewhere?",
+            answer: "Yes"
+          },
+          children: [
+            { type: "question", name: "question-1a",
+              value:  {
+                title: "Results, data or figures:",
+                answer: "Figure 2: Abico in singularis ut delenit sed mara volutpat os os dignissim. Illum luptatum quod capio augue ex abdo imputo probo in appellatio. Tincid-unt abbas velit quis convention. Luptatum distineo virtus feugait. Utrum neque decet. Et melior a ratis abdo cui nisl."
+              }
+            },
+            { type: "text", name: "attachment-file-name", value: "riceCooker-man.pdf" },
+            { type: "text", name: "attachment-title", value: "Singularis ut Delenit" },
+            { type: "text", name: "attachment-caption", value: "Illum luptatum quod capio augue ex abdo imputo. Sed utrum duis vel refoveo interdico facilisi zelus. Tincidunt abbas velit quis conventio. Luptatum distineo virtus feugait. Utrum neque decet." },
+          ]
+        },
+
+        { type: "question", name: "question-2", value:
+          {
+            title: "Is this manuscript being submitted in conjunction with another submission?",
+            answer: "No"
+          },
+          children: []
+        },
+
+        { type: "question", name: "question-3", value:
+          {
+            title: "Please indicate whether you have had any of the following previous interactions about this manuscript. Check all that apply.",
+            answer: ""
+          },
+          children: [
+            { type: "properties", name: "Responses:", children: [
+              { type: "question", name: "question-3a",
+                value:  {
+                  title: "I submitted a presubmission inquiry for this manuscript",
+                  answer: "2094. Sarah Tusk"
+                }
+              },
+              { type: "question", name: "question-3b",
+                value:  {
+                  title: "One or more authors (including myself) curently serve, or have previously served, as an Academic Editor or Guest Editor for this journal.",
+                  answer: ""
+                }
+              },
+            ]}
+          ]
+        },
+
+        { type: "question", name: "question-4", value:
+          {
+            title: "If your submission is intendd for a PLOS Collection, enter the name of the collection in the box below. Please also ensure the name of the collection is included in your cover letter.",
+            answer: ""
+          },
+          children: [
+            { type: "question", name: "question-4a",
+              value:  {
+                title: "Name of PLOS Collection",
+                answer: "Oceanic Critter Collection"
+              }
+            },
+          ]
+        },
+
+        { type: "question", name: "question-5", value:
+          {
+            title: "Are you or any of the contributing authors an employee of the United States Government",
+            answer: "Yes"
+          },
+        }
       ]}
     ]
+
   }, // new
 
   diffProperties: function(oldProperties, newProperties){
@@ -117,26 +242,40 @@ export default Ember.Object.extend({
     }
     let returnValue = [];
 
-    if(oldProperty.type === "question" ) {
+    if(oldProperty.type === "properties" || newProperty.type === "properties") {
+      if((oldProperty.children && oldProperty.children.length > 0) || (newProperty.children && newProperty.children.length > 0)){
+        let diff = this.diffProperties(oldProperty.children, newProperty.children);
+        returnValue.push({ type: "propertiesDiff", diffs: diff, name: newProperty.name });
+      }
+    } else if(oldProperty.type === "question" || newProperty.type === "question") {
       let oldTitle = oldProperty.value.title || "";
       let newTitle = newProperty.value.title || "";
       let oldAnswer = oldProperty.value.answer || "";
       let newAnswer = newProperty.value.answer || "";
 
-      let diff = JsDiff.diffWords(oldTitle, newTitle);
-      diff = diff.concat( JsDiff.diffWords(oldAnswer, newAnswer));
-      returnValue.push({ type: "propertyDiff", diffs: diff, name: newProperty.name });
+      let diffResults = [];
+
+      diffResults.push(
+        { type: "question-text", diffs: JsDiff.diffWords(oldTitle, newTitle), name: "title" }
+      );
+      diffResults.push(
+        { type: "question-answer", diffs:  JsDiff.diffWords(oldAnswer, newAnswer), name: "value" }
+      );
+
+      if((oldProperty.children && oldProperty.children.length > 0) || (newProperty.children && newProperty.children.length > 0)){
+        let new_diff = this.diffProperties(oldProperty.children, newProperty.children);
+        diffResults.push({ type: "propertiesDiff", diffs: new_diff, name: newProperty.name });
+      }
+
+      returnValue.push({ type: "question", diffs: diffResults, name: newProperty.name });
+
     } else if(oldProperty.type === "text"){
       let diff = JsDiff.diffSentences(oldProperty.value.toString(), newProperty.value.toString());
-      returnValue.push({ type: "propertyDiff", diffs: diff, name: newProperty.name });
+      returnValue.push({ type: "text", diffs: diff, name: newProperty.name });
+
     } else if (oldProperty.type === "boolean") {
       let diff = JsDiff.diffWords(oldProperty.value.toString(), newProperty.value.toString());
-      returnValue.push({ type: "propertyDiff", diffs: diff, name: newProperty.name });
-    }
-
-    if((oldProperty.children && oldProperty.children.length > 0) || (newProperty.children && newProperty.children.length > 0)){
-      let diff = this.diffProperties(oldProperty.children, newProperty.children);
-      returnValue.push({ type: "propertiesDiff", diffs: diff, name: newProperty.name });
+      returnValue.push({ type: "boolean", diffs: diff, name: newProperty.name });
     }
 
     return returnValue;
--- client/app/pods/components/task-diff/component.js
+++ client/app/pods/components/task-diff/component.js
@@ -11,40 +11,49 @@ export default Ember.Component.extend({
     return this.buildHTML(diffResults);
   }),
 
-  buildHTML: function(diffResults, levels){
+  buildHTML: function(diffResults, levels, insideList){
     let html = "";
     levels = levels || 0;
 
-    if(levels == 0){
+    if(levels === 0){
       html += `<div class='diffing-${diffResults[0].name}'>`;
     }
 
     _.each(diffResults, function(element){
-      if(element.type === "propertiesDiff"){
-        html +=  `<div class="diff-child">
-            <h3>${element.name}</h3>
-            ${this.buildHTML(element.diffs, levels+1)
-          }</div>`;
-      } else if(element.type === "propertyDiff"){
-        html += `<div class="property"> <label>${element.name}:</label> `;
+      if(element.type === "propertiesDiff" || element.type === "question"){
+        console.log("A: ", element);
+        if(insideList){
+          html += "<li>";
+        }
+        html +=  `
+          <h3>${element.name}</h3>
+          <ol class="diff-child diff-${element.type}">
+            ${this.buildHTML(element.diffs, levels+1, true)
+          }</ol>`;
+        if(insideList){
+          html += "</li>";
+        }
+
+      } else {
+        html += `<li class="property ${element.type}"> <label>${element.name}:</label> `;
 
         _.each(element.diffs, function(e){
+          console.log("B:", e);
           if(e.added) {
-              html += ` <span class="added">${e.value}</span> `;
+            html += ` <span class="added">${e.value}</span> `;
           } else if(e.removed) {
             html += ` <span class="removed">${e.value}</span> `;
           } else {
             html += ` <span class="unchanged">${e.value}</span> `;
           }
         });
-        html += "</div>";
+        html += "</li>";
       }
     }, this);
 
-    if(levels == 0){
+    if(levels === 0){
       html += "</div'>";
     }
-
 
     return html;
   }

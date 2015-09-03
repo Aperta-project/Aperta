9ea6c4a38df75839fc725b387ac0e32059de68d7
Hildebrando Rueda <harq.soft@gmail.com>
2015-09-03 15:57:33 -0500

    Remove unused methods from mmt edit route

--- client/app/pods/admin/journal/manuscript-manager-template/edit/route.js
+++ client/app/pods/admin/journal/manuscript-manager-template/edit/route.js
@@ -4,15 +4,10 @@ export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('manuscriptManagerTemplate', params.manuscript_manager_template_id);
   },
-  afterModel: function(model) {
-    if (!model.get('phaseTemplates.length')) {
-      return model.reload();
-    }
-  },
+
   actions: {
     saveChanges: function() {
       return this.controller.send('saveTemplate', this.get('attemptingTransition'));
-    },
-    didRollBack: function() {}
+    }
   }
 });

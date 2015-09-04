8b7d1f2b491b74417e37a2583c4e3ff4a438cb2b
Tad Park <june@tutormentor.org>
2015-09-04 10:13:50 -0700

    Change withdrawal reasons to withdrawals

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821183003) do
+ActiveRecord::Schema.define(version: 20150904165522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -273,7 +273,7 @@ ActiveRecord::Schema.define(version: 20150821183003) do
     t.string   "publishing_state"
     t.datetime "submitted_at"
     t.string   "salesforce_manuscript_id"
-    t.text     "withdrawal_reasons",       default: [],                  array: true
+    t.json     "withdrawals",              default: [],                  array: true
   end
 
   add_index "papers", ["doi"], name: "index_papers_on_doi", unique: true, using: :btree

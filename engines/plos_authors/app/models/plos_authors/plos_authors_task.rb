9bb3063800b51b09098671d195f9fe6e057b6592
Zach Dennis <zach.dennis@gmail.com>
2015-09-09 08:59:17 -0400

    WIP: Task#nested_questions is no longer a basic has_many.
    Now, the task class itself should own the master list (at some point should be moved to seed files). This will allow for less duplication of questions throughout the system and will allow us to associate answers with questions by id.
    
    This also will allow for i18n more easily should they happen and will require less migrations if question text changes.
    
    Also, this maintains the Task#api so nested_questions are returned to the front-end.

--- engines/plos_authors/app/models/plos_authors/plos_authors_task.rb
+++ engines/plos_authors/app/models/plos_authors/plos_authors_task.rb
@@ -10,19 +10,29 @@ module PlosAuthors
     validates_with AssociationValidator, association: :plos_authors, fail: :set_completion_error, if: :completed?
     validate :corresponding_plos_authors, if: :completed?
 
-    def self.add_questions(task)
-      task.nested_questions.build text: "This is a corresponding author", ident: "corresponding", value_type: "boolean"
-      task.nested_questions.build text: "This person is deceased", ident: "deceased", value_type: "boolean"
+    def self.nested_questions
+      questions = []
 
-      author_contributions = task.nested_questions.build text: "Author contributions", ident: "contributions", value_type: "question-set"
-      author_contributions.children.build text: "Conceived and designed the experiments", ident: "contributed_experiments", value_type: "boolean", position: 1
-      author_contributions.children.build text: "Performed the experiments", ident: "contributed_performing_experiments", value_type: "boolean", position: 2
-      author_contributions.children.build text: "Analyzed the data", ident: "analyzed_data", value_type: "boolean", position: 3
-      author_contributions.children.build text: "Contributed reagents/materials/analysis tools", ident: "contributed_tools", value_type: "boolean", position: 4
-      author_contributions.children.build text: "Contributed to the writing of the manuscript", ident: "contributed_writing", value_type: "boolean", position: 5
-      author_contributions.children.build text: "Other", ident: "contributed_other", value_type: "text", position: 6
+      questions << NestedQuestion.new(owner_id:1, owner_type: name, text: "This is a corresponding author", ident: "corresponding", value_type: "boolean")
+      questions << NestedQuestion.new(owner_id:1, owner_type: name, text: "This person is deceased", ident: "deceased", value_type: "boolean")
 
-      task
+      author_contributions = NestedQuestion.new owner_id:1, owner_type: name, text: "Author contributions", ident: "contributions", value_type: "question-set"
+      author_contributions.children.build owner_id:1, owner_type: name, text: "Conceived and designed the experiments", ident: "contributed_experiments", value_type: "boolean", position: 1
+      author_contributions.children.build owner_id:1, owner_type: name, text: "Performed the experiments", ident: "contributed_performing_experiments", value_type: "boolean", position: 2
+      author_contributions.children.build owner_id:1, owner_type: name, text: "Analyzed the data", ident: "analyzed_data", value_type: "boolean", position: 3
+      author_contributions.children.build owner_id:1, owner_type: name, text: "Contributed reagents/materials/analysis tools", ident: "contributed_tools", value_type: "boolean", position: 4
+      author_contributions.children.build owner_id:1, owner_type: name, text: "Contributed to the writing of the manuscript", ident: "contributed_writing", value_type: "boolean", position: 5
+      author_contributions.children.build owner_id:1, owner_type: name, text: "Other", ident: "contributed_other", value_type: "text", position: 6
+      questions << author_contributions
+
+      # TODO MOVE THIS OUT TO A SEED FILE
+      questions.each do |q|
+        unless NestedQuestion.where(owner_id:1, owner_type:name, ident:q.ident).exists?
+          q.save!
+        end
+      end
+
+       NestedQuestion.where(owner_id:1, owner_type:name).all
     end
 
     def active_model_serializer
@@ -41,6 +51,10 @@ module PlosAuthors
     def corresponding_plos_authors
       return true if plos_authors.where(corresponding: true).exists?
       self.errors.add(:corresponding, "You must have at least one corresponding author.")
+    end
+
+    def nested_questions
+      self.class.nested_questions
     end
 
     def set_completion_error

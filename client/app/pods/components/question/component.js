45446648b157cb6aa627659ff2b2e184224f119a
Matt Mercieca <mercieca@gmail.com>
2015-09-10 10:54:25 -0400

    +Converting competing interests component coffeescript to javascript +Converting question radio component coffeescript to javascript
    This was a straight conversion. No functionaility has changed at this point.
    
    [Pivotal Card #101433534]

--- client/app/pods/components/question/component.js
+++ client/app/pods/components/question/component.js
@@ -7,6 +7,7 @@ QuestionComponent = Ember.Component.extend({
   disabled: false,
   readonly: false,
   noResponseText: "[No response]",
+
   model: (function() {
     var decision, ident, question, task;
     ident = this.get('ident');
@@ -20,6 +21,7 @@ QuestionComponent = Ember.Component.extend({
     }
     return question;
   }).property('task', 'ident'),
+
   createNewQuestion: function() {
     var data, key, question, task, value;
     task = this.get('task');
@@ -40,11 +42,13 @@ QuestionComponent = Ember.Component.extend({
     task.get('questions').pushObject(question);
     return question;
   },
+
   additionalData: Ember.computed.alias('model.additionalData'),
   change: function() {
     Ember.run.debounce(this, this._saveModel, this.get('model'), 200);
     return false;
   },
+
   _saveModel: function(model) {
     return model.save();
   }

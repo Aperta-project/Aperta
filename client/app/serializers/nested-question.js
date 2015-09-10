a7581e1fa51ca70fe868a53afa9396b107d4c375
Matt Mercieca <mercieca@gmail.com>
2015-09-10 16:27:18 -0400

    Getting a nested questions to work with data and in Ember Adding nested-question-textarea

---
+++ client/app/serializers/nested-question.js
@@ -0,0 +1,8 @@
+import ApplicationSerializer from 'tahi/serializers/application';
+import DS from 'ember-data';
+
+export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin, {
+  attrs: {
+    children: {embedded: 'always'}
+  }
+});

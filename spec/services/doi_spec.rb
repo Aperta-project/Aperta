9affa7ecd01fa39ca0f2e4e8fe34fedfd6856787
Benjamin Moses Lieb <contact@pixelearth.net>
2015-09-10 14:46:32 -0400

    fixing doi specs

--- spec/services/doi_spec.rb
+++ spec/services/doi_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 describe Doi do
 
   let(:journal_doi) do
-    [journal[:doi_publisher_prefix],
-    journal[:doi_journal_prefix]].join('/') +
-    "." + journal[:last_doi_issued]
+    [journal[:doi_publisher_prefix], journal[:doi_journal_prefix]].join('/') + "." + journal[:last_doi_issued]
   end
 
   describe ".valid?" do

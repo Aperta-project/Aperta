52d29fb47e31fea6dba9f016cbb032f3cb300ece
Zach Dennis <zach.dennis@gmail.com>
2015-09-11 12:02:52 -0400

    Competing interests now successfully saves a value for the "statement" in both cases where Yes/No was selected
    Note: when selecting No text value was still being saved. It was a trinity boolean: Yes\No\text.
    
    * nested-question-radio now uses true/false for its yes/no values.
    * update NestedQuestionAnswer to return the interpreted value based on its value_type
    
    [Pivotal Card #101433534]

--- engines/tahi_standard_tasks/client/app/controllers/overlays/competing-interests.js
+++ engines/tahi_standard_tasks/client/app/controllers/overlays/competing-interests.js
@@ -3,7 +3,26 @@ import SavesQuestionsOnClose from 'tahi/mixins/saves-questions-on-close';
 var CompetingInterestsOverlayController;
 
 CompetingInterestsOverlayController = TaskController.extend(SavesQuestionsOnClose, {
-  declareNoCompeteCopy: "The authors have declared that no competing interests exist."
+  declareNoCompeteCopy: "The authors have declared that no competing interests exist.",
+
+  competingInterestsStatementQuestion: Ember.computed(function(){
+    return this.get('model').findQuestion('competing_interests.statement');
+  }),
+
+  setCompetingInterestStatement: function(text) {
+    let answer = this.get('competingInterestsStatementQuestion.answer');
+    answer.set('value', text);
+    answer.save();
+  },
+
+  actions: {
+    userSelectedNo: function(){
+      this.setCompetingInterestStatement(this.get('declareNoCompeteCopy'));
+    },
+    userSelectedYes: function() {
+      this.setCompetingInterestStatement('');
+    }
+  }
 });
 
 export default CompetingInterestsOverlayController;

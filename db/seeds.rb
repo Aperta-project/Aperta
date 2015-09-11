961a90677c5a44fc7e5298d67335275930da11e6
Benjamin Moses Lieb <contact@pixelearth.net>
2015-09-11 12:31:31 -0400

    add attrs necessary for do to journal seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ else
   Rake::Task['data:create_task_types'].invoke
 
   # Create Journal
-  plos_journal = Journal.first_or_create!(name: 'PLOS Yeti', logo: '')
+  plos_journal = Journal.first_or_create!(name: 'PLOS Yeti', logo: '', doi_publisher_prefix: "yetipub", doi_journal_prefix: "yetijour", last_doi_issued: "1000000")
 
   # Create Users
   # These Users should match Personas, by convention

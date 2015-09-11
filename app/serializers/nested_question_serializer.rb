e24b53c958d8107b96cc5083ed064a77566cfb90
Zach Dennis <zach.dennis@gmail.com>
2015-09-10 22:43:07 -0400

    Task now sets the owner of nested questions to itself when asked for them.
    This allows the front-end to see questions as belonging a task even tho in the DB they are owned by a master task (this avoids unnecessary duplication of questions).
    
    Additional notes:
    
     * the front-end now sets the task nested-question/component now sets the task to the nested-question-answer since it needs it in order to save back to the server.

--- app/serializers/nested_question_serializer.rb
+++ app/serializers/nested_question_serializer.rb
@@ -1,4 +1,8 @@
 class NestedQuestionSerializer < ActiveModel::Serializer
-  attributes :id, :parent_id, :text, :ident, :value, :value_type
+  attributes :id, :parent_id, :text, :ident, :value, :value_type, :task_id
   has_many :children, serializer: NestedQuestionSerializer
+
+  def task_id
+    object.owner_id
+  end
 end

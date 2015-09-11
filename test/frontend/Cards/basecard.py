b72c4e923a01cb191e4f788f1116f5cd5662b3c3
Sebastian Bassi <sebastian.bassi@globant.com>
2015-09-10 21:27:41 -0300

    APERTA-53 Partial work. Testing online a button that work with AJAX

--- test/frontend/Cards/basecard.py
+++ test/frontend/Cards/basecard.py
@@ -3,6 +3,7 @@
 import time
 
 from selenium.webdriver.common.by import By
+from loremipsum import generate_paragraph
 
 from frontend.Pages.authenticated_page import AuthenticatedPage, application_typeface
 
@@ -23,7 +24,7 @@ class BaseCard(AuthenticatedPage):
       "//span[contains(text(), 'Your notepad')]/preceding-sibling::i")
     self._header_link = (By.CLASS_NAME, 'overlay-header-link')
     self._manuscript_icon = (By.CLASS_NAME, 'manuscript-icon')
-    # *[local-name() = 'svg']/*[local-name() = 'path']
+    self._discussion_div = (By.CLASS_NAME, 'overlay-discussion-board')
 
   # Common actions for all cards
   def click_close_button(self):
@@ -46,15 +47,22 @@ class BaseCard(AuthenticatedPage):
     return self._get(self._notepad_textarea).get_attribute('value')
 
   def insert_text_notepad(self, text, clear=True):
-    """
-    Insert text in the textarea space in the notepad element
-    """
+    """Insert text in the textarea space in the notepad element"""
     if clear:
       self._get(self._notepad_textarea).clear()
     self._get(self._notepad_textarea).send_keys(text)
     # Insert a wait time so it can be saved
     time.sleep(3)
     return self
+
+  def insert_text_discussion(self, text, clear=True):
+    """Insert text in the textarea space as comment"""
+    discussion_text_area = self._get(self._discussion_div).find_element_by_tag_name('textarea')
+    if clear:
+      discussion_text_area.clear()
+    discussion_text_area.send_keys(text)
+    # Insert a wait time so it can be saved
+    time.sleep(3)
 
   def is_notepad_icon_open(self):
     """Check if notepad icon is in open position"""
@@ -77,6 +85,19 @@ class BaseCard(AuthenticatedPage):
     assert title.value_of_css_property('line-height') == '16.2px', title.value_of_css_property('line-height')
     assert title.value_of_css_property('color') == 'rgba(57, 163, 41, 1)'
 
+  @staticmethod
+  def validate_card_h2_style(title):
+    """
+    Ensure consistency in rendering page and overlay h2 section headings across the all cards
+    :param title: title to validate
+    # TODO: Validate with the result of #103123812
+    """
+    assert application_typeface in title.value_of_css_property('font-family')
+    assert title.value_of_css_property('font-size') == '20px'
+    assert title.value_of_css_property('font-weight') == '500'
+    assert title.value_of_css_property('line-height') == '22px'
+    assert title.value_of_css_property('color') == 'rgba(51, 51, 51, 1)'
+
   def validate_common_elements_styles(self):
     """Validate styles from elements common to all cards"""
     header_link = self._get(self._header_link)
@@ -90,4 +111,26 @@ class BaseCard(AuthenticatedPage):
     # Close btn
     close_btn = self._get(self._close_button)
     self.validate_secondary_green_button_style(close_btn)
-    
+    #h2
+    discussion_div = self._get(self._discussion_div)
+    discussion_title = discussion_div.find_element_by_tag_name('h2')
+    assert discussion_title.text == 'Discussion', discussion_title.text
+    self.validate_card_h2_style(discussion_title)
+    # Text area before clicking on it
+    discussion_text_area = discussion_div.find_element_by_tag_name('textarea')
+    assert discussion_text_area.get_attribute('placeholder') == 'Type your message here'
+    # Text area after clicking on it
+    discussion_text_area.click()
+    time.sleep(1)
+    discussion_div = self._get(self._discussion_div)
+    post_btn = discussion_div.find_element_by_tag_name('button')
+    assert post_btn.text == 'POST MESSAGE', post_btn.text
+    self.validate_secondary_green_button_style(post_btn)
+    cancel_lnk = discussion_div.find_element_by_tag_name('a')
+    assert cancel_lnk.text == 'Cancel', cancel_lnk.text
+    self.validate_default_link_style(cancel_lnk)
+    # Enter some text
+    expected_text = generate_paragraph()[2]
+    self.insert_text_discussion(expected_text)
+    post_btn.click()
+    time.sleep(10)

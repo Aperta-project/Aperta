53db3ef7fdd14134eae507dd71b9d24afc6c4aa4
Hildebrando Rueda <harq.soft@gmail.com>
2015-09-11 18:09:42 -0500

    Remove stale mixin
    this mixin sets a lastEditable property and observe for changes to
    update this property but is never used in the app

--- client/app/mixins/views/paper-index.js
+++ client/app/mixins/views/paper-index.js
@@ -1,31 +1,28 @@
 import Ember from 'ember';
-import RedirectsIfEditable from 'tahi/mixins/views/redirects-if-editable';
 
-let on = Ember.on;
-
-export default Ember.Mixin.create(RedirectsIfEditable, {
+export default Ember.Mixin.create({
   classNames: ['edit-paper'],
   editor: null,
   locked: Ember.computed.alias('controller.locked'),
   isEditing: Ember.computed.alias('controller.isEditing'),
 
-  setBackgroundColor: on('didInsertElement', function() {
+  setBackgroundColor: Ember.on('didInsertElement', function() {
     $('html').addClass('matte');
   }),
 
-  resetBackgroundColor: on('willDestroyElement', function() {
+  resetBackgroundColor: Ember.on('willDestroyElement', function() {
     $('html').removeClass('matte');
   }),
 
-  applyManuscriptCss: on('didInsertElement', function() {
+  applyManuscriptCss: Ember.on('didInsertElement', function() {
     $('#paper-body').attr('style', this.get('controller.model.journal.manuscriptCss'));
   }),
 
-  teardownControlBarSubNav: on('willDestroyElement', function() {
+  teardownControlBarSubNav: Ember.on('willDestroyElement', function() {
     $('html').removeClass('control-bar-sub-nav-active');
   }),
-
-  saveTitleChanges: on('willDestroyElement', function() {
+  
+  saveTitleChanges: Ember.on('willDestroyElement', function() {
     this.timeoutSave();
   })
 });

f79d52eb65f0260af5900b664090de7c9a02c264
Zach Dennis <zach.dennis@gmail.com>
2015-09-11 17:58:57 -0400

    Spec'ing NestedQuestionAnswersController.

---
+++ spec/factories/nested_questions.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :nested_question do
+    association :owner, factory: :task
+    sequence(:ident){ |n| "ident_#{n}"}
+    sequence(:position){ |n| n }
+    sequence(:value){ |n| "value #{n}" }
+    value_type "text"
+  end
+end

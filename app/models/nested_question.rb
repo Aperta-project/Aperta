2e7b5664bc45f82ced97cf6f19c5743076a97490
Zach Dennis <zach.dennis@gmail.com>
2015-09-13 16:02:27 -0400

    Spec'ing NestedQuestion and NestedQuestionAnswer.

--- app/models/nested_question.rb
+++ app/models/nested_question.rb
@@ -3,6 +3,9 @@ class NestedQuestion < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
   has_many :nested_question_answers
 
+  validates :ident, presence: true
+  validates :value_type, presence: true
+
   # A question itself doesn't have a single answer so we don't save answers
   # here. However, it sure is nice when working with questions in a particular
   # context to be able to easily access its answer.

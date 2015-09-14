5c278d379de8cc3e3cc983c482e02b1696bff444
Zach Dennis <zach.dennis@gmail.com>
2015-09-13 21:00:24 -0400

    Adding client-side tests for Task#findQuestion
    * marking #findQuestions and #childrenOfQuestions as private methods as they're not consumed by anyone outside of Task

--- client/app/models/task.js
+++ client/app/models/task.js
@@ -33,30 +33,6 @@ export default DS.Model.extend(CardThumbnailObserver, {
     inverse: 'task',
     async: false,
   }),
-  findQuestion: function(ident){
-    let pathParts = ident.split(".");
-    let nestedQuestions = this.get('nestedQuestions').toArray();
-    let foundQuestions = this.findQuestions(pathParts, nestedQuestions);
-    return _.first(foundQuestions);
-  },
-
-  findQuestions: function(pathParts, questions){
-    let currentIdent = _.first(pathParts);
-    let remainingPathParts = _.rest(pathParts);
-    let foundQuestions = _.select(questions, (q) => { return q.get('ident') === currentIdent; });
-
-    if(_.isEmpty(remainingPathParts)){
-      return foundQuestions;
-    } else {
-      return this.findQuestions(remainingPathParts, this.childrenOfQuestions(foundQuestions).toArray());
-    }
-  },
-
-  childrenOfQuestions: function(questions){
-    let children =  _(questions).invoke("get", "children");
-    let allTheChildren = _.invoke(children, "toArray");
-    return _.flatten( allTheChildren );
-  },
 
   body: DS.attr(),
   completed: DS.attr('boolean'),
@@ -68,4 +44,29 @@ export default DS.Model.extend(CardThumbnailObserver, {
   role: DS.attr('string'),
   title: DS.attr('string'),
   type: DS.attr('string'),
+
+  findQuestion: function(ident){
+    let pathParts = ident.split(".");
+    let nestedQuestions = this.get('nestedQuestions').toArray();
+    let foundQuestions = this._findQuestions(pathParts, nestedQuestions);
+    return _.first(foundQuestions);
+  },
+
+  _findQuestions: function(pathParts, questions){
+    let currentIdent = _.first(pathParts);
+    let remainingPathParts = _.rest(pathParts);
+    let foundQuestions = _.select(questions, (q) => { return q.get('ident') === currentIdent; });
+
+    if(_.isEmpty(remainingPathParts)){
+      return foundQuestions;
+    } else {
+      return this._findQuestions(remainingPathParts, this._childrenOfQuestions(foundQuestions).toArray());
+    }
+  },
+
+  _childrenOfQuestions: function(questions){
+    let children =  _(questions).invoke("get", "children");
+    let allTheChildren = _.invoke(children, "toArray");
+    return _.flatten( allTheChildren );
+  }
 });

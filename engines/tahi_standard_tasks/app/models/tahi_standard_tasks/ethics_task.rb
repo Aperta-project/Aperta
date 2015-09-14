cd32027eb2df3abf632119532eef924c8f5f2725
Zach Dennis <zach.dennis@gmail.com>
2015-09-14 10:22:54 -0400

    Updating the EthicsTask to use nested-question components and data model.
    [Pivotal Card #101433534]

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/ethics_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/ethics_task.rb
@@ -2,5 +2,24 @@ module TahiStandardTasks
   class EthicsTask < Task
     include MetadataTask
     register_task default_title: "Ethics Statement", default_role: "author"
+
+    def self.nested_questions
+      questions = []
+      questions << NestedQuestion.new(owner_id:nil, owner_type: name, ident: "human_subjects", value_type: "boolean", text: "Does your study involve Human Subject Research (human participants and/or tissue)?", children: [
+        NestedQuestion.new(owner_id:nil, owner_type: name, ident: "participants", value_type: "text", text: "Please enter the name of the IRB or Ethics Committee that approved this study in the space below. Include the approval number and/or a statement indicating approval of this research.")
+      ])
+
+      questions << NestedQuestion.new(owner_id:nil, owner_type: name, ident: "animal_subjects", value_type: "boolean", text: "Does your study involve Animal Research (vertebrate animals, embryos or tissues)?", children: [
+        NestedQuestion.new(owner_id:nil, owner_type: name, ident: "field_permit", value_type: "text", text: "Please enter your statement below:")
+      ])
+
+      questions.each do |q|
+        unless NestedQuestion.where(owner_id:nil, owner_type:name, ident:q.ident).exists?
+          q.save!
+        end
+      end
+
+      NestedQuestion.where(owner_id:nil, owner_type:name).all
+    end
   end
 end
--- engines/tahi_standard_tasks/client/app/controllers/overlays/data-availability.js
+++ engines/tahi_standard_tasks/client/app/controllers/overlays/data-availability.js
@@ -2,14 +2,10 @@ import TaskController from 'tahi/pods/paper/task/controller';
 
 export default TaskController.extend({
   dataLocationQuestion: Ember.computed("model", function(){
-    let model = this.get("model");
-    Ember.assert("Expected model to be set, but it wasn't.", model);
-    return model.findQuestion("data_location");
+    return this.get("model").findQuestion("data_location");
   }),
 
   fullyAvailableQuestion: Ember.computed("model", function(){
-    let model = this.get("model");
-    Ember.assert("Expected model to be set, but it wasn't.", model);
-    return model.findQuestion("data_fully_available");
+    return this.get("model").findQuestion("data_fully_available");
   }),
 });

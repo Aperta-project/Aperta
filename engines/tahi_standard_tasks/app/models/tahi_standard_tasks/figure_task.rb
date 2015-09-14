3836f2b164718b0c0016ac032fe5e11654f74038
Zach Dennis <zach.dennis@gmail.com>
2015-09-14 15:39:30 -0400

    Updating FigureTask to use nested-question components and data model.
    [Pivotal Card #101433534]

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/figure_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/figure_task.rb
@@ -4,6 +4,25 @@ module TahiStandardTasks
 
     register_task default_title: "Figures", default_role: "author"
 
+    def self.nested_questions
+      questions = []
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "figure_complies",
+        value_type: "boolean",
+        text: "Yes - I confirm our figures comply with the guidelines."
+      )
+
+      questions.each do |q|
+        unless NestedQuestion.where(owner_id:nil, owner_type:name, ident:q.ident).exists?
+          q.save!
+        end
+      end
+
+      NestedQuestion.where(owner_id:nil, owner_type:name).all
+    end
+
     def figure_access_details
       paper.figures.map(&:access_details)
     end
--- engines/tahi_standard_tasks/client/app/controllers/overlays/figure.js
+++ engines/tahi_standard_tasks/client/app/controllers/overlays/figure.js
@@ -11,6 +11,10 @@ export default TaskController.extend(FileUploadMixin, {
               .sortBy('createdAt').reverse();
   }.property('model.paper.figures.[]', 'model.paper.figures.@each.createdAt'),
 
+  figureCompliesQuestion: Ember.computed('model', function(){
+    return this.get('model').findQuestion('figure_complies');
+  }),
+
   actions: {
     uploadFinished(data, filename) {
       this.uploadFinished(data, filename);

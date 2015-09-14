c9d62a24633d4956333fa7541847840eea525665
Zach Dennis <zach.dennis@gmail.com>
2015-09-14 15:39:25 -0400

    Making a nested-question-check component based on question-check component.

---
+++ client/app/pods/components/nested-question-check/component.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import NestedQuestionComponent from 'tahi/pods/components/nested-question/component';
+
+export default NestedQuestionComponent.extend({
+  displayQuestion: null,
+
+  checked: Ember.computed('model.answer.value', {
+    get() {
+      let answer = this.get('model.answer.value');
+      return answer === 'true' || answer === true;
+    },
+    set(key, newValue) {
+      return this.set('model.answer.value', newValue);
+    }
+  }),
+
+  actions: {
+    additionalDataAction() {
+      this.get('additionalData').pushObject({});
+    }
+  }
+});

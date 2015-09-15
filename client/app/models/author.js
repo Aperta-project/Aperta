592a6d303532446b638386e343b77cb53aa5c8cd
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-09-15 13:29:46 -0400

    Move plos_author attrs to core author model

--- client/app/models/author.js
+++ client/app/models/author.js
@@ -3,9 +3,26 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   paper: DS.belongsTo('paper', { async: false }),
+  authorsTask: DS.belongsTo('authorsTask'),
 
   firstName: DS.attr('string'),
+  middleInitial: DS.attr('string'),
   lastName: DS.attr('string'),
+  email: DS.attr('string'),
+  title: DS.attr('string'),
+  department: DS.attr('string'),
+
+  affiliation: DS.attr('string'),
+  ringgoldId: DS.attr('string'),
+
+  secondaryAffiliation: DS.attr('string'),
+  secondaryRinggoldId: DS.attr('string'),
+
+  corresponding: DS.attr('boolean'),
+  deceased: DS.attr('boolean'),
+  position: DS.attr('number'),
+  contributions: DS.attr(),
+
   position: DS.attr('number'),
 
   fullName: Ember.computed('firstName', 'middleInitial', 'lastName', function() {

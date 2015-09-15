63419c2e5276f35a431db56d68a94c697eb6c33a
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-09-15 16:54:14 -0400

    Remove authorTask dependency from core ember tests

--- client/tests/factories/plos-authors-task.js
+++
@@ -1,9 +0,0 @@
-import FactoryGuy from "ember-data-factory-guy";
-
-FactoryGuy.define('authors-task', {
-  default: {
-    title: 'Authors',
-    type: 'AuthorsTask',
-    completed: false,
-  }
-});

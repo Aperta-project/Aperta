7118d3488f53336146e9afe999c6b81aa4650989
Zach Dennis <zach.dennis@gmail.com>
2015-09-16 08:41:00 -0400

    Converting the Funder portion of the FinancialDisclosureTask to the nested-question components and data model.
    This is not working yet because the ember-side needs to be updated to so that a NestedQuestion/Answer can belong to non-Task models like Funder.

--- engines/tahi_standard_tasks/app/serializers/tahi_standard_tasks/funder_serializer.rb
+++ engines/tahi_standard_tasks/app/serializers/tahi_standard_tasks/funder_serializer.rb
@@ -5,5 +5,7 @@ module TahiStandardTasks
 
     has_one :task, embed: :ids
     has_many :authors, embed: :ids, include: true
+    has_many :nested_questions, serializer: NestedQuestionSerializer, embed: :ids, include: true
+    has_many :nested_question_answers, serializer: NestedQuestionAnswerSerializer, embed: :ids, include: true
   end
 end

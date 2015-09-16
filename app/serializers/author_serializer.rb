3215630dfc76be6e660ac3dcad9a4a0b956c4d94
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-09-16 14:14:05 -0400

    Adjust indentation

--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -1,18 +1,7 @@
 class AuthorSerializer < ActiveModel::Serializer
-  attributes :id,
-    :paper_id,
-    :position,
-    :first_name,
-    :middle_initial,
-    :last_name,
-    :email,
-    :affiliation,
-    :secondary_affiliation,
-    :title,
-    :corresponding,
-    :deceased,
-    :department,
-    :contributions
+  attributes :affiliation, :contributions, :corresponding, :deceased, :department,
+             :email, :first_name, :id, :last_name, :middle_initial, :paper_id,
+             :position, :secondary_affiliation, :title
 
   has_one :authors_task, embed: :id
 end

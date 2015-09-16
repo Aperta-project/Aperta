7194f26d44bc502f4d1c67c3c91be9de6fffcfdd
Zach Dennis <zach.dennis@gmail.com>
2015-09-16 11:49:15 -0400

    Updating NestedQuestionAnswer to handle update and destroy actions.
    * because ember and rails have different philosophies on polymorphic associations we have to introduce picking logic for finding the correct "owner_type" for an answer. *sigh*. 
    
         E.g. ember submits "Funder" but we have TahiStandardTasks::Funder
    
    I am not sure how pluggable architecture was ever going to work for things not embedded directly into core.
    
    In an ideal world it seems like the ember-side models would be able to be looked up via "tahi-standard-tasks/funder" so it would resubmit that as a value to the server. *shrug*

--- app/serializers/nested_question_answer_serializer.rb
+++ app/serializers/nested_question_answer_serializer.rb
@@ -2,7 +2,7 @@ class NestedQuestionAnswerSerializer < ActiveModel::Serializer
   attributes :id, :value, :value_type, :owner, :nested_question_id
 
   def owner
-    { id: object.owner_id, type: object.owner_type }
+    { id: object.owner_id, type: object.owner_type.demodulize }
   end
 
 end
--- engines/tahi_standard_tasks/client/app/components/funder-dataset.js
+++ engines/tahi_standard_tasks/client/app/components/funder-dataset.js
@@ -4,11 +4,11 @@ import Utils from "tahi/services/utils";
 export default  Ember.Component.extend({
   classNames: ["dataset"],
 
-  funderHadInfluenceQuestion: Ember.computed("model", "model.nestedQuestions.@each", function(){
+  funderHadInfluenceQuestion: Ember.computed("model", "model.nestedQuestions.@each", "model.nestedQuestionAnswers.@each", function(){
     return this.get("model").findQuestion("funder_had_influence");
   }),
 
-  funderRoleDescriptionQuestion: Ember.computed("model", "model.nestedQuestions.@each", function(){
+  funderRoleDescriptionQuestion: Ember.computed("model", "model.nestedQuestions.@each", "model.nestedQuestionAnswers.@each", function(){
     return this.get("model").findQuestion("funder_role_description");
   }),
 
@@ -24,17 +24,19 @@ export default  Ember.Component.extend({
     return Ember.run.debounce(this, this._saveModel, 400);
   },
 
+  setFunderRoleDescriptionAnswer: function(value){
+    let answer = this.get('funderRoleDescriptionQuestion.answer');
+    answer.set('value', '');
+    answer.save();
+  },
+
   actions: {
     userSelectedYesForFunderInfluence: function(){
-      // let answer = this.get('funderHadInfluenceQuestion.answer');
-      // answer.set('value', '');
-      // answer.save();
+      this.setFunderRoleDescriptionAnswer('');
     },
 
     userSelectedNoForFunderInfluence: function(){
-      let answer = this.get('funderHadInfluenceQuestion.answer');
-      answer.destroyRecord();
-      // this.get("model.getNestedQuestion")
+      this.setFunderRoleDescriptionAnswer('');
     },
 
     removeFunder: function(disabled) {

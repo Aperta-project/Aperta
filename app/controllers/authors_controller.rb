d215186516e2923286c0b6b4f7230c1140bc3d31
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-09-16 12:14:24 -0400

    Enforce access policy for Author
    This was previously being enforce on PlosAuthorsController. Only
    enforcing the access policy on half of the Author-PlosAuthor arrangement
    was probably not enough, but this should now be fine.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,25 +1,44 @@
 class AuthorsController < ApplicationController
   before_action :authenticate_user!
+  before_action :enforce_policy
   respond_to :json
 
   def create
-    author = Author.create(author_params)
+    author.save
+
+    # render all authors, since position is controlled by acts_as_list
     render json: author.paper.authors, each_serializer: AuthorSerializer
   end
 
   def update
-    author = Author.find(params[:id])
-    author.update author_params
+    author.update(author_params)
+
+    # render all authors, since position is controlled by acts_as_list
     render json: author.paper.authors, each_serializer: AuthorSerializer
   end
 
   def destroy
-    author = Author.find(params[:id])
     author.destroy
+
+    # render all authors, since position is controlled by acts_as_list
     render json: author.paper.authors, each_serializer: AuthorSerializer
   end
 
   private
+
+  def enforce_policy
+    authorize_action!(author: author)
+  end
+
+  def author
+    @author ||= begin
+      if params[:id].present?
+        Author.find(params[:id])
+      else
+        Author.new(author_params)
+      end
+    end
+  end
 
   def author_params
     params.require(:author).permit(

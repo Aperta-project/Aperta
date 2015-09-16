2f4f4b4075cc5df6c09ca7d941dc6fbe0f513cba
Hildebrando Rueda <harq.soft@gmail.com>
2015-09-16 11:26:18 -0500

    Show all connected papers in paper tracker

--- spec/controllers/paper_tracker_controller_spec.rb
+++ spec/controllers/paper_tracker_controller_spec.rb
@@ -2,51 +2,32 @@ require 'rails_helper'
 
 describe PaperTrackerController do
   let(:user) { FactoryGirl.create :user }
-  let(:assigned_journal) { FactoryGirl.create :journal }
-  let(:unassigned_journal) { FactoryGirl.create :journal }
-  let!(:user_role) do
-    role = FactoryGirl.create(
-      :role,
-      :flow_manager,
-      journal: assigned_journal)
-    UserRole.create(role: role, user: user)
-   end
 
   before { sign_in user }
 
-  describe "index" do
-    context "there's a paper in the assigned journal" do
-      let!(:paper) { FactoryGirl.create(:paper, :submitted, journal: assigned_journal) }
-      let(:response) { get :index, format: :json }
-      let(:response_papers) { JSON.parse(response.body)['papers'] }
+  describe "on GET #index" do
 
-      it "it appears in the list" do
-        expect(response_papers.count).to be(1)
-      end
+    it "list the paper where user have a role" do
+      paper = FactoryGirl.create(:paper, :submitted)
+      assign_paper_role(paper, user, PaperRole::ADMIN)
+      get :index, format: :json
+      json = JSON.parse(response.body)
+      expect(json["papers"].size).to eq 1
+      expect(json["papers"][0]["title"]).to eq paper.title
     end
-  end
 
-  describe "index" do
-    context "there's a paper in the unassigned journal" do
-      let!(:paper) { FactoryGirl.create(:paper, :submitted, journal: unassigned_journal) }
-      let(:response) { get :index, format: :json }
-      let(:response_papers) { JSON.parse(response.body)['papers'] }
-
-      it "does not appear" do
-        expect(response_papers.count).to be(0)
-      end
+    it "do not list the paper if is not submitted" do
+      paper = FactoryGirl.create(:paper)
+      assign_paper_role(paper, user, PaperRole::ADMIN)
+      get :index, format: :json
+      json = JSON.parse(response.body)
+      expect(json["papers"].size).to eq 0
     end
-  end
 
-  describe "index" do
-    context "there's an unsubmitted paper in the assigned journal" do
-      let!(:paper) { FactoryGirl.create(:paper, journal: assigned_journal) }
-      let(:response) { get :index, format: :json }
-      let(:response_papers) { JSON.parse(response.body)['papers'] }
-
-      it "does not appear" do
-        expect(response_papers.count).to be(0)
-      end
+    it "do not list the paper where user do not have a role" do
+      get :index, format: :json
+      json = JSON.parse(response.body)
+      expect(json["papers"].size).to eq 0
     end
   end
 end

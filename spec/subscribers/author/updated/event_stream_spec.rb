c7fbf4dd3745ac366ffeffbdca35e0cfb78cabee
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-09-16 13:58:41 -0400

    Remove factory trait
    `with_paper` has been removed since it is assumed you almost always
    want to build a paper when building an author

--- spec/subscribers/author/updated/event_stream_spec.rb
+++ spec/subscribers/author/updated/event_stream_spec.rb
@@ -4,7 +4,7 @@ describe Author::Updated::EventStream do
   include EventStreamMatchers
 
   let(:pusher_channel) { mock_delayed_class(TahiPusher::Channel) }
-  let(:author) { FactoryGirl.build(:author, :with_paper) }
+  let(:author) { FactoryGirl.build(:author) }
 
   it "serializes author down the paper channel on update" do
     expect(pusher_channel).to receive_push(serialize: :authors, down: 'paper', on: 'updated')

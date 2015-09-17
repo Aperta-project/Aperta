b53b7da97afcd53cc73c858e5642b4fdf6fb73a0
Hildebrando Rueda <harq.soft@gmail.com>
2015-09-17 09:21:48 -0500

    Prevent duplicate ids od journals

--- app/controllers/paper_tracker_controller.rb
+++ app/controllers/paper_tracker_controller.rb
@@ -4,8 +4,13 @@ class PaperTrackerController < ApplicationController
   respond_to :json
 
   def index
-    journal_ids = current_user.roles.pluck(:journal_id)
     papers = Paper.submitted.where(journal_id: journal_ids)
     respond_with papers, each_serializer: PaperTrackerSerializer, root: 'papers'
+  end
+
+  private
+
+  def journal_ids
+    current_user.roles.pluck(:journal_id).uniq
   end
 end

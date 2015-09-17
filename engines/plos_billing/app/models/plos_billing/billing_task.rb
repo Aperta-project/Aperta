4960484fec00ce3afccc039a48871fa86f87d7a8
Zach Dennis <zach.dennis@gmail.com>
2015-09-17 11:59:13 -0400

    Beginning to etch nested questions for the PLOS Billing card.
    This only touches the first part of the form which is the user filling out their billing info.
    
    * need to look at institutional search component
    * need to look at the question-select component
    * need to look at everything at Payment Options and below
    
    [Pivotal Card #101433534]

--- engines/plos_billing/app/models/plos_billing/billing_task.rb
+++ engines/plos_billing/app/models/plos_billing/billing_task.rb
@@ -4,6 +4,106 @@ module PlosBilling
 
     register_task default_title: "Billing", default_role: "author"
 
+    def self.nested_questions
+      questions = []
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "first_name",
+        value_type: "text",
+        text: "First Name"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "last_name",
+        value_type: "text",
+        text: "Last Name"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "title",
+        value_type: "text",
+        text: "Title"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "department",
+        value_type: "text",
+        text: "Department"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "phone_number",
+        value_type: "text",
+        text: "Phone"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "email",
+        value_type: "text",
+        text: "Email"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "address1",
+        value_type: "text",
+        text: "Address Line 1 (optional)"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "address2",
+        value_type: "text",
+        text: "Address Line 2 (optional)"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "city",
+        value_type: "text",
+        text: "City"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "state",
+        value_type: "text",
+        text: "State or Province"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "postal_code",
+        value_type: "text",
+        text: "ZIP or Postal Code"
+      )
+
+      questions.each do |q|
+        unless NestedQuestion.where(owner_id:nil, owner_type:name, ident:q.ident).exists?
+          q.save!
+        end
+      end
+
+      NestedQuestion.where(owner_id:nil, owner_type:name).all
+    end
+
     def active_model_serializer
       TaskSerializer
     end

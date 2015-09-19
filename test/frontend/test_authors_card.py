ab7b5173a2c979bfab1d303291e7886122ba1581
Sebastian Bassi <sebastian.bassi@globant.com>
2015-09-19 19:07:06 -0300

    APERTA-3: PR requested changes. Debug

--- test/frontend/test_authors_card.py
+++ test/frontend/test_authors_card.py
@@ -26,19 +26,14 @@ class AuthorsCardTest(CommonTest):
 
   def _go_to_authors_card(self, init=True):
     """Go to the authors card"""
-    dashboard = LoginPage.login() if init else DashboardPage(self.getDriver())
-    article_name = LoginPage.select_preexisting_article(init=False)
+    dashboard = self.login() if init else DashboardPage(self.getDriver())
+    article_name = self.select_preexisting_article(init=False)
     manuscript_page = ManuscriptPage(self.getDriver())
     manuscript_page.click_authors_card()
     return AuthorsCard(self.getDriver()), article_name
 
   def test_validate_components_styles(self):
-    """
-    Validates the presence of the following elements:
-      Optional Invitation Welcome text and button,
-      My Submissions Welcome Text, button, info text and manuscript display
-      Modals: View Invites and Create New Submission
-    """
+    """Validates styles for the author card"""
     authors_card, title = self._go_to_authors_card()
     header_link = authors_card._get(authors_card._header_link)
     assert header_link.text == title, (header_link.text, title)

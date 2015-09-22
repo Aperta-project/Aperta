d05a8977be4391f4c2cfc43030f9f8b0564f37e3
Jerry Nummi <jerry@neo.com>
2015-09-22 14:34:02 -0400

    Ember Watson: CP and Route Resource fixes

--- client/app/router.js
+++ client/app/router.js
@@ -11,7 +11,7 @@ Router.map(function() {
   this.route('flow_manager');
   this.route('paper_tracker', function() {});
 
-  this.resource('paper', { path: '/papers/:paper_id' }, function() {
+  this.route('paper', { path: '/papers/:paper_id' }, function() {
     this.route('index', { path: '/' }, function() {
       this.route('discussions', function() {
         this.route('new',  { path: '/new' });

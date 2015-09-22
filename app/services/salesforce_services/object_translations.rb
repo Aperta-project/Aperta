c4e927292d43b679e542c0a549bc3e1cc81903a3
Ryan Wold <pair+ryan.wold@neo.com>
2015-09-21 17:17:11 -0700

    place the DOI in the Manuscript_Number__c instead of DOI
    per Allyndreth at PLoS

--- app/services/salesforce_services/object_translations.rb
+++ app/services/salesforce_services/object_translations.rb
@@ -18,7 +18,7 @@ module SalesforceServices
           "Revision__c" => @paper.decisions.latest.revision_number,
           "Title__c" => @paper.title,
           "OriginalSubmissionDate__c" => @paper.submitted_at,
-          "DOI__c" => @paper.doi
+          "Manuscript_Number__c" => @paper.doi
         }
       end
     end

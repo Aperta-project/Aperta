78920003b55109c6d11b306bdf70e596066a061a
Ryan Wold <pair+ryan.wold@neo.com>
2015-09-22 12:40:31 -0700

    use find to wait for the element
    first doesn’t wait

--- spec/support/pages/overlays/assign_team_overlay.rb
+++ spec/support/pages/overlays/assign_team_overlay.rb
@@ -14,6 +14,8 @@ class AssignTeamOverlay < CardOverlay
   end
 
   def assign_role_for_user(role_name, user)
+    find(".invite-reviewers")
+
     select2 role_name, from: "Role"
     wait_for_ajax
 

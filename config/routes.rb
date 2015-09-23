d37c45645537281df71192761f1e9dc9c9257926
Hildebrando Rueda <harq.soft@gmail.com>
2015-09-23 11:31:06 -0500

    Make full path and not relative path for image proxy

--- config/routes.rb
+++ config/routes.rb
@@ -183,7 +183,7 @@ Tahi::Application.routes.draw do
     get :download, on: :member
   end
 
-  get '/papers/attachments/figures/:figure_id', to: 'image_proxy#show'
+  get '/attachments/figures/:figure_id', to: 'image_proxy#show'
 
   # Fall through to ember app
   #

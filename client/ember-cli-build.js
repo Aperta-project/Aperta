e3d6269cb0d8982826195473f4ba285fa878ba13
Jerry Nummi <jerry@neo.com>
2015-09-23 17:22:56 -0400

    Cleanup ember-cli-build file paths

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -5,9 +5,7 @@ var Funnel     = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // calculated config will be stored in the final tahi.js file
     storeConfigInMeta: false,
-
     emberCliFontAwesome: { includeFontAwesomeAssets: false },
     sourcemaps: {
       enabled: true,
@@ -15,15 +13,12 @@ module.exports = function(defaults) {
     }
   });
 
-  var b = app.bowerDirectory + '/';
-  var v = 'vendor/';
-
-  app.import(b + 'underscore/underscore.js');
-  app.import(b + 'moment/moment.js');
+  app.import(app.bowerDirectory + '/underscore/underscore.js');
+  app.import(app.bowerDirectory + '/moment/moment.js');
 
   // Pusher
-  app.import(b + 'pusher/dist/pusher.js');
-  app.import(b + 'ember-pusher/ember-pusher.amd.js', {
+  app.import(app.bowerDirectory + '/pusher/dist/pusher.js');
+  app.import(app.bowerDirectory + '/ember-pusher/ember-pusher.amd.js', {
     exports: {
       'ember-pusher/controller':    ['Controller'],
       'ember-pusher/bindings':      ['Bindings'],
@@ -32,40 +27,40 @@ module.exports = function(defaults) {
   });
 
   // jQuery UI
-  app.import(b + 'jquery-ui/ui/core.js');
-  app.import(b + 'jquery-ui/ui/widget.js');
-  app.import(b + 'jquery-ui/ui/mouse.js');
-  app.import(b + 'jquery-ui/ui/sortable.js');
+  app.import(app.bowerDirectory + '/jquery-ui/ui/core.js');
+  app.import(app.bowerDirectory + '/jquery-ui/ui/widget.js');
+  app.import(app.bowerDirectory + '/jquery-ui/ui/mouse.js');
+  app.import(app.bowerDirectory + '/jquery-ui/ui/sortable.js');
 
   // FileUpload
   // (has jquery.ui.widget.js dependency, imported above with jQuery UI)
-  app.import(v + 'jquery.iframe-transport.js');
-  app.import(v + 'jquery.fileupload/jquery.fileupload.css');
-  app.import(v + 'jquery.fileupload/jquery.fileupload.js');
+  app.import('vendor/jquery.iframe-transport.js');
+  app.import('vendor/jquery.fileupload/jquery.fileupload.css');
+  app.import('vendor/jquery.fileupload/jquery.fileupload.js');
 
   // Select 2
-  app.import(b + 'select2/select2.js');
-  app.import(b + 'select2/select2.css');
-  var select2Assets = new Funnel(b + 'select2', {
+  app.import(app.bowerDirectory + '/select2/select2.js');
+  app.import(app.bowerDirectory + '/select2/select2.css');
+  var select2Assets = new Funnel(app.bowerDirectory + '/select2', {
     srcDir: '/',
     files: ['*.gif', '*.png'],
     destDir: '/assets'
   });
 
   // JsDiff
-  app.import('bower_components/jsdiff/diff.js');
+  app.import(app.bowerDirectory + '/jsdiff/diff.js');
 
   // Bootstrap
-  app.import(b + 'bootstrap/js/collapse.js');
-  app.import(b + 'bootstrap/js/dropdown.js');
-  app.import(b + 'bootstrap/js/tooltip.js');
-  app.import(b + 'bootstrap-datepicker/css/datepicker3.css');
-  app.import(b + 'bootstrap-datepicker/js/bootstrap-datepicker.js');
+  app.import(app.bowerDirectory + '/bootstrap/js/collapse.js');
+  app.import(app.bowerDirectory + '/bootstrap/js/dropdown.js');
+  app.import(app.bowerDirectory + '/bootstrap/js/tooltip.js');
+  app.import(app.bowerDirectory + '/bootstrap-datepicker/css/datepicker3.css');
+  app.import(app.bowerDirectory + '/bootstrap-datepicker/js/bootstrap-datepicker.js');
 
   if (app.env !== 'production') {
-    app.import(b + 'sinon/index.js', { type: 'test' });
-    app.import(b + 'ember/ember-template-compiler.js', { type: 'test' });
-    app.import(v + 'pusher-test-stub.js', { type: 'test' });
+    app.import(app.bowerDirectory + '/sinon/index.js', { type: 'test' });
+    app.import(app.bowerDirectory + '/ember/ember-template-compiler.js', { type: 'test' });
+    app.import('vendor/pusher-test-stub.js', { type: 'test' });
   }
 
   return mergeTrees([app.toTree(), select2Assets], {overwrite: true});

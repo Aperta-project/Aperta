fee936afbed9bd66022a72bd592af2c6b1914b94
Zach Dennis <zach.dennis@gmail.com>
2015-09-24 13:51:28 -0400

    Updating nested-question component to not save an answer when it is a newRecord. In this case it's up to the calling code to be responsible for saving.
    This prevents an issue where an answer tries to save prematurely. E.g. we're building a new parent object that hasn't been persisted to the server yet.

--- client/app/pods/components/nested-question/component.js
+++ client/app/pods/components/nested-question/component.js
@@ -42,7 +42,11 @@ NestedQuestionComponent = Ember.Component.extend({
   },
 
   _saveAnswer: function(answer){
-    answer.save();
+    if(answer.get("owner.isNew")){
+      // no-op
+    } else {
+      answer.save();
+    }
   }
 });
 

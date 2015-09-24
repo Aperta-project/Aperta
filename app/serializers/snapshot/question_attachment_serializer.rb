3b7a51825b427e6baea86e6254e07cf00ad5d9e0
Matt Mercieca <mercieca@gmail.com>
2015-09-24 17:06:05 -0400

    Starting nested question snapshot serializers

---
+++ app/serializers/snapshot/question_attachment_serializer.rb
@@ -0,0 +1,22 @@
+module Snapshot
+  class QuestionAttachmentSerializer < BaseSerializer
+
+    def initialize(question_attachment)
+      @question_attachment = question_attachment
+    end
+
+    def snapshot
+      if @question_attachment.nil?
+        return nil
+      end
+
+      { file: @question_attachment.file,
+        title: @question_attachment.title,
+        caption: @question_attachment.caption,
+        kind: @question_attachment.kind,
+        created_at: @question_attachment.created_at,
+        updated_at: @question_attachment.updated_at
+      }
+    end
+  end
+end

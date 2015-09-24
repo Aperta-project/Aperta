04f6eb99a765730d989f0a1a7188b524ad5e5470
Zach Dennis <zach.dennis@gmail.com>
2015-09-24 15:24:29 -0400

    squash me nested-question-answer

--- client/app/helpers/nested-question-answer.js
+++ client/app/helpers/nested-question-answer.js
@@ -4,7 +4,7 @@ export default Ember.Helper.helper(function(params, hash) {
   Ember.assert("Must provide task", hash.task);
   Ember.assert("Must provide ident", hash.ident);
 
-  let answer = hash.task.answerForQuestionQuestion(hash.ident);
+  let answer = hash.task.answerForQuestion(hash.ident);
   if(answer){
     return answer.get("value");
   } else {

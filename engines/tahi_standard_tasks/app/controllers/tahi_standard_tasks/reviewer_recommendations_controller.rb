8152c2f22d0363298dca0f75248c27983dea370a
Zach Dennis <zach.dennis@gmail.com>
2015-09-24 15:49:54 -0400

    Updating Reviewer Recommendation/Candidate(s) card to save only when the user clicks done.
    * Similar approach to what we did in the Authors card

--- engines/tahi_standard_tasks/app/controllers/tahi_standard_tasks/reviewer_recommendations_controller.rb
+++ engines/tahi_standard_tasks/app/controllers/tahi_standard_tasks/reviewer_recommendations_controller.rb
@@ -1,28 +1,19 @@
 module TahiStandardTasks
   class ReviewerRecommendationsController < ::ApplicationController
     before_action :authenticate_user!
+    before_action :enforce_policy
     respond_to :json
 
     def create
-      task = Task.find(reviewer_recommendation_params[:reviewer_recommendations_task_id])
-      reviewer_recommendation = task.reviewer_recommendations.new(reviewer_recommendation_params)
       reviewer_recommendation.save
       render json: reviewer_recommendation, status: :created
     end
 
-    def update
-      reviewer_recommendation = ReviewerRecommendation.find(params[:id])
-      reviewer_recommendation.update_attributes(reviewer_recommendation_params)
-      render json: reviewer_recommendation
-    end
-
-    def destroy
-      reviewer_recommendation = ReviewerRecommendation.find(params[:id])
-      reviewer_recommendation.destroy
-      render json: reviewer_recommendation
-    end
-
     private
+
+    def reviewer_recommendation
+      @reviewer_recommendation ||= ReviewerRecommendation.new(reviewer_recommendation_params)
+    end
 
     def reviewer_recommendation_params
       params.require(:reviewer_recommendation).permit(
@@ -37,6 +28,10 @@ module TahiStandardTasks
         :ringgold_id,
         :recommend_or_oppose,
         :reason)
+    end
+
+    def enforce_policy
+      authorize_action!(reviewer_recommendation: reviewer_recommendation)
     end
   end
 end

de7a3d84ea0b43e21f3002d1ae4a691dc62c9688
Sam Bleckley <sam@mutuallyhuman.com>
2015-09-24 10:35:01 -0400

    APERTA-3272 APERTA-3204 APERTA-3270 Add policies to tasks_controller; move task controller relations.

--- app/policies/papers_policy.rb
+++ app/policies/papers_policy.rb
@@ -2,7 +2,7 @@ class PapersPolicy < ApplicationPolicy
   primary_resource :paper
 
   def show?
-    can_view_paper?
+    can_view_paper? paper
   end
 
   def create?
@@ -10,15 +10,15 @@ class PapersPolicy < ApplicationPolicy
   end
 
   def update?
-    can_view_paper?
+    can_view_paper? paper
   end
 
   def upload?
-    can_view_paper?
+    can_view_paper? paper
   end
 
   def edit?
-    can_view_paper?
+    can_view_paper? paper
   end
 
   def manage?
@@ -26,7 +26,7 @@ class PapersPolicy < ApplicationPolicy
   end
 
   def download?
-    can_view_paper?
+    can_view_paper? paper
   end
 
   def heartbeat?
@@ -38,11 +38,11 @@ class PapersPolicy < ApplicationPolicy
   end
 
   def submit?
-    can_view_paper?
+    can_view_paper? paper
   end
 
   def withdraw?
-    can_view_paper?
+    can_view_paper? paper
   end
 
   def reactivate?
@@ -54,32 +54,10 @@ class PapersPolicy < ApplicationPolicy
   end
 
   def manuscript_activities?
-    can_view_paper?
+    can_view_paper? paper
   end
 
   def versioned_texts?
-    can_view_paper?
-  end
-
-  private
-
-  def can_view_paper?
-    current_user.site_admin? || paper.assigned_users.where(id: current_user.id).exists? || can_view_manuscript_manager?
-  end
-
-  PaperRole::ALL_ROLES.each do |role|
-    define_method "paper_#{role}?" do
-      paper.role_for(role: role, user: current_user).exists?
-    end
-  end
-
-  def can_view_manuscript_manager?
-    current_user.site_admin? || current_user.roles.where(journal_id: paper.journal).
-      where("can_view_assigned_manuscript_managers = ? OR can_view_all_manuscript_managers = ?", true, true).
-      exists?
-  end
-
-  def author?
-    author_of_paper? paper
+    can_view_paper? paper
   end
 end

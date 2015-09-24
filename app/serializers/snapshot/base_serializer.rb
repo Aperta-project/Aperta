31182dfdd4a7d2dd7e74d3ead756a830182505e7
Matt Mercieca <mercieca@gmail.com>
2015-09-24 17:06:05 -0400

    Starting nested question snapshot serializers

---
+++ app/serializers/snapshot/base_serializer.rb
@@ -0,0 +1,15 @@
+module Snapshot
+  class BaseSerializer
+    def snapshot_property name, type, value
+      { :name => name, :type => type, :value => value }
+    end
+
+    def snapshot_children plural_name, singular_name, value
+      children = []
+      value.each do |child|
+        children.push snapshot_property(singular_name, "text", child)
+      end
+      { :name => plural_name, :type => "properties", :children => children }
+    end
+  end
+end
---
+++ app/serializers/snapshot/reviewer_recommendation_task_serializer.rb
@@ -0,0 +1,19 @@
+module Snapshot
+  class ReviewerRecommendationTaskSerializer < BaseSerializer
+
+    def initialize(task)
+      @task = task
+    end
+
+    def snapshot
+      recommendations = []
+
+      @task.reviewer_recommendations.each do |recommendation|
+        puts recommendation
+        serializer = Snapshot::ReviewerRecommendationSerializer.new recommendation
+        recommendations << serializer.snapshot
+      end
+      recommendations
+    end
+  end
+end

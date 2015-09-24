574434d6777b861031a7dc459e644256bef3b012
Zach Dennis <zach.dennis@gmail.com>
2015-09-24 09:21:07 -0400

    On the ember-side only support a one-sided relationship between a NestedQuestionOwner and a NestedQuestion.
    This is to get around the case where we need additional join model representations on the client and the server.
    
    A NestedQuestionOwner will know about the NestedQuestion(s) it can answer, but not vise versa, since a question may apply to many different owners (and we don't need to explicitly model this since we'll not looking for owners through questions on the client side).
    
    * remove client-side NestedQuestion#owners relationship.

--- spec/factories/nested_question_answers.rb
+++ spec/factories/nested_question_answers.rb
@@ -3,11 +3,5 @@ FactoryGirl.define do
     association :nested_question
     sequence(:value){ |n| "value #{n}" }
     value_type "text"
-
-    after(:build) do |nested_question_answer, evaluator|
-      unless evaluator.nested_question.owner
-        nested_question_answer.owner = evaluator.nested_question.owner
-      end
-    end
   end
 end

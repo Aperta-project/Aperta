89cdf2233ec5be01cac734e9e1f336276eb7d089
Zach Dennis <zach.dennis@gmail.com>
2015-09-25 09:48:37 -0400

    Converting ReviewerReportTask to use the nested-question data model and components.
    [Pivotal Card #101433534]

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/reviewer_report_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/reviewer_report_task.rb
@@ -4,6 +4,112 @@ module TahiStandardTasks
 
     before_create :assign_to_latest_decision
 
+    def self.nested_questions
+      questions = []
+
+      # questions??
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "competing_interests",
+        value_type: "text",
+        text: "Do you have any potential or perceived competing interests that may influence your review?"
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "support_conclusions",
+        value_type: "boolean",
+        text: "Is the manuscript technically sound, and do the data support the conclusions?",
+        children: [
+          NestedQuestion.new(
+            owner_id:nil,
+            owner_type: name,
+            ident: "explanation",
+            value_type: "text",
+            text: "Explanation"
+          )
+        ]
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "statistical_analysis",
+        value_type: "boolean",
+        text: "Has the statistical analysis been performed appropriately and rigorously?",
+        children: [
+          NestedQuestion.new(
+            owner_id:nil,
+            owner_type: name,
+            ident: "explanation",
+            value_type: "text",
+            text: "Statistical Analysis Explanation"
+          )
+        ]
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "standards",
+        value_type: "boolean",
+        text: "Does the manuscript adhere to standards in this field for data availability?",
+        children: [
+          NestedQuestion.new(
+            owner_id:nil,
+            owner_type: name,
+            ident: "explanation",
+            value_type: "text",
+            text: "Standards Explanation"
+          )
+        ]
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "intelligible",
+        value_type: "boolean",
+        text: "Is the manuscript presented in an intelligible fashion and written in standard English?",
+        children: [
+          NestedQuestion.new(
+            owner_id:nil,
+            owner_type: name,
+            ident: "explanation",
+            value_type: "text",
+            text: "Intelligible Explanation"
+          )
+        ]
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "additional_comments",
+        value_type: "text",
+        text: "(Optional) Please offer any additional comments to the author."
+      )
+
+      questions << NestedQuestion.new(
+        owner_id:nil,
+        owner_type: name,
+        ident: "identity",
+        value_type: "text",
+        text: "(Optional) If you'd like your identity to be revealed to the authors, please include your name here."
+      )
+
+      questions.each do |q|
+        unless NestedQuestion.where(owner_id:nil, owner_type:name, ident:q.ident).exists?
+          q.save!
+        end
+      end
+
+      NestedQuestion.where(owner_id:nil, owner_type:name).all
+    end
+
     def body
       # body is a json column by default which returns an Array. We don't want
       # an array, we want to store properties. So if we get a blank

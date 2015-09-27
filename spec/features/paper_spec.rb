c55ce65f0ff94988b8a01c2ea99f2b00ad4ec7b4
Benjamin Moses Lieb <contact@pixelearth.net>
2015-09-27 14:17:34 -0700

    feature spec for validations - currently failing

--- spec/features/paper_spec.rb
+++ spec/features/paper_spec.rb
@@ -31,4 +31,37 @@ feature "Editing paper", js: true do
       expect(edit_paper.has_body_text?("Contrary to popular belief")).to be(true)
     end
   end
+
+  context "with a billing task" do
+
+    before do
+      @user  = FactoryGirl.create :user, :site_admin
+      @paper = FactoryGirl.create :paper_with_task, creator: @user, task_params: { title: "Billing", type: "PlosBilling::BillingTask", role: "author" }
+      login_as @user
+      visit "/"
+    end
+
+    it "shows validations", selenium: true do
+      click_link(@paper.title)
+      find('.workflow-link').click
+      click_link('Billing')
+
+
+      find(".affiliation-field b[role='presentation']").click #slect PFA from dropdown
+      find("li.select2-result div", :text => /PLOS Publication Fee Assistance Program \(PFA\)/).click #slect PFA from dropdown
+
+      within(".question-dataset") do 
+        find("input[id='plos_billing.pfa_question_1-yes']").click  #doens't work: find("#plos_billing.pfa_question_1-yes").click
+        find("input[name='plos_billing.pfa_question_1b']").set "foo"
+
+        binding.pry
+
+      end
+
+      #fill in crap
+      #
+      #check for errors
+      #check for disabled
+    end
+  end
 end

237e69b4249087ce241b212e4e33e66127223cfa
Matt Mercieca <mercieca@gmail.com>
2015-09-28 11:18:06 -0400

    Adding properties to snapshot of ReviewerRecommendations task. Making the name of Snapshot::ReviewerRecommendationsTaskSerializer match the task name

--- app/serializers/snapshot/reviewer_recommendation_serializer.rb
+++ app/serializers/snapshot/reviewer_recommendation_serializer.rb
@@ -6,6 +6,24 @@ module Snapshot
     end
 
     def snapshot
+      recommendation = []
+      recommendation << snapshot_properties
+      recommendation << snapshot_nested_questions
+    end
+
+    def snapshot_properties
+      properties = []
+      properties << snapshot_property("first_name", "text", @reviewer_recommendation.first_name)
+      properties << snapshot_property("last_name", "text", @reviewer_recommendation.last_name)
+      properties << snapshot_property("middle_initial", "text", @reviewer_recommendation.middle_initial)
+      properties << snapshot_property("email", "text", @reviewer_recommendation.email)
+      properties << snapshot_property("department", "text", @reviewer_recommendation.department)
+      properties << snapshot_property("title", "text", @reviewer_recommendation.title)
+      properties << snapshot_property("affiliation", "text", @reviewer_recommendation.affiliation)
+      properties << snapshot_property("ringgold_id", "text", @reviewer_recommendation.ringgold_id)
+    end
+
+    def snapshot_nested_questions
       recommendation_snapshot = []
       nested_questions = TahiStandardTasks::ReviewerRecommendation.nested_questions.where(parent_id: nil).order('id')
 

df58b31a2e32f262607b5c84aee662a520b29b53
Benjamin Moses Lieb <contact@pixelearth.net>
2015-09-28 12:36:51 -0700

    converted validation object static properties to computed properties to avoid race condition issues

--- engines/plos_billing/client/app/controllers/overlays/billing.js
+++ engines/plos_billing/client/app/controllers/overlays/billing.js
@@ -334,7 +334,7 @@ export default TaskController.extend({
   buildPfaValidator: function(){
     var numericalityConfig = { numericality: { allowBlank: true, messages: { numericality: "Must be a number and contain no symobls, or letters"}}}
 
-    var x = Ember.Object.extend(EmberValidations.Mixin, {
+    var pfaDataClass = Ember.Object.extend(EmberValidations.Mixin, {
       init: function(){
         this.set('validations', { });
 
@@ -344,9 +344,10 @@ export default TaskController.extend({
          'pfa_question_4a',
          'pfa_amount_to_pay'].forEach(function(ident)
           {
-            console.log(ident); 
-            console.log(this.findPfaQuestion(ident)); 
-            this.set(ident, this.findPfaQuestion(ident)); //add named prop to obj
+            this.set(ident, Ember.computed("model.questions.@each", function() {
+                return this.findPfaQuestion(ident);
+              }.bind(this))
+            ); //add named prop to obj
             this.validations[ident + ".answer"] = numericalityConfig; //add prop name to validations
           }.bind(this)
         );
@@ -361,7 +362,7 @@ export default TaskController.extend({
       },
     })
 
-    this.set('pfaData', x.create());
+    this.set('pfaData', pfaDataClass.create());
   },
 
   /*

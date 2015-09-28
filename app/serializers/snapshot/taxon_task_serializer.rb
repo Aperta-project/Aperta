2114ca81caf34f1c89defe18cb440a214d72affa
Matt Mercieca <mercieca@gmail.com>
2015-09-28 14:53:14 -0400

    Moving Taxon task snapshotter to use the base task snapshot serializer (that's all it needs)

--- app/serializers/snapshot/taxon_task_serializer.rb
+++ app/serializers/snapshot/taxon_task_serializer.rb
@@ -1,18 +1,4 @@
 module Snapshot
-  class TaxonTaskSerializer < BaseSerializer
-
-    def initialize(task)
-      @task = task
-    end
-
-    def snapshot
-      taxon_task = []
-      nested_questions = TahiStandardTasks::TaxonTask.nested_questions
-      nested_questions.each do |question|
-        serializer = Snapshot::NestedQuestionSerializer.new(question, @task)
-        taxon_task << serializer.snapshot
-      end
-      taxon_task
-    end
+  class TaxonTaskSerializer < BaseTaskSerializer
   end
 end

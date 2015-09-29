24f5b3e454b6e468bc243ebb5c9d8f2839fcf404
Hildebrando Rueda <harq.soft@gmail.com>
2015-09-29 16:40:56 -0500

    Add new reviewer-row component to standard tasks

---
+++ engines/tahi_standard_tasks/client/app/components/reviewer-row.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['authors-overlay-item'],
+  classNameBindings: ['isHovered:__hover', 'isEditable:__editable'],
+  isHovered: false,
+  isDeleting: false,
+  isEditing: false,
+
+  didInsertElement() {
+    if (this.get('disabled')) { return; }
+    this.$().hover(() => {
+      this.toggleProperty('isHovered');
+    });
+  },
+
+  willDestroyElement() {
+    this.$().off('mouseenter mouseleave');
+  },
+
+  actions: {
+
+    edit() {
+      this.set('isEditing', true);
+    },
+
+    delete() {
+      this.set('isDeleting', true);
+    },
+
+    cancelDeletion() {
+      this.set('isDeleting', false);
+    },
+
+    confirmDeletion() {
+      this.$().fadeOut(250, ()=> {
+        this.sendAction('delete', this.get('reviewer'));
+      });
+    }
+  }
+});

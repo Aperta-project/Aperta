470dbac96e6ac78663e2138242c6c8c69462f235
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2015-09-29 14:02:03 -0400

    Re-remove the `plos_authors` engine
    No idea why this is necessary. These files shouldn't exist anymore.
    
    *spooky ghost noise*

--- engines/plos_authors/config/initializers/subscriptions.rb
+++
@@ -1,6 +0,0 @@
-Subscriptions.configure do
-  add 'author:updated', PlosAuthors::PlosAuthor::Updated::NotifyPlosAuthorChange
-  add 'plos_authors/plos_author:created', PlosAuthors::PlosAuthor::Created::EventStream
-  add 'plos_authors/plos_author:updated', PlosAuthors::PlosAuthor::Updated::EventStream, PlosAuthors::PlosAuthor::Updated::NotifyAuthorChange
-  add 'plos_authors/plos_author:destroyed', PlosAuthors::PlosAuthor::Destroyed::EventStream
-end

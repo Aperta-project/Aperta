c992bcd8e9f3bfc4b49af851ca8a10b75b2cc779
Zach Dennis <zach.dennis@gmail.com>
2015-09-30 11:24:56 -0400

    Make sure when a paper is resubmitted all of the ReviewerReportTasks are marked as incomplete, not just the first.
    Thanks to @surfacedamage for catching this.
    
    [Pivotal Card #104136262]

--- engines/tahi_standard_tasks/spec/subscribers/paper/resubmitted/reopen_revision_tasks_spec.rb
+++ engines/tahi_standard_tasks/spec/subscribers/paper/resubmitted/reopen_revision_tasks_spec.rb
@@ -7,6 +7,8 @@ describe Paper::Resubmitted::ReopenRevisionTasks do
   let(:paper) { FactoryGirl.create(:paper) }
   let(:paper_reviewer_task) { FactoryGirl.create(:paper_reviewer_task, paper: paper, completed: true) }
   let(:register_decision_task) { FactoryGirl.create(:register_decision_task, paper: paper, completed: true) }
+  let(:reviewer_report_task_1) { FactoryGirl.create(:reviewer_report_task, paper: paper, completed: true) }
+  let(:reviewer_report_task_2) { FactoryGirl.create(:reviewer_report_task, paper: paper, completed: true) }
 
   it "marks all revision tasks as incomplete" do
     expect(paper_reviewer_task).to be_completed
@@ -14,6 +16,14 @@ describe Paper::Resubmitted::ReopenRevisionTasks do
     described_class.call("tahi:paper:resubmitted", { paper: paper })
     expect(paper_reviewer_task.reload).to_not be_completed
     expect(register_decision_task.reload).to_not be_completed
+  end
+
+  it "marks all ReviewerReportTask(s) for the paper as incomplete" do
+    expect(reviewer_report_task_1).to be_completed
+    expect(reviewer_report_task_2).to be_completed
+    described_class.call("tahi:paper:resubmitted", { paper: paper })
+    expect(reviewer_report_task_1.reload).to_not be_completed
+    expect(reviewer_report_task_2.reload).to_not be_completed
   end
 
 end

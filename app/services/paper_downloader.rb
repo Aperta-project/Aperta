4c3ed0fd036fc02dabdcc2b4d95cfc1977dd3c3c
Hildebrando Rueda <harq.soft@gmail.com>
2015-09-30 17:07:33 -0500

    Add new paper downloader service class

---
+++ app/services/paper_downloader.rb
@@ -0,0 +1,37 @@
+class PaperDownloader
+
+  attr_reader :paper
+
+  def initialize(paper)
+    @paper = paper
+  end
+
+  def body
+    if paper.body
+      "#{paper_body_with_figures}#{download_supporting_information}"
+    end
+  end
+
+  private
+
+  def paper_body_with_figures
+    paper.body.gsub(/\/attachments\/figures\/(\d+)\?version=detail/) do |match|
+      Figure.find($1).attachment.detail.url
+    end
+  end
+
+  def download_supporting_information
+    return if paper.supporting_information_files.empty?
+
+    html_result = "<h2>Supporting Information</h2>"
+
+    paper.supporting_information_files.each do |file|
+      if file.preview_src
+        html_result.concat "<p>#{file.download_link file.preview_image}</p>"
+      end
+      html_result.concat "<p>#{file.download_link}</p>"
+    end
+
+    html_result
+  end
+end

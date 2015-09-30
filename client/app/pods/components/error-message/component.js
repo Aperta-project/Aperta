fbc79205f8d71640900af16afd38aa468fc98eaf
Anna Petry <hello@annapetry.com>
2015-09-30 10:09:48 -0700

    add validations to required inputs

--- client/app/pods/components/error-message/component.js
+++ client/app/pods/components/error-message/component.js
@@ -2,9 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['error-message'],
-  classNameBindings: ['visible'],
+  classNameBindings: ['visible', 'addPassedClass'],
 
   visible: Ember.computed('message', function() {
     return this.get('message') ? '' : 'error-message--hidden';
+  }),
+
+  addPassedClass: Ember.computed(function() {
+    return this.get('class') ? this.get('class') : '';
   })
 });
--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/production_metadata_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/production_metadata_task.rb
@@ -3,9 +3,35 @@ module TahiStandardTasks
 
     register_task default_title: 'Production Metadata', default_role: 'admin'
 
+    validate :publication_date, :volume_number, :issue_number,  if: :task_completed?
+
     def active_model_serializer
       ProductionMetadataTaskSerializer
     end
 
+    def publication_date
+      question = a_question('publicationDate')
+      errors.add(:publicationDate, "Can't be blank") unless question && question.answer
+    end
+
+    def volume_number
+      question = a_question('volumeNumber')
+      errors.add(:volumeNumber, "Invalid Volume Number") unless question && question.answer.to_i > 0
+    end
+
+    def issue_number
+      question = a_question('issueNumber')
+      errors.add(:issueNumber, "Invalid Issue Number") unless question && question.answer.to_i > 0
+    end
+
+    private
+
+    def task_completed?
+      self.completed?
+    end
+
+    def a_question(type)
+      questions.detect{ |q| q.ident == "production_metadata.#{type}" }
+    end
   end
 end

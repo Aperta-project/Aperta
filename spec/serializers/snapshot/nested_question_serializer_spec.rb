40db28aec4846fce3ea9110997e6f8dd6a37cb93
Matt Mercieca <mercieca@gmail.com>
2015-09-30 14:41:57 -0400

    Adding spec for attachment serializer

--- spec/serializers/snapshot/nested_question_serializer_spec.rb
+++ spec/serializers/snapshot/nested_question_serializer_spec.rb
@@ -62,15 +62,14 @@ describe Snapshot::BaseTaskSerializer do
 
 
       expect(snapshot[:questions][0][:answers].count).to eq(1)
-      expect(snapshot[:questions][0][:answers][0][:value]).to eq("Answer value")
+      expect(snapshot[:questions][0][:answers][0][:value]).to eq("Answer Value")
       expect(snapshot[:questions][1][:answers].count).to eq(0)
       expect(snapshot[:questions][2][:answers].count).to eq(0)
     end
 
-
     it "doesn't matter what order questions are answered in" do
       nested_question_answers = []
-      nested_question_answer = make_nested_question_answer(task.nested_questions[1].id, "Last Value")
+      nested_question_answer = make_nested_question_answer(task.nested_questions[2].id, "Last Value")
       nested_question_answers << nested_question_answer
       nested_question_answer = make_nested_question_answer(task.nested_questions[0].id, "First Value")
       nested_question_answers << nested_question_answer
@@ -84,7 +83,28 @@ describe Snapshot::BaseTaskSerializer do
       expect(snapshot[:questions][0][:answers][0][:value]).to eq("First Value")
       expect(snapshot[:questions][1][:answers].count).to eq(0)
       expect(snapshot[:questions][2][:answers].count).to eq(1)
-      expect(snapshot[:questions][2][:answers][0][:value]).to eq("Second Value")
+      expect(snapshot[:questions][2][:answers][0][:value]).to eq("Last Value")
+    end
+
+    it "serializes attachments" do
+      nested_question_answers = []
+      nested_question_answer = FactoryGirl.create(:nested_question_answer)
+      attachment = FactoryGirl.create(:question_attachment, :with_fake_attachment)
+      attachment.question_id = nested_question_answer.id
+      attachment.save!
+      nested_question_answer.owner_id = task.id
+      nested_question_answer.owner_type = "task"
+      nested_question_answer.value = "text that is different"
+      nested_question_answer.nested_question_id = task.nested_questions.first.id
+      nested_question_answers << nested_question_answer
+      allow_any_instance_of(Task).to receive(:nested_question_answers).and_return(nested_question_answers)
+
+
+      snapshot = Snapshot::BaseTaskSerializer.new(task).snapshot
+
+
+      expect(snapshot[:questions][0][:answers][0][:attachment][:file]).to eq(attachment[:attachment])
+      expect(snapshot[:questions][0][:answers][0][:value]).to eq("text that is different")
     end
   end
 end

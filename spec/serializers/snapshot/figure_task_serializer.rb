e2f1a0c8cc5fc15f53605e20c647be91bd2ef957
Matt Mercieca <mercieca@gmail.com>
2015-10-01 15:51:56 -0400

    Spec for figure task snapshotter

---
+++ spec/serializers/snapshot/figure_task_serializer.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe Snapshot::FigureTaskSerializer do
+  let(:task) { FactoryGirl.create(:figure_task) }
+  let(:figure) { FactoryGirl.create(:figure) }
+  let(:paper) { FactoryGirl.create(:paper) }
+  let(:attachment) { FactoryGirl.create(:attachment, :with_task)}
+
+  before do
+    figure.title = "figure title"
+    figure.caption = "figure caption"
+    paper.figures << figure
+    task.paper = paper
+  end
+
+  it "snapshots a figure task" do
+    snapshot = Snapshot::FigureTaskSerializer.new(task).snapshot
+    figure = snapshot[:properties][0]
+
+    expect(figure[:title]).to eq(attachment.title)
+    expect(figure[:caption]).to eq(attachment.caption)
+  end
+end

5f3187cbbf7747eff0454c9a4c5d0137ab72733f
Zach Dennis <zach.dennis@gmail.com>
2015-10-05 12:46:44 -0400

    Removing question model on client-side and server-side. It's no longer used and has been replaced by the NestedQuestion and NestedQuestionAnswer data model.
    * remove references from serializers
    
    [Pivotal Card #101433534]

--- app/serializers/question_serializer.rb
+++
@@ -1,5 +0,0 @@
-class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :ident, :question, :answer, :additional_data, :created_at, :updated_at
-  has_one :question_attachment, embed: :id, include: true
-  has_one :task, embed: :id, polymorphic: true
-end

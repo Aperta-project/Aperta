840f96243101f809a31fb12eb8a8b5e8c83fbb15
Matt Mercieca <mercieca@gmail.com>
2015-10-05 11:27:36 -0400

    Only resizing images that are uploading.
    -TIFF/EPS files are transcoded
    -Images are resized
    -Non-images are left alone

--- spec/uploaders/attachment_uploader_spec.rb
+++ spec/uploaders/attachment_uploader_spec.rb
@@ -9,4 +9,11 @@ describe AttachmentUploader do
       expect(uploader.store_dir).to eq "uploads/paper/#{paper.id}/figure/attachment/#{figure.id}"
     end
   end
+
+  describe "#needs_transcoding?" do
+    paper = FactoryGirl.create(:paper)
+    figure = paper.figures.create!
+    uploader = AttachmentUploader.new(figure, :attachment)
+    expect(uploader.needs_transcoding?).to eq(false)
+  end
 end

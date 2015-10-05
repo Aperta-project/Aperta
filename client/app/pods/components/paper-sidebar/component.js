cd706638b8d35b2e0f3daae9aa5c9ae713e85bba
Hildebrando Rueda <harq.soft@gmail.com>
2015-10-05 17:41:15 -0500

    Move computed properties from paper model to paper-sidebar component

--- client/app/pods/components/paper-sidebar/component.js
+++ client/app/pods/components/paper-sidebar/component.js
@@ -9,6 +9,31 @@ export default Ember.Component.extend({
   metadataTasks: Ember.computed.filterBy('paper.tasks', 'isMetadataTask', true),
   assignedTasks: Ember.computed.setDiff('currentUserTasks', 'metadataTasks'),
 
+  allSubmissionTasksCompleted: Ember.computed('allSubmissionTasks.@each.completed', function() {
+    return this.get('allSubmissionTasks').everyProperty('completed', true);
+  }),
+
+  submittableState: Ember.computed('publishingState', function() {
+    let state = this.get('publishingState');
+    return state === 'unsubmitted' || state === 'in_revision';
+  }),
+
+  preSubmission: Ember.computed('submittableState', 'allSubmissionTasksCompleted', function() {
+    return (this.get('submittableState') &&
+            !this.get('allSubmissionTasksCompleted'));
+  }),
+
+  allSubmissionTasks: Ember.computed('tasks.content.@each.isSubmissionTask', function() {
+    return this.get('tasks').filterBy('isSubmissionTask');
+  }),
+
+  readyToSubmit: Ember.computed('submittableState', 'allSubmissionTasksCompleted', function() {
+    return (this.get('submittableState') &&
+            this.get('allSubmissionTasksCompleted'));
+  }),
+
+  postSubmission: Ember.computed.not('submittableState'),
+
   currentUserTasks: Ember.computed.filter('paper.tasks', function(task) {
     return task.get('participations').mapBy('user').contains(this.get('user'));
   }),

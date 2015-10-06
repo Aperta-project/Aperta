f5bcccf21da7c87d8188926ab2ef34b35354dbc0
Zach Dennis <zach.dennis@gmail.com>
2015-10-06 10:54:46 -0400

    Updating :question_attachment_factory to associate :question as a NestedQuestionAnswer.
    [Pivotal Card #101433534]

--- spec/factories/question_attachment_factory.rb
+++ spec/factories/question_attachment_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :question_attachment do
-    question
+    association :question, factory: :nested_question_answer
 
     trait :with_fake_attachment do
       after :build do |attachment|

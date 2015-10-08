81542b584f19619ec5b026185a3b162e645fca04
Zach Dennis <zach.dennis@gmail.com>
2015-10-08 09:09:41 -0400

    Removing 'questionAttachment' association on the client-side QuestionAttachmentOwner.
    [Pivotal Card #101433534]

--- client/app/models/question-attachment-owner.js
+++ client/app/models/question-attachment-owner.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  attachment: DS.belongsTo('question-attachment', { async: false, inverse: 'nestedQuestionAnswer' }),
-  questionAttachment: DS.belongsTo('question-attachment', { async: false, inverse: 'question' })
+  attachment: DS.belongsTo('question-attachment', { async: false, inverse: 'nestedQuestionAnswer' })
 });

1f0482902e593fa793126495b49a690d12745b2c
Matt Mercieca <mercieca@gmail.com>
2015-10-12 16:24:37 -0400

    WIP

--- spec/uploaders/attachment_uploader_spec.rb
+++ spec/uploaders/attachment_uploader_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 describe AttachmentUploader do
+  before do
+    AttachmentUploader.enable_processing = true
+  end
+
   describe "#store_dir" do
     it "includes the paper id in the path" do
       paper = FactoryGirl.create(:paper)
       figure = paper.figures.create!
       uploader = AttachmentUploader.new(figure, :attachment)
+      binding.pry
       expect(uploader.store_dir).to eq "uploads/paper/#{paper.id}/figure/attachment/#{figure.id}"
     end
   end

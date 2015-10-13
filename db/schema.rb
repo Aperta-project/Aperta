9d42bda2d5bd41eb29c19189aab24d49d8df0153
Zach Dennis <zach.dennis@gmail.com>
2015-10-13 10:18:06 -0400

    Removing not null DB constraint on ReviewerRecommendation#recommend_or_oppose.
    This is now answered as a NestedQuestionAnswer and the field will be removed after the data migration to nested questions is complete.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925132328) do
+ActiveRecord::Schema.define(version: 20151013141357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -450,7 +450,7 @@ ActiveRecord::Schema.define(version: 20150925132328) do
     t.string   "department"
     t.string   "title"
     t.string   "affiliation"
-    t.string   "recommend_or_oppose",              null: false
+    t.string   "recommend_or_oppose"
     t.text     "reason"
     t.datetime "created_at"
     t.datetime "updated_at"

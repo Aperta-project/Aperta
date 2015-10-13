42d219b1d3627f8df6fc6e5770e16cad4f0414bc
Sam Bleckley <sam@mutuallyhuman.com>
2015-10-13 14:46:51 -0400

    APERTA-3435 Condenses shared code in LitePaperSerializer.

--- app/serializers/lite_paper_serializer.rb
+++ app/serializers/lite_paper_serializer.rb
@@ -4,27 +4,24 @@ class LitePaperSerializer < ActiveModel::Serializer
   def related_at_date
     return unless scoped_user.present?
 
-    roles = object.paper_roles.select do |role|
-      role.user_id == scoped_user.id
-    end
-
-    roles.map(&:created_at).sort.first
+    my_roles.map(&:created_at).sort.first
   end
 
-  #TODO: this method does not include a tooltip if user is related to paper
-  #      by just being a task participant (see pivotal #82588944)
   def roles
     return unless scoped_user.present?
 
-    # rocking this in memory because eager-loading
-    roles = object.paper_roles.select { |role|
-      role.user_id == scoped_user.id
-    }.map(&:description)
+    roles = my_roles.map(&:description)
     roles << "My Paper" if object.user_id == scoped_user.id
     roles
   end
 
   private
+
+  def my_roles
+    @my_roles_mem ||= object.paper_roles.select do |role|
+      role.user_id == scoped_user.id
+    end
+  end
 
   def scoped_user
     scope.presence || options[:user]

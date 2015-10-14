ddf05b3dd9ded525c8f52da92a1bbc8e17d92a32
Erik Hetzner <ehetzner@plos.org>
2015-10-14 15:04:10 -0700

    APERTA-2751 Call Paper update on all ihat callback
    - Also use standard webmock in rspec.
    - Move verification to controller, because sidekiq & rails do not have a
      shared secret

--- spec/controllers/ihat/jobs_controller_spec.rb
+++ spec/controllers/ihat/jobs_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Ihat::JobsController, :type => :controller do
+describe Ihat::JobsController, type: :controller do
 
   describe "POST create" do
     before do
@@ -10,12 +10,13 @@ describe Ihat::JobsController, :type => :controller do
 
     let(:encrypted_metadata) { Verifier.new(paper_id: "123").encrypt }
     let(:ihat_job_params) { { job: { id: 4, state: ihat_job_state, options: { metadata: encrypted_metadata }, outputs: [{ file_type: "epub", url: "http://amazon.localhost/1234" }] }, format: :json } }
+    let(:worker_params) { { id: 4, state: ihat_job_state, options: { metadata: { paper_id: "123" } }, outputs: [{ file_type: "epub", url: "http://amazon.localhost/1234" }] } }
 
     context "the ihat job status is 'completed'" do
       let(:ihat_job_state) { "completed" }
 
       it "calls the PaperUpdateWorker" do
-        expect(PaperUpdateWorker).to receive(:perform_async).with("123", "http://amazon.localhost/1234")
+        expect(PaperUpdateWorker).to receive(:perform_async).with(worker_params)
         post :create, ihat_job_params
       end
 
@@ -29,27 +30,26 @@ describe Ihat::JobsController, :type => :controller do
     context "the ihat job status is not 'completed'" do
       let(:ihat_job_state) { "errored" }
 
-      it "does not call the PaperUpdateWorker" do
-        expect(PaperUpdateWorker).to_not receive(:perform_async)
+      it "calls the PaperUpdateWorker" do
+        expect(PaperUpdateWorker).to receive(:perform_async).with(worker_params)
         post :create, ihat_job_params
       end
 
       it "returns success" do
         allow(PaperUpdateWorker).to receive(:perform_async)
         post :create, ihat_job_params
-        expect(response.status).to eq(202)
+        expect(response.status).to eq(200)
       end
     end
 
     context "missing required parameters" do
       let(:ihat_job_state) { "completed" }
-      let(:invalid_ihat_job_params) { { random_hash_of_things: [1,2,3] }}
+      let(:invalid_ihat_job_params) { { random_hash_of_things: [1, 2, 3] } }
 
       it "returns 422" do
         post :create, invalid_ihat_job_params
         expect(response.status).to eq(422)
       end
     end
-
   end
 end

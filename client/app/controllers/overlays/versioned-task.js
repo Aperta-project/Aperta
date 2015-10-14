bac40354a2ae8e5bcf03eaaac826b932e9a0dfd6
Zach Dennis <zach.dennis@gmail.com>
2015-10-14 16:01:24 -0400

    This rebases from master and makes an update so we no longer reference the non-existent "rest-less" service (it was renamed to restless but we aren't using it).
    Go to: http://localhost:5000/papers/330/versioned_task

--- client/app/controllers/overlays/versioned-task.js
+++ client/app/controllers/overlays/versioned-task.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
 import TaskController from 'tahi/pods/paper/task/controller';
-import RESTless from 'tahi/services/rest-less';
 
 export default TaskController.extend({
-  
+
 
 });

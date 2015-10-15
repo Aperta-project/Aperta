7f2a4437ab17e5a990986fd19d00fd7641677353
Zach Dennis <zach.dennis@gmail.com>
2015-10-15 10:51:30 -0400

    Resolving an issue with taxon task where the user wasn't getting the "Please read..." notice and "All authors comply..." checkbox.
    * Use block form of nested-question-check so we can determine when to show the aforementioned notice and checkbox
    
    * Move away from supplying questions from the controller and instead rely on passing in the ident and letting the nested-question-check component do the heavy lifting (and matching up question/answer).
    
    APERTA-3495

--- engines/tahi_standard_tasks/client/app/controllers/overlays/taxon.js
+++ engines/tahi_standard_tasks/client/app/controllers/overlays/taxon.js
@@ -3,22 +3,6 @@ import SavesQuestionsOnClose from 'tahi/mixins/saves-questions-on-close';
 
 var TaxonOverlayController;
 
-TaxonOverlayController = TaskController.extend(SavesQuestionsOnClose, {
-  taxonQuestionForZoological: Ember.computed(function(){
-    return this.get('model').findQuestion('taxon_zoological');
-  }),
-
-  compliesQuestionForZoological: Ember.computed(function(){
-    return this.get('model').findQuestion('taxon_zoological.complies');
-  }),
-
-  taxonQuestionForBotanical: Ember.computed(function(){
-    return this.get('model').findQuestion('taxon_botanical');
-  }),
-
-  compliesQuestionForBotanical: Ember.computed(function(){
-    return this.get('model').findQuestion('taxon_botanical.complies');
-  })
-});
+TaxonOverlayController = TaskController.extend(SavesQuestionsOnClose);
 
 export default TaxonOverlayController;

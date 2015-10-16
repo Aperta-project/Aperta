43746b6ae2a322bee4c267b07ceac7c15670bc80
Zach Dennis <zach.dennis@gmail.com>
2015-10-16 09:00:08 -0400

    This resolves an issue in development mode where the reloading of classes would cause the reloading errors.
        ArgumentError: A copy of <Some Class Here> has been removed from the module tree but is still active!
    
    This was because we were keeping a class reference within scope. To resolve this only store the name and re-constantize when the time is necessary to pass along the event notification.

--- lib/subscriptions/subscriber.rb
+++ lib/subscriptions/subscriber.rb
@@ -10,8 +10,14 @@ module Subscriptions
     #
     def self.subscribe(event, subscribers)
       subscribers.flatten.each do |subscriber|
+        # +subscriber_name+ is so we do not reference the subscriber directly.
+        # If we do it breaks auto reloading in development environment by
+        # keeping an old class reference around. Instead, store its name and
+        # constantize at the we need to use it.
+        subscriber_name = subscriber.name
+
         ActiveSupport::Notifications.subscribe(/\A#{APPLICATION_EVENT_NAMESPACE}:#{event}/) do |name, _start, _finish, _id, data|
-          subscriber.call(name, data)
+          subscriber_name.constantize.call(name, data)
         end
       end
     end

43a27d49e67f43853a08030dc9eafc85e4758d4f
Erik Hetzner <ehetzner@plos.org>
2015-10-17 11:42:37 -0700

    APERTA-2751 Use standard key, paper -> record

--- engines/tahi_upload_manuscript/app/subscribers/paper/data_extracted/finish_upload_manuscript_task.rb
+++ engines/tahi_upload_manuscript/app/subscribers/paper/data_extracted/finish_upload_manuscript_task.rb
@@ -1,11 +1,7 @@
 class Paper::DataExtracted::FinishUploadManuscriptTask
-
   def self.call(_event_name, event_data)
-    paper = event_data[:paper]
+    paper = event_data[:record]
 
-    paper.tasks_for_type("TahiUploadManuscript::UploadManuscriptTask").each do |task|
-      task.complete!
-    end
+    paper.tasks_for_type(TahiUploadManuscript::UploadManuscriptTask.to_s).each(&:complete!)
   end
-
 end

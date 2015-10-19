0c3e6c9b94391d5fc5524482df6f03d2406ebf90
Matt Mercieca <mercieca@gmail.com>
2015-10-19 12:01:18 -0400

    APERTA-5419 Updating metadata storage of named types.
    Authors, figures, attachments, reviewer recommendations, and supporting ing information
    was stored in the JSON as 'author: ['.  This broke the generic diffing.
    This commit converts these named types to the properties type, which already handled
    containing multiple values.

--- app/serializers/snapshot/figure_task_serializer.rb
+++ app/serializers/snapshot/figure_task_serializer.rb
@@ -9,11 +9,12 @@ class Snapshot::FigureTaskSerializer < Snapshot::BaseTaskSerializer
   end
 
   def snapshot_figure figure
-    properties = []
     attachment_serializer = Snapshot::AttachmentSerializer.new figure.attachment
-    properties << ["attachment", attachment_serializer.snapshot]
+    properties << {name: "attachment", type: "properties", children: attachment_serializer.snapshot} 
     properties << snapshot_property("title", "text", figure.title)
     properties << snapshot_property("caption", "text", figure.caption)
     properties << snapshot_property("status", "text", figure.status)
+
+    {name: "figure", type: "properties", children: properties}
   end
 end

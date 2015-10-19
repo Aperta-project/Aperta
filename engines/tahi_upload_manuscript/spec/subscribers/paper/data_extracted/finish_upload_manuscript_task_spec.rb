d43d23233ff61bb443441ecf1666060cb29f28b6
Erik Hetzner <ehetzner@plos.org>
2015-10-19 15:51:53 -0700

    APERTA-2751 Use IhatJobResponse record

--- engines/tahi_upload_manuscript/spec/subscribers/paper/data_extracted/finish_upload_manuscript_task_spec.rb
+++ engines/tahi_upload_manuscript/spec/subscribers/paper/data_extracted/finish_upload_manuscript_task_spec.rb
@@ -4,11 +4,11 @@ describe Paper::DataExtracted::FinishUploadManuscriptTask do
   include EventStreamMatchers
 
   let(:upload_task) { FactoryGirl.create(:upload_manuscript_task) }
-  let(:paper) { upload_task.paper }
+  let(:response) { IhatJobResponse.new(state: 'pending', options: { metadata: { paper_id: upload_task.paper.id } }) }
 
   it "marks the upload manuscript task as completed" do
     expect(upload_task).to_not be_completed
-    described_class.call("tahi:paper:data_extracted", { action: "created", record: paper })
+    described_class.call("tahi:paper:data_extracted", record: response)
     expect(upload_task.reload).to be_completed
   end
 end

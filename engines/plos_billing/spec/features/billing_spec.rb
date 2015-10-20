f7259738f5177ce5ae69db2d6c557f472c472115
Jerry Nummi <jerry@neo.com>
2015-10-20 11:19:59 -0400

    Bring back Billing spec in correct location

---
+++ engines/plos_billing/spec/features/billing_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+feature "Billing Task", js: true do
+  let(:user) { FactoryGirl.create :user }
+
+  context "with a billing task" do
+    before do
+      @user  = FactoryGirl.create :user, :site_admin
+      @paper = FactoryGirl.create :paper_with_task, creator: @user, task_params: { title: "Billing", type: "PlosBilling::BillingTask", role: "author" }
+      login_as @user
+      visit "/"
+    end
+
+    it "shows validations" do
+      click_link(@paper.title)
+      find('.workflow-link').click
+      click_link('Billing')
+
+      expect(find("input#task_completed")[:disabled]).to be(nil) # not disabled at start
+
+      find(".affiliation-field b[role='presentation']").click # open payment types dropdown
+      find("div.select2-result-label", :text => /PLOS Publication Fee Assistance Program \(PFA\)/).click #select PFA from dropdown
+
+      expect(find("input#task_completed")[:disabled]).to be(nil)
+      expect(page).not_to have_selector(".overlay-completed-checkbox .error-message") # make sure no error msg
+
+      within(".question-dataset") do
+        find("input[id='plos_billing.pfa_question_1-yes']").click  # doesn't work: find("#plos_billing.pfa_question_1-yes").click
+        find("input[id='plos_billing.pfa_question_2-yes']").click
+        find("input[id='plos_billing.pfa_question_3-yes']").click
+        find("input[id='plos_billing.pfa_question_4-yes']").click
+
+        # numeric fields
+        ['pfa_question_1b', 'pfa_question_2b', 'pfa_question_3a', 'pfa_question_4a', 'pfa_amount_to_pay'].each do |ident|
+          find("input[name='plos_billing.#{ident}']").set "foo"
+          expect(find("#error-for-#{ident}")).to have_content("Must be a number and contain no symbols, or letters")
+        end
+      end
+
+      expect(find("input#task_completed")[:disabled]).to be_truthy # complete is disabled
+      expect(find(".overlay-completed-checkbox .error-message").text).to eq("Errors in form") # shows error
+
+      # change to a different payment system
+      find(".affiliation-field b[role='presentation']").click # open payment types dropdown
+      find("div.select2-result-label", :text => /I will pay the full fee/).click # change back to non-pfa
+      expect(find("input#task_completed")[:disabled]).to be(nil) # not disabled after change
+      expect(page).not_to have_selector(".overlay-completed-checkbox .error-message") # make sure no error msg
+    end
+  end
+end

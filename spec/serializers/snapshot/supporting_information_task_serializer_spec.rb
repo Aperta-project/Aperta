a624669e27fa2988cfd706ba6a2f6f59da417ab1
Matt Mercieca <mercieca@gmail.com>
2015-10-20 14:53:02 -0400

    APERTA-3495 Updating supporting information snapshot to new format

--- spec/serializers/snapshot/supporting_information_task_serializer_spec.rb
+++ spec/serializers/snapshot/supporting_information_task_serializer_spec.rb
@@ -1,21 +1,6 @@
 require "rails_helper"
 
 describe Snapshot::SupportingInformationTaskSerializer do
-  let(:paper) { FactoryGirl.create(:paper, :with_tasks) }
-
-  before :each do
-    files = [double(:file, access_details: :hello)]
-    paper = double(:paper, files: files, to_i: 1)
-    allow(supporting_information_task).to receive(:paper).and_return(paper)
-  end
-
-  let(:supporting_information_task) do
-    ::TahiStandardTasks::SupportingInformationTask.create! completed: true,
-      phase: paper.phases.first,
-      title: "Supporting Info",
-      role: "author"
-  end
-
   it "serializes a supporting information task" do
     task = FactoryGirl.create(:supporting_information_task)
     snapshot = Snapshot::SupportingInformationTaskSerializer.new(task).snapshot
@@ -24,6 +9,13 @@ describe Snapshot::SupportingInformationTaskSerializer do
   end
 
   it "serializes files if they are present" do
+    paper = FactoryGirl.create(:paper, :with_tasks)
+    supporting_information_task = ::TahiStandardTasks::SupportingInformationTask.create!(completed: true,
+      phase: paper.phases.first,
+      title: "Supporting Info",
+      role: "author")
+    allow(supporting_information_task).to receive(:paper).and_return(paper)
+
     file1 = FactoryGirl.create(:supporting_information_file)
     file2 = FactoryGirl.create(:supporting_information_file)
     file1.attachment = "file1.eps"
@@ -36,7 +28,12 @@ describe Snapshot::SupportingInformationTaskSerializer do
     paper.supporting_information_files << file2
 
     snapshot = Snapshot::SupportingInformationTaskSerializer.new(supporting_information_task).snapshot
-    binding.pry
 
+    expect(snapshot[0][:children][0][:children][1][:value]).to eq(file1.title)
+    expect(snapshot[0][:children][1][:name]).to eq("publishable")
+    expect(snapshot[0][:children][2][:name]).to eq("status")
+    expect(snapshot[1][:children][0][:children][1][:value]).to eq(file2.title)
+    expect(snapshot[1][:children][1][:name]).to eq("publishable")
+    expect(snapshot[1][:children][2][:name]).to eq("status")
   end
 end

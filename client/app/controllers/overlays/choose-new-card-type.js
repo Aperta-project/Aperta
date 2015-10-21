70ce5c6cec619709e3591792da80ec13ed25e983
Hildebrando Rueda <harq.soft@gmail.com>
2015-10-21 15:49:50 -0500

    APERTA-3446 Update choose-new-card overlay

--- client/app/controllers/overlays/choose-new-card-type.js
+++ client/app/controllers/overlays/choose-new-card-type.js
@@ -3,21 +3,20 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   overlayClass: 'overlay--fullscreen choose-new-card-type-overlay',
   taskTypeSort: ['title:asc'],
-  taskType: null,
+  taskList: [],
   sortedTaskTypes: Ember.computed.sort('journalTaskTypes', 'taskTypeSort'),
-  formattedTaskTypes: Ember.computed('sortedTaskTypes.@each', function() {
-    return this.get('sortedTaskTypes').map(function(taskType) {
-      return {
-        id: taskType.get('id'),
-        text: taskType.get('title')
-      };
-    });
-  }),
-  formattedTaskTypesReady: Ember.computed.notEmpty('formattedTaskTypes'),
+  authorTasks: Ember.computed.filterBy('sortedTaskTypes', 'role', 'author'),
+  staffTasks: Ember.computed.setDiff('sortedTaskTypes', 'authorTasks'),
 
   actions: {
-    taskTypeSelected(taskType) {
-      this.set('taskType', this.get('journalTaskTypes').findBy('id', taskType.id));
+
+    updateList(checkbox) {
+
+      if (checkbox.get("checked")) {
+        this.get('taskList').addObject(checkbox.get("task"));
+      } else {
+        this.get('taskList').removeObject(checkbox.get("task"));
+      }
     }
   }
 });

03c3a7e31ff7b7574d0ec55e8601bbe5677a5f8a
Hildebrando Rueda <harq.soft@gmail.com>
2015-10-21 17:29:50 -0500

    APERTA-3446 Remove no longer necessary templates

--- client/app/views/overlays/add-manuscript-template-card.js
+++
@@ -1,7 +0,0 @@
-import OverlayView from 'tahi/views/overlay';
-
-export default OverlayView.extend({
-  templateName: 'overlays/add-manuscript-template-card',
-  layoutName: 'layouts/blank_overlay',
-  skipAnimation: false
-});

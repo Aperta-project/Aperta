b1924f5b081a1be7464b97c0cd0e097b6408ac80
Hildebrando Rueda <harq.soft@gmail.com>
2015-10-22 11:56:45 -0500

    APERTA-3446 update parameter

--- client/app/controllers/overlays/choose-new-card-type.js
+++ client/app/controllers/overlays/choose-new-card-type.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   overlayClass: 'overlay--fullscreen choose-new-card-type-overlay',
   taskTypeSort: ['title:asc'],
-  taskList: [],
+  taskTypeList: [],
   sortedTaskTypes: Ember.computed.sort('journalTaskTypes', 'taskTypeSort'),
   authorTasks: Ember.computed.filterBy('sortedTaskTypes', 'role', 'author'),
   staffTasks: Ember.computed.setDiff('sortedTaskTypes', 'authorTasks'),
@@ -13,9 +13,9 @@ export default Ember.Controller.extend({
     updateList(checkbox) {
 
       if (checkbox.get("checked")) {
-        this.get('taskList').addObject(checkbox.get("task"));
+        this.get('taskTypeList').addObject(checkbox.get("task"));
       } else {
-        this.get('taskList').removeObject(checkbox.get("task"));
+        this.get('taskTypeList').removeObject(checkbox.get("task"));
       }
     }
   }

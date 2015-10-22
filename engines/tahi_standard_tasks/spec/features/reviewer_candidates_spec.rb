43b8a4916360a01cc69243f79459207d78ac2c52
Zach Dennis <zach.dennis@gmail.com>
2015-10-22 16:17:25 -0400

    Adding a feature spec around adding a reviewer recommendation.
    * It is re-using author CSS classes. Rather than tackle the display issue now introduce reviewer classes and use those in the tests. Changing CSS is out of scope.
    
    APERTA-5417

---
+++ engines/tahi_standard_tasks/spec/features/reviewer_candidates_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+feature "User adding reviewer candidates", js: true do
+  let(:admin) { create :user, site_admin: true, first_name: 'Admin' }
+  let!(:paper)  { create :paper, :with_tasks, creator: admin }
+  let!(:reviewer_recommendations_task) do
+    FactoryGirl.create(
+      :reviewer_recommendations_task,
+      phase: paper.phases.first
+    )
+  end
+
+  before do
+    # load nested questions
+    TahiStandardTasks::ReviewerRecommendation.nested_questions
+
+    login_as(admin, scope: :user)
+    visit "/papers/#{paper.id}/workflow"
+  end
+
+  scenario "A user can add reviewer candidates" do
+    page = Page.new
+    overlay = page.view_card("Reviewer Candidates")
+
+    # Bringing up the new reviewer candidate form
+    click_button "New Reviewer Candidate"
+    wait_for_ajax
+    expect(page).to have_selector(".reviewer-form")
+
+    # Clicking cancel hides the new reviewer form
+    within ".reviewer-form" do
+      find("a", text: "cancel").click
+    end
+    expect(page).to_not have_selector(".reviewer-form")
+
+    click_button "New Reviewer Candidate"
+    wait_for_ajax
+
+    # Add a new reviewer
+    within ".reviewer-form" do
+      find(".first-name input[type=text]").set "Barb"
+      find(".last-name input[type=text]").set "AraAnn"
+      find(".email input[type=text]").set "barb@example.com"
+      choose "Recommend"
+      find("textarea[name*=reason]").set "Because they do good work"
+      click_button "done"
+    end
+    wait_for_ajax
+
+    # See the new reviewer
+    within ".reviewer" do
+      expect(page).to have_selector(".full-name", text: "Barb AraAnn")
+      expect(page).to have_selector(".email", text: "barb@example.com")
+      expect(page).to have_selector(".reason", text: "Because they do good work")
+    end
+
+    # Edit the reviewer
+    find(".reviewer").hover
+    find(".fa-pencil").click
+    within ".reviewer-form" do
+      first_name_input = find(".first-name input[type=text]")
+      expect(first_name_input.value).to eq("Barb")
+
+      find(".first-name input[type=text]").set "Bob"
+      click_button "done"
+    end
+
+    # See updated reviewer
+    within ".reviewer" do
+      expect(page).to have_selector(".full-name", text: "Bob AraAnn")
+    end
+  end
+
+end

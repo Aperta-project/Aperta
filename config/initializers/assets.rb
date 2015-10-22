c90da4ba8557b309f39fdebd76e253c5384b2ab7
Erik Hetzner <ehetzner@plos.org>
2015-10-22 16:35:38 -0700

    APERTA-5405 Enable woff2 files in asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.initialize_on_precompile = true
-Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|woff2)$/
 Rails.application.config.assets.precompile += %w(email.scss auth.scss)
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets')

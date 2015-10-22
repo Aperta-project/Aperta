b1565bfa3d1838777f23d8732d9ce12bb4442674
Erik Hetzner <ehetzner@plos.org>
2015-10-22 16:34:20 -0700

    APERTA-5405 Fix asset host for child apps
    All heroku apps have HEROKU_APP_NAME set. Only override configured
    RAILS_ASSET_HOST if HEROKU_PARENT_APP_NAME is set.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,7 +4,7 @@ Tahi::Application.configure do
   config.basic_auth_required = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  if ENV["HEROKU_APP_NAME"].present?
+  if ENV["HEROKU_PARENT_APP_NAME"].present?
     # this is only set for review apps. they end up with a domain like
     # "tahi-staging-pr-1786", which we can use to build up the asset host for
     # review apps

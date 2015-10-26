15b98f96a3e274c1e08832697b3adf45db176f97
Jerry Nummi <jerry@neo.com>
2015-10-26 10:38:05 -0400

    WIP

--- engines/tahi_standard_tasks/client/app/views/overlays/cover-letter.js
+++ engines/tahi_standard_tasks/client/app/views/overlays/cover-letter.js
@@ -8,9 +8,48 @@ export default OverlayView.extend({
   attributeBindings: ['data-width'],
   'data-width': 0,
 
-  _width: Ember.on('didInsertElement', function() {
-    Ember.run.later(this, function() {
-      this.set('data-width', this.$().width());
-    }, 200);
+  minWidth: 450,
+  startX: 0,
+  updateWidth() {
+    this.set('data-width', this.$().width());
+  },
+
+  _setInitialWidth: Ember.on('didInsertElement', function(){
+    Ember.run.later(()=> {
+      this.updateWidth();
+    }, 50);
+  }),
+
+  _startDragListen: Ember.on('didInsertElement', function() {
+    const self    = this;
+    const overlay = $('#overlay');
+    const element = this.$();
+    const handle  = this.$('.task-drag-handle');
+
+    const down = 'mousedown.' + self.elementId;
+    const move = 'mousemove.' + self.elementId;
+    const up   = 'mouseup.'   + self.elementId;
+
+    handle.on(down, function(e) {
+      const startWidth = overlay.width();
+      self.set('startX', e.pageX);
+      $('html').addClass('dragging');
+
+      $(document).on(move, function(event) {
+        const change = self.get('startX') - event.pageX;
+        const newWidth = startWidth + change;
+        overlay.css('width', newWidth);
+        Ember.run.debounce(self, self.updateWidth, 50);
+      });
+    });
+
+    $(document).on(up, function() {
+      $('html').removeClass('dragging');
+      $(document).off(move);
+    });
+  }),
+
+  _endDragListen: Ember.on('willDestroyElement', function() {
+    this.$('.task-drag-handle').off();
   })
 });

de9a8abde79d8e7448c72cfd54202c467ee45dbe
Jerry Nummi <jerry@neo.com>
2015-10-27 13:13:48 -0400

    WIP

---
+++ client/app/mixins/overlay-escape-listener.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  setupKeyup: Ember.on('didInsertElement', function() {
+    $('body').on('keyup.' + this.get('elementId'), (e)=> {
+      if (e.keyCode !== 27 || e.which !== 27) { return; }
+      if ($(e.target).is('input, textarea')) { return; }
+      this.send('closeOverlay');
+    });
+  }),
+
+  tearDownKeyup: Ember.on('willDestroyElement', function() {
+    $('body').off('keyup.' + this.get('elementId'));
+  })
+});

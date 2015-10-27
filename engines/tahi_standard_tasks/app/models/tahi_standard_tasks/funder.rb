469a6dc5e39fa0fd570b354c37f94f2e171b6799
Zach Dennis <zach.dennis@gmail.com>
2015-10-27 15:51:30 -0400

    Removing inline seeding of nested questions.
    The work that moved this out to a rake task was done in 7f6a8cb51572c64a53a5317a8ed14bcb868e4aa4
    
    APERTA-5419
    
    Removing more inline tasks.

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/funder.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/funder.rb
@@ -7,32 +7,6 @@ module TahiStandardTasks
     has_many :authors, through: :funded_authors
 
     def self.nested_questions
-      questions = []
-      questions << NestedQuestion.new(
-        owner_id:nil,
-        owner_type: name,
-        ident: "funder_had_influence",
-        value_type: "boolean",
-        text: "Did the funder have a role in study design, data collection and analysis, decision to publish, or preparation of the manuscript?",
-        position: 1,
-        children: [
-          NestedQuestion.new(
-            owner_id:nil,
-            owner_type: name,
-            ident: "funder_role_description",
-            value_type: "text",
-            text: "Describe the role of any sponsors or funders in the study design, data collection and analysis, decision to publish, or preparation of the manuscript.",
-            position: 1
-          )
-        ]
-      )
-
-      questions.each do |q|
-        unless NestedQuestion.where(owner_id:nil, owner_type:name, ident:q.ident).exists?
-          q.save!
-        end
-      end
-
       NestedQuestion.where(owner_id:nil, owner_type:name).all
     end
 

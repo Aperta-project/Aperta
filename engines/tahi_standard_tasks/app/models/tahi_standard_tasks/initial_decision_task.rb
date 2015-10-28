fbe6935148e32c2d3a550a6f432f02d8e734dffb
Hildebrando Rueda <harq.soft@gmail.com>
2015-10-28 11:21:23 -0500

    APERTA-3644 Register initial decision endpoint
    For now the event triggered will be major_revision

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/initial_decision_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/initial_decision_task.rb
@@ -3,6 +3,14 @@ module TahiStandardTasks
 
     register_task default_title: 'Initial Decision', default_role: 'editor'
 
+    def initial_decision
+      paper.decisions.first
+    end
+
+    def send_email
+      # InitialDecisionMailer.delay.notify_author
+    end
+
     def active_model_serializer
       InitialDecisionTaskSerializer
     end

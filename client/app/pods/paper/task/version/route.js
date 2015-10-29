4fb43f2243f5a7380bff7512aee2efd6dd0d5491
Sam Bleckley <sam@mutuallyhuman.com>
2015-10-29 15:07:22 -0400

    APERTA-3368 Adds versioned metadata task overlay.
    Adds an overlay for showing a tasks old versions.

--- client/app/pods/paper/task/version/route.js
+++ client/app/pods/paper/task/version/route.js
@@ -19,31 +19,19 @@ export default Ember.Route.extend({
   },
 
   setupController(controller, model) {
-    // TODO: Rename AdHocTask to Task (here, in views, and in templates)
-    let redirectOptions = this.get('cardOverlayService.reviousRouteOptions');
-    let currentType     = model.get('type') === 'Task' ? 'AdHocTask' : model.get('type');
-    let baseObjectName  = (currentType || 'AdHocTask').replace('Task', '');
-    let taskController  = this.controllerFor('overlays/' + baseObjectName);
-
-    this.set('baseObjectName', baseObjectName);
+    let redirectOptions = this.get('cardOverlayService.previousRouteOptions');
+    let taskController  = this.controllerFor('overlays/versioned_metadata');
     this.set('taskController', taskController);
-
     taskController.setProperties({
       model: model,
       comments: model.get('comments'),
       participations: model.get('participations'),
       onClose: Ember.isEmpty(redirectOptions) ? 'redirectToDashboard' : 'redirect'
     });
-
-    taskController.trigger('didSetupController');
-  },
-
-  resetController(controller, isExiting) {
-    if (isExiting) { controller.set('isNewTask', false); }
   },
 
   renderTemplate() {
-    this.render('overlays/' + this.get('baseObjectName'), {
+    this.render('overlays/versioned_metadata', {
       into: 'application',
       outlet: 'overlay',
       controller: this.get('taskController')

d334c3a0c21c651ce3094074cc964a1947a011d0
Hildebrando Rueda <harq.soft@gmail.com>
2015-10-30 17:33:07 -0500

    APERTA-3446 Fix issue with default value on component

--- client/app/pods/components/choose-new-card/component.js
+++ client/app/pods/components/choose-new-card/component.js
@@ -3,16 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   journalTaskTypes: null, // passed-in
   taskTypeSort: ['title:asc'],
-  taskTypeList: [],
   sortedTaskTypes: Ember.computed.sort('journalTaskTypes', 'taskTypeSort'),
   authorTasks: Ember.computed.filterBy('sortedTaskTypes', 'role', 'author'),
   staffTasks: Ember.computed.setDiff('sortedTaskTypes', 'authorTasks'),
+
+  setuptaskTypeList: Ember.on('init', function() {
+    if (!this.get('taskTypeList')) {
+      this.set('taskTypeList', []);
+    }
+  }),
 
   actions: {
     updateList(checkbox) {
 
       if (checkbox.get("checked")) {
-        this.get('taskTypeList').addObject(checkbox.get("task"));
+        this.get('taskTypeList').pushObject(checkbox.get("task"));
       } else {
         this.get('taskTypeList').removeObject(checkbox.get("task"));
       }

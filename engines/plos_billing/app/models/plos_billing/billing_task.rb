5289820c6a241004c949f5c8309eff0ce0b4acb9
Zach Dennis <zach.dennis@gmail.com>
2015-11-02 10:47:01 -0500

    Removing all of the class-level "nested_questions".
    * a default class-level method is provided when including the NestedQuestionable module
    * all of the removed methods were doing the same thing as the default
    
    APERTA-5419

--- engines/plos_billing/app/models/plos_billing/billing_task.rb
+++ engines/plos_billing/app/models/plos_billing/billing_task.rb
@@ -4,10 +4,6 @@ module PlosBilling
 
     register_task default_title: "Billing", default_role: "author"
 
-    def self.nested_questions
-      NestedQuestion.where(owner_id:nil, owner_type:name).all
-    end
-
     def active_model_serializer
       TaskSerializer
     end
--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/competing_interests_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/competing_interests_task.rb
@@ -2,9 +2,5 @@ module TahiStandardTasks
   class CompetingInterestsTask < ::Task
     include MetadataTask
     register_task default_title: "Competing Interests", default_role: "author"
-
-    def self.nested_questions
-      NestedQuestion.where(owner_id:nil, owner_type:name).all
-    end
   end
 end

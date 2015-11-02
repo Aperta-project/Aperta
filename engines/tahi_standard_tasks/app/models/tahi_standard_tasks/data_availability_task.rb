0a8b565df4fa29d71ed7869a6cecb95fb3d4cdb7
Zach Dennis <zach.dennis@gmail.com>
2015-11-02 10:47:01 -0500

    Removing all of the class-level "nested_questions".
    * a default class-level method is provided when including the NestedQuestionable module
    * all of the removed methods were doing the same thing as the default
    
    APERTA-5419

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/data_availability_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/data_availability_task.rb
@@ -2,9 +2,5 @@ module TahiStandardTasks
   class DataAvailabilityTask < ::Task
     include MetadataTask
     register_task default_title: "Data Availability", default_role: "author"
-
-    def self.nested_questions
-      NestedQuestion.where(owner_id:nil, owner_type:name).all
-    end
   end
 end
--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/financial_disclosure_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/financial_disclosure_task.rb
@@ -3,10 +3,5 @@ module TahiStandardTasks
     include MetadataTask
     register_task default_title: "Financial Disclosure", default_role: "author"
     has_many :funders, inverse_of: :task, foreign_key: :task_id
-
-    def self.nested_questions
-      NestedQuestion.where(owner_id:nil, owner_type:name).all
-    end
-
   end
 end

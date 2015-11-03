c1b2ee3a58ced0b9c394b688e1c475ba5da0b60b
Erik Hetzner <ehetzner@plos.org>
2015-11-03 15:31:43 -0800

    Configure ember-cli-rails to use local bower

--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -2,6 +2,8 @@ EmberCLI.configure do |c|
   c.app(:tahi, {
     path: Rails.root.join('client'),
     build_timeout: 35,
-    enable: -> path { !%r{/(?:api|assets)/}.match(path) }
+    enable: -> path { !%r{/(?:api|assets)/}.match(path) },
+    # use locally installed bower bin
+    bower_path: Rails.root.join('client', 'node_modules', '.bin', 'bower')
   })
 end

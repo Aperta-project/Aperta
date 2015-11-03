0d8e3462bbaad920318d3294b07f0fb5600695ab
Hildebrando Rueda <harq.soft@gmail.com>
2015-11-03 18:18:37 -0500

    Create new full-screen-modal component

---
+++ client/app/pods/components/full-screen-modal/component.js
@@ -0,0 +1,7 @@
+import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
+
+export default ModalDialog.extend({
+  overlayClassNames: 'overlay overlay--fullscreen',
+  containerClassNames: 'overlay-container',
+  targetAttachment: 'none'
+});

739eb452c0fa946e21ef9de4eac138842b6f60bc
Aaron Christy <aaron@mutuallyhuman.com>
2015-11-04 16:34:35 -0500

    change button label from CAS to PLOS

--- spec/features/sign_in_cas_spec.rb
+++ spec/features/sign_in_cas_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 feature "CAS account creation" do
   scenario "User can create account for cas" do
     sign_in_page = SignInPage.visit
-    expect(sign_in_page).to have_link('Create CAS Account')
+    expect(sign_in_page).to have_link('Create PLOS Account')
   end
 
   scenario "User can login with cas" do
     sign_in_page = SignInPage.visit
-    expect(sign_in_page).to have_link('Sign in with CAS')
+    expect(sign_in_page).to have_link('Sign in with PLOS')
   end
 end

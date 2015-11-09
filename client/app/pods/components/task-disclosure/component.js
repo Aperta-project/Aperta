68d161566b5a8e58e7ca3ada10342aa4ad48ba1f
Jerry Nummi <jerry@neo.com>
2015-11-09 18:44:38 -0500

    Sync MS sidebar with latest mocks

--- client/app/pods/components/task-disclosure/component.js
+++ client/app/pods/components/task-disclosure/component.js
@@ -3,21 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNameBindings: [
     ':task-disclosure',
-    'taskVisible:task-disclosure--open',
-    'task.completed:completed'
+    'taskVisible:task-disclosure--open'
   ],
 
   taskVisible: false,
 
-  _propertiesCheck: Ember.on('init', function() {
-    Ember.assert('You must pass a task property to the TaskDisclosureComponent', this.hasOwnProperty('task'));
-  }),
-
-  task: null,
-
-  unreadCommentsCount: Ember.computed('task.commentLooks.[]', function() {
-    return this.get('task.commentLooks').length;
-  }),
+  title: null,
+  completed: false,
 
   actions: {
     toggleVisibility() {

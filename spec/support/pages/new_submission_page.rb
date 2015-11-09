4863a11a859f463826ac68c9af11a8be86f022d8
Jerry Nummi <jerry@neo.com>
2015-11-09 14:39:52 -0500

    Fix paper-title selector on New Manuscript Overlay

--- spec/support/pages/new_submission_page.rb
+++ spec/support/pages/new_submission_page.rb
@@ -1,6 +1,6 @@
 class NewSubmissionPage < Page
   def create_submission(short_title:, journal:, paper_type:)
-    fill_in 'paper-short-title', with: short_title
+    fill_in 'paper-title', with: short_title
     select2 journal, css: ".paper-new-journal-select"
     select2 paper_type, css: ".paper-new-paper-type-select"
     click_on 'Create'

c2035b63b44838140b133837b33ee3d048b045eb
Hildebrando Rueda <harq.soft@gmail.com>
2015-11-09 16:44:26 -0500

    APERTA-5537 participation factory now accepts notify keyword

--- spec/services/participation_factory_spec.rb
+++ spec/services/participation_factory_spec.rb
@@ -3,46 +3,42 @@ require "rails_helper"
 describe ParticipationFactory do
   let(:task) { FactoryGirl.create(:task) }
 
-  describe ".create" do
+  describe '.create' do
     let(:assignee) { FactoryGirl.create(:user) }
     let(:assigner) { FactoryGirl.create(:user) }
+    let(:full_params) { { task: task, assignee: assignee, assigner: assigner } }
+    let(:same_params) { { task: task, assignee: assignee, assigner: assignee } }
 
-    context "when the assignee is already a participant" do
-      before do
-        Participation.create(task: task, user: assignee)
-      end
-
-      it "does not create a participation" do
-        expect {
-          ParticipationFactory.create(task: task, assignee: assignee, assigner: assigner)
-        }.to_not change(Participation, :count)
-      end
+    it 'Do not sent notification if the notify flag is set to true' do
+      expect(UserMailer).to_not receive(:delay)
+      ParticipationFactory.create(task: task, assignee: assignee, notify: false)
     end
 
-    context "assigner and assignee are different" do
-      it "creates a participation that does not notify the assigner" do
-        participation = ParticipationFactory.create(task: task, assignee: assignee, assigner: assigner)
-        expect(participation.notify_requester).to eq(false)
-      end
-
-      it "emails the assignee" do
-        expect(UserMailer).to receive_message_chain("delay.add_participant")
-        ParticipationFactory.create(task: task, assignee: assignee, assigner: assigner)
-      end
+    it 'Sent an email notification to the assignee' do
+      expect(UserMailer).to receive_message_chain('delay.add_participant')
+      ParticipationFactory.create(full_params)
     end
 
-    context "assigner is the same as the assignee" do
-      let(:user) { FactoryGirl.create(:user) }
+    it 'Does not create a participation if already participant' do
+      Participation.create(task: task, user: assignee)
+      expect do
+        ParticipationFactory.create(full_params)
+      end.to_not change(Participation, :count)
+    end
 
-      it "creates a participation that notifies the assigner" do
-        participation = ParticipationFactory.create(task: task, assignee: user, assigner: user)
-        expect(participation.notify_requester).to eq(true)
-      end
+    it 'Creates a participation that does not notify the assigner' do
+      participation = ParticipationFactory.create(full_params)
+      expect(participation.notify_requester).to eq(false)
+    end
 
-      it "does not email the assignee" do
-        expect(UserMailer).to_not receive(:delay)
-        ParticipationFactory.create(task: task, assignee: user, assigner: user)
-      end
+    it 'Creates a participation that notifies the assigner if is the same' do
+      participation = ParticipationFactory.create(same_params)
+      expect(participation.notify_requester).to eq(true)
+    end
+
+    it 'Does not email the assignee if is the same as the assigner' do
+      expect(UserMailer).to_not receive(:delay)
+      ParticipationFactory.create(same_params)
     end
   end
 end

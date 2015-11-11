22f638e90d4fef80745c86b64e2482b8d87d139d
Jerry Nummi <jerry@neo.com>
2015-11-11 12:06:20 -0500

    Fix New MS Overlay paper title styles

--- spec/support/pages/new_submission_page.rb
+++ spec/support/pages/new_submission_page.rb
@@ -1,10 +1,10 @@
 class NewSubmissionPage < Page
   def create_submission(short_title:, journal:, paper_type:)
-    fill_in 'paper-title', with: short_title
+    fill_in 'new-paper-title', with: short_title
     select2 journal, css: ".paper-new-journal-select"
     select2 paper_type, css: ".paper-new-paper-type-select"
     click_on 'Create'
-    expect(session).to have_css('#paper-title')
+    expect(session).to have_css('#new-paper-title')
     PaperPage.new
   end
 end

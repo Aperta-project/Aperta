d1d8f86ad52cf5729e00561ad1501350abbf1f13
Sam Bleckley <sam@mutuallyhuman.com>
2015-11-11 17:15:51 -0500

    APERTA-3653 Adds apex delivery component
    Displays the status of apex uploads.

--- engines/tahi_standard_tasks/client/app/controllers/overlays/send-to-apex.js
+++ engines/tahi_standard_tasks/client/app/controllers/overlays/send-to-apex.js
@@ -3,7 +3,10 @@ import TaskController from 'tahi/pods/paper/task/controller';
 var SendToApexOverlayController = TaskController.extend({
   actions: {
     sendToApex: function() {
-
+      let apexDelivery = this.store.createRecord('apex-delivery', {
+        task: this.get('model')
+      });
+      apexDelivery.save();
     }
   }
 });
--- engines/tahi_standard_tasks/config/initializers/subscriptions.rb
+++ engines/tahi_standard_tasks/config/initializers/subscriptions.rb
@@ -4,4 +4,6 @@ Subscriptions.configure do
   add 'paper:resubmitted', Paper::Resubmitted::ReopenRevisionTasks
 
   add 'tahi_standard_tasks/apex_delivery:updated', stream_to_user
+  add 'tahi_standard_tasks/apex_delivery:delivery_succeeded',
+      ApexDelivery::DeliverySucceeded::FlashSuccessMessage
 end

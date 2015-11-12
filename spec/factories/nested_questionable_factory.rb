d36ff6f66db6c34e65a5b20e0fe347795ce167d1
AJ Hekman <aj@mutuallyhuman.com>
2015-11-12 17:03:22 -0500

    APEX-3564 improvements to NestedQuestionableFactory

--- spec/factories/nested_questionable_factory.rb
+++ spec/factories/nested_questionable_factory.rb
@@ -2,31 +2,33 @@ class NestedQuestionableFactory
   class << self
     def create(owner, questions:)
       @owner = owner
-      build_questions(questions)
+      create_questions(questions)
       @owner
     end
 
-    def build_questions(questions, parent: nil)
+    def create_questions(questions, parent: nil)
       questions.each do |question_hash|
-        question = build_question(question_hash, parent)
-        build_answer(question_hash, question)
+        question = create_question(question_hash, parent)
+        create_answer(question_hash, question)
         if question_hash[:questions]
-          build_questions(question_hash[:questions], parent: question)
+          create_questions(question_hash[:questions], parent: question)
         end
       end
     end
 
-    def build_question(question_hash, parent)
+    def create_question(question_hash, parent)
       FactoryGirl.create(
         :nested_question,
         parent: parent,
+        text: question_hash[:text],
         ident: question_hash[:ident],
         owner: @owner,
+        owner_type: @owner.class.name,
         value_type: question_hash[:value_type]
       )
     end
 
-    def build_answer(question_hash, question)
+    def create_answer(question_hash, question)
       FactoryGirl.create(
         :nested_question_answer,
         nested_question: question,

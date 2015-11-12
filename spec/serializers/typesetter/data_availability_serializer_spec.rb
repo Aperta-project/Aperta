872d954b713e4d981fc7b5f3478d8cbc6848c1fe
AJ Hekman <aj@mutuallyhuman.com>
2015-11-12 17:34:27 -0500

    APERTA-3564 DataAvailabilitySerializer specs

---
+++ spec/serializers/typesetter/data_availability_serializer_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe Typesetter::DataAvailabilitySerializer do
+  subject(:serializer) { described_class.new(task) }
+
+  let!(:task) do
+    NestedQuestionableFactory.create(
+      FactoryGirl.create(:data_availability_task),
+      questions: [
+        {
+          ident: 'data_fully_available',
+          answer: 'true',
+          value_type: 'boolean'
+        },
+        {
+          ident: 'data_location',
+          answer: 'holodeck',
+          value_type: 'text'
+        }
+      ]
+    )
+  end
+
+  let(:output) { serializer.serializable_hash }
+
+  it 'has data availability fields' do
+    expect(output.keys).to contain_exactly(
+      :data_fully_available,
+      :data_location_statement)
+  end
+
+  describe 'data fully available value' do
+    it 'is the answer to the data fully available quesiton' do
+      expect(output[:data_fully_available]).to eq(true)
+    end
+  end
+
+  describe 'data location statement value' do
+    it 'is the answer to the data location statement quesiton' do
+      expect(output[:data_location_statement]).to eq('holodeck')
+    end
+  end
+end

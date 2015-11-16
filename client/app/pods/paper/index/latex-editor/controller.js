296ff9a6f8a6ffe6bd7bc19091cf80a55d8865a8
Sam Bleckley <sam@mutuallyhuman.com>
2015-11-16 15:21:41 -0500

    APERTA-5546 Removes latex and html editor directories.
    Had to remove some of the editor-mode sneakiness from the paper.index
    route.

--- client/app/pods/paper/index/latex-editor/controller.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-import PaperBase from 'tahi/mixins/controllers/paper-base';
-import Discussions from 'tahi/mixins/discussions/route-paths';
-
-export default Ember.Controller.extend(PaperBase, Discussions);

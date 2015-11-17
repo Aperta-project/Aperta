0b697352550920e34ce7606d0239b80be37dc28b
Tad Park <tadpark@gmail.com>
2015-11-17 13:55:39 -0800

    APERTA-3553 Refactor by creating FtpService with optional initializer

--- app/services/ftp_services/ftp_service.rb
+++ app/services/ftp_services/ftp_service.rb
@@ -1,28 +1,50 @@
 module FtpServices
   class FtpService
     require 'net/ftp'
+ def initialize(params={})
+      @host = params[:host] || ENV['FTP_HOST']
+      @mode = params[:passive_mode] || true
+      @user = params[:user] || ENV['FTP_USER']
+      @password = params[:password] || ENV['FTP_PASSWORD']
+      @port = params[:port] || 21
+      @file_path = Rails.root.join('public', 'images', 'cat-scientists-3.jpg')
+    end
 
-    def connection
-      ftp = Net::FTP.new(ENV['FTP_HOST'])
-      ftp.passive = true
-      ftp.login(user = ENV['FTP_USER'], passwd = ENV['FTP_PASSWORD'])
-      remote_directories = ftp.nlst
-      ftp.mkdir('packages') unless remote_directories.index('packages')
-      ftp.chdir('packages')
-      count = 0
-      filename = "test.jpg"
-      temporary_name = "temp_#{filename}"
-      ftp.putbinaryfile(File.new(Rails.root.join('public', 'images', 'cat-scientists-3.jpg')), temporary_name, 100000) do |block|
-        count += 100
-        puts "#{count} kilobytes uploaded"
-      end
-      if ftp.last_response_code == "226"
-        ftp.rename(temporary_name, filename)
+    def upload
+      connect_to_server
+      enter_packages_directory
+      upload_to_temporary_file
+      if @ftp.last_response_code == "226"
+        @ftp.rename(temporary_name, filename)
         puts "Transfer successful"
       else
         puts "Transfer failed. Please try again."
       end
-      ftp.close
+      @ftp.close
     end
+
+    def connect_to_server
+      @ftp = Net::FTP.new
+      @ftp.connect(@host, @port)
+      @ftp.passive = @mode
+      @ftp.login(user = @user, passwd = @password)
+    end
+
+    def enter_packages_directory
+      remote_directories = @ftp.nlst
+      @ftp.mkdir('packages') unless remote_directories.index('packages')
+      @ftp.chdir('packages')
+    end
+
+    def upload_to_temporary_file
+      @count = 0
+      filename = "test.jpg"
+      temporary_name = "temp_#{filename}"
+      @ftp.putbinaryfile(File.new(@file_path), temporary_name, 100000) do |block|
+        @count += 100
+        puts "#{@count} kilobytes uploaded"
+      end
+    end
+
   end
 end
--- spec/services/ftp_services/ftp_service_spec.rb
+++ spec/services/ftp_services/ftp_service_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe FtpServices::FtpService do
 
-  describe "#connection" do
-    it "success for transfer" do
-      FtpServices::FtpService.new.connection
+  describe "#upload" do
+    it "successfully transfers file to server" do
+      FtpServices::FtpService.new.upload
       expect(1).to eq(1)
     end
   end

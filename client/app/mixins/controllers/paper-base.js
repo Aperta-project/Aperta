12ff845481838d33d6b3748a79ffb50234b07e16
Jerry Nummi <jerry@mutuallyhuman.com>
2015-11-17 15:07:10 -0500

    Fix activity and collaborators overlays in versions

--- client/app/mixins/controllers/paper-base.js
+++ client/app/mixins/controllers/paper-base.js
@@ -1,13 +1,22 @@
 import Ember from 'ember';
 import DocumentDownload from 'tahi/services/document-download';
 import ENV from 'tahi/config/environment';
+import Utils from 'tahi/services/utils';
 
 const { computed } = Ember;
 
 export default Ember.Mixin.create({
+  restless: Ember.inject.service('restless'),
+
   subRouteName: 'index',
   versioningMode: false,
   canViewManuscriptManager: false,
+
+  activityIsLoading: false,
+  showActivityOverlay: false,
+  activityFeed: null,
+
+  showCollaboratorsOverlay: false,
 
   supportedDownloadFormats: computed(function() {
     return ENV.APP.iHatExportFormats.map(format => {
@@ -25,11 +34,39 @@ export default Ember.Mixin.create({
 
   actions: {
     exportDocument(downloadType) {
-      return DocumentDownload.initiate(this.get('model.id'), downloadType.format);
+      return DocumentDownload.initiate(
+        this.get('model.id'),
+        downloadType.format
+      );
     },
 
     saveManuscriptTitle() {
       Ember.run.debounce(this, this.save, 500);
+    },
+
+    hideActivityOverlay() {
+      this.set('showActivityOverlay', false);
+    },
+
+    showActivity(type) {
+      this.set('activityIsLoading', true);
+      this.set('showActivityOverlay', true);
+      const url = `/api/papers/${this.get('model.id')}/activity/${type}`;
+
+      this.get('restless').get(url).then((data)=> {
+        this.setProperties({
+          activityIsLoading: false,
+          activityFeed: Utils.deepCamelizeKeys(data.feeds)
+        });
+      });
+    },
+
+    showCollaboratorsOverlay() {
+      this.set('showCollaboratorsOverlay', true);
+    },
+
+    hideCollaboratorsOverlay() {
+      this.set('showCollaboratorsOverlay', false);
     }
   }
 });

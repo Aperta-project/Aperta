99d269a3d041da49ea0bbc42807e12ed19e3e629
Tad Park <tadp@users.noreply.github.com>
2015-11-16 16:22:52 -0800

    APERTA-3553 Create folder if it doesn't exist and use temporary files

--- app/services/ftp_services/ftp_service.rb
+++ app/services/ftp_services/ftp_service.rb
@@ -6,14 +6,22 @@ module FtpServices
       ftp = Net::FTP.new(ENV['FTP_HOST'])
       ftp.passive = true
       ftp.login(user = ENV['FTP_USER'], passwd = ENV['FTP_PASSWORD'])
-      ftp.chdir('test')
+      remote_directories = ftp.nlst
+      ftp.mkdir('packages') unless remote_directories.index('packages')
+      ftp.chdir('packages')
       count = 0
-      ftp.putbinaryfile(File.new(Rails.root.join('public', 'images', 'cat-scientists-3.jpg')), 'test.jpg', 100000) do |block|
+      filename = "test.jpg"
+      temporary_name = "temp_#{filename}"
+      ftp.putbinaryfile(File.new(Rails.root.join('public', 'images', 'cat-scientists-3.jpg')), temporary_name, 100000) do |block|
         count += 100
         puts "#{count} kilobytes uploaded"
       end
-      ftp.last_response
-      ftp.last_response_code
+      if ftp.last_response_code == "226"
+        ftp.rename(temporary_name, filename)
+        puts "Transfer successful"
+      else
+        puts "Transfer failed. Please try again."
+      end
       ftp.close
     end
   end

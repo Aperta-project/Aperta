d2fa37c5e0adeca323f255f64d272b4bd56bb47d
AJ Hekman <aj@mutuallyhuman.com>
2015-11-17 14:23:21 -0500

    APERTA-3564 NestedQuestionableFactory rm @owner
    Remove @owner class variable and pass owner around to helper methods.

--- spec/factories/nested_questionable_factory.rb
+++ spec/factories/nested_questionable_factory.rb
@@ -1,38 +1,40 @@
 class NestedQuestionableFactory
   class << self
     def create(owner, questions:)
-      @owner = owner
-      create_questions(questions)
-      @owner
+      create_questions(questions, owner: owner)
+      owner
     end
 
-    def create_questions(questions, parent: nil)
+    def create_questions(questions, parent: nil, owner: nil)
       questions.each do |question_hash|
-        question = create_question(question_hash, parent)
-        create_answer(question_hash, question)
+        question = create_question(question_hash, parent, owner)
+        create_answer(question_hash, question, owner)
         if question_hash[:questions]
-          create_questions(question_hash[:questions], parent: question)
+          create_questions(
+            question_hash[:questions],
+            parent: question,
+            owner: owner)
         end
       end
     end
 
-    def create_question(question_hash, parent)
+    def create_question(question_hash, parent, owner)
       FactoryGirl.create(
         :nested_question,
         parent: parent,
         text: question_hash[:text],
         ident: question_hash[:ident],
-        owner: @owner,
-        owner_type: @owner.class.name,
+        owner: owner,
+        owner_type: owner.class.name,
         value_type: question_hash[:value_type]
       )
     end
 
-    def create_answer(question_hash, question)
+    def create_answer(question_hash, question, owner)
       FactoryGirl.create(
         :nested_question_answer,
         nested_question: question,
-        owner: @owner,
+        owner: owner,
         value: question_hash[:answer],
         value_type: question_hash[:value_type]
       )

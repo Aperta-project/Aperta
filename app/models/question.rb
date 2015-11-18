5691da23533b914fc022f788b7acf2e83bc5d8bf
Matt Mercieca <mercieca@gmail.com>
2015-11-18 14:22:40 -0500

    APERTA-5558 Updating authors model and tests to remove unused columns

--- app/models/question.rb
+++
@@ -1,7 +0,0 @@
-class Question < ActiveRecord::Base
-  belongs_to :task
-  belongs_to :decision
-  has_one :question_attachment, dependent: :destroy, as: :question
-
-  validates :ident, presence: true
-end

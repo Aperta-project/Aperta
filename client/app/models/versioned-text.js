64d5d142e971d0d810a0b8b9934aa49668b4aa55
Sam Bleckley <sam@mutuallyhuman.com>
2015-11-19 17:01:39 -0500

    APERTA-3368 Display versioned text's updated at date.

--- client/app/models/versioned-text.js
+++ client/app/models/versioned-text.js
@@ -1,4 +1,6 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+import formatDate from 'tahi/lib/format-date';
 
 
 export default DS.Model.extend({
@@ -6,6 +8,15 @@ export default DS.Model.extend({
   text: DS.attr('string'),
   majorVersion: DS.attr(),
   minorVersion: DS.attr(),
-  createdAt: DS.attr('date'),
-  versionString: DS.attr('string')
+  updatedAt: DS.attr('date'),
+  versionString: Ember.computed(
+    'majorVersion',
+    'minorVersion',
+    'updatedAt',
+    function() {
+      let date = formatDate(this.get('updatedAt'), {format: 'MMM DD, YYYY'});
+      let version = `R${this.get('majorVersion')}.${this.get('minorVersion')}`;
+      return `${version} - ${date}`;
+    }
+  )
 });

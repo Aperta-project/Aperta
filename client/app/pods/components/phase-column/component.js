40f19b632d2fed78807caa0e95fb3dac6b8ca367
Hildebrando Rueda <harq.soft@gmail.com>
2015-11-23 16:35:41 -0500

    APERTA-5585 update phase-column component
    Replace logic with the use of the sortable-list component

--- client/app/pods/components/phase-column/component.js
+++ client/app/pods/components/phase-column/component.js
@@ -4,7 +4,6 @@ const { computed } = Ember;
 
 export default Ember.Component.extend({
   classNames: ['column'],
-
   phase: null,
   paper: null,
 
@@ -12,13 +11,9 @@ export default Ember.Component.extend({
     return this.get('phase.position') + 1;
   }),
 
-  commentLooks: null,
-
   sortedTasks: computed('phase.tasks.[]', function() {
     return this.get('phase.tasks').sortBy('position');
   }),
-
-  noCards: computed.empty('sortedTasks'),
 
   checkIfNewPhase: Ember.on('didInitAttrs', function() {
     if(this.get('phase.isNew')) {
@@ -26,51 +21,6 @@ export default Ember.Component.extend({
         this.animateIn();
       });
     }
-  }),
-
-  setupSortable: Ember.on('didInsertElement', function() {
-    const phaseId = this.get('phase.id');
-    const self = this;
-
-    this.$('.sortable').sortable({
-      items: '.card',
-      scroll: false,
-      containment: '.columns',
-      connectWith: '.sortable',
-
-      update(event, ui) {
-        let updatedOrder      = {};
-        const senderPhaseId   = phaseId;
-        const receiverPhaseId = ui.item.parent().data('phase-id') + '';
-        const task = self.getTaskByID(ui.item.find('.card-content').data('id'));
-
-        if(senderPhaseId !== receiverPhaseId) {
-          self.sendAction('changePhaseForTask', task, receiverPhaseId);
-          Ember.run.scheduleOnce('afterRender', self, function() {
-            ui.item.remove();
-          });
-        }
-
-        $(this).find('.card-content').each(function(index) {
-          updatedOrder[$(this).data('id')] = index + 1;
-        });
-
-        self.updateSortOrder(updatedOrder);
-      },
-
-      start(event, ui) {
-        // class added to set overflow: visible;
-        $(ui.item).addClass('card--dragging')
-                  .closest('.column-content')
-                  .addClass('column-content--dragging');
-      },
-
-      stop(event, ui) {
-        $(ui.item).removeClass('card--dragging')
-                  .closest('.column-content')
-                  .removeClass('column-content--dragging');
-      }
-    });
   }),
 
   animateIn() {
@@ -100,21 +50,6 @@ export default Ember.Component.extend({
       duration: 200,
       easing: [0.00, 0.00, 1.00, 0.02],
       begin: function() { el.css('overflow', 'hidden'); }
-    });
-  },
-
-  updateSortOrder(updatedOrder) {
-    this.beginPropertyChanges();
-    this.get('phase.tasks').forEach(function(task) {
-      task.set('position', updatedOrder[task.get('id')]);
-    });
-    this.endPropertyChanges();
-    this.get('phase.tasks').invoke('save');
-  },
-
-  getTaskByID(taskId) {
-    return this.get('phase.tasks').find(function(t) {
-      return t.get('id') === taskId.toString();
     });
   },
 

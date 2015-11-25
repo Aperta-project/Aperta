1481aaf86c99376431792402a9cf70cb9f5317ee
AJ Hekman <aj@mutuallyhuman.com>
2015-11-25 12:04:10 -0500

    APERTA-3564 Add spec for Typesetter Editor Serializer
    * also make affiliation optional

--- app/serializers/typesetter/editor_serializer.rb
+++ app/serializers/typesetter/editor_serializer.rb
@@ -2,7 +2,8 @@ module Typesetter
   # Serializes editor for the typesetter.
   # Expects an editor as its object to serialize.
   class EditorSerializer < ActiveModel::Serializer
-    attributes :first_name, :last_name, :email, :department, :title
+    attributes :first_name, :last_name, :email, :department, :title,
+               :organization
 
     private
 
@@ -11,11 +12,18 @@ module Typesetter
     end
 
     def department
+      return unless affiliation
       affiliation.department
     end
 
     def title
+      return unless affiliation
       affiliation.title
+    end
+
+    def organization
+      return unless affiliation
+      affiliation.name
     end
   end
 end
---
+++ spec/serializers/typesetter/editor_serializer_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+RSpec.shared_examples 'editor fields' do
+  let!(:editor) do
+    FactoryGirl.create(
+      :user,
+      first_name: first_name,
+      last_name: last_name,
+      email: email
+    )
+  end
+
+  let(:first_name) { 'Bob' }
+  let(:last_name) { 'Ross' }
+  let(:email) { 'happytrees@example.com' }
+
+  describe 'first_name' do
+    it "is the editor's first_name" do
+      expect(output[:first_name]).to eq(first_name)
+    end
+  end
+  describe 'last_name' do
+    it "is the editor's last_name" do
+      expect(output[:last_name]).to eq(last_name)
+    end
+  end
+  describe 'email' do
+    it "is the editor's email" do
+      expect(output[:email]).to eq(email)
+    end
+  end
+end
+
+describe Typesetter::EditorSerializer do
+  subject(:serializer) { described_class.new(editor) }
+  let(:output) { serializer.serializable_hash }
+
+  context 'no affiliation' do
+    include_examples 'editor fields'
+
+    it 'has nil values for fidlds affiliations' do
+      %w(department title organization).each do |field_name|
+        expect(output[field_name.to_sym]).to be(nil)
+      end
+    end
+  end
+
+  context 'with affiliation' do
+    include_examples 'editor fields'
+    let!(:affiliation) do
+      FactoryGirl.create(
+        :affiliation,
+        user: editor,
+        name: affiliation_name,
+        title: affiliation_title,
+        department: affiliation_department
+      )
+    end
+    let(:affiliation_name) { 'PBS' }
+    let(:affiliation_title) { 'Artist' }
+    let(:affiliation_department) { 'Art' }
+
+    describe 'organization' do
+      it "is the editor's organization" do
+        expect(output[:organization]).to eq(affiliation_name)
+      end
+    end
+
+    describe 'title' do
+      it "is the editor's title" do
+        expect(output[:title]).to eq(affiliation_title)
+      end
+    end
+
+    describe 'department' do
+      it "is the editor's department" do
+        expect(output[:department]).to eq(affiliation_department)
+      end
+    end
+  end
+end

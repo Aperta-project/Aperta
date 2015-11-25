cde0f617244c5f8626185b3ac98db05b92424bd0
Aaron Christy <aaron@mutuallyhuman.com>
2015-11-25 14:35:10 -0500

    IhatJobRequest looks up recipe name
    * using file format, determine correct recipe name

--- app/services/ihat_job_request.rb
+++ app/services/ihat_job_request.rb
@@ -2,8 +2,7 @@
 class IhatJobRequest
   attr_reader :file
 
-  def initialize(file:, recipe_name: 'docx_to_html',
-                 callback_url: nil, metadata: {})
+  def initialize(file:, recipe_name:, callback_url: nil, metadata: {})
     @file = file
     @recipe_name = recipe_name
     @callback_url = callback_url
@@ -14,6 +13,20 @@ class IhatJobRequest
     { recipe_name: @recipe_name }.tap do |options|
       options[:callback_url] = @callback_url if @callback_url
       options[:metadata] = encrypted_payload if @metadata
+    end
+  end
+
+  def self.recipe_name(from_format:, to_format:)
+    from, to = [from_format, to_format].map(&:to_sym)
+    case [from, to]
+    when [:doc, :html]
+      'doc_to_html'
+    when [:docx, :html]
+      'docx_to_html'
+    when [:html, :docx]
+      'html_to_docx'
+    else
+      fail "Unable to find ihat recipe name for converting '#{from_format}' to '#{to_format}'"
     end
   end
 

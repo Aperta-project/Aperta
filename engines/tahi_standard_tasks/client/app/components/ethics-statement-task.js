13eff9fc724c25c8697af595818b380df95eefd3
Jerry Nummi <jerry@mutuallyhuman.com>
2015-12-02 13:22:43 -0500

    Ethics Task overlay to component

---
+++ engines/tahi_standard_tasks/client/app/components/ethics-statement-task.js
@@ -0,0 +1,43 @@
+import TaskComponent from 'tahi/pods/components/task-base/component';
+import SavesQuestionsOnClose from 'tahi/mixins/saves-questions-on-close';
+import Ember from 'ember';
+
+const { computed } = Ember;
+
+export default TaskComponent.extend(SavesQuestionsOnClose, {
+  humanSubjectsQuestion: computed('task', function(){
+    return this.get('task').findQuestion('human_subjects');
+  }),
+
+  participantsQuestion: computed('task', function(){
+    return this.get('task').findQuestion('human_subjects.participants');
+  }),
+
+  animalSubjectsQuestion: computed('task', function(){
+    return this.get('task').findQuestion('animal_subjects');
+  }),
+
+  fieldPermitQuestion: computed('task', function(){
+    return this.get('task').findQuestion('animal_subjects.field_permit');
+  }),
+
+  actions: {
+    destroyAttachment(attachment) {
+      attachment.destroyRecord();
+    },
+
+    userSelectedNoOnHumanSubjects() {
+      const question = this.get('participantsQuestion');
+      const answer = question.answerForOwner(this.get('task'));
+      answer.set('value', '');
+      answer.save();
+    },
+
+    userSelectedNoOnAnimalSubjects() {
+      const question = this.get('fieldPermitQuestion');
+      const answer = question.answerForOwner(this.get('task'));
+      answer.set('value', '');
+      answer.save();
+    },
+  }
+});
--- engines/tahi_standard_tasks/client/app/views/overlays/ethics.coffee
+++
@@ -1,7 +0,0 @@
-`import OverlayView from 'tahi/views/overlay'`
-
-EthicsOverlayView = OverlayView.extend
-  templateName: 'overlays/ethics'
-  layoutName: 'layouts/overlay' #TODO: include assignee here?
-
-`export default EthicsOverlayView`

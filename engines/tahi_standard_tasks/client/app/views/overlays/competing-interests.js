4023759098b060342132f93e744511a555b8fa7a
Jerry Nummi <jerry@mutuallyhuman.com>
2015-12-02 11:18:49 -0500

    Competing Interests Task overlay to component

--- engines/tahi_standard_tasks/client/app/views/overlays/competing-interests.js
+++
@@ -1,9 +0,0 @@
-import OverlayView from 'tahi/views/overlay';
-var CompetingInterestsOverlayView;
-
-CompetingInterestsOverlayView = OverlayView.extend({
-  templateName: 'overlays/competing_interests',
-  layoutName: 'layouts/overlay'
-});
-
-export default CompetingInterestsOverlayView;

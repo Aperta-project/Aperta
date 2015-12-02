7fa79bdc9fdbc9408a023f183e94e6ca1082ede5
Jerry Nummi <jerry@mutuallyhuman.com>
2015-12-02 13:09:11 -0500

    New Taxon Task overlay to component

---
+++ engines/tahi_standard_tasks/client/app/components/new-taxon-task.js
@@ -0,0 +1,4 @@
+import TaskComponent from 'tahi/pods/components/task-base/component';
+import SavesQuestionsOnClose from 'tahi/mixins/saves-questions-on-close';
+
+export default TaskComponent.extend(SavesQuestionsOnClose);
--- engines/tahi_standard_tasks/client/app/controllers/overlays/taxon.js
+++
@@ -1,8 +0,0 @@
-import TaskController from 'tahi/pods/paper/task/controller';
-import SavesQuestionsOnClose from 'tahi/mixins/saves-questions-on-close';
-
-var TaxonOverlayController;
-
-TaxonOverlayController = TaskController.extend(SavesQuestionsOnClose);
-
-export default TaxonOverlayController;

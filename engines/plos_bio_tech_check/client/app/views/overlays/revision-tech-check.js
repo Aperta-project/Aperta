27863b941d1d0b13ba54169692b21df2d42206ca
Jerry Nummi <jerry@mutuallyhuman.com>
2015-12-04 14:24:26 -0500

    Register Decision and Revision Tech Check Task overlays to components

--- engines/plos_bio_tech_check/client/app/views/overlays/revision-tech-check.js
+++
@@ -1,7 +0,0 @@
-import OverlayView from 'tahi/views/overlay';
-
-export default OverlayView.extend({
-  templateName: 'overlays/revision-tech-check',
-  layoutName: 'layouts/overlay',
-  cardName: 'revision-tech-check'
-});

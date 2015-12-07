bd758c74e179757aa0608e00b7b309966a0a81e1
Jerry Nummi <jerry@neo.com>
2015-12-07 11:21:55 -0500

    Initial Decision Task overlay to component

--- engines/tahi_standard_tasks/client/app/views/overlays/initial-decision.js
+++
@@ -1,7 +0,0 @@
-import OverlayView from 'tahi/views/overlay';
-
-export default OverlayView.extend({
-  templateName: 'overlays/initial-decision',
-  layoutName: 'layouts/overlay',
-  cardName: 'initial-decision'
-});

d0906e4a690f52fab30b1e6479a12e22f383900a
Sam Bleckley <sam@mutuallyhuman.com>
2015-12-07 16:27:01 -0500

    APERTA-3611 Adds text-diff specific scss

--- client/app/pods/components/text-diff/component.js
+++ client/app/pods/components/text-diff/component.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['html-diff'],
+  classNames: ['text-diff'],
 
   // This is the text of the version being viewed (left dropdown)
   viewingText: null,

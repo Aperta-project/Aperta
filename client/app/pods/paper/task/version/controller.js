bdd9fd594e30399621ea2ac75a4be87cc3207be2
Jerry Nummi <jerry@neo.com>
2015-12-08 11:40:24 -0500

    Versioned Metadata overlay to new style

--- client/app/pods/paper/task/version/controller.js
+++ client/app/pods/paper/task/version/controller.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
+
 export default Ember.Controller.extend({
-  queryParams: ['selectedVersion1', 'selectedVersion2']
+  queryParams: ['selectedVersion1', 'selectedVersion2'],
+
+  selectedVersion1Snapshot: Ember.computed('model', 'selectedVersion1', function(){
+    return this.get('model').getSnapshotForVersion(this.get('selectedVersion1'));
+  }),
+
+  selectedVersion2Snapshot: Ember.computed('model', 'selectedVersion2', function(){
+    return this.get('model').getSnapshotForVersion(this.get('selectedVersion2'));
+  }),
+
+  actions: {
+    close() {
+      this.transitionToRoute('paper.versions', this.get('model.paper'));
+    }
+  }
 });

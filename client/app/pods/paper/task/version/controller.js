aa71c34a76df6cc174a534e5149c4420f3fbb068
Jerry Nummi <jerry@neo.com>
2015-12-09 10:44:06 -0500

    Task Version overlay to component

--- client/app/pods/paper/task/version/controller.js
+++ client/app/pods/paper/task/version/controller.js
@@ -2,13 +2,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['majorVersion', 'minorVersion'],
 
-  snapshot: Ember.computed('model', 'majorVersion', 'minorVersion', function(){
-    return this.get('model').getSnapshotForVersion(
-      this.get('majorVersion'),
-      this.get('minorVersion')
-    );
-  }),
-
   actions: {
     close() {
       this.transitionToRoute('paper.versions', this.get('model.paper'));
--- client/app/pods/paper/task/version/route.js
+++ client/app/pods/paper/task/version/route.js
@@ -3,9 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.find('task', params.task_id);
-  },
-
-  afterModel(model) {
-    return Ember.RSVP.all([model.get('snapshots')]);
   }
 });

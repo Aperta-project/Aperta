b5e09c98773cd55f99a0a712483113ba01f7445d
Jerry Nummi <jerry@neo.com>
2015-12-09 20:58:07 -0500

    Use task type in place of task title for finding component

--- client/app/helpers/task-component-name.js
+++ client/app/helpers/task-component-name.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Helper.helper(function(params) {
-  return Ember.String.dasherize(params[0].toLowerCase()) + '-task';
+  if(params[0] === 'Task') { return 'ad-hoc-task'; }
+  return Ember.String.dasherize(params[0]);
 });

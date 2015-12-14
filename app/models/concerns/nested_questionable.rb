a500db0775b5790ef225993770a1c0e4bcd163a6
Aaron Christy <aaron@mutuallyhuman.com>
2015-12-14 15:51:50 -0500

    Remove unused #find_nested_questions method

--- app/models/concerns/nested_questionable.rb
+++ app/models/concerns/nested_questionable.rb
@@ -42,37 +42,4 @@ module NestedQuestionable
     nested_question_answers.includes(:nested_question).find_by(nested_questions: { ident: ident } )
   end
 
-  protected
-
-  # Recursively searches the given +nested_questions+ based on the collection
-  # of +path_parts+ provided to find all questions matching the given path_parts.
-  #
-  # ==== Non-recursive Example
-  #
-  #   find_nested_questions(["foo"], nested_questions)
-  #
-  # The above will only search the top-level elements provided in +nested_questions+
-  # to see if any has a matching ident of 'foo'.
-  #
-  # ==== Recursive Example (searching children)
-  #
-  #   find_nested_questions(["foo", "bar", "baz"], nested_questions)
-  #
-  # The above will expect to find "baz" nested two levels deep inside of
-  # +nested_questions+. It will expect to find "foo" in the top-level, then
-  # it will expect to find "bar" in foo's children. Lastly, it will expect to
-  # find "baz" in the children of bar.
-  #
-  def find_nested_questions(path_parts, nested_questions)
-    current_ident = path_parts.first
-    remaining_path_parts = path_parts[1..-1]
-    found_questions = nested_questions.select { |question| question.ident == current_ident }
-
-    if remaining_path_parts.empty?
-      found_questions
-    else
-      find_nested_questions(remaining_path_parts, found_questions.map(&:children).flatten)
-    end
-  end
-
 end

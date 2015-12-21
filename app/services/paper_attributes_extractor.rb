eb1773f8f5fb1c96cae762e8d37ec8bb9b9023bf
Matt Mercieca <mercieca@gmail.com>
2015-12-21 15:42:18 -0500

    APERTA-5549 Removing title from HTML returned from ihat

--- app/services/paper_attributes_extractor.rb
+++ app/services/paper_attributes_extractor.rb
@@ -1,3 +1,4 @@
+# Updates paper with attributes extracted from an epub returned from ihat
 class PaperAttributesExtractor
   attr_reader :epub_stream
 
@@ -6,22 +7,35 @@ class PaperAttributesExtractor
   end
 
   def sync!(paper)
-    paper.update!(attributes)
+    paper.update!(attributes(paper))
   end
 
-  def attributes
+  def attributes(paper)
     @attributes ||= {
-      body: extract_file("body"),
-      title: extract_file("title"),
-      abstract: extract_file("abstract")
+      body: extract_body(paper),
+      title: extract_title(paper),
+      abstract: extract_file('abstract')
     }
   end
 
   private
 
+  def extract_body(paper)
+    title = extract_title(paper)
+    body = extract_file('body')
+
+    body.sub(title, "<h3 data-aperta-title='#{title}'></h3>") if title
+  end
+
+  def extract_title(paper)
+    paper.title unless paper.title.blank?
+    extract_file('title')
+  end
+
   def extract_file(filename)
-    TahiEpub::Zip.extract(stream: epub_stream, filename: filename).force_encoding("UTF-8")
-  rescue TahiEpub::FileNotFoundError => e
+    TahiEpub::Zip.extract(stream: epub_stream, filename: filename) \
+      .force_encoding('UTF-8')
+  rescue TahiEpub::FileNotFoundError
     nil # the filename doesn't exist in the response epub
   end
 end

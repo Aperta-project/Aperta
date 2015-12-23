ece332e12b1caf2c801a17adf0c94ce2c714dc98
Tad Park <tadp@users.noreply.github.com>
2015-12-22 16:39:44 -0800

    APERTA-5794 Don't show element div styling unless it should

--- client/app/pods/components/nested-question-textarea/component.js
+++ client/app/pods/components/nested-question-textarea/component.js
@@ -1,7 +1,7 @@
 import NestedQuestionComponent from 'tahi/pods/components/nested-question/component';
 export default NestedQuestionComponent.extend({
   placeholder: null,
-  displayContent: true,
+  displayContent: false,
   inputClassNames: ['form-control'],
 
   willUpdate() {

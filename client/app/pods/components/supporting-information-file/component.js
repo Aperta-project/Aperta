aa81a8d3eb2533718638290ed842850b1cd4fd7e
Sam Bleckley <sam@mutuallyhuman.com>
2015-12-30 16:24:27 -0500

    APERTA-5756 Delete state for SI files
    We had some concerns about the existing delete confirmation paradigm --
    namely, that the full-green overlay concealed *what* the user was
    deleting, and that it required a larger amount of space that was
    available in the new accordion view (the new SI file design also has
    less padding, etc).
    
    In this commit we propose a new delete confirmation, based on a mobile
    pattern of slide-y buttons.
    
    -- with AJ Hekman <aj@mutuallyhuman.com>

--- client/app/pods/components/supporting-information-file/component.js
+++ client/app/pods/components/supporting-information-file/component.js
@@ -2,6 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['si-file'],
+  classNameBindings: ['deleteState:si-file-grey'],
   file: null,
-  isEditable: false
+  isEditable: false,
+  deleteState: false,
+
+  actions: {
+    enterDeleteState: function() {
+      this.set('deleteState', true);
+    },
+
+    delete: function() {
+      this.get('file').destroyRecord();
+      this.set('deleteState', false);
+    },
+
+    cancelDelete: function() {
+      this.set('deleteState', false);
+    }
+  }
 });

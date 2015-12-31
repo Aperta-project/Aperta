8c1718492dfc2300c939d1b247453ead4968951b
AJ Hekman <aj@mutuallyhuman.com>
2015-12-31 14:14:34 -0500

    APERTA-5756 add edit state to SI file
    paired with Sam Bleckley <sam@mutuallyhuman.com>

--- client/app/pods/components/supporting-information-file/component.js
+++ client/app/pods/components/supporting-information-file/component.js
@@ -2,23 +2,44 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['si-file'],
-  classNameBindings: ['deleteState:si-file-grey'],
+  classNameBindings: ['siFileState'],
   file: null,
   isEditable: false,
+  uiState: 'edit', // view, edit, delete
   deleteState: false,
+  editState: false,
+
+  siFileState: Ember.computed('uiState', function(){
+    return 'si-file-' + this.get('uiState');
+  }),
 
   actions: {
     enterDeleteState: function() {
-      this.set('deleteState', true);
+      this.set('uiState', 'delete');
     },
 
     delete: function() {
       this.get('file').destroyRecord();
-      this.set('deleteState', false);
+      this.set('uiState', 'view');
     },
 
     cancelDelete: function() {
-      this.set('deleteState', false);
+      this.set('uiState', 'view');
+    },
+
+    enterEditState: function() {
+      console.log("ENTERING EDIT STATE");
+      this.set('uiState', 'edit');
+    },
+
+    cancelEdit: function(){
+      this.set('uiState', 'view');
+    },
+
+    saveEdit: function(){
+      this.set('uiState', 'view');
+      alert('NO OP');
     }
+
   }
 });

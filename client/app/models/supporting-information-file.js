a422cc762d319f0714aa9831f7ccc9ba3819bdcd
AJ Hekman <aj@mutuallyhuman.com>
2015-12-31 15:52:08 -0500

    APERTA-5756 add category field to SI model

--- client/app/models/supporting-information-file.js
+++ client/app/models/supporting-information-file.js
@@ -8,7 +8,7 @@ export default DS.Model.extend({
   src: DS.attr('string'),
   status: DS.attr('string'),
   title: DS.attr('string'),
-  caption: DS.attr('string'),
+  category: DS.attr('string'),
   label: DS.attr('string'),
   caption: DS.attr('string'),
   publishable: DS.attr('boolean')

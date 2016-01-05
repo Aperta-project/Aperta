d36eee6ee6e833454162a56076be8d5e37b21532
Hildebrando Rueda <harq.soft@gmail.com>
2016-01-05 10:37:12 -0500

    APERTA-5753 New attachment-manager component

---
+++ client/app/pods/components/attachment-manager/component.js
@@ -0,0 +1,45 @@
+import Ember from 'ember';
+import FileUpload from 'tahi/models/file-upload';
+
+export default Ember.Component.extend({
+  classNames: ['attachment-manager'],
+  description: 'Please select a file.',
+  buttonText: 'Upload File',
+  hasFile: false,
+  fileName: null,
+  fileType: null,
+  fileUpload: null,
+  uploadInProgress: Ember.computed.notEmpty('fileUpload'),
+
+  actions: {
+    replace() {
+      console.log('replace');
+    },
+
+    delete() {
+      console.log('delete');
+    },
+
+    fileAdded(file){
+      this.setProperties({ fileName: file.name, fileType: file.type });
+      this.set('fileUpload', FileUpload.create({ file: file }));
+    },
+
+    uploadProgress(data) {
+      this.get('fileUpload').setProperties({
+        dataLoaded: data.loaded,
+        dataTotal: data.total
+      });
+    },
+
+    uploadFinished(s3Url){
+     console.log('uploadFinished', s3Url);
+     this.set('hasFile', true);
+     this.set('fileUpload', null);
+    },
+
+    uploadFailed(reason){
+     console.log('uploadFailed', reason);
+    }
+  }
+});

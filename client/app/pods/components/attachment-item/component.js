3cc18116b8f8cbf4fcfea54a5cc7972e3d60ec66
Hildebrando Rueda <harq.soft@gmail.com>
2016-01-06 15:27:43 -0500

    APERTA-5753 Add new attachment-item component

--- client/app/pods/components/attachment-item/component.js
+++ client/app/pods/components/attachment-item/component.js
@@ -3,32 +3,43 @@ import FileUpload from 'tahi/models/file-upload';
 import extensionFont from 'tahi/lib/extension-font';
 
 export default Ember.Component.extend({
-  classNames: ['attachment-manager'],
-  description: 'Please select a file.',
-  buttonText: 'Upload File',
+  classNames: ['attachment-item'],
+  hasNote: false,
+  fileName: null,
   fileUpload: null,
+  note:null,
   uploadInProgress: Ember.computed.notEmpty('fileUpload'),
-  currentUpload: null,
-  multiple: false,
-  hasUploads: Ember.computed.notEmpty('attachments'),
-  showAddButton: Ember.computed('multiple', 'hasUploads', function() {
-    if (this.get('hasUploads') && !this.get('multiple')) return false;
-    return true;
+
+  fileTypeClass: Ember.computed('fileName', function(){
+    return extensionFont(this.get('fileName'));
   }),
 
   init() {
     this._super(...arguments);
     Ember.assert('Please provide filePath property', this.get('filePath'));
-    if (!this.get('attachments')) this.set('attachments', []);
   },
 
   actions: {
 
+    deleteFile() {
+      if (this.attrs.deleteFile) {
+        this.attrs.deleteFile();
+      }
+    },
+
+    noteChanged() {
+      if (this.attrs.noteChanged) {
+        this.attrs.noteChanged(this.get('note'));
+      }
+    },
+
     fileAdded(file){
-      this.setProperties({
-        fileUpload: FileUpload.create({ file: file }),
-        currentUpload: Ember.Object.create({fileName: file.name})
-      });
+      this.setProperties({ fileName: file.name,
+                           fileUpload: FileUpload.create({ file: file })});
+    },
+
+    triggerFileSelection() {
+      this.$().find('input').click();
     },
 
     uploadProgress(data) {
@@ -39,8 +50,7 @@ export default Ember.Component.extend({
     },
 
     uploadFinished(s3Url){
-      this.get('attachments').addObject(this.get('currentUpload'));
-      this.setProperties({fileUpload: null, currentUpload: null});
+      this.setProperties({fileUpload: null});
       if (this.attrs.uploadFinished) {
         this.attrs.uploadFinished(s3Url);
       }

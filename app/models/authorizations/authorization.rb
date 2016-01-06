6bae545879cc388d0b8afd3d539591a23232f502
Zach Dennis <zach.dennis@gmail.com>
2016-01-06 10:21:05 -0500

    Moving from compact module & class definitions to multiline ones
    This is to make the linter happy,

--- app/models/authorizations/authorization.rb
+++ app/models/authorizations/authorization.rb
@@ -1,10 +1,12 @@
-# Holds authorizations
-class Authorizations::Authorization
-  attr_reader :assignment_to, :authorizes, :via
+module Authorizations
+  # Holds authorizations
+  class Authorization
+    attr_reader :assignment_to, :authorizes, :via
 
-  def initialize(assignment_to:, authorizes:, via:)
-    @assignment_to = assignment_to
-    @authorizes = authorizes
-    @via = via
+    def initialize(assignment_to:, authorizes:, via:)
+      @assignment_to = assignment_to
+      @authorizes = authorizes
+      @via = via
+    end
   end
 end
--- app/models/authorizations/configuration.rb
+++ app/models/authorizations/configuration.rb
@@ -1,16 +1,18 @@
-# Configuration system for authorizations
-class Authorizations::Configuration
-  attr_accessor :authorizations
+module Authorizations
+  # Configuration system for authorizations
+  class Configuration
+    attr_accessor :authorizations
 
-  def initialize
-    @authorizations = []
-  end
+    def initialize
+      @authorizations = []
+    end
 
-  def assignment_to(assignment_to, authorizes:, via:)
-    @authorizations << Authorizations::Authorization.new(
-      assignment_to: assignment_to,
-      authorizes: authorizes,
-      via: via
-    )
+    def assignment_to(assignment_to, authorizes:, via:)
+      @authorizations << Authorizations::Authorization.new(
+        assignment_to: assignment_to,
+        authorizes: authorizes,
+        via: via
+      )
+    end
   end
 end

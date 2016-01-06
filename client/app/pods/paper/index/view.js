dc0deecfd19a20894ea0e075f475b1e0bdce379e
Matt Mercieca <mercieca@gmail.com>
2016-01-06 16:18:39 -0500

    APERTA-5840 Using MathJax to render equations in Chrome
    MathML support was removed from Chrome http://www.cnet.com/news/google-subtracts-mathml-from-chrome-and-anger-multiplies/ .

--- client/app/pods/paper/index/view.js
+++ client/app/pods/paper/index/view.js
@@ -1,4 +1,37 @@
 import Ember from 'ember';
 import PaperIndexMixin from 'tahi/mixins/views/paper-index';
+import LazyLoader from 'ember-cli-lazyloader/lib/lazy-loader';
+import ENV from 'tahi/config/environment';
 
-export default Ember.View.extend(PaperIndexMixin);
+export default Ember.View.extend(PaperIndexMixin, {
+  renderEquations: function() {
+    return navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+  },
+
+  didInserElement: function() {
+    this.loadMathJax();
+  },
+
+  didRender: function() {
+    this.refreshEquations();
+  },
+
+  loadMathJax: function() {
+    if (this.renderEquations) {
+      LazyLoader.loadScripts([ENV.mathjax.url]).then(() => {
+        this.refreshEquations();
+      });
+    }
+  },
+
+  refreshEquations:  function() {
+    if (!this.renderEquations) { return; }
+    else if (!window.MathJax) { this.loadMathJax(); return; }
+    else if (!window.MathJax.Hub) { return; }
+
+    Ember.run.next(() => {
+      MathJax.Hub.Queue(['Typeset', MathJax.Hub, this.$()[0]]);
+    });
+  }
+
+});

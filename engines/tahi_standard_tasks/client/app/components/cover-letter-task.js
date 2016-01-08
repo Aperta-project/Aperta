28e8ac86ee31f4104f743db588a59558d6941f07
Hildebrando Rueda <harq.soft@gmail.com>
2016-01-08 17:32:38 -0500

    APERTA-5769 Add attachment manager to cover letter

--- engines/tahi_standard_tasks/client/app/components/cover-letter-task.js
+++ engines/tahi_standard_tasks/client/app/components/cover-letter-task.js
@@ -2,6 +2,8 @@ import TaskComponent from 'tahi/pods/components/task-base/component';
 import Ember from 'ember';
 
 export default TaskComponent.extend({
+  restless: Ember.inject.service('restless'),
+
   letterBody: Ember.computed('task.body', function() {
     return this.get('task.body')[0];
   }),
@@ -14,6 +16,23 @@ export default TaskComponent.extend({
     saveCoverLetter() {
       this.set('task.body', [this.get('letterBody')]);
       this.get('task').save();
+    },
+
+    uploadFinished(s3Url, file) {
+      const store = this.container.lookup('store:main');
+      const path = `/api/tasks/${this.get('task.id')}/attachments`;
+      this.get('restless').post(path, {url: s3Url}).then((response) => {
+        response.attachment.filename = file.name
+        store.pushPayload(response);
+      });
+    },
+
+    deleteAttachment(attachment){
+      attachment.destroyRecord();
+    },
+
+    noteChanged(note){
+      console.log(note);
     }
   }
 });

e8fd661af6ed054f2edf92c3f7248ad19bdb054e
Matt Mercieca <mercieca@gmail.com>
2016-01-12 16:11:51 -0500

    APERTA-5835 Moving notification of mention email out of model and in to controller

--- app/controllers/discussion_replies_controller.rb
+++ app/controllers/discussion_replies_controller.rb
@@ -9,11 +9,13 @@ class DiscussionRepliesController < ApplicationController
 
   def create
     discussion_reply.save
+    notify_mentioned_people
     respond_with(discussion_reply)
   end
 
   def update
     discussion_reply.update(update_params)
+    notify_mentioned_people
     respond_with(discussion_reply)
   end
 
@@ -45,5 +47,15 @@ class DiscussionRepliesController < ApplicationController
 
   def enforce_policy
     authorize_action!(discussion_reply: discussion_reply)
+  end
+
+  def notify_mentioned_people
+    people_mentioned = UserMentions.new(discussion_reply.body,
+                                        current_user).people_mentioned
+    people_mentioned.each do |mentionee|
+      UserMailer.notify_mention_in_discussion(mentionee.id, discussion_topic.id,
+                                              id)
+        .deliver_later
+    end
   end
 end
--- app/models/discussion_reply.rb
+++ app/models/discussion_reply.rb
@@ -3,15 +3,4 @@ class DiscussionReply < ActiveRecord::Base
 
   belongs_to :discussion_topic, inverse_of: :discussion_replies
   belongs_to :replier, inverse_of: :discussion_replies, class_name: 'User'
-
-  after_save :notify_mentioned_people
-
-  def notify_mentioned_people
-    people_mentioned = UserMentions.new(body, replier).people_mentioned
-    people_mentioned.each do |mentionee|
-      UserMailer.notify_mention_in_discussion(mentionee.id, discussion_topic.id,
-                                              id)
-        .deliver_later
-    end
-  end
 end

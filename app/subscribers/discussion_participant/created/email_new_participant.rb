e6966b7e2efd36705ed9c749667fc88e6f8ecc01
Matt Mercieca <mercieca@gmail.com>
2016-01-14 15:56:35 -0500

    APERTA-5835 Method name fix

--- app/subscribers/discussion_participant/created/email_new_participant.rb
+++ app/subscribers/discussion_participant/created/email_new_participant.rb
@@ -3,8 +3,7 @@
 class DiscussionParticipant::Created::EmailNewParticipant
   def self.call(_event_name, event_data)
     participant = event_data[:record]
-    UserMailer.delay
-      .notify_user_added_to_topic(participant.user_id,
-                                  participant.discussion_topic_id)
+    UserMailer.delay.notify_added_to_topic(participant.user_id,
+                                           participant.discussion_topic_id)
   end
 end
--- spec/subscribers/discussion_participant/created/email_new_participant_spec.rb
+++ spec/subscribers/discussion_participant/created/email_new_participant_spec.rb
@@ -7,7 +7,7 @@ describe DiscussionParticipant::Created::EmailNewParticipant do
   let!(:participant) { FactoryGirl.create(:discussion_participant) }
 
   it 'sends an email to people added to discussions' do
-    expect(mailer).to receive(:notify_user_added_to_topic)
+    expect(mailer).to receive(:notify_added_to_topic)
       .with(participant.user_id, participant.discussion_topic_id)
     described_class.call('tahi:discussion_participant:created',
                          record: participant)

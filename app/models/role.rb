fb7b3e88599fe485aa1f1897d82f7883d4d6d3d6
Erik Hetzner <ehetzner@plos.org>
2016-01-15 10:16:39 -0800

    Add roles/permission seed helpers
    Helpers to easily create new roles and permissions, which should be
    especially useful for seeding the database.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,19 @@
 class Role < ActiveRecord::Base
   belongs_to :journal
   has_and_belongs_to_many :permissions
+
+  def self.ensure(name, journal: nil, participates_in: [])
+    role = Role.where(name: name, journal: journal).first_or_create!
+
+    # Ensure user passed in valid participates_in
+    whitelist = [Task, Paper]
+    fail StandardError, "Bad participates_in: #{participates_in}" unless \
+      ((whitelist | participates_in) == whitelist)
+
+    participates_in.each do |klass|
+      role.update("participates_in_#{klass.to_s.downcase.pluralize}" => true)
+    end
+    yield(role) if block_given?
+    role
+  end
 end

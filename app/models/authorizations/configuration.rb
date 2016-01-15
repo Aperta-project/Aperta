81eb35a5c13b48ebf9714f81f0a326c66682e8a9
Erik Hetzner <ehetzner@plos.org>
2016-01-14 21:44:00 -0800

    Ensure that authorization configuration persists
    Previously, if rails did its code reload magic, authorization
    configuration would be lost. This change ensures that it sticks around.

--- app/models/authorizations/configuration.rb
+++ app/models/authorizations/configuration.rb
@@ -1,24 +1,26 @@
 module Authorizations
   # Configuration houses the individual Authorization(s) being used to
   # configure the authorization sub-system.
-  class Configuration
+  module Configuration
+    extend self
 
     # 'authorizations' returns the collection of authorization(s) that have \
     # been configured
     attr_accessor :authorizations
 
-    def initialize
-      @authorizations = []
-    end
-
     # Creates an authorization thru the given assignment_to object and
     # options.
     def assignment_to(assignment_to, authorizes:, via:)
+      @authorizations ||= []
       @authorizations << Authorizations::Authorization.new(
         assignment_to: assignment_to,
         authorizes: authorizes,
         via: via
       )
+    end
+
+    def reset
+      @authorizations = []
     end
   end
 end

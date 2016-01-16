8877d7f8e98312354621ef8a41340066ceb91506
Erik Hetzner <ehetzner@plos.org>
2016-01-15 16:32:51 -0800

    Change Permission.ensure to use states properly.
    a permission with a different set of states is a different permission.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -3,13 +3,18 @@ class Permission < ActiveRecord::Base
   has_and_belongs_to_many :states, class_name: 'PermissionState'
 
   def self.ensure(action, applies_to:, role: nil, states: ['*'])
-    perm = Permission.where(action: action, applies_to: applies_to.to_s)
+    permission_states = states.map do |state_name|
+      PermissionState.where(name: state_name).first_or_create!
+    end
+    permission_states_ids = permission_states.map(&:id)
+    perm = Permission.includes(:states).where(action: action,
+                                              applies_to: applies_to.to_s,
+                                              permission_states: {
+                                                id: permission_states_ids
+                                              })
            .first_or_create!
+    perm.states = permission_states
     role.permissions = (role.permissions | [perm]) unless role.nil?
-    perm.states = (perm.states |
-                   states.map do |state_name|
-                     PermissionState.where(name: state_name).first_or_create!
-                   end)
     perm
   end
 end

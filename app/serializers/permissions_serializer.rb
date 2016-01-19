842fd71b1a46c92c849422e19a67de3ad4c6b3d3
Zach Dennis <zach.dennis@gmail.com>
2016-01-19 10:32:45 -0500

    Updating code comment reference.
    Additional note: this is because ember-data expects resources on the server to have numeric IDs by default otherwise it gets angry.
    
    Rather than work against the grain right now we're working with it. This may change in upcoming work if this has cause other issues. Perhaps to not use ember-data for permissions on the client-side (maybe restless) and then we could get rid of a fake id.
    
    [APERTA-5538]

--- app/serializers/permissions_serializer.rb
+++ app/serializers/permissions_serializer.rb
@@ -6,7 +6,7 @@ class PermissionsSerializer < ActiveModel::Serializer
 
   # This is needed to allow the permissions to side load. Works in
   # conjuction with `has_one :permissions, include: true, embed: :ids` on the
-  # controller's serializer
+  # CurrentUserSerializer
   def id
     1
   end

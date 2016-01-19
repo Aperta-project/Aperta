f104089955769f295a2bbb8fcab96f02ed58360f
Erik Hetzner <ehetzner@plos.org>
2016-01-18 22:54:51 -0800

    {Role,Permission}::ensure -> ensure_exists
    Role#ensure_permission -> Role#ensure_permission_exists

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,7 +2,7 @@ class Permission < ActiveRecord::Base
   has_and_belongs_to_many :roles
   has_and_belongs_to_many :states, class_name: 'PermissionState'
 
-  def self.ensure(action, applies_to:, role: nil, states: ['*'])
+  def self.ensure_exists(action, applies_to:, role: nil, states: ['*'])
     permission_states = states.map do |state_name|
       PermissionState.where(name: state_name).first_or_create!
     end

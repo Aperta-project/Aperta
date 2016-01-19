a3c8736a24e96f242bca93d83022578dcc23729b
Zach Dennis <zach.dennis@gmail.com>
2016-01-19 09:36:49 -0500

    Updating permissions_serializer to fix its spec
    [APERTA-5538]

--- app/serializers/permissions_serializer.rb
+++ app/serializers/permissions_serializer.rb
@@ -1,7 +1,7 @@
 # Serializer for Roles and Permissions
 class PermissionsSerializer < ActiveModel::Serializer
   def serializable_hash
-    object.to_h.merge id: id
+    object.to_h.merge(id: id).as_json
   end
 
   # This is needed to allow the permissions to side load. Works in

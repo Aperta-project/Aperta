f71348dfeaf5c418e7b7ba101e0c4cf104cfe0fd
Sara Gibbons <sara@mutuallyhuman.com>
2016-01-20 10:40:54 -0500

    APERTA-5828: allow for a nested_question_answer to have many attachments

--- app/models/question_attachment.rb
+++ app/models/question_attachment.rb
@@ -1,9 +1,13 @@
 class QuestionAttachment < ActiveRecord::Base
   include EventStream::Notifiable
 
-  belongs_to :nested_question_answer, inverse_of: :attachment
+  belongs_to :nested_question_answer, inverse_of: :attachments
 
   mount_uploader :attachment, QuestionAttachmentUploader
+
+  def filename
+    self[:attachment]
+  end
 
   def paper
     nested_question_answer.owner.try(:paper)
--- app/serializers/question_attachment_serializer.rb
+++ app/serializers/question_attachment_serializer.rb
@@ -1,10 +1,6 @@
 class QuestionAttachmentSerializer < ActiveModel::Serializer
   attributes :id, :title, :status, :filename, :src
 
-  def filename
-    object[:attachment]
-  end
-
   def src
     object.attachment.url
   end

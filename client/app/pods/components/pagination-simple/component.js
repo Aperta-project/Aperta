50cbf37383c020bee74a835ff9871920ed9dd535
Benjamin Moses Lieb <contact@pixelearth.net>
2016-01-20 16:04:10 -0800

    APERTA-5861 pagination initial implementation and controller spec

---
+++ client/app/pods/components/pagination-simple/component.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  page: null,
+  totalCount: null, // should be overwritten in route w server meta data
+  perPage: null,   // should be overwritten in route w server meta data
+  setPage() {},  // pass in as closure action
+
+  init() {
+    this._super(...arguments);
+    this.set('page', parseInt(this.get('page'))); // params come in as str
+  },
+
+  pages: Ember.computed(function(){
+    return Math.ceil(this.get('totalCount') / this.get('perPage'))
+  }),
+
+  pagesUI: Ember.computed(function(){
+    if (this.get('totalCount') == 0) { return '?'; }
+    return this.get('totalCount');
+  }),
+
+  hasPrev: Ember.computed(function(){
+    return this.get('page') > 1
+  }),
+
+  hasNext: Ember.computed(function(){
+    return this.get('page') < this.get('pages')
+  }),
+
+  actions: {
+    prev() {
+      this.setPage(this.get('page') - 1);
+    },
+
+    next() {
+      this.setPage(this.get('page') + 1);
+    }
+  }
+});
--- client/app/pods/paper-tracker/index/controller.js
+++ client/app/pods/paper-tracker/index/controller.js
@@ -1,3 +1,14 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend();
+export default Ember.Controller.extend({
+  queryParams: ['page'],
+  page: null,
+  perPage: null,
+  totalCount: null,
+
+  actions: {
+    setPage(page) {
+      this.set('page', page); // triggers route reload
+    }
+  }
+});

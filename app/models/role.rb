9590114ec7bc623d2f23c4552411bfa3a508e5dd
Erik Hetzner <ehetzner@plos.org>
2016-01-21 10:27:30 -0800

    APERTA-3399 Add delete_stray_permissions flag
    Add delete_stray_permissions flag to Role.ensure_exists. This flag can
    be used to ensure that stray permissions are deleted when setting up a
    role's permissions.

--- app/models/role.rb
+++ app/models/role.rb
@@ -23,7 +23,10 @@ class Role < ActiveRecord::Base
     where(name: STAFF_ADMIN_ROLE).first_or_create!
   end
 
-  def self.ensure_exists(name, journal: nil, participates_in: [], &block)
+  def self.ensure_exists(name, journal: nil,
+                               participates_in: [],
+                               delete_stray_permissions: false,
+                         &block)
     role = Role.where(name: name, journal: journal).first_or_create!
 
     # Ensure user passed in valid participates_in
@@ -34,12 +37,31 @@ class Role < ActiveRecord::Base
     participates_in.each do |klass|
       role.update("participates_in_#{klass.to_s.downcase.pluralize}" => true)
     end
+
+    role.send(:reset_tracked_permissions)
     yield(role) if block_given?
+    role.send(:delete_stray_permissions) if delete_stray_permissions
     role
   end
 
   def ensure_permission_exists(action, applies_to:, states: ['*'])
-    Permission.ensure_exists(action, applies_to: applies_to, role: self,
-                              states: states)
+    perm = Permission.ensure_exists(action, applies_to: applies_to, role: self,
+                                            states: states)
+    @ensured_permission_ids << perm.id
+    perm
+  end
+
+  private
+
+  def reset_tracked_permissions
+    @ensured_permission_ids = []
+  end
+
+  def delete_stray_permissions
+    fail StandardError, "Role.ensure_exists called with
+delete_stray_permissions, but no permissions created." \
+                        if @ensured_permission_ids.blank?
+    permissions.delete(permissions.where.not(id: @ensured_permission_ids))
+    reset_tracked_permissions
   end
 end

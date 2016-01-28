7a1bbeb3591501be2fff09945cce07724149c008
Sam Bleckley <sam@mutuallyhuman.com>
2016-01-28 10:49:31 -0800

    Query language: building queries

--- app/services/query_parser.rb
+++ app/services/query_parser.rb
@@ -7,41 +7,59 @@ class QueryParser
   extend QueryLanguageParser
   extend Rsec::Helpers
 
+  def self.join(klass)
+    unless @joins.include? klass
+      @root = @root
+              .join(klass.arel_table)
+              .on(klass.arel_table[:paper_id].eq(Paper.arel_table[:id]))
+      @joins.push klass
+    end
+  end
+
   add_simple_expression(keyword: 'STATUS IS NOT') do |status|
-    ParsedQuery.new(not_status: status)
+    Paper.arel_table[:publishing_state].not_eq(status)
   end
 
   add_simple_expression(keyword: 'STATUS IS') do |status|
-    ParsedQuery.new(status: status)
+    Paper.arel_table[:publishing_state].eq(status)
   end
 
   add_simple_expression(keyword: 'TYPE IS NOT') do |type|
-    ParsedQuery.new(not_type: type)
+    Paper.arel_table[:paper_type].not_eq(type)
   end
 
   add_simple_expression(keyword: 'TYPE IS') do |type|
-    ParsedQuery.new(type: type)
+    Paper.arel_table[:paper_type].eq(type)
   end
 
   add_expression(keywords: ['TITLE IS']) do |_|
     symbol('TITLE IS') >> /.*/.r.map do |title|
-      ParsedQuery.new(title: title)
+      Paper.arel_table[:title].eq(title)
     end
   end
 
   add_simple_expression(keyword: 'DECISION IS NOT') do |decision|
-    ParsedQuery.new(not_decision: decision)
+    join Decision
+    Decision.arel_table[:verdict].not_eq(decision)
   end
 
   add_simple_expression(keyword: 'DECISION IS') do |decision|
-    ParsedQuery.new(decision: decision)
+    join Decision
+    Decision.arel_table[:verdict].eq(decision)
   end
 
   add_simple_expression(keyword: 'DOI IS') do |doi|
-    ParsedQuery.new(doi: doi)
+    Paper.arel_table[:doi].eq(doi)
   end
 
-  add_statement(/^\d+/.r.map { |d| ParsedQuery.new(doi: d) })
+  add_statement(/^\d+/.r.map { Paper.arel_table[:doi].eq(doi) })
 
-  add_statement(/^.+/.r.map { |t| ParsedQuery.new(title: t) })
+  add_statement(/^.+/.r.map { Paper.arel_table[:title].eq(title) })
+
+  def self.build(str)
+    @joins = []
+    @root = Paper.arel_table
+    query = parse str
+    @root.where(query)
+  end
 end

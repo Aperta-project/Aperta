12ae31751bd995052185f3756072071bb80ebf94
AJ Hekman <aj@mutuallyhuman.com>
2016-01-28 11:30:52 -0500

    APERTA-5371 add side_load helper for serializers

---
+++ app/helpers/sideloadable_serializer_helper.rb
@@ -0,0 +1,48 @@
+# This helper method makes it easy to side load permissions (or other
+# resources) into the JSON response.
+# Usage:
+#   side_load :example_method_to_side_load
+#
+#   # The key in the sideloaded JSON will match method name
+#   def example_method_to_side_load
+#      return {example_property: 'example_key'}
+#   end
+#
+#  JSON Returned from serializer:
+#  {example_method_to_side_load: {example_property: 'example_key'} }
+module SideloadableSerializerHelper
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  # Included class methods
+  module ClassMethods
+    def side_load(side_load_source)
+      to_side_load(side_load_source)
+    end
+
+    def to_side_load(side_load_source = nil)
+      @to_side_load ||= []
+      if side_load_source
+        @to_side_load << side_load_source
+        return nil
+      else
+        return @to_side_load
+      end
+    end
+  end
+
+  def as_json(*args)
+    hash = super(*args)
+    hash.merge(side_loader)
+  end
+
+  def side_loader
+    side_loading = self.class.to_side_load
+    result = {}
+    side_loading.each do |method|
+      result[method] = send(method).as_json
+    end
+    result
+  end
+end
--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -1,13 +1,11 @@
 class CurrentUserSerializer < ActiveModel::Serializer
+  include SideloadableSerializerHelper
+
   has_many :affiliations, include: true, embed: :ids
   attributes :id, :full_name, :first_name, :avatar_url, :username,
              :email, :site_admin
 
-  def as_json(*args)
-    hash = super(*args)
-    hash[:permissions] = permissions.as_json
-    hash
-  end
+  side_load :permissions
 
   def permissions
     object.filter_authorized(:view_profile, object).serializable

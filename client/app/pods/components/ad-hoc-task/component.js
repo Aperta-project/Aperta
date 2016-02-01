b78fb1e935f07d6f5bc6c0b55afe45a13d717544
Hildebrando Rueda <harq.soft@gmail.com>
2016-02-01 17:32:50 -0500

    APERTA-5930 Replace file-uploader component

--- client/app/pods/components/ad-hoc-task/component.js
+++ client/app/pods/components/ad-hoc-task/component.js
@@ -1,15 +1,10 @@
 import TaskComponent from 'tahi/pods/components/task-base/component';
 import Ember from 'ember';
 import BuildsTaskTemplate from 'tahi/mixins/controllers/builds-task-template';
-import FileUploadMixin from 'tahi/mixins/file-upload';
 
-export default TaskComponent.extend(BuildsTaskTemplate, FileUploadMixin, {
-  restless: Ember.inject.service('restless'),
+export default TaskComponent.extend(BuildsTaskTemplate, {
+  restless: Ember.inject.service(),
   blocks: Ember.computed.alias('task.body'),
-
-  imageUploadUrl: Ember.computed('task.id', function() {
-    return '/api/tasks/' + this.get('task.id') + '/attachments';
-  }),
 
   actions: {
     setTitle(title) {
@@ -48,14 +43,16 @@ export default TaskComponent.extend(BuildsTaskTemplate, FileUploadMixin, {
       attachment.destroyRecord();
     },
 
-    uploadFinished(data, filename) {
+    uploadFinished(s3Url){
       const store = this.container.lookup('store:main');
+      const path = `/api/tasks/${this.get('task.id')}/attachments`;
+      this.get('restless').post(path, {url: s3Url}).then((response) => {
+        store.pushPayload(response);
+      });
+    },
 
-      this.uploadFinished(data, filename);
-      store.pushPayload('attachment', data);
-
-      const attachment = store.getById('attachment', data.attachment.id);
-      this.get('task.attachments').pushObject(attachment);
-    }
+    uploadFailed(reason) {
+      console.log(reason);
+    },
   }
 });

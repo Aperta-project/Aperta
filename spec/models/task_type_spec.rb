da9dac4832d40c878c461d31f9702258b0e1bc03
Zach Dennis <zach.dennis@gmail.com>
2016-02-01 16:12:31 -0500

    Updating task_type_spec to pull up sample class into the top-level describe.
    * rename the sample class to be longer since it gets added to the global namespace.
    
    [APERTA-5629]

--- spec/models/task_type_spec.rb
+++ spec/models/task_type_spec.rb
@@ -1,46 +1,39 @@
 require 'rails_helper'
 
 describe TaskType do
+  class SampleTaskForTestingTaskType ; end
+
   after do
-    TaskType.deregister(SampleTask)
+    TaskType.deregister(SampleTaskForTestingTaskType)
   end
 
   describe ".register" do
-    before do
-      class SampleTask; end
-    end
-
     it "will add to the list of task types" do
-      TaskType.register(SampleTask, "title", "old_role")
-      expect(TaskType.types.keys).to include("SampleTask")
+      TaskType.register(SampleTaskForTestingTaskType, "title", "old_role")
+      expect(TaskType.types.keys).to include("SampleTaskForTestingTaskType")
     end
   end
 
   describe ".deregister" do
     before do
-      class SampleTask; end
-    end
-
-    before do
-      TaskType.register(SampleTask, "title", "old_role")
+      TaskType.register(SampleTaskForTestingTaskType, "title", "old_role")
     end
 
     it "will remove the class from the list of registered task types" do
-      TaskType.deregister(SampleTask)
-      expect(TaskType.types.keys).to_not include("SampleTask")
+      TaskType.deregister(SampleTaskForTestingTaskType)
+      expect(TaskType.types.keys).to_not include("SampleTaskForTestingTaskType")
     end
   end
 
   describe ".constantize!" do
-
     context "with a registered class" do
       before do
-        class SampleTask; end
-        TaskType.register(SampleTask, "title", "old_role")
+        TaskType.register(SampleTaskForTestingTaskType, "title", "old_role")
       end
 
       it "constantizes" do
-        expect(TaskType.constantize!("SampleTask")).to eq(SampleTask)
+        expect(TaskType.constantize!("SampleTaskForTestingTaskType"))
+          .to eq(SampleTaskForTestingTaskType)
       end
     end
 

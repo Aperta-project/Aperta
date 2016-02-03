0d69bbdb330b0874617e98db23ddf99fba916509
Jerry Nummi <jerry@mutuallyhuman.com>
2016-02-03 15:43:43 -0500

    Move billing validations into file

--- engines/plos_billing/client/app/components/billing-task.js
+++ engines/plos_billing/client/app/components/billing-task.js
@@ -1,44 +1,13 @@
 import TaskComponent from 'tahi/pods/components/task-base/component';
 import Ember from 'ember';
 import DATA from 'tahi/plos-billing-form-data';
+import validations from 'tahi/billing-task-validations';
 
 const { computed } = Ember;
 
-const pfaCheck = function(task) {
-  return task.responseToQuestion('plos_billing--payment_method') !== 'pfa';
-};
-
-const PFA_VALIDATION = {
-  type: 'number',
-  allowBlank: true,
-  onlyInteger: true,
-  message: `Must be a number and contain no symbols,
-            or letters, e.g. $1,000.00 should be written 1000`,
-  skipCheck(key) {
-    const task      = this.get('task');
-    const notPFA    = pfaCheck(task);
-    const notActive = !(task.responseToQuestion(key));
-    return notPFA || notActive;
-  }
-};
 
 export default TaskComponent.extend({
-  validations: {
-    'plos_billing--first_name':        ['presence'],
-    'plos_billing--last_name':         ['presence'],
-    'plos_billing--department':        ['presence'],
-    'plos_billing--affiliation1':      ['presence'],
-    'plos_billing--phone_number':      ['presence'],
-    'plos_billing--email':             ['presence'],
-    'plos_billing--address1':          ['presence'],
-    'plos_billing--city':              ['presence'],
-    'plos_billing--postal_code':       ['presence'],
-    'plos_billing--pfa_question_1b':   [PFA_VALIDATION],
-    'plos_billing--pfa_question_2b':   [PFA_VALIDATION],
-    'plos_billing--pfa_question_3a':   [PFA_VALIDATION],
-    'plos_billing--pfa_question_4a':   [PFA_VALIDATION],
-    'plos_billing--pfa_amount_to_pay': [PFA_VALIDATION],
-  },
+  validations: validations,
 
   init() {
     this._super(...arguments);

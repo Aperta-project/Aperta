ce6ecbc3b236c844bd70f1abf163c13955c63947
Jerry Nummi <jerry@mutuallyhuman.com>
2016-02-03 16:25:30 -0500

    Set `this` on generateErrorMessage

--- client/app/lib/validator.js
+++ client/app/lib/validator.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
+const { isEmpty } = Ember;
 
 // 1. Import validation:
 
 import presence from 'tahi/lib/validations/presence';
 import number   from 'tahi/lib/validations/number';
-
-const { isEmpty } = Ember;
 
 // 2. Add imported validation to TYPES:
 
@@ -41,16 +40,16 @@ export default {
 
     return _.compact(
       _.map(validations, function(validation) {
-        // if validation is defined as key: ['name']
+        // if validation is defined as { key: ['name'] }
         if(typeof validation === 'string') {
           const pass = TYPES[validation](value);
           if(!pass) {
-            return _generateErrorMessage(validation);
+            return _generateErrorMessage.call(context, validation);
           }
         }
 
         // if validation is defined with options:
-        // key: [{type: 'name', ...}]
+        // { key: [{type: 'name', ...}] }
         if(typeof validation === 'object') {
           const options = _.clone(validation);
           const type = options.type;

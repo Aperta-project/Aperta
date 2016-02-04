b3500cd1152a74adcb7c1684aedb5f884e0a71d8
Hildebrando Rueda <harq.soft@gmail.com>
2016-02-04 15:16:30 -0500

    APERTA-5900 Update billing task tests

--- engines/plos_billing/spec/features/billing_spec.rb
+++ engines/plos_billing/spec/features/billing_spec.rb
@@ -1,35 +1,39 @@
 require 'rails_helper'
 
-feature "Billing feature", js: true do
-  let(:user) { FactoryGirl.create :user }
+feature 'Billing Task', js: true do
+  before do
+    user  = create :user, :site_admin
+    paper = create :paper_with_task,
+                   creator: user,
+                   task_params: { title: 'Billing',
+                                  type: 'PlosBilling::BillingTask',
+                                  old_role: 'author' }
+    login_as user
+    visit "/papers/#{paper.id}/tasks/#{paper.tasks.first.id}"
+  end
 
-  context "with a billing task" do
+  it 'shows validations', vcr: { cassette_name: 'ned_countries' } do
+    p = PageFragment.new(find('.overlay'))
+    p.select2('PLOS Publication Fee Assistance Program (PFA)',
+              css: '.payment-method')
 
-    before do
-      @user  = FactoryGirl.create :user, :site_admin
-      @paper = FactoryGirl.create :paper_with_task, creator: @user, task_params: { title: "Billing", type: "PlosBilling::BillingTask", old_role: "author" }
-      login_as @user
-      visit "/papers/#{@paper.id}/tasks/#{@paper.tasks.first.id}"
-    end
+    within('.question-dataset') do
+      find("input[id*='plos_billing--pfa_question_1-yes']").click
+      find("input[id*='plos_billing--pfa_question_2-yes']").click
+      find("input[id*='plos_billing--pfa_question_3-yes']").click
+      find("input[id*='plos_billing--pfa_question_4-yes']").click
 
-    it "shows validations", vcr: { cassette_name: "ned_countries" } do
-      expect(find(".task-completed")[:disabled]).to be(nil) # not disabled at start
+      # numeric fields
+      idents = ['plos_billing--pfa_question_1b',
+                'plos_billing--pfa_question_2b',
+                'plos_billing--pfa_question_3a',
+                'plos_billing--pfa_question_4a',
+                'plos_billing--pfa_amount_to_pay']
 
-      p = PageFragment.new(find('.overlay'))
-      p.select2("PLOS Publication Fee Assistance Program (PFA)", css: '.payment-method')
-
-      within(".question-dataset") do
-        find("input[id*='plos_billing--pfa_question_1-yes']").click
-        find("input[id*='plos_billing--pfa_question_2-yes']").click
-        find("input[id*='plos_billing--pfa_question_3-yes']").click
-        find("input[id*='plos_billing--pfa_question_4-yes']").click
-
-        # numeric fields
-        ['plos_billing--pfa_question_1b', 'plos_billing--pfa_question_2b', 'plos_billing--pfa_question_3a', 'plos_billing--pfa_question_4a', 'plos_billing--pfa_amount_to_pay'].each do |ident|
-          find("input[name*='#{ident}']").set 'foo'
-          page.execute_script("$(\"input[name*='#{ident}']\").blur()")
-          expect(find("#error-for-#{ident}")).to have_content("Must be a number and contain no symbols, or letters")
-        end
+      idents.each do |ident|
+        find("input[name*='#{ident}']").set 'foo'
+        page.execute_script("$(\"input[name*='#{ident}']\").blur()")
+        expect(find("#error-for-#{ident}")).to have_content('Must be a number')
       end
     end
   end

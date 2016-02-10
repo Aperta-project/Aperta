c6954d0726f36496cfcbd37ee1276799ec5a30b6
Erik Hetzner <ehetzner@plos.org>
2016-02-10 15:26:38 -0800

    APERTA-3399 Fix bad merge in task_role_updater

--- engines/tahi_standard_tasks/app/services/task_role_updater.rb
+++ engines/tahi_standard_tasks/app/services/task_role_updater.rb
@@ -11,35 +11,17 @@ class TaskRoleUpdater
   def update
     paper.transaction do
       # only one `assignee` can exist on `paper` with the same `paper_role_name`
+      # New
+      role = Role.for_old_role(paper_role_name, paper: paper)
+      paper.assignments.where(role: role).destroy_all
+      paper.assignments.create!(user: assignee, role: role)
+
+      # Old
       paper.paper_roles.for_old_role(paper_role_name).destroy_all
       paper.paper_roles.for_old_role(paper_role_name).create!(user: assignee)
       paper.tasks.for_old_role(paper_role_name).incomplete.each do |task|
         ParticipationFactory.create(task: task, assignee: assignee)
       end
     end
-  end
-
-  private
-
-  def assign_related_tasks
-    related_tasks.each do |task|
-      ParticipationFactory.create(task: task, assignee: assignee)
-    end
-  end
-
-  # only one `assignee` can exist on `paper` with the same `paper_role_name`
-  def assign_paper_role!
-    # New
-    role = Role.for_old_role(paper_role_name, paper: paper)
-    paper.assignments.where(role: role).destroy_all
-    paper.assignments.create!(user: assignee, role: role)
-
-    # Old
-    paper.paper_roles.for_old_role(paper_role_name).destroy_all
-    paper.paper_roles.for_old_role(paper_role_name).create!(user: assignee)
-  end
-
-  def related_tasks
-    paper.tasks.for_old_role(paper_role_name).incomplete
   end
 end

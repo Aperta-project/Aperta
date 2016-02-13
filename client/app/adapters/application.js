ab4df1f1e8f2acdbe16ad5a923375bdd689043d2
Zach Dennis <zach.dennis@gmail.com>
2016-02-13 15:03:10 -0500

    Update the application adapter for ember-data to override ajaxError.
    This allows to ensure that 403 errors from the server don't break ember. This is not handled by the instance-initializers/error-handler.js as this uses the DS.ActiveModelAdapter which provides its own ajaxError implementation.
    
    APERTA-5634

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -7,5 +7,14 @@ export default DS.ActiveModelAdapter.extend({
       namespace: 'api',
       'PUSHER_SOCKET_ID': this.get('container').lookup('pusher:main').get('socketId')
     };
-  }.property().volatile()
+  }.property().volatile(),
+
+  ajaxError: function(event, jqXHR, ajaxSettings, thrownError) {
+    let status     = jqXHR.status;
+
+    // don't blow up in case of a 403 from rails
+    if (status === 403) { return; }
+
+    return this._super(...arguments);
+  }
 });

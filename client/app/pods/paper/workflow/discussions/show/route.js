9f50e777dbada7f7b3c715dab80674a16a0336c2
Sara Gibbons <sara@mutuallyhuman.com>
2016-02-17 15:34:42 -0500

    change workflow discussion routes to authorize for new permissions [APERTA-5627]

--- client/app/pods/paper/workflow/discussions/show/route.js
+++ client/app/pods/paper/workflow/discussions/show/route.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 import DiscussionsShowRouteMixin from 'tahi/mixins/discussions/show/route';
+import AuthorizedRoute from 'tahi/routes/authorized';
 
-export default Ember.Route.extend(DiscussionsShowRouteMixin, {
+export default AuthorizedRoute.extend(DiscussionsShowRouteMixin, {
   // required to generate route paths:
-  subRouteName: 'workflow'
+  subRouteName: 'workflow',
+  can: Ember.inject.service('can'),
+
+  afterModel(topic, transition){
+    return this.get('can').can('view', topic).then( (value)=> {
+      if (!value){
+        return this.handleUnauthorizedRequest(transition);
+      }
+      this.setModelChannel(topic);
+    });
+  },
 });

dab8ea12d84e9e15d4d3851ae34d52a88ea372a6
Sara Gibbons <sara@mutuallyhuman.com>
2016-02-17 10:13:46 -0500

    for DiscussionParticipant display participants without delete or add abilities [APERTA-5627]

--- client/app/pods/components/participant-selector/component.js
+++ client/app/pods/components/participant-selector/component.js
@@ -2,28 +2,48 @@ import Ember from 'ember';
 var ParticipantSelectorComponent;
 
 ParticipantSelectorComponent = Ember.Component.extend({
+
   classNames: ['participant-selector', 'select2-multiple'],
+
+  init: function(){
+    this._super(...arguments);
+    this.set('selectedTemplate', this.getSelectedTemplate());
+  },
+
   setupTooltips: (function() {
     return Ember.run.schedule('afterRender', this, function() {
       this.$('.select2-search-choice img').tooltip({
         placement: "bottom"
       });
-      return this.$('.add-participant-button').tooltip({
-        placement: "bottom"
-      });
+
+      if (this.get('canManage')) {
+        return this.$('.add-participant-button').tooltip({
+          placement: "bottom"
+        });
+      }
     });
   }).on('didInsertElement').observes('currentParticipants.[]'),
+
   resultsTemplate: function(user) {
     var userInfo;
     userInfo = user.old_roles.length ? user.username + ", " + (user.old_roles.join(', ')) : user.username;
     return '<strong>' + user.full_name + '</strong><br><div class="suggestion-sub-value">' + userInfo + '</div>';
   },
-  selectedTemplate: function(user) {
-    var name, url;
-    name = user.full_name || user.get('fullName');
-    url = user.avatar_url || user.get('avatarUrl');
-    return Ember.String.htmlSafe("<img alt='" + name + "' class='user-thumbnail-small' src='" + url + "' data-toggle='tooltip' title='" + name + "'/>");
+
+  getSelectedTemplate: function() {
+    return (user) => {
+      var name, url;
+      name = user.full_name || user.get('fullName');
+      url = user.avatar_url || user.get('avatarUrl');
+      if (this.get('canManage')) {
+        return Ember.String.htmlSafe("<img alt='" + name + "' class='user-thumbnail-small' src='" + url + "' data-toggle='tooltip' title='" + name + "'/>");
+      }
+      else {
+        return Ember.String.htmlSafe("<img alt='" + name + "' class='user-thumbnail-small' src='" + url + "' title='" + name + "'/>");
+      }
+    }
   },
+
   sortByCollaboration: function(a, b) {
     if (a.old_roles.length && !b.old_roles.length) {
       return -1;
@@ -39,6 +59,7 @@ ParticipantSelectorComponent = Ember.Component.extend({
       }
     }
   },
+
   remoteSource: (function() {
     return {
       url: "/api/filtered_users/users/" + (this.get('paperId')) + "/",
@@ -59,6 +80,7 @@ ParticipantSelectorComponent = Ember.Component.extend({
       })(this)
     };
   }).property(),
+
   actions: {
     addParticipant: function(newParticipant) {
       return this.attrs.onSelect(newParticipant.id);

be5ab2b7c785cf120a503e50461818330c44f529
Sam Bleckley <sam@mutuallyhuman.com>
2016-02-18 12:52:56 -0500

    Cause all tasks to use isEditable
    There are three pretty mechanical changes going on here:
    
    - Change all submissionTaskNotEditable to isNotEditable, all
      submissionTaskIsEditable with isEditable, and all fieldsDisabled with
      isNotEditable.
    
    - Change all nested question components to use `ident`, not `model` --
      there is a bug with putting nested questions in computed properties.
    
    - Change the hover states of authors and reviewer candidates
      to respond properly to hovering when editable and when not editable

--- engines/tahi_standard_tasks/client/app/components/ethics-task.js
+++ engines/tahi_standard_tasks/client/app/components/ethics-task.js
@@ -1,65 +1,6 @@
 import TaskComponent from 'tahi/pods/components/task-base/component';
 import Ember from 'ember';
 
-const { computed } = Ember;
-
 export default TaskComponent.extend({
-  classNames: ['ethics-task'],
-  humanSubjectsQuestion: computed('task', function(){
-    return this.get('task')
-               .findQuestion('ethics--human_subjects');
-  }),
-
-  participantsQuestion: computed('task', function(){
-    return this.get('task')
-               .findQuestion('ethics--human_subjects--participants');
-  }),
-
-  animalSubjectsQuestion: computed('task', function(){
-    return this.get('task')
-               .findQuestion('ethics--animal_subjects');
-  }),
-
-  fieldPermitQuestion: computed('task', function(){
-    return this.get('task')
-               .findQuestion('ethics--animal_subjects--field_permit');
-  }),
-
-  fieldArriveQuestion: computed('task', function(){
-    return this.get('task')
-               .findQuestion('ethics--animal_subjects--field_arrive');
-  }),
-
-  fieldStudyQuestion: computed(function(){
-    return this.get('task')
-              .findQuestion('ethics--field_study');
-  }),
-
-  fieldPermitNumberQuestion: computed(function(){
-    return this.get('task')
-               .findQuestion('ethics--field_study--field_permit_number');
-  }),
-
-  actions: {
-    userSelectedNoOnHumanSubjects() {
-      const question = this.get('participantsQuestion');
-      const answer = question.answerForOwner(this.get('task'));
-      answer.set('value', '');
-      answer.save();
-    },
-
-    userSelectedNoOnAnimalSubjects() {
-      const question = this.get('fieldPermitQuestion');
-      const answer = question.answerForOwner(this.get('task'));
-      answer.set('value', '');
-      answer.save();
-    },
-
-    userSelectedNoOnFieldStudy(){
-      const question = this.get('fieldStudyQuestion');
-      const answer = question.answerForOwner(this.get('task'));
-      answer.set('value', '');
-      answer.save();
-    }
-  }
+  classNames: ['ethics-task']
 });

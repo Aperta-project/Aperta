eaf2cc7b76117d463ecd32097ba211fcb486deed
Sam Bleckley <sam@mutuallyhuman.com>
2016-02-18 12:52:56 -0500

    Cause all tasks to use isEditable
    There are three pretty mechanical changes going on here:
    
    - Change all submissionTaskNotEditable to isNotEditable, all
      submissionTaskIsEditable with isEditable, and all fieldsDisabled with
      isNotEditable.
    
    - Change all nested question components to use `ident`, not `model` --
      there is a bug with putting nested questions in computed properties.
    
    - Change the hover states of authors and reviewer candidates
      to respond properly to hovering when editable and when not editable

--- engines/tahi_standard_tasks/client/app/components/data-availability-task.js
+++ engines/tahi_standard_tasks/client/app/components/data-availability-task.js
@@ -1,14 +1,3 @@
 import TaskComponent from 'tahi/pods/components/task-base/component';
-import Ember from 'ember';
 
-export default TaskComponent.extend({
-  dataLocationQuestion: Ember.computed('task', function(){
-    return this.get('task')
-               .findQuestion('data_availability--data_location');
-  }),
-
-  fullyAvailableQuestion: Ember.computed('task', function(){
-    return this.get('task')
-               .findQuestion('data_availability--data_fully_available');
-  })
-});
+export default TaskComponent.extend({});

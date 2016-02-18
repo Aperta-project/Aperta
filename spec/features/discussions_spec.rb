263ec3900b5397a48cfdca0355450e5016551b72
Sara Gibbons <sara@mutuallyhuman.com>
2016-02-17 21:20:10 -0500

    add discussion feature spec [APERTA-5627]

---
+++ spec/features/discussions_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+feature "Discussions", js: true, selenium: true do
+  let(:creator) { create :user }
+  let!(:journal) { FactoryGirl.create :journal }
+  let!(:paper) { FactoryGirl.create :paper, :submitted, :with_tasks, creator: creator, journal: journal }
+  let(:discussion_page) { DiscussionsPage.new }
+
+  context 'access paper and manage discussions' do
+    let!(:user) { FactoryGirl.create :user }
+
+    scenario 'can create discussions, replies and add participants' do
+      login_as(creator, scope: :user)
+      visit "/papers/#{paper.id}"
+      wait_for_ajax
+      find('#nav-discussions').click
+      wait_for_ajax
+
+      discussion_page.new_topic
+      discussion_page.fill_in_topic
+      discussion_page.create_topic
+      discussion_page.expect_topic_created_succesfully(creator)
+      discussion_page.expect_can_add_participant
+      discussion_page.add_reply
+      discussion_page.expect_reply_created(creator, 2)
+    end
+  end
+
+  context 'access to paper no access to discussions' do
+    let(:user) { create :user }
+    let!(:discussion_topic) { FactoryGirl.create :discussion_topic, paper: paper }
+
+    before { paper.add_collaboration(user) }
+
+    scenario 'can view paper, no discussions load' do
+      login_as(user, scope: :user)
+      visit "/papers/#{paper.id}"
+      wait_for_ajax
+      find('#nav-discussions').click
+      wait_for_ajax
+      discussion_page.expect_no_create_button
+      discussion_page.expect_view_no_discussions
+    end
+  end
+
+  context 'access to paper, participant only on a discussion' do
+    let(:user) { create :user }
+    let!(:discussion_topic) { FactoryGirl.create :discussion_topic, paper: paper }
+    let!(:discussion_participant) { FactoryGirl.create :discussion_participant, user: user, discussion_topic: discussion_topic }
+
+    before { paper.add_collaboration(user) }
+
+    scenario 'can see discussion and add reply' do
+      login_as(user, scope: :user)
+      visit "/papers/#{paper.id}"
+      wait_for_ajax
+      find('#nav-discussions').click
+      wait_for_ajax
+
+      discussion_page.expect_no_create_button
+      discussion_page.click_topic
+      discussion_page.expect_view_topic
+      discussion_page.expect_view_only_participants
+      discussion_page.add_reply
+      discussion_page.expect_reply_created(user, 1)
+    end
+  end
+end
---
+++ spec/support/pages/discussions_page.rb
@@ -0,0 +1,76 @@
+class DiscussionsPage < Page
+  def new_topic
+    find(create_topic_button).click
+    wait_for_ajax
+  end
+
+  def fill_in_topic
+    find('input.discussion-topic-title-field').set('Great Title')
+    find('.discussion-topic-comment-field').set('first!!')
+  end
+
+  def create_topic
+    find('.discussions-show-content .button-primary').click
+    wait_for_ajax
+  end
+
+  def click_topic
+    find('a.discussions-index-topic').click
+    wait_for_ajax
+  end
+
+  def expect_topic_created_succesfully(user)
+    expect_view_topic
+    user_name_in_comments(user, 1)
+  end
+
+  def expect_can_add_participant
+    expect(page).to have_css(add_participant_button)
+  end
+
+  def expect_no_create_button
+    expect(page).not_to have_css(create_topic_button)
+  end
+
+  def expect_view_topic
+    expect(find('h1.discussions-show-title'))
+    expect(find('.participant-selector'))
+    expect(find('.discussions-show-form'))
+  end
+
+  def add_reply
+    find('.discussions-show-form .new-comment-field').set('new reply')
+    wait_for_ajax
+    find('button.new-comment-submit-button').click
+    wait_for_ajax
+  end
+
+  def expect_reply_created(user, count)
+    user_name_in_comments(user, count)
+    expect(page).to have_content('new reply')
+  end
+
+  def expect_view_only_participants
+    expect(page).not_to have_css(add_participant_button)
+    expect(page).to have_css('.participant-selector')
+  end
+
+  def expect_view_no_discussions
+    expect(page).to have_css('.discussions-index-header')
+    expect(page).not_to have_css('.discussions-index-topic')
+  end
+
+  private
+
+  def create_topic_button
+    '.discussions-index-header .button-secondary'
+  end
+
+  def add_participant_button
+    '.add-participant-button'
+  end
+
+  def user_name_in_comments(user, count)
+    within('.discussions-show-content') { expect(page).to have_content(user.full_name, count: count) }
+  end
+end

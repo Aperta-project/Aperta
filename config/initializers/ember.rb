a74201113dda27428ded3a77395d66b9652db975
Erik Hetzner <ehetzner@plos.org>
2016-02-20 11:01:13 -0800

    Fix bower_path in ember

--- config/initializers/ember.rb
+++ config/initializers/ember.rb
@@ -2,7 +2,7 @@ EmberCli.configure do |c|
   c.app(:tahi, {
     path: Rails.root.join('client'),
     # use locally installed bower bin
-    bower_path: Rails.root.join('client', 'node_modules', '.bin', 'bower'),
+    bower_path: Rails.root.join('node_modules', '.bin', 'bower'),
     build_timeout: 40
   })
 end

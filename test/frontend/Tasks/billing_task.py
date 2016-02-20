9da5585a02fc8f44ec613f34ba1091ee2171e063
Sebastian Bassi <sebastian.bassi@globant.com>
2016-02-19 16:15:24 -0800

    APERTA-6084 Changing path for import to work without a specific local env var

--- test/frontend/Tasks/billing_task.py
+++ test/frontend/Tasks/billing_task.py
@@ -5,7 +5,7 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-from frontend.Resources import billing_data as bd
+from Base.Resources import billing_data as bd
 
 from frontend.Tasks.basetask import BaseTask
 

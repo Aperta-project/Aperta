2646f8018cc2198a32252c6cdd714bf7c9649995
Erik Hetzner <ehetzner@plos.org>
2016-02-22 09:53:37 -0800

    APERTA-5628 Stub user.can? in authors_controller
    Speeds up & simplifies test by only testing actual controller
    functionality.

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -1,18 +1,9 @@
 require 'rails_helper'
 
 describe AuthorsController do
+  let(:user) { FactoryGirl.create(:user) }
   let(:task) { FactoryGirl.create(:authors_task, paper: paper) }
-  let(:journal) do
-    FactoryGirl.create(:journal,
-                       :with_publishing_services_user,
-                       :with_staff_admin_user)
-  end
-  let(:paper) do
-    FactoryGirl.create(:paper,
-                       :with_handling_editor_user,
-                       :with_internal_editor_user,
-                       :with_integration_journal)
-  end
+  let(:paper) { FactoryGirl.create(:paper) }
   let(:post_request) do
     post :create,
          format: :json,
@@ -31,10 +22,15 @@ describe AuthorsController do
   end
 
   before do
-    sign_in user
+    allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+    allow(controller).to receive(:current_user).and_return(user)
   end
 
-  shared_examples_for 'user who can edit authors' do
+  describe "when the current user can edit_authors on the paper" do
+    before do
+      allow(user).to receive(:can?).with(:edit_authors, paper).and_return(true)
+    end
+
     it 'a POST request creates a new author' do
       expect { post_request }.to change { Author.count }.by(1)
     end
@@ -49,7 +45,11 @@ describe AuthorsController do
     end
   end
 
-  shared_examples_for 'user who cannot edit authors' do
+  describe "when the current user can NOT edit_authors on the paper" do
+    before do
+      allow(user).to receive(:can?).with(:edit_authors, paper).and_return(false)
+    end
+
     it 'a POST request does not create a new author' do
       expect { post_request }.not_to change { Author.count }
     end
@@ -77,52 +77,5 @@ describe AuthorsController do
       delete_request
       expect(response).to have_http_status(:forbidden)
     end
-  end
-
-  context "when the current user is assigned the creator role on the paper" do
-    let(:user) { paper.creator }
-
-    # These tests are possibly too much
-    context 'and the paper is in a user-editable state' do
-      Paper::EDITABLE_STATES.each do |state|
-        describe "and the state is #{state}" do
-          let(:paper) { FactoryGirl.create(:paper, publishing_state: state) }
-
-          include_examples 'user who can edit authors'
-        end
-      end
-
-      context 'and the paper is NOT in a user-editable state' do
-        Paper::UNEDITABLE_STATES.each do |state|
-          describe "and the state is #{state}" do
-            let(:paper) { FactoryGirl.create(:paper, publishing_state: state) }
-
-            include_examples 'user who cannot edit authors'
-          end
-        end
-      end
-    end
-  end
-
-  [:publishing_services_role, :staff_admin_role].each do |role|
-    context "when the current user is assigned the #{role} on the journal" do
-      let(:user) { journal.assignments.where(role: journal.send(role)).first.user }
-
-      include_examples 'user who can edit authors'
-    end
-  end
-
-  [:handling_editor_role, :internal_editor_role].each do |role|
-    context "when the current user is assigned the #{role} on the paper" do
-      let(:user) { paper.assignments.where(role: journal.send(role)).first.user }
-
-      include_examples 'user who can edit authors'
-    end
-  end
-
-  context 'when the current user has no relevant role' do
-    let(:user) { FactoryGirl.create(:user) }
-
-    include_examples 'user who cannot edit authors'
   end
 end

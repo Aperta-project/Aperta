0b715160d0ea2afc33ce06b75ce3da5d2f053773
Tad Park <tadp@users.noreply.github.com>
2016-02-25 15:29:23 -0800

    APERTA-5633 Remove qunit tests

--- client/tests/factories/data-availability-task.js
+++
@@ -1,9 +0,0 @@
-import FactoryGuy from "ember-data-factory-guy";
-
-FactoryGuy.define('data-availability-task', {
-  default: {
-    title: 'Data Availability',
-    type: 'DataAvailabilityTask',
-    completed: false
-  }
-});

502bce8a2225569983ac469dfb6cc871a62f60ef
Matt Mercieca <mercieca@gmail.com>
2016-02-25 08:27:59 -0500

    APERTA-5637 Adding permission to view cover letter task

--- app/services/journal_factory.rb
+++ app/services/journal_factory.rb
@@ -225,6 +225,16 @@ class JournalFactory
 
       # AEs can ONLY view reviewer report tasks
       role.ensure_permission_exists(:view, applies_to: TahiStandardTasks::ReviewerReportTask)
+
+      # AEs can also view some other tasks
+      role.ensure_permission_exists(:view, applies_to: TahiStandardTasks::CoverLetterTask, states: ['*'])
+
+      # Discussions
+      role.ensure_permission_exists(:start_discussion, applies_to: Paper, states: ['*'])
+      role.ensure_permission_exists(:view, applies_to: DiscussionTopic, states: ['*'])
+      role.ensure_permission_exists(:edit, applies_to: DiscussionTopic, states: ['*'])
+      role.ensure_permission_exists(:manage_participant, applies_to: DiscussionTopic, states: ['*'])
+      role.ensure_permission_exists(:reply, applies_to: DiscussionTopic, states: ['*'])
     end
 
     Role.ensure_exists(Role::DISCUSSION_PARTICIPANT, journal: @journal) do |role|

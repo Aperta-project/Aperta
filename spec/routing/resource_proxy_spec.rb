26f806ff3f397167c2f6d8494f2d7532a5a66cb5
Erik Hetzner <ehetzner@plos.org>
2016-03-02 16:13:38 -0800

    Fix ember routes
    The ember routes should come at the end, so that all other routes are
    given priority.
    
    Fixes a bug with the resource proxy, for which a test is added.

--- spec/routing/resource_proxy_spec.rb
+++ spec/routing/resource_proxy_spec.rb
@@ -4,20 +4,21 @@ describe 'routes for resource_proxy' do
   context 'without version' do
     it 'routes to the right things' do
       expect({ get: '/resource_proxy/my_resource/my_token'}).to route_to(
-        'ember_app'  => :tahi,
-        'controller' => 'ember_cli/ember',
-        'action'     => 'index',
-        'rest'       => 'resource_proxy/my_resource/my_token'
+        'controller' => 'resource_proxy',
+        'action'     => 'url',
+        'resource'   => 'my_resource',
+        'token'      => 'my_token'
       )
     end
   end
   context 'with version' do
     it 'routes to the right things' do
       expect({ get: '/resource_proxy/my_resource/my_token/my_version'}).to route_to(
-        'ember_app'  => :tahi,
-        'controller' => 'ember_cli/ember',
-        'action'     => 'index',
-        'rest'       => 'resource_proxy/my_resource/my_token/my_version'
+        'controller' => 'resource_proxy',
+        'action'     => 'url',
+        'resource'   => 'my_resource',
+        'token'      => 'my_token',
+        'version'    => 'my_version'
       )
     end
   end

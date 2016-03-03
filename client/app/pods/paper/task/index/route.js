f63425c54e675abbcf4f551e1d363bfc4bbd1d64
Zach Dennis <zach.dennis@gmail.com>
2016-03-03 15:28:51 -0500

    Updating the paper task index route to treat a rejected promise as an unauthorized request.
    This resolves an issue where someone trying to go to a task they do not have access to fails with an infinite spinner because the task fails to load and all that we currently get is a rejected promise.
    
    [APERTA-5635]

--- client/app/pods/paper/task/index/route.js
+++ client/app/pods/paper/task/index/route.js
@@ -1,13 +1,23 @@
 import Ember from 'ember';
+import AuthorizedRoute from 'tahi/routes/authorized';
 
-export default Ember.Route.extend({
+export default AuthorizedRoute.extend({
+  beforeModel(transition){
+    this.set('previousTransition', transition);
+  },
+
   model(params) {
     // Force the reload of the task when visiting the tasks' route.
     let task = this.store.findTask(params.task_id);
     if (task) {
       return task.reload();
     } else {
-      return this.store.find('task', params.task_id);
+      return this.store.find('task', params.task_id).then(
+        (task) => { return task; },
+        () => {
+          this.handleUnauthorizedRequest(this.get('previousTransition'));
+        }
+      );
     }
   },
 

966cb72a2bbe0998709dadf444246f6d15f91449
Jerry Nümmi <jnummi@gmail.com>
2016-03-07 13:21:20 -0500

    Fix up get-json-task

--- client/app/lib/get-json-task.js
+++ client/app/lib/get-json-task.js
@@ -1,10 +1,10 @@
-import { task, } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 
 export default task(function * (url, data) {
   let xhr;
   try {
     xhr = Ember.$.getJSON(url, data);
-    let result = yield xhr.promise();
+    const result = yield xhr.promise();
     return result;
   } finally {
     xhr.abort();

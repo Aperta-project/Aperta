34f1250f1cb935c1cfacf3a303f2c6a9b2971c91
Matt Mercieca <mercieca@gmail.com>
2016-03-07 16:27:52 -0500

    APERTA-6102 Fixing participant lookup
    + Passing paperId down to participant selector
    + Changing the status of send_message to 204 to keep Ember happy

--- client/app/pods/components/ad-hoc-task/component.js
+++ client/app/pods/components/ad-hoc-task/component.js
@@ -13,6 +13,10 @@ export default TaskComponent.extend(BuildsTaskTemplate, {
     return `/api/tasks/${this.get('task.id')}/attachments`;
   }),
 
+  paperId: Ember.computed('task', function() {
+    return this.get('task').get('paper').get('id');
+  }),
+
   attachmentsRequest(path, method, s3Url, file) {
     const store = this.container.lookup('store:main');
     const restless = this.get('restless');

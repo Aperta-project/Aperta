b59bce43ed725b62671a2890d84c45066428aca9
Sam Bleckley <sam@mutuallyhuman.com>
2016-03-08 08:36:27 -0500

    APERTA-5865 Schema fix.

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160225165251) do
+ActiveRecord::Schema.define(version: 20160225184558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

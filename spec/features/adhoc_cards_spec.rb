2f61ae15e1ea763bd559219485b203c97a5a3786
Matt Mercieca <mercieca@gmail.com>
2016-03-08 15:02:09 -0500

    APERTA-6102 Adding spec

--- spec/features/adhoc_cards_spec.rb
+++ spec/features/adhoc_cards_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 feature 'Adhoc cards', js: true do
   let(:author) { create :user, first_name: 'Author' }
+  let(:admin) { FactoryGirl.create :user, site_admin: true }
   let(:paper) do
     FactoryGirl.create :paper_with_task,
                        :with_integration_journal,
@@ -10,13 +11,13 @@ feature 'Adhoc cards', js: true do
   end
   let(:overlay) { AdhocOverlay.new }
 
-  before do
-    paper.tasks.each { |t| t.add_participant(author) }
-    login_as(author, scope: :user)
-    visit "/papers/#{paper.id}/tasks/#{paper.tasks.first.id}"
-  end
-
   context 'As a participant' do
+    before do
+      paper.tasks.each { |t| t.add_participant(author) }
+      login_as(author, scope: :user)
+      visit "/papers/#{paper.id}/tasks/#{paper.tasks.first.id}"
+    end
+
     scenario 'uploads an image to ad-hoc card' do
       overlay.upload_attachment('yeti.jpg')
       within('.attachment-item') do
@@ -44,6 +45,29 @@ feature 'Adhoc cards', js: true do
       find('.file-link', text: 'yeti.jpg')
       find('.delete-attachment').click
       expect(page).not_to have_css('.attachment-item')
+    end
+  end
+
+  context 'As someone who can manage email participants' do
+    before do
+      login_as(admin, scope: :user)
+      visit "/papers/#{paper.id}/tasks/#{paper.tasks.first.id}"
+    end
+
+    scenario 'allows sending email from ad-hoc card' do
+      overlay.find('.fa-plus').click
+      overlay.find('.adhoc-toolbar-item--email').click
+      overlay.fill_in('Enter a subject', with: 'subject')
+      overlay.find('.email-body').click
+      overlay.find('.email-body').send_keys('body')
+      overlay.find_all('.button-secondary', text: 'SAVE').first.click
+      overlay.find('.email-send-participants').click
+      overlay.find_all('.add-participant-button', text: '+').first.click
+      overlay.find('.select2-input').send_keys('author')
+      overlay.find('.select2-results').click
+      overlay.find('.send-email-action').click
+
+      expect(overlay).to have_text('Your email has been sent.')
     end
   end
 end

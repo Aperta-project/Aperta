31e326aaf691fd4c489d0087eb37bba02cc1bead
Sam Bleckley <sam@mutuallyhuman.com>
2016-03-10 10:40:50 -0500

    APERTA-6113 Migration has mock classes

--- db/migrate/20160303203916_add_author_list_item.rb
+++ db/migrate/20160303203916_add_author_list_item.rb
@@ -1,11 +1,15 @@
-class Author < ActiveRecord::Base
-  has_one :author_list_item
-end
-
-class AuthorListItem < ActiveRecord::Base
-end
-
+# Authors and GroupAuthors belong to the same list; this makes the
+# class that joins them.
 class AddAuthorListItem < ActiveRecord::Migration
+  # Fake author so's changes to the code don't break this migration
+  class Author < ActiveRecord::Base
+    has_one :author_list_item
+  end
+
+  # Ditto
+  class AuthorListItem < ActiveRecord::Base
+  end
+
   def up
     create_table :author_list_items do |t|
       t.integer :position
@@ -14,7 +18,7 @@ class AddAuthorListItem < ActiveRecord::Migration
       t.timestamps
     end
 
-    Author.all().each do |author|
+    Author.all.each do |author|
       AuthorListItem.create!(
         author_id: author.id,
         author_type: "Author",
@@ -31,7 +35,7 @@ class AddAuthorListItem < ActiveRecord::Migration
     add_column :authors, :authors_task_id, :integer
     add_column :authors, :position, :integer
 
-    Author.all().each do |author|
+    Author.all.each do |author|
       author.update(
         authors_task_id: author.author_list_item.task_id,
         position: author.author_list_item.position

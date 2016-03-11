18c41a45d412e0a9defd0c5bd5fcb4170cf0543a
Erik Hetzner <ehetzner@plos.org>
2016-03-10 16:29:56 -0800

    Basic capistrano setup for ngingx/puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,11 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
+set :application, 'tahi'
+set :repo_url, 'git@github.com:tahi-project/tahi.git'
+set :rails_env, 'production'
+set :chruby_ruby, 'ruby-2.2.3'
+set :chruby_exec, '/usr/bin/chruby-exec'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -23,10 +25,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('env.production')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -35,7 +37,6 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :keep_releases, 5
 
 namespace :deploy do
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
@@ -46,3 +47,5 @@ namespace :deploy do
   end
 
 end
+
+set :nginx_config_name, 'tahi.conf'

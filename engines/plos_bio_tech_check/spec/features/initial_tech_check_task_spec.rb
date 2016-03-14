26d0d3ac1c2ed76826cc34fff94509684e6615ad
Sara Gibbons <sara@mutuallyhuman.com>
2016-03-14 10:52:38 -0400

    use editor for testing rather than admin as admin has more access, user editor will provide a better spec [APERTA-5650]

--- engines/plos_bio_tech_check/spec/features/initial_tech_check_task_spec.rb
+++ engines/plos_bio_tech_check/spec/features/initial_tech_check_task_spec.rb
@@ -2,30 +2,31 @@ require 'rails_helper'
 
 feature 'Initial Tech Check', js: true do
   let(:journal) { create :journal, :with_roles_and_permissions }
-  let(:admin) { create :user, site_admin: true }
+  let(:editor) { create :user }
   let(:author) { create :user }
   let(:paper) { create :paper, :submitted, journal: journal, creator: author }
   let(:task) { create :initial_tech_check_task, paper: paper }
 
   before do
-    assign_journal_role journal, admin, :admin
+    assign_journal_role journal, editor, :editor
   end
 
   scenario 'Initial Tech Check triggers Changes For Author' do
-    SignInPage.visit.sign_in admin
+    # Editor
+    login_as(editor, scope: :user)
     overlay = Page.view_task_overlay(paper, task)
-    visit "/papers/#{paper.id}/tasks/#{task.id}"
     expect(PlosBioTechCheck::ChangesForAuthorTask.count).to eq(0)
     overlay.create_author_changes_card
     overlay.expect_author_chages_saved
     overlay.mark_as_complete
     overlay.expect_task_to_be_completed
     overlay.dismiss
-    Page.new.sign_out
+    Warden.test_reset!
 
     change_author_task = PlosBioTechCheck::ChangesForAuthorTask.first
 
-    SignInPage.visit.sign_in author
+    # Author
+    login_as(author, scope: :user)
     overlay = Page.view_task_overlay(paper, change_author_task)
     overlay.expect_to_see_change_list
     overlay.click_changes_have_been_made
@@ -34,11 +35,11 @@ feature 'Initial Tech Check', js: true do
     # creator cannot access iniital tech task
     visit "/papers/#{paper.id}/tasks/#{task.id}"
     expect(page).to have_content("You don't have access to that content")
-    Page.new.sign_out
+    Warden.test_reset!
   end
 
   scenario "list the unselected question items in the author changes letter" do
-    SignInPage.visit.sign_in admin
+    login_as(editor, scope: :user)
     overlay = Page.view_task_overlay(paper, task)
     overlay.display_letter
     overlay.click_autogenerate_email_button
@@ -57,7 +58,7 @@ feature 'Initial Tech Check', js: true do
   end
 
   scenario "selected questions don't show up in the auto-generated author change letter" do
-    SignInPage.visit.sign_in admin
+    login_as(editor, scope: :user)
     overlay = Page.view_task_overlay(paper, task)
     overlay.display_letter
     overlay.click_autogenerate_email_button
@@ -74,7 +75,7 @@ feature 'Initial Tech Check', js: true do
   end
 
   scenario "unchecking a box with no associated text has no effect" do
-    SignInPage.visit.sign_in admin
+    login_as(editor, scope: :user)
     overlay = Page.view_task_overlay(paper, task)
     overlay.display_letter
     overlay.click_autogenerate_email_button

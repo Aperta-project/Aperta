ed38eaff4f241c046c1c2425e69c4374c7d07ad7
Zach Dennis <zach.dennis@gmail.com>
2016-03-14 11:32:45 -0400

    Use a key-map of role to role eligibility in EligibleUserService.
    It makes it easier to add, remove, change, or otherwise refactor.
    
    APERTA-5648

--- app/services/eligible_user_service.rb
+++ app/services/eligible_user_service.rb
@@ -12,14 +12,15 @@ class EligibleUserService
   def initialize(paper:, role:)
     @paper = paper
     @role = role
+
+    @eligible_user_blocks = {
+      role.journal.handling_editor_role => -> { internal_editors },
+      role.journal.cover_editor_role => -> { internal_editors }
+    }
   end
 
   def eligible_users
-    handling_editor_role = role.journal.handling_editor_role
-    cover_editor_role = role.journal.cover_editor_role
-    if [handling_editor_role, cover_editor_role].include?(role)
-      eligible_users_for_cover_and_handling_editors
-    else
+    block = @eligible_user_blocks.fetch(role) do
       fail NotImplementedError, <<-MESSAGE.strip_heredoc
         Don't know how to find eligible users for the role:
           #{role.inspect}
@@ -27,17 +28,19 @@ class EligibleUserService
         eligible users for this role may need to be implemented.
       MESSAGE
     end
-  end
 
-  private
-
-  def eligible_users_for_cover_and_handling_editors
-    internal_editors = role.journal.internal_editor_role.users.uniq
+    eligible_users = block.call.uniq
     users_already_assigned = begin
       User.all
       .joins(:assignments)
       .where(assignments: { role: role, assigned_to: paper })
     end
-    internal_editors - users_already_assigned
+    eligible_users - users_already_assigned
+  end
+
+  private
+
+  def internal_editors
+    role.journal.internal_editor_role.users
   end
 end

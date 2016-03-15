d5fb1554389f58545253c66e57deb8124e007e3f
Mr. Jeffrey Gray <jgray@plos.org>
2016-03-15 09:22:48 -0700

    APERTA-6332 Complete: Removed checks for reset password, general code cleanup.

--- test/frontend/test_profile.py
+++ test/frontend/test_profile.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-This test case validates the Aperta workflow page
-"""
-
-__author__ = 'sbassi@plos.org'
-
 import time
+import logging
 
 from Base.Decorators import MultiBrowserFixture
 from Base.Resources import login_valid_uid, super_admin_login
 from frontend.common_test import CommonTest
 from frontend.Pages.profile_page import ProfilePage
 from Pages.dashboard import DashboardPage
+
+"""
+This test case validates the Aperta workflow page
+"""
+
+__author__ = 'sbassi@plos.org'
 
 
 @MultiBrowserFixture
@@ -29,7 +30,8 @@ class ApertaProfileTest(CommonTest):
   def _go_to_profile(self, init=True):
     """Go to the profile page"""
     # APERTA-6146 Can only use super admin login for the time being.
-    dashboard = self.cas_login(email=super_admin_login['email']) if init else DashboardPage(self.getDriver())
+    dashboard = self.cas_login(email=super_admin_login['email']) if init \
+        else DashboardPage(self.getDriver())
     dashboard.click_profile_link()
     return ProfilePage(self.getDriver())
 
@@ -44,15 +46,12 @@ class ApertaProfileTest(CommonTest):
     """Testing add/delete affiliations"""
     profile_page = self._go_to_profile()
     # Validate image upload
-    # TODO: Check following method after Pivotal #101632186 is fixed
-    #profile_page.validate_image_upload()
+    profile_page.validate_image_upload()
     # add affiliations
     profile_page.click_add_affiliation_button()
     # Check affiliation css elements
     profile_page.validate_affiliation_form_css()
-    profile_page.validate_reset_password()
     return self
-
 
 if __name__ == '__main__':
   CommonTest._run_tests_randomly()

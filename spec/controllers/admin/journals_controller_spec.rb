eb0660dd632f57793cf5dbe2e2b4cd733c171a30
Tad Park <tadp@users.noreply.github.com>
2016-03-15 11:48:33 -0700

    APERTA-5656 Update journals_controller_spec to be similar to other RandP specs

--- spec/controllers/admin/journals_controller_spec.rb
+++ spec/controllers/admin/journals_controller_spec.rb
@@ -11,13 +11,19 @@ describe Admin::JournalsController, redis: true do
   let(:image_file) { fixture_file_upload 'yeti.jpg' }
 
   describe '#create' do
-    context 'when the user is authorized' do
+    subject(:do_request) do
+      post :create,
+           format: 'json',
+           admin_journal: { name: 'new journal name', description: 'new journal desc' }
+    end
+
+    context 'when the user has access' do
       before do
         sign_in site_admin
       end
 
       it 'creates a journal' do
-        post :create, admin_journal: { name: 'new journal name', description: 'new journal desc' }, format: :json
+        do_request
         journal = Journal.last
         expect(journal.name).to eq 'new journal name'
         expect(journal.description).to eq 'new journal desc'
@@ -25,37 +31,47 @@ describe Admin::JournalsController, redis: true do
       end
     end
 
-    context "when the user has insufficient permissions" do
+    context "when the user does not have access" do
       let(:journal_admin) { create :user }
       before { assign_journal_role(journal, journal_admin, :admin) }
 
       before do
         sign_in journal_admin
       end
+      it_behaves_like "an unauthenticated json request"
 
       it "renders status 403" do
-        xhr :post, :create, admin_journal: { name: 'journal name', description: 'journal desc' }
+        do_request
         expect(response.status).to eq 403
       end
     end
 
     context "when the user is unauthorized" do
       it "renders status 401" do
-        xhr :post, :create, admin_journal: { name: 'journal name', description: 'journal desc' }
+        do_request
         expect(response.status).to eq 401
       end
     end
   end
 
   describe '#update' do
-    context "when the journal is updated successfully" do
+    subject(:do_request) do
+      patch :update,
+            format: 'json',
+            id: journal.id,
+            admin_journal: { epub_cover: image_file }
+    end
+
+    it_behaves_like "an unauthenticated json request"
+
+    context "when the user has access" do
       before do
-        sign_in site_admin
+        sign_in journal_admin
       end
 
-      it "renders status 2xx" do
+      it "renders status 2xx and the journal is updated successfully" do
         with_aws_cassette('admin_journal_controller') do
-          patch :update, id: journal.id, admin_journal: { epub_cover: image_file }, format: :json
+          do_request
         end
         expect(response.status).to eq 204
       end
@@ -78,10 +94,10 @@ describe Admin::JournalsController, redis: true do
       end
     end
 
-    context "when the user is unauthorized" do
+    context "when the user does not have access" do
       it "renders status 401" do
         with_aws_cassette('admin_journal_controller') do
-          xhr :patch, :update, id: journal.id, admin_journal: { epub_cover: image_file }
+          do_request
         end
         expect(response.status).to eq 401
       end

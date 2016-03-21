5ef5936b9b5b29dcd276c516ac4e0c63a85f566f
Jack LaBarba <brakit@gmail.com>
2016-03-21 14:59:13 -0700

    Send funder additional comments to salesforce

--- engines/tahi_standard_tasks/spec/models/funder_spec.rb
+++ engines/tahi_standard_tasks/spec/models/funder_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe TahiStandardTasks::Funder do
   let(:task) { FactoryGirl.create(:financial_disclosure_task) }
   let(:funder) { FactoryGirl.create(:funder, task: task) }
+  let(:comment_only_funder) do
+    TahiStandardTasks::Funder.new(additional_comments: 'Im da bes')
+  end
 
   describe "#paper" do
     it "always proxies to paper" do
@@ -12,10 +15,11 @@ describe TahiStandardTasks::Funder do
 
   describe "#funding_statement" do
     let(:funder) do
-      FactoryGirl.create(:funder,
-                         task: task,
-                         name: 'Something Foundation',
-                         grant_number: '00-3324-23498')
+      FactoryGirl.build(:funder,
+                        task: task,
+                        name: 'Something Foundation',
+                        grant_number: '00-3324-23498',
+                        additional_comments: 'Darmok and Jalad')
     end
     it "includes funder name" do
       expect(funder.funding_statement).to include(funder.name)
@@ -23,6 +27,34 @@ describe TahiStandardTasks::Funder do
 
     it "includes funder's grant number" do
       expect(funder.funding_statement).to include(funder.grant_number)
+    end
+
+    it "includes funder's additional comments" do
+      expect(funder.funding_statement).to include(funder.additional_comments)
+    end
+
+    it "only includes the comment if that's all that's provided" do
+      expected = "#{comment_only_funder.additional_comments}."
+      expect(comment_only_funder.funding_statement).to eq expected
+    end
+  end
+
+  describe "#only_has_additional_comments" do
+    it "is false when nothing is set on the model" do
+      funder = TahiStandardTasks::Funder.new
+      expect(funder.send(:only_has_additional_comments?)).to be false
+    end
+
+    it "is true when only additional_comments is set" do
+      funder = TahiStandardTasks::Funder.new(\
+        additional_comments: 'whatever bro')
+      expect(funder.send(:only_has_additional_comments?)).to be true
+    end
+
+    it "is false when additional_comments and anything else is set" do
+      funder = TahiStandardTasks::Funder.new(\
+        additional_comments: 'whatever bro', name: 'Dobis')
+      expect(funder.send(:only_has_additional_comments?)).to be false
     end
   end
 end

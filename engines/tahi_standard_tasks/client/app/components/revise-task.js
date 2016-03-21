cba69989baf416ddc6d0b85a71b2b33642b2c1a8
Jerry Nummi <jerry@mutuallyhuman.com>
2016-03-21 11:47:52 -0400

    Revise task gets attachments

--- engines/tahi_standard_tasks/client/app/components/revise-task.js
+++ engines/tahi_standard_tasks/client/app/components/revise-task.js
@@ -1,9 +1,16 @@
 import TaskComponent from 'tahi/pods/components/task-base/component';
 import Ember from 'ember';
+import getOwner from 'ember-getowner-polyfill';
 
-const { computed, on } = Ember;
+const {
+  computed,
+  inject: {service},
+  on
+} = Ember;
 
 export default TaskComponent.extend({
+  restless: service(),
+
   latestDecision: computed('previousDecisions', function() {
     return this.get('task.paper.decisions')
                .sortBy('revisionNumber').reverse()[1];
@@ -25,6 +32,19 @@ export default TaskComponent.extend({
     });
   }),
 
+  attachmentsPath: computed('task.id', function() {
+    return `/api/tasks/${this.get('task.id')}/attachments`;
+  }),
+
+  attachmentsRequest(path, method, s3Url, file) {
+    const store = getOwner(this).lookup('store:main');
+    const restless = this.get('restless');
+    restless.ajaxPromise(method, path, {url: s3Url}).then((response) => {
+      response.attachment.filename = file.name;
+      store.pushPayload(response);
+    });
+  },
+
   actions: {
     saveAuthorResponse() {
       this.get('latestDecision').save().then(()=> {
@@ -34,6 +54,19 @@ export default TaskComponent.extend({
 
     editAuthorResponse() {
       this.set('editingAuthorResponse', true);
+    },
+
+    deleteAttachment(attachment) {
+      attachment.destroyRecord();
+    },
+
+    createAttachment(s3Url, file) {
+      this.attachmentsRequest(this.get('attachmentsPath'), 'POST', s3Url, file);
+    },
+
+    updateAttachment(s3Url, file, attachment) {
+      const path = `${this.get('attachmentsPath')}/${attachment.id}/update_attachment`;
+      this.attachmentsRequest(path, 'PUT', s3Url, file);
     }
   }
 });

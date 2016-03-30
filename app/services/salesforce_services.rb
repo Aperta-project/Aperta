6cd1938c3c716603e96d716b106192e18e5ffd33
Zach Dennis <zach.dennis@gmail.com>
2016-03-30 12:24:18 -0400

    Adding additional logging statements for salesforce sync'ing.
    If too chatty they can be removed, but they should be helpful for seeing what salesforce communication is going on from the log files.
    
    APERTA-6321

--- app/services/salesforce_services.rb
+++ app/services/salesforce_services.rb
@@ -5,13 +5,18 @@ module SalesforceServices
 
   # Only send data to Salesforce if the author is
   # requesting publication fee assistance.
-  def self.sync_paper!(paper)
+  def self.sync_paper!(paper, logger: Rails.logger)
     answer = paper.answer_for('plos_billing--payment_method')
     should_send_to_salesforce = answer.try(:value) == "pfa"
 
     if should_send_to_salesforce
       SalesforceServices::PaperSync.sync!(paper: paper)
+      logger.info "Salesforce: Paper #{paper.id} sync'd successfully"
+
       SalesforceServices::BillingSync.sync!(paper: paper)
+      logger.info "Salesforce: Billing info on #{paper.id} sync'd successfully"
+    else
+      logger.info "Salesforce: Paper #{paper.id} is not PFA, skipping sync."
     end
   end
 end
--- spec/services/salesforce_services_spec.rb
+++ spec/services/salesforce_services_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe SalesforceServices do
   describe '.sync_paper!' do
-    subject(:sync_paper!) { SalesforceServices.sync_paper!(paper) }
-    let(:paper) { instance_double(Paper) }
+    subject(:sync_paper!) do
+      SalesforceServices.sync_paper!(paper, logger: logger)
+    end
+    let(:paper) { instance_double(Paper, id: 99) }
+    let(:logger) { Logger.new(StringIO.new) }
 
     context "when the paper's billing payment method is PFA" do
       before do
+        allow(SalesforceServices::PaperSync).to receive(:sync!)
+        allow(SalesforceServices::BillingSync).to receive(:sync!)
         allow(paper).to receive(:answer_for)
           .with('plos_billing--payment_method')
           .and_return instance_double(NestedQuestionAnswer, value: 'pfa')
@@ -18,6 +23,16 @@ describe SalesforceServices do
           .ordered
         expect(SalesforceServices::BillingSync).to receive(:sync!)
           .with(paper: paper)
+          .ordered
+        sync_paper!
+      end
+
+      it 'logs the sync was successful' do
+        expect(logger).to receive(:info)
+          .with("Salesforce: Paper #{paper.id} sync'd successfully")
+          .ordered
+        expect(logger).to receive(:info)
+          .with("Salesforce: Billing info on #{paper.id} sync'd successfully")
           .ordered
         sync_paper!
       end
@@ -35,6 +50,12 @@ describe SalesforceServices do
         expect(SalesforceServices::BillingSync).to_not receive(:sync!)
         sync_paper!
       end
+
+      it 'logs the sync was skipped' do
+        expect(logger).to receive(:info)
+          .with("Salesforce: Paper #{paper.id} is not PFA, skipping sync.")
+        sync_paper!
+      end
     end
 
     context 'when the paper is without a billing payment method' do
@@ -47,6 +68,12 @@ describe SalesforceServices do
       it 'does not sync the paper nor the billing information' do
         expect(SalesforceServices::PaperSync).to_not receive(:sync!)
         expect(SalesforceServices::BillingSync).to_not receive(:sync!)
+        sync_paper!
+      end
+
+      it 'logs the sync was skipped' do
+        expect(logger).to receive(:info)
+          .with("Salesforce: Paper #{paper.id} is not PFA, skipping sync.")
         sync_paper!
       end
     end

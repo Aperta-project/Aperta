4c037a0c3504dc2724350f822e999f7be1437993
Zach Dennis <zach.dennis@gmail.com>
2016-04-12 15:41:37 -0400

    Updating inlining_css_in_emails_spec to fail for the same reason it failed in the SFO staging box.
    Now all we need is a fix!
    
    APERTA-6543

--- spec/mailers/inlining_css_in_emails_spec.rb
+++ spec/mailers/inlining_css_in_emails_spec.rb
@@ -20,8 +20,26 @@ describe 'Inlining CSS into emails', type: :mailer do
   describe 'premailer-rails' do
     let(:email) { FakeMailer.send_fake_email }
 
+    before(:each) do
+      # The assets pipeline is nil when Rails.application.assets.config
+      # is set to false when Rails boots up. However, if we set it to false
+      # now it doesn't do anything. To not affect the entire test environment
+      # we will explicitly
+      @rails_assets = Rails.application.assets
+      @rails_asset_compile = Rails.application.config.assets.compile
+      Rails.application.assets = nil
+      Rails.application.config.assets.compile = false
+    end
+
+    after(:each) do
+      # Restore original Rails settings
+      Rails.application.config.assets.compile = @rails_asset_compile
+      Rails.application.assets = @rails_assets
+    end
+
     it 'inlines css' do
-      expect(email.body).to match(/<div class='.mailer' css='width: 600px;'>/)
+      delivered_email = email.deliver_now
+      expect(delivered_email.body).to match(/<div class='.mailer' css='width: 600px;'>/)
     end
   end
 end

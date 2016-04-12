4c79a2bb1ad89cab36a7a7eb5f027d1bd199c07e
Zach Dennis <zach.dennis@gmail.com>
2016-04-12 18:06:39 -0400

    Updating inlining_css_in_emails_spec to precompile the default email CSS file and run ensure that CSS is inlined in a production-like set-up.
    APERTA-6543

--- spec/mailers/inlining_css_in_emails_spec.rb
+++ spec/mailers/inlining_css_in_emails_spec.rb
@@ -12,13 +12,16 @@ describe 'Inlining CSS into emails', type: :mailer do
     end
 
     def send_fake_email
-      stub_template "fake_mailer/send_fake_email.erb" => "<div class='.mailer'>test</div>"
+      stub_template 'fake_mailer/send_fake_email.erb' => '<div class=".mailer">test</div>'
       mail(to: 'noone@example.com', subject: 'fake mail')
     end
   end
 
   describe 'premailer-rails' do
     let(:email) { FakeMailer.send_fake_email }
+
+    # temporary storage for precompiling assets in this test
+    let(:test_assets_path) { Rails.root.join('tmp/test-assets') }
 
     before(:each) do
       # The assets pipeline is nil when Rails.application.assets.config
@@ -27,19 +30,40 @@ describe 'Inlining CSS into emails', type: :mailer do
       # we will explicitly
       @rails_assets = Rails.application.assets
       @rails_asset_compile = Rails.application.config.assets.compile
+      @rails_asset_digest = Rails.application.config.assets.digest
+      @rails_asset_js_compressor = Rails.application.config.assets.js_compressor
+
       Rails.application.assets = nil
       Rails.application.config.assets.compile = false
+      Rails.application.config.assets.digest = true
+      Rails.application.config.assets.paths << test_assets_path
+
+      precompile_mailer_css
     end
 
     after(:each) do
       # Restore original Rails settings
+      Rails.application.config.assets.digest = @rails_asset_digest
       Rails.application.config.assets.compile = @rails_asset_compile
       Rails.application.assets = @rails_assets
+
+      # Clean up our files
+      FileUtils.rm_rf(test_assets_path)
+    end
+
+    def precompile_mailer_css
+      environment = Sprockets::Environment.new
+      environment.append_path(Rails.root.join('app/assets/stylesheets'))
+
+      manifest = Sprockets::Manifest.new(environment.index, test_assets_path)
+      manifest.compile('email.css')
     end
 
     it 'inlines css' do
       delivered_email = email.deliver_now
-      expect(delivered_email.body).to match(/<div class='.mailer' css='width: 600px;'>/)
+      body = Nokogiri::HTML(delivered_email.html_part.body.to_s)
+      element = body.search('.mailer[style*="width: 600px"]')
+      expect(element).to be
     end
   end
 end

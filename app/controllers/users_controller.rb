93eb3e19e84a810041666e8cdd210e7b2d0c9ea4
Chris Westra <cwestra@gmail.com>
2016-04-19 10:59:23 -0400

    Remove UsersController #show and #index
    Both endpoints are no longer called from the client.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   respond_to :json
 
-  def show
-    user = User.find(params[:id])
-    requires_user_can :view, user
-    respond_with user
-  end
-
-  def index
-    requires_user_can :view, current_user
-    users = current_user
-      .filter_authorized(:view, User.all.includes(:affiliations))
-      .objects
-    render users
-  end
-
   def update_avatar
     if DownloadAvatar.call current_user, params[:url]
       render json: {avatar_url: current_user.avatar.url}

fe93f02673c9da02f83c9bfa25299fa2bea1a3a8
Chris Westra <cwestra@gmail.com>
2016-04-20 16:57:19 -0400

    Remove most of FilteredUsersController code
    Paired with Zach Dennis (@zdennis)
    
    APERTA-6673

--- app/controllers/filtered_users_controller.rb
+++ app/controllers/filtered_users_controller.rb
@@ -7,35 +7,4 @@ class FilteredUsersController < ApplicationController
     respond_with users, each_serializer: FilteredUserSerializer,
                         paper_id: params[:paper_id]
   end
-
-  def editors
-    # TODO: Restrict this to editors.
-    uninvited_users
-  end
-
-  def admins
-    render_selectable_users(:admins)
-  end
-
-  def uninvited_users
-    users = User.fuzzy_search params[:query]
-    paper = Paper.find(params[:paper_id])
-    respond_with find_uninvited_users(users, paper), each_serializer: SelectableUserSerializer
-  end
-
-  private
-
-  def render_selectable_users(old_role)
-    paper = Paper.find(params[:paper_id])
-    journal_role_ids = paper.journal.send(old_role).pluck(:id)
-
-    users = User.where(id: journal_role_ids)
-    users = users.fuzzy_search(params[:query]) if params[:query]
-
-    respond_with find_uninvited_users(users, paper), each_serializer: SelectableUserSerializer
-  end
-
-  def find_uninvited_users(users, paper)
-    Invitation.find_uninvited_users_for_paper(users, paper)
-  end
 end

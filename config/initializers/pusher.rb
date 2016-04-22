a89f1be7b288040b7c0b5e3491552ff87feaac30
Jack LaBarba <jlabarba@plos.org>
2016-04-22 09:01:12 -0700

    testing pusher config init

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,7 +1,9 @@
 Pusher.url = ENV.fetch('PUSHER_URL')
 Pusher.logger = Rails.logger
 
+Rails.logger.error('SETTING PUSHER SSL VERIFICATION . . .')
 if ConfigHelper.read_boolean_env('DISABLE_PUSHER_SSL_VERIFICATION')
+  Rails.logger.error('DISABLING SSL VERIFICATION')
   Pusher.default_client.sync_http_client.ssl_config.verify_mode =
     OpenSSL::SSL::VERIFY_NONE
 end

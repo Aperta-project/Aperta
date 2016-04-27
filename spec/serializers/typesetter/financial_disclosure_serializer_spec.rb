53c1a80f2edfd6b953bc51689460166ab6a9f946
Matt Mercieca <mercieca@gmail.com>
2016-04-27 15:18:24 -0400

    APERTA-5754 Adding funding statement to financial disclosure

--- spec/serializers/typesetter/financial_disclosure_serializer_spec.rb
+++ spec/serializers/typesetter/financial_disclosure_serializer_spec.rb
@@ -26,12 +26,19 @@ describe Typesetter::FinancialDisclosureSerializer do
   it 'has competing interests fields' do
     expect(output.keys).to contain_exactly(
       :author_received_funding,
+      :funding_statement,
       :funders)
   end
 
   describe 'author_recieved_funding' do
     it 'marks whether the author received funding' do
       expect(output[:author_received_funding]).to eq(author_received_funding)
+    end
+  end
+
+  describe 'funding_statement' do
+    it 'includes the funding statement from the task' do
+      expect(output[:funding_statement]).to eq(task.funding_statement)
     end
   end
 

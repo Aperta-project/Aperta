1057f299f780d053b675b7299163d04d3e5a8693
Tad Park <tadp@users.noreply.github.com>
2016-04-27 13:57:20 -0700

    APERTA-6137 Add client side spec to make sure permissionState is delegated

--- client/tests/models/task-test.js
+++ client/tests/models/task-test.js
@@ -1,47 +1,61 @@
 import Ember from 'ember';
-import { test, moduleForModel } from 'ember-qunit';
-moduleForModel('task', 'Unit: Task Model', {
-  needs: ['model:author', 'model:user', 'model:figure', 'model:table', 'model:bibitem',
-    'model:journal', 'model:discussion-topic', 'model:versioned-text',
-    'model:decision', 'model:invitation', 'model:affiliation', 'model:attachment',
-    'model:question-attachment', 'model:comment-look',
-    'model:phase', 'model:task', 'model:comment', 'model:participation',
-    'model:card-thumbnail', 'model:nested-question-owner',
-    'model:nested-question', 'model:nested-question-answer', 'model:collaboration',
-    'model:supporting-information-file', 'model:paper', 'model:snapshot',
-    'model:paper-task-type', 'model:group-author']
+import { module } from 'qunit';
+import { test } from 'ember-qunit';
+import startApp from '../helpers/start-app';
+import registerStoreHelpers from '../helpers/store-helpers';
+
+var app = null;
+module('Unit: Task Model', {
+  beforeEach: function() {
+    app = startApp();
+  },
+  afterEach: function() {
+    Ember.run(app, app.destroy);
+  }
 });
 
-test("findQuestion finds and returns the first nestedQuestion when the given path matches its ident exactly", function(assert) {
-  let store = this.store(),
-    task, nestedQuestion;
+ test("findQuestion finds and returns the first nestedQuestion when the given path matches its ident exactly", function(assert) {
+   let store = getStore();
+   let task, nestedQuestion;
+   Ember.run(() => {
+     task = store.createRecord('task');
+     nestedQuestion = store.createRecord('nested-question', {
+       ident: "foobar",
+       task: task
+     });
+     task.get('nestedQuestions').addObject(nestedQuestion);
+     return task;
+   });
+   assert.equal(task.findQuestion('foobar'), nestedQuestion);
+ });
 
+ test("findQuestion returns null when it doesn't have a nestedQuestion whose ident that matches the given path", function(assert) {
+   let store = getStore();
+   let task, nestedQuestion;
+   Ember.run(() => {
+     task = store.createRecord('task');
+     nestedQuestion = store.createRecord('nested-question', {
+       ident: "foobar",
+       task: task
+     });
+     task.get('nestedQuestions').addObject(nestedQuestion);
+     return task;
+   });
+   assert.equal(task.findQuestion('bazbaz'), null);
+});
+
+test("permissionState delegates permission state to paper", function(assert) {
+  registerStoreHelpers();
+  let store = getStore();
+  let paper, task;
   Ember.run(() => {
-    task = store.createRecord('task');
-    nestedQuestion = store.createRecord('nested-question', {
-      ident: "foobar",
-      task: task
+    paper = store.createRecord('paper', {
+      permissionState: 'submitted'
     });
-    task.get('nestedQuestions').addObject(nestedQuestion);
-    return task;
+    task = store.createRecord('task', {
+      paper: paper
+    });
   });
-
-  assert.equal(task.findQuestion('foobar'), nestedQuestion);
+  assert.equal(task.get('permissionState'), 'submitted');
 });
 
-test("findQuestion returns null when it doesn't have a nestedQuestion whose ident that matches the given path", function(assert) {
-  let store = this.store(),
-    task, nestedQuestion;
-
-  Ember.run(() => {
-    task = store.createRecord('task');
-    nestedQuestion = store.createRecord('nested-question', {
-      ident: "foobar",
-      task: task
-    });
-    task.get('nestedQuestions').addObject(nestedQuestion);
-    return task;
-  });
-
-  assert.equal(task.findQuestion('bazbaz'), null);
-});

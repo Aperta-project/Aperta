e74aee1360b7ebccc4d0b22c8b194efb7160adee
Sam Bleckley <sam@mutuallyhuman.com>
2016-04-28 15:50:16 -0400

    APERTA-5458 RelatedArticleController, permission
    And specs!

--- config/routes.rb
+++ config/routes.rb
@@ -122,12 +122,13 @@ Tahi::Application.routes.draw do
     resources :permissions, only: [:show]
     resources :question_attachments, only: [:create, :update, :show, :destroy]
     resources :questions, only: [:create, :update]
-
     resources :nested_questions, only: [:index] do
       resources :answers, only: [:create, :update, :destroy], controller: 'nested_question_answers'
     end
 
     resources :old_roles, only: [:show, :create, :update, :destroy]
+
+    resources :related_articles, only: [:show, :create, :update, :destroy]
     resources :tasks, only: [:update, :create, :show, :destroy] do
       get :nested_questions
       get :nested_question_answers

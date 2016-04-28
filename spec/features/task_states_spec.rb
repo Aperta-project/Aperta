201f5fb31acc35af4e6c606a982e65c5645459e6
Zach Dennis <zach.dennis@gmail.com>
2016-04-28 16:51:45 -0400

    Update spec/features/task_states_spec to explicitly use :with_creator when creating the unsubmitted paper
    Resolves spec failure
    
    Traired with Chris Westra (@Bestra) and Tad Park (@tadp)
    
    APERTA-6137

--- spec/features/task_states_spec.rb
+++ spec/features/task_states_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'Task states permissions', js: true do
   let(:submitted_paper) { FactoryGirl.create(:paper, :with_integration_journal, :submitted) }
   let(:submitted_paper_author) { User.first }
-  let(:unsubmitted_paper) { FactoryGirl.create(:paper, :unsubmitted, journal: Journal.first) }
+  let(:unsubmitted_paper) { FactoryGirl.create(:paper, :unsubmitted, :with_creator, journal: Journal.first) }
   let(:unsubmitted_paper_author) { unsubmitted_paper.creator }
   let(:staff_admin) { FactoryGirl.create(:user) }
   let(:task) { FactoryGirl.create(:data_availability_task, paper: submitted_paper) }

99a64a379c2afab83a2078a47527ede2ec41b541
Sam Bleckley <sam@mutuallyhuman.com>
2016-05-02 10:36:45 -0400

    APETA-5458 Tests and test blueprints for engines

---
+++ engines/tahi_standard_tasks/client/test-support/integration/related-articles-task-test.js
@@ -0,0 +1,31 @@
+import {module, test} from 'qunit';
+import qunit from 'qunit';
+
+//import hbs from 'htmlbars-inline-precompile';
+
+
+module('related-articles-task'
+                   //{integration: true}
+                   );
+
+// var template = hbs`{{related-articles-task task=task}}`;
+
+// var task = function (){
+//   return {
+//     id: 2,
+//     title: 'Related Articles',
+//     type: 'TahiStandardTasks::RelatedArticles',
+//     completed: false,
+//     isMetadataTask: false,
+//     isSubmissionTask: false,
+//     assignedToMe: false
+//   };
+// };
+
+test('a very important test', function(assert){
+  console.log(arguments);
+  // this.set('task', task());
+
+  // this.render(template);
+  assert(false);
+});

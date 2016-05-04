263844a1845b539242714b4f44506ecd25495860
Sara Gibbons <saragibby@gmail.com>
2016-05-04 09:46:57 -0400

    switch from computed properties to Ember computed methods [APERTA-6404]

--- client/app/pods/components/nested-snapshot/component.js
+++ client/app/pods/components/nested-snapshot/component.js
@@ -7,46 +7,29 @@ export default Ember.Component.extend({
   classNames: ['snapshot'],
   classNameBindings: ['levelClassName'],
 
-  levelClassName: Ember.computed('nestedLevel', function(){
-    return `nested-level-${this.get('nestedLevel')}`;
-  }),
-
   generalCase: Ember.computed.not('specialCase'),
   specialCase: Ember.computed.or(
     'authorsTask', 'figure', 'supportingInfo', 'funder'),
 
-  // General Case
+  authorsTask: Ember.computed.equal('snapshot1.name', 'authors-task'),
+  boolean: Ember.computed.equal('snapshot1.type', 'boolean'),
+  booleanQuestion: Ember.computed.equal(
+    'snapshot1.value.answer_type',
+    'boolean'),
+  figure: Ember.computed.equal('snapshot1.name', 'figure-task'),
+  funder: Ember.computed.equal('snapshot1.name', 'funder'),
+  id: Ember.computed.equal('snapshot1.name', 'id'),
+  integer: Ember.computed.equal('snapshot1.type', 'integer'),
+  question: Ember.computed.equal('snapshot1.type', 'question'),
+  supportingInfo: Ember.computed.equal(
+    'snapshot1.name',
+    'supporting-information-task'),
+  text: Ember.computed.equal('snapshot1.type', 'text'),
+  text_or_integer: Ember.computed.or('integer', 'text'),
+  userEnteredValue: Ember.computed.not('id'),
+
   raw: Ember.computed('snapshot1.type', function(){
-    let type = this.get('snapshot1.type');
-    return type === 'text' || type === 'integer';
-  }),
-
-  boolean: Ember.computed('snapshot1.type', function(){
-    return this.get('snapshot1.type') === 'boolean';
-  }),
-
-  booleanQuestion: Ember.computed('snapshot1.value.answer_type', function(){
-    return this.get('snapshot1.value.answer_type') === 'boolean';
-  }),
-
-  question: Ember.computed('snapshot1.type', function(){
-    return this.get('snapshot1.type') === 'question';
-  }),
-
-  figure: Ember.computed('snapshot1.name', function(){
-    return this.get('snapshot1.name') === 'figure-task';
-  }),
-
-  authorsTask: Ember.computed('snapshot1.name', function(){
-    return this.get('snapshot1.name') === 'authors-task';
-  }),
-
-  supportingInfo: Ember.computed('snapshot1.name', function(){
-    return this.get('snapshot1.name') === 'supporting-information-task';
-  }),
-
-  funder: Ember.computed('snapshot1.name', function(){
-    return this.get('snapshot1.name') === 'funder';
+    return this.get('text_or_integer') && this.get('userEnteredValue');
   }),
 
   children: Ember.computed(
@@ -56,7 +39,12 @@ export default Ember.Component.extend({
       return _.zip(
         this.get('snapshot1.children'),
         this.get('snapshot2.children') || []);
-    }),
+    }
+  ),
+
+  levelClassName: Ember.computed('nestedLevel', function(){
+    return `nested-level-${this.get('nestedLevel')}`;
+  }),
 
   incrementedNestedLevel: Ember.computed('nestedLevel', function(){
     return this.incrementProperty('nestedLevel');

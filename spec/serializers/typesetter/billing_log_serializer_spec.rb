ee58b54122816beccd91fadef5dfabb9d8597858
Mauro Porras P <mauroporrasp@gmail.com>
2016-05-09 09:26:30 -0500

    Add test cases for all the fields in the billing log serializer

--- spec/serializers/typesetter/billing_log_serializer_spec.rb
+++ spec/serializers/typesetter/billing_log_serializer_spec.rb
@@ -44,17 +44,12 @@ describe Typesetter::BillingLogSerializer do
     paper.phases.first.tasks.push(*metadata_tasks)
   end
 
-  it 'has short_title' do
-    expect(output[:short_title]).to eq('my paper short')
-  end
-
   describe 'doi' do
     let(:our_task) do
       paper_task.call('PlosBilling::BillingTask')
     end
 
     before do
-      
     end
 
     it 'has doi' do
@@ -74,11 +69,6 @@ describe Typesetter::BillingLogSerializer do
     expect(output[:paper_type]).to eq('Pandas')
   end
 
-  it 'has journal_title' do
-    paper.journal.name = 'Pandas'
-    expect(output[:journal_title]).to eq('Pandas')
-  end
-
   it 'has first_submitted_at' do
     now = Time.zone.now
     paper.update_attribute(:first_submitted_at, now)
@@ -96,145 +86,138 @@ describe Typesetter::BillingLogSerializer do
     expect(output[:paper_title]).to eq('here is the title')
   end
 
-  describe 'publication_date' do
-    let(:our_task) do
-      paper_task.call('TahiStandardTasks::ProductionMetadataTask')
-    end
-
-    context "with valid date" do
-      before do
-        allow_any_instance_of(TahiStandardTasks::ProductionMetadataTask)
-          .to receive(:publication_date).and_return("11/16/2015")
-      end
-
-      it 'has a date' do
-        expect(output[:publication_date]).to eq(Date.new(2015, 11, 16))
-      end
-    end
-
-    context "with no date" do
-      before do
-        allow_any_instance_of(TahiStandardTasks::ProductionMetadataTask)
-          .to receive(:publication_date).and_return(nil)
-      end
-
-      it 'accepts no publication date' do
-        expect(output[:publication_date]).to be_nil
-      end
-    end
+  it 'has journal_id' do
+    paper.journal_id = '1234'
+    expect(output[:journal_id]).to eq('1234')
   end
 
-  shared_examples_for 'serializes :has_one paper task' do |opts|
-    opts[:factory] || fail(ArgumentError, 'Must pass in a :factory')
-    opts[:serializer] || fail(ArgumentError, 'Must pass in a :serializer')
-    opts[:json_key] || fail(ArgumentError, 'Must pass in a :json_key')
-
-    let(:task) do
-      FactoryGirl.create(opts[:factory], phase: paper.phases.first)
-    end
-    let(:fake_serialized_data) { 'Fake serialized data' }
-    let(:fake_instance_double) do
-      instance_double(
-        "#{opts[:serializer]}",
-        serializable_hash: fake_serialized_data
-      )
-    end
-
-    before do
-      expect(opts[:serializer]).to receive(:new).and_return fake_instance_double
-    end
-
-    it "serializes the #{opts[:json_key]} using the #{opts[:serializer]}" do
-      actual_output = output[opts[:json_key]]
-      expect(actual_output).to eq(fake_serialized_data)
-    end
+  it 'has firstname' do
+    paper.firstname = 'Foo'
+    expect(output[:firstname]).to eq('Foo')
   end
 
-  shared_examples_for 'serializes :has_many property' do |opts|
-    opts[:property] || fail(ArgumentError, 'Must pass in a :property')
-    opts[:factory] || fail(ArgumentError, 'Must pass in a :factory')
-    opts[:serializer] || fail(ArgumentError, 'Must pass in a :serializer')
-    opts[:json_key] || fail(ArgumentError, 'Must pass in a :json_key')
-
-    let(:has_many_property_value) do
-      [FactoryGirl.build(opts[:factory])]
-    end
-    let(:fake_serialized_data) { 'Fake serialized data' }
-    let(:fake_instance_double) do
-      instance_double(
-        "#{opts[:serializer]}",
-        serializable_hash: fake_serialized_data
-      )
-    end
-
-    before do
-      expected_message = opts[:message_chain] || opts[:property]
-      allow(paper).to receive_message_chain(expected_message)
-        .and_return has_many_property_value
-      expect(opts[:serializer]).to receive(:new).and_return fake_instance_double
-    end
-
-    it "serializes the #{opts[:json_key]} using the #{opts[:serializer]}" do
-      actual_output = output[opts[:json_key]]
-      expect(actual_output).to eq([fake_serialized_data])
-    end
+  it 'has middlename' do
+    paper.middlename = 'Biz'
+    expect(output[:middlename]).to eq('Biz')
   end
 
-  context 'academic_editors' do
-    include_examples(
-      'serializes :has_many property',
-      property: :academic_editors,
-      factory: :user,
-      serializer: Typesetter::EditorSerializer,
-      json_key: :academic_editors
-    )
+  it 'has lastname' do
+    paper.lastname = 'Buz'
+    expect(output[:lastname]).to eq('Buz')
   end
 
-  context 'competing_interests' do
-    include_examples(
-      'serializes :has_one paper task',
-      factory: :competing_interests_task,
-      serializer: Typesetter::CompetingInterestsSerializer,
-      json_key: :competing_interests
-    )
+  it 'has institute' do
+    paper.institute = 'Sample Institute'
+    expect(output[:institute]).to eq('Sample Institute')
   end
 
-  context 'data_availability' do
-    include_examples(
-      'serializes :has_one paper task',
-      factory: :data_availability_task,
-      serializer: Typesetter::DataAvailabilitySerializer,
-      json_key: :data_availability
-    )
+  it 'has department' do
+    paper.department = 'Sample Department'
+    expect(output[:department]).to eq('Sample Department')
   end
 
-  context 'financial_disclosure' do
-    include_examples(
-      'serializes :has_one paper task',
-      factory: :financial_disclosure_task,
-      serializer: Typesetter::FinancialDisclosureSerializer,
-      json_key: :financial_disclosure
-    )
+  it 'has address1' do
+    paper.address1 = '42 Wallaby Way, Sydney'
+    expect(output[:address1]).to eq('42 Wallaby Way, Sydney')
   end
 
-  context 'authors' do
-    include_examples(
-      'serializes :has_many property',
-      property: :authors,
-      factory: :author,
-      serializer: Typesetter::AuthorSerializer,
-      json_key: :authors
-    )
+  it 'has address2' do
+    paper.address2 = '742 Evergreen Terrace'
+    expect(output[:address2]).to eq('742 Evergreen Terrace')
   end
 
-  context 'supporting_information_files' do
-    include_examples(
-      'serializes :has_many property',
-      property: :supporting_information_files,
-      message_chain: 'supporting_information_files.publishable',
-      factory: :supporting_information_file,
-      serializer: Typesetter::SupportingInformationFileSerializer,
-      json_key: :supporting_information_files
-    )
+  it 'has address3' do
+    paper.address3 = '1600 Pennsylvania Ave NW'
+    expect(output[:address3]).to eq('1600 Pennsylvania Ave NW')
+  end
+
+  it 'has city' do
+    paper.city = 'Sacramento'
+    expect(output[:city]).to eq('Sacramento')
+  end
+
+  it 'has state' do
+    paper.state = 'California'
+    expect(output[:state]).to eq('California')
+  end
+
+  it 'has zip' do
+    paper.zip = '54321'
+    expect(output[:zip]).to eq('54321')
+  end
+
+  it 'has country' do
+    paper.country = 'United States'
+    expect(output[:country]).to eq('United States')
+  end
+
+  it 'has phone1' do
+    paper.phone1 = '3214567'
+    expect(output[:phone1]).to eq('3214567')
+  end
+
+  it 'has phone2' do
+    paper.phone2 = '4561234'
+    expect(output[:phone2]).to eq('4561234')
+  end
+
+  it 'has fax' do
+    paper.fax = '7894321'
+    expect(output[:fax]).to eq('7894321')
+  end
+
+  it 'has email' do
+    paper.email = 'user@domain.com'
+    expect(output[:email]).to eq('user@domain.com')
+  end
+
+  it 'has pubdnumber' do
+    paper.pubdnumber = 'This is a sample pubdnumber'
+    expect(output[:pubdnumber]).to eq('This is a sample pubdnumber')
+  end
+
+  it 'has dtitle' do
+    paper.dtitle = 'This is a sample dtitle'
+    expect(output[:dtitle]).to eq('This is a sample dtitle')
+  end
+
+  it 'has fundRef' do
+    paper.fundRef = 'This is a sample fundRef'
+    expect(output[:fundRef]).to eq('This is a sample fundRef')
+  end
+
+  it 'has collectionID' do
+    paper.collectionID = '54321'
+    expect(output[:collectionID]).to eq('54321')
+  end
+
+  it 'has collection' do
+    paper.collection = 'This is a sample collection'
+    expect(output[:collection]).to eq('This is a sample collection')
+  end
+
+  it 'has direct_bill_response' do
+    paper.direct_bill_response = 'This is a sample direct_bill_response'
+    expect(output[:direct_bill_response]).to eq('This is a sample direct_bill_response')
+  end
+
+  it 'has gpi_response' do
+    paper.gpi_response = 'This is a sample gpi_response'
+    expect(output[:gpi_response]).to eq('This is a sample gpi_response')
+  end
+
+  it 'has final_dispo_accept' do
+    paper.final_dispo_accept = 'This is a sample final_dispo_accept'
+    expect(output[:final_dispo_accept]).to eq('This is a sample final_dispo_accept')
+  end
+
+  it 'has category' do
+    paper.category = 'This is a sample category'
+    expect(output[:category]).to eq('This is a sample category')
+  end
+
+  it 'has s3_url' do
+    paper.s3_url = 'https://s3-eu-west-1.amazonaws.com/foo/bar'
+    expect(output[:s3_url]).to eq('https://s3-eu-west-1.amazonaws.com/foo/bar')
   end
 end

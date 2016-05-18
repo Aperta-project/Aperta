1d6fc9e81a99d4908b71dc8a66bffe7c07976fb7
Sara Gibbons <sara@mutuallyhuman.com>
2016-05-18 16:21:36 -0400

    switch conditional logic to turn off ssl verify mode missed when switching from DISABLE to ENABLE logic with TahiEnv changes [APERTA-6697]

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,7 +1,7 @@
 Pusher.url = ENV.fetch('PUSHER_URL')
 Pusher.logger = Rails.logger
 
-if TahiEnv.pusher_ssl_verify?
+unless TahiEnv.pusher_ssl_verify?
   Pusher.default_client.sync_http_client.ssl_config.verify_mode =
     OpenSSL::SSL::VERIFY_NONE
 end

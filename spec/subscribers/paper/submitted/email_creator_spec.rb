e31e7fe6d258a3c379c740af5d584a662480902d
Matt Mercieca <mercieca@gmail.com>
2016-05-19 14:10:32 -0400

    APERTA-6772 Updating specs for new emails

--- spec/subscribers/paper/submitted/email_creator_spec.rb
+++ spec/subscribers/paper/submitted/email_creator_spec.rb
@@ -10,15 +10,31 @@ describe Paper::Submitted::EmailCreator do
 
   describe "when a paper is submitted" do
     it "notifies the creator of a submission" do
+      allow(paper).to receive(:previous_changes).and_return(
+        publishing_state: ["unsubmitted", "submitted"])
       expect(mailer).to receive(:notify_creator_of_paper_submission).with(paper.id)
-      described_class.call("tahi:paper:submitted", event_name: 'submit',
-                                                   record: paper)
+      described_class.call("tahi:paper:submitted", record: paper)
     end
 
-    it "notifies the creator of a minor revision submission" do
+    it "notifies the creator of a revision (major or minor) submission" do
+      allow(paper).to receive(:previous_changes).and_return(
+        publishing_state: ["in_revision", "submitted"])
       expect(mailer).to receive(:notify_creator_of_revision_submission).with(paper.id)
-      described_class.call("tahi:paper:submitted", event_name: 'submit_minor_revision',
-                                                   record: paper)
+      described_class.call("tahi:paper:submitted", record: paper)
+    end
+
+    it "notifies the creator of a tech check submission" do
+      allow(paper).to receive(:previous_changes).and_return(
+        publishing_state: ["checking", "submitted"])
+      expect(mailer).to receive(:notify_creator_of_check_submission).with(paper.id)
+      described_class.call("tahi:paper:submitted", record: paper)
+    end
+
+    it "defaults to the submission email" do
+      allow(paper).to receive(:previous_changes).and_return(
+        publishing_state: ["does_not_exist", "submitted"])
+      expect(mailer).to receive(:notify_creator_of_paper_submission).with(paper.id)
+      described_class.call("tahi:paper:submitted", record: paper)
     end
   end
 end

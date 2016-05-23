a1fa2c776a91dbe61a684fdd10e9f3cb4426cdbb
Tad Park <tadp@users.noreply.github.com>
2016-05-23 16:04:18 -0700

    APERTA-5475 Return the report back after create_report method

--- app/models/billing_log_report.rb
+++ app/models/billing_log_report.rb
@@ -29,7 +29,7 @@ class BillingLogReport < ActiveRecord::Base
 
   def self.create_report(from_date: nil)
     from_date ||= BillingLogReport.last.created_at if BillingLogReport.any?
-    BillingLogReport.new(from_date: from_date).save_and_send_to_s3!
+    BillingLogReport.new(from_date: from_date).tap(&:save_and_send_to_s3!)
   end
 
   def save_and_send_to_s3!

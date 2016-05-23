97757c5d291b978c0e5ca502e22282932d2a546a
Tad Park <tadp@users.noreply.github.com>
2016-05-23 10:14:51 -0700

    APERTA-5475 Require csv

--- app/models/billing_log.rb
+++ app/models/billing_log.rb
@@ -1,4 +1,6 @@
 # This table stores the information that can be exported to csv for processing
+require 'csv'
+
 class BillingLog < ActiveRecord::Base
   belongs_to :paper, class_name: 'Paper', foreign_key: 'documentid'
   belongs_to :journal

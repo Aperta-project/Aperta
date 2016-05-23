9797d4fb5d4d90e1af36fdcc42606656f0a4c355
Chris Westra <cwestra@gmail.com>
2016-05-23 11:42:10 -0400

    APERTA-6831 add comment for CI

--- client/app/lib/api-path-helpers.js
+++ client/app/lib/api-path-helpers.js
@@ -1,3 +1,8 @@
+/**
+ * This file functions as a grab bag of routes.
+ * Eventually we might want to refactor this into an object but
+ * for now top-level functions will do just fine.
+ */
 export function eligibleUsersPath(taskId, userType) {
   return `api/tasks/${taskId}/eligible_users/${userType}`;
 }

bf1e58e319261bc430630b6b2dff310d0c3cfbea
AJ Hekman <aj@mutuallyhuman.com>
2016-05-23 20:42:03 -0400

    APERTA-6584 use inline styles for PDF

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.initialize_on_precompile = true
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|woff2)$/
-Rails.application.config.assets.precompile += %w(email.scss auth.scss pdf.scss)
+Rails.application.config.assets.precompile += %w(email.scss auth.scss)
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')

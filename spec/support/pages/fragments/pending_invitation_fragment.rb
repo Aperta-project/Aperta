ce09c40ae03ee668b0f9fb13d063276d7578ef93
Zach Dennis <zach.dennis@gmail.com>
2016-05-26 16:01:50 -0400

    Updating the inviting_academic_editor button text.
    APERTA-6872

--- spec/support/pages/fragments/pending_invitation_fragment.rb
+++ spec/support/pages/fragments/pending_invitation_fragment.rb
@@ -1,13 +1,13 @@
 class PendingInvitationFragment < PageFragment
-  def accept
+  def accept(button_text='Accept')
     element_text = element.text
-    click_button 'yes'
+    click_button button_text
     session.has_content? element_text
   end
 
-  def reject
+  def reject(button_text='Decline')
     element_text = element.text
-    click_button 'no'
+    click_button button_text
     session.has_content? element_text
   end
 end

3ae224e3b4b444d7a1073eef83c90281b79b711b
Zach Dennis <zach.dennis@gmail.com>
2016-06-01 18:13:17 -0400

    Filled in the client-side front matter reviewer report integration test
    Paired with Mauro Porras (@mporras)
    
    APERTA-6846

--- engines/tahi_standard_tasks/client/test-support/integration/components/tasks/front-matter-reviewer-report-task-test.js
+++ engines/tahi_standard_tasks/client/test-support/integration/components/tasks/front-matter-reviewer-report-task-test.js
@@ -142,26 +142,26 @@ test('Readonly mode: Not able to provide reviewer feedback', function(assert) {
   const url = '/papers/' + currentPaper.id + '/tasks/' + taskId;
   Factory.createPermission('FrontMatterReviewerReportTask', taskId, ['view']);
   return visit(url).then(function() {
-    assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=accept]'), 'User cannot provide an accept for publication recommendation');
-    assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=reject]'), 'User cannot provide a reject recommendation');
-    assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=major_revision]'), 'User cannot provide a major revision recommendation');
-    assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=minor_revision]'), 'User cannot provide a minor revision recommendation');
+    assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=accept]', 'User cannot provide an accept for publication recommendation');
+    assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=reject]', 'User cannot provide a reject recommendation');
+    assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=major_revision]', 'User cannot provide a major revision recommendation');
+    assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=minor_revision]', 'User cannot provide a minor revision recommendation');
 
-    assert.elementNotFound('textarea[name=front_matter_reviewer_report--competing_interests]'), 'User cannot provide their competing interests statement');
+    assert.elementNotFound('textarea[name=front_matter_reviewer_report--competing_interests]', 'User cannot provide their competing interests statement');
 
-    assert.elementNotFound('textarea[name=front_matter_reviewer_report--competing_interests]'), 'User cannot provide their competing interests statement');
+    assert.elementNotFound('textarea[name=front_matter_reviewer_report--competing_interests]', 'User cannot provide their competing interests statement');
 
-    assert.elementNotFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=true]'), 'User cannot provide yes response to biology suitability');
-    assert.elementNotFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=false]'), 'User cannot provide no response to biology suitability');
-    assert.elementNotFound('textarea[name=front_matter_reviewer_report--suitable--comment]'), 'User cannot provide their review of biology suitability');
+    assert.elementNotFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=true]', 'User cannot provide yes response to biology suitability');
+    assert.elementNotFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=false]', 'User cannot provide no response to biology suitability');
+    assert.elementNotFound('textarea[name=front_matter_reviewer_report--suitable--comment]', 'User cannot provide their review of biology suitability');
 
-    assert.elementNotFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=true]'), 'User cannot provide respond yes to statistical analysis');
-    assert.elementNotFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=false]'), 'User cannot provide response no to statistical analysis');
-    assert.elementNotFound('textarea[name=front_matter_reviewer_report--includes_unpublished_data--explanation]'), 'User cannot provide their review of statistical analysis');
+    assert.elementNotFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=true]', 'User cannot provide respond yes to statistical analysis');
+    assert.elementNotFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=false]', 'User cannot provide response no to statistical analysis');
+    assert.elementNotFound('textarea[name=front_matter_reviewer_report--includes_unpublished_data--explanation]', 'User cannot provide their review of statistical analysis');
 
-    assert.elementNotFound('textarea[name=front_matter_reviewer_report--additional_comments]'), 'User cannot provide additional comments');
+    assert.elementNotFound('textarea[name=front_matter_reviewer_report--additional_comments]', 'User cannot provide additional comments');
 
-    assert.elementNotFound('textarea[name=front_matter_reviewer_report--identity]'), 'User cannot provide their identity');
+    assert.elementNotFound('textarea[name=front_matter_reviewer_report--identity]', 'User cannot provide their identity');
   });
 });
 
@@ -170,25 +170,25 @@ test('Edit mode: Providing reviewer feedback', function(assert) {
   const url = '/papers/' + currentPaper.id + '/tasks/' + taskId;
   Factory.createPermission('FrontMatterReviewerReportTask', taskId, ['edit']);
   return visit(url).then(function() {
-    assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=accept]'), 'User can provide an accept for publication recommendation');
-    assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=reject]'), 'User can provide a reject recommendation');
-    assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=major_revision]'), 'User can provide a major revision recommendation');
-    assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=minor_revision]'), 'User can provide a minor revision recommendation');
+    assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=accept]', 'User can provide an accept for publication recommendation');
+    assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=reject]', 'User can provide a reject recommendation');
+    assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=major_revision]', 'User can provide a major revision recommendation');
+    assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=minor_revision]', 'User can provide a minor revision recommendation');
 
-    assert.elementFound('textarea[name=front_matter_reviewer_report--competing_interests]'), 'User can provide their competing interests statement');
+    assert.elementFound('textarea[name=front_matter_reviewer_report--competing_interests]', 'User can provide their competing interests statement');
 
-    assert.elementFound('textarea[name=front_matter_reviewer_report--competing_interests]'), 'User can provide their competing interests statement');
+    assert.elementFound('textarea[name=front_matter_reviewer_report--competing_interests]', 'User can provide their competing interests statement');
 
-    assert.elementFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=true]'), 'User can respond yes to biology suitability');
-    assert.elementFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=false]'), 'User can respond no to biology suitability');
-    assert.elementFound('textarea[name=front_matter_reviewer_report--suitable--comment]'), 'User can provide their review of biology suitability');
+    assert.elementFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=true]', 'User can respond yes to biology suitability');
+    assert.elementFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=false]', 'User can respond no to biology suitability');
+    assert.elementFound('textarea[name=front_matter_reviewer_report--suitable--comment]', 'User can provide their review of biology suitability');
 
-    assert.elementFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=true]'), 'User can provide respond yes to statistical analysis');
-    assert.elementFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=false]'), 'User can provide response no to statistical analysis');
-    assert.elementFound('textarea[name=front_matter_reviewer_report--includes_unpublished_data--explanation]'), 'User can provide their review of statistical analysis');
+    assert.elementFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=true]', 'User can provide respond yes to statistical analysis');
+    assert.elementFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=false]', 'User can provide response no to statistical analysis');
+    assert.elementFound('textarea[name=front_matter_reviewer_report--includes_unpublished_data--explanation]', 'User can provide their review of statistical analysis');
 
-    assert.elementFound('textarea[name=front_matter_reviewer_report--additional_comments]'), 'User can provide additional comments');
+    assert.elementFound('textarea[name=front_matter_reviewer_report--additional_comments]', 'User can provide additional comments');
 
-    assert.elementFound('textarea[name=front_matter_reviewer_report--identity]'), 'User can provide their identity');
+    assert.elementFound('textarea[name=front_matter_reviewer_report--identity]', 'User can provide their identity');
   });
 });

a2f2cbfc4899fe95436567513a86cdbff5c92243
Sebastian Bassi <sebastian.bassi@globant.com>
2016-06-01 17:14:17 -0700

    Test removing click completion button in the ms page since was pressed in billing method

--- test/frontend/Pages/manuscript_viewer.py
+++ test/frontend/Pages/manuscript_viewer.py
@@ -552,11 +552,14 @@ class ManuscriptViewerPage(AuthenticatedPage):
       billing_task = BillingTask(self._driver)
       billing_task.complete(data)
       # complete_billing task
+      task.click()
+      """
       if not base_task.completed_state():
         base_task.click_completion_button()
         manuscript_id_text = self._get(self._paper_sidebar_manuscript_id)
         self._actions.move_to_element(manuscript_id_text).perform()
         task.click()
+      """
       time.sleep(1)
     elif task_name == 'Revise Manuscript':
       revise_manuscript = ReviseManuscriptTask(self._driver)

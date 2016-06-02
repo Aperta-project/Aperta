23103401eb4c6f9d4a30c0517da82071269fa8a1
Sara Gibbons <sara@mutuallyhuman.com>
2016-06-02 16:00:38 -0400

    add flash message to invitations overlay [APERTA-6565]

--- client/app/pods/components/invitations-x/component.js
+++ client/app/pods/components/invitations-x/component.js
@@ -1,23 +1,28 @@
 import Ember from 'ember';
 import EscapeListenerMixin from 'tahi/mixins/escape-listener';
-
 export default Ember.Component.extend(EscapeListenerMixin, {
+  flash: Ember.inject.service(),
 
   hasInvitations: Ember.computed.notEmpty('invitations'),
 
-  closeOverlay: function() {
+  closeOverlayIfLast: function() {
     if(!this.get('hasInvitations')) {
       this.get('close')();
     }
   },
 
+  invitationFeedbackIsBlank: function(invitation) {
+    return Ember.isBlank(invitation.get('reviewerSuggestions')) &&
+      Ember.isBlank(invitation.get('declineReason'));
+  },
+
   actions: {
     close(){
-      this.closeOverlay();
+      this.closeOverlayIfLast();
     },
 
     accept(invitation) {
-      this.get('accept')(invitation).then(()=>{this.closeOverlay()});
+      this.get('accept')(invitation).then(()=>{this.closeOverlayIfLast()});
     },
 
     aquireFeedback(invitation) {
@@ -26,7 +31,14 @@ export default Ember.Component.extend(EscapeListenerMixin, {
     },
 
     update(invitation) {
-      this.get('update')(invitation).then(()=>{this.closeOverlay()});
+      if (this.invitationFeedbackIsBlank(invitation)){
+        invitation.feedbackSent();
+        return this.closeOverlayIfLast();
+      }
+
+      this.get('update')(invitation).then(()=>{
+        this.get('flash').displayMessage('success', 'Thank you for your feedback!');
+        this.closeOverlayIfLast()});
     }
   }
 });

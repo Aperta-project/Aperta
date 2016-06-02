561c1137971b3892d128a1121d06ca2beaf3cbb3
Chris Westra <cwestra@gmail.com>
2016-06-02 14:35:45 -0400

    APERTA-5664 PR feedback fixes
    - Change email based on newest google doc
    - Change case statement to if/else
    - Misc test tweaks

--- app/subscribers/paper/submitted/email_creator.rb
+++ app/subscribers/paper/submitted/email_creator.rb
@@ -3,17 +3,14 @@ class Paper::Submitted::EmailCreator
     paper = event_data[:record]
     previous_state = paper.previous_changes[:publishing_state][0]
 
-    case previous_state
-    when 'in_revision'
+    if previous_state == 'in_revision'
       UserMailer.delay.notify_creator_of_revision_submission(paper.id)
-    when 'checking'
+    elsif previous_state == 'checking'
       UserMailer.delay.notify_creator_of_check_submission(paper.id)
+    elsif paper.publishing_state == "initially_submitted"
+      UserMailer.delay.notify_creator_of_initial_submission(paper.id)
     else
-      if paper.publishing_state == "initially_submitted"
-        UserMailer.delay.notify_creator_of_initial_submission(paper.id)
-      else
-        UserMailer.delay.notify_creator_of_paper_submission(paper.id)
-      end
+      UserMailer.delay.notify_creator_of_paper_submission(paper.id)
     end
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -39,7 +39,7 @@ describe UserMailer, redis: true do
     it_behaves_like "recipient without email address"
 
     it 'sends the email to the inivitees email address with correct subject' do
-      expect(email.to).to include(invitee.email)
+      expect(email.to).to contain_exactly(invitee.email)
       expect(email.subject).to eq "You've been added as a collaborator to the manuscript, \"#{paper.display_title}\""
     end
 
@@ -57,8 +57,8 @@ describe UserMailer, redis: true do
     it_behaves_like "invitor is not available"
     it_behaves_like "recipient without email address"
 
-    it 'sends the email to the inivitees email address with the correct subject' do
-      expect(email.to).to include(invitee.email)
+    it 'sends the email to the invitees email address with the correct subject' do
+      expect(email.to).to contain_exactly(invitee.email)
       expect(email.subject).to eq "You've been added to a conversation on the manuscript, \"#{task.paper.display_title}\""
     end
 
@@ -99,7 +99,7 @@ describe UserMailer, redis: true do
     let(:email) { UserMailer.assigned_editor(invitee.id, task.paper.id) }
 
     it 'sends the email to the inivitees email address with correct subject' do
-      expect(email.to).to include(invitee.email)
+      expect(email.to).to contain_exactly(invitee.email)
       expect(email.subject).to eq "You've been assigned as an editor for the manuscript, \"#{task.paper.display_title}\""
     end
 
@@ -119,7 +119,7 @@ describe UserMailer, redis: true do
     it_behaves_like "recipient without email address"
 
     it 'sends the email to the mentioned user with the correct subject' do
-      expect(email.to).to eq [invitee.email]
+      expect(email.to).to contain_exactly(invitee.email)
       expect(email.subject).to eq "You've been mentioned on the manuscript, \"#{task.paper.display_title}\""
     end
 
@@ -140,7 +140,7 @@ describe UserMailer, redis: true do
     let(:email) { UserMailer.notify_creator_of_paper_submission(paper.id) }
 
     it "sends the email to the paper's creator with the correct subject" do
-      expect(email.to).to eq [author.email]
+      expect(email.to).to contain_exactly(author.email)
       expect(email.subject).to eq "Thank you for submitting your manuscript to #{paper.journal.name}"
     end
 
@@ -159,19 +159,18 @@ describe UserMailer, redis: true do
     let(:email) { UserMailer.notify_creator_of_initial_submission(paper.id) }
 
     it "sends the email to the paper's creator with the correct subject" do
-      expect(email.to).to eq [author.email]
+      expect(email.to).to contain_exactly(author.email)
       expect(email.subject).to eq "Thank you for submitting to #{paper.journal.name}"
     end
 
     it "includes key points in the text" do
-      expect(email.body).to include "If the editorial evaluation is positive"
+      expect(email.body).to include "whether your manuscript meets the criteria"
       expect(email.body).to include paper.title
       expect(email.body).to include paper.journal.name
     end
   end
 
   describe '#notify_admin_of_paper_submission' do
-    let(:author) { FactoryGirl.create(:user) }
     let(:admin) { FactoryGirl.create(:user) }
     let(:paper) do
       FactoryGirl.create(
@@ -184,7 +183,7 @@ describe UserMailer, redis: true do
     let(:email) { UserMailer.notify_admin_of_paper_submission(paper.id, admin.id) }
 
     it "send email to the paper's admin with the correct subject" do
-      expect(email.to).to eq [admin.email]
+      expect(email.to).to contain_exactly(admin.email)
       expect(email.subject).to eq "New manuscript submitted to PLOS #{paper.journal.name}: \"#{paper.display_title}\""
     end
 

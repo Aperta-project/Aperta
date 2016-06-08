983ace70dddb67a60416dbc7e01e6e8d8e4b3bd9
Tad Park <tadp@users.noreply.github.com>
2016-06-08 14:42:22 -0700

    APERTA-7014 Clean up reference to institutional accounts

--- app/controllers/institutional_accounts_controller.rb
+++ app/controllers/institutional_accounts_controller.rb
@@ -1,5 +1,5 @@
 class InstitutionalAccountsController < ApplicationController
   def index
-    render json: { institutional_accounts: ReferenceJson.find_by(name: 'Institutional Account List').items }
+    render json: { institutional_accounts: ReferenceJson.institutional_accounts.items }
   end
 end
--- app/models/reference_json.rb
+++ app/models/reference_json.rb
@@ -1,2 +1,13 @@
+# Stores reference information in jsonb columns in Postgres
+# This helps us avoid long hashes in our Ember code that is hard to update
+# without a code change
+#
+# Example:
+# `ReferenceJson.find_by(name: 'Institutional Account List').items`
+# Will return an array of hashes
+#
 class ReferenceJson < ActiveRecord::Base
+  def self.institutional_accounts
+    find_by(name: 'Institutional Account List')
+  end
 end

b28841b3c5810ade1f7c00c21d34967225ac29d5
Sebastian Bassi <sebastian.bassi@globant.com>
2016-06-09 16:19:23 -0700

    Trying refresh page due to bug 6843

--- test/frontend/test_production_metadata_card.py
+++ test/frontend/test_production_metadata_card.py
@@ -90,9 +90,10 @@ class ProductionMetadataCardTest(CommonTest):
     product_metadata_card = ProductionMedataCard(self.getDriver())
     product_metadata_card.check_style()
     # test content, it should be saved
+    # Due to bug APERTA-6843, I have to refresh
+    product_metadata_card.refresh()
     data = product_metadata_card.complete_card()
     time.sleep(2)
-    workflow_page.click_production_metadata_card()
     # read card data from the DB and compare
     task_id = PgSQL().query(
         'SELECT id from tasks WHERE paper_id = %s and title = %s;',

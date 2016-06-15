29f65101bf58b40b30099f110e743d94ce964091
Chris Westra <chris@mutuallyhuman.com>
2016-06-15 13:48:48 -0400

    APERTA-7042 set figure title on upload

--- app/workers/download_figure_worker.rb
+++ app/workers/download_figure_worker.rb
@@ -1,10 +1,19 @@
 class DownloadFigureWorker
   include Sidekiq::Worker
 
+  def figure_regex
+    /fig(ure)?[^[:alnum:]]*(?<label>\d+)/i
+  end
+
   def perform(figure_id, url)
     figure = Figure.find figure_id
     figure.attachment.download! url
-    figure.title = figure.attachment.filename unless figure.title
+    unless figure.title
+      figure.title = "Unlabeled"
+      figure_regex.match(figure.attachment.filename) do |match|
+        figure.title = "Fig. #{match['label']}"
+      end
+    end
     figure.status = "done"
     figure.save
     figure

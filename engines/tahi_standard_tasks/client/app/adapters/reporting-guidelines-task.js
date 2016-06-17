9e7801a08e3135a91598cad069416fcdd639a696
Chris Westra <cwestra@gmail.com>
2016-06-17 16:46:26 -0400

    Convert files

---
+++ engines/tahi_standard_tasks/client/app/adapters/reporting-guidelines-task.js
@@ -0,0 +1,5 @@
+import TaskAdapter from 'tahi/adapters/task';
+
+let ReportingGuidelinesTask = TaskAdapter.extend();
+
+export default ReportingGuidelinesTask;
--- engines/tahi_standard_tasks/client/app/adapters/reviewer-report-task.coffee
+++
@@ -1,5 +0,0 @@
-`import TaskAdapter from 'tahi/adapters/task'`
-
-ReviewerReportTaskAdapter= TaskAdapter.extend()
-
-`export default ReviewerReportTaskAdapter`

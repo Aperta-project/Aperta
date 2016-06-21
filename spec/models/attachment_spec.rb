36322e4fddde8cb9113735446914de11b6437fa9
Zach Dennis <zach.dennis@gmail.com>
2016-06-21 15:30:16 -0400

    Fixing figures controller spec and ensuring attachments.paper_id is set when creating a figure thru an association.
    APERTA-6881

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -5,20 +5,26 @@ describe Attachment do
     FactoryGirl.build(:attachment)
   end
 
-  describe '#owner=' do
-    let(:paper) { FactoryGirl.build_stubbed(:paper) }
-    let(:task) { FactoryGirl.build_stubbed(:task, paper: paper) }
+  describe 'setting #paper' do
+    let(:paper) { FactoryGirl.create(:paper) }
+    let(:task) { FactoryGirl.create(:task, paper: paper) }
 
-    it 'sets the #paper when the owner is a Paper' do
+    it 'is set when assigning #owner and the owner is a Paper' do
       expect do
         attachment.owner = paper
       end.to change(attachment, :paper).to paper
     end
 
-    it 'sets the #paper when the owner responds to #paper' do
+    it 'is set when assigning #owner and the owner responds to :paper' do
       expect do
         attachment.owner = task
       end.to change(attachment, :paper).to task.paper
+    end
+
+    it 'is set when the creating thru an association that has a paper' do
+      attachment = task.attachments.create
+      expect(attachment.paper).to eq(paper)
+      expect(attachment.paper_id).to eq(paper.id)
     end
   end
 end

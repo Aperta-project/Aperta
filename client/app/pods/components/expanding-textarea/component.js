39ea9c9749d3f1f39d44f92f6f47583b9d40081a
Zach Dennis <zach.dennis@gmail.com>
2016-06-20 21:40:52 -0400

    Update the ember expanding-textarea component to implement resize for all browsers
    * encapsulate all styles, logic, etc inside of the component
    * implement via CSS3 the lines that make up the drag arrows in the bottom right corner of a text area
    * remove CSS "resize" declaration since there are no browser events that fire on the textarea resizing. This was a bummer to find out.
    
    APERTA-6805

--- client/app/pods/components/expanding-textarea/component.js
+++ client/app/pods/components/expanding-textarea/component.js
@@ -1,45 +1,104 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['expanding-textarea'],
+  textareaClassNames: null,
+  placeholder: null,
+  value: null,
+
   _setupGrowingTextarea: Ember.on('didInsertElement', function() {
     Ember.run.scheduleOnce('afterRender', this, function() {
-      var $textarea = this.$('textarea');
+      // $textarea: the textarea to auto-expand and turn on resizing for
+      // $resizeArrow: container that contains the two diagonal lines
+      // $line1: the longer, inner resize line
+      // $line2: the shorter, outer resize line
+      var $textarea = this.$('textarea'),
+        $resizeArrow = this.$('.expanding-textarea-arrow'),
+        $line1 = this.$('.expanding-textarea-arrow-line1', $resizeArrow),
+        $line2 = this.$('.expanding-textarea-arrow-line2', $resizeArrow);
 
-      $textarea.on('input', function() {
-        this.style.height = this.scrollHeight + 'px';
+      // Use the initial height of the textarea as the minimum height.
+      // We will not let the user resize the textarea smaller than this.
+      var minOuterHeight = $textarea.outerHeight();
+
+      // When a user types into the textarea auto-resize the height unless
+      // they have manually starting resizing it. Once they do that they
+      // are in control of the textarea's height.
+      $textarea.on('input', () => {
+        if(!this.get('user-resized')){
+          $textarea[0].style.height = '';
+          let height = $textarea[0].scrollHeight;
+          $textarea.css('height', Math.max(minOuterHeight, height) + 'px');
+        }
       }).trigger('input');
 
+      // Apply CSS styles inside the component so there are no external
+      // dependencies
+      $resizeArrow.css({
+        'cursor': 'row-resize',
+        'position': 'relative'
+      });
 
-      // Only keep the resize bar if the browser is IE.
-      var ua = window.navigator.userAgent;
-      var msie = ua.indexOf('MSIE ');
-      var trident = ua.indexOf('Trident/');
-      var edge = ua.indexOf('Edge/');
+      $line1.css({
+      	'float': 'right',
+      	'position': 'absolute',
+      	'right': '6px',
+      	'bottom': '8px',
+        'height': '7px',
+        'width': '7px',
+        'border-right': '1px solid gray',
+        '-moz-transform': 'skew(-45deg)',
+        '-webkit-transform': 'skew(-45deg)',
+        'transform': 'skew(-45deg)'
+      });
 
-      var $dragToResize = this.$('div.drag-to-resize');
-      if (msie > 0 || trident > 0 || edge > 0) {
-        var $document = $(document);
-        var dragOffset = $dragToResize.data('drag-offset');
-        $dragToResize.on('mousedown', function(e) {
+      $line2.css({
+      	'float': 'right',
+      	'position': 'absolute',
+      	'right': '4px',
+      	'bottom': '8px',
+        'height': '3px',
+        'width': '3px',
+        'border-right': '1px solid gray',
+        '-moz-transform': 'skew(-45deg)',
+        '-webkit-transform': 'skew(-45deg)',
+        'transform': 'skew(-45deg)'
+      });
+
+      var $document = $(document);
+
+      // newHeight: contains the new height of the textarea based on the
+      //            current dragging of the user
+      var newHeight;
+
+      $resizeArrow.on('mousedown', (e) => {
+        e.preventDefault();
+
+        this.set('user-resized', true);
+
+        $document.on('mousemove', function(e) {
           e.preventDefault();
+          newHeight = e.screenY - $textarea.offset().top - minOuterHeight;
 
-          $document.on('mousemove', function(e) {
-            e.preventDefault();
-            $textarea.css('height', e.screenY - $textarea.offset().top - dragOffset);
-          });
+          // Ensure that the user cannot make the textarea smaller than
+          // the minOuterHeight
+          if(newHeight >= minOuterHeight){
+            $textarea.css('height', newHeight);
+          }
         });
+      });
 
-        $document.on('mouseup', function(e) {
-          e.preventDefault();
-          $document.off('mousemove');
-        });
-      } else {
-        $dragToResize.remove();
-      }
+      $document.on('mouseup', function(e) {
+        e.preventDefault();
+        $document.off('mousemove');
+      });
     });
   }),
 
   _teardownGrowingTextarea: Ember.on('willDestroyElement', function() {
-    this.$('textarea').off();
+    let $textarea = this.$('textarea'),
+      $resizeArrow = this.$('.expanding-textarea-arrow');
+    $resizeArrow.off();
+    $textarea.off();
   })
 });

0ad49b09c7f5f20521112e5cc8ef6b071430d4ba
Tad Park <tadp@users.noreply.github.com>
2016-06-21 16:55:00 -0700

    APERTA-7080 Reverse default order of paper types

--- app/serializers/journal_serializer.rb
+++ app/serializers/journal_serializer.rb
@@ -1,3 +1,10 @@
 class JournalSerializer < ActiveModel::Serializer
   attributes :id, :name, :logo_url, :paper_types, :manuscript_css
+
+  # We want to reverse the order of paper_types here so that the oldest paper
+  # types appear first so users do not need to dig through tons of types
+  # that may not be relevant
+  def paper_types
+    object.paper_types.reverse
+  end
 end

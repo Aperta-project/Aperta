24757db5f0958d2c936ec359d79260e3564f08db
Zach Dennis <zach.dennis@gmail.com>
2016-06-21 15:34:54 -0400

    Fixing question_attachments_controller_spec
    * refer to owner instead of nested_question_answer
    * remove paper method from QuestionAttachment, it's not necessary
    
    APERTA-6881

--- app/models/question_attachment.rb
+++ app/models/question_attachment.rb
@@ -1,10 +1,6 @@
 class QuestionAttachment < Attachment
   mount_uploader :file, QuestionAttachmentUploader
 
-  def paper
-    nested_question_answer.owner.try(:paper)
-  end
-
   def src
     non_expiring_proxy_url if done?
   end

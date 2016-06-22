6207792d648a9ac1a4d2fcff8b7756d149e899ab
Zach Dennis <zach.dennis@gmail.com>
2016-06-22 14:06:58 -0400

    Adding class-docs. No code changes.
    * AdhocAttachment model
    * Figure model (text from Glenn Carroll @ PLOS)
    * QuestionAttachment model
    * SupportingInformationFile model (text from James Harney @ PLOS)
    * AdhocAttachmentSerializer
    
    APERTA-6881

--- app/models/adhoc_attachment.rb
+++ app/models/adhoc_attachment.rb
@@ -1,3 +1,6 @@
+# AdhocAttachment represents a file attachment that is added to an
+# adhoc task card that does not fall into the other predefined attachment
+# categories such as Figure(s), SupportingInformationFile(s), etc.
 class AdhocAttachment < Attachment
   mount_uploader :file, AdhocAttachmentUploader
 

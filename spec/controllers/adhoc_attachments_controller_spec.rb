88a1813312fe7bee0ebe72b18cdf28d04465779e
Zach Dennis <zach.dennis@gmail.com>
2016-06-27 11:30:01 -0400

    Update adhoc_attachments_controller_spec
    * backfill missing example
    * ensure that only authenticated users can access it
    * bring in line with project conventions for how to check permissions
    
    APERTA-6881

--- spec/controllers/adhoc_attachments_controller_spec.rb
+++ spec/controllers/adhoc_attachments_controller_spec.rb
@@ -1,150 +1,232 @@
 require 'rails_helper'
 
 describe AdhocAttachmentsController do
-  let(:user) { create :user }
+  let(:user) { FactoryGirl.create :user }
   let(:journal) { FactoryGirl.create(:journal, :with_creator_role) }
-  let(:paper) do
-    FactoryGirl.create(:paper, journal: journal, creator: user)
-  end
+  let(:paper) { FactoryGirl.create(:paper, journal: journal, creator: user) }
   let(:task) { FactoryGirl.create(:task, paper: paper) }
 
-  describe "viewing the attachment" do
-    let(:attachment) { task.attachments.create! }
+  describe 'GET #index' do
+    subject(:do_request) { get :index, format: 'json', task_id: task.to_param }
 
-    context "with permission to view the task" do
+    it_behaves_like 'an unauthenticated json request'
+
+    context 'when the user is signed in' do
       before do
+        task.attachments.create!
+        task.attachments.create!
+
         stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:view, task)
-          .and_return true
       end
 
-      it "returns the attachment" do
-        get :index, format: "json", task_id: task.to_param
-        expect(response).to be_success
-      end
-    end
+      context 'when the user has access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:view, task)
+            .and_return true
+        end
 
-    context "without permission to view the task" do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:view, task)
-          .and_return false
+        it 'responds with a list of attachments' do
+          do_request
+          expect(res_body['attachments'].length).to eq(task.attachments.length)
+        end
+
+        it 'returns 200 OK' do
+          do_request
+          expect(response.status).to eq(200)
+        end
       end
 
-      it "returns a 403" do
-        get :index, format: "json", task_id: task.to_param
-        expect(response.status).to eq(403)
+      context 'when the user does not have access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:view, task)
+            .and_return false
+        end
+
+        it 'returns a 403' do
+          do_request
+          expect(response.status).to eq(403)
+        end
       end
     end
   end
 
-  describe "destroying the attachment" do
-    subject(:do_request) { delete :destroy, id: task.attachments.last.id, paper_id: paper.id }
-    before(:each) do
+  describe 'GET #show' do
+    let(:attachment) { task.attachments.create! }
+
+    subject(:do_request) do
+      get :show, format: 'json', task_id: task.to_param, id: attachment.to_param
+    end
+
+    it_behaves_like 'an unauthenticated json request'
+
+    context 'when the user is signed in' do
+      before do
+        stub_sign_in user
+      end
+
+      context 'when the user has access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:view, task)
+            .and_return true
+        end
+
+        it 'responds with the attachment' do
+          do_request
+          expect(res_body['attachment']['id']).to eq(attachment.id)
+        end
+
+        it 'returns 200 OK' do
+          do_request
+          expect(response.status).to eq(200)
+        end
+      end
+
+      context 'when the user does not have access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:view, task)
+            .and_return false
+        end
+
+        it 'returns a 403' do
+          do_request
+          expect(response.status).to eq(403)
+        end
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    subject(:do_request) do
+      xhr :delete, :destroy, id: task.attachments.last.id, paper_id: paper.id
+    end
+
+    before do
       task.attachments.create!
     end
 
-    context "with permission to edit the task" do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:edit, task)
-          .and_return true
+    it_behaves_like 'an unauthenticated json request'
+
+    context 'when the user is signed in' do
+      before { stub_sign_in user }
+
+      context 'when the user has access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:edit, task)
+            .and_return true
+        end
+
+        it 'destroys the attachment record' do
+          expect { do_request }.to change { AdhocAttachment.count }.by(-1)
+        end
       end
 
-      it "destroys the attachment record" do
-        expect { do_request }.to change { Attachment.count }.by(-1)
-      end
-    end
+      context 'when the user does not have access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:edit, task)
+            .and_return false
+        end
 
-    context "without permission to edit the task" do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:edit, task)
-          .and_return false
-      end
+        it 'does not destroy the attachment' do
+          expect { do_request }.to change { AdhocAttachment.count }.by(0)
+        end
 
-      it "leaves the attachment alone" do
-        expect { do_request }.to change { Attachment.count }.by(0)
-      end
-
-      it "returns a 403" do
-        do_request
-        expect(response.status).to eq(403)
-      end
-    end
-  end
-
-  describe "POST 'create'" do
-    let(:url) { "http://someawesomeurl.com" }
-    context "with permission to edit the task" do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:edit, task)
-          .and_return true
-      end
-
-      it "causes the creation of the attachment" do
-        expect(DownloadAttachmentWorker).to receive(:perform_async)
-        post :create, format: "json", task_id: task.to_param, title: 'Cool'
-        expect(response).to be_success
-      end
-    end
-
-    context "without permission to edit the task" do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:edit, task)
-          .and_return false
-      end
-
-      it "returns a 403" do
-        post :create, format: "json", task_id: task.to_param, title: 'Cool'
-        expect(response.status).to eq(403)
+        it 'returns a 403' do
+          do_request
+          expect(response.status).to eq(403)
+        end
       end
     end
   end
 
-  describe "PUT 'update_attachment'" do
+  describe 'POST #create' do
+    subject(:do_request) do
+      post :create, format: 'json', task_id: task.to_param, title: 'Cool'
+    end
+    let(:url) { 'http://someawesomeurl.com' }
+
+    it_behaves_like 'an unauthenticated json request'
+
+    context 'when the user is signed in' do
+      before { stub_sign_in user }
+
+      context 'when the user has access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:edit, task)
+            .and_return true
+        end
+
+        it 'causes the creation of the attachment' do
+          expect(DownloadAttachmentWorker).to receive(:perform_async)
+          do_request
+          expect(response).to be_success
+        end
+      end
+
+      context 'when the user does not have access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:edit, task)
+            .and_return false
+        end
+
+        it 'returns a 403' do
+          do_request
+          expect(response.status).to eq(403)
+        end
+      end
+    end
+  end
+
+  describe 'PUT #update_attachment' do
+    subject(:do_request) do
+      put :update_attachment, format: 'json', task_id: task.to_param, id: attachment.id, url: url
+    end
+
     let(:url) { "http://someawesomeurl.com" }
     let(:attachment) { task.attachments.create! }
 
-    context "with permission to edit the task" do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:edit, task)
-          .and_return true
+    it_behaves_like 'an unauthenticated json request'
+
+    context 'when the user is signed in' do
+      before { stub_sign_in user }
+
+      context 'when the user has access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:edit, task)
+            .and_return true
+        end
+
+        it 'calls DownloadAttachmentWorker' do
+          expect(DownloadAttachmentWorker).to receive(:perform_async).with(attachment.id, url)
+          do_request
+          expect(response).to be_success
+        end
       end
 
-      it "calls DownloadAttachmentWorker" do
-        expect(DownloadAttachmentWorker).to receive(:perform_async).with(attachment.id, url)
-        put :update_attachment, format: "json", task_id: task.to_param, id: attachment.id, url: url
-        expect(response).to be_success
-      end
-    end
+      context 'when the user does not have access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:edit, task)
+            .and_return false
+        end
 
-    context "without permission to edit the task" do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:edit, task)
-          .and_return false
-      end
-
-      it "returns a 403" do
-        put :update_attachment, format: "json", task_id: task.to_param, id: attachment.id, url: url
-        expect(response.status).to eq(403)
+        it 'returns a 403' do
+          do_request
+          expect(response.status).to eq(403)
+        end
       end
     end
   end
 
-  describe "PUT 'update'" do
+  describe 'PUT #update' do
     subject(:do_request) do
       patch :update,
             id: task.attachments.last.id,
@@ -156,39 +238,42 @@ describe AdhocAttachmentsController do
             format: :json
     end
 
-    before(:each) do
+    before do
       task.attachments.create!
     end
 
-    context "with permission to edit the task" do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:edit, task)
-          .and_return true
+    it_behaves_like 'an unauthenticated json request'
+
+    context 'when the user is signed in' do
+      before { stub_sign_in user }
+
+      context 'when the user has access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:edit, task)
+            .and_return true
+        end
+
+        it 'allows updates for title and caption' do
+          do_request
+
+          attachment = task.attachments.last
+          expect(attachment.caption).to eq('new caption')
+          expect(attachment.title).to eq('new title')
+        end
       end
 
-      it "allows updates for title and caption" do
-        do_request
+      context 'when the user does not have access' do
+        before do
+          allow(user).to receive(:can?)
+            .with(:edit, task)
+            .and_return false
+        end
 
-        attachment = task.attachments.last
-        expect(attachment.caption).to eq("new caption")
-        expect(attachment.title).to eq("new title")
-      end
-    end
-
-    context "without permission to edit the task" do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:edit, task)
-          .and_return false
-      end
-
-      it "returns a 403" do
-        do_request
-
-        expect(response.status).to eq(403)
+        it 'returns a 403' do
+          do_request
+          expect(response.status).to eq(403)
+        end
       end
     end
   end

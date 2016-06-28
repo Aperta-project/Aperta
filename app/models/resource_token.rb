e666d1b71467880aa569f1d06cc97b54cd4143cb
AJ Hekman <aj@mutuallyhuman.com>
2016-06-28 14:45:56 -0400

    add class doc to ResourceToken
    APERTA-6881

--- app/models/resource_token.rb
+++ app/models/resource_token.rb
@@ -1,3 +1,6 @@
+# ResourceToken is a class to link internal models to external resources. It is
+# used to when we want to hand out a permalink for an asset that is hosted on S3
+# that requires a temporary signed url.
 class ResourceToken < ActiveRecord::Base
   # writes to `token` attr on create
   # `regenerate_token` for new token

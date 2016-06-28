16327a103dee34123cd53c2556cb48a1b2438c00
AJ Hekman <aj@mutuallyhuman.com>
2016-06-28 15:43:28 -0400

    simplify resource_proxy_controller_spec
    * use factory to create attachment, resource_token
    * remove `receive_message_chain`
    * stub #url on attachment

--- spec/controllers/resource_proxy_controller_spec.rb
+++ spec/controllers/resource_proxy_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe ResourceProxyController do
   let(:example_token) { 'proxy_token' }
   let(:aws_url) { 'https://awsurl.example.com' }
+  let(:resource_token) do
+    FactoryGirl.build_stubbed :resource_token, owner: attachment
+  end
+  let(:attachment) { FactoryGirl.build_stubbed :attachment }
 
   describe 'GET #url without version' do
     let(:attachment_double) { double }
@@ -11,8 +15,11 @@ describe ResourceProxyController do
     end
 
     it 'redirects to S3 URL for unversioned resources' do
-      allow(attachment_double).to receive_message_chain(:owner, :file, :url) { aws_url }
-      allow(ResourceToken).to receive(:find_by!).with(token: example_token) { attachment_double }
+      allow(attachment).to receive(:url) { aws_url }
+      allow(ResourceToken)
+        .to receive(:find_by!)
+          .with(token: example_token) { resource_token }
+
       expect(subject).to redirect_to(aws_url)
     end
   end
@@ -30,9 +37,13 @@ describe ResourceProxyController do
     describe 'preview version' do
       let(:version) { :preview }
       it 'redirects to S3 URL for preview version resources' do
-        expect(url_double).to receive(:url).with('preview') { aws_url }
-        allow(attachment_double).to receive_message_chain(:owner, :file) { url_double }
-        allow(ResourceToken).to receive(:find_by!).with(token: example_token) { attachment_double }
+        expect(attachment)
+          .to receive(:url)
+            .with('preview') { aws_url }
+
+        allow(ResourceToken)
+          .to receive(:find_by!)
+            .with(token: example_token) { resource_token }
         expect(subject).to redirect_to(aws_url)
       end
     end
@@ -40,9 +51,14 @@ describe ResourceProxyController do
     describe 'detail version' do
       let(:version) { :detail }
       it 'redirects to S3 URL for preview version resources' do
-        expect(url_double).to receive(:url).with('detail') { aws_url }
-        allow(attachment_double).to receive_message_chain(:owner, :file) { url_double }
-        allow(ResourceToken).to receive(:find_by!).with(token: example_token) { attachment_double }
+        allow(ResourceToken)
+          .to receive(:find_by!)
+            .with(token: example_token) { resource_token }
+
+        expect(attachment)
+          .to receive(:url)
+            .with('detail') { aws_url }
+
         expect(subject).to redirect_to(aws_url)
       end
     end
@@ -57,7 +73,9 @@ describe ResourceProxyController do
     end
 
     it 'is not found' do
-      expect(ResourceToken).to receive(:find_by!).with(token: example_token) { fail ActiveRecord::RecordNotFound }
+      expect(ResourceToken)
+        .to receive(:find_by!)
+          .with(token: example_token) { fail ActiveRecord::RecordNotFound }
       expect(subject.status).to eq 404
     end
   end

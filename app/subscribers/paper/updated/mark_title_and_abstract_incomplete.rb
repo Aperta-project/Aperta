3e002cfef5451b93611d2fd3f470a4987000bace
Matt Mercieca <mercieca@gmail.com>
2016-06-28 16:43:13 -0400

    APERTA-6755 Checking processing flag to know when to update title and abstract card

--- app/subscribers/paper/updated/mark_title_and_abstract_incomplete.rb
+++ app/subscribers/paper/updated/mark_title_and_abstract_incomplete.rb
@@ -3,20 +3,15 @@
 class Paper::Updated::MarkTitleAndAbstractIncomplete
   def self.call(_event_name, event_data)
     paper = event_data[:record]
-    return unless title_changed(paper) || abstract_changed(paper)
+    return unless processing_changed(paper)
 
     paper.tasks.of_type(TahiStandardTasks::TitleAndAbstractTask)
       .map(&:incomplete!)
   end
 
-  def self.title_changed(paper)
-    return unless paper.previous_changes && paper.previous_changes['title']
-    paper.previous_changes['title'][0] != paper.previous_changes['title'][1]
-  end
-
-  def self.abstract_changed(paper)
-    return unless paper.previous_changes && paper.previous_changes['abstract']
-    paper.previous_changes['abstract'][0] !=
-      paper.previous_changes['abstract'][1]
+  def self.processing_changed(paper)
+    return unless paper.previous_changes && paper.previous_changes['processing']
+    paper.previous_changes['processing'][0] !=
+      paper.previous_changes['processing'][1]
   end
 end

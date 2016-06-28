1a48debad808f1195ee5cb6394942c5f3c48c95e
Matt Mercieca <mercieca@gmail.com>
2016-06-28 10:33:59 -0400

    APERTA-6755 Corrected marking title and abstract incomplete
    Only marking card incomplete when title or abstract have changed

--- spec/subscribers/paper/updated/mark_title_and_abstract_incomplete_spec.rb
+++ spec/subscribers/paper/updated/mark_title_and_abstract_incomplete_spec.rb
@@ -7,7 +7,11 @@ describe Paper::Updated::MarkTitleAndAbstractIncomplete do
   let!(:task) { FactoryGirl.create(:title_and_abstract_task, paper: paper) }
   let!(:other_task) { FactoryGirl.create(:authors_task, paper: paper) }
 
-  context 'when there is a title and abstract task' do
+  context 'when the title has changied' do
+    before do
+      allow(paper).to receive(:previous_changes).and_return("title" => ["first", "second"])
+    end
+
     it 'marks the title and abstract task incomplete if there is one' do
       task.completed = true
       task.save
@@ -16,10 +20,29 @@ describe Paper::Updated::MarkTitleAndAbstractIncomplete do
         described_class.call('tahi:paper:updated', record: paper)
       end.to change { task.reload.completed }.to(false)
     end
+
+    it 'does nothing to other tasks' do
+      expect do
+        described_class.call('tahi:paper:updated', record: paper)
+      end.to_not change { other_task.reload.completed }
+    end
   end
 
-  context 'when there is no title and abstract task' do
-    it 'does nothing' do
+  context 'when something else has changied' do
+    before do
+      allow(paper).to receive(:previous_changes).and_return("body" => ["first", "second"])
+    end
+
+    it 'marks the title and abstract task incomplete if there is one' do
+      task.completed = true
+      task.save
+
+      expect do
+        described_class.call('tahi:paper:updated', record: paper)
+      end.to_not change { task.reload.completed }
+    end
+
+    it 'does nothing to other tasks' do
       expect do
         described_class.call('tahi:paper:updated', record: paper)
       end.to_not change { other_task.reload.completed }

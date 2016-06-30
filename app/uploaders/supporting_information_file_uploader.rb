2d9b7760b1177651a587612cb66e7f99c59f1e2d
Zach Dennis <zach.dennis@gmail.com>
2016-06-30 11:47:26 -0400

    Removing SupportingInformationFileAttachmentUploader, it's no longer a snowflake.
    SupportingInformationFile can now just use AttachmentUploader
    
    APERTA-6881

--- app/uploaders/supporting_information_file_uploader.rb
+++
@@ -1,8 +0,0 @@
-# Temporary uploader for supporting information file. This will be replaced once
-# we start versioning attachment data on S3.
-class SupportingInformationFileUploader < AttachmentUploader
-  def store_dir
-    model.try(:s3_dir) ||
-      "uploads/attachments/#{model.id}/supporting_information_file/attachment/#{model.id}"
-  end
-end

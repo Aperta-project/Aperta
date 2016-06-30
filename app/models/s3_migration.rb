a20c70f76973cf57238c5310f57c4d19edcfb055
Zach Dennis <zach.dennis@gmail.com>
2016-06-30 10:08:16 -0400

    Updating the S3Migration process for attachment(s)
    * skip orphaned attachments
    * update existing task snapshots
    * create snapshots for attachments 
    * use update_column over update_attributes to not invoke carrier wave callbacks
    
    Paired with @ajhekman
    
    APERTA-6881

--- app/models/s3_migration.rb
+++ app/models/s3_migration.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 # An S3Migration represents a record of an S3 attachment that needs to
 # be migrated from its old +store_dir+ to a new +store_dir+. It assumes the
 # old +store_dir+ is cached on the original attachment in a column called
@@ -81,27 +83,40 @@ class S3Migration < ActiveRecord::Base
 
   private
 
-  def storage
-    @storage ||= Fog::Storage.new(
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION']
-    )
-  end
-
   def perform_migration
+    # There is a lot of orphaned data (at least on staging) so this is to
+    # ignore it.
+    if attachment.is_a?(Figure)
+      if attachment.paper.nil?
+        puts "Task or Paper is nil. Attachment (id=#{attachment.id}) is an orphan. :("
+        return
+      end
+    elsif attachment.task.nil? || attachment.paper.nil?
+      puts "Task or Paper is nil. Attachment (id=#{attachment.id}) is an orphan. :("
+      return
+    end
+
     previous_file_hash = attachment.previous_file_hash || s3object.data[:headers]['ETag'].gsub('"', '')
     new_file_hash = Digest::SHA256.hexdigest(s3object.data[:body])
+    # new_file_hash = SecureRandom.uuid # <-- this value is used for manual testing
 
-    # Do not modify the file hashes if we're migrating a version as they
-    # use their parent attachment(s) file hash. Otherwise, each version of
-    # an attachment will end up in its own "directory" on S3.
+    # Do not modify the file hashes if we're migrating a version (e.g detail,
+    # preview, etc.) as they  use their parent (CarrierWave Uploader object)
+    # attachment's file hash. Otherwise, each version of an attachment will end
+    # up in its own "directory" on S3.
     unless version?
-      attachment.update_attributes!(
-        previous_file_hash: previous_file_hash,
-        file_hash: new_file_hash,
-      )
+      # do not use update_attributes because we only want to save these
+      # columns, and not trigger any carrierwave functionality at this point.
+      attachment.update_column :previous_file_hash, previous_file_hash
+      attachment.update_column :file_hash, new_file_hash
+    end
+
+
+    # only backfill an attachment snapshot if we found an existing
+    # task snapshot for this attachment
+    task_snapshot = find_and_update_task_snapshot(previous_file_hash, new_file_hash)
+    if task_snapshot
+      SnapshotService.new(attachment.paper).snapshot!(attachment)
     end
 
     destination_url = attachment.file.new_store_path(File.basename(source_url))
@@ -125,14 +140,55 @@ class S3Migration < ActiveRecord::Base
     end
     completed!
   rescue Exception => ex
-    update_attributes(
+    update_attributes!(
       error_message: ex.message,
       error_backtrace: ex.backtrace.join("\n"),
       errored_at: Time.zone.now
     )
     failed!
     msg = "Failed S3 migration on #{self.inspect}. See S3Migration.find(#{id}) for more information."
-    STDERR.puts msg
+    STDERR.puts msg, ex.message, ex.backtrace, ""
     Rails.logger.error msg
   end
+
+  def storage
+    @storage ||= Fog::Storage.new(
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION']
+    )
+  end
+
+  # Finds the first Snapshot"that makes reference of the previous_file_hash,
+  # updating that reference to point to the new_file_hash, then returning
+  # that Snapshot. Returns nil if not snapshot matches.
+  def find_and_update_task_snapshot(previous_file_hash, new_file_hash)
+    ::Snapshot.all.each do |snapshot|
+      child = find_child_with_file_hash previous_file_hash, snapshot.contents
+      if child
+        # update snapshot to use new_file_hash instead of the previous_file_hash
+        child['value'] = new_file_hash
+        child.save!
+        snapshot
+      end
+    end
+  end
+
+  # This is helper method that is used to walk the the 'children' inside of
+  # a given node (Hash data structure) look for the first file_hash child
+  # that matches the given file_hash. Recursively calls itself until a
+  # node is found, otherwise returns nil.
+  def find_child_with_file_hash(file_hash, node)
+    if node['name'] == 'file_hash' && node['value'] == file_hash
+      node
+    elsif node['children']
+      node['children'].detect do |node|
+        find_child_with_file_hash file_hash, node
+      end
+    else
+      nil
+    end
+  end
+
 end

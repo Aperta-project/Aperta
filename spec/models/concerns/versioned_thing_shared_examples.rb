caf78bd552c783159556919d5e92a49d46008b8c
Erik Hetzner <ehetzner@plos.org>
2016-06-30 16:16:49 -0700

    APERTA-6719 Test version ordering for unversioned
    Versioned things that lack a major/minor version should be sorted as
    though they are the most 'recent' thing.

--- spec/models/concerns/versioned_thing_shared_examples.rb
+++ spec/models/concerns/versioned_thing_shared_examples.rb
@@ -28,12 +28,24 @@ RSpec.shared_examples 'a thing with major and minor versions' do |name|
       things = paper.send(@things).versioned
       expect(things.version_desc.to_a).to match([version_0_1, version_0_0])
     end
+
+    it 'should return an unversioned thing first' do
+      things = paper.send(@things)
+      expect(things.version_desc.first.major_version).to be(nil)
+      expect(things.version_desc.first.minor_version).to be(nil)
+    end
   end
 
   describe '#version_asc' do
     it 'should return the versioned things in ascending version order' do
       things = paper.send(@things).versioned
       expect(things.version_asc.to_a).to match([version_0_0, version_0_1])
+    end
+
+    it 'should return an unversioned thing last' do
+      things = paper.send(@things)
+      expect(things.version_asc.last.major_version).to be(nil)
+      expect(things.version_asc.last.minor_version).to be(nil)
     end
   end
 end

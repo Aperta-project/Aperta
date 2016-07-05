20419e7176898ac5f4c1853d5f51e1aae5195d5f
Tad Park <tadp@users.noreply.github.com>
2016-07-05 15:04:23 -0700

    APERTA-7161 Make paper-container a class and hook up Ember

--- client/app/pods/components/paper-version-picker/component.js
+++ client/app/pods/components/paper-version-picker/component.js
@@ -3,67 +3,17 @@ import PaperBase from 'tahi/mixins/controllers/paper-base';
 import Discussions from 'tahi/mixins/discussions/route-paths';
 
 export default Ember.Component.extend(PaperBase, Discussions, {
-  queryParams: ['selectedVersion1', 'selectedVersion2'],
-  routing: Ember.inject.service('-routing'),
-
-  taskToDisplay: null,
-  showTaskOverlay: false,
-  previousURL: null,
-
-  generateTaskVersionURL(task) {
-    return this.get('routing.router.router').generate(
-      'paper.task.version',
-      task.get('paper'),
-      task.get('id'),
-      {
-        queryParams: {
-          selectedVersion1: this.get('selectedVersion1'),
-          selectedVersion2: this.get('selectedVersion2')
-        }
-      }
-    );
-  },
-
-  generatePaperVersionURL(paper) {
-    return this.get('routing.router.router').generate(
-      'paper.versions',
-      paper,
-      {
-        queryParams: {
-          selectedVersion1: this.get('selectedVersion1'),
-          selectedVersion2: this.get('selectedVersion2')
-        }
-      }
-    );
-  },
-
+ 
   actions: {
-    viewCard(task) {
-      const r = this.get('routing.router.router');
-      const newURL = this.generateTaskVersionURL(task);
-      const previousURL = this.generatePaperVersionURL(task.get('paper'));
-
-      r.updateURL(newURL);
-
-      this.setProperties({
-        previousURL: previousURL,
-        taskToDisplay: task,
-        showTaskOverlay: true
-      });
-    },
-
-    hideTaskOverlay() {
-      this.get('routing.router.router')
-          .updateURL(this.get('previousURL'));
-
-      this.set('showTaskOverlay', false);
-    },
-
     setViewingVersion(version) {
       this.set('viewingVersion', version);
       this.set(
         'selectedVersion1',
         `${version.get('majorVersion')}.${version.get('minorVersion')}`);
+
+      this.get('setQueryParam')(
+        'selectedVersion1', this.get('selectedVersion1')
+      );
     },
 
     setComparisonVersion(version) {
@@ -71,6 +21,8 @@ export default Ember.Component.extend(PaperBase, Discussions, {
       this.set(
         'selectedVersion2',
         `${version.get('majorVersion')}.${version.get('minorVersion')}`);
+      let action = this.get('setQueryParam');
+      action('selectedVersion2', this.get('selectedVersion2'));
     }
   }
 });

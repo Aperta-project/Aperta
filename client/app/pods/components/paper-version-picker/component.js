737116f0a82e1411f621bae4ba9e8c8b10a7ed4e
Tad Park <tadp@users.noreply.github.com>
2016-07-05 15:46:26 -0700

    APERTA-7161 Fix spec and refactor component

--- client/app/pods/components/paper-version-picker/component.js
+++ client/app/pods/components/paper-version-picker/component.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend(PaperBase, Discussions, {
         'selectedVersion1',
         `${version.get('majorVersion')}.${version.get('minorVersion')}`);
 
-      this.get('setQueryParam')(
+      this.attrs.setQueryParam(
         'selectedVersion1', this.get('selectedVersion1')
       );
     },

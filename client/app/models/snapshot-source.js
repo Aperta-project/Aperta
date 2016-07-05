afcc24f4acd92739bb8507a9fb003c256f2ab4fb
Zach Dennis <zach.dennis@gmail.com>
2016-07-05 15:55:40 -0400

    Move to a Snapshottable mixin over a SnapshotSource
    Thanks to @Bestra for figuring out what this wasn't happy originally:
    
    When ember data looks up the mixin for the relationship, it probably
    just appends the name you provide to `mixins:`, so if the mixin is in
    `/mixins/models/foo` you'd need to say `DS.belongsTo('models/foo')`.
    Conversely if you say `DS.belongsTo('bar'), the mixin needs to live in
    'mixins/bar'
    
    Paired with @Bestra
    
    APERTA-6881

--- client/app/models/snapshot-source.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-import NestedQuestionOwner from 'tahi/models/nested-question-owner';
-
-export default NestedQuestionOwner.extend({
-  snapshots: DS.hasMany('snapshot', {
-    inverse: 'source',
-    async: true
-  })
-});

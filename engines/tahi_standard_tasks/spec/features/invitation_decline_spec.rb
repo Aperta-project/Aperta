eadae8519445349233b6a3640ae970d918335211
Sara Gibbons <sara@mutuallyhuman.com>
2016-07-07 10:31:00 -0400

    add scenario for reviewer declining invitations and then closing invitations overlay [APERTA-6870]

--- engines/tahi_standard_tasks/spec/features/invitation_decline_spec.rb
+++ engines/tahi_standard_tasks/spec/features/invitation_decline_spec.rb
@@ -37,8 +37,34 @@ feature "Invite Reviewer", js: true do
     expect(invitation.decline_reason).to eq('reason for decline')
     expect(invitation.reviewer_suggestions).to eq('new reviewer suggestions')
 
-    invitation2.reload
-    expect(invitation2.decline_reason).to eq(nil)
-    expect(invitation2.reviewer_suggestions).to eq(nil)
+    invitation_no_feedback.reload
+    expect(invitation_no_feedback.decline_reason).to eq(nil)
+    expect(invitation_no_feedback.reviewer_suggestions).to eq(nil)
+  end
+
+  scenario 'decline invitations' do
+    invitation_overlay.expect_pending_invitation_count(2)
+    invitation_overlay.expect_declined_waiting_feedback_invitation_count(0)
+
+    # decline, do nothing
+    invitation_overlay.decline_invitation(1)
+    invitation_overlay.expect_pending_invitation_count(1)
+    invitation_overlay.expect_declined_waiting_feedback_invitation_count(1)
+
+    # decline and cancel feedback
+    invitation_overlay.decline_invitation(1)
+    invitation_overlay.expect_pending_invitation_count(0)
+    invitation_overlay.expect_declined_waiting_feedback_invitation_count(2)
+
+    dashboard.expect_active_invitations_count(2)
+
+    expect(invitation_no_feedback).not_to be_declined
+    expect(invitation).not_to be_declined
+
+    invitation_overlay.close_overlay
+
+    expect(invitation_no_feedback).to be_declined
+    expect(invitation).to be_declined
+    dashboard.expect_active_invitations_count(0)
   end
 end

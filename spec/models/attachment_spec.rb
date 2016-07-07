f51261d92677ef2eb45fe137130ed959ba132451
Zach Dennis <zach.dennis@gmail.com>
2016-07-07 15:21:08 -0400

    Adding a spec around Attachment.authenticated_url_for_key
    APERTA-6881

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -5,6 +5,28 @@ describe Attachment do
     FactoryGirl.build(:attachment)
   end
 
+  describe '.authenticated_url_for_key' do
+    subject(:authenticated_url) do
+      described_class.authenticated_url_for_key('my/path.jpg')
+    end
+
+    it 'returns an authenticated S3 URL for the given url/path' do
+      expected_url = 'https://tahi-test.s3-us-west-1.amazonaws.com/my/path.jpg'
+      expect(authenticated_url).to match(/#{Regexp.escape(expected_url)}\?/)
+
+      uri = URI.parse(authenticated_url)
+      query_params = CGI.parse(uri.query)
+      expect(query_params.keys).to contain_exactly(
+        'X-Amz-Expires',
+        'X-Amz-Date',
+        'X-Amz-Algorithm',
+        'X-Amz-Credential',
+        'X-Amz-SignedHeaders',
+        'X-Amz-Signature'
+      )
+    end
+  end
+
   describe 'setting #paper' do
     let(:paper) { FactoryGirl.create(:paper) }
     let(:task) { FactoryGirl.create(:task, paper: paper) }
@@ -34,3 +56,12 @@ describe Attachment do
     end
   end
 end
+__END__
+  def self.authenticated_url_for_key(key)
+    uploader = new.file
+    CarrierWave::Storage::Fog::File.new(
+      uploader,
+      uploader.send(:storage),
+      key
+    ).url
+  end

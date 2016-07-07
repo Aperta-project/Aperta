07f8fc6b68397350fe22dbd7b6f2d75ef5d9b5e0
Mr. Jeffrey Gray <jgray@plos.org>
2016-07-06 22:38:49 -0700

    APERTA-7071 Implemented add and delete candidate tests. Got a start on Card POM.

--- test/frontend/Cards/reviewer_candidates_card.py
+++ test/frontend/Cards/reviewer_candidates_card.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 import logging
-import os
-import random
 import time
 
-from loremipsum import generate_paragraph
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
-from Base.CustomException import ElementDoesNotExistAssertionError
-from frontend.Tasks.basetask import BaseTask
+from Base.CustomException import ElementDoesNotExistAssertionError, ElementExistsAssertionError
+from Base.PostgreSQL import PgSQL
+from frontend.Cards.basecard import BaseCard
 
 __author__ = 'jgray@plos.org'
 
 
-class ReviewerCandidatesTask(BaseTask):
+class ReviewerCandidatesCard(BaseCard):
   """
-  Page Object Model for Upload Manuscript task
+  Page Object Model for the Reviewer Candidates Card
   """
+  def __init__(self, driver, url_suffix='/'):
+    super(ReviewerCandidatesCard, self).__init__(driver)
 
-  def __init__(self, driver):
-    super(ReviewerCandidatesTask, self).__init__(driver)
-
-    # Locators - Instance members
+    #Locators - Instance members
     self._intro_text = (By.CSS_SELECTOR, 'div.task-main-content > p')
     # APERTA-7177 Typo in class name for the main form
     self._new_candidate_btn = (By.CSS_SELECTOR, 'div.reviewer-canditates-wrapper > button')
@@ -75,11 +71,18 @@ class ReviewerCandidatesTask(BaseTask):
     self._cand_view_delete_confirm_delete = (By.CSS_SELECTOR,
                                              'div.authors-overlay-item-delete > button + button')
 
-  # POM Actions
+
+
+    #POM Actions
+
   def validate_styles(self):
     """
-    Validate styles in Reviewer Candidates Task
+    Validate styles in the Title and Abstract Card
+    :return: void function
     """
+    card_title = self._get(self._card_heading)
+    assert card_title.text == 'Reviewer Candidates'
+    self.validate_application_title_style(card_title)
     intro_text = self._get(self._intro_text)
     self.validate_application_ptext(intro_text)
     assert intro_text.text == 'Use the fields below to give us contact information for each ' \
@@ -90,6 +93,41 @@ class ReviewerCandidatesTask(BaseTask):
                               'reviewers.', intro_text.text
     new_cand_btn = self._get(self._new_candidate_btn)
     assert new_cand_btn.text == 'NEW REVIEWER CANDIDATE'
+    # validate the extant entry if present
+    entry = self._get(self._cand_entry_view)
+    if entry:
+      full_name = entry.find_element(*self._cand_view_full_name)
+      self.validate_application_ptext(full_name)
+      assert user_object['name'] == full_name.text, full_name.text
+      email = entry.find_element(*self._cand_view_email)
+      self.validate_application_ptext(email)
+      assert user_object['email'] == email.text, email.text
+      decision = entry.find_element(*self._cand_view_decision)
+      self.validate_application_ptext(decision)
+      assert choice in decision.text.lower(), decision.text.lower()
+      stated_reason = entry.find_element(*self._cand_view_reason)
+      self.validate_application_ptext(stated_reason)
+      assert reason == stated_reason.text, '{0} != {1}'.format(reason, stated_reason.text)
+      self._actions.move_to_element(entry).perform()
+      delete_recommendation = entry.find_element(*self._cand_view_delete)
+      time.sleep(.5)
+      self._actions.move_to_element(delete_recommendation).perform()
+      time.sleep(.5)
+      delete_recommendation.click()
+      # Now need to validate the delete confirmation bits
+      confirm_question = self._get(self._cand_view_delete_confirm_text)
+      assert 'This will permanently delete the suggested reviewer. Are you sure?' \
+             in confirm_question.text, confirm_question.text
+      confirm_cancel = self._get(self._cand_view_delete_confirm_cancel)
+      # Note that this cancel button is of a style that is different than we use elsewhere, so I
+      #  will skip style validation for now - APERTA-7187
+      assert 'cancel' in confirm_cancel.text.lower()
+      confirm_delete = self._get(self._cand_view_delete_confirm_delete)
+      self.validate_green_on_green_button_style(confirm_delete)
+      assert 'DELETE FOREVER' == confirm_delete.text
+      confirm_cancel.click()
+      time.sleep(1)
+    # Now validate the new candidate form
     self.validate_primary_big_green_button_style(new_cand_btn)
     self.validate_common_elements_styles()
     new_cand_btn.click()
@@ -129,42 +167,42 @@ class ReviewerCandidatesTask(BaseTask):
     mail_input = self._get(self._cand_email_input)
     self.validate_input_field_style(mail_input)
     assert 'jane.doe@example.com' in mail_input.get_attribute('placeholder'), \
-        mail_input.get_attribute('placeholder')
+      mail_input.get_attribute('placeholder')
 
     title_element = self._get(self._cand_title)
     assert 'required' not in title_element.get_attribute('class'), \
-        title_element.get_attribute('class')
+      title_element.get_attribute('class')
     title_label = self._get(self._cand_title_label)
     self.validate_input_field_inside_label_style(title_label)
     assert 'Title' in title_label.text, title_label.text
     title_input = self._get(self._cand_title_input)
     self.validate_input_field_style(title_input)
     assert 'Professor' in title_input.get_attribute('placeholder'), \
-        title_input.get_attribute('placeholder')
+      title_input.get_attribute('placeholder')
 
     dept_element = self._get(self._cand_department)
     assert 'required' not in dept_element.get_attribute('class'), \
-        dept_element.get_attribute('class')
+      dept_element.get_attribute('class')
     dept_label = self._get(self._cand_department_label)
     self.validate_input_field_inside_label_style(dept_label)
     assert 'Department' in dept_label.text, dept_label.text
     dept_input = self._get(self._cand_department_input)
     self.validate_input_field_style(dept_input)
     assert 'Biology' in dept_input.get_attribute('placeholder'), \
-        dept_input.get_attribute('placeholder')
+      dept_input.get_attribute('placeholder')
 
     inst_element = self._get(self._cand_institution)
     assert 'required' not in inst_element.get_attribute('class'), \
-        inst_element.get_attribute('class')
+      inst_element.get_attribute('class')
     inst_input = self._get(self._cand_inst_input)
     self.validate_input_field_style(inst_input)
     assert 'Institution' in inst_input.get_attribute('placeholder'), \
-        inst_input.get_attribute('placeholder')
+      inst_input.get_attribute('placeholder')
     self._get(self._cand_inst_btn)
 
     reviewer_type_question = self._get(self._cand_recc_or_oppose)
     assert 'Are you recommending or opposing this reviewer?' in reviewer_type_question.text, \
-        reviewer_type_question.text
+      reviewer_type_question.text
     # APERTA-7178
     # assert 'required' in reviewer_type_question.get_attribute('class'), \
     #   reviewer_type_question.get_attribute('class')
@@ -180,7 +218,7 @@ class ReviewerCandidatesTask(BaseTask):
     opt_reason_field = self._get(self._cand_reason)
     assert 'Optional: reason for recommending or opposing ' \
            'this reviewer' in opt_reason_field.get_attribute('placeholder'), \
-        opt_reason_field.get_attribute('placeholder')
+      opt_reason_field.get_attribute('placeholder')
 
     cancel_link = self._get(self._cand_form_cancel)
     assert 'cancel' in cancel_link.text, cancel_link.text
@@ -189,133 +227,9 @@ class ReviewerCandidatesTask(BaseTask):
     assert 'DONE' in save_button.text, save_button.text
     self.validate_green_on_green_button_style(save_button)
 
-  def complete_reviewer_cand_form(self, user_object):
+  def check_initial_population(self, decision, reason):
     """
-    Fill out the New Candidate form with random data
-    Also validate the styles and elements for the display view of an entered candidate
-    :param user_object: dictionary for user whose data to use in filling out the recommendation
-    :returns choice, reason: the selection to recommend or oppose and the reason offered
+    Verify that the values populated in the form are those were submitted
+    :return: void function
     """
-    new_cand_btn = self._get(self._new_candidate_btn)
-    new_cand_btn.click()
-    fn = user_object['name'].split(' ')[0]
-    fn_input = self._get(self._cand_first_name_input)
-    fn_input.send_keys(fn + Keys.ENTER)
-    mi_input = self._get(self._cand_middle_initial_input)
-    ln = user_object['name'].split(' ')[1]
-    ln_input = self._get(self._cand_last_name_input)
-    ln_input.send_keys(ln + Keys.ENTER)
-    mail = user_object['email']
-    mail_input = self._get(self._cand_email_input)
-    mail_input.send_keys(mail + Keys.ENTER)
-    title = 'Grand Pubah'
-    title_input = self._get(self._cand_title_input)
-    title_input.send_keys(title + Keys.ENTER)
-    dept = 'Semiotics'
-    dept_input = self._get(self._cand_department_input)
-    dept_input.send_keys(dept + Keys.ENTER)
-    inst = 'University of Minnesota Twin Cities'
-    inst_input = self._get(self._cand_inst_input)
-    inst_input.send_keys(inst + Keys.ENTER)
-    choice = random.choice(['recommend', 'oppose'])
-    if choice == 'recommend':
-      self._get(self._cand_recc_radio_btn).click()
-    else:
-      self._get(self._cand_opp_radio_btn).click()
-    reason = generate_paragraph(start_with_lorem=True)[2]
-    opt_reason_field = self._get(self._cand_reason)
-    opt_reason_field.send_keys(reason)
-    save_button = self._get(self._cand_form_save)
-    save_button.click()
-    self.check_for_flash_error()
-    time.sleep(2)
-    # Now to do a little style validation for the view listing of the recommendation
-    entry = self._get(self._cand_entry_view)
-    full_name = entry.find_element(*self._cand_view_full_name)
-    self.validate_application_ptext(full_name)
-    assert user_object['name'] == full_name.text, full_name.text
-    email = entry.find_element(*self._cand_view_email)
-    self.validate_application_ptext(email)
-    assert user_object['email'] == email.text, email.text
-    decision = entry.find_element(*self._cand_view_decision)
-    self.validate_application_ptext(decision)
-    assert choice in decision.text.lower(), decision.text.lower()
-    stated_reason = entry.find_element(*self._cand_view_reason)
-    self.validate_application_ptext(stated_reason)
-    assert reason == stated_reason.text, '{0} != {1}'.format(reason, stated_reason.text)
-    self._actions.move_to_element(entry).perform()
-    delete_recommendation = entry.find_element(*self._cand_view_delete)
-    time.sleep(.5)
-    self._actions.move_to_element(delete_recommendation).perform()
-    time.sleep(.5)
-    delete_recommendation.click()
-    # Now need to validate the delete confirmation bits
-    confirm_question = self._get(self._cand_view_delete_confirm_text)
-    assert 'This will permanently delete the suggested reviewer. Are you sure?' \
-           in confirm_question.text, confirm_question.text
-    confirm_cancel = self._get(self._cand_view_delete_confirm_cancel)
-    # Note that this cancel button is of a style that is different than we use elsewhere, so I
-    #  will skip style validation for now - APERTA-7187
-    assert 'cancel' in confirm_cancel.text.lower()
-    confirm_delete = self._get(self._cand_view_delete_confirm_delete)
-    self.validate_green_on_green_button_style(confirm_delete)
-    assert 'DELETE FOREVER' == confirm_delete.text
-    confirm_cancel.click()
-    time.sleep(1)
-    return choice, reason
-
-  def delete_reviewer_cand_entry(self, user_object):
-    """
-    Fill out the New Candidate form with random data, then delete that entry
-    :param user_object: dictionary for user whose data to use in filling out the recommendation
-    """
-    new_cand_btn = self._get(self._new_candidate_btn)
-    new_cand_btn.click()
-    fn = user_object['name'].split(' ')[0]
-    fn_input = self._get(self._cand_first_name_input)
-    fn_input.send_keys(fn + Keys.ENTER)
-    mi_input = self._get(self._cand_middle_initial_input)
-    ln = user_object['name'].split(' ')[1]
-    ln_input = self._get(self._cand_last_name_input)
-    ln_input.send_keys(ln + Keys.ENTER)
-    mail = user_object['email']
-    mail_input = self._get(self._cand_email_input)
-    mail_input.send_keys(mail + Keys.ENTER)
-    title = 'Grand Poobah'
-    title_input = self._get(self._cand_title_input)
-    title_input.send_keys(title + Keys.ENTER)
-    dept = 'Physical Education'
-    dept_input = self._get(self._cand_department_input)
-    dept_input.send_keys(dept + Keys.ENTER)
-    inst = 'Southern Connecticut State University'
-    inst_input = self._get(self._cand_inst_input)
-    inst_input.send_keys(inst + Keys.ENTER)
-    choice = random.choice(['recommend', 'oppose'])
-    if choice == 'recommend':
-      self._get(self._cand_recc_radio_btn).click()
-    else:
-      self._get(self._cand_opp_radio_btn).click()
-    reason = generate_paragraph(start_with_lorem=True)[2]
-    opt_reason_field = self._get(self._cand_reason)
-    opt_reason_field.send_keys(reason)
-    save_button = self._get(self._cand_form_save)
-    save_button.click()
-    self.check_for_flash_error()
-    time.sleep(2)
-
-    entry = self._get(self._cand_entry_view)
-    self._actions.move_to_element(entry).perform()
-    delete_recommendation = entry.find_element(*self._cand_view_delete)
-    time.sleep(.5)
-    self._actions.move_to_element(delete_recommendation).perform()
-    time.sleep(.5)
-    delete_recommendation.click()
-    confirm_delete = self._get(self._cand_view_delete_confirm_delete)
-    confirm_delete.click()
-    time.sleep(1)
-    try:
-      self._get(self._cand_entry_view)
-    except ElementDoesNotExistAssertionError:
-      logging.info('No listing was found - pass case')
-      return
-    raise(StandardError, 'Reviewer Recommendation found - should have been deleted')
+    pass
--- test/frontend/test_reviewer_candidates_task.py
+++ test/frontend/test_reviewer_candidates_task.py
@@ -12,7 +12,8 @@ import random
 import time
 
 from Base.Decorators import MultiBrowserFixture
-from Base.Resources import users
+from Base.Resources import users, reviewer_login, editorial_users
+from Cards.reviewer_candidates_card import ReviewerCandidatesCard
 from frontend.common_test import CommonTest
 from Pages.manuscript_viewer import ManuscriptViewerPage
 from Pages.workflow_page import WorkflowPage
@@ -58,6 +59,124 @@ class ReviewerCandidatesTaskTest(CommonTest):
     rev_cand_task = ReviewerCandidatesTask(self.getDriver())
     time.sleep(1)
     rev_cand_task.validate_styles()
+
+  def test_core_add_reviewer_candidate(self):
+    """
+    test_reviewer_candidates_task: Validates the submission of a recommended or opposed reviewer.
+      Validates the presentation of the data submitted from the task on the workflow card.
+    :return: void function
+    """
+    # Users logs in and make a submission
+    creator_user = random.choice(users)
+    dashboard_page = self.cas_login(email=creator_user['email'])
+    dashboard_page.set_timeout(60)
+    dashboard_page.click_create_new_submission_button()
+    self.create_article(journal='PLOS Wombat',
+                        type_='generateCompleteApexData',
+                        random_bit=True,
+                        title='Reviewer Candidates Test'
+                        )
+    # Time needed for iHat conversion. This is not quite enough time in all circumstances
+    dashboard_page.restore_timeout()
+    time.sleep(5)
+    manuscript_page = ManuscriptViewerPage(self.getDriver())
+    manuscript_page.validate_ihat_conversions_success(timeout=15)
+    # Note: Request title to make sure the required page is loaded
+    paper_id = manuscript_page.get_paper_db_id()
+    time.sleep(2)
+    # figures
+    manuscript_page.click_task('review_candidates')
+    time.sleep(3)
+    rev_cand_task = ReviewerCandidatesTask(self.getDriver())
+    time.sleep(2)
+    choice, reason = rev_cand_task.complete_reviewer_cand_form(reviewer_login)
+    rev_cand_task.click_completion_button()
+    rev_cand_task.check_for_flash_error()
+    rev_cand_task.logout()
+
+    # login as privileged user to validate the presentation of the data on the RC Card
+    staff_user = random.choice(editorial_users)
+    logging.info('Logging in as user: {0}'.format(['name']))
+    dashboard_page = self.cas_login(email=staff_user['email'])
+    time.sleep(3)
+    dashboard_page.go_to_manuscript(paper_id)
+    self._driver.navigated = True
+    paper_viewer = ManuscriptViewerPage(self.getDriver())
+    time.sleep(3)
+    # go to wf
+    paper_viewer.click_workflow_link()
+    workflow_page = WorkflowPage(self.getDriver())
+    time.sleep(2)
+    workflow_page.click_card('reviewer_candidates')
+    time.sleep(10)
+    rcc = ReviewerCandidatesCard(self.getDriver())
+    rcc.validate_styles()
+    rcc.check_initial_population(choice, reason)
+
+  def test_core_delete_reviewer_candidate(self):
+    """
+    test_reviewer_candidates_task: Validates the submission of a recommended or opposed reviewer.
+      Following submission deletes that submitted reviewer.
+      Validates no submitted data present on the workflow card.
+    :return: void function
+    """
+    # Users logs in and make a submission
+    creator_user = random.choice(users)
+    dashboard_page = self.cas_login(email=creator_user['email'])
+    dashboard_page.set_timeout(60)
+    dashboard_page.click_create_new_submission_button()
+    self.create_article(journal='PLOS Wombat',
+                        type_='generateCompleteApexData',
+                        random_bit=True,
+                        title='Reviewer Candidates Test'
+                        )
+    # Time needed for iHat conversion. This is not quite enough time in all circumstances
+    dashboard_page.restore_timeout()
+    time.sleep(5)
+    manuscript_page = ManuscriptViewerPage(self.getDriver())
+    manuscript_page.validate_ihat_conversions_success(timeout=15)
+    # Note: Request title to make sure the required page is loaded
+    paper_id = manuscript_page.get_paper_db_id()
+    time.sleep(2)
+    # figures
+    manuscript_page.click_task('review_candidates')
+    time.sleep(2)
+    rev_cand_task = ReviewerCandidatesTask(self.getDriver())
+    time.sleep(1)
+    rev_cand_task.delete_reviewer_cand_entry(random.choice(users))
+    time.sleep(2)
+    rev_cand_task.click_completion_button()
+    rev_cand_task.check_for_flash_error()
+    rev_cand_task.logout()
+
+    # login as privileged user to validate the presentation of the data on the RC Card
+    staff_user = random.choice(editorial_users)
+    logging.info('Logging in as user: {0}'.format(['name']))
+    dashboard_page = self.cas_login(email=staff_user['email'])
+    time.sleep(3)
+    dashboard_page.go_to_manuscript(paper_id)
+    self._driver.navigated = True
+    paper_viewer = ManuscriptViewerPage(self.getDriver())
+    time.sleep(3)
+    # go to wf
+    paper_viewer.click_workflow_link()
+    workflow_page = WorkflowPage(self.getDriver())
+    time.sleep(2)
+    workflow_page.click_card('reviewer_candidates')
+    time.sleep(10)
+    # TODO: Instantiate POM for Reviewer Candidate Card, validate information presented.
+
+  def test_core_reviewer_candidates_permissions(self):
+    """
+    test_reviewer_candidates_task: Validates the access permissions for the reviewer candidates task
+      a Reviewer will never ever see a reviewer recommendations card
+      an AE will always be able to view the reviewer recommendations card
+      a paper Creator can view/edit the reviewer recommendations card
+      a paper Collaborator can view/edit the reviewer recommendations card
+      Staff can view/edit the reviewer recommendations card
+    :return: void function
+    """
+    pass
 
 if __name__ == '__main__':
   CommonTest._run_tests_randomly()

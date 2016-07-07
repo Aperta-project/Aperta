a0849a58988a8f30073c7a3d538eaed1411808de
Erik Hetzner <ehetzner@plos.org>
2016-07-07 15:16:36 -0700

    APERTA-6719 Fix typo

--- client/app/pods/components/rescind-decision/component.js
+++ client/app/pods/components/rescind-decision/component.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   isEditable: true, // pass in false to disable the rescind button
 
   beforeRescind: null,
-  aferRescind: null,
+  afterRescind: null,
 
   // States:
   confirmingRescind: false,

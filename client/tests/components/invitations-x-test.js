c65ec4e8240bb4cc51ff5241140bd2955b8ff5d3
Sara Gibbons <sara@mutuallyhuman.com>
2016-07-07 20:10:50 -0400

    add test for invitations-x component; fix test refactoring test failures [APERTA-6870]

---
+++ client/tests/components/invitations-x-test.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+import {moduleForComponent, test} from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('invitations-x',
+                   'Integration | Component | invitations-x',
+                   {integration: true,
+                    beforeEach: function() {
+                      this.set('accept', () => {return;});
+                      this.set('close', () => {return;});
+                      this.set('decline', () => {return;});
+                      this.set('update', () => {return;});
+
+                      this.set('invitation', Ember.Object.create({
+                        declineReason: null,
+                        inviteeRole: 'Reviewer',
+                        pendingFeedback: false,
+                        reviewerSuggestions: null,
+                        state: 'pending',
+                        title: 'Awesome Paper!'
+                      }));
+                      this.set('invitations',[this.get('invitation')]);
+                    }});
+
+var template = hbs`{{invitations-x invitations=invitations
+                      accept=(action accept invitation)
+                      close=(action close invitation)
+                      decline=(action decline invitation)
+                      update=(action update invitation)
+                      }}`;
+
+test('pending invitation loads with accept/decline options', function(assert){
+  this.render(template);
+
+  assert.textPresent('h4', 'Reviewer Invitation');
+  assert.textPresent('button.invitation-accept', 'Accept Reviewer Invitation');
+  assert.textPresent('button.invitation-decline', 'Decline');
+  assert.elementFound('.pending-invitation', 'flagged as pending invitation');
+  assert.elementFound('.dashboard-paper-title', 'has paper title');
+});

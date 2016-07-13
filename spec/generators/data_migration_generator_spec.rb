2e29168f1753bb89e45f6fe2c1278aec8f593f64
Zach Dennis <zach.dennis@gmail.com>
2016-07-13 09:11:56 -0400

    Update data migration generator to create a migration that fail noisily by default.
    APETA-6861

--- spec/generators/data_migration_generator_spec.rb
+++ spec/generators/data_migration_generator_spec.rb
@@ -27,7 +27,7 @@ describe DataMigrationGenerator, type: :generator do
   it 'provides a default migration skeleton' do
     expect(File.read(migration)).to eq <<-FILE.strip_heredoc
       class PaintTheSkyBlue < ActiveRecord::Migration
-        RAKE_TASK_UP = 'fill_me_in'
+        RAKE_TASK_UP = place_rake_task_as_string_here
         # RAKE_TASK_DOWN = 'fill_me_in or delete me if unnecessary'
       end
     FILE

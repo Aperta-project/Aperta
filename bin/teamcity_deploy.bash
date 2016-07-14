89fde13f5641d96764f5d5bb8b558e474cb0125f
Mr. Jeffrey Gray <jgray@plos.org>
2016-07-13 22:22:26 -0700

    Incorporated feedback from egh. Moved to sourcing the chruby version and separated that out so it is applied for all following commends in the next pipeline.

--- bin/teamcity_deploy.bash
+++ bin/teamcity_deploy.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "Deploying release to Production..."
 sudo -u aperta -i /bin/bash << "EOF"
 cd %teamcity.build.checkoutDir%
 export BRANCH_NAME=%teamcity.build.branch%
 export VCS_NUMBER=%build.vcs.number%
+echo "Deploying release..."
 echo $BRANCH_NAME
 echo $VCS_NUMBER
-echo $HOME
 # Can't use -i as have to stay in checkout directory
 # Need to set any needed vars in Parameters as env vars
 eval `ssh-agent -s`
 echo $SSH_AGENT_PID
 ssh-add /home/aperta/.ssh/id_rsa
 ssh-add -l
-chruby-exec 2.2.3 -- gem install bundler && bundle install && bundle exec cap production deploy || exit 1
+chruby `cat .ruby-version` || exit 1
+gem install bundler && bundle install && bundle exec cap production deploy || exit 1
 EOF

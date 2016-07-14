4c6181891c9d4ad6a568c8745f654a6a5168ea7d
Sara Gibbons <saragibby@gmail.com>
2016-07-14 10:43:01 -0400

    add scheduled task to run billing log export daily [APERTA-6957]

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,10 @@ env :HOME, '/home/aperta'
 
 job_type :rake, 'cd :path && chruby-exec 2.2.3 -- bundle exec dotenv -f env rake :task --silent :output'
 
+every :day, at: '08:00' do
+  rake 'plos_billing:daily_billing_log_export'
+end
+
 every :day, at: '09:00' do
   rake 'simple_report'
 end

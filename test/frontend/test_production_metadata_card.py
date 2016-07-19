3a36e02b1a02207fe0252fa3c71b138a88187677
Sebastian Bassi <sebastian.bassi@globant.com>
2016-07-19 15:17:28 -0700

    Fix for production metadata test

--- test/frontend/test_production_metadata_card.py
+++ test/frontend/test_production_metadata_card.py
@@ -62,10 +62,13 @@ class ProductionMetadataCardTest(CommonTest):
     # Time needed for iHat conversion. This is not quite enough time in all circumstances
     time.sleep(5)
     manuscript_page = ManuscriptViewerPage(self.getDriver())
-    manuscript_page.validate_ihat_conversions_success(timeout=15)
+    # Give more time since successful status is needed for next step
+    manuscript_page.validate_ihat_conversions_success(timeout=60)
     paper_url = manuscript_page.get_current_url()
     paper_id = paper_url.split('/')[-1]
     logging.info('The paper ID of this newly created paper is: {0}'.format(paper_id))
+    if not paper_id:
+      raise ValueError('Manuscript is in unexpected state')
     manuscript_page.click_submit_btn()
     manuscript_page.confirm_submit_btn()
     # Now we get the submit confirmation overlay

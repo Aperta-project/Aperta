ab45ee1699a36257fad979316be4acc09cf3a629
Zach Dennis <zach.dennis@gmail.com>
2016-07-19 22:04:39 -0400

    Updating paper_state_transition_examples to remove unnecessary conditionals.
    APERTA-7066

--- spec/support/shared_examples/paper_state_transition_shared_examples.rb
+++ spec/support/shared_examples/paper_state_transition_shared_examples.rb
@@ -1,16 +1,13 @@
 shared_examples_for "transitions save state_updated_at" do |aasm_event_hash|
   it "sets state_updated_at to the current time" do
-    blk = aasm_event_hash.values.first
-    callable = blk || proc {
-      if [:submit!, :submit_minor_check!].include? aasm_event
-        paper.send(aasm_event, paper.creator, *method_args)
-      else
-        paper.send(aasm_event, *method_args)
-      end
-    }
+    blk = aasm_event_hash.values.first ||
+      fail(
+        ArgumentError,
+        "Please provide a key/value proc that performs the state transition, " +
+        "e.g.: submit: -> { paper.submit! arg1, arg2 }")
 
     Timecop.freeze(Time.current.utc) do
-      instance_eval &callable
+      instance_eval &blk
       expect(paper.state_updated_at).to eq(Time.current.utc)
     end
   end

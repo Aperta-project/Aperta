e30764e36410e4b33ca176b0f06ddd118f23b8a4
Zach Dennis <zach.dennis@gmail.com>
2016-07-19 21:03:03 -0400

    Adding a data-migration and corresponding rake task for setting PLOS Biology's staff_email.
    This will only set it if it's not already set.
    
    APERTA-7066

---
+++ db/migrate/20160720010120_set_plos_biology_staff_email.rb
@@ -0,0 +1,3 @@
+class SetPlosBiologyStaffEmail < DataMigration
+  RAKE_TASK_UP = 'data:migrate:set_plos_biology_staff_email'
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719220517) do
+ActiveRecord::Schema.define(version: 20160720010120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

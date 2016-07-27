d1f7fb92b092f0b2fea4285031d49250277872b2
Sebastian Bassi <sebastian.bassi@globant.com>
2016-07-27 13:17:18 -0700

    APERTA-7097 Initial commit to test on CI

---
+++ test/frontend/Cards/reviewer_report_card.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import logging
+import re
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from Base.PostgreSQL import PgSQL
+from frontend.Cards.basecard import BaseCard
+
+
+__author__ = 'sbassi@plos.org'
+
+class ReviewerReportCard(BaseCard):
+  """
+  Page Object Model for Reviewer Report Card
+  """
+  def __init__(self, driver):
+    super(ReviewerReportCard(, self).__init__(driver)
+
+    # Locators - Instance members
+
+
+    self._edit_invite_heading = (By.CSS_SELECTOR, 'h3.invite-to')
+    self._edit_invite_textarea = (By.CSS_SELECTOR, 'div.taller-textarea')
+    self._edit_invite_text_cancel = (By.CSS_SELECTOR, 'button.cancel')
+    self._edit_invite_text_send_invite_button = (By.CSS_SELECTOR, 'button.invite-reviewer-button')
+
+    self._invitees_table = (By.CLASS_NAME, 'invitees')
+
+
+  # POM Actions
+  def validate_card_elements_styles(self, paper_id):
+    """
+    This method validates the styles of the card elements including the common card elements
+    :return void function
+    """
+    self.validate_common_elements_styles(paper_id)
+
+  def validate_reviewer_report(self):
+    """
+    Invites the reviewer that is passed as parameter, verifying the composed email. Makes
+      function and style validations.
+    :param reviewer: user to invite as reviewer specified as email, or, if in system, name,
+        or username
+    :param title: title of the manuscript - for validation of invite content. Assumed to be unicode
+    :param creator: user object of the creator of the manuscript
+    :param manu_id: paper id of the manuscript
+    :return void function
+    """
+    time.sleep(.5)
+    self._get(self._recipient_field).send_keys(reviewer['email'] + Keys.ENTER)
+    self._get(self._compose_invitation_button).click()
+    time.sleep(2)
+    invite_heading = self._get(self._edit_invite_heading).text
+    # Since the reviewer is potentially off system, we can only validate email
+    assert reviewer['email'] in invite_heading, invite_heading
+    invite_text = self._get(self._edit_invite_textarea).text
+    # Always remember that our ember text always normalizes whitespaces down to one
+    #  Painful lesson
+    title = re.sub(r'[ \t\f\v]+', ' ', title)
+    # and need to scrub latin-1 non-breaking spaces
+    title = re.sub(u'\xa0', u' ', title)
+    assert title in invite_text, \
+        title + '\nNot found in \n' +invite_text
+    assert 'PLOS Wombat' in invite_text, invite_text
+    assert '***************** CONFIDENTIAL *****************' in invite_text, invite_text
+    creator_fn, creator_ln = creator['name'].split(' ')[0], creator['name'].split(' ')[1]
+    assert '{0}, {1}'.format(creator_ln, creator_fn) in invite_text, invite_text
+    abstract = PgSQL().query('SELECT abstract FROM papers WHERE id=%s;', (manu_id,))[0][0]
+    if abstract is not None:
+      # strip html, and remove whitespace
+      # NOTA BENE: BeautifulSoup4 inherently handles str to unicode conversion
+      abstract = self.get_text(abstract).strip()
+    if abstract is not None:
+      # Always remember that our ember text always normalizes whitespaces down to one
+      #  Painful lesson
+      abstract = re.sub(r'[ \t\f\v]+', ' ', abstract)
+      # It also removes trailing spaces
+      abstract = re.sub(r'[ \t\f\v]+\n', r'\n', abstract)
+      # and need to scrub latin-1 non-breaking spaces
+      abstract = re.sub(u'\xa0', u' ', abstract)
+      assert abstract in invite_text, abstract + '\nNot equal to\n' + invite_text
+    else:
+      assert 'Abstract is not available' in invite_text, invite_text
+    self._get(self._edit_invite_text_send_invite_button).click()
+    time.sleep(1)
+    invitee = self._get(self._invitee_listing)
+    invitee.find_element(*self._invitee_avatar)
+    invitee.find_element(*self._invitee_full_name)
+    invitee.find_element(*self._invitee_updated_at)
+    status = invitee.find_element(*self._invitee_state)
+    invitee.find_element(*self._invitee_revoke)
+    self.validate_invitation(reviewer, 'Reviewer')
+    assert 'Invited' in status.text, status.text
---
+++ test/frontend/test_reviewer_report.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+This behavioral test case validates Reviewer Report card
+This test requires the following data:
+The test document tarball from http://bighector.plos.org/aperta/docs.tar.gz extracted into
+    frontend/assets/docs/
+"""
+import logging
+import random
+import time
+
+from Base.Decorators import MultiBrowserFixture
+from Base.PostgreSQL import PgSQL
+from Base.Resources import prod_staff_login, reviewer_login, users, editorial_users
+from frontend.common_test import CommonTest
+from Cards.invite_reviewer_card import InviteReviewersCard
+from Cards.reviewer_report_card import ReviewerReportCard
+from Pages.manuscript_viewer import ManuscriptViewerPage
+from Pages.workflow_page import WorkflowPage
+
+__author__ = 'sbassi@plos.org'
+
+
+@MultiBrowserFixture
+class ReviewerReportCardTest(CommonTest):
+  """
+  Validate the elements, styles, functions of the Reviewer Report card
+  """
+
+  def test_reviewer_report_actions(self):
+    """
+    test_reviewer_report_card: Validates the elements, styles, roles and functions of invite
+      reviewers from new document creation through inviting reviewer, validation of the invite on
+      the invitees dashboard, acceptance and rejections
+    :return: void function
+    """
+    # Users logs in and make a submission
+    creator_user = random.choice(users)
+    dashboard_page = self.cas_login(email=creator_user['email'])
+    dashboard_page.set_timeout(60)
+    dashboard_page.click_create_new_submission_button()
+    self.create_article(journal='PLOS Wombat',
+                        type_='OnlyInitialDecisionCard',
+                        random_bit=True,
+                        )
+    dashboard_page.restore_timeout()
+    # Time needed for iHat conversion. This is not quite enough time in all circumstances
+    time.sleep(5)
+    manuscript_page = ManuscriptViewerPage(self.getDriver())
+    # Abbreviate the timeout for conversion success message
+    manuscript_page.validate_ihat_conversions_success(timeout=30)
+    # Note: Request title to make sure the required page is loaded
+    paper_id = manuscript_page.get_paper_id_from_url()
+    manuscript_page.click_submit_btn()
+    manuscript_page.confirm_submit_btn()
+    manuscript_page.close_modal()
+    # logout and enter as editor
+    manuscript_page.logout()
+    # Set up a handling editor, academic editor and cover editor for this paper
+
+    # self.set_editors_in_db(paper_id)
+    # login as editorial user
+    editorial_user = random.choice(editorial_users)
+    logging.info(editorial_user)
+    dashboard_page = self.cas_login(email=editorial_user['email'])
+    dashboard_page._wait_for_element(
+        dashboard_page._get(dashboard_page._dashboard_create_new_submission_btn))
+    dashboard_page.go_to_manuscript(paper_id)
+    self._driver.navigated = True
+    paper_viewer = ManuscriptViewerPage(self.getDriver())
+    paper_viewer._wait_for_element(paper_viewer._get(paper_viewer._tb_workflow_link))
+    # go to wf
+    paper_viewer.click_workflow_link()
+    workflow_page = WorkflowPage(self.getDriver())
+    workflow_page._wait_for_element(workflow_page._get(workflow_page._add_new_card_button))
+    workflow_page.click_card('invite_reviewers')
+    invite_reviewers = InviteReviewersCard(self.getDriver())
+    ##invite_reviewers.validate_card_elements_styles(paper_id)
+    logging.info('Paper id is: {0}.'.format(paper_id))
+    invite_reviewers.invite_reviewer(reviewer_login)
+    invite_reviewers.click_close_button()
+    workflow_page.logout()
+
+    # login as reviewer respond to invite
+    dashboard_page = self.cas_login(email=reviewer_login['email'])
+    dashboard_page.click_view_invites_button()
+
+    ms_title = PgSQL().query('SELECT title from papers WHERE id = %s;', (paper_id,))[0][0]
+    ms_title = unicode(ms_title, encoding='utf-8', errors='strict')
+    dashboard_page.accept_invitation(ms_title)
+    time.sleep(3)
+    dashboard_page.go_to_manuscript(paper_id)
+    self._driver.navigated = True
+    paper_viewer = ManuscriptViewerPage(self.getDriver())
+    paper_viewer._wait_for_element(paper_viewer._get(paper_viewer._tb_workflow_link))
+    # go to wf
+    paper_viewer.click_workflow_link()
+    workflow_page = WorkflowPage(self.getDriver())
+    workflow_page._wait_for_element(workflow_page._get(workflow_page._add_new_card_button))
+    workflow_page.click_card('reviewer_report')
+    reviewer_report_card = ReviewerReportCard(self.getDriver())
+    reviewer_report_card.validate_card_elements_styles()
+
+
+if __name__ == '__main__':
+  CommonTest._run_tests_randomly()

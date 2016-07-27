01f288c3920dba4c8710e16c6ef61a53ee11d862
Chris Westra <chris@mutuallyhuman.com>
2016-07-27 10:22:19 -0400

    Remove erroneous assertion
    The has_content check here was never actually passing.  Since it wasn't
    an assertion, it would simply time out and return false.

--- spec/support/pages/fragments/pending_invitation_fragment.rb
+++ spec/support/pages/fragments/pending_invitation_fragment.rb
@@ -2,12 +2,11 @@ class PendingInvitationFragment < PageFragment
   def accept(button_text='Accept')
     element_text = element.text
     click_button button_text
-    session.has_content? element_text
+    expect(session).to have_content element_text
   end
 
   def reject(button_text='Decline')
-    element_text = element.text
     click_button button_text
-    session.has_content? element_text
+    expect(session).to have_content("You've successfully declined this invitation")
   end
 end

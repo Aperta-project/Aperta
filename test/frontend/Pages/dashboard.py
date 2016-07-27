708c30baf08839c6accd58c37d806a3a56c76559
Sebastian Bassi <sebastian.bassi@globant.com>
2016-07-27 14:17:21 -0700

    APERTA-7097 Fixing search title on invitations

--- test/frontend/Pages/dashboard.py
+++ test/frontend/Pages/dashboard.py
@@ -150,16 +150,8 @@ class DashboardPage(AuthenticatedPage):
     invite_listings = self._gets(self._view_invites_invite_listing)
     for listing in invite_listings:
       if title in listing.text:
-        try:
-          # Remember to use single quotes outside to ensure you don't run afoul of
-          #  a single quote in the title - More often, double-quotes in titles are
-          #  rendered as smart quotes (unicode) so don't as often cause problems.
-          self._driver.find_element_by_xpath('//*[contains(text(), "{0}")]'.format(title))
-        except UnicodeEncodeError:
-          self._driver.find_element_by_xpath('//*[contains(text(), "{0}")]'
-                                           .format(title.encode('utf8')))
-      yes_btn = listing.find_element(*self._invite_yes_btn)
-      yes_btn.click()
+        yes_btn = listing.find_element(*self._invite_yes_btn)
+        yes_btn.click()          
 
   def accept_or_reject_invitation(self, title):
     """

968374d7e0294d41085daea820d7df12722c9e8b
Chris Westra <chris@mutuallyhuman.com>
2016-07-28 16:12:35 -0400

    Fix the content check in PendingInvitationFragment

--- spec/support/pages/fragments/pending_invitation_fragment.rb
+++ spec/support/pages/fragments/pending_invitation_fragment.rb
@@ -2,7 +2,7 @@ class PendingInvitationFragment < PageFragment
   def accept(button_text='Accept')
     element_text = element.text
     click_button button_text
-    expect(session).to have_content element_text
+    expect(session).to_not have_content element_text
   end
 
   def reject(button_text='Decline')

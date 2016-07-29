3ea0b6898a2eb8c0af67afe0e17d14732cb59526
Tad Park <tadp@users.noreply.github.com>
2016-07-29 12:13:44 -0700

    APERTA-6415 Send personalized email and subject with decision

--- engines/tahi_standard_tasks/app/controllers/tahi_standard_tasks/register_decision_controller.rb
+++ engines/tahi_standard_tasks/app/controllers/tahi_standard_tasks/register_decision_controller.rb
@@ -14,7 +14,9 @@ module TahiStandardTasks
       else
         decision = task.latest_decision
         task.complete_decision
-        task.send_email
+        to_field = task.answer_for('register_decision_questions--to-field').value
+        subject = task.answer_for('register_decision_questions--subject-field')
+        task.send_email(to_field: to_field, subject_field: subject)
         Activity.decision_made! decision, user: current_user
         render json: decision, status: :created
       end

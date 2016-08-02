281779184b93f7d66d7467336d36ae4524108fcd
Chris Westra <cwestra@gmail.com>
2016-08-02 10:16:19 -0400

    inline mixin

--- client/app/mixins/views/paper-index.js
+++ client/app/mixins/views/paper-index.js
@@ -1,10 +1,27 @@
 import Ember from 'ember';
-import RedirectsIfEditable from 'tahi/mixins/views/redirects-if-editable';
 
 const { on } = Ember;
 
-export default Ember.Mixin.create(RedirectsIfEditable, {
+export default Ember.Mixin.create({
   classNames: ['edit-paper'],
+
+  editable: Ember.computed.alias('controller.model.editable'),
+  supportedDownloadFormats: Ember.computed.alias('controller.supportedDownloadFormats'),
+
+  toggleEditable() {
+    if (this.get('editable') !== this.get('lastEditable')) {
+      this.set('lastEditable', this.get('editable'));
+    }
+  },
+
+  setupEditableToggle: Ember.on('didInsertElement', function() {
+    this.set('lastEditable', this.get('editable'));
+    this.addObserver('editable', this, this.toggleEditable);
+  }),
+
+  teardownEditableToggle: Ember.on('willDestroyElement', function() {
+    this.removeObserver('editable', this, this.toggleEditable);
+  }),
 
   _applyManuscriptCss: on('didInsertElement', function() {
     Ember.run.scheduleOnce('afterRender', ()=> {

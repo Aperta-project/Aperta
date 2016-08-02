231bbc6c9c5b641dd2915b4344b91bcec6b528f6
Chris Westra <cwestra@gmail.com>
2016-08-02 13:46:16 -0400

    Remove extra editable logic
    Neither template that used this mixin had the editable checkbox.
    This stuff all looks vestigial

--- client/app/mixins/views/paper-index.js
+++ client/app/mixins/views/paper-index.js
@@ -3,24 +3,6 @@ import Ember from 'ember';
 const { on } = Ember;
 
 export default Ember.Mixin.create({
-  editable: Ember.computed.alias('controller.model.editable'),
-  supportedDownloadFormats: Ember.computed.alias('controller.supportedDownloadFormats'),
-
-  toggleEditable() {
-    if (this.get('editable') !== this.get('lastEditable')) {
-      this.set('lastEditable', this.get('editable'));
-    }
-  },
-
-  setupEditableToggle: Ember.on('didInsertElement', function() {
-    this.set('lastEditable', this.get('editable'));
-    this.addObserver('editable', this, this.toggleEditable);
-  }),
-
-  teardownEditableToggle: Ember.on('willDestroyElement', function() {
-    this.removeObserver('editable', this, this.toggleEditable);
-  }),
-
   _applyManuscriptCss: on('didInsertElement', function() {
     Ember.run.scheduleOnce('afterRender', ()=> {
       this.get('controller.model.journal').then(function(journal) {

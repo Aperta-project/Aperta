e790998d8a0442d2a4c80bcd171e2ccb21a94b13
Sebastian Bassi <sebastian.bassi@globant.com>
2016-08-02 17:44:50 -0700

    For testing in TC

--- test/frontend/test_invite_reviewers.py
+++ test/frontend/test_invite_reviewers.py
@@ -12,7 +12,7 @@ import time
 
 from Base.Decorators import MultiBrowserFixture
 from Base.PostgreSQL import PgSQL
-from Base.Resources import prod_staff_login, reviewer_login, users, editorial_users
+from Base.Resources import prod_staff_login, reviewer_login, users, editorial_users, creator_login12
 from frontend.common_test import CommonTest
 from Cards.invite_reviewer_card import InviteReviewersCard
 from Pages.manuscript_viewer import ManuscriptViewerPage
@@ -35,7 +35,8 @@ class InviteReviewersCardTest(CommonTest):
     :return: void function
     """
     # Users logs in and make a submission
-    creator_user = random.choice(users)
+    ##creator_user = random.choice(users)
+    creator_user = creator_login12
     dashboard_page = self.cas_login(email=creator_user['email'])
     dashboard_page.set_timeout(60)
     dashboard_page.click_create_new_submission_button()

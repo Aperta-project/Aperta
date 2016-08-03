04109e10f72a11461dcba02ae357fa2773c63807
Chris Westra <cwestra@gmail.com>
2016-08-03 09:45:43 -0400

    Remove last view, start tests

--- client/app/pods/components/rendered-manuscript/component.js
+++ client/app/pods/components/rendered-manuscript/component.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import LazyLoader from 'ember-cli-lazyloader/lib/lazy-loader';
+import ENV from 'tahi/config/environment';
 
 export default Ember.Component.extend({
   paper: null, // passed-in
@@ -6,7 +8,7 @@ export default Ember.Component.extend({
 
   loadJournalStyles() {
     //paper's journal is async
-    this.get('paper.journal').then(function(journal) {
+    this.get('paper.journal').then((journal) => {
       const style = journal.get('manuscriptCss');
       this.$('.manuscript').attr('style', style);
     });
@@ -15,5 +17,30 @@ export default Ember.Component.extend({
   didInsertElement() {
     this._super(...arguments);
     Ember.run.scheduleOnce('afterRender', this, this.loadJournalStyles);
+  },
+
+  didRender() {
+    this._super(...arguments);
+    Ember.run.scheduleOnce('afterRender', this, this.refreshEquations);
+  },
+
+  updateEquations: Ember.observer('paper.body', function() {
+    Ember.run.scheduleOnce('afterRender', this, this.refreshEquations);
+  }),
+
+  loadMathJax: function() {
+    LazyLoader.loadScripts([ENV.mathjax.url]).then(() => {
+      this.refreshEquations();
+    });
+  },
+
+  refreshEquations:  function() {
+    if (!window.MathJax) { this.loadMathJax(); return; }
+    else if (!window.MathJax.Hub) { return; }
+
+    var view = this.$()[0];
+    Ember.run.next(() => {
+      MathJax.Hub.Queue(['Typeset', MathJax.Hub, view]);
+    });
   }
 });

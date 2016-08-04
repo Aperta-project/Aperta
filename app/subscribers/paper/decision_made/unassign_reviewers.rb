30baf15c3a64fe388642e6befdb9e898931aec34
Chris Westra <cwestra@gmail.com>
2016-08-04 09:52:04 -0400

    APERTA-7094 add comments

--- app/subscribers/paper/decision_made/unassign_reviewers.rb
+++ app/subscribers/paper/decision_made/unassign_reviewers.rb
@@ -1,5 +1,8 @@
+# Removes reviewers from a paper when a decision is made.
+# Note that reviewer invitations are also rescinded, but that
+# process is owned by Paper::DecisionMade::InvalidateReviewerInvitations
+# which lives in the tahi_standard_tasks engine
 class Paper::DecisionMade::UnassignReviewers
-
   def self.call(_event_name, event_data)
     paper = event_data[:record]
 
--- engines/tahi_standard_tasks/app/subscribers/paper/decision_made/invalidate_reviewer_invitations.rb
+++ engines/tahi_standard_tasks/app/subscribers/paper/decision_made/invalidate_reviewer_invitations.rb
@@ -1,5 +1,8 @@
+# Rescinds any in-flight reviewer invitations when a decision is made.
+# Reviewers are also unassigned from the paper, but that
+# process is owned by Paper::DecisionMade::UnassignReviewers
+# which lives in the main app.
 class Paper::DecisionMade::InvalidateReviewerInvitations
-
   def self.call(_event_name, event_data)
     paper = event_data[:record]
 

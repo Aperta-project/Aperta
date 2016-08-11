9c520b3ef6379f9246a04a8f72337647288c0195
Erik Hetzner and Jack LaBarba <pair+erik+jack@plos.org>
2016-08-11 13:58:17 -0700

    APERTA-6719 modify rescind migration to initialize some VersionedTexts as drafts

--- db/migrate/20160809204309_rescind.rb
+++ db/migrate/20160809204309_rescind.rb
@@ -8,14 +8,14 @@ class Rescind < ActiveRecord::Migration
       self == paper.decisions.reorder('created_at asc').last
     end
 
-    def make_draft!
+    def be_draft!
       update!(
         major_version: nil,
         minor_version: nil,
         registered_at: nil)
     end
 
-    def make_complete!
+    def be_complete!
       update!(
         major_version: revision_number,
         minor_version: 0,
@@ -24,12 +24,62 @@ class Rescind < ActiveRecord::Migration
   end
 
   # Redundantly define class for future-proofing
+  class VersionedText < ActiveRecord::Base
+    belongs_to :paper
+
+    def latest_on_paper?
+      self == paper.versioned_texts
+        .reorder('major_version asc, minor_version asc').last
+    end
+
+    def be_draft!
+      update! major_version: nil, minor_version: nil
+    end
+
+    # Give the text a new MAJOR version.
+    def be_major_version!
+      update!(
+        major_version: (paper.major_version || -1) + 1,
+        minor_version: 0)
+    end
+
+    # Give the text a new MINOR version
+    def be_minor_version!
+      update!(
+        major_version: (paper.major_version || 0),
+        minor_version: (paper.minor_version || -1) + 1)
+    end
+  end
+
+  # Redundantly define class for future-proofing
   class Paper < ActiveRecord::Base
     has_many :decisions
+    has_many :versioned_texts
 
     # States that come before a final decision has been made
     def non_terminal_publishing_state?
       !['accepted', 'rejected', 'published'].member?(publishing_state)
+    end
+
+    def draft_state?
+      draft_states = [
+        'unsubmitted',
+        'invited_for_full_submission',
+        'checking',
+        'in_revision']
+      draft_states.member?(publishing_state)
+    end
+
+    def minor_version_state?
+      ["checking", "invited_for_full_submission"].member?(publishing_state)
+    end
+
+    def major_version
+      versioned_texts.maximum(:major_version)
+    end
+
+    def minor_version
+      versioned_texts.maximum(:minor_version)
     end
   end
 
@@ -52,19 +102,26 @@ class Rescind < ActiveRecord::Migration
                 # assumed to be decisions that are in the process of being
                 # authored (the editor has selected a decision but hasn't
                 # clicked the register button yet).
-                decision.make_draft!
+                decision.be_draft!
               else
                 # assume last decision is complete if the paper is in a
                 # terminal publishing state and the decision has a verdict
-                decision.make_complete!
+                decision.be_complete!
               end
             else
               # decision has no verdict, so it can't be complete
-              decision.make_draft!
+              decision.be_draft!
             end
           else
             # decision is not latest on paper, so it must be complete
-            decision.make_complete!
+            decision.be_complete!
+          end
+        end
+
+        VersionedText.find_each do |versioned_text|
+          paper = versioned_text.paper
+          if versioned_text.latest_on_paper? && paper.draft_state?
+            versioned_text.be_draft!
           end
         end
       end
@@ -76,6 +133,14 @@ class Rescind < ActiveRecord::Migration
           else
             last_decision_index = decision.paper.decisions.count - 1
             decision.update!(revision_number: last_decision_index)
+          end
+        end
+
+        VersionedText.where(major_version: nil).find_each do |versioned_text|
+          if paper.minor_version_state?
+            versioned_text.be_minor_version!
+          else
+            versioned_text.be_major_version!
           end
         end
       end

c952fd9d94f3c3be92ad6f8189be1618d2e6d40e
Chris Westra <chris@mutuallyhuman.com>
2016-08-11 10:14:00 -0400

    Move invitations relationship to task.
    A polymorphic belongsTo will have trouble maintaining its inverse if the
    key doesn't point to the appropriate model name.  We already follow this
    pattern for comment looks, etc.

--- engines/tahi_standard_tasks/client/app/models/paper-editor-task.js
+++ engines/tahi_standard_tasks/client/app/models/paper-editor-task.js
@@ -1,13 +1,9 @@
-import Ember from 'ember';
 import DS from 'ember-data';
 import Task from 'tahi/models/task';
-
-const { computed } = Ember;
 
 export default Task.extend({
   academicEditors: DS.belongsTo('user'),
   relationshipsToSerialize: ['academicEditors', 'participants'],
   inviteeRole: DS.attr('string'),
-  invitations: DS.hasMany('invitation', { async: false }),
   invitationTemplate: DS.attr()
 });

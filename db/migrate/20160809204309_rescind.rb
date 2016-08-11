cef3a41edc9095f105c354e0273e9b678cb8c1d7
Erik Hetzner and Jack LaBarba <pair+erik+jack@plos.org>
2016-08-11 11:00:10 -0700

    APERTA-6719 modify rescind migration to properly establish drafts

--- db/migrate/20160809204309_rescind.rb
+++ db/migrate/20160809204309_rescind.rb
@@ -1,5 +1,38 @@
 # Prepare our data model for rescinded decisions
 class Rescind < ActiveRecord::Migration
+  # Redundantly define class for future-proofing
+  class Decision < ActiveRecord::Base
+    belongs_to :paper
+
+    def latest_on_paper?
+      self == paper.decisions.reorder('created_at asc').last
+    end
+
+    def make_draft!
+      update!(
+        major_version: nil,
+        minor_version: nil,
+        registered_at: nil)
+    end
+
+    def make_complete!
+      update!(
+        major_version: revision_number,
+        minor_version: 0,
+        registered_at: created_at)
+    end
+  end
+
+  # Redundantly define class for future-proofing
+  class Paper < ActiveRecord::Base
+    has_many :decisions
+
+    # States that come before a final decision has been made
+    def non_terminal_publishing_state?
+      !['accepted', 'rejected', 'published'].member?(publishing_state)
+    end
+  end
+
   # rubocop:disable Metrics/MethodLength
   def change
     change_column_null :versioned_texts, :major_version, true
@@ -11,19 +44,40 @@ class Rescind < ActiveRecord::Migration
 
     reversible do |direction|
       direction.up do
-        execute <<-SQL
-          UPDATE decisions
-          SET major_version = revision_number,
-              minor_version = 0,
-              registered_at = created_at;
-        SQL
+        Decision.find_each do |decision|
+          if decision.latest_on_paper?
+            if decision.verdict
+              if decision.paper.non_terminal_publishing_state?
+                # paper isn't in a terminal publishing state. These are
+                # assumed to be decisions that are in the process of being
+                # authored (the editor has selected a decision but hasn't
+                # clicked the register button yet).
+                decision.make_draft!
+              else
+                # assume last decision is complete if the paper is in a
+                # terminal publishing state and the decision has a verdict
+                decision.make_complete!
+              end
+            else
+              # decision has no verdict, so it can't be complete
+              decision.make_draft!
+            end
+          else
+            # decision is not latest on paper, so it must be complete
+            decision.make_complete!
+          end
+        end
       end
 
       direction.down do
-        execute <<-SQL
-          UPDATE decisions
-          SET revision_number = major_version;
-        SQL
+        Decision.find_each do |decision|
+          if decision.major_version.present?
+            decision.update! revision_number: decision.major_version
+          else
+            last_decision_index = decision.paper.decisions.count - 1
+            decision.update!(revision_number: last_decision_index)
+          end
+        end
       end
     end
 

5089a6eb7d1b6d25f934a631fc4a9aa1bf87dc6b
Chris Westra <chris@mutuallyhuman.com>
2016-08-12 14:48:59 -0400

    rough in show state

--- engines/tahi_standard_tasks/client/app/components/invitation-detail-row.js
+++ engines/tahi_standard_tasks/client/app/components/invitation-detail-row.js
@@ -2,15 +2,28 @@ import Ember from 'ember';
 import { PropTypes } from 'ember-prop-types';
 
 export default Ember.Component.extend({
-  tagName: 'tr',
-  classNames: 'invitation',
+  tagName: '',
   propTypes: {
     invitation: PropTypes.EmberObject.isRequired,
-    destroyAction: PropTypes.func.isRequired,
-    editAction: PropTypes.func
+    destroyAction: PropTypes.func.isRequired
   },
 
   invitee: Ember.computed.alias('invitation.invitee'),
   displayDestroy: Ember.computed.not('invitation.accepted'),
-  displayEdit: Ember.computed.and('invitation.pending', 'editAction')
+  displayEdit: Ember.computed.and('invitation.pending', 'showDetails', 'editAction'),
+
+  showDetails: false,
+
+  actions: {
+    editInvitation(invitation) {
+      invitation.fetchDetails().then(() => {
+        this.set('showDetails', true);
+      });
+    },
+    showDetails(invitation) {
+      invitation.fetchDetails().then(() => {
+        this.set('showDetails', true);
+      });
+    }
+  }
 });

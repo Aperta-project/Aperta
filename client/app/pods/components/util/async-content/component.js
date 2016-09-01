83dd5af91be20d3ed0029607fb0928e9a76fc954
Chris Westra <cwestra@gmail.com>
2016-09-01 08:37:17 -0400

    promise-loader -> async-content

--- client/app/pods/components/util/async-content/component.js
+++ client/app/pods/components/util/async-content/component.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  promiseTask: null, // an ember-concurrency task
+  task: null, // an ember-concurrency task
 
   init() {
     this._super(...arguments);
-    this.get('promiseTask').perform();
+    this.get('task').perform();
   }
 });

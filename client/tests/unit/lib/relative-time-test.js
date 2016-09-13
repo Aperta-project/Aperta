f2cfba1a046ee9f862fec626083ad8a57e65cf6f
Zach Dennis <zach.dennis@gmail.com>
2016-09-13 13:17:56 -0400

    Paper Tracker was incorrectly communicating how many days ago its version date or submission date was.
    It was communicating an off-by error in the case where the date in question happened later in the day then whatever the current time is. It would treat this has happening a day earlier.
    
    The relative-time implementation has been updated to always start from the end of "today". This is to ensure that anything that happened over the course of yesterday will be considered as "1 day ago". 
    
    This treats the _whole_ day as a day rather than just counting 24-hour cycles from whatever the current time is.
    
    APERTA-7365

---
+++ client/tests/unit/lib/relative-time-test.js
@@ -0,0 +1,64 @@
+import { module, test } from 'qunit';
+import relativeTime from 'tahi/lib/relative-time';
+
+module('RelativeTime');
+
+test('3 days ago, beginning of day', function(assert) {
+  const startOfDay = moment().utc().startOf('day');
+  const endOfDay = moment().utc().endOf('day');
+
+  var date = new Date('September 10, 2016 00:00:09 UTC');
+
+  assert.equal(relativeTime(date, startOfDay), '3 days ago', 'returns a human readable string for how many days ago a date was');
+  assert.equal(relativeTime(date, endOfDay), '3 days ago', 'returns a human readable string for how many days ago a date was');
+});
+
+test('3 days ago, noon time', function(assert) {
+  const startOfDay = moment().utc().startOf('day');
+  const endOfDay = moment().utc().endOf('day');
+
+  var date = new Date('September 10, 2016 12:00:00 UTC');
+
+  assert.equal(relativeTime(date, startOfDay), '3 days ago', 'returns a human readable string for how many days ago a date was');
+  assert.equal(relativeTime(date, endOfDay), '3 days ago', 'returns a human readable string for how many days ago a date was');
+});
+
+test('3 days ago, end of day', function(assert) {
+  const startOfDay = moment().utc().startOf('day');
+  const endOfDay = moment().utc().endOf('day');
+
+  var date = new Date('September 10, 2016 23:59:59 UTC');
+
+  assert.equal(relativeTime(date, startOfDay), '3 days ago', 'returns a human readable string for how many days ago a date was');
+  assert.equal(relativeTime(date, endOfDay), '3 days ago', 'returns a human readable string for how many days ago a date was');
+});
+
+test('61 days ago, beginning of day', function(assert) {
+  const startOfDay = moment().utc().startOf('day');
+  const endOfDay = moment().utc().endOf('day');
+
+  var date = new Date('July 14, 2016 00:00:09 UTC');
+
+  assert.equal(relativeTime(date, startOfDay), '61 days ago', 'returns a human readable string for how many days ago a date was');
+  assert.equal(relativeTime(date, endOfDay), '61 days ago', 'returns a human readable string for how many days ago a date was');
+});
+
+test('61 days ago, noon time', function(assert) {
+  const startOfDay = moment().utc().startOf('day');
+  const endOfDay = moment().utc().endOf('day');
+
+  var date = new Date('July 14, 2016 12:00:00 UTC');
+
+  assert.equal(relativeTime(date, startOfDay), '61 days ago', 'returns a human readable string for how many days ago a date was');
+  assert.equal(relativeTime(date, endOfDay), '61 days ago', 'returns a human readable string for how many days ago a date was');
+});
+
+test('61 days ago, end of day', function(assert) {
+  const startOfDay = moment().utc().startOf('day');
+  const endOfDay = moment().utc().endOf('day');
+
+  var date = new Date('July 14, 2016 23:59:59 UTC');
+
+  assert.equal(relativeTime(date, startOfDay), '61 days ago', 'returns a human readable string for how many days ago a date was');
+  assert.equal(relativeTime(date, endOfDay), '61 days ago', 'returns a human readable string for how many days ago a date was');
+});

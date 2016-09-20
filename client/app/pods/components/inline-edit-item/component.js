3738d7c10318e27a928a6e733a0fb89b0d537b71
Ben Beckwith <bnbeckwith@gmail.com>
2016-09-20 16:18:18 -0400

    APERTA-7632 use component helper

--- client/app/pods/components/inline-edit-item/component.js
+++ client/app/pods/components/inline-edit-item/component.js
@@ -1,18 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isTextItem:     Ember.computed.equal('item.type', 'text'),
-  isCheckboxItem: Ember.computed.equal('item.type', 'checkbox'),
-  isEmailItem:    Ember.computed.equal('item.type', 'email'),
-  isLabelItem:    Ember.computed.equal('item.type', 'adhoc-label'),
-  isSendable: true,
-
   // attrs
-  item: null,
+  allUsers: null,
   block: null,
   editing: false,
-  allUsers: null,
-  overlayParticipants: null,
   emailSentStates: null,
-  isNew: false
+  isNew: false,
+  isSendable: true,
+  item: null,
+  overlayParticipants: null
 });

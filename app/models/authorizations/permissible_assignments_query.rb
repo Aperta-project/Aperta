5281004397a646394b5c43163ca4bd78b1cb56ac
Zach Dennis <zach.dennis@gmail.com>
2016-10-06 16:28:43 -0400

    Adding inline documentation to PermissibleAssignmentsQuery
    APERTA-7505

--- app/models/authorizations/permissible_assignments_query.rb
+++ app/models/authorizations/permissible_assignments_query.rb
@@ -1,10 +1,43 @@
 module Authorizations
-  # The assignments table gathers all permissions, roles, and states from role assignments.
-  # This table is joined with every auth_config
+
+  # PermissibleAssignmentsQuery represents the query responsible for finding
+  # all Assignment(s) that have a given permission for a collection of
+  # types/classes.
+  #
+  # == Columns returned
+  #
+  # Running this query will return the following columns:
+  #
+  #   * assignments.id AS id
+  #   * assignments.assigned_to_type AS assigned_to_type
+  #   * assignments.assigned_to_id AS assigned_to_id
+  #   * roles.id AS role_id
+  #   * roles.name AS role_name
+  #   * permissions.id AS permission_id
+  #
+  # This query will also GROUP BY the following columns to ensure that
+  # we do not have duplicate assignments <-> roles <-> permissions records
+  # returned:
+  #
+  #   * assignments.assigned_to_type
+  #   * assignments.assigned_to_id
+  #   * assignments.id
+  #   * roles.id
+  #   * permissions.id
+  #
   class PermissibleAssignmentsQuery
     attr_reader :user, :permission, :klass, :applies_to, :auth_configs, :participations_only
 
-    def initialize user:, permission:, klass:, applies_to:, auth_configs:, participations_only: true
+    # == Constructor Arguments
+    # * user: the user who the query will be check for authorization against
+    # * permission: the permission action to check against, e.g. :view
+    # * klass: the type/class that is being queried against
+    # * applies_to: the collection of types/classes that should be included \
+    #     when checking against the permissions table
+    # * auth_configs: the collection of Authorization(s) to JOIN against
+    # * participations_only: a boolean specifying if only targets a user \
+    #     participates in should be returned.
+    def initialize(user:, permission:, klass:, applies_to:, auth_configs:, participations_only:)
       @user = user
       @permission = permission
       @klass = klass
@@ -38,7 +71,8 @@ module Authorizations
 
     # Returns an AREL AST which represents a basic query for finding
     # all assignments for a user joining on their roles, permissions,
-    # and permission_states.
+    # and permission_states. This is a static part of this query with the
+    # user.id being the only dynamic bit.
     def base_query
       @base_query ||= begin
         # In the beginning we cheat by using ActiveRecord to reduce the
@@ -56,14 +90,32 @@ module Authorizations
         .joins(permissions: :states)
         .where(assignments: { user_id: user.id })
 
-        # explicitly calling .arel rather than converting Assignment.all
+        # explicitly call .arel rather than convert Assignment.all
         # to user.assignments. The reason is that ActiveRecord::Relation
         # will produce bind parameters that will not get handled correctly
-        # when we convert to AREL queries below
+        # when AREL is asked to generate SQL
         assignments.arel
       end
     end
 
+    # This loops over all of the authorization paths and adds a
+    # LEFT OUTER JOIN from the assignments table. This is a dynamic part of the
+    # the query.
+    #
+    # == Example of SQL Generated
+    #
+    #   LEFT JOIN "journals" on "journals"."id" = "assignments"."assigned_to_id"
+    #     AND "assignments"."assigned_to_type" = 'Journal'
+    #   LEFT JOIN "papers" on "papers"."id" = "assignments"."assigned_to_id"
+    #     AND "assignments"."assigned_to_type" = 'Paper'
+    #   LEFT JOIN "tasks" on "tasks"."id" = "assignments"."assigned_to_id"
+    #     AND "assignments"."assigned_to_type" = 'Task'
+    #
+    # == Note:
+    #
+    # Do not use an INNER JOIN here because the authorization paths may look at
+    # at different tables, meaning only one of those tables will have have
+    # results. We'll filter in the method: add_auth_config_conditions.
     def add_auth_config_joins(query)
       auth_configs.each do |ac|
         join_table = ac.assignment_to.arel_table
@@ -81,6 +133,20 @@ module Authorizations
       end
     end
 
+    # Whereas add_auth_config_joins adds the JOINs for the authorization
+    # paths this adds the WHERE conditions for those JOINs. It loops over
+    # the authorization paths and filters out any rows that do not have one
+    # e primary key value populated for the JOINed records.
+    #
+    # == Example of SQL Generated
+    #   (
+    #     (
+    #      "journals"."id" is not null
+    #       OR "papers"."id" is not null
+    #     )
+    #     OR "tasks"."id" is not null
+    #   )
+    #
     def add_auth_config_conditions(query)
       arel_conditions = auth_configs.reduce(nil) do |conditions, ac|
         if conditions
@@ -97,14 +163,30 @@ module Authorizations
         .where(Permission.arel_table[:applies_to].in(applies_to))
     end
 
-    # If we're looking for the wildcard permission then we aren't interested in any one
-    # permission, but all of the possible permissions
+    # If we're looking for the wildcard permission then we aren't interested
+    # in any one permission, but all of the possible permissions. When we're not
+    # looking for the wildcard permission then add a new condition the
+    # WHERE clause.
+    #
+    # == Example of SQL Generated
+    #
+    #   "permissions"."action" = 'view'
+    #
     def add_wildcard_permission(query)
       if @permission.to_sym != Permission::WILDCARD.to_sym
         query.where(Permission.arel_table[:action].eq(@permission))
       end
     end
 
+    # If @participations_only is true then check to see if any of the roles
+    # the user is assigned to have a participates_in_NNN column. If so,
+    # add a WHERE clause condition that filters out records that do not have
+    # that column set to true.
+    #
+    # == Example of SQL Generated
+    #
+    #   "roles"."participates_in_papers" = 't'
+    #
     def add_participations_only(query)
       if @participations_only
         role_accessibility_method = "participates_in_#{@klass.table_name}"

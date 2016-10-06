13302d364f25b8f5a60729443d5d37f2bc86741a
Zach Dennis <zach.dennis@gmail.com>
2016-10-06 16:22:41 -0400

    Update HydrateObjects to lazily memoize the result set so it doesn't repeat the query every time the result set is requested.
    Also, use the current object's permission_actions.
    
    APERTA-7505

--- app/models/authorizations/hydrate_objects.rb
+++ app/models/authorizations/hydrate_objects.rb
@@ -10,27 +10,28 @@ module Authorizations
     end
 
     def to_result_set
-      # pull out permissions
-      objects.each_with_object(Query::ResultSet.new) do |object, rs|
-        permission_states = Hash.new { |h,k| h[k] = { states: [] } }
+      @results_set ||= begin
+        objects.each_with_object(Query::ResultSet.new) do |object, rs|
+          permission_states = Hash.new { |h,k| h[k] = { states: [] } }
 
-        # Permission_actions come thru as a string column, e.g:
-        #   "read:*, talk:in_progress, talk:in_review, view:*, write:in_progress"
-        #
-        # They need to be parsed out and the permission states should be
-        # grouped by their corresponding permission action.
-        objects.first.permission_actions.
-          split(/\s*,\s*/).
-          map { |f| f.split(/:/) }.
-          each { |permission, state| permission_states[permission][:states] << state }
-        rs.add_object(object, with_permissions: permission_states)
+          # Permission_actions come thru as a string column, e.g:
+          #   "read:*, talk:in_progress, talk:in_review, view:*, write:in_progress"
+          #
+          # They need to be parsed out and the permission states should be
+          # grouped by their corresponding permission action.
+          object.permission_actions.
+            split(/\s*,\s*/).
+            map { |f| f.split(/:/) }.
+            each { |permission, state| permission_states[permission][:states] << state }
+          rs.add_object(object, with_permissions: permission_states)
+        end
       end
     end
 
     private
 
     def objects
-      target.from( Arel.sql("(#{query.to_sql}) AS #{klass.table_name} ") )
+      target.from Arel.sql("(#{query.to_sql}) AS #{klass.table_name} ")
     end
   end
 end

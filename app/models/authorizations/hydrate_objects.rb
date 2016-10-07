3b1bc569a2d81e62e07b975ba479b9d8ff8d3fca
Zach Dennis <zach.dennis@gmail.com>
2016-10-07 11:31:07 -0400

    Adding class and inline method documentation for HydrateObjects.
    APERTA-7505

--- app/models/authorizations/hydrate_objects.rb
+++ app/models/authorizations/hydrate_objects.rb
@@ -1,14 +1,27 @@
 module Authorizations
+
+  # HydrateObjects is responsible for taking a HydrateObjectsQuery instance,
+  # executing it against a given @target ActiveRecord::Relation, and
+  # returning a ResultSet that contains all of ActiveRecord model instances
+  # that are returned.
   class HydrateObjects
     include QueryHelpers
+
     attr_reader :klass, :query, :target
 
+    # == Constructor Arguments
+    # * klass: the type/class that is being queried against (e.g. Task, Paper)
+    # * query: a HydratObjectsQuery instance or any query that includes a \
+    #    permission_actions column
+    # * target: the ActiveRecord::Relation being queried against. This will be
+    #    used to further filter down the given query results.
     def initialize(query:, klass:, target:)
       @klass = klass
       @query = query
       @target = target
     end
 
+    # Returns a ResultSet instance containing the results of the query.
     def to_result_set
       @results_set ||= begin
         objects.each_with_object(Query::ResultSet.new) do |object, rs|
@@ -22,9 +35,25 @@ module Authorizations
 
     private
 
+    # Returns an ActiveRecord::Relation instance that combines @target
+    # and the passed in @query.
     def objects
       target.from Arel.sql("(#{query.to_sql}) AS #{klass.table_name} ")
     end
+
+    # permission_actions come thru as a string column, e.g:
+    #   "view:*, edit:unsubmitted, submit:unsubmitted, withdraw:submitted"
+    #
+    # These permission_actions need to be parsed out into a hash map that
+    # groups state by their permission action. E.g.
+    #
+    #   {
+    #     view: ['*'],
+    #     edit: ['unsubmitted'],
+    #     withdraw: ['unsubmitted', 'submitted', 'in_review']
+    #    }
+    #
+    # Note: the above Hash is meant to be illustrative, not accurate.
     def convert_permission_actions_into_hash_map(permission_actions)
       permissions_map = Hash.new { |h,k| h[k] = { states: [] } }
 

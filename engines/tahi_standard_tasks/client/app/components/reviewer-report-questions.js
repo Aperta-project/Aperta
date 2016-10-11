dfe880cf837f66c03f22712515906d27759c5bda
Jerry Nummi <jerry@mutuallyhuman.com>
2016-10-11 12:35:03 -0700

    Dynamic Reviewer Report competing interests link

--- engines/tahi_standard_tasks/client/app/components/reviewer-report-questions.js
+++ engines/tahi_standard_tasks/client/app/components/reviewer-report-questions.js
@@ -2,5 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   decision: null,
-  readOnly: false
+  readOnly: false,
+
+  competingInterestsLink: Ember.computed(function() {
+    const journal = this.get('model.paper.journal.name')
+                        .toLowerCase()
+                        .replace(' ', '');
+
+    return 'http://journals.plos.org/' + journal + '/s/reviewer-guidelines#loc-competing-interests';
+  }),
 });

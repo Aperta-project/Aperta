b9fffe2b02b8b14e59b3d8cd9776475261be9f35
Jerry Nummi <jerry@mutuallyhuman.com>
2016-10-15 13:45:08 -0400

    Popover menu component

---
+++ client/app/pods/components/basic-popover-trigger/component.js
@@ -0,0 +1,5 @@
+import BasicDropdownTrigger from 'ember-basic-dropdown/components/basic-dropdown/trigger';
+
+export default BasicDropdownTrigger.extend({
+  classNames: ['basic-popover-trigger']
+});
---
+++ client/app/pods/components/basic-popover/component.js
@@ -0,0 +1,6 @@
+import BasicDropdown from 'ember-basic-dropdown/components/basic-dropdown';
+
+export default BasicDropdown.extend({
+  triggerComponent: 'basic-popover-trigger',
+  contentComponent: 'basic-popover-content'
+});

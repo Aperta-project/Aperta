4c013ac81f72de59cfbaa557ff6367f82b572c9e
AJ Hekman <aj@mutuallyhuman.com>
2016-10-18 11:00:48 -0400

    APERTA-7554 create OrcidAccounts for existing users

---
+++ db/migrate/20161018143343_add_orcid_account_records_for_existing_users.rb
@@ -0,0 +1,15 @@
+# For ease and simplification of implementation all users are given an
+# OrcidAccount db record regardless if they have connected to orcid or not.
+class AddOrcidAccountRecordsForExistingUsers < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      INSERT INTO orcid_accounts (user_id, created_at, updated_at)
+      SELECT                      id,      created_at, updated_at
+      FROM users;
+    SQL
+  end
+
+  def down
+    execute "DELETE FROM orcid_accounts"
+  end
+end

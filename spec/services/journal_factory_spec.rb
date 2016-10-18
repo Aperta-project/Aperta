494faba97825e0e558a33b73d9dbba7894e9d795
Zach Dennis <zach.dennis@gmail.com>
2016-10-18 13:38:58 -0400

    Adding an explicit test for Reviewers not being able to access PlosBioTechCheck tasks.
    The functionality was already in place.
    
    APERTA-7543

--- spec/services/journal_factory_spec.rb
+++ spec/services/journal_factory_spec.rb
@@ -1107,8 +1107,13 @@ describe JournalFactory, flaky: true do
             expect(permissions).not_to include(*changes_for_author_permissions)
           end
 
+          it 'can do nothing on any of the PlosBioTechCheck tasks' do
+            tech_check_permissions = Permission.where(
+              applies_to: tech_check_klasses.map(&:name)
+            ).all
+            expect(permissions).not_to include(*tech_check_permissions)
+          end
         end
-
       end
 
       context 'Reviewer Report Owner' do

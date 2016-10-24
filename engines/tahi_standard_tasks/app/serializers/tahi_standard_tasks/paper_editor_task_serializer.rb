5c6a939f0f10bac30ecb89f6f14f70c240ee1578
Chris Westra <cwestra@gmail.com>
2016-10-24 17:14:19 -0400

    APERTA--5579 remove invite_queues from serializer

--- engines/tahi_standard_tasks/app/serializers/tahi_standard_tasks/paper_editor_task_serializer.rb
+++ engines/tahi_standard_tasks/app/serializers/tahi_standard_tasks/paper_editor_task_serializer.rb
@@ -3,7 +3,6 @@ module TahiStandardTasks
     embed :ids
     has_many :academic_editors
     has_many :invitations, include: true
-    has_many :invite_queues, include: true
     attributes :invitation_template, :invitee_role
   end
 end

89fd31cd87bfcaec5d8b2d53083f5244391d8257
Jerry Nummi <jerry@mutuallyhuman.com>
2016-10-24 14:35:36 -0700

    Close basic-popover when clicked

--- client/app/pods/components/basic-popover-content/component.js
+++ client/app/pods/components/basic-popover-content/component.js
@@ -1,5 +1,25 @@
 import BasicDropdownContent from 'ember-basic-dropdown/components/basic-dropdown/content';
 
 export default BasicDropdownContent.extend({
-  class: 'popover-menu'
+  class: 'popover-menu',
+
+  init() {
+    this._super(...arguments);
+    const self = this;
+    $('body').on('click.basic-popover-content', function(e) {
+      self.handleContentClick(e);
+    });
+  },
+
+  handleContentClick(e) {
+    const clickedInPopover = $(e.target).closest('.ember-basic-dropdown-content');
+    if(clickedInPopover.length) {
+      this.get('dropdown').actions.close();
+    }
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    $('body').off('click.basic-popover-content');
+  }
 });

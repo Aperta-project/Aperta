5f61d12bb1eab8193bd289ee86bda103f525c941
Zach Dennis <zach.dennis@gmail.com>
2016-10-26 15:58:23 -0400

    Updating paper_editor_mailer_spec to have an example around invitation attachments being included in the email when present and adding a few missing examples.
    APERTA-8096

--- engines/tahi_standard_tasks/spec/mailers/tahi_standard_tasks/paper_editor_mailer_spec.rb
+++ engines/tahi_standard_tasks/spec/mailers/tahi_standard_tasks/paper_editor_mailer_spec.rb
@@ -1,22 +1,27 @@
 require 'rails_helper'
 
 describe TahiStandardTasks::PaperEditorMailer do
-
-  let(:invitation) { FactoryGirl.create(:invitation, body: "Hiya, chief!") }
-
-  let(:email) { described_class.notify_invited(invitation_id:invitation.id) }
+  include ClientRouteHelper
 
   describe "#notify_invited" do
-    it "has correct subject line" do
+    subject(:email) do
+      described_class.notify_invited(invitation_id: invitation.id)
+    end
+
+    let(:invitation) do
+      FactoryGirl.create(:invitation, body: "Hiya, chief!")
+    end
+
+    it "has the correct subject line" do
       expect(email.subject).to eq "You've been invited as an editor for the manuscript, \"#{invitation.task.paper.display_title}\""
     end
 
-    it "has correct body content" do
+    it "has the correct body content" do
       expect(email.body).to include invitation.body
     end
 
-    it "sends email to the invitations email" do
-      expect(email.to).to eq([invitation.email])
+    it "sends the email to the invitee's email" do
+      expect(email.to).to contain_exactly(invitation.email)
     end
 
     it "contains a link to the dashboard" do
@@ -25,6 +30,34 @@ describe TahiStandardTasks::PaperEditorMailer do
 
     it "bcc's apertachasing@plos.org to support chasing in Salesforce" do
       expect(email.bcc).to include('apertachasing@plos.org')
+    end
+
+    it "attaches attachments on the invitation" do
+      invitation.attachments << FactoryGirl.build(
+        :invitation_attachment,
+        file: File.open(Rails.root.join("spec/fixtures/bill_ted1.jpg"))
+      )
+      invitation.attachments << FactoryGirl.build(
+        :invitation_attachment,
+        file: File.open(Rails.root.join("spec/fixtures/yeti.gif"))
+      )
+
+      expect(email.attachments.length).to eq(2)
+      expect(email.attachments.map(&:filename)).to contain_exactly(
+        "bill_ted1.jpg",
+        "yeti.gif"
+      )
+    end
+
+    describe "links" do
+      it "has a link to Aperta's dashboard for accepting the invitation in the email body" do
+        expect(email.body).to include client_dashboard_url
+      end
+
+      it "has a link to decline the invitation in the email body" do
+        expect(email.body).to include
+        confirm_decline_invitation_url(invitation.token)
+      end
     end
   end
 end

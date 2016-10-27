b80f92d8dc9ab0b0a0df24afeebb99d877014b98
Zach Dennis <zach.dennis@gmail.com>
2016-10-27 10:58:12 -0400

    Improve handling when orcid connect is not enabled
    * do not include orcid_account_id in CurrentUserSerializer or UserSerializer orcid connect is not enabled
    
    * update OrcidAccountsController to render a 404  with a meaningful response body when orcid connect is not enabled
    
    * now that the OrcidAccountsController won't return orcid accounts when disabled we no longer need to  conditionally return an id in the OrcidAccountSerializer
    
    * do not show the orcid-connect component/button on the profile page when the user has no orcidAccount record.
    
    * introduce a ErrorSerializer that takes an error message for rendering errors over HTTP
    
    APERTA-7394

--- app/serializers/orcid_account_serializer.rb
+++ app/serializers/orcid_account_serializer.rb
@@ -5,12 +5,4 @@ class OrcidAccountSerializer < ActiveModel::Serializer
     :profile_url,
     :status,
     :oauth_authorize_url
-
-  def id
-    # Returning nil for the ID is an effective way to disable the feature on the
-    # frontend. The connect to orcid button will remain disabled without an
-    # OrcidAccount.
-    return nil unless TahiEnv.orcid_connect_enabled?
-    object.id
-  end
 end

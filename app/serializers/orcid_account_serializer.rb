7c3ad34b701566338505bbdd6bf0358c30c38c6d
Zach Dennis <zach.dennis@gmail.com>
2016-10-27 13:49:19 -0400

    Only include name, oauth_authorize_url, and status of an orcid account when retrieving for the current_user.
    The other fields are publicly available fields.
    
    APERTA-7394

--- app/serializers/orcid_account_serializer.rb
+++ app/serializers/orcid_account_serializer.rb
@@ -5,4 +5,22 @@ class OrcidAccountSerializer < ActiveModel::Serializer
     :profile_url,
     :status,
     :oauth_authorize_url
+
+  private
+
+  def include_name?
+    is_current_user?
+  end
+
+  def include_oauth_authorize_url?
+    is_current_user?
+  end
+
+  def include_status?
+    is_current_user?
+  end
+
+  def is_current_user?
+    current_user.id == object.user_id
+  end
 end

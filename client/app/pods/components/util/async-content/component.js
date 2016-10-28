ceb4219f5aad16a4d9b81c0e336e9852a32187d9
Chris Westra <cwestra@gmail.com>
2016-10-28 16:19:58 -0400

    APERTA-5579 rename task to concurrencyTask

--- client/app/pods/components/util/async-content/component.js
+++ client/app/pods/components/util/async-content/component.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  task: null, // an ember-concurrency task
+  concurrencyTask: null, // an ember-concurrency task
 
   init() {
     this._super(...arguments);
-    this.get('task').perform();
+    this.get('concurrencyTask').perform();
   }
 });

5c3c8e52dd4812df9ff8963d3abc8db35f10c79d
Erik Hetzner <ehetzner@plos.org>
2016-11-01 20:42:53 -0700

    APERTA-7940 Fix imports

--- client/tests/addons/inviting-a-reviewer-test.js
+++ client/tests/addons/inviting-a-reviewer-test.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
-import startApp from 'tahi/tests/helpers/start-app';
+import startApp from '../helpers/start-app';
 import FactoryGuy from 'ember-data-factory-guy';
 import Factory from '../helpers/factory';
 import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
--- client/tests/integration/components/tasks/paper-reviewer-task-test.js
+++ client/tests/integration/components/tasks/paper-reviewer-task-test.js
@@ -1,9 +1,8 @@
 import {moduleForComponent, test} from 'ember-qunit';
-import startApp from '../helpers/start-app';
+import startApp from '../../../helpers/start-app';
 import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
-// Pretend like you're in client/tests
-import FakeCanService from '../helpers/fake-can-service';
+import FakeCanService from '../../../helpers/fake-can-service';
 
 let app;
 

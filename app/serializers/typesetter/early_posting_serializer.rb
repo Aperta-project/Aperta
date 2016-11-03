06c3f63dc2fcf8521e46400080e1911ef9de27b4
Brandon Zylstra <brandon@mutuallyhuman.com>
2016-11-03 11:47:35 -0400

    remove statement from early_posting in Apex JSON
    Instead of
    
        "early_article_posting": {
            "consent": true,
            "statement": "Yes, I agree to publish an early version of my article"
        },
    
    it'll become
    
        "early_article_posting": {
            "consent": true,
        },

--- app/serializers/typesetter/early_posting_serializer.rb
+++ app/serializers/typesetter/early_posting_serializer.rb
@@ -2,14 +2,10 @@ module Typesetter
   # Serializes the early posting data for the typesetter.
   # Expects the early posting task as its object to serialize.
   class EarlyPostingSerializer < Typesetter::TaskAnswerSerializer
-    attributes :consent, :statement
+    attributes :consent
 
     def consent
       object.answer_for('early-posting--consent').try(:value)
-    end
-
-    def statement
-      object.question_for('early-posting--consent').try(:text)
     end
   end
 end

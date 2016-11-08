4247bbb2417c58c7d4ee8049d45f8872b5c79c8e
AJ Hekman <aj@mutuallyhuman.com>
2016-11-08 16:07:28 -0500

    APERTA-7932 log save location on S3

--- app/uploaders/billing_log_uploader.rb
+++ app/uploaders/billing_log_uploader.rb
@@ -3,9 +3,15 @@
 class BillingLogUploader < CarrierWave::Uploader::Base
   storage Rails.application.config.carrierwave_storage
 
+  after :store, :log_file_location
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def log_file_location(_)
+    Rails.logger.info "Saved Billing log to #{@file.path}"
   end
 end

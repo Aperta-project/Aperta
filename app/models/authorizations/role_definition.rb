faa624522ae67916842af3c7da1cda24bce616c9
Zach Dennis <zach.dennis@gmail.com>
2016-11-08 10:28:44 -0500

    Improving how R&P definitions are imported into the database.
    Build up an in-memory definition of a role and its permissions; then import each role one at a time with its permissions.
    
    * This also resolves a bug where permissions that were no longer referred to were kept in the database. For example: if Cover Editor had :manage_collaborators on Paper and then we removed it, the permission would still exist in the database. :(
    
    * Adds activerecord-import gem to do the heavy lifting
    
    Paired with @Bestra
    
    APERTA-8253

---
+++ app/models/authorizations/role_definition.rb
@@ -0,0 +1,49 @@
+module Authorizations
+  # RoleDefinition represents the definition a role. It is used for building
+  # up role definitions that can be imported with Authorizations::RoleImporter
+  # efficiently.
+  class RoleDefinition
+    attr_accessor :name, :journal, :participates_in, :permission_definitions
+
+    def self.ensure_exists(name, journal: nil, participates_in: [], &block)
+      role_definition = new(
+        name: name,
+        journal: journal,
+        participates_in: participates_in
+      )
+      yield role_definition if block
+      role_definition.ensure_exists!
+    end
+
+    def initialize(name:, journal:, participates_in: [])
+      @name = name
+      @journal = journal
+      @participates_in = participates_in || []
+
+      whitelist = [::Task, ::Paper]
+      fail StandardError, "Bad participates_in: #{participates_in}" unless \
+        ((whitelist | participates_in) == whitelist)
+
+      @permission_definitions = []
+    end
+
+    def ensure_exists!
+      RoleImporter.new(self).import!
+    end
+
+    def ensure_permission_exists(action, applies_to:, states: [Permission::WILDCARD])
+      if applies_to.is_a?(Class) && applies_to.try(:name).nil?
+        Rails.logger.warn <<-WARN
+          Skippping #ensure_permission_exists for class without a name. If
+          this is in the text environment it can be ignored.
+        WARN
+      else
+        @permission_definitions << PermissionDefinition.new(
+          action: action.to_s,
+          applies_to: (applies_to.try(:name) || applies_to),
+          states: states.map(&:to_s).sort
+        )
+      end
+    end
+  end
+end

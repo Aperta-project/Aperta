f7c2bc438d9aa71560ca98cd83615eb52baadd4b
Matt Mercieca <mercieca@gmail.com>
2016-11-09 11:06:44 -0500

    APERTA-7777 getting rid of deprecation in Ember routing

--- client/app/adapters/paper.js
+++ client/app/adapters/paper.js
@@ -1,7 +1,7 @@
 import ApplicationAdapter from 'tahi/adapters/application';
 
 export default ApplicationAdapter.extend({
-  queryRecord(store, type, query) {
+  query(store, type, query) {
     return this.ajax(`/api/papers/${query.shortDoi}`, 'GET');
   }
 });

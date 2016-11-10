91aa804b313be9e300102dd49a2572d140698d68
Brandon Zylstra <brandon@mutuallyhuman.com>
2016-11-10 16:35:46 -0500

    remove extra whitespace

--- engines/tahi_standard_tasks/spec/models/early_posting_task_spec.rb
+++ engines/tahi_standard_tasks/spec/models/early_posting_task_spec.rb
@@ -7,6 +7,4 @@ describe TahiStandardTasks::EarlyPostingTask do
     it_behaves_like '<Task class>.restore_defaults update title to the default'
     it_behaves_like '<Task class>.restore_defaults update old_role to the default'
   end
-
 end
-

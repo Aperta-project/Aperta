e36d57c22ae27319822e2ffb379de2cd426fff29
Brandon Zylstra <brandon@mutuallyhuman.com>
2016-11-09 20:32:56 -0500

    default checkbox to true when creating an early posting task

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/early_posting_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/early_posting_task.rb
@@ -3,5 +3,12 @@ module TahiStandardTasks
     include MetadataTask
     DEFAULT_TITLE = 'Early Article Posting'
     DEFAULT_ROLE = 'author'
+
+    def self.task_added_to_workflow(early_posting_task)
+      question = NestedQuestion.find_by!(ident: 'early-posting--consent')
+      answer = early_posting_task.find_or_build_answer_for(nested_question: question)
+      answer.value = true
+      answer.save
+    end
   end
 end

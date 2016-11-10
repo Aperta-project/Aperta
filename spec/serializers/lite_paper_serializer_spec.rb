4127cae2210f349638ce3e0e8bf7bd9162254fea
Chris Westra <cwestra@gmail.com>
2016-11-10 11:39:59 -0500

    APERTA-7457 fix lite paper serializer spec

--- spec/serializers/lite_paper_serializer_spec.rb
+++ spec/serializers/lite_paper_serializer_spec.rb
@@ -21,37 +21,12 @@ describe LitePaperSerializer do
   end
 
   describe 'a paper created by a user' do
+    let(:json) { serializer.as_json[:paper] }
+
+    let(:journal) { FactoryGirl.create(:journal, :with_creator_role) }
+
     let(:paper) do
-      FactoryGirl.create(:paper, :with_integration_journal, creator: user)
-    end
-    it "includes the 'My Paper' role" do
-      expect(deserialized_content[:lite_paper])
-        .to match(hash_including(roles: include('My Paper')))
-    end
-
-    describe 'old_roles' do
-      let!(:one_day_ago) { 1.day.ago }
-      let!(:two_days_ago) { 2.days.ago }
-
-      context 'when the user has role descriptions on this paper' do
-        before do
-          allow(paper).to receive(:role_descriptions_for)
-            .with(user: user)
-            .and_return ['Author', 'Collaborator']
-        end
-
-        it "serializes those descriptions" do
-          expect(json[:old_roles]).to contain_exactly('Author', 'Collaborator')
-        end
-      end
-
-      context 'when there is no user' do
-        subject(:serializer) { described_class.new(paper, user: nil, root: :paper) }
-
-        it 'is serialized as nil' do
-          expect(json).to match hash_including(old_roles: nil)
-        end
-      end
+      FactoryGirl.create(:paper, journal: journal, creator: user)
     end
 
     describe 'related_at_date' do

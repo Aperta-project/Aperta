84958b0f9c21bda7194dea20aeecdb8eaa2f04d3
Chris Westra <cwestra@gmail.com>
2016-11-10 15:14:09 -0500

    APERTA-7457 dont stream assignments for users

--- app/subscribers/assignment/notify_assignee.rb
+++ app/subscribers/assignment/notify_assignee.rb
@@ -7,4 +7,11 @@ class Assignment::NotifyAssignee < EventStreamSubscriber
   def payload
     payload_for_record(record.assigned_to)
   end
+
+  def run
+    # Pushing a user payload would result in Ember trying to get a user from
+    # an api/user route. No such route exists, so we prohibit sending updates
+    # for anything assigned to a user
+    super unless record.assigned_to.is_a? User
+  end
 end

c13e7cc10833cae87b8075d4c88a834af1b22b1a
Ben Beckwith <ben@mutuallyhuman.com>
2016-11-28 10:39:46 -0500

    Include pdfviewer in the precompiled assets
    APERTA-7828

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,4 @@ Rails.application.config.assets.precompile += %w(email.scss auth.scss)
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'pdf.js-viewer-0.3.3')

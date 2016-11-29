f3748e46fc72b1b1c12cdc74c183b77a4a46e119
Tad Park <tadp@users.noreply.github.com>
2016-11-29 11:20:10 -0800

    APERTA-7826 Update notifications to show successful upload of pdf

--- app/controllers/ihat/jobs_controller.rb
+++ app/controllers/ihat/jobs_controller.rb
@@ -6,14 +6,28 @@ module Ihat
     rescue_from ActionController::ParameterMissing, with: :render_invalid_params
 
     def create
-      params_safe =
-        params.require(:job).permit(:id, :state, outputs: [:file_type, :url], options: [:callback_url, :metadata])
-      params_safe[:options][:metadata] = Verifier.new(params_safe[:options][:metadata]).decrypt
-      PaperUpdateWorker.perform_async(params_safe)
+      if file_type == 'pdf'
+        # This is where we can put in a worker specific to pdfs to process any pdf epubs
+        response = IhatJobResponse.new(safe_params)
+        Notifier.notify(event: "paper:data_extracted", data: { record: response })
+      else
+        PaperUpdateWorker.perform_async(safe_params)
+      end
       head :ok
     end
 
     private
+
+    def file_type
+      safe_params[:outputs].first[:file_type]
+    end
+
+    def safe_params
+      params_safe =
+        params.require(:job).permit(:id, :state, outputs: [:file_type, :url], options: [:callback_url, :metadata])
+      params_safe[:options][:metadata] = Verifier.new(params_safe[:options][:metadata]).decrypt
+      params_safe
+    end
 
     def render_invalid_params(e)
       render status: :unprocessable_entity, json: { error: e.message }

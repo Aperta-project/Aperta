8d82468ce53b54f8f8562db2de61f25a441bf23d
Chris Westra <cwestra@gmail.com>
2016-12-01 15:54:15 -0500

    Add comment to blocks method

--- app/models/ad_hoc_task.rb
+++ app/models/ad_hoc_task.rb
@@ -7,6 +7,17 @@ class AdHocTask < Task
   end
 
   # type can be "attachments", "text", "email", "h1"
+  # for adhoc tasks, the body is composed of an array of 'blocks',
+  # each of which are composed of an array of items.
+  # There can be more than one item in a block, but every item will have the same "type",
+  # hence we can see what 'type' the block is by just looking at the first item.
+  # [
+  #   [{ "type" => "text", "value" => "foo" }],
+  #   [{ "type" => "checkbox", "value" => "foo" }
+  #    { "type" => "checkbox", "value" => "bar" }
+  #   ],
+  #   [{ "type" => "attachments", "value" => "Please select a file." }]
+  # ]
   def blocks(type)
     body.select { |b| b[0]["type"] == type }
   end

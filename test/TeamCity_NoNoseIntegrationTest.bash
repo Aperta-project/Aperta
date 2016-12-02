b2863bfc6068bdf691d5b8a6e39257c4c064645e
Mr. Jeffrey Gray <jgray@plos.org>
2016-12-01 19:58:51 -0800

    Adding in missing tests

--- test/TeamCity_NoNoseIntegrationTest.bash
+++ test/TeamCity_NoNoseIntegrationTest.bash
@@ -67,8 +67,10 @@ python -m frontend.test_metadata_versioning
 python -m frontend.test_paper_tracker
 python -m frontend.test_production_metadata_card
 python -m frontend.test_profile
+python -m frontend.test_reactivate_ms
 python -m frontend.test_register_decision
 python -m frontend.test_reviewer_candidates
+python -m frontend.test_reviewer_report
 python -m frontend.test_revise_task
 python -m frontend.test_revision_tech_check
 python -m frontend.test_title_abstract_card

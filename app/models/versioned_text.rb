538eb57b3e504c16fda55f5b384fbf99117a040c
Jerry Nummi <jerry@mutuallyhuman.com>
2016-12-08 11:50:54 -0800

    Add file_type to VersionedText
    This is necessary to handle PDFs on the versions view. A file type field
    is added to versioned texts. Existing data is updated to be ‘docx’ since
    PDF uploads are not yet enabled.
    
    APERTA-7829

--- app/models/versioned_text.rb
+++ app/models/versioned_text.rb
@@ -23,14 +23,18 @@ class VersionedText < ActiveRecord::Base
   def be_major_version!
     update!(
       major_version: (paper.major_version || -1) + 1,
-      minor_version: 0)
+      minor_version: 0,
+      file_type: paper.file_type
+    )
   end
 
   # Give the text a new MINOR version
   def be_minor_version!
     update!(
       major_version: (paper.major_version || 0),
-      minor_version: (paper.minor_version || -1) + 1)
+      minor_version: (paper.minor_version || -1) + 1,
+      file_type: paper.file_type
+    )
   end
 
   def submitted?
@@ -55,7 +59,9 @@ class VersionedText < ActiveRecord::Base
       d.update!(
         major_version: nil,
         minor_version: nil,
-        submitting_user: nil) # makes duplicate of S3 file
+        submitting_user: nil,
+        file_type: paper.file_type
+      ) # makes duplicate of S3 file
     end
   end
 

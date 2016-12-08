a646065703374fc8d18e88919526115080e3d8be
Zach Dennis <zach.dennis@gmail.com>
2016-12-08 16:32:42 -0500

    Updating the billing-task component test to ensure that it can be uncompleted even there are validation errors.
    * moving all of the nested-questions out into a FactoryGuy fixture
    * createQuestionWithAnswer(...) helper function now does a find or create instead of always creating
    * added two new qunit assertions:  mockjaxRequestMade, mockjaxRequestNotMade
    * adding a class name to the billing task component aptly named 'billing-task'
    
    The mockjax qunit assertions are so I could get the test to fail when I took out the skip validation logic.
    
    APERTA-8466

--- client/tests/components/billing-task-test.js
+++ client/tests/components/billing-task-test.js
@@ -3,38 +3,48 @@ import hbs from 'htmlbars-inline-precompile';
 import { manualSetup, make } from 'ember-data-factory-guy';
 import { createQuestion, createQuestionWithAnswer } from 'tahi/tests/factories/nested-question';
 import registerCustomAssertions from '../helpers/custom-assertions';
-import FakeCanService from '../helpers/fake-can-service';
+import Factory from '../helpers/factory';
 import Ember from 'ember';
 import wait from 'ember-test-helpers/wait';
+
 moduleForComponent('billing-task', 'Integration | Component | billing task', {
   integration: true,
   beforeEach() {
     registerCustomAssertions();
     manualSetup(this.container);
+
+    this.registry.register('pusher:main', Ember.Object.extend({socketId: 'foo'}));
+    $.mockjax({url: '/api/countries', status: 200, responseText: {
+      countries: [],
+    }});
+    $.mockjax({url: '/api/institutional_accounts', status: 200, responseText: {
+      institutional_accounts: [],
+    }});
+    Factory.createPermission('billingTask', 1, ['edit', 'view']);
+  },
+  afterEach() {
+    $.mockjax.clear();
   }
 });
 
-let template = hbs`{{billing-task
-                      task=task}}`;
+let template = hbs`{{billing-task task=testTask}}`;
 
-test('validates numericality of a few fields', function(assert) {
-  this.registry.register('pusher:main', Ember.Object.extend({socketId: 'foo'}));
-  this.registry.register('service:can', FakeCanService);
-  $.mockjax({url: '/api/countries', status: 200, responseText: {
-    countries: [],
-  }});
-  $.mockjax({url: '/api/institutional_accounts', status: 200, responseText: {
-    institutional_accounts: [],
-  }});
+let createTask = function(){
+  return make('billing-task');
+};
 
-  let task =  make('billing-task');
-  let fake = this.container.lookup('service:can');
-  fake.allowPermission('edit', task);
+// for readability
+let createInvalidTask = function(){
+  return createTask();
+}
 
-  // all nested questions referenced in the hbs template must
-  // exist, but they don't have to have answers.  I've put them
-  // in for kicks.
+let createValidTask = function(){
+  let task = createTask();
+  fillInBasicBillingInfoForTask(task);
+  return task;
+}
 
+let fillInBasicBillingInfoForTask = function(task){
   createQuestionWithAnswer(task, 'plos_billing--first_name', 'John');
   createQuestionWithAnswer(task, 'plos_billing--last_name', 'Doe');
   createQuestionWithAnswer(task, 'plos_billing--title', 'Prof');
@@ -56,18 +66,19 @@ test('validates numericality of a few fields', function(assert) {
   createQuestionWithAnswer(task, 'plos_billing--pfa_question_2a', '');
   createQuestionWithAnswer(task, 'plos_billing--pfa_question_3', true);
   createQuestionWithAnswer(task, 'plos_billing--pfa_question_4', true);
-  createQuestionWithAnswer(task, 'plos_billing--pfa_amount_to_pay', 'foo');
+  createQuestionWithAnswer(task, 'plos_billing--pfa_amount_to_pay', '99.00');
   createQuestionWithAnswer(task, 'plos_billing--pfa_supporting_docs', 'foo');
   createQuestionWithAnswer(task, 'plos_billing--pfa_additional_comments', 'foo');
   createQuestionWithAnswer(task, 'plos_billing--affirm_true_and_complete', false);
+};
 
-  // these fields have PFA validation, which we'll test
-  createQuestion(task, 'plos_billing--pfa_question_1b');
-  createQuestion(task, 'plos_billing--pfa_question_2b');
-  createQuestion(task, 'plos_billing--pfa_question_3a');
-  createQuestion(task, 'plos_billing--pfa_question_4a');
+test('validates numericality of a few fields', function(assert) {
+  let testTask = createValidTask();
+  this.set('testTask', testTask);
 
-  this.set('task', task);
+  fillInBasicBillingInfoForTask(testTask);
+
+  this.set('task', testTask);
   this.render(template);
 
   // filling in a nested question's text input and firing change()
@@ -75,10 +86,12 @@ test('validates numericality of a few fields', function(assert) {
   $.mockjax({url: /\/api\/nested_questions/, type: 'PUT', status: 204});
   $.mockjax({url: /\/api\/nested_questions/, type: 'POST', status: 204});
 
+  // Make the PFA questions invalid
   this.$('input[name=plos_billing--pfa_question_1b]').val('not a number').change();
   this.$('input[name=plos_billing--pfa_question_2b]').val('not a number').change();
   this.$('input[name=plos_billing--pfa_question_3a]').val('not a number').change();
   this.$('input[name=plos_billing--pfa_question_4a]').val('not a number').change();
+
   let done = assert.async();
   wait().then(() => {
     assert.textPresent('#error-for-plos_billing--pfa_question_1b', 'Must be a number');
@@ -86,5 +99,82 @@ test('validates numericality of a few fields', function(assert) {
     assert.textPresent('#error-for-plos_billing--pfa_question_3a', 'Must be a number');
     assert.textPresent('#error-for-plos_billing--pfa_question_4a', 'Must be a number');
     done();
+  });
+});
+
+test('it reports validation errors on the task when attempting to complete', function(assert) {
+  let testTask = createTask();
+  this.set('testTask', testTask);
+  this.render(template);
+  this.$('.billing-task button.task-completed').click();
+
+  let done = assert.async();
+  wait().then(() => {
+    // Error at the task level
+    assert.textPresent('.billing-task', 'Please fix all errors');
+    done();
+  });
+});
+
+test('it does not allow the user to complete when there are validation errors', function(assert) {
+  let testTask = createTask();
+  this.set('testTask', testTask);
+  this.render(template);
+  this.$('.billing-task button.task-completed').click();
+
+  let done = assert.async();
+  wait().then(() => {
+    assert.equal(testTask.get('completed'), false, 'task remained incomplete');
+    done();
+  });
+});
+
+test('it lets you complete the task when there are no validation errors', function(assert) {
+  let testTask = createValidTask();
+  this.set('testTask', testTask);
+
+  $.mockjax({url: '/api/tasks/1', type: 'PUT', status: 204, responseText: '{}'});
+  this.render(template);
+
+  // try to complete
+  this.$('.billing-task button.task-completed').click();
+
+  let done = assert.async();
+  wait().then(() => {
+    assert.equal(testTask.get('completed'), true, 'task was completed');
+    assert.mockjaxRequestMade('/api/tasks/1', 'PUT');
+    done();
+  });
+});
+
+test('it lets you uncomplete the task when it has validation errors', function(assert) {
+  let testTask = createInvalidTask();
+  this.set('testTask', testTask);
+
+  Ember.run(() => {
+    testTask.set('completed', true);
+  });
+
+  $.mockjax({url: '/api/tasks/1', type: 'PUT', status: 204, responseText: '{}'});
+  this.render(template);
+
+  assert.equal(testTask.get('completed'), true, 'task was initially completed');
+  this.$('.billing-task button.task-completed').click();
+
+  let done = assert.async();
+  wait().then(() => {
+    assert.equal(testTask.get('completed'), false, 'task was marked as incomplete');
+    assert.mockjaxRequestMade('/api/tasks/1', 'PUT');
+    $.mockjax.clear();
+
+    // try complete again
+    this.$('.billing-task button.task-completed').click();
+
+    wait().then(() => {
+      assert.textPresent('.billing-task', 'Please fix all errors');
+      assert.equal(testTask.get('completed'), false, 'task did not change completion status');
+      assert.mockjaxRequestNotMade('/api/tasks/1', 'PUT');
+      done();
+    });
   });
 });

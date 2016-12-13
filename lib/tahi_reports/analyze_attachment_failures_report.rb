23d9c11782b276b1bfcb5f619812c8546d2d0736
Zach Dennis <zach.dennis@gmail.com>
2016-12-13 10:16:35 -0500

    Extracting bin/reports/analyze-attachment-failures.rb into lib/tahi_reports/analyze_attachment_failures_report.rb
    This is so it can be scheduled and called as a rake task:
    
       rake reports:analyze_attachments:run
       rake reports:analyze_attachments:send_email[zach@example.com]
    
    APERTA-8159

---
+++ lib/tahi_reports/analyze_attachment_failures_report.rb
@@ -0,0 +1,171 @@
+module TahiReports
+  class AnalyzeAttachmentFailuresReport
+    TIMEFRAMES = [0.days, 1.day, 1.week, 2.weeks, 1.month, 1.year]
+
+    attr_reader :output, :attachment_klass
+
+    def self.run(output:, attachment_klass: Attachment)
+      new(output: output, attachment_klass: attachment_klass).run
+    end
+
+    def initialize(output:, attachment_klass:)
+      @output = output
+      @attachment_klass = Attachment
+    end
+
+    def run
+      print_summary
+      output.puts
+      output.puts
+
+      print_attachments_stuck_in_processing
+      output.puts
+      output.puts
+
+      print_attachments_stuck_in_errored
+      output.puts
+      output.puts
+
+      print_number_of_attachments_per_error_breakdown
+      output.puts
+      output.puts
+      output
+    end
+
+    def attachments_processing
+      @attachments_processing ||= attachment_klass.where(status: 'processing')
+    end
+
+    def attachments_done
+      @attachments_done ||= attachment_klass.where(status: 'done')
+    end
+
+    def attachments_errored
+      @attachments_errored ||= attachment_klass.where(status: 'error')
+    end
+
+    def attachments_unknown
+      @attachments_unknown ||= attachment_klass.where.not(status: %w(processing error done))
+    end
+
+    private
+
+    def print_summary
+      total_count = attachment_klass.count
+      done_count = attachments_done.count
+      processing_count = attachments_processing.count
+      errored_count = attachments_errored.count
+      unknown_count = attachments_unknown.count
+
+      # E.g. AdhocAttachment -> "Adhoc Attachment"
+      attachment_types = [Attachment].concat(Attachment.subclasses).map do |kl|
+        kl.name.underscore.humanize.titleize.pluralize
+      end
+
+      output.puts <<-MESSAGE.strip_heredoc
+        Hello,
+
+        Below is the results of running the Attachment analysis report run on #{Date.today}.
+
+        The information below contains information for all attachment types including: #{attachment_types.join(', ')}.
+      MESSAGE
+      output.puts
+      output.puts
+
+      output.puts "Total count of #{attachment_klass.name}(s): #{total_count}"
+      output.puts "-------------------------------------------"
+      output.puts "# of done: #{done_count}"
+      output.puts "# of processing: #{processing_count}"
+      output.puts "# of errored: #{errored_count}"
+      output.puts "# of unknown: #{unknown_count}"
+    end
+
+    def print_attachments_stuck_in_processing
+      attachments_stuck_in_processing = {}
+      TIMEFRAMES.each_with_index do |timeframe, i|
+        if i == 0
+          attachments_stuck_in_processing[timeframe] = attachments_processing.where(
+            created_at: (timeframe.ago.beginning_of_day.utc..Time.now.end_of_day.utc)
+          ).count
+        else
+          attachments_stuck_in_processing[timeframe] = attachments_processing.where(
+            created_at: (timeframe.ago.beginning_of_day.utc..TIMEFRAMES[i-1].ago.end_of_day.utc)
+          ).count
+        end
+      end
+
+      output.puts "#{attachment_klass.name}(s) stuck in processing"
+      output.puts "-------------------------------------------"
+      attachments_stuck_in_processing.each_pair do |timeframe, count|
+        output.puts "Count in processing in the past #{timeframe.inspect}: #{count}"
+      end
+    end
+
+    def print_attachments_stuck_in_errored
+      attachments_stuck_in_errored = {}
+      TIMEFRAMES.each_with_index do |timeframe, i|
+        if i == 0
+          attachments_stuck_in_errored[timeframe] = attachments_errored.where(
+            created_at: (timeframe.ago.beginning_of_day.utc..Time.now.end_of_day.utc)
+          ).count
+        else
+          attachments_stuck_in_errored[timeframe] = attachments_errored.where(
+            created_at: (timeframe.ago.beginning_of_day.utc..TIMEFRAMES[i-1].ago.end_of_day.utc)
+          ).count
+        end
+      end
+      output.puts "#{attachment_klass.name}(s) that errored out"
+      output.puts "-------------------------------------------"
+      if attachments_stuck_in_errored.empty?
+        output.puts "  [none]"
+      else
+        attachments_stuck_in_errored.each_pair do |timeframe, count|
+          output.puts "Count in error state in the past #{timeframe.inspect}: #{count}"
+        end
+      end
+    end
+
+    def print_number_of_attachments_per_error_breakdown
+      output.puts "Number of #{attachment_klass.name}(s) per error"
+      output.puts "-------------------------------------------"
+      attachments_stuck_in_errored = {}
+      TIMEFRAMES.each_with_index do |timeframe, i|
+        output.puts unless i == 0
+        output.puts "Errors in the past #{timeframe.inspect}"
+        if i == 0
+          attachments_by_error = attachments_errored.where(
+            created_at: (timeframe.ago.beginning_of_day.utc..Time.now.end_of_day.utc)
+          ).each do |a|
+            a.error_message = a.error_message.gsub("\n", "  ").gsub(/(identify)[^']+'/, '\1 <file-path-extracted>')
+          end
+
+          if attachments_by_error.empty?
+            output.puts "  [none]"
+          else
+            attachments_by_error.group_by(&:error_message).each do |error_message, attachments|
+              output.puts "  #{attachments.length} failed with error: #{error_message}"
+              output.puts "  ids=#{attachments.map(&:id).inspect}"
+              output.puts
+            end
+          end
+        else
+          attachments_by_error = attachments_errored.where(
+            created_at: (timeframe.ago.beginning_of_day.utc..TIMEFRAMES[i-1].ago.end_of_day.utc)
+          ).each do |a|
+            a.error_message = a.error_message.gsub("\n", "  ").gsub(/(identify)[^']+'/, '\1 <file-path-extracted>')
+          end
+
+          if attachments_by_error.empty?
+            output.puts "  [none]"
+          else
+            attachments_by_error.group_by(&:error_message).each do |error_message, attachments|
+              output.puts "  #{attachments.length} failed with error: #{error_message}"
+              output.puts "  ids=#{attachments.map(&:id).inspect}"
+              output.puts
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/tahi_reports/analyze_attachment_failures_report_spec.rb
@@ -0,0 +1,119 @@
+require 'rails_helper'
+require 'stringio'
+require 'tahi_reports/analyze_attachment_failures_report'
+
+describe TahiReports::AnalyzeAttachmentFailuresReport do
+  describe 'running the report' do
+    subject(:run_report) do
+      described_class.run(output: output, attachment_klass: Attachment)
+    end
+
+    let(:output) { StringIO.new }
+    def report_contents
+      output.tap(&:rewind).read
+    end
+
+    let!(:processing_attachment_from_today) { FactoryGirl.create(:attachment, :processing, created_at: Date.today) }
+    let!(:processing_attachment_from_a_week_ago) { FactoryGirl.create(:attachment, :processing, created_at: 1.week.ago) }
+    let!(:processing_attachment_from_a_month_ago) { FactoryGirl.create(:attachment, :processing, created_at: 1.month.ago) }
+    let!(:processing_attachment_from_a_year_ago) { FactoryGirl.create(:attachment, :processing, created_at: 1.year.ago) }
+
+    let!(:errored_attachment_from_today) { FactoryGirl.create(:attachment, :errored, created_at: Date.today) }
+    let!(:errored_attachment_from_a_week_ago) { FactoryGirl.create(:attachment, :errored, created_at: 1.week.ago, error_message: "Failed because of A") }
+    let!(:errored_attachment_from_a_month_ago) { FactoryGirl.create(:attachment, :errored, created_at: 1.month.ago) }
+    let!(:errored_attachment_from_a_year_ago) { FactoryGirl.create(:attachment, :errored, created_at: 1.year.ago, error_message: "Failed because of B") }
+
+    let!(:completed_attachment_from_today) { FactoryGirl.create(:attachment, :completed, created_at: Date.today) }
+    let!(:completed_attachment_from_a_week_ago) { FactoryGirl.create(:attachment, :completed, created_at: 1.week.ago) }
+    let!(:completed_attachment_from_a_month_ago) { FactoryGirl.create(:attachment, :completed, created_at: 1.month.ago) }
+    let!(:completed_attachment_from_a_year_ago) { FactoryGirl.create(:attachment, :completed, created_at: 1.year.ago) }
+
+    let!(:unknown_state_attachment_from_today) { FactoryGirl.create(:attachment, :unknown_state, created_at: Date.today) }
+    let!(:unknown_state_attachment_from_a_week_ago) { FactoryGirl.create(:attachment, :unknown_state, created_at: 1.week.ago) }
+    let!(:unknown_state_attachment_from_a_month_ago) { FactoryGirl.create(:attachment, :unknown_state, created_at: 1.month.ago) }
+    let!(:unknown_state_attachment_from_a_year_ago) { FactoryGirl.create(:attachment, :unknown_state, created_at: 1.year.ago) }
+
+    it 'outputs to the given IO object' do
+      run_report
+      expect(report_contents.present?).to be true
+    end
+
+    it 'prints a summary' do
+      run_report
+      expect(report_contents).to include "Below is the results of running the Attachment analysis report run on #{Date.today}."
+      expect(report_contents).to include <<-STRING.strip_heredoc
+        Total count of Attachment(s): 16
+        -------------------------------------------
+        # of done: 4
+        # of processing: 4
+        # of errored: 4
+        # of unknown: 4
+      STRING
+    end
+
+    it 'prints a how many attachments are stuck in a processing state' do
+      run_report
+      expect(report_contents).to include <<-STRING.strip_heredoc
+        Attachment(s) stuck in processing
+        -------------------------------------------
+        Count in processing in the past 0 days: 1
+        Count in processing in the past 1 day: 1
+        Count in processing in the past 7 days: 1
+        Count in processing in the past 14 days: 1
+        Count in processing in the past 1 month: 1
+        Count in processing in the past 1 year: 2
+      STRING
+    end
+
+    it 'prints how many attachments are stuck in an errored state' do
+      run_report
+      expect(report_contents).to include <<-STRING.strip_heredoc
+        Attachment(s) that errored out
+        -------------------------------------------
+        Count in error state in the past 0 days: 1
+        Count in error state in the past 1 day: 1
+        Count in error state in the past 7 days: 1
+        Count in error state in the past 14 days: 1
+        Count in error state in the past 1 month: 1
+        Count in error state in the past 1 year: 2
+      STRING
+    end
+
+    it 'prints a number of attachments per error breakdown including the database ids for each attachment' do
+      run_report
+      expect(report_contents).to include <<-STRING.strip_heredoc
+        Errors in the past 0 days
+          1 failed with error: Failed for some reason
+          ids=[#{errored_attachment_from_today.id}]
+
+
+        Errors in the past 1 day
+          1 failed with error: Failed for some reason
+          ids=[#{errored_attachment_from_today.id}]
+
+
+        Errors in the past 7 days
+          1 failed with error: Failed because of A
+          ids=[#{errored_attachment_from_a_week_ago.id}]
+
+
+        Errors in the past 14 days
+          1 failed with error: Failed because of A
+          ids=[#{errored_attachment_from_a_week_ago.id}]
+
+
+        Errors in the past 1 month
+          1 failed with error: Failed for some reason
+          ids=[#{errored_attachment_from_a_month_ago.id}]
+
+
+        Errors in the past 1 year
+          1 failed with error: Failed for some reason
+          ids=[#{errored_attachment_from_a_month_ago.id}]
+
+          1 failed with error: Failed because of B
+          ids=[#{errored_attachment_from_a_year_ago.id}]
+      STRING
+    end
+  end
+end

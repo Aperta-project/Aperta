b10c93f4d71f7c29be6c73e962d995d903275447
Chris Westra <cwestra@gmail.com>
2016-12-13 13:42:18 -0500

    APERTA-7810 add flag to papers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206175332) do
+ActiveRecord::Schema.define(version: 20161213162902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -467,6 +467,7 @@ ActiveRecord::Schema.define(version: 20161206175332) do
     t.boolean  "processing",                            default: false
     t.boolean  "uses_research_article_reviewer_report", default: false
     t.string   "short_doi"
+    t.boolean  "number_reviewer_reports",               default: false, null: false
   end
 
   add_index "papers", ["doi"], name: "index_papers_on_doi", unique: true, using: :btree

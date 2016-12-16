36770a646809103b7c299fcc6661cbf1e09011ff
Jack LaBarba <jlabarba@plos.org>
2016-12-16 13:37:00 -0800

    APERTA-8673 fix check for msie

--- client/app/services/browser-detector.js
+++ client/app/services/browser-detector.js
@@ -3,12 +3,12 @@ import bowser from 'ember-bowser';
 
 export default Ember.Service.extend({
   init() {
-    if (!this.get('name')) { this.set('name', bowser.name); }
+    if (!this.get('msie')) { this.set('msie', bowser.msie); }
     if (!this.get('version')) { this.set('version', bowser.version); }
   },
 
   isIE11OrLess: Ember.computed('name', 'version', function() {
     const lessThan12 = (bowser.compareVersions([this.get('version'), '12']) === -1);
-    return (lessThan12 && this.get('name') === 'msie');
+    return (lessThan12 && this.get('msie'));
   })
 });
--- client/tests/unit/services/browser-detector-test.js
+++ client/tests/unit/services/browser-detector-test.js
@@ -7,21 +7,21 @@ test('isIE11OrLess', function(assert) {
   assert.expect(7);
   let service = this.subject();
 
-  function testIsIE11OrLess({ name, version, expect}) {
-    service.set('name', name);
+  function testIsIE11OrLess({ msie, version, expect}) {
+    service.set('msie', msie);
     service.set('version', version);
     if (expect) {
-      assert.ok(service.get('isIE11OrLess'), `expect ${name} ${version} to be ${expect}`);
+      assert.ok(service.get('isIE11OrLess'), `expect msie:${msie} ${version} to be ${expect}`);
     } else {
-      assert.notOk(service.get('isIE11OrLess'), `expect ${name} ${version} to be ${expect}`);
+      assert.notOk(service.get('isIE11OrLess'), `expect msie:${msie} ${version} to be ${expect}`);
     }
   }
 
-  testIsIE11OrLess({name: 'msie', version: '11.0', expect: true});
-  testIsIE11OrLess({name: 'msie', version: '11.101.12', expect: true});
-  testIsIE11OrLess({name: 'msie', version: '10.101.12', expect: true});
-  testIsIE11OrLess({name: 'msie', version: '10', expect: true});
-  testIsIE11OrLess({name: 'msie', version: '12.0', expect: false});
-  testIsIE11OrLess({name: 'Chrome', version: '11.0', expect: false});
-  testIsIE11OrLess({name: 'Firefox', version: '11.0', expect: false});
+  testIsIE11OrLess({msie: true, version: '11.0', expect: true});
+  testIsIE11OrLess({msie: true, version: '11.101.12', expect: true});
+  testIsIE11OrLess({msie: true, version: '10.101.12', expect: true});
+  testIsIE11OrLess({msie: true, version: '10', expect: true});
+  testIsIE11OrLess({msie: true, version: '12.0', expect: false});
+  testIsIE11OrLess({msie: false, version: '11.0', expect: false});
+  testIsIE11OrLess({msie: false, version: '11.0', expect: false});
 });

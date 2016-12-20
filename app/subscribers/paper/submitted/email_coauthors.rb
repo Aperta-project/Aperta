3e9b1e2a45cc1b5ab88e7f5dc14a0bee1f6d9faa
sahar aghajani <saghajani@plos.org>
2016-12-20 11:53:29 -0800

    APERTA-8013 feedback from code review

--- app/subscribers/paper/submitted/email_coauthors.rb
+++ app/subscribers/paper/submitted/email_coauthors.rb
@@ -4,7 +4,7 @@ class Paper::Submitted::EmailCoauthors
     previous_state = paper.previous_changes[:publishing_state][0]
 
     if previous_state == 'unsubmitted' || previous_state == 'invited_for_full_submission'
-      coauthors = paper.all_authors.reject { |author| (author.try(:email) || author.try(:contact_email)) == paper.creator.email }
+      coauthors = paper.all_authors.reject { |author| author.try(:email) == paper.creator.email }
       coauthors.each do |coauthor|
         UserMailer.delay.notify_coauthor_of_paper_submission(paper.id, coauthor.id, coauthor.class.name)
       end

9b1aa7a3abd9a55ea1da428e1a1850204218ac97
Jerry Nummi <jerry@mutuallyhuman.com>
2016-12-28 12:48:28 -0500

    Allow editing of alternate invitation when primary is invited
    APERTA-8735

--- engines/tahi_standard_tasks/client/app/components/invitation-detail-row.js
+++ engines/tahi_standard_tasks/client/app/components/invitation-detail-row.js
@@ -6,7 +6,7 @@ import { task as concurrencyTask } from 'ember-concurrency';
 const {
   Component,
   computed,
-  computed: { equal, reads, and, or }
+  computed: { and, equal, not, or, reads }
 } = Ember;
 
 /*
@@ -30,6 +30,10 @@ export default Component.extend(DragNDrop.DraggableMixin, {
   currentRound: computed.not('previousRound'),
   invitationsInFlight: false,
 
+  isActiveInvitation: computed('activeInvitation', 'invitation', function() {
+    return this.get('activeInvitation') === this.get('invitation');
+  }),
+
   draggable: computed('previousRound', 'invitation.canReposition', 'invitationsInFlight', function(){
     if (this.get('previousRound') || this.get('invitationsInFlight')) { return false; }
     return this.get('invitation.canReposition');
@@ -39,13 +43,15 @@ export default Component.extend(DragNDrop.DraggableMixin, {
     return 'invitation-item--' + this.get('uiState');
   }),
 
-  disabled: computed('activeInvitationState', 'invitation', 'activeInvitation', 'invitationGroupCantSendInvite', function(){
-    if ((this.get('activeInvitationState') === 'edit') && (this.get('activeInvitation') !== this.get('invitation'))) {
+  disabled: computed('activeInvitationState', 'isActiveInvitation', 'invitationGroupCantSendInvite', function(){
+    if ((this.get('activeInvitationState') === 'edit') && !this.get('isActiveInvitation')) {
       return true;
     } else {
       return this.get('invitationGroupCantSendInvite');
     }
   }),
+
+  editDisabled: not('isActiveInvitation'),
 
   primary: computed('invitation.primary', function(){
     return this.get('invitation.primary') || this.get('invitation');
@@ -141,7 +147,7 @@ export default Component.extend(DragNDrop.DraggableMixin, {
     },
 
     editInvitation(invitation) {
-      if (this.get('disabled')) { return; }
+      if (this.get('editDisabled')) { return; }
 
       this.setProperties({
         invitationBodyStateBeforeEdit: invitation.get('body')

9c619bcca329c080cba9621d0fab70d3bed575b4
Tad Park <tadp@users.noreply.github.com>
2016-12-29 11:53:49 -0800

    APERTA-8586 Return false when paper doesn't exist

--- app/models/authorizations/user_helper.rb
+++ app/models/authorizations/user_helper.rb
@@ -39,6 +39,7 @@ module Authorizations
     end
 
     def unaccepted_and_invited_to?(paper:)
+      return false unless paper.present?
       paper.draft_decision.invitations.where(state: 'invited', invitee_id: 'id').exists?
     end
 

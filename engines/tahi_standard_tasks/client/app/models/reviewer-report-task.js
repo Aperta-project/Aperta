933ee45d2e6db730cdde541b3c55542174c7ca54
Tad Park <tadp@users.noreply.github.com>
2017-01-04 14:43:55 -0800

    APERTA-8707 Remove extraneous 'paperReview' vestige

--- engines/tahi_standard_tasks/client/app/models/reviewer-report-task.js
+++ engines/tahi_standard_tasks/client/app/models/reviewer-report-task.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 import Task from 'tahi/models/task';
 
 export default Task.extend({
-  paperReview: DS.belongsTo('paperReview'),
   decisions: DS.hasMany('decision'),
   isSubmitted: DS.attr('boolean'),
 

c4b59908a25f9c2dfbed1ade76f1cd4c49c9c735
Jack LaBarba <brakit@gmail.com>
2017-01-11 13:30:51 -0800

    APERTA-8641 extract editable assertions into function

--- client/tests/integration/components/tasks/front-matter-reviewer-report-task-test.js
+++ client/tests/integration/components/tasks/front-matter-reviewer-report-task-test.js
@@ -15,34 +15,7 @@ moduleForComponent('task/front-matter-reviewer-report-task', 'Integration | Comp
   }
 });
 
-test('Readonly mode: Not able to provide reviewer feedback', function(assert) {
-  Ember.run(() => {
-    this.task.set('decisions', [make('decision', { draft: true })]);
-  });
-  this.render(hbs`{{front-matter-reviewer-report-task task=task}}`);
-
-  assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=accept]', 'User cannot provide an accept for publication recommendation');
-  assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=reject]', 'User cannot provide a reject recommendation');
-  assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=major_revision]', 'User cannot provide a major revision recommendation');
-  assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=minor_revision]', 'User cannot provide a minor revision recommendation');
-  assert.elementNotFound('textarea[name=front_matter_reviewer_report--competing_interests]', 'User cannot provide their competing interests statement');
-  assert.elementNotFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=true]', 'User cannot provide yes response to biology suitability');
-  assert.elementNotFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=false]', 'User cannot provide no response to biology suitability');
-  assert.elementNotFound('textarea[name=front_matter_reviewer_report--suitable--comment]', 'User cannot provide their review of biology suitability');
-  assert.elementNotFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=true]', 'User cannot provide respond yes to statistical analysis');
-  assert.elementNotFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=false]', 'User cannot provide response no to statistical analysis');
-  assert.elementNotFound('textarea[name=front_matter_reviewer_report--includes_unpublished_data--explanation]', 'User cannot provide their review of statistical analysis');
-  assert.elementNotFound('textarea[name=front_matter_reviewer_report--additional_comments]', 'User cannot provide additional comments');
-  assert.elementNotFound('textarea[name=front_matter_reviewer_report--identity]', 'User cannot provide their identity');
-});
-
-test('Edit mode: Providing reviewer feedback', function(assert) {
-  this.can.allowPermission('edit', this.task);
-  Ember.run(() => {
-    this.task.set('decisions', [make('decision', { draft: true })]);
-  });
-  this.render(hbs`{{front-matter-reviewer-report-task task=task}}`);
-
+function assertEditable(assert) {
   assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=accept]', 'User can provide an accept for publication recommendation');
   assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=reject]', 'User can provide a reject recommendation');
   assert.elementFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=major_revision]', 'User can provide a major revision recommendation');
@@ -57,6 +30,42 @@ test('Edit mode: Providing reviewer feedback', function(assert) {
   assert.elementFound('textarea[name=front_matter_reviewer_report--includes_unpublished_data--explanation]', 'User can provide their review of statistical analysis');
   assert.elementFound('textarea[name=front_matter_reviewer_report--additional_comments]', 'User can provide additional comments');
   assert.elementFound('textarea[name=front_matter_reviewer_report--identity]', 'User can provide their identity');
+}
+
+function assertNotEditable(assert) {
+  assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=accept]', 'User cannot provide an accept for publication recommendation');
+  assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=reject]', 'User cannot provide a reject recommendation');
+  assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=major_revision]', 'User cannot provide a major revision recommendation');
+  assert.elementNotFound('input[name*=front_matter_reviewer_report--decision_term][type=radio][value=minor_revision]', 'User cannot provide a minor revision recommendation');
+  assert.elementNotFound('textarea[name=front_matter_reviewer_report--competing_interests]', 'User cannot provide their competing interests statement');
+  assert.elementNotFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=true]', 'User cannot provide yes response to biology suitability');
+  assert.elementNotFound('input[name*=front_matter_reviewer_report--suitable][type=radio][value=false]', 'User cannot provide no response to biology suitability');
+  assert.elementNotFound('textarea[name=front_matter_reviewer_report--suitable--comment]', 'User cannot provide their review of biology suitability');
+  assert.elementNotFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=true]', 'User cannot provide respond yes to statistical analysis');
+  assert.elementNotFound('input[name*=front_matter_reviewer_report--includes_unpublished_data][type=radio][value=false]', 'User cannot provide response no to statistical analysis');
+  assert.elementNotFound('textarea[name=front_matter_reviewer_report--includes_unpublished_data--explanation]', 'User cannot provide their review of statistical analysis');
+  assert.elementNotFound('textarea[name=front_matter_reviewer_report--additional_comments]', 'User cannot provide additional comments');
+  assert.elementNotFound('textarea[name=front_matter_reviewer_report--identity]', 'User cannot provide their identity');
+}
+
+
+test('Readonly mode: Not able to provide reviewer feedback', function(assert) {
+  Ember.run(() => {
+    this.task.set('decisions', [make('decision', { draft: true })]);
+  });
+  this.render(hbs`{{front-matter-reviewer-report-task task=task}}`);
+
+  assertNotEditable(assert);
+});
+
+test('Edit mode: Providing reviewer feedback', function(assert) {
+  this.can.allowPermission('edit', this.task);
+  Ember.run(() => {
+    this.task.set('decisions', [make('decision', { draft: true })]);
+  });
+  this.render(hbs`{{front-matter-reviewer-report-task task=task}}`);
+
+  assertEditable(assert);
 });
 
 test('When the decision is a draft', function(assert) {
@@ -65,7 +74,8 @@ test('When the decision is a draft', function(assert) {
     this.task.set('decisions', [make('decision', { draft: true })]);
   });
   this.render(hbs`{{front-matter-reviewer-report-task task=task}}`);
-  assert.nElementsFound('textarea', 5, 'The report should be editable');
+
+  assertEditable(assert);
 });
 
 test('When the decision is not a draft', function(assert) {
@@ -74,7 +84,8 @@ test('When the decision is not a draft', function(assert) {
     this.task.set('decisions', [make('decision', { draft: false })]);
   });
   this.render(hbs`{{front-matter-reviewer-report-task task=task}}`);
-  assert.nElementsFound('textarea', 0, 'The report should not be editable');
+
+  assertNotEditable(assert);
 });
 
 test('History when there are completed decisions', function(assert) {

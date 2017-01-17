388d34b30ae469d477d760ddfd1ab01984d60e11
Tad Park <tadp@users.noreply.github.com>
2017-01-16 18:07:19 -0800

    APERTA-8707 Make Reviewer Reports has many on decision

--- engines/tahi_standard_tasks/client/app/components/reviewer-report-task.js
+++ engines/tahi_standard_tasks/client/app/components/reviewer-report-task.js
@@ -2,7 +2,7 @@ import TaskComponent from 'tahi/pods/components/task-base/component';
 import Ember from 'ember';
 
 export default TaskComponent.extend({
-  currentReviewerReport: Ember.computed.alias('draftDecision.reviewerReport'),
+  currentReviewerReport: Ember.computed.alias('draftDecision.reviewerReports.firstObject'),
   draftDecision: Ember.computed.alias('task.paper.draftDecision'),
   previousDecisions: Ember.computed.alias('task.paper.previousDecisions'),
 

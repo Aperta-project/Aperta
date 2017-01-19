f7ba1cec02d8c2ca73792aeaf527fc9fb5428e52
Curtis Autery <curtis@mutuallyhuman.com>
2017-01-19 10:53:37 -0500

    Remove tests referring to epubs with no source
    There is no longer a use case (so I’ve been assured) of creating an epub
    file without a source doc/docx attachment. This removes (failing) tests
    that made that assumption, and the last of the tests that interrogate
    the extracted HTML, which is no longer included in the epub file.
    
    APERTA-7962

--- spec/services/epub_converter_spec.rb
+++ spec/services/epub_converter_spec.rb
@@ -18,8 +18,6 @@ describe EpubConverter do
       user)
   end
 
-  let(:doc) { Nokogiri::HTML(converter.epub_html) }
-
   def read_epub_stream(stream)
     entries = []
     Zip::InputStream.open(stream) do |io|
@@ -33,44 +31,6 @@ describe EpubConverter do
   # These tests previously did some epub HTML DOM interrogation that in reality
   # should only take place in IHAT. TAHI should no longer be including HTML
   # content in epubs when sending manuscript content to IHAT
-  describe '#epub_html' do
-    context 'a paper' do
-      after { expect(doc.errors.length).to be 0 }
-
-      context 'when paper has figures' do
-        let(:figure) { paper.figures.first }
-        let(:figure_img) { doc.css('img').first }
-
-        before do
-          paper.figures.create!(
-            resource_tokens: [ResourceToken.new],
-            file: File.open('spec/fixtures/yeti.tiff'),
-            status: Figure::STATUS_DONE
-          )
-
-          paper.update_attributes(body: "<p>Figure 1.</p>")
-        end
-
-        it 'replaces img src urls (which are normally proxied) with resolveable urls' do
-          expected_uri = URI.parse(figure.proxyable_url)
-          actual_uri = URI.parse(figure.proxyable_url)
-
-          expect(actual_uri.scheme).to eq expected_uri.scheme
-          expect(actual_uri.host).to eq expected_uri.host
-          expect(actual_uri.path).to eq expected_uri.path
-          expect(CGI.parse(actual_uri.query).keys).to \
-            contain_exactly(
-              'X-Amz-Expires',
-              'X-Amz-Date',
-              'X-Amz-Algorithm',
-              'X-Amz-Credential',
-              'X-Amz-SignedHeaders',
-              'X-Amz-Signature'
-            )
-        end
-      end
-    end
-  end
 
   describe '#title' do
     context 'short_title is nil because it has not been set yet' do
@@ -96,17 +56,6 @@ describe EpubConverter do
   end
 
   describe '#epub_stream' do
-    it 'returns a stream of data' do
-      expect(converter.epub_stream.string.length).to be > 0
-    end
-
-    context 'paper with no uploaded source' do
-      it 'has no source in the epub' do
-        entries = read_epub_stream(converter.epub_stream)
-        expect(entries.map(&:name)).to_not include(/source/)
-      end
-    end
-
     context 'paper with uploaded source' do
       let(:file) do
         File.open(Rails.root.join('spec', 'fixtures', 'about_cats.doc'), 'r')
@@ -117,6 +66,10 @@ describe EpubConverter do
         allow(converter).to receive(:manuscript_source_contents).and_return(file.read)
         allow(converter).to receive(:_manuscript_source_path)
           .and_return(Pathname.new(file.path))
+      end
+
+      it 'returns a stream of data' do
+        expect(converter.epub_stream.string.length).to be > 0
       end
 
       it "includes the source file, calling it 'source' with same

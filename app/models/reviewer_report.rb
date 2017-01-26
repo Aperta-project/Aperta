9ddff9829019a6d741588a92135963227926bcdd
Ben Beckwith <ben@mutuallyhuman.com>
2017-01-26 16:43:34 -0500

    Create status on reviewer reports
    Provide status functionality for reviewer reports.
    
    Also, change them from being created every time an invitation is
    accepted to only the first time. There will be changes to follow that
    create new reviewer reports upon paper submission instead. This will let
    the state track all relevant reviewer statuses.
    
    APERTA-7993

--- app/models/reviewer_report.rb
+++ app/models/reviewer_report.rb
@@ -6,6 +6,29 @@ class ReviewerReport < ActiveRecord::Base
   belongs_to :user
   belongs_to :decision
 
-  validates :task, uniqueness: { scope: [:task_id, :user_id, :decision_id],
-    message: 'Only one report allowed per reviewer per decision' }
+  validates :task,
+    uniqueness: { scope: [:task_id, :user_id, :decision_id],
+                  message: 'Only one report allowed per reviewer per decision' }
+
+  def invitation
+    decision.invitations.find_by(invitee_id: user.id)
+  end
+
+  # status will look at the reviewer, invitations and the submitted state of
+  # this task to get an overall status for the review
+  def status
+    if invitation
+      if invitation.state == "accepted"
+        if task.completed
+          "completed"
+        else
+          "pending"
+        end
+      else
+        invitation.state
+      end
+    else
+      "not_invited"
+    end
+  end
 end

c69beb5ad4fc01a5bc38caea4178df8a82171297
Santiago Murcia <scadavid@plos.org>
2017-02-03 17:07:43 -0500

    APERTA-9101: The Card Review by [Name], is not displaying the Journal selected in the sixth step of the Card

--- test/frontend/Tasks/reviewer_report_task.py
+++ test/frontend/Tasks/reviewer_report_task.py
@@ -242,10 +242,11 @@ class ReviewerReportTask(BaseTask):
       assert qh5.text == u'Additional comments may include concerns about dual publication, '\
           u'research or publication ethics.\n\nThese comments will not be transmitted to the '\
           u'authors.', qh5.text
-      '''assert qh6.text == u'If so, please specify which journal and whether you will be willing' \
-          u' to continue there as reviewer. PLOS Wombat is committed to facilitate the transfer' \
-          u' between journals of suitable manuscripts to reduce redundant review cycles, and we' \
-          u' appreciate your support.', qh6.text''' #Disabled due to APERTA-9101
+      #Disabled due to APERTA-9101
+      #assert qh6.text == u'If so, please specify which journal and whether you will be willing' \
+          #u' to continue there as reviewer. PLOS Wombat is committed to facilitate the transfer' \
+          #u' between journals of suitable manuscripts to reduce redundant review cycles, and we' \
+          #u' appreciate your support.', qh6.text
     else:
       assert u'Please refer to our referee guidelines and information on our article ' \
                          u'types.' in review_note.text, review_note.text

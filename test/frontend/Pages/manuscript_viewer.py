5a072678bdd3b6b73e0b360b81491068903d1310
Santiago Murcia <scadavid@plos.org>
2017-02-08 16:31:57 -0500

    APERTA-8899: Add click_completion_button() and click_covered_element() in New Taxon section

--- test/frontend/Pages/manuscript_viewer.py
+++ test/frontend/Pages/manuscript_viewer.py
@@ -705,6 +705,8 @@ class ManuscriptViewerPage(AuthenticatedPage):
           logging.info('Completing New Taxon Task')
           new_taxon_task = NewTaxonTask(self._driver)
           new_taxon_task.generate_random_taxon()
+          base_task.click_completion_button()
+          self.click_covered_element(task)
     else:
       raise ValueError('No information on this task: {0}'.format(task_name))
     base_task.restore_timeout()

f9242d9efc024a8931e63275e07c1de26de9c740
Sam Bleckley <sam@mutuallyhuman.com>
2017-02-09 22:15:56 -0500

    APERTA-8989 Remove unused serializer.

--- app/serializers/feature_flag_serializer.rb
+++
@@ -1,5 +0,0 @@
-# FeatureFlags represent unfinished features which are disabled in
-# production. See FeatureFlag model for more information.
-class FeatureFlagSerializer < ActiveModel::Serializer
-  attributes :name, :active
-end

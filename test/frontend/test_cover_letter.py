e72290c2abb6f7f8ca317ed32840430c05a4c18a
Ian Vieira <ivieira@plos.org>
2017-02-10 16:07:38 -0200

    APERTA-8894 - Changes addressed on CR

--- test/frontend/test_cover_letter.py
+++ test/frontend/test_cover_letter.py
@@ -49,10 +49,11 @@ class CoverLetterTaskTest(CommonTest):
     cover_letter_task = CoverLetterTask(self.getDriver())
     cover_letter_task.task_ready()
     cover_letter_task.validate_styles()
+    # Mark task as complete
     cover_letter_task.click_completion_button()
     cover_letter_task.logout()
 
-    # Test card styles
+    # Test card styles and activity feed
     staff_user = random.choice(editorial_users)
     dashboard_page = self.cas_login(email=staff_user['email'])
     dashboard_page.page_ready()
@@ -64,149 +65,161 @@ class CoverLetterTaskTest(CommonTest):
     paper_viewer.click_workflow_link()
     workflow_page = WorkflowPage(self.getDriver())
     workflow_page.page_ready()
-    workflow_page.click_card('cover_letter')
-    cover_letter_card = CoverLetterCard(self.getDriver())
-    cover_letter_card.card_ready()
-    cover_letter_card.validate_common_elements_styles(short_doi)
-    cover_letter_card.validate_styles()
-    cover_letter_card.click_close_button()
-    # Wait for modal transition effect
-    time.sleep(.5)
 
+    # Test task completion on activity feed
     workflow_page.click_recent_activity_link()
     workflow_page.validate_recent_activity_entry('Cover Letter card was '
                                                  'marked as complete',
                                                  user_type['name'])
+    workflow_page.click_close_overlay()
 
-  def test_cover_letter_file_submission(self):
-    """
-    test_cover_letter_file_submission: Tests the cover letter file upload and the admin card file
-      download
-    :return: void function
-    """
-    logging.info('Test Cover Letter Task::letter_file_upload')
-    user_type = random.choice(users)
-    dashboard = self.cas_login(user_type['email'])
-    dashboard.click_create_new_submission_button()
-    self.create_article(journal='PLOS Wombat', type_='Research', format='word')
-    manuscript_page = ManuscriptViewerPage(self.getDriver())
-    manuscript_page.page_ready_post_create()
-    short_doi = manuscript_page.get_short_doi()
-    manuscript_page.click_task('Cover Letter')
-    cover_letter_task = CoverLetterTask(self.getDriver())
-    cover_letter_task.task_ready()
-    cover_letter_task.upload_letter()
-    cover_letter_task.click_completion_button()
-    cover_letter_task.logout()
-
-    # Test card
-    staff_user = random.choice(editorial_users)
-    dashboard_page = self.cas_login(email=staff_user['email'])
-    dashboard_page.page_ready()
-    dashboard_page.go_to_manuscript(short_doi)
-    self._driver.navigated = True
-    paper_viewer = ManuscriptViewerPage(self.getDriver())
-    paper_viewer.page_ready()
-    # go to wf
-    paper_viewer.click_workflow_link()
-    workflow_page = WorkflowPage(self.getDriver())
-    workflow_page.page_ready()
     workflow_page.click_card('cover_letter')
     cover_letter_card = CoverLetterCard(self.getDriver())
     cover_letter_card.card_ready()
-    cover_letter_card.validate_uploaded_file_download(
-        cover_letter_task.get_last_uploaded_letter_file())
+    # cover_letter_card.validate_common_elements_styles(short_doi)
+    cover_letter_card.validate_styles()
+    # Mark card as incomplete
+    cover_letter_card.click_completion_button()
+    cover_letter_card.click_close_button()
+    # Wait for modal transition
+    time.sleep(0.5)
 
-  def test_cover_letter_file_replace(self):
-    """
-    test_cover_letter_file_replace: Tests the replacing of an uploaded cover letter file
-    :return: void function
-    """
-    logging.info('Test Cover Letter Task::letter_file_replace')
-    user_type = random.choice(users)
-    dashboard = self.cas_login(user_type['email'])
-    dashboard.click_create_new_submission_button()
-    self.create_article(journal='PLOS Wombat', type_='Research')
-    manuscript_page = ManuscriptViewerPage(self.getDriver())
-    manuscript_page.page_ready_post_create()
-    manuscript_page.click_task('Cover Letter')
-    cover_letter_task = CoverLetterTask(self.getDriver())
-    cover_letter_task.task_ready()
-    cover_letter_task.upload_letter()
-    cover_letter_task.replace_letter()
+    # Test card incompletion on activity feed
+    workflow_page.click_recent_activity_link()
+    workflow_page.validate_recent_activity_entry('Cover Letter card was '
+                                                 'marked as incomplete',
+                                                 staff_user['name'])
+    workflow_page.click_close_overlay()
 
-  def test_cover_letter_file_delete(self):
-    """
-    test_cover_letter_file_delete: Tests the deleting of an uploaded cover letter file
-    :return: void function
-    """
-    logging.info('Test Cover Letter Task::letter_file_delete')
-    user_type = random.choice(users)
-    dashboard = self.cas_login(user_type['email'])
-    dashboard.click_create_new_submission_button()
-    self.create_article(journal='PLOS Wombat', type_='Research')
-    manuscript_page = ManuscriptViewerPage(self.getDriver())
-    manuscript_page.page_ready_post_create()
-    manuscript_page.click_task('Cover Letter')
-    cover_letter_task = CoverLetterTask(self.getDriver())
-    cover_letter_task.task_ready()
-    cover_letter_task.upload_letter()
-    cover_letter_task.remove_letter()
-
-  def test_cover_letter_file_download(self):
-    """
-    test_cover_letter_file_delete: Tests the downloading of an uploaded cover letter file
-    :return: void function
-    """
-    logging.info('Test Cover Letter Task::letter_file_download')
-    user_type = random.choice(users)
-    dashboard = self.cas_login(user_type['email'])
-    dashboard.click_create_new_submission_button()
-    self.create_article(journal='PLOS Wombat', type_='Research')
-    manuscript_page = ManuscriptViewerPage(self.getDriver())
-    manuscript_page.page_ready_post_create()
-    manuscript_page.click_task('Cover Letter')
-    cover_letter_task = CoverLetterTask(self.getDriver())
-    cover_letter_task.task_ready()
-    cover_letter_task.upload_letter()
-    cover_letter_task.download_letter()
-
-  def test_cover_letter_text_submission(self):
-    """
-    test_cover_letter_text_submission: Tests the cover letter text submission and admin card view
-    :return: void function
-    """
-    logging.info('Test Cover Letter Task::letter_textarea')
-    user_type = random.choice(users)
-    dashboard = self.cas_login(user_type['email'])
-    dashboard.click_create_new_submission_button()
-    self.create_article(journal='PLOS Wombat', type_='Research')
-    manuscript_page = ManuscriptViewerPage(self.getDriver())
-    manuscript_page.page_ready_post_create()
-    short_doi = manuscript_page.get_short_doi()
-    manuscript_page.click_task('Cover Letter')
-    cover_letter_task = CoverLetterTask(self.getDriver())
-    cover_letter_task.task_ready()
-    cover_letter_task.validate_letter_textarea()
-    cover_letter_task.logout()
-
-    # Test card
-    staff_user = random.choice(editorial_users)
-    dashboard_page = self.cas_login(email=staff_user['email'])
-    dashboard_page.page_ready()
-    dashboard_page.go_to_manuscript(short_doi)
-    self._driver.navigated = True
-    paper_viewer = ManuscriptViewerPage(self.getDriver())
-    paper_viewer.page_ready()
-    # go to wf
-    paper_viewer.click_workflow_link()
-    workflow_page = WorkflowPage(self.getDriver())
-    workflow_page.page_ready()
-    workflow_page.click_card('cover_letter')
-    cover_letter_card = CoverLetterCard(self.getDriver())
-    cover_letter_card.card_ready()
-    cover_letter_card.validate_textarea_submitted_text(cover_letter_task.get_textarea_sample_text())
-    cover_letter_card.validate_textarea_text_editing()
+  # def test_cover_letter_file_submission(self):
+  #   """
+  #   test_cover_letter_file_submission: Tests the cover letter file upload and the admin card file
+  #     download
+  #   :return: void function
+  #   """
+  #   logging.info('Test Cover Letter Task::letter_file_upload')
+  #   user_type = random.choice(users)
+  #   dashboard = self.cas_login(user_type['email'])
+  #   dashboard.click_create_new_submission_button()
+  #   self.create_article(journal='PLOS Wombat', type_='Research', format='word')
+  #   manuscript_page = ManuscriptViewerPage(self.getDriver())
+  #   manuscript_page.page_ready_post_create()
+  #   short_doi = manuscript_page.get_short_doi()
+  #   manuscript_page.click_task('Cover Letter')
+  #   cover_letter_task = CoverLetterTask(self.getDriver())
+  #   cover_letter_task.task_ready()
+  #   cover_letter_task.upload_letter()
+  #   cover_letter_task.click_completion_button()
+  #   cover_letter_task.logout()
+  #
+  #   # Test card
+  #   staff_user = random.choice(editorial_users)
+  #   dashboard_page = self.cas_login(email=staff_user['email'])
+  #   dashboard_page.page_ready()
+  #   dashboard_page.go_to_manuscript(short_doi)
+  #   self._driver.navigated = True
+  #   paper_viewer = ManuscriptViewerPage(self.getDriver())
+  #   paper_viewer.page_ready()
+  #   # go to wf
+  #   paper_viewer.click_workflow_link()
+  #   workflow_page = WorkflowPage(self.getDriver())
+  #   workflow_page.page_ready()
+  #   workflow_page.click_card('cover_letter')
+  #   cover_letter_card = CoverLetterCard(self.getDriver())
+  #   cover_letter_card.card_ready()
+  #   cover_letter_card.validate_uploaded_file_download(
+  #       cover_letter_task.get_last_uploaded_letter_file())
+  #
+  # def test_cover_letter_file_replace(self):
+  #   """
+  #   test_cover_letter_file_replace: Tests the replacing of an uploaded cover letter file
+  #   :return: void function
+  #   """
+  #   logging.info('Test Cover Letter Task::letter_file_replace')
+  #   user_type = random.choice(users)
+  #   dashboard = self.cas_login(user_type['email'])
+  #   dashboard.click_create_new_submission_button()
+  #   self.create_article(journal='PLOS Wombat', type_='Research')
+  #   manuscript_page = ManuscriptViewerPage(self.getDriver())
+  #   manuscript_page.page_ready_post_create()
+  #   manuscript_page.click_task('Cover Letter')
+  #   cover_letter_task = CoverLetterTask(self.getDriver())
+  #   cover_letter_task.task_ready()
+  #   cover_letter_task.upload_letter()
+  #   cover_letter_task.replace_letter()
+  #
+  # def test_cover_letter_file_delete(self):
+  #   """
+  #   test_cover_letter_file_delete: Tests the deleting of an uploaded cover letter file
+  #   :return: void function
+  #   """
+  #   logging.info('Test Cover Letter Task::letter_file_delete')
+  #   user_type = random.choice(users)
+  #   dashboard = self.cas_login(user_type['email'])
+  #   dashboard.click_create_new_submission_button()
+  #   self.create_article(journal='PLOS Wombat', type_='Research')
+  #   manuscript_page = ManuscriptViewerPage(self.getDriver())
+  #   manuscript_page.page_ready_post_create()
+  #   manuscript_page.click_task('Cover Letter')
+  #   cover_letter_task = CoverLetterTask(self.getDriver())
+  #   cover_letter_task.task_ready()
+  #   cover_letter_task.upload_letter()
+  #   cover_letter_task.remove_letter()
+  #
+  # def test_cover_letter_file_download(self):
+  #   """
+  #   test_cover_letter_file_delete: Tests the downloading of an uploaded cover letter file
+  #   :return: void function
+  #   """
+  #   logging.info('Test Cover Letter Task::letter_file_download')
+  #   user_type = random.choice(users)
+  #   dashboard = self.cas_login(user_type['email'])
+  #   dashboard.click_create_new_submission_button()
+  #   self.create_article(journal='PLOS Wombat', type_='Research')
+  #   manuscript_page = ManuscriptViewerPage(self.getDriver())
+  #   manuscript_page.page_ready_post_create()
+  #   manuscript_page.click_task('Cover Letter')
+  #   cover_letter_task = CoverLetterTask(self.getDriver())
+  #   cover_letter_task.task_ready()
+  #   cover_letter_task.upload_letter()
+  #   cover_letter_task.download_letter()
+  #
+  # def test_cover_letter_text_submission(self):
+  #   """
+  #   test_cover_letter_text_submission: Tests the cover letter text submission and admin card view
+  #   :return: void function
+  #   """
+  #   logging.info('Test Cover Letter Task::letter_textarea')
+  #   user_type = random.choice(users)
+  #   dashboard = self.cas_login(user_type['email'])
+  #   dashboard.click_create_new_submission_button()
+  #   self.create_article(journal='PLOS Wombat', type_='Research')
+  #   manuscript_page = ManuscriptViewerPage(self.getDriver())
+  #   manuscript_page.page_ready_post_create()
+  #   short_doi = manuscript_page.get_short_doi()
+  #   manuscript_page.click_task('Cover Letter')
+  #   cover_letter_task = CoverLetterTask(self.getDriver())
+  #   cover_letter_task.task_ready()
+  #   cover_letter_task.validate_letter_textarea()
+  #   cover_letter_task.logout()
+  #
+  #   # Test card
+  #   staff_user = random.choice(editorial_users)
+  #   dashboard_page = self.cas_login(email=staff_user['email'])
+  #   dashboard_page.page_ready()
+  #   dashboard_page.go_to_manuscript(short_doi)
+  #   self._driver.navigated = True
+  #   paper_viewer = ManuscriptViewerPage(self.getDriver())
+  #   paper_viewer.page_ready()
+  #   # go to wf
+  #   paper_viewer.click_workflow_link()
+  #   workflow_page = WorkflowPage(self.getDriver())
+  #   workflow_page.page_ready()
+  #   workflow_page.click_card('cover_letter')
+  #   cover_letter_card = CoverLetterCard(self.getDriver())
+  #   cover_letter_card.card_ready()
+  #   cover_letter_card.validate_textarea_submitted_text(cover_letter_task.get_textarea_sample_text())
+  #   cover_letter_card.validate_textarea_text_editing()
 
 if __name__ == '__main__':
   CommonTest._run_tests_randomly()

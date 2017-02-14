8b4441cde2c098927023e51d7cc888f94c390d45
Abraham Choe <achoe@plos.org>
2017-02-13 17:37:30 -0800

    Revert "Run only cover letter test temporarily"
    This reverts commit 82ca45add54954ba2a474ea07d6ef63b002ede3e.

--- test/TeamCity_NoNoseIntegrationTest.bash
+++ test/TeamCity_NoNoseIntegrationTest.bash
@@ -44,39 +44,38 @@ rm Base/*.pyc
 rm frontend/*.pyc
 rm frontend/Pages/*.pyc
 
-
-# python -m frontend.test_addl_info_task
-# python -m frontend.test_admin
-#python -m frontend.test_assign_team
-# python -m frontend.test_authors_task
-# python -m frontend.test_bdd_cns
-# python -m frontend.test_bdd_create_to_submit
-# python -m frontend.test_changes_for_author
+python -m frontend.test_addl_info_task
+python -m frontend.test_admin
+python -m frontend.test_assign_team
+python -m frontend.test_authors_task
+python -m frontend.test_bdd_cns
+python -m frontend.test_bdd_create_to_submit
+python -m frontend.test_changes_for_author
 python -m frontend.test_cover_letter
-# python -m frontend.test_dashboard
-# python -m frontend.test_discussion_forum
-# python -m frontend.test_early_article_posting
-# python -m frontend.test_figure_task
-# python -m frontend.test_final_tech_check
-# python -m frontend.test_initial_decision_card
-# python -m frontend.test_initial_tech_check
-# python -m frontend.test_invite_ae_card
-# python -m frontend.test_invite_reviewers
-# python -m frontend.test_journal_admin
-# python -m frontend.test_login
-# python -m frontend.test_manuscript_viewer
-# python -m frontend.test_metadata_versioning
-# python -m frontend.test_paper_tracker
-# python -m frontend.test_production_metadata_card
-# python -m frontend.test_profile
-# python -m frontend.test_reactivate_ms
-# python -m frontend.test_register_decision
-# python -m frontend.test_reporting_guidelines
-# python -m frontend.test_reviewer_candidates
-# python -m frontend.test_reviewer_report
-# python -m frontend.test_revise_task
-# python -m frontend.test_revision_tech_check
-# python -m frontend.test_supporting_information
-# python -m frontend.test_title_abstract_card
-# python -m frontend.test_withdraw_ms
-# python -m frontend.test_workflow
+python -m frontend.test_dashboard
+python -m frontend.test_discussion_forum
+python -m frontend.test_early_article_posting
+python -m frontend.test_figure_task
+python -m frontend.test_final_tech_check
+python -m frontend.test_initial_decision_card
+python -m frontend.test_initial_tech_check
+python -m frontend.test_invite_ae_card
+python -m frontend.test_invite_reviewers
+python -m frontend.test_journal_admin
+python -m frontend.test_login
+python -m frontend.test_manuscript_viewer
+python -m frontend.test_metadata_versioning
+python -m frontend.test_paper_tracker
+python -m frontend.test_production_metadata_card
+python -m frontend.test_profile
+python -m frontend.test_reactivate_ms
+python -m frontend.test_register_decision
+python -m frontend.test_reporting_guidelines
+python -m frontend.test_reviewer_candidates
+python -m frontend.test_reviewer_report
+python -m frontend.test_revise_task
+python -m frontend.test_revision_tech_check
+python -m frontend.test_supporting_information
+python -m frontend.test_title_abstract_card
+python -m frontend.test_withdraw_ms
+python -m frontend.test_workflow

b2f0cfa08ee10189d1cbb3ffbdb8646ae49b56b4
Aaron Christy <aaron@mutuallyhuman.com>
2017-02-17 09:18:40 -0500

    Better watching for new journals

--- client/app/pods/components/admin-page/component.js
+++ client/app/pods/components/admin-page/component.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   journals: [],
   classNames: ['admin-page'],
 
-  multipleJournals: Ember.computed('journals', function() {
+  multipleJournals: Ember.computed('journals.[]', function() {
     return this.get('journals.length') > 1;
   })
 });

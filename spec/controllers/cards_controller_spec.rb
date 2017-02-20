4755f93e355997c1924308912074582a738f8e26
Chris Westra <cwestra@gmail.com>
2017-02-20 16:41:13 -0500

    Update specs

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 describe CardsController do
   subject(:do_request) do
     get :show, format: 'json', owner_type: object.class.name.underscore, owner_id: object.id
@@ -11,6 +13,12 @@ describe CardsController do
   describe 'GET index' do
     subject(:do_request) { get :index, format: :json }
     it_behaves_like "an unauthenticated json request"
+
+    before do
+      FactoryGirl.create(:card, journal: my_journal, name: 'My Journal')
+      FactoryGirl.create(:card, journal: my_other_journal, name: 'My Other Journal')
+      FactoryGirl.create(:card, journal: not_my_journal, name: 'Not My Journal')
+    end
 
     context 'and the user is signed in' do
       before do
@@ -26,25 +34,23 @@ describe CardsController do
       context 'for all journals' do
         it { is_expected.to responds_with 200 }
 
-        it 'returns task types for journals the user has access to' do
+        it 'returns cards for journals the user has access to' do
           do_request
-          cards = res_body['cards']
-          all_tasks = (my_journal.journal_task_types.count +
-                       my_other_journal.journal_task_types.count)
-          expect(cards.count).to eq(all_tasks)
+          card_names = res_body['cards'].map { |h| h['name'] }
+          expect(card_names).to contain_exactly('My Journal', 'My Other Journal')
         end
       end
 
       context 'for one journal' do
-        it 'returns no task types for journals the user has no access to' do
+        it 'returns no cards for journals the user has no access to' do
           get :index, journal_id: not_my_journal.id, format: :json
           expect(res_body['cards'].count).to eq(0)
         end
 
-        it 'returns all task types for a journal the user has access to' do
+        it 'returns all cards for the specified journal' do
           get :index, journal_id: my_journal.id, format: :json
-          cards = res_body['cards']
-          expect(cards.count).to eq(my_journal.journal_task_types.count)
+          card_names = res_body['cards'].map { |h| h['name'] }
+          expect(card_names).to contain_exactly('My Journal')
         end
       end
     end

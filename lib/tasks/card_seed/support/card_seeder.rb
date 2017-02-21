d8f551d28025e12b40f250cf28e7706492c757ba
Erik Hetzner <ehetzner@plos.org>
2017-02-21 14:24:57 -0800

    Change CardSeeder to work on all journals

--- lib/tasks/card_seed/support/card_seeder.rb
+++ lib/tasks/card_seed/support/card_seeder.rb
@@ -5,15 +5,17 @@
 # details
 class CardSeeder
   def self.seed_card(card_name, content)
-    card = Card.find_or_create_by!(name: card_name, journal: Journal.first)
-    content_root = CardContent.find_or_create_by!(card: card, ident: nil, parent: nil)
-    content.each do |c|
-      c[:parent] = content_root
-      c[:card] = card
-    end
+    Journal.all.each do |journal|
+      card = Card.find_or_create_by!(name: card_name, journal: journal)
+      content_root = CardContent.find_or_create_by!(card: card, ident: nil, parent: nil)
+      content.each do |c|
+        c[:parent] = content_root
+        c[:card] = card
+      end
 
-    # we don't want the content_root to be affected by the update_all_exactly
-    # operation, specify that ident must not be null
-    CardContent.where(card: card).where.not(ident: nil).update_all_exactly!(content)
+      # we don't want the content_root to be affected by the update_all_exactly
+      # operation, specify that ident must not be null
+      CardContent.where(card: card).where.not(ident: nil).update_all_exactly!(content)
+    end
   end
 end

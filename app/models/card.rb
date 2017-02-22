fe2a837044a36a69d40a97ea49b6f3082a3a0ddf
Sam Bleckley <sam@mutuallyhuman.com>
2017-02-22 11:14:37 -0500

    APERTA-9067 Better error messages

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,8 +4,12 @@ class Card < ActiveRecord::Base
 
   belongs_to :journal
   has_many :card_content, inverse_of: :card, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: { message: "Please give your card a name." }
   validates :journal, presence: true
+  validates :name, uniqueness: {
+    scope: :journal,
+    message: "That card name is taken. Please give your card a new name."
+  }
 
   def content_root
     card_content.roots.first

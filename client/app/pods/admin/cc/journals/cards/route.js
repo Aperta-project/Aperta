50cbdaf9f5968fc013efd3dd9e533f85e1ffa1f7
Sam Bleckley <sam@mutuallyhuman.com>
2017-02-22 11:14:15 -0500

    APERTA-9067 Add overlay to card catalogue

--- client/app/pods/admin/cc/journals/cards/route.js
+++ client/app/pods/admin/cc/journals/cards/route.js
@@ -4,6 +4,13 @@ export default Ember.Route.extend({
   model() {
     var journal = this.modelFor('admin.cc.journals').journal;
     var journalID = (journal && journal.get('id'));
-    return this.store.query('card', {'journal_id': journalID});
+
+    return Ember.RSVP.hash({
+      cards: this.store.query(
+        'card',
+        {'journal_id': journalID}
+      ),
+      journal: journal
+    });
   }
 });
--- client/app/pods/components/admin-page/card-catalogue/component.js
+++ client/app/pods/components/admin-page/card-catalogue/component.js
@@ -1,5 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  cards: []
+  cards: [],
+  journal: null,
+  newCardOverlayVisible: false,
+  saving: [],
+
+  allCards: Ember.computed.union('cards', 'saving'),
+  cardsSorting: ['name'],
+  sortedCards: Ember.computed.sort('allCards', 'cardsSorting'),
+
+  actions: {
+    showNewCardOverlay() {
+      this.set('newCardOverlayVisible', true);
+    },
+
+    hideNewCardOverlay() {
+      this.set('newCardOverlayVisible', false);
+    },
+
+    createCard(card) {
+      this.set('saving', [card]);
+      this.get('cards').update().then(() => {
+        this.set('saving', []);
+      });
+    }
+  }
 });

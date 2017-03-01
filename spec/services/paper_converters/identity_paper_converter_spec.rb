e91dfb7e86049908695c80d1f0a17565a8e2064c
Jack LaBarba <jlabarba@plos.org>
2017-03-01 14:40:44 -0800

    APERTA-9205 add test for SourceConverter

--- spec/services/paper_converters/identity_paper_converter_spec.rb
+++ spec/services/paper_converters/identity_paper_converter_spec.rb
@@ -4,7 +4,7 @@ describe PaperConverters::IdentityPaperConverter do
   let(:export_format) { 'pdf' }
   let(:paper) { create(:paper, :version_with_file_type) }
   let(:versioned_text) { paper.latest_version }
-  let(:converter) { PaperConverters::IdentityPaperConverter.new(versioned_text, export_format) }
+  let(:converter) { described_class.new(versioned_text, export_format) }
 
   it_behaves_like 'a redirecting paper converter'
 end

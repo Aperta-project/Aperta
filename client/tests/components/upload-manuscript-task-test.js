52a2fee69dec610aa1873a75280f27ec930391a3
Curtis Autery <curtis@mutuallyhuman.com>
2017-03-02 16:14:10 -0500

    Add upload manuscript task source file tests
    Tests that validation errors on uploading source files appear where
    appropriate. They should only appear if these 3 conditions are all met:
    * The paper has a PDF manuscript
    * The paper is in revision or have a version_text major version of > 0
    * The paper does not have a source file attached already

--- client/tests/components/upload-manuscript-task-test.js
+++ client/tests/components/upload-manuscript-task-test.js
@@ -3,6 +3,8 @@ import hbs from 'htmlbars-inline-precompile';
 import { manualSetup, make } from 'ember-data-factory-guy';
 import registerCustomAssertions from 'tahi/tests/helpers/custom-assertions';
 import FakeCanService from '../helpers/fake-can-service';
+import Ember from 'ember';
+import wait from 'ember-test-helpers/wait';
 
 moduleForComponent('upload-manuscript-task', 'Integration | Component | manuscript upload task', {
   integration: true,
@@ -12,15 +14,25 @@ moduleForComponent('upload-manuscript-task', 'Integration | Component | manuscri
     manualSetup(this.container);
     this.registry.register('service:can', FakeCanService);
 
+    const paper = make('paper', {
+      fileType: 'docx',
+      publishingState: 'in_revision',
+      journal: {
+        pdfAllowed: true
+      },
+      sourceFile: null,
+      versionedTexts: [{
+        id: 1,
+        majorVersion: 1,
+        minorVersion: 1,
+      }]
+    });
+
     this.set('task', make('upload-manuscript-task', {
       completed: false,
-      paper: {
-        fileType: 'docx',
-        journal: {
-          pdfAllowed: true
-        }
-      }
+      paper: paper
     }));
+    this.registry.register('pusher:main', Ember.Object.extend({socketId: 'foo'}));
   }
 });
 
@@ -48,4 +60,75 @@ test('Upload sourcefile component renders for pdf', function(assert) {
 
   this.render(hbs`{{upload-manuscript-task task=task}}`);
   assert.equal(this.$('#upload-sourcefile').length, 1, 'Upload sourcefile component does render');
+});
+
+test('docx papers don\'t require sourcefiles', function(assert) {
+  let fake = this.container.lookup('service:can');
+  fake.allowPermission('edit', this.get('task'));
+  $.mockjax({url: '/api/tasks/1', type: 'PUT', status: 204, responseText: '{}'});
+
+  this.render(hbs`{{upload-manuscript-task task=task}}`);
+  this.$('button.task-completed').click();
+
+  let done = assert.async();
+  wait().then(() => {
+    assert.equal(this.get('task.completed'), true, 'task was completed');
+    assert.mockjaxRequestMade('/api/tasks/1', 'PUT');
+    done();
+  });
+});
+
+test('pdf papers display a validation error if no sourcefile is attached', function(assert) {
+  let fake = this.container.lookup('service:can');
+  fake.allowPermission('edit', this.get('task'));
+  $.mockjax({url: '/api/tasks/1', type: 'PUT', status: 204, responseText: '{}'});
+
+  this.render(hbs`{{upload-manuscript-task task=task}}`);
+  this.set('task.paper.fileType', 'pdf');
+  this.$('button.task-completed').click();
+
+  let done = assert.async();
+  wait().then(() => {
+    assert.equal(this.get('task.completed'), false, 'task did not complete');
+    assert.mockjaxRequestMade('/api/tasks/1', 'PUT');
+    assert.equal(this.$('.error-message').length, 2);
+    done();
+  });
+});
+
+test('pdf papers validate if a sourcefile is attached', function(assert) {
+  let fake = this.container.lookup('service:can');
+  fake.allowPermission('edit', this.get('task'));
+  $.mockjax({url: '/api/tasks/1', type: 'PUT', status: 204, responseText: '{}'});
+
+  this.render(hbs`{{upload-manuscript-task task=task}}`);
+  this.set('task.paper.fileType', 'pdf');
+  this.set('task.paper.sourcefile', 'some.file');
+  this.$('button.task-completed').click();
+
+  let done = assert.async();
+  wait().then(() => {
+    assert.equal(this.get('task.completed'), true, 'task was completed');
+    assert.mockjaxRequestMade('/api/tasks/1', 'PUT');
+    done();
+  });
+});
+
+test('pdf papers not in revision don\'t require sourcefiles', function(assert) {
+  let fake = this.container.lookup('service:can');
+  fake.allowPermission('edit', this.get('task'));
+  $.mockjax({url: '/api/tasks/1', type: 'PUT', status: 204, responseText: '{}'});
+  this.set('task.paper.fileType', 'pdf');
+  this.set('task.paper.versionedTexts', []);
+  this.set('task.paper.publishingState', '');
+
+  this.render(hbs`{{upload-manuscript-task task=task}}`);
+  this.$('button.task-completed').click();
+
+  let done = assert.async();
+  wait().then(() => {
+    assert.equal(this.get('task.completed'), true, 'task was completed');
+    assert.mockjaxRequestMade('/api/tasks/1', 'PUT');
+    done();
+  });
 });

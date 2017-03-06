1b4e13984e6746614aad3c2a90f2246c3c427a41
Michael Walsh <noxiousthing@gmail.com>
2017-03-06 13:20:39 -0800

    Add copy changes related to new PDF and Word document expectations

--- engines/tahi_standard_tasks/app/subscribers/paper/data_extracted/notify_user.rb
+++ engines/tahi_standard_tasks/app/subscribers/paper/data_extracted/notify_user.rb
@@ -1,3 +1,5 @@
+# This class deals with pushing out notifications to users related to
+# the success or failure of manuscript uploads
 class Paper::DataExtracted::NotifyUser < FlashMessageSubscriber
   def user
     User.find(@event_data[:record].user_id)
@@ -13,8 +15,13 @@ class Paper::DataExtracted::NotifyUser < FlashMessageSubscriber
 
   def message
     if @event_data[:record].completed?
-      "Finished loading #{pdf_type? ? 'PDF' : 'Word'} file. Any images that had been included" +
-      ' in the manuscript should be uploaded directly to the figures card.'
+      message = "Finished loading #{pdf_type? ? 'PDF' : 'Word'} file."
+      unless pdf_type?
+        addendum = ' Any figures included in the file will have been removed' \
+                   ' should now be uploaded directly by clicking \'Figures\'.'
+        message.concat(addendum)
+      end
+      message
     elsif @event_data[:record].errored?
       "There was an error loading your #{pdf_type? ? 'PDF' : 'Word'} file."
     end

d452aeee2270c05b65255e01f89a2843b80763d2
Erik Hetzner <ehetzner@plos.org>
2017-03-06 20:47:27 -0800

    APERTA-9226 Delete nested question factories

--- spec/factories/nested_question_factory.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :nested_question do
-    association :owner, factory: :ad_hoc_task
-    sequence(:ident) { |n| "ident_#{n}" }
-    sequence(:position) { |n| n }
-    value_type "text"
-  end
-end

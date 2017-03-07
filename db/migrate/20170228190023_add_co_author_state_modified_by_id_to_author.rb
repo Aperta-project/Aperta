ecb9d01ae984c234090a1e1a277c48237757c788
Michael Walsh <noxiousthing@gmail.com>
2017-03-07 08:58:16 -0800

    Remove unneeded migration

--- db/migrate/20170228190023_add_co_author_state_modified_by_id_to_author.rb
+++
@@ -1,9 +0,0 @@
-class AddCoAuthorStateModifiedByIdToAuthor < ActiveRecord::Migration
-  def up
-    add_reference :authors, :co_author_state_modified_by, foreign_key: true
-  end
-
-  def down
-    remove_reference :authors, :co_author_state_modified_by
-  end
-end

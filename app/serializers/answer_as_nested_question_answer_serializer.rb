8d30b11f0f359f14bc69952024cd6ceb36159017
Chris Westra <cwestra@gmail.com>
2017-03-08 15:48:37 -0500

    Coerce answer value in shim serializer

--- app/serializers/answer_as_nested_question_answer_serializer.rb
+++ app/serializers/answer_as_nested_question_answer_serializer.rb
@@ -1,3 +1,4 @@
+# Sends Answers to the frontend in the same shape as NestedQuestionAnswers
 class AnswerAsNestedQuestionAnswerSerializer < ActiveModel::Serializer
   root :nested_question_answer
   attributes :id, :value, :value_type, :owner, :nested_question_id, :decision_id
@@ -7,11 +8,16 @@ class AnswerAsNestedQuestionAnswerSerializer < ActiveModel::Serializer
     { id: object.owner_id, type: object.owner_type.demodulize }
   end
 
+  def value
+    object.coerced_value
+  end
+
   def nested_question_id
     object.card_content_id
   end
 
   def decision_id
-    # TODO: What goes here?
+    # TODO: What goes here? The client doesn't actually use decision ids on
+    # answers anymore anyways (Reviewer Reports made them obsolete)
   end
 end

567e6ef7fe6d52ff87262269a4021586a384482a
Edem Kumodzi <ekumodzi@plos.org>
2017-03-09 20:47:49 +0100

    Fixed linting issues

--- app/services/paper_converters/pdf_paper_converter.rb
+++ app/services/paper_converters/pdf_paper_converter.rb
@@ -1,12 +1,13 @@
 module PaperConverters
+  # PDF Converter used to convert docx papers
   class PdfPaperConverter < SynchronousPaperConverter
     include DownloadablePaper
 
     def output_data
-          PDFKit.new(pdf_html,
-               footer_right: publishing_info_presenter.downloader_name,
-               footer_font_name: 'Times New Roman',
-               footer_font_size: '10').to_pdf
+      PDFKit.new(pdf_html,
+        footer_right: publishing_info_presenter.downloader_name,
+        footer_font_name: 'Times New Roman',
+        footer_font_size: '10').to_pdf
     end
 
     def output_filename
@@ -27,14 +28,19 @@ module PaperConverters
       PublishingInformationPresenter.new(@versioned_text.paper, @current_user)
     end
 
+    # rubocop:disable Rails/OutputSafety
+    def paper_body
+      @versioned_text.figureful_text(direct_img_links: true).html_safe
+    end
+
     def pdf_html
       render(
         'pdf',
         layout: nil,
         locals: {
           should_proxy_previews: false,
-          paper: @versioned_text.paper ,
-          paper_body: @versioned_text.figureful_text(direct_img_links: true).html_safe,
+          paper: @versioned_text.paper,
+          paper_body: paper_body,
           publishing_info_presenter: publishing_info_presenter,
           supporting_information_files: supporting_information_files
         }

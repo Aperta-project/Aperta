fc3c3eb0ee63badfc56f804419623b6771df7a1f
Aaron Christy <aaron@mutuallyhuman.com>
2017-03-14 15:30:24 -0400

    Add journal overlay to create a new journal

--- client/app/pods/components/admin-page/component.js
+++ client/app/pods/components/admin-page/component.js
@@ -4,10 +4,21 @@ export default Ember.Component.extend({
   journals: [],
   journalSort: ['name:asc'],
   sortedJournals: Ember.computed.sort('journals', 'journalSort'),
+  newJournalOverlayVisible: false,
 
   classNames: ['admin-page'],
 
   multipleJournals: Ember.computed('journals.[]', function() {
     return this.get('journals.length') > 1;
-  })
+  }),
+
+  actions: {
+    showNewJournalOverlay() {
+      this.set('newJournalOverlayVisible', true);
+    },
+
+    hideNewJournalOverlay() {
+      this.set('newJournalOverlayVisible', false);
+    }
+  }
 });

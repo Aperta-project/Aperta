8d40f34e817923144f09419a3674cf9ccb6b5502
Aaron Christy <aaron@mutuallyhuman.com>
2017-03-15 17:14:01 -0400

    Background the journal logo processing
    Previously, the journal logo processing was happening inline.  So,
    improve performance by backgrounding this and leaning on journal event
    streaming to return the processed logo when complete.
    
    In addition, allow the logo url to be sent with the update and create
    parameters rather than a separate call.  Retire the upload_logo route.

--- spec/controllers/admin/journals_controller_spec.rb
+++ spec/controllers/admin/journals_controller_spec.rb
@@ -49,17 +49,26 @@ describe Admin::JournalsController, redis: true do
 
       describe 'uploading journal logo' do
         context 'when url is provided' do
-          let(:logo_url) { 'http://s3.com/logo.png' }
+          let(:logo_url) { 'http://s3.com/pending/logo.png' }
 
           it 'calls the journal logo service class' do
-            expect(DownloadLogo).to receive(:call).with(instance_of(Journal), logo_url)
+            expect(DownloadJournalLogoWorker).to receive(:perform_async).with(kind_of(Integer), logo_url)
             do_request
           end
         end
 
         context 'when url is not provided' do
           it 'does not call the journal logo service class' do
-            expect(DownloadLogo).to_not receive(:call)
+            expect(DownloadJournalLogoWorker).to_not receive(:perform_async)
+            do_request
+          end
+        end
+
+        context 'when url is not a new logo stored in a temporary s3 bucket' do
+          let(:logo_url) { 'http://s3.com/logo.png' }
+
+          it 'does not call the journal logo service class' do
+            expect(DownloadJournalLogoWorker).to_not receive(:perform_async)
             do_request
           end
         end
@@ -125,60 +134,6 @@ describe Admin::JournalsController, redis: true do
       end
 
       it "renders status 403" do
-        do_request
-        expect(response.status).to eq 403
-      end
-    end
-  end
-
-  describe '#upload_logo' do
-    subject(:do_request) do
-      patch :upload_logo,
-            format: 'json',
-            id: journal.id,
-            url: logo_url
-    end
-
-    let(:logo_url) { 'http://s3.com/logo.png' }
-
-    it_behaves_like 'an unauthenticated json request'
-
-    context 'when the user has access' do
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:administer, journal)
-          .and_return true
-      end
-
-      context 'when url is provided' do
-        it 'calls the journal logo service class' do
-          expect(DownloadLogo).to receive(:call).with(instance_of(Journal), logo_url)
-          do_request
-        end
-      end
-
-      context 'when url is not provided' do
-        let(:logo_url) { nil }
-
-        it 'does not call the journal logo service class' do
-          expect(DownloadLogo).to_not receive(:call)
-          do_request
-        end
-      end
-    end
-
-    context 'when the user does not have access' do
-      let(:logo_url) { nil }
-
-      before do
-        stub_sign_in user
-        allow(user).to receive(:can?)
-          .with(:administer, journal)
-          .and_return false
-      end
-
-      it 'renders status 403' do
         do_request
         expect(response.status).to eq 403
       end

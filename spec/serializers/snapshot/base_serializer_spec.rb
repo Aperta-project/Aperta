7bcd5b0f083ceb73156fa3cb7641b05112218cbf
Erik Hetzner <ehetzner@plos.org>
2017-03-15 10:26:25 -0700

    APERTA-9226 Rename nested_questions to card_content

--- spec/serializers/snapshot/base_serializer_spec.rb
+++ spec/serializers/snapshot/base_serializer_spec.rb
@@ -15,9 +15,9 @@ describe Snapshot::BaseSerializer do
 
   describe "snapshot ordering of children" do
     subject(:serializer) { Snapshot::TestSerializer.new(model) }
-    let(:model) { OpenStruct.new(nested_questions: [], id: 1) }
+    let(:model) { OpenStruct.new(card_content: [], id: 1) }
 
-    it "snapshots nested questions first, then id, then other properties" do
+    it "snapshots card_content first, then id, then other properties" do
       expect(serializer.as_json[:children]).to eq([
         { questions: "here" },
         { name: "id", type: "integer", value: 1 },

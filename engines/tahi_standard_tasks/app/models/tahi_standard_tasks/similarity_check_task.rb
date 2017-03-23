354f6743a55d97bcbb69021dca79eb2d058f9c58
Edem Kumodzi <ekumodzi@plos.org>
2017-03-23 09:16:15 +0000

    generated similarity check task

---
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/similarity_check_task.rb
@@ -0,0 +1,22 @@
+module TahiStandardTasks
+  # The model class for the Similarity Check task, which is
+  # used by SOMEONE for SOMETHING. FILL ME OUT PLEASE.
+  class SimilarityCheckTask < Task
+    DEFAULT_TITLE = 'Similarity Check'.freeze
+    DEFAULT_ROLE_HINT = 'author'.freeze
+
+    # You should include MetadataTask if the task is required for
+    # submission and should be visible to reviewers
+    #
+    # include MetadataTask
+    #
+    # You should include SubmissionTask if the task is required for
+    # submission but should NOT be visible to reviewers.
+    #
+    # include SubmissionTask
+
+    def active_model_serializer
+      TahiStandardTasks::SimilarityCheckTaskSerializer
+    end
+  end
+end

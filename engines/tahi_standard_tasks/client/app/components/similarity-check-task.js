4292c8917745cb0df67f6977fd52230ed8bdf8b3
Edem Kumodzi <ekumodzi@plos.org>
2017-03-26 20:15:26 +0000

    similarity check card

--- engines/tahi_standard_tasks/client/app/components/similarity-check-task.js
+++ engines/tahi_standard_tasks/client/app/components/similarity-check-task.js
@@ -1,5 +1,13 @@
 import TaskComponent from 'tahi/pods/components/task-base/component';
 
 export default TaskComponent.extend({
-  classNames: ['similarity-check-task']
+  classNames: ['similarity-check-task'],
+  actions: {
+    generateReport() {
+      this.set('confirmVisible', true);
+    },
+    cancel() {
+      this.set('confirmVisible', false);
+    }
+  }
 });

846a2f69e4ed74937491c77f7ae99bc9681a1900
Sam Bleckley <sam@mutuallyhuman.com>
2017-03-28 15:15:17 -0400

    APERTA-9076 Update seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320141043) do
+ActiveRecord::Schema.define(version: 20170328151358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

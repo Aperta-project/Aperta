5720f2216ca7908fefcf52a07407c362733ff0f4
Edem Kumodzi <ekumodzi@plos.org>
2017-03-28 18:00:25 +0000

    move similarity check task from author to staff column

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/similarity_check_task.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/similarity_check_task.rb
@@ -3,7 +3,7 @@ module TahiStandardTasks
   # used by SOMEONE for SOMETHING. FILL ME OUT PLEASE.
   class SimilarityCheckTask < Task
     DEFAULT_TITLE = 'Similarity Check'.freeze
-    DEFAULT_ROLE_HINT = 'author'.freeze
+    DEFAULT_ROLE_HINT = 'admin'.freeze
 
     # You should include MetadataTask if the task is required for
     # submission and should be visible to reviewers

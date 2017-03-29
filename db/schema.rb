8847dd84a6cfc4fc28beeb3eb9b4eaf26ba03f35
Curtis Autery <curtis@mutuallyhuman.com>
2017-03-29 06:58:56 -0400

    Include new schema.rb with APERTA-7039

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301212950) do
+ActiveRecord::Schema.define(version: 20170328150006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

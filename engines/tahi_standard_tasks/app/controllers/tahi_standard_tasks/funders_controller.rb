c3299ca54aed7e494346e532cd01dc0d1274c7ab
Aaron Christy <aaron@mutuallyhuman.com>
2017-03-30 16:00:36 -0400

    This fixes a bug that prevented Funder editing
    Bugsnag info:
    https://app.bugsnag.com/tahi-project/soma-prod-tahi/errors/58dced060214bc1700835ce2
    ActiveRecord::UnknownAttributeError·unknown attribute 'card_id' for TahiStandardTasks::Funder
    
    At a point during the card config work, the idea of versioning was
    introduced.  This means that all places that were sending `card_id`
    should have been changed to send `card_version_id`.  This one controller
    was missed and strong params was incorrectly filtering out `card_version_id`.

--- engines/tahi_standard_tasks/app/controllers/tahi_standard_tasks/funders_controller.rb
+++ engines/tahi_standard_tasks/app/controllers/tahi_standard_tasks/funders_controller.rb
@@ -36,7 +36,7 @@ module TahiStandardTasks
                 :grant_number,
                 :website,
                 :task_id,
-                :card_id,
+                :card_version_id,
                 author_ids: [])
     end
   end

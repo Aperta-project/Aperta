0eb2ea9c0f6f755092a6e32a74a7ccaee34549ba
Michael Walsh <noxiousthing@gmail.com>
2017-04-03 10:32:05 -0700

    Fix invitation spec

--- spec/models/invitation_attachment_spec.rb
+++ spec/models/invitation_attachment_spec.rb
@@ -12,7 +12,7 @@ describe InvitationAttachment do
     it_behaves_like 'attachment#download! stores the file'
     it_behaves_like 'attachment#download! caches the s3 store_dir'
     it_behaves_like 'attachment#download! sets the file_hash'
-    it_behaves_like 'attachment#download! sets title to file name'
+    it_behaves_like 'attachment#download! sets title_html to file name'
     it_behaves_like 'attachment#download! sets the status'
     it_behaves_like 'attachment#download! always keeps snapshotted files on s3'
     it_behaves_like 'attachment#download! manages resource tokens'

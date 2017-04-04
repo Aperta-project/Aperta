d9253b8cf8eb3c4ad60ad93556c956069da1aff8
Michael Walsh <noxiousthing@gmail.com>
2017-04-04 13:10:08 -0700

    fix shared example spec failures

--- engines/tahi_standard_tasks/spec/models/supporting_information_task_spec.rb
+++ engines/tahi_standard_tasks/spec/models/supporting_information_task_spec.rb
@@ -5,7 +5,7 @@ module TahiStandardTasks
     it_behaves_like 'is a metadata task'
 
     describe '.restore_defaults' do
-      it_behaves_like '<Task class>.restore_defaults update title_html to the default'
+      it_behaves_like '<Task class>.restore_defaults update title to the default'
     end
 
     describe "#file_access_details" do

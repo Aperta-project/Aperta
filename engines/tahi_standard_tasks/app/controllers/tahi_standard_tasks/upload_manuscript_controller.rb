ea1332cb848637bbe27af88969904f0370509ce1
Yetunde Sanni <yetunde.sanni@andela.com>
2017-04-06 23:58:06 +0300

    APERTA-9484-#WIP to display error message on duplicate files

--- engines/tahi_standard_tasks/app/controllers/tahi_standard_tasks/upload_manuscript_controller.rb
+++ engines/tahi_standard_tasks/app/controllers/tahi_standard_tasks/upload_manuscript_controller.rb
@@ -13,6 +13,28 @@ module TahiStandardTasks
     def upload_manuscript
       requires_user_can :edit, task
 
+      attachment = task.paper.file
+      # A new download is needed to compare the file data
+      attachment.file.download! params[:url]
+      new_file_hash = Digest::SHA256.hexdigest(attachment.file.file.read)
+      if new_file_hash == attachment.previous_file_hash
+        TahiPusher::Channel.delay(queue: :eventstream, retry: false).push(
+          channel_name: "private-user@#{current_user.id}",
+          event_name: 'flashMessage',
+          payload: {
+            messageType: 'error',
+            message: "<b>Duplicate file.</b> Please note: The specified
+              file <i>#{attachment.title}</i> has been re-processed.
+              <br>If you need to make any changes to your manuscript,
+              you can upload again by clicking the <i>Replace</i> link. "
+          }
+        )
+      else
+        # Updating the previous_file_hash with the new
+        # file_hash when they are not equal
+        attachment.update_column(:previous_file_hash, attachment.file_hash)
+      end
+
       DownloadManuscriptWorker.download_manuscript(
         task.paper,
         params[:url],

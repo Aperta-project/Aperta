08514e41b11235fd02ea5fd36560acee5793e089
Chris Westra <cwestra@gmail.com>
2017-04-12 13:40:35 -0400

    Populate default answer values for tasks

--- app/models/card_version.rb
+++ app/models/card_version.rb
@@ -18,4 +18,14 @@ class CardVersion < ActiveRecord::Base
     scope: :card_id,
     message: "Card version numbers are unique for a given card"
   }
+
+  def create_default_answers(task)
+    card_contents.where.not(default_answer_value: nil).find_each do |content|
+      task.answers.create(
+        card_content: content,
+        paper: task.paper,
+        value: content.default_answer_value
+      )
+    end
+  end
 end

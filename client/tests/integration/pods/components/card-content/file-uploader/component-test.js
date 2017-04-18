c319a041783a9f62d99222af6ea7412bbd613546
Chris Westra <cwestra@gmail.com>
2017-04-18 11:16:49 -0400

    Add tests for file-uploader
    The file-uploader wraps the attachment-manager component we use in several other
    contexts, and which has a few different types of tests for it specifically.

---
+++ client/tests/integration/pods/components/card-content/file-uploader/component-test.js
@@ -0,0 +1,54 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import registerCustomAssertions from 'tahi/tests/helpers/custom-assertions';
+import Ember from 'ember';
+
+moduleForComponent(
+  'card-content/file-uploader',
+  'Integration | Component | card content | file uploader',
+  {
+    integration: true,
+    beforeEach() {
+      registerCustomAssertions();
+      this.setProperties({
+        actionStub: function() {},
+        content: Ember.Object.create({
+          ident: 'test',
+          allowFileCaptions: true,
+          allowMultipleUploads: true,
+          label: 'Upload',
+          text: 'Please upload a file'
+        }),
+        answer: Ember.Object.create({ value: null })
+      });
+    }
+  }
+);
+
+let template = hbs`{{card-content/file-uploader
+                      answer=answer
+                      content=content
+                      disabled=disabled
+                      preview=preview
+                    }}`;
+
+test(`shows an uploader with text and a button`, function(assert) {
+  this.render(template);
+  assert.textPresent(
+    '.description',
+    'Please upload a file',
+    `the content's text is the uploader's description`
+  );
+
+  assert.textPresent(
+    '.fileinput-button',
+    'Upload',
+    'The label displayed as the file upload button text'
+  );
+
+  this.set('disabled', true);
+  assert.elementFound('.button--disabled', 'disables but still shows the button');
+
+  this.setProperties({disabled: false, preview: true});
+  assert.elementFound('.button--disabled', 'preview will also disable the button');
+});

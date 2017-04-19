02d7bc7020b70785fdf97579b1e280ff966135e7
Aaron Christy <aaron@mutuallyhuman.com>
2017-04-19 11:10:44 -0400

    funder

--- spec/factories/funder_factory.rb
+++ spec/factories/funder_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :funder, class: TahiStandardTasks::Funder do
     association :task, factory: :ad_hoc_task
+    card_version
     sequence(:grant_number) { |n| "10000#{n}" }
     name         "Leia Organa-Solo"
     website      "http://alderaan.gov"

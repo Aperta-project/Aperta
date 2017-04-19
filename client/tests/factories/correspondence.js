a38011953dcc5545a9d3101371bbea3be749f1c7
Brandon Zylstra <brandon@mutuallyhuman.com>
2017-04-19 10:24:10 -0400

    frontend for correspondence history including styles

---
+++ client/tests/factories/correspondence.js
@@ -0,0 +1,10 @@
+import FactoryGuy from 'ember-data-factory-guy';
+
+FactoryGuy.define('correspondence', {
+  default: {
+    date: '2014-09-28T13:54:58.028Z',
+    subject: 'Thank you for submitting your manuscript to PLOS Abominable Snowman',
+    recipient: 'john.doe@example.com',
+    sender: 'joe@example.com'
+  }
+});

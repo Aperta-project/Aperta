18569c1f2c0f439ae7b7ba762140785f66f08b27
Erik Hetzner <ehetzner@plos.org>
2017-04-21 13:04:21 -0700

    APERTA-9087 Fix redis sentinel configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,12 @@
 redis_config = if TahiEnv.redis_sentinel_enabled?
                  {
+                   url: ENV[ENV['REDIS_PROVIDER'] || 'REDIS_URL'],
+                   role: :master,
                    master_name: 'aperta',
-                   sentinels: TahiEnv.redis_sentinels,
+                   sentinels: TahiEnv.redis_sentinels.map do |s|
+                     u = URI.parse(s)
+                     { host: u.host, port: (u.port || 26_379) }
+                   end,
                    failover_reconnect_timeout: 20,
                    namespace: "tahi_#{Rails.env}"
                  }
@@ -10,6 +15,7 @@ redis_config = if TahiEnv.redis_sentinel_enabled?
                    namespace: "tahi_#{Rails.env}"
                  }
                end
+
 Sidekiq.configure_server do |config|
   config.redis = redis_config
   # allow configuration of concurrency without redeploy

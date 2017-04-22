52cb16b75c3571bb3ec4148185715e7a0fdd546f
Jack LaBarba <brakit@gmail.com>
2017-04-22 12:02:25 -0700

    APERTA-9479 add a little test for SimilarityCheck#create

--- spec/models/similarity_check_spec.rb
+++ spec/models/similarity_check_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 describe SimilarityCheck, type: :model do
+  describe "#create" do
+    let(:versioned_text) { create :versioned_text }
+    let(:do_create) { SimilarityCheck.create!(versioned_text: versioned_text) }
+
+    it "enqueues a SimilarityCheckStartReportWorker job" do
+      expect do
+        do_create
+      end.to change { SimilarityCheckStartReportWorker.jobs.size }.by(1)
+    end
+
+    it "enqueues a job with the SimilarityCheck record id as an arg" do
+      similarity_report = do_create
+      args = SimilarityCheckStartReportWorker.jobs.first["args"]
+      expect(similarity_report.id).to be_present
+      expect(args).to eq [similarity_report.id]
+    end
+  end
 end

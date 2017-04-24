e7d415a60462e8a87a626dd63c2503323fe192d7
Chris Westra <cwestra@gmail.com>
2017-04-24 16:21:30 -0400

    Add polish to card thumbnails

--- client/app/models/card.js
+++ client/app/models/card.js
@@ -1,6 +1,25 @@
 import DS from 'ember-data';
 import Ember from 'ember';
 
+let cardStates = {
+  icon: {
+    draft: 'fa-pencil-square-o',
+    published: 'fa-flag',
+    publishedWithChanges: 'fa-pencil-square'
+  },
+  name: {
+    draft: 'draft',
+    published: 'published card',
+    publishedWithChanges: 'published card with unpublished changes'
+  },
+  description: {
+    draft: `This card is a draft. It can only be viewed by admins at this time. This card cannot
+     be added to the workflow until it is published`,
+    publishedWithChanges: `This card has unpublished changes. You can preview both the published card and the
+     new unpublished draft of this card in the card preview`
+  }
+};
+
 export default DS.Model.extend({
   restless: Ember.inject.service(),
   journal: DS.belongsTo('admin-journal'),
@@ -12,23 +31,18 @@ export default DS.Model.extend({
   xml: DS.attr('string'),
 
   stateIcon: Ember.computed('state', function() {
-    return {
-      draft: 'fa-pencil-square-o',
-      published: 'fa-flag',
-      publishedWithChanges: 'fa-pencil-square'
-    }[this.get('state')];
+    return cardStates.icon[this.get('state')];
   }),
 
   stateName: Ember.computed('state', function() {
-    return {
-      draft: 'draft',
-      published: 'published card',
-      publishedWithChanges: 'published card with unpublished changes'
-    }[this.get('state')];
+    return cardStates.name[this.get('state')];
+  }),
+
+  stateDescription: Ember.computed('state', function() {
+    return cardStates.description[this.get('state')];
   }),
 
   publish() {
-    return this.get('restless')
-      .putUpdate(this, `/publish`);
+    return this.get('restless').putUpdate(this, `/publish`);
   }
 });
--- client/app/pods/components/admin-page/card-thumbnail/component.js
+++ client/app/pods/components/admin-page/card-thumbnail/component.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['admin-card-thumbnail'],
-  card: null
+  classNames: [],
+  card: null,
+  showDescription: false,
+
+  actions: {
+    toggleDescription(v) {
+      this.set('showDescription', v);
+    }
+  }
 });

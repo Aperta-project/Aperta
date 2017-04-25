90c21562c59c250fbb75f3d36f0d41d7d2a56aed
Edem Kumodzi <ekumodzi@plos.org>
2017-04-25 13:57:36 +0000

    Adding Correspondence Overlay modal

--- client/app/pods/paper/correspondence/controller.js
+++ client/app/pods/paper/correspondence/controller.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  correspondence: Ember.computed.alias('model')
+  correspondence: Ember.computed.alias('model'),
+  showCorrespondenceOverlay: true,
+  actions: {
+    showCorrespondenceOverlay() {
+      this.set('showCorrespondenceOverlay', true);
+    },
+    hideCorrespondenceOverlay() {
+      this.set('showCorrespondenceOverlay', false);
+    }
   }
-);
+});

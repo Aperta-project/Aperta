c4c21f866ef2f4d89aa0e3c9d164948eb66a8b82
Jack LaBarba <jlabarba@plos.org>
2017-04-25 10:36:51 -0700

    APERTA-9479 complete tests for SimilarityCheckStartReportWorker

--- spec/workers/similarity_check_start_report_worker_spec.rb
+++ spec/workers/similarity_check_start_report_worker_spec.rb
@@ -4,12 +4,46 @@ describe SimilarityCheckStartReportWorker, type: :worker, sidekiq: :inline! do
   let(:paper) { create :paper, :version_with_file_type }
   let!(:similarity_check) { create :similarity_check, versioned_text: paper.latest_version }
   let(:stubbed_url) { paper.file.url }
+  let(:fake_doc_id) { Faker::Number.number(8).to_i }
+  let(:fake_ithenticate_response) do
+    {
+      "api_status" => 200,
+      "uploaded" => [
+        {
+          "id" => fake_doc_id
+        }
+      ]
+    }
+  end
+  subject(:perform) do
+    SimilarityCheckStartReportWorker.perform_async(similarity_check.id)
+  end
 
   before do
     stub_request(:get, stubbed_url).to_return(body: "turtles")
   end
 
-  it "works" do
-    SimilarityCheckStartReportWorker.perform_async(similarity_check.id)
+  it "adds a document through the IthenticateApi" do
+    expect(IthenticateApi).to(
+      receive_message_chain(:new_from_tahi_env, :add_document)
+        .and_return(fake_ithenticate_response)
+    )
+    perform
+  end
+
+  describe "successful ithenticate api calls" do
+    before do
+      allow(IthenticateApi).to(
+        receive_message_chain(:new_from_tahi_env, :add_document)
+          .and_return(fake_ithenticate_response)
+      )
+    end
+
+    it "updates the SimilarityCheck record" do
+      expect do
+        perform
+      end.to change { similarity_check.reload.ithenticate_document_id }
+               .from(nil).to(fake_doc_id)
+    end
   end
 end

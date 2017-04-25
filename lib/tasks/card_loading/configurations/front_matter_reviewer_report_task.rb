21bb488711a2fc6d9dd5203d141b74d3646761d9
Aaron Christy <aaron@mutuallyhuman.com>
2017-04-25 14:58:37 -0400

    oops!  move around the config files

--- lib/tasks/card_loading/configurations/front_matter_reviewer_report_task.rb
+++ lib/tasks/card_loading/configurations/front_matter_reviewer_report_task.rb
@@ -13,57 +13,7 @@ module CardConfiguration
     end
 
     def self.content
-      [
-        {
-          ident: 'front_matter_reviewer_report--decision_term',
-          value_type: 'text',
-          text: 'Please provide your publication recommendation:'
-        },
-
-        {
-          ident: "front_matter_reviewer_report--competing_interests",
-          value_type: "text",
-          text: "Do you have any potential or perceived competing interests that may influence your review?"
-        },
-
-        {
-          ident: "front_matter_reviewer_report--suitable",
-          value_type: "boolean",
-          text: "Is this manuscript suitable in principle for the magazine section of <em>PLOS Biology</em>?",
-          children: [
-            {
-              ident: "front_matter_reviewer_report--suitable--comment",
-              value_type: "html",
-              text: "Suitable Comment"
-            }
-          ]
-        },
-
-        {
-          ident: "front_matter_reviewer_report--includes_unpublished_data",
-          value_type: "boolean",
-          text: "If previously unpublished data are included to support the conclusions, please note in the box below whether:",
-          children: [
-            {
-              ident: "front_matter_reviewer_report--includes_unpublished_data--explanation",
-              value_type: "text",
-              text: "Includes Published Data Explanation"
-            }
-          ]
-        },
-
-        {
-          ident: "front_matter_reviewer_report--additional_comments",
-          value_type: "text",
-          text: "(Optional) Please offer any additional confidential comments to the editor"
-        },
-
-        {
-          ident: "front_matter_reviewer_report--identity",
-          value_type: "text",
-          text: "(Optional) If you'd like your identity to be revealed to the authors, please include your name here."
-        }
-      ]
+      []
     end
   end
 end

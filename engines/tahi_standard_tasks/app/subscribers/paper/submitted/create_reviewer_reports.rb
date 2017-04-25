01088c8cee48f62ffccbe53772d383e852221108
Aaron Christy <aaron@mutuallyhuman.com>
2017-04-25 16:01:57 -0400

    reviewer reports

--- engines/tahi_standard_tasks/app/subscribers/paper/submitted/create_reviewer_reports.rb
+++ engines/tahi_standard_tasks/app/subscribers/paper/submitted/create_reviewer_reports.rb
@@ -9,11 +9,28 @@ class Paper::Submitted::CreateReviewerReports
 
     reviewer_tasks = paper.tasks.where(type: REVIEWER_SPECIFIC_TASKS)
     reviewer_tasks.each do |task|
-      ReviewerReport.find_or_create_by!(
+      report = ReviewerReport.find_or_initialize_by(
         task: task,
         decision: paper.draft_decision,
         user: task.reviewer
       )
+      report.card_version = report.card_version ||
+                            latest_card_version(paper)
+
+      report.save!
     end
+  end
+
+  def self.latest_card_version(paper)
+    klass = if paper.uses_research_article_reviewer_report
+      "ReviewerReport"
+    else
+      # note: this AR model does not yet exist, but
+      # is being done as preparatory / consistency for
+      # card config work
+      "TahiStandardTasks::FrontMatterReviewerReport"
+    end
+
+    Card.find_by_class_name(klass).latest_card_version
   end
 end

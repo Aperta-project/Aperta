62e79ef2f9a92dfbed6a9e10388d5b75096aebc5
Aaron Christy <aaron@mutuallyhuman.com>
2017-04-26 09:55:49 -0400

    Create missing Card configuration classes
    Since all answerables need associated to a Card via a CardVersion,
    create an explicit configuration class for each of them.  This should
    also include configuration classes for any test classes that are
    created as part of the rspec tests (InvitableTestTask, MetadataTestTask).

---
+++ lib/tasks/card_loading/configurations/nonstandard_configurations/metadata_test_task.rb
@@ -0,0 +1,21 @@
+# This class defines the specific attributes of a particular Card and it can be
+# used to create a new valid Card into the system.  The `content` can be used
+# to create CardContent for the Card.
+#
+# This particular class is only used by rspec test factories.
+#
+module CardConfiguration
+  class MetadataTestTask
+    def self.name
+      "MetadataTestTask"
+    end
+
+    def self.title
+      "Metadata Test Task"
+    end
+
+    def self.content
+      []
+    end
+  end
+end

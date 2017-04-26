87f29ff8df5632d8159430b1bbb7d45c377e6820
Aaron Christy <aaron@mutuallyhuman.com>
2017-04-26 15:36:31 -0400

    Update non-Task answerable models with Card association
    All answerable models should have a Card association created through
    CardVersions, so ensure that they are set when the answerable model
    gets created.

--- engines/tahi_standard_tasks/app/subscribers/paper/submitted/create_reviewer_reports.rb
+++ engines/tahi_standard_tasks/app/subscribers/paper/submitted/create_reviewer_reports.rb
@@ -2,18 +2,37 @@
 # reviewer report for any existing review tasks
 class Paper::Submitted::CreateReviewerReports
   REVIEWER_SPECIFIC_TASKS = ["TahiStandardTasks::FrontMatterReviewerReportTask",
-                             "TahiStandardTasks::ReviewerReportTask"]
+                             "TahiStandardTasks::ReviewerReportTask"].freeze
 
   def self.call(_, event_data)
     paper = event_data[:record]
 
     reviewer_tasks = paper.tasks.where(type: REVIEWER_SPECIFIC_TASKS)
     reviewer_tasks.each do |task|
-      ReviewerReport.find_or_create_by!(
+      report = ReviewerReport.find_or_initialize_by(
         task: task,
         decision: paper.draft_decision,
         user: task.reviewer
       )
+
+      # assign latest card version if one is not already set
+      card_version = report.card_version || latest_card_version(paper)
+      report.card_version = card_version
+
+      report.save!
     end
+  end
+
+  def self.latest_card_version(paper)
+    klass = if paper.uses_research_article_reviewer_report
+              "ReviewerReport"
+            else
+              # note: this AR model does not yet exist, but
+              # is being done as preparatory / consistency for
+              # card config work
+              "TahiStandardTasks::FrontMatterReviewerReport"
+            end
+
+    Card.find_by_class_name(klass).latest_card_version
   end
 end

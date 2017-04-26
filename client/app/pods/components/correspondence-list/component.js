b111faf9fdb4a454d11f79d56337e543414d9cf6
Edem Kumodzi <ekumodzi@plos.org>
2017-04-26 12:40:16 +0000

    move correspondence overlay into correspondence list component

---
+++ client/app/pods/components/correspondence-list/component.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+export default Ember.Component.extend({
+  showCorrespondenceOverlay: true,
+  actions: {
+    showCorrespondenceOverlay() {
+      this.set('showCorrespondenceOverlay', true);
+    },
+    hideCorrespondenceOverlay() {
+      this.set('showCorrespondenceOverlay', false);
+    }
+  }
+});
--- client/app/pods/paper/correspondence/controller.js
+++ client/app/pods/paper/correspondence/controller.js
@@ -2,13 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   correspondence: Ember.computed.alias('model'),
-  showCorrespondenceOverlay: true,
-  actions: {
-    showCorrespondenceOverlay() {
-      this.set('showCorrespondenceOverlay', true);
-    },
-    hideCorrespondenceOverlay() {
-      this.set('showCorrespondenceOverlay', false);
-    }
-  }
 });

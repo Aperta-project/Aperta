ff778aa83743597fc4eb714874f4594b4ef52b28
Chris Westra <cwestra@gmail.com>
2017-04-26 09:39:28 -0400

    Fix flaky query parser spec
    Somehow Faker::Internet.unique.email wasn't always returning unique emails.  By
    combining a factory girl sequence with the faker output we'll get a unique email
    that's also something more realistic than test-user-n@example.com

--- spec/services/query_parser_spec.rb
+++ spec/services/query_parser_spec.rb
@@ -272,8 +272,7 @@ describe QueryParser do
         create(:user,
           username: Faker::Lorem.unique.word,
           first_name: Faker::Name.unique.first_name,
-          last_name: Faker::Name.unique.last_name,
-          email: Faker::Internet.unique.email)
+          last_name: Faker::Name.unique.last_name)
       end
 
       before do
@@ -282,8 +281,7 @@ describe QueryParser do
           create(:user,
             username: Faker::Lorem.unique.word,
             first_name: Faker::Name.unique.first_name,
-            last_name: Faker::Name.unique.last_name,
-            email: Faker::Internet.unique.email)
+            last_name: Faker::Name.unique.last_name)
         end
       end
 

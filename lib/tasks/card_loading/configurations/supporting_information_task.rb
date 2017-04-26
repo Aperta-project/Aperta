fe04db362f324e957db1051e15660106ff5bef71
Aaron Christy <aaron@mutuallyhuman.com>
2017-04-26 09:55:49 -0400

    Create missing Card configuration classes
    Since all answerables need associated to a Card via a CardVersion,
    create an explicit configuration class for each of them.  This should
    also include configuration classes for any test classes that are
    created as part of the rspec tests (InvitableTestTask, MetadataTestTask).

---
+++ lib/tasks/card_loading/configurations/supporting_information_task.rb
@@ -0,0 +1,19 @@
+# This class defines the specific attributes of a particular Card and it can be
+# used to create a new valid Card into the system.  The `content` can be used
+# to create CardContent for the Card.
+#
+module CardConfiguration
+  class SupportingInformationTask
+    def self.name
+      "TahiStandardTasks::SupportingInformationTask"
+    end
+
+    def self.title
+      "Supporting Info"
+    end
+
+    def self.content
+      []
+    end
+  end
+end

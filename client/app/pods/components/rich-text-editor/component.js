6368923634f7f22d0a0c51722a2dee54f49828e7
Jim Haungs <jhaungs@plos.org>
2017-04-26 16:09:13 -0700

    Add headers and preview toolbar icon

--- client/app/pods/components/rich-text-editor/component.js
+++ client/app/pods/components/rich-text-editor/component.js
@@ -8,17 +8,18 @@ export default Ember.Component.extend({
   editorConfigurations: {
     basic: {
       menubar: false,
+      plugins: 'preview',
       statusbar: false,
-      toolbar: 'italic | subscript superscript | undo redo',
+      toolbar: 'italic | subscript superscript | undo redo | preview',
       valid_elements: 'p,br,em/i,sub,sup'
     },
 
     expanded: {
       menubar: false,
       plugins: 'code preview table',
+      block_formats: 'Header 1=h1;Header 2=h2;Header 3=h3;Header 4=h4',
       toolbar: 'bold italic underline | subscript superscript | \
                 bullist numlist table | undo redo | code preview | formatselect',
-      block_formats: 'Header 1=h1;Header 2=h2;Header 3=h3;Header 4=h4',
       valid_elements: 'p,br,strong/b,em/i,u,sub,sup,ol,ul,li,h1,h2,h3,h4,table,thead,tbody,tr,th,td'
     }
   },
@@ -28,8 +29,6 @@ export default Ember.Component.extend({
   editorOptions: Ember.computed('editorStyle', 'editorConfigurations', function() {
     let options = this.get('editorConfigurations');
     let style = this.get('editorStyle') || 'expanded';
-    let hash = options[style];
-    // hash['placeholder'] = this.get('placeholder');
-    return hash;
+    return options[style];
   }),
 });

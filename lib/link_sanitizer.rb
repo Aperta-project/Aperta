c17322d1e2c683b83d948ebdcd411fffeaa99bc0
Edem Kumodzi <ekumodzi@plos.org>
2017-04-27 21:29:17 +0000

    cleanup

--- lib/link_sanitizer.rb
+++ lib/link_sanitizer.rb
@@ -6,7 +6,9 @@ module LinkSanitizer
       link.replace('[' + link.text + ' (' +
         link.attributes['href'].value + ')]')
     end
-    doc.css('table.btn.btn-primary').remove_attr('class')
+    doc.css('table.btn.btn-primary').each do |table|
+      table.attributes['class'].value = 'btn'
+    end
     doc.to_s
   end
 end
--- spec/lib/link_sanitizer_spec.rb
+++ spec/lib/link_sanitizer_spec.rb
@@ -4,7 +4,7 @@ require 'link_sanitizer'
 describe 'LinkSanitizer' do
   it 'sanitizes a text block' do
     text = '<table class="btn btn-primary">hello <a href="http://plos.org">world</a></table>'
-    expected_text = '<table>hello [world (http://plos.org)]</table>'
+    expected_text = '<table class="btn">hello [world (http://plos.org)]</table>'
     expect(LinkSanitizer.sanitize(text)).to include expected_text
     expect(LinkSanitizer.sanitize(text)).to_not include text
   end

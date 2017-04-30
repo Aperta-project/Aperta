cdf970a21f5f7f8b5093f0f920135f5d0870b580
Edem Kumodzi <ekumodzi@plos.org>
2017-04-30 21:16:02 +0000

    cleanup

--- client/app/pods/components/correspondence-list/component.js
+++ client/app/pods/components/correspondence-list/component.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
       return null;
     }
   },
-  baseUrl: () => {
+  baseUrl: Ember.computed(function(){
     let parts = window.location.href.split('/');
     if (window.location.href.includes('viewcorrespondence')){
       return parts.slice(0,parts.length - 2).join('/');
@@ -19,16 +19,16 @@ export default Ember.Component.extend({
     else{
       return window.location.href;
     }
-  },
+  }),
   actions: {
     showCorrespondenceOverlay(message) {
       this.set('showCorrespondenceOverlay', true);
       this.set('message', message);
-      window.history.replaceState({}, null, this.baseUrl() + '/viewcorrespondence/' + message.id);
+      window.history.replaceState({}, null, this.get('baseUrl') + '/viewcorrespondence/' + message.id);
     },
     hideCorrespondenceOverlay() {
       this.set('showCorrespondenceOverlay', false);
-      window.history.replaceState({}, null, this.baseUrl());
+      window.history.replaceState({}, null, this.get('baseUrl'));
     }
   },
   didRender() {

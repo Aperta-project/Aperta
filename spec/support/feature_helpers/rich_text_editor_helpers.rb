00880cb607392eebeee18cb273a587e25445cbcc
Jim Haungs <jhaungs@plos.org>
2017-05-01 16:11:23 -0700

    Test finding tinymce editors

--- spec/support/feature_helpers/rich_text_editor_helpers.rb
+++ spec/support/feature_helpers/rich_text_editor_helpers.rb
@@ -8,7 +8,6 @@ module RichTextEditorHelpers
   def fill_in_rich_text(editor:, with: value)
     id = id_for_editor(editor)
     editor = "tinymce.editors.#{id}"
-    binding.pry
     page.execute_script("#{editor}.setContent('#{value}')")
     page.execute_script("#{editor}.triggerSave()")
   end

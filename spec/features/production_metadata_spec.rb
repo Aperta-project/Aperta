fcb39c5f5d26af29fa7cc8a26d58e68924377dae
Jim Haungs <jhaungs@plos.org>
2017-05-09 08:11:51 -0700

    Fix rich-text helper inclusion

--- spec/features/production_metadata_spec.rb
+++ spec/features/production_metadata_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
+include RichTextEditorHelpers
 
 feature 'Production Metadata Card', js: true do
-  include RichTextEditorHelpers
 
   let(:admin) { create :user, :site_admin, first_name: 'Admin' }
   let(:author) { create :user, first_name: 'Author' }

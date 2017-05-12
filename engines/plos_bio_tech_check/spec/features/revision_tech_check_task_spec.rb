6c0bc6d14c91730b009b0ecdde5b7da085548729
Jim Haungs <jhaungs@plos.org>
2017-05-12 13:29:49 -0700

    Add rich-text editor to Additional Information card

--- engines/plos_bio_tech_check/spec/features/revision_tech_check_task_spec.rb
+++ engines/plos_bio_tech_check/spec/features/revision_tech_check_task_spec.rb
@@ -40,6 +40,7 @@ feature 'Revision Tech Check', js: true do
   scenario "list the unselected question items in the author changes letter" do
     login_as(editor, scope: :user)
     overlay = Page.view_task_overlay(paper, task)
+    wait_for_editors
     overlay.display_letter
     overlay.click_autogenerate_email_button
     text = overlay.letter_text
@@ -59,6 +60,7 @@ feature 'Revision Tech Check', js: true do
   scenario "selected questions don't show up in the auto-generated author change letter" do
     login_as(editor, scope: :user)
     overlay = Page.view_task_overlay(paper, task)
+    wait_for_editors
     overlay.display_letter
     overlay.click_autogenerate_email_button
     text = overlay.letter_text
@@ -76,6 +78,7 @@ feature 'Revision Tech Check', js: true do
   scenario "unchecking a box with no associated text has no effect" do
     login_as(editor, scope: :user)
     overlay = Page.view_task_overlay(paper, task)
+    wait_for_editors
     overlay.display_letter
     overlay.click_autogenerate_email_button
     text_before = overlay.letter_text

d2e6bbb9510243bd35ddb5769860c52727dabb96
Yetunde Sanni <yetunde.sanni@andela.com>
2017-05-15 16:27:09 +0300

    APERTA-8858-updated-ember-integration-specs

--- client/tests/components/paper-downloads-test.js
+++ client/tests/components/paper-downloads-test.js
@@ -86,3 +86,49 @@ test('pdf versions with sources display source and download links', function(ass
   assert.equal(this.$('.download-source').length, 1);
   assert.equal(this.$('.download-pdf').length, 1);
 });
+
+test('pdf papers with no version fileType', function(assert) {
+  const noFileTypeSourcePaperPdf = make('paper', {
+    file: {
+      file_type: 'pdf',
+      pending_url: 'http://pdf-pending-url',
+    },
+    versionedTexts: [{
+      id: 3,
+      isDraft: false,
+      majorVersion: 1,
+      minorVersion: 1,
+      fileType: '',
+    }]
+  });
+  this.set('noFileTypeSourcePaperPdf', noFileTypeSourcePaperPdf);
+  this.render(hbs`
+    {{paper-downloads paper=noFileTypeSourcePaperPdf}}
+  `);
+  const href = this.$('a.download-pdf');
+  assert.equal(href.length, 1, 'download-pdf anchor exists');
+  assert.equal(href.attr('href'), 'http://pdf-pending-url');
+});
+
+test('docx papers with no version fileType', function(assert) {
+  const noFileTypeSourcePaperDoc = make('paper', {
+    file: {
+      file_type: 'docx',
+      pending_url: 'http://doc-pending-url',
+    },
+    versionedTexts: [{
+      id: 4,
+      isDraft: false,
+      majorVersion: 1,
+      minorVersion: 1,
+      fileType: '',
+    }]
+  });
+  this.set('noFileTypeSourcePaperDoc', noFileTypeSourcePaperDoc);
+  this.render(hbs`
+    {{paper-downloads paper=noFileTypeSourcePaperDoc}}
+  `);
+  const href = this.$('a.download-docx');
+  assert.equal(href.length, 1, 'download-docx anchor exists');
+  assert.equal(href.attr('href'), 'http://doc-pending-url');
+});

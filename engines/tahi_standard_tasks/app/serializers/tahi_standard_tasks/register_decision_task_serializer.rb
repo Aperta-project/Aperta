529b7c4a6867d1f8c19e1ce51c386ece41377350
Ben Beckwith <ben@mutuallyhuman.com>
2017-05-18 11:24:03 -0400

    Render the decision letter templates upon serializing
    We have to pass in the manuscript here because the letter templates
    only know about the journal.
    
    APERTA-9857

--- engines/tahi_standard_tasks/app/serializers/tahi_standard_tasks/register_decision_task_serializer.rb
+++ engines/tahi_standard_tasks/app/serializers/tahi_standard_tasks/register_decision_task_serializer.rb
@@ -4,5 +4,11 @@ module TahiStandardTasks
     has_many :letter_templates,
              embed: :ids,
              include: true
+
+    def letter_templates
+      object.letter_templates.map do |t|
+        t.render(manuscript: object.paper)
+      end
+    end
   end
 end

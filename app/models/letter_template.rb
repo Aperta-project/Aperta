a495414134279cb253391bf1150fdae5f65b9a01
Ben Beckwith <ben@mutuallyhuman.com>
2017-05-18 11:22:28 -0400

    Run letter_templates through Liquid
    For the subject, to, and letter fields, try to parse them as Liquid
    templates with the supplied options.
    
    APERTA-9857

--- app/models/letter_template.rb
+++ app/models/letter_template.rb
@@ -3,6 +3,19 @@
 # would be useful.
 class LetterTemplate < ActiveRecord::Base
   belongs_to :journal
+
   validates :letter, presence: true
   validates :subject, presence: true
+
+  def render_attr(attr, options = {})
+    Liquid::Template.parse(attr).render(options)
+  end
+
+  def render(options = {})
+    tap do |my|
+      my.subject = render_attr(subject, options)
+      my.to = render_attr(to, options)
+      my.letter = render_attr(letter, options)
+    end
+  end
 end

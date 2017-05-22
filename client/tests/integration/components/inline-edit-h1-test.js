bb0d2c0f8fdd366c33e4d7f425846ce949431bcd
Michael Ogala <michael.ogala@andela.com>
2017-05-22 23:05:05 +0100

    Refactor tests for inline-edit-h1

--- client/tests/integration/components/inline-edit-h1-test.js
+++ client/tests/integration/components/inline-edit-h1-test.js
@@ -1,31 +1,31 @@
 import hbs from 'htmlbars-inline-precompile';
 import { test, moduleForComponent } from 'ember-qunit';
 
+let template = hbs`{{inline-edit-h1 title=title canManage=canManage setTitle=setTitle}}`;
+const title = 'Adhoc For Staff';
+const sampleText = 'Adhoc For Paper Submission';
+
 moduleForComponent('inline-edit-h1', 'Integration | Component | inline edit h1', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    this.set('title', title);
+    this.set('setTitle', 'setTitle');
+
+    this.render(template);
+  }
 });
 
-let template = hbs`{{inline-edit-h1 title=title canManage=canManage setTitle=setTitle}}`;
 test('cannot edit title when canManage=false', function(assert) {
-  this.set('title', 'Adhoc For Staff');
   this.set('canManage', false);
-  this.set('setTitle', 'setTitle');
 
-  this.render(template);
-
-  assert.equal(this.$('h1').text().trim(), 'Adhoc For Staff', 'displays the correct header');
+  assert.equal(this.$('h1').text().trim(), title, 'displays the correct header');
   assert.equal(this.$('.button-secondary').text().trim(), 'Save', 'displays the save button');
   assert.equal(this.$('.button-link').text().trim(), 'Cancel', 'displays the cancel button');
   assert.elementNotFound('.inline-edit-icon');
 });
 
 test('can edit title when canManage=true', function(assert) {
-  const title = 'Adhoc For Staff';
-  this.set('title', title);
   this.set('canManage', true);
-  this.set('setTitle', 'setTitle');
-
-  this.render(template);
   //check that the edit icon is displayed
   assert.elementFound('.inline-edit-icon');
 
@@ -33,13 +33,10 @@ test('can edit title when canManage=true', function(assert) {
   //check that the edit form is displayed
   assert.elementFound('.inline-edit-form');
   assert.elementFound('.title--test');
+});
 
-  const sampleText = 'Adhoc For Paper Submission';
-  
-  //fill form and click cancel button
-  this.$('.title--test').val(sampleText);
-  this.$('.button-link').click();
-  assert.equal(this.$('h1').text().trim(), title, 'retains title when cancel button is clicked');
+test('clicking save retains a the new title', function(assert) {
+  this.set('canManage', true);
 
   const start = assert.async();
   this.set('setTitle', () => {
@@ -48,7 +45,18 @@ test('can edit title when canManage=true', function(assert) {
     start();
   });
 
-  //fill form again and click save button
+  this.$('.inline-edit-icon').click();
+  //fill form and click save button
   this.$('.title--test').val(sampleText);
   this.$('.button-secondary').click();
+});
+
+test('clicking the cancel button while editing retains the previous title', function(assert) {
+  this.set('canManage', true);
+
+  this.$('.inline-edit-icon').click();
+  //fill form and click cancel button
+  this.$('.title--test').val(sampleText);
+  this.$('.button-link').click();
+  assert.equal(this.$('h1').text().trim(), title, 'retains title when cancel button is clicked');
 });

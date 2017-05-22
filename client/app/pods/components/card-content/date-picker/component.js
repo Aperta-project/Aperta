81777304c71640cdb4e8b4261885a3d0ac1b6372
Andrew Max <anm898989@gmail.com>
2017-05-22 17:55:17 -0500

    improved date picker and xml and date picker template

--- client/app/pods/components/card-content/date-picker/component.js
+++ client/app/pods/components/card-content/date-picker/component.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 import { PropTypes } from 'ember-prop-types';
 
 export default Ember.Component.extend({
-  tagName: '',
-
   propTypes: {
     content: PropTypes.EmberObject.isRequired,
     disabled: PropTypes.bool,

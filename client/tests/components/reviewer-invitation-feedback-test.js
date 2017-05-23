5ae28bfdc7120c95c5041dbfe5531ba987efa956
Jim Haungs <jhaungs@plos.org>
2017-05-23 16:16:26 -0700

    Get rest of reviewer invitation feedback tests working

--- client/tests/components/reviewer-invitation-feedback-test.js
+++ client/tests/components/reviewer-invitation-feedback-test.js
@@ -1,63 +1,58 @@
 import Ember from 'ember';
 import {moduleForComponent, test} from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import {setRichText} from 'tahi/tests/helpers/rich-text-editor-helpers';
 
-moduleForComponent('reviewer-invitation-feedback',
-                   'Integration | Component | reviewer invitation feedback',
-                   {integration: true,
-                    beforeEach: function() {
-                      this.set('decline', () => {return;});
-                      this.set('invitation', Ember.Object.create({
-                        title: 'Awesome Paper!',
-                        declineReason: null,
-                        reviewerSuggestions: null
-                      }));
-                    }});
+moduleForComponent(
+  'reviewer-invitation-feedback',
+  'Integration | Component | reviewer invitation feedback',
+  {integration: true,
+    beforeEach: function() {
+      this.set('decline', () => {return;});
+      this.set('invitation', Ember.Object.create({
+        title: 'Awesome Paper!',
+        declineReason: null,
+        reviewerSuggestions: null
+      }));
+    }});
 
 let template = hbs`{{reviewer-invitation-feedback
                       invitation=invitation
                       decline=(action decline invitation)
                       }}`;
 
-let fillText = function(context, selector, text) {
-  context.$(selector).val(text);
-  context.$(selector).change();
+let setThenTest = function (context, field, text, assertion) {
+  context.render(template);
+  setRichText(field, text);
+  Ember.run.next(assertion);
 };
 
 test('displays paper title', function(assert){
   assert.expect(1);
-
   this.render(template);
   assert.textPresent('.feedback-invitation-title', 'Awesome Paper!');
 });
 
-test('can set decline reason', function(assert){
-  assert.expect(1);
-
-  this.render(template);
-  fillText(this, 'textarea[name="declineReason"]', 'Too busy!');
-
-  assert.equal(this.get('invitation.declineReason'), 'Too busy!');
+test('can set decline reason', function(assert) {
+  let text = 'Too busy!';
+  setThenTest(this, 'declineReason', text, () => {
+    assert.equal(this.get('invitation.declineReason'), `<p>${text}</p>`);
+  });
 });
 
 test('can set reviewer suggestions', function(assert){
-  assert.expect(1);
-
+  let text = 'Other guy is great';
   this.render(template);
-  fillText(this, 'textarea[name="reviewerSuggestions"]', 'Other guy is great');
-
-  assert.equal(
-    this.get('invitation.reviewerSuggestions'),
-    'Other guy is great'
-  );
+  setThenTest(this, 'reviewerSuggestions', () => {
+    assert.equal(this.get('invitation.reviewerSuggestions'), `<p>${text}</p>`);
+  });
 });
 
 test('The form is constructed with the expected markup', function(assert){
-  assert.expect(6);
+  assert.expect(4);
 
   this.render(template);
 
-  assert.selectorHasClasses('label>textarea', ['feedback-textarea']);
   assert.selectorHasClasses(
     '.reviewer-feedback-buttons > .reviewer-decline-feedback',
     ['button-link', 'button--green']
@@ -72,7 +67,7 @@ test('can respond "no thank you" to giving feedback', function(assert){
   assert.expect(2);
 
   this.set('invitation.declineFeedback', function(){
-    assert.ok(true, 'declineFeedback is called on invitation')
+    assert.ok(true, 'declineFeedback is called on invitation');
   });
 
   this.set('decline', () => {
@@ -86,24 +81,26 @@ test('can respond "no thank you" to giving feedback', function(assert){
 test('can Send Feedback', function(assert){
   assert.expect(3);
   this.set('decline', (invitation) => {
-    assert.ok(this.get('invitation')===invitation,
+    assert.ok(this.get('invitation') === invitation,
       'The invitation object is passed in to the action');
 
     // assert the values are set on the invitation
-    assert.equal(invitation.get('declineReason'),
-                'some value',
-                'Expected decline reason to be our value'
-    );
-    assert.equal(invitation.get('reviewerSuggestions'),
-                'some other value',
-                'Expected decline reason to be our other value'
-    );
+    Ember.run.next(() => {
+      assert.equal(invitation.get('declineReason'),
+                  '<p>some value</p>',
+                  'Expected decline reason to be our value'
+      );
+      assert.equal(invitation.get('reviewerSuggestions'),
+                  '<p>some other value</p>',
+                  'Expected decline reason to be our other value'
+      );
+    });
   });
 
   this.render(template);
 
-  fillText(this, 'textarea[name="declineReason"]', 'some value');
-  fillText(this, 'textarea[name="reviewerSuggestions"]', 'some other value');
+  setRichText('declineReason', 'some value');
+  setRichText('reviewerSuggestions', 'some other value');
 
   this.$('.reviewer-feedback-buttons > .reviewer-send-feedback').click();
 });

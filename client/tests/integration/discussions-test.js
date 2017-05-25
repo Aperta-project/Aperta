2df887b61f10343c64a0df81021372bf86ab6597
Ben Beckwith <ben@mutuallyhuman.com>
2017-05-25 16:02:53 -0400

    Integration tests for /discussions compoenents
    This leverage the existing tests, but for the top-level route.
    
    APERTA-8540

--- client/tests/integration/discussions-test.js
+++ client/tests/integration/discussions-test.js
@@ -84,10 +84,37 @@ test('can see a list of topics', function(assert) {
   });
 });
 
+test('can see a list of topics for /discussions', function(assert) {
+  Ember.run(function() {
+    mockFind('discussion-topic').returns({ model: topic });
+
+    visit('/discussions/' + paper.id);
+
+    andThen(function() {
+      const firstTopic = find('.discussions-index-topic:first');
+      assert.ok(firstTopic.length, 'Topic is found: ' + firstTopic.text());
+    });
+  });
+});
+
 test('cannot create discussion without title', function(assert) {
   Ember.run(function() {
     mockFind('discussion-topic').returns({ model: topic });
     visit('/papers/' + paper.id + '/workflow/discussions/new');
+    click('#create-topic-button');
+
+    andThen(function() {
+      const titleFieldContainer = find('#topic-title-field').parent();
+      assert.ok(titleFieldContainer.hasClass('error'), 'Error is displayed');
+    });
+  });
+});
+
+test('cannot create discussion without title for /discussions', function(assert) {
+  Ember.run(function() {
+    mockFind('discussion-topic').returns({ model: topic });
+
+    visit('/discussions/' + paper.id + '/new');
     click('#create-topic-button');
 
     andThen(function() {
@@ -111,9 +138,45 @@ test('can see a non-editable topic with view permissions', function(assert) {
   });
 });
 
+test('can see a non-editable topic with view permissions for /discussions', function(assert) {
+  Factory.createPermission('DiscussionTopic', 1, ['view']);
+
+  Ember.run(function() {
+    mockFind('discussion-topic').returns({ model: topic });
+
+    visit('/discussions/' + paper.id + '/' + topic.get('id'));
+    andThen(function() {
+      const titleText = find('.discussions-show-title').text().trim();
+      assert.equal(titleText, 'Hipster Ipsum Dolor', 'Topic title is found: ' + titleText);
+    });
+
+  });
+});
+
 test('can reply to a topic with view permissions', function(assert) {
   const replyText = 'test';
   const topicScreen = '/papers/' + paper.id + '/workflow/discussions/' + topic.get('id');
+
+  Factory.createPermission('DiscussionTopic', 1, ['view']);
+  mockFind('discussion-topic').returns({ model: topic });
+  mockCreate('discussion-reply').returns({ body: replyText });
+
+  visit(topicScreen).then(function() {
+    triggerEvent(find('.new-comment-field'), 'focus').then(function() {
+      find('.new-comment-field').val(replyText).trigger('change');
+      return triggerEvent(find('.new-comment-submit-button'), 'click');
+    });
+  });
+
+  andThen(function() {
+    const text = $('.message-comment:last .comment-body').text();
+    assert.equal(text, replyText, 'Reply is found');
+  });
+});
+
+test('can reply to a topic with view permissions for /discussions', function(assert) {
+  const replyText = 'test';
+  const topicScreen = '/discussions/' + paper.id + '/' + topic.get('id');
 
   Factory.createPermission('DiscussionTopic', 1, ['view']);
   mockFind('discussion-topic').returns({ model: topic });
@@ -151,6 +214,25 @@ test('reply is cached if unsaved', function(assert) {
   });
 });
 
+test('reply is cached if unsaved for /discussions', function(assert) {
+  const topicScreen = '/discussions/' + paper.id + '/' + topic.get('id');
+  const replyText = 'test';
+
+  Factory.createPermission('DiscussionTopic', 1, ['view']);
+  mockFind('discussion-topic').returns({ model: topic });
+
+  visit(topicScreen).then(function() {
+    find('.new-comment-field').val(replyText).trigger('keyup');
+    find('.sheet-toolbar-button').click();
+  });
+
+  visit(topicScreen);
+
+  andThen(function() {
+    assert.equal(find('.new-comment-field').val(), replyText, 'Text cached');
+  });
+});
+
 test('comment body line returns converted to break tags', function(assert) {
   Factory.createPermission('DiscussionTopic', 1, ['view']);
 
@@ -165,12 +247,41 @@ test('comment body line returns converted to break tags', function(assert) {
   });
 });
 
+test('comment body line returns converted to break tags for /discussions', function(assert) {
+  Factory.createPermission('DiscussionTopic', 1, ['view']);
+
+  Ember.run(function() {
+    mockFind('discussion-topic').returns({ model: topic });
+    visit('/discussions/' + paper.id + '/' + topic.get('id'));
+
+    andThen(function() {
+      const replyText = find('.comment-body:first').html();
+      assert.equal(replyText, 'hey<br>how are you?', 'break tags found');
+    });
+
+  });
+});
+
 test('can see an editable topic with edit permissions', function(assert) {
   Factory.createPermission('DiscussionTopic', 1, ['view', 'edit']);
 
   Ember.run(function() {
     mockFind('discussion-topic').returns({ model: topic });
     visit('/papers/' + paper.id + '/workflow/discussions/' + topic.get('id'));
+
+    andThen(function() {
+      const titleText = find('.discussions-show-title input').val();
+      assert.equal(titleText, 'Hipster Ipsum Dolor', 'Topic title is found: ' + titleText);
+    });
+  });
+});
+
+test('can see an editable topic with edit permissions for /discussions', function(assert) {
+  Factory.createPermission('DiscussionTopic', 1, ['view', 'edit']);
+
+  Ember.run(function() {
+    mockFind('discussion-topic').returns({ model: topic });
+    visit('/discussions/' + paper.id + '/' + topic.get('id'));
 
     andThen(function() {
       const titleText = find('.discussions-show-title input').val();
@@ -197,6 +308,41 @@ test('cannot persist empty title', function(assert) {
       triggerEvent(titleField, 'blur').then(()=> {
         assert.ok(titleFieldContainer.hasClass('error'), 'Error is displayed on title');
       });
+    });
+  });
+});
+
+test('cannot persist empty title for /discussions', function(assert) {
+  Factory.createPermission('DiscussionTopic', 1, ['view', 'edit']);
+
+  Ember.run(function() {
+    mockFind('discussion-topic').returns({ model: topic });
+    visit('/discussions/' + paper.id + '/' + topic.get('id'));
+
+    andThen(function() {
+      const titleField = find('.discussions-show-title input');
+      const titleFieldContainer = find('.discussions-show-title');
+
+      titleField.focus();
+      titleField.val('');
+      titleField.blur();
+
+      triggerEvent(titleField, 'blur').then(()=> {
+        assert.ok(titleFieldContainer.hasClass('error'), 'Error is displayed on title');
+      });
+    });
+  });
+});
+
+test('pops out discussions', function(assert){
+  Ember.run(function() {
+    mockFind('discussion-topic').returns({ model: topic });
+    
+    visit('/papers/' + paper.id + '/workflow/discussions/');
+    
+    andThen(function() {
+      const firstTopic = find('.discussions-index-topic:first');
+      assert.ok(firstTopic.length, 'Topic is found: ' + firstTopic.text());
     });
   });
 });

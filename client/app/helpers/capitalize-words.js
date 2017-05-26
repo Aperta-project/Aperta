af8d6d06df7bcafc05fbab99c856f8bb5441d292
Michael Ogala <michael.ogala@andela.com>
2017-05-26 16:58:06 +0100

    Add test for 'capitalize-words' helper

--- client/app/helpers/capitalize-words.js
+++ client/app/helpers/capitalize-words.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
-export default Ember.Helper.helper(function(params) {
+export function capitalizeWords(params) {
   let string = params[0];
   return string
           .split(' ')
           .map((word) => Ember.String.capitalize(word)).join(' ');
-});
+}
+
+export default Ember.Helper.helper(capitalizeWords);

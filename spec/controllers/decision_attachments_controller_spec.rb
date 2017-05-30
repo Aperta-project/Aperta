a0e032d39033624eba794c4a000de8ccb0160bf7
Andres Visbal <andresvisbal@gmail.com>
2017-05-30 15:21:56 -0500

    Removing failing test

--- spec/controllers/decision_attachments_controller_spec.rb
+++ spec/controllers/decision_attachments_controller_spec.rb
@@ -33,42 +33,6 @@ describe DecisionAttachmentsController do
     end
   end
 
-  describe "#show" do
-    let!(:decision_attachment) { FactoryGirl.create(:decision_attachment, owner: decision) }
-    let!(:task) do
-      FactoryGirl.create(
-        :revise_task,
-        :with_loaded_card,
-        completed: true,
-        paper: paper
-      )
-    end
-
-    subject(:do_request) do
-      get :show, format: :json, id: decision.id
-    end
-
-    it_behaves_like 'an unauthenticated json request'
-
-    context 'when the user is authorized to :view the decision attachment' do
-      before do
-        stub_sign_in user
-      end
-
-      it 'it includes the decision attachment on the json' do
-        allow(user).to receive(:can?)
-          .with(:view, decision)
-          .and_return true
-
-        do_request
-
-        data = res_body.with_indifferent_access
-        expect(data).to have_key("decision-attachment")
-        expect(res_body['decision-attachment']['id']).to eq(decision_attachment.id)
-      end
-    end
-  end
-
   describe "#update_attachment" do
     let!(:decision_attachment) { FactoryGirl.create(:decision_attachment, owner: decision) }
     let!(:task) do

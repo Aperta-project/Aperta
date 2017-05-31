dfc36c7ffaea0d5026842400822cf95f69fd8612
Chris Westra <cwestra@gmail.com>
2017-05-31 10:11:05 -0400

    APERTA-9951 create automated similarity check
    This is the actual "work" of APERTA-9951.  When a paper is submitted or
    resubmitted, a new event listener will check to see if the paper has a
    SimilarityCheck task, and will then check the task's TaskTemplate to see if and
    when an automated similarity check is supposed to run.

---
+++ app/services/automated_similarity_check.rb
@@ -0,0 +1,84 @@
+# Service class that automates similarity checks
+class AutomatedSimilarityCheck
+  attr_reader :paper, :previous_paper_state
+
+  def initialize(paper, previous_paper_state)
+    @paper = paper
+    @previous_paper_state = previous_paper_state
+  end
+
+  # I could use a hash of procs for this, but folks will probably
+  # think it's way more weird than a case statement
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def should_run?
+    return false unless setting
+    case setting.value
+    when 'off'
+      false
+    when 'at_first_full_submission'
+      submitted_after_first_full_submission?
+    when 'after_any_first_revise_decision'
+      submitted_after_any_first_revise_decision?
+    when 'after_first_minor_revise_decision'
+      submitted_after_first_minor_revise_decision?
+    when 'after_first_major_revise_decision'
+      submitted_after_first_major_revise_decision?
+    end
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
+
+  def setting
+    @setting ||=
+      begin
+        # does the paper have a similarity check task?
+        check_task = paper.tasks.find_by(
+          type: "TahiStandardTasks::SimilarityCheckTask"
+        )
+
+        return nil unless check_task && check_task.task_template
+        check_task.task_template.setting('ithenticate_automation')
+      end
+  end
+
+  def run
+    if should_run?
+      similarity_check = SimilarityCheck.create!(
+        versioned_text: paper.latest_submitted_version
+      )
+      Rails.logger.info <<-HERE
+        Performing automated similarity check
+        #{similarity_check.id} for paper #{paper.id}. Checks are set to run #{setting.value}
+      HERE
+      similarity_check.start_report_async
+      similarity_check
+    end
+  end
+
+  def submitted_after_first_full_submission?
+    ['unsubmitted', 'invited_for_full_submission'].include? previous_paper_state
+  end
+
+  def previous_verdict
+    paper.last_completed_decision.verdict
+  end
+
+  def first_revision?
+    paper.decisions.completed.revisions.count == 1 &&
+      previous_paper_state == 'in_revision'
+  end
+
+  def submitted_after_first_major_revise_decision?
+    first_revision? &&
+      previous_verdict == 'major_revision'
+  end
+
+  def submitted_after_first_minor_revise_decision?
+    first_revision? &&
+      previous_verdict == 'minor_revision'
+  end
+
+  def submitted_after_any_first_revise_decision?
+    first_revision? &&
+      ['minor_revision', 'major_revision'].include?(previous_verdict)
+  end
+end

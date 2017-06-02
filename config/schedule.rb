a31d78c9232a9d027f1ec8e8cbc46e77d29246e5
Ben Miller <bmiller@plos.org>
2017-06-02 16:09:33 -0700

    Move plos billing into sidekiq schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,7 +7,6 @@ job_type :rake, "cd :path && chruby-exec #{RUBY_VERSION} -- "\
                 "bundle exec dotenv -f env rake :task --silent :output"
 
 every :day, at: '00:01' do
-  rake 'plos_billing:daily_billing_log_export'
   rake 'clean:temp_files'
   rake 'reports:analyze_attachments:send_email[apertadevteam@plos.org]'
 end

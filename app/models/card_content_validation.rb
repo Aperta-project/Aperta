8349057983a8b5e0373a061002f0f6834be24849
Edem Kumodzi <ekumodzi@plos.org>
2017-06-06 15:53:35 -0700

    string length validations

--- app/models/card_content_validation.rb
+++ app/models/card_content_validation.rb
@@ -1,6 +1,7 @@
 # This class handles concerns about validations of Answers tied to
 # Card Content
 class CardContentValidation < ActiveRecord::Base
+  acts_as_paranoid
   belongs_to :card_content
 
   def validate_answer(answer)
@@ -13,5 +14,15 @@ class CardContentValidation < ActiveRecord::Base
     regex = Regexp.new(validator)
     result = answer.value =~ regex
     result.present?
+  end
+
+  def validate_by_string_length_minimum(answer)
+    return false if validator !~ /^[0-9]+$/
+    answer.value.length >= validator.to_i
+  end
+
+  def validate_by_string_length_maximum(answer)
+    return false if validator !~ /^[0-9]+$/
+    answer.value.length <= validator.to_i
   end
 end

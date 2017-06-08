6a158e0e1bb4cee546a8005b973f3aabbe0081cb
Andrew Max <anm898989@gmail.com>
2017-06-08 18:10:58 -0500

    moves client side readyable to mixin

--- client/app/models/answer.js
+++ client/app/models/answer.js
@@ -1,22 +1,17 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import Readyable from 'tahi/mixins/models/readyable';
 
-export default DS.Model.extend({
+export default DS.Model.extend(Readyable, {
   additionalData: DS.attr(),
   value: DS.attr(),
 
   attachments: DS.hasMany('question-attachment', { async: false }),
   cardContent: DS.belongsTo('card-content', { async: false }),
-  ready: DS.attr('boolean'),
-  readyIssues: DS.attr(),
   owner: DS.belongsTo('answerable', { async: false, polymorphic: true }),
   paper: DS.belongsTo('paper'), //TODO APERTA-8972 consider removing from client
 
   wasAnswered: Ember.computed('value', function(){
     return Ember.isPresent(this.get('value'));
-  }),
-
-  readyIssuesArray: Ember.computed('readyIssues.[]', function(){
-    return this.getWithDefault('readyIssues', []);
   })
 });
--- client/app/models/question-attachment.js
+++ client/app/models/question-attachment.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
+import Readyable from 'tahi/mixins/models/readyable';
 
-export default DS.Model.extend({
+export default DS.Model.extend(Readyable, {
   answer: DS.belongsTo('answer', { async: false }),
   nestedQuestionAnswer: DS.belongsTo('nested-question-answer', { async: false }),
 
@@ -8,11 +9,5 @@ export default DS.Model.extend({
   src: DS.attr('string'),
   status: DS.attr('string'),
   title: DS.attr('string'),
-  caption: DS.attr('string'),
-  ready: DS.attr('boolean'),
-  readyIssues: DS.attr(),
-
-  readyIssuesArray: Ember.computed('readyIssues.[]', function(){
-    return this.getWithDefault('readyIssues', []);
-  })
+  caption: DS.attr('string')
 });

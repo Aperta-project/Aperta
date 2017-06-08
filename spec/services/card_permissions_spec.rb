c307080e03b291713fd2df5c1b5a3dd89ce7626c
Erik Hetzner <ehetzner@plos.org>
2017-06-08 14:45:44 -0700

    APERTA-9473 Add tests to CardPermissions service

---
+++ spec/services/card_permissions_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+describe CardPermissions do
+  let(:journal) { FactoryGirl.create(:journal) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:card) { FactoryGirl.create(:card, journal: journal) }
+  let(:role) { FactoryGirl.create(:role, journal: journal, name: Faker::Name.title) }
+  let(:query) { { action: 'eat', applies_to: 'Task', filter_by_card_id: card.id } }
+
+  shared_examples_for "permission creator" do
+    it "should create 4 permissions" do
+      expect do
+        subject
+      end.to change { Permission.all.count }.from(0).to(4)
+    end
+
+    it "should create a new permission with a wildcard state" do
+      subject
+      expect(role.permissions.find_by(query)).to be
+      expect(role.permissions.find_by(query).applies_to).to eq('Task')
+    end
+
+    it 'should create a view permission on the CardVersion' do
+      subject
+      expect(role.permissions.where(action: 'view', applies_to: 'CardVersion', filter_by_card_id: card.id).count).to be(1)
+    end
+  end
+
+  describe ".add_roles" do
+    subject { CardPermissions.add_roles(card, "eat", [role]) }
+
+    it_should_behave_like "permission creator"
+
+    context 'when the permission already exists' do
+      let!(:permission) do
+        Permission.ensure_exists(
+          'eat',
+          applies_to: 'Task',
+          role: role,
+          states: [Permission::WILDCARD],
+          filter_by_card_id: card.id
+        )
+      end
+      let(:new_role) { FactoryGirl.create(:role, journal: journal, name: Faker::Name.title) }
+
+      it 'adds the new role' do
+        CardPermissions.add_roles(card, "eat", [new_role])
+        expect(role.reload.permissions.reload.where(query).count).to be(1)
+        expect(new_role.permissions.reload.where(query).count).to be(1)
+      end
+    end
+  end
+
+  describe '.set_roles' do
+    subject { CardPermissions.set_roles(card, "eat", [role]) }
+
+    it_should_behave_like "permission creator"
+
+    context 'when the permission already exists' do
+      let!(:permission) do
+        Permission.ensure_exists(
+          'eat',
+          applies_to: 'Task',
+          role: role,
+          states: [Permission::WILDCARD]
+        )
+      end
+      let(:new_role) { FactoryGirl.create(:role, journal: journal, name: Faker::Name.title) }
+
+      it 'adds the new role and removes the old' do
+        CardPermissions.set_roles(card, "eat", [new_role])
+        expect(role.permissions.where(query).count).to be(0)
+        expect(new_role.permissions.where(query).count).to be(1)
+      end
+    end
+  end
+end

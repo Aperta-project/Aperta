9181f2100be7174559ecf2f2f45f3486dbe74f3f
Brandon Zylstra <brandon@mutuallyhuman.com>
2017-06-14 16:04:33 -0400

    add date-picker-link component

---
+++ client/app/pods/components/date-picker-link/component.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'a',
+  classNames: ['date-picker-link'],
+  date: null,
+  calendarIcon: 'fa-calendar',
+  linkText: null,
+
+  _setup: Ember.on('didInsertElement', function() {
+    let $picker = this.$().datepicker({
+      autoclose: true,
+      startDate: this.get('startDate'),
+    });
+
+    $picker.on('changeDate', (event)=> {
+      this.sendAction('dateChanged', event.date);
+    });
+  }),
+});

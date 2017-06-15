21995717bedf181ad1d3f37134c341bc414669a7
Owajigbanam Ogbuluijah <owajigbanam.ogbuluijah@andela.com>
2017-06-15 01:36:31 +0100

    APERTA-9047 Form fields mirror letters.
    The fields in this form mirror what is seen in letters. Currently, only
    one attachment can be sent alongside.

--- client/app/pods/components/correspondence-form/component.js
+++ client/app/pods/components/correspondence-form/component.js
@@ -1,5 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  attachment: null,
   close: null,
+  doneUploading: false,
+  isUploading: false,
+  actions: {
+    removeAttachment() {
+      this.setProperties({
+        doneUploading: false,
+        attachment: null
+      });
+    },
+
+    uploadStarted() {
+      this.set('isUploading', true);
+    },
+
+    uploadFinished(_data, _filename) {
+      this.setProperties({
+        isUploading: false,
+        doneUploading: true,
+        attachment: {
+          data: _data,
+          filename: _filename
+        }
+      });
+    },
+    submit() {}
+  }
 });

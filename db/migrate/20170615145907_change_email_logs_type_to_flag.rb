b131b47b109c075f1aecda4ff29e2189cfc16d45
Owajigbanam Ogbuluijah <owajigbanam.ogbuluijah@andela.com>
2017-06-15 22:01:41 +0100

    APERTA-9047 Fixed migration issue + bug
    Correspondence Attachment now finds the correspondence accordingly

--- db/migrate/20170615145907_change_email_logs_type_to_flag.rb
+++
@@ -1,6 +0,0 @@
-class ChangeEmailLogsTypeToFlag < ActiveRecord::Migration
-  def change
-    remove_column :email_logs, :type, :string
-    add_column    :email_logs, :external, :boolean, default: false
-  end
-end

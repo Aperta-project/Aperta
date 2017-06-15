b5dc5c8f2b189601e86ea6469a66eb93d935726e
Owajigbanam Ogbuluijah <owajigbanam.ogbuluijah@andela.com>
2017-06-15 15:55:46 +0100

    Correspondence list gets sorted by entered time
    No refresh is needed to make the newly added correspondence join the
    list of correspondences.

--- client/app/models/correspondence.js
+++ client/app/models/correspondence.js
@@ -4,7 +4,9 @@ export default DS.Model.extend({
   paper: DS.belongsTo('paper', { async: false }),
   date: DS.attr('string'),
   subject: DS.attr('string'),
+  recipients: DS.attr('string'),
   recipient: DS.attr('string'),
   sender: DS.attr('string'),
-  body: DS.attr('string')
+  body: DS.attr('string'),
+  sentAt: DS.attr('string')
 });

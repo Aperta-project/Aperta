e459230ecff4d234d75e782cab4807db43f4cc74
Shawn Gatchell <shawng03@gmail.com>
2017-06-15 10:33:41 -0700

    route support for All My Journals

--- client/app/pods/admin/journals/route.js
+++ client/app/pods/admin/journals/route.js
@@ -1,16 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
+  model(params) {
     return this.store.findAll('admin-journal').then((journals) => {
       return {
         journals: journals,
-        journal: journals.get('firstObject')
+        journal: journals.find(j => j.id === params.journal_id)
       };
     });
   },
 
   afterModel(model) {
-    this.transitionTo('admin.journal.workflows', model.journal.id);
+    //
+    // for users with only one journal, transition to a route that
+    // specifies a specific journal rather than behaving like
+    // 'all journals'.
+    //
+    var defaultJournalID = 'all';
+    if (!model.journal && model.journals.get('length') === 1) {
+      defaultJournalID = model.journals.get('firstObject.id');
+    }
+    this.transitionTo('admin.journal.workflows', defaultJournalID);
   }
 });

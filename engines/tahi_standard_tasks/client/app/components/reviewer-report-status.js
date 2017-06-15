a1900d8d4e9260ea944e40b6a4e747f7f0acd21e
Brandon Zylstra <brandon@mutuallyhuman.com>
2017-06-15 18:02:44 -0400

    use zone without moment-timezone

--- engines/tahi_standard_tasks/client/app/components/reviewer-report-status.js
+++ engines/tahi_standard_tasks/client/app/components/reviewer-report-status.js
@@ -42,7 +42,7 @@ export default Ember.Component.extend({
 
   reviewDueAt: Ember.computed('report.dueAt', function(){
     const date = this.get('report.dueAt');
-    const format = 'MMMM D [at] hA';
+    const format = 'MMMM D, ha z';
     return moment(date).format(format);
   }),
 

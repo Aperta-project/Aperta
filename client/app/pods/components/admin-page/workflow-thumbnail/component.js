163f6325bceea1ee25c7133a5ada2953a23f53ca
Shawn Gatchell <shawng03@gmail.com>
2017-06-19 21:45:39 -0700

    Clicking trash shows confirmation

--- client/app/pods/components/admin-page/workflow-thumbnail/component.js
+++ client/app/pods/components/admin-page/workflow-thumbnail/component.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   workflow: {},
 
-  classNames: []
+  classNames: [],
+  confirmDestroy: false,
+
+  actions: {
+    toggleConfirmDestroy() {
+      this.toggleProperty('confirmDestroy');
+    },
+
+    destroyTemplate() {
+      // this.attrs.destroyTemplate(this.get('model'));
+    }
+  }
 });

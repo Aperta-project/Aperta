f7451b91929985927107c208f121f98295fe234e
Owajigbanam Ogbuluijah <owajigbanam.ogbuluijah@andela.com>
2017-06-23 17:54:45 +0100

    APERTA-10197 Removed permission_requirements
    For some reason, this table which was removed a long time ago reappeared
    in the schema. This commit re-removes it. This is per [this
    comment](https://github.com/Tahi-project/tahi/pull/3144#discussion_r123647293)
    on the PR

--- db/schema.rb
+++ db/schema.rb
@@ -564,16 +564,6 @@ ActiveRecord::Schema.define(version: 20170615205634) do
   add_index "papers", ["short_doi"], name: "index_papers_on_short_doi", unique: true, using: :btree
   add_index "papers", ["user_id"], name: "index_papers_on_user_id", using: :btree
 
-  create_table "permission_requirements", force: :cascade do |t|
-    t.integer  "permission_id"
-    t.integer  "required_on_id"
-    t.string   "required_on_type"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  add_index "permission_requirements", ["permission_id", "required_on_id", "required_on_type"], name: "permission_requirements_uniq_idx", unique: true, using: :btree
-
   create_table "permission_states", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false

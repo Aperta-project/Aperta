f61c8435479612309e0460cbfe3439af30e6b4f5
Andres Visbal <andresvisbal@gmail.com>
2017-06-28 09:33:13 -0500

    Fixing duplicate call

--- engines/tahi_standard_tasks/client/app/components/title-and-abstract-task.js
+++ engines/tahi_standard_tasks/client/app/components/title-and-abstract-task.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import TaskComponent from 'tahi/pods/components/task-base/component';
-import Ember from 'ember';
 
 const taskValidations = {
   'paperTitle': ['presence'],

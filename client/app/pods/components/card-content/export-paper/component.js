ba1f8558c38777014541d3f81f473bac92538a82
Tad Park <tadp@users.noreply.github.com>
2017-06-30 17:56:50 -0700

    APERTA-10344 Update styles to new export-history styles
    This incorporates the work of APERTA-10344 into the export-paper
    component.

--- client/app/pods/components/card-content/export-paper/component.js
+++ client/app/pods/components/card-content/export-paper/component.js
@@ -3,14 +3,14 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
   task: Ember.computed.reads('owner'),
+  destination: Ember.computed.reads('content.text'),
   actions: {
     sendToApex: function() {
       const apexDelivery = this.get('store').createRecord('apex-delivery', {
         task: this.get('task'),
-        destination: this.get('content.text')
+        destination: this.get('destination')
       });
       apexDelivery.save();
     }
   }
 });
-

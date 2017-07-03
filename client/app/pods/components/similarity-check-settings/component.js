96eb17baa508f97c173dbcb6cae4ab338c64b33f
Edem Kumodzi <ekumodzi@plos.org>
2017-07-03 11:30:24 -0400

    refactoring similarity check settings

--- client/app/pods/components/similarity-check-settings/component.js
+++ client/app/pods/components/similarity-check-settings/component.js
@@ -21,12 +21,8 @@ export default Ember.Component.extend({
   ],
 
   initialSetting: Ember.computed('initialSetting', function() {
-    /*var setting = this.get('taskToConfigure.settings')
-      .filter((x) => {
-        return x.name === 'ithenticate_automation';
-      });*/
     let setting = this.get('taskToConfigure.settings').findBy('name', 'ithenticate_automation');
-    return setting.length > 0 ? setting[0].value : null;
+    return setting === undefined ? null : setting.value;
   }),
 
   submissionOption: Ember.computed('initialSetting', function() {
@@ -36,7 +32,7 @@ export default Ember.Component.extend({
   selectedOption: Ember.computed('initialSetting', function() {
     let setting = this.get('initialSetting');
     if (this.get('selectableOptions').map((x) => x.id).includes(setting)){
-      return this.get('selectableOptions').findBy('id', setting)[0];
+      return this.get('selectableOptions').findBy('id', setting);
     }
     else{
       return this.get('selectableOptions')[0];

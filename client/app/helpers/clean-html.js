068984431b18bff7b1563332aa29cabe91169df1
Owajigbanam Ogbuluijah <owajigbanam.ogbuluijah@andela.com>
2017-07-05 12:38:33 +0100

    APERTA-10600 changed clean-html to space-out-break
    The original solution tended to rewrite a library `strip-html`. This new
    approach focuses on the breaks only. The integrated solution in uses the
    `strip-html` helper with the `space-out-breaks` helper to achieve the AC

--- client/app/helpers/clean-html.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import cleanHtml from 'tahi/lib/clean-html';
-
-export default Ember.Helper.helper(function(params) {
-  return cleanHtml(params[0]);
-});
--- client/app/lib/clean-html.js
+++
@@ -1,8 +0,0 @@
-export default function (string) {
-  // This function is intended to make HTML markup into a line. It plans
-  // to achieve this by doing two things.
-  //
-  // 1. Replace carriage-returns with spaces
-  // 2. Replace HTML tags with a space
-  return string.replace(/<\/?[^>]+(>|$)/g, ' ').replace(/[\n\r]/g, '');
-}

2958a0feb1d8cde6aa5420170c63eb92e217d2fa
Chris Westra <cwestra@gmail.com>
2017-07-06 10:22:05 -0400

    APERTA-9958 manual/automatic similarity checks

--- engines/tahi_standard_tasks/app/serializers/tahi_standard_tasks/similarity_check_task_serializer.rb
+++ engines/tahi_standard_tasks/app/serializers/tahi_standard_tasks/similarity_check_task_serializer.rb
@@ -1,4 +1,10 @@
 module TahiStandardTasks
   class SimilarityCheckTaskSerializer < ::TaskSerializer
+    attributes :current_setting_value
+
+    def current_setting_value
+      return nil unless object.task_template
+      object.task_template.setting('ithenticate_automation').try(:value)
+    end
   end
 end

a330bd9d31ce58f2bf9b6a44f70573539e783ad4
John Fesenko <jfesenko@plos.org>
2017-07-06 19:33:45 -0700

    APERTA-10244: qunit test

---
+++ client/tests/integration/pods/components/card-content/tabbed-textarea/component-test.js
@@ -0,0 +1,58 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import registerCustomAssertions from 'tahi/tests/helpers/custom-assertions';
+
+moduleForComponent(
+  'card-content/tabbed-textarea',
+  'Integration | Component | card content | tabbed textarea',
+  {
+    integration: true,
+    beforeEach() {
+      registerCustomAssertions();
+      this.set('actionStub', function() {});
+    }
+  }
+);
+
+let annotationTemplate = hbs`{{card-content/tabbed-textarea
+          annotationChanged=(action actionStub)
+          show_annotation=true
+          annotation='annotation test'}}`;
+
+test(`it can render annotation text alone`, function(assert) {
+  this.render(annotationTemplate);
+  assert.elementFound('.annotation-text');
+  assert.elementNotFound('.instruction-text');
+});
+
+let instructionTemplate = hbs`{{card-content/tabbed-textarea
+          annotationChanged=(action actionStub)
+          show_annotation=false
+          instruction_text='instruction test'}}`;
+
+test(`it can render instruction text alone`, function(assert) {
+  this.render(instructionTemplate);
+  assert.textPresent('.instruction-text', 'instruction test');
+  assert.elementNotFound('.annotation-text');
+});
+
+let combinedTemplate = hbs`{{card-content/tabbed-textarea
+          annotationChanged=(action actionStub)
+          show_annotation=true
+          annotation='annotation test'
+          instruction_text='instruction test'}}`;
+
+test(`it can render cobined text together`, function(assert) {
+  this.render(combinedTemplate);
+  assert.elementFound('.annotation-text');
+  assert.textPresent('.instruction-text', 'instruction test');
+});
+
+test(`it sends 'annotationChanged' on change`, function(assert) {
+  assert.expect(1);
+  this.set('actionStub', function(e) {
+    assert.equal(e.target.value, 'changed value');
+  });
+  this.render(annotationTemplate);
+  this.$('textarea.tabbed-textarea').val('changed value').trigger('change');
+});

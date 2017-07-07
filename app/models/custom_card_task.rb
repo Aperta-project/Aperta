05933a2ede46ca437a19c50ca13b39f338a6a86a
Curtis Autery <curtis@mutuallyhuman.com>
2017-07-07 10:39:46 -0400

    APERTA-10097 add specs and clearer comments

--- app/models/custom_card_task.rb
+++ app/models/custom_card_task.rb
@@ -1,6 +1,9 @@
 # This class represents a customized Task built with CardContent
 class CustomCardTask < Task
-  # TODO: Verify that all custom cards need to include MetadataTask
+  # This sets all custom cards to be snapshottable submission tasks. Another
+  # mechanism for handling these class-level attributes on the custom card's
+  # latest version instance will be needed as soon as we convert a legacy card
+  # that is not both a submission task and snapshottable.
   include MetadataTask
   DEFAULT_TITLE = 'Custom Card'.freeze
 

cc989eef025b97ce291b36de7113e7c6df12e470
Erik Hetzner <ehetzner@plos.org>
2017-07-07 19:07:37 -0700

    APERTA-10668 Use Typesetter::BaseSerializer

--- app/serializers/typesetter/related_article_serializer.rb
+++ app/serializers/typesetter/related_article_serializer.rb
@@ -1,6 +1,6 @@
 module Typesetter
   # Serializes a RelatedArticle for the typesetter.
-  class RelatedArticleSerializer < ActiveModel::Serializer
+  class RelatedArticleSerializer < Typesetter::BaseSerializer
     attributes :linked_title, :linked_doi
 
     def linked_title

e0e5c9aa3833671a2a1870d6221623188a8e6e94
John Fesenko <jfesenko@plos.org>
2017-07-10 15:22:40 -0700

    APERTA-10244: make contentRoot property in custom card task component read only

--- client/app/pods/components/custom-card-task/component.js
+++ client/app/pods/components/custom-card-task/component.js
@@ -2,7 +2,7 @@ import TaskComponent from 'tahi/pods/components/task-base/component';
 import Ember from 'ember';
 
 export default TaskComponent.extend({
-  contentRoot: Ember.computed.alias('task.cardVersion.contentRoot'),
+  contentRoot: Ember.computed.reads('task.cardVersion.contentRoot'),
 
   renderAdditionalText: Ember.computed('task.cardVersion.contentRoot.unsortedChildren.[]', function() {
     let cardContents = this.get('task.cardVersion.contentRoot.unsortedChildren');

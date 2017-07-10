995951c5e68c5df2b22b674d028a094e37f3b94a
Curtis Autery <curtis@mutuallyhuman.com>
2017-07-10 10:14:47 -0400

    Fix custom card viewing for non-authors
    I made a bad assumption regarding how to identify who could view custom
    cards. Chris W came up with a more elegant solution, namely to has-one
    card_version on the custom card task serializer, so we can inject card
    version data without instantiating the card versions controller.
    
    APERTA-10097

--- spec/controllers/card_versions_controller_spec.rb
+++ spec/controllers/card_versions_controller_spec.rb
@@ -9,7 +9,6 @@ describe CardVersionsController do
     end
     let(:card_version) { FactoryGirl.create(:card_version) }
     let(:card) { card_version.card }
-    let(:task) { FactoryGirl.create(:task, title: 'Competing Interests') }
 
     it_behaves_like 'an unauthenticated json request'
 
@@ -37,38 +36,6 @@ describe CardVersionsController do
         it 'returns the card version' do
           do_request
           expect(res_body['card_version']['id']).to be card_version.id
-        end
-      end
-
-      context 'and task associated with card version is present' do
-        before do
-          allow(Task).to receive_message_chain('joins.find_by').and_return(task)
-        end
-
-        context 'when user does not have access' do
-          before do
-            stub_sign_in(user)
-            allow(user).to receive(:can?)
-              .with(:view, task)
-              .and_return(false)
-            do_request
-          end
-
-          it { is_expected.to responds_with(403) }
-        end
-
-        context 'when user has access' do
-          before do
-            stub_sign_in user
-            allow(user).to receive(:can?).with(:view, task).and_return(true)
-          end
-
-          it { is_expected.to responds_with 200 }
-
-          it 'returns the card version' do
-            do_request
-            expect(res_body['card_version']['id']).to be card_version.id
-          end
         end
       end
     end

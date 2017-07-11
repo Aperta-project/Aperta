725ea218fda62fab92c1bfcb0804351cc6221f34
John Fesenko <jfesenko@plos.org>
2017-07-10 18:22:03 -0700

    APERTA-10244: added null check for cardContents to fix card editor test failure

--- client/app/pods/components/card-editor/preview/overlay/component.js
+++ client/app/pods/components/card-editor/preview/overlay/component.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   classNames: ['card-editor-preview-overlay'],
   renderAdditionalText: Ember.computed('card.content.unsortedChildren.[]', function() {
     let cardContents = this.get('card.content.unsortedChildren');
-    return cardContents.any((cardContent) => {
+    return cardContents !== null && cardContents.any((cardContent) => {
       return cardContent.get('renderAdditionalText');
     });
   }),
--- client/app/pods/components/custom-card-task/component.js
+++ client/app/pods/components/custom-card-task/component.js
@@ -6,7 +6,7 @@ export default TaskComponent.extend({
 
   renderAdditionalText: Ember.computed('task.cardVersion.contentRoot.unsortedChildren.[]', function() {
     let cardContents = this.get('task.cardVersion.contentRoot.unsortedChildren');
-    return cardContents.any((cardContent) => {
+    return cardContents !== null && cardContents.any((cardContent) => {
       return cardContent.get('renderAdditionalText');
     });
   }),

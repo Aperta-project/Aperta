c91aa4ffbcd141185f9e1554cdc9a40b06547e72
Aaron Christy <aaron@mutuallyhuman.com>
2017-07-19 15:26:21 -0400

    Remove legacy CoverLetterTask
    Removes any custom, hard code for things related to CoverLetterTask.
    Going forward, a cover letter will be supported by a CustomCardTask.

--- app/services/journal_services/create_default_manuscript_manager_templates.rb
+++ app/services/journal_services/create_default_manuscript_manager_templates.rb
@@ -12,7 +12,6 @@ module JournalServices
                                 TahiStandardTasks::SupportingInformationTask,
                                 TahiStandardTasks::AuthorsTask,
                                 TahiStandardTasks::UploadManuscriptTask,
-                                TahiStandardTasks::CoverLetterTask,
                                 CustomCard::Configurations::CoverLetter
                               ])
 

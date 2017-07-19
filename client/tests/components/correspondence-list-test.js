859185253b12a257b8067801d2ff850f050db21e
Yetunde Sanni <yetunde.sanni@andela.com>
2017-07-19 21:45:15 +0100

    APERTA-9677-added component tests to the correspondence list template

--- client/tests/components/correspondence-list-test.js
+++ client/tests/components/correspondence-list-test.js
@@ -5,6 +5,7 @@ import { manualSetup } from 'ember-data-factory-guy';
 import hbs from 'htmlbars-inline-precompile';
 import FakeCanService from 'tahi/tests/helpers/fake-can-service';
 import wait from 'ember-test-helpers/wait';
+import formatDate from 'tahi/lib/format-date';
 
 
 moduleForComponent('correspondence', 'Integration | Component | Correspondence', {
@@ -40,15 +41,44 @@ let template = hbs`
 
 test('can manage workflow, list appears', function(assert) {
   let paper = FactoryGuy.make('paper');
-  FactoryGuy.make('correspondence', { paper: paper });
+  let correspondence = FactoryGuy.make('correspondence', { paper: paper });
   const can = FakeCanService.create().allowPermission('manage_workflow', paper);
   this.register('service:can', can.asService());
 
+  this.set('correspondence', [correspondence]);  
   this.set('paper', paper);
   let done = assert.async();
   this.render(template);
   wait().then(() => {
     assert.equal(this.$('.correspondence-table').length, 1);
+    assert.equal(this.$('tbody').length, 1);
+    assert.textPresent('tr:last td:nth-child(1)', formatDate(correspondence.get('date'), {}));
+    assert.textPresent('tr:last td:nth-child(2)', correspondence.get('subject'));
+    assert.textPresent('tr:last td:nth-child(3)', correspondence.get('recipient'));
+    assert.textPresent('tr:last td:nth-child(4)', correspondence.get('manuscriptVersionStatus'));
+    assert.textPresent('tr:last td:nth-child(5)', correspondence.get('sender'));
+    done();
+  });
+});
+
+test('can manage workflow, list appears for manually created correspondence', function(assert) {
+  let paper = FactoryGuy.make('paper');
+  let correspondence = FactoryGuy.make('correspondence', 'externalCorrespondence', { paper: paper });
+  const can = FakeCanService.create().allowPermission('manage_workflow', paper);
+  this.register('service:can', can.asService());
+
+  this.set('correspondence', [correspondence]);  
+  this.set('paper', paper);
+  let done = assert.async();
+  this.render(template);
+  wait().then(() => {
+    assert.equal(this.$('.correspondence-table').length, 1);
+    assert.equal(this.$('tbody').length, 1);
+    assert.textPresent('tr:last td:nth-child(1)', formatDate(correspondence.get('date'), {}));
+    assert.textPresent('tr:last td:nth-child(2)', correspondence.get('subject'));
+    assert.textPresent('tr:last td:nth-child(3)', correspondence.get('recipient'));
+    assert.textPresent('tr:last td:nth-child(4)', 'Unavailable');
+    assert.textPresent('tr:last td:nth-child(5)', correspondence.get('sender'));
     done();
   });
 });

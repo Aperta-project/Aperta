af4ce155832049fe417c33e6904d8f7ce312f0f1
Shawn Gatchell <shawng03@gmail.com>
2017-07-21 16:27:06 -0700

    Add unit specs, refactor validation

--- app/models/concerns/unique_email.rb
+++ app/models/concerns/unique_email.rb
@@ -4,23 +4,13 @@ module UniqueEmail
   extend ActiveSupport::Concern
 
   included do
-    validate :email, :check_duplicate_email
+    validate :check_duplicate_email
 
     def check_duplicate_email
       paper = Paper.find(paper_id)
-      emails = all_author_emails(paper)
-      unless new_record?
-        emails.delete(email) unless email_changed?
-      end
-      return unless email.in?(emails)
-      errors.add(:email, 'Duplicate email address for this manuscript')
-    end
-
-  private
-
-    def all_author_emails(paper)
-      fields = [[paper.authors, :email], [paper.group_authors, :contact_email]]
-      Set.new.tap { |set| fields.each { |assoc, field| set.merge(assoc.pluck(field)) } }
+      emails = (paper.authors.pluck(:email) + paper.group_authors.pluck(:email)).uniq
+      emails.delete(email) unless new_record? || email_changed?
+      errors.add(:email, 'Duplicate email address for this manuscript') if email.in?(emails)
     end
   end
 end

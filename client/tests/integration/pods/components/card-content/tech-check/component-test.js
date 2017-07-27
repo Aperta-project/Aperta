75026cb22a2af8b0d3ecdcd770ae5420efa33ef8
Chris Westra <cwestra@gmail.com>
2017-07-27 11:36:37 -0400

    WIP start tech-check tests

--- client/tests/integration/pods/components/card-content/tech-check/component-test.js
+++ client/tests/integration/pods/components/card-content/tech-check/component-test.js
@@ -1,4 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import { manualSetup, make } from 'ember-data-factory-guy';
+import wait from 'ember-test-helpers/wait';
 import hbs from 'htmlbars-inline-precompile';
 import registerCustomAssertions from 'tahi/tests/helpers/custom-assertions';
 import Ember from 'ember';
@@ -9,6 +11,7 @@ moduleForComponent(
   {
     integration: true,
     beforeEach() {
+      manualSetup(this.container);
       registerCustomAssertions();
       this.set('actionStub', function() {});
       this.set('content', Ember.Object.create({ ident: 'test' }));
@@ -20,12 +23,39 @@ moduleForComponent(
 let template = hbs`{{card-content/tech-check
 content=content
 disabled=disabled
+owner=owner
 answer=answer
+preview=true
 valueChanged=(action actionStub)
 }}`;
 
+let createCheckWithSendback = () => {
+  let tc = make('card-content', {
+    contentType: 'tech-check',
+    valueType: 'boolean'
+  });
+  let sendback = make('card-content', {
+    contentType: 'sendback-reason',
+    parent: tc
+  });
+  let checkbox = make('card-content', {
+    contentType: 'check-box',
+    valueType: 'boolean',
+    parent: sendback
+  });
+  make('card-content', { // pencil
+    contentType: 'check-box',
+    valueType: 'boolean',
+    parent: sendback
+  });
+  make('card-content', { // sendback reason textarea
+    contentType: 'paragraph-input',
+    parent: sendback
+  });
+  return [tc, checkbox];
+};
 test(`it displays the 'Pass' label`, function(assert) {
-  this.set('labelText', 'my label' );
+  this.set('labelText', 'my label');
   this.render(template);
   assert.textPresent('.checked-label-text', 'Pass');
 });
@@ -39,17 +69,47 @@ test(`it displays content.text as unescaped html`, function(assert) {
 test(`the label is for the input`, function(assert) {
   this.set('content', Ember.Object.create({ label: 'test' }));
   this.render(template);
-  assert.ok(this.$('input').attr('name'), 'the name is set automatically if no ident');
-  assert.ok(this.$('input').attr('id'), 'the id is set automatically if no ident');
-  assert.ok(this.$('label').attr('for'), 'the for is set automatically if no ident');
+  assert.ok(
+    this.$('input').attr('name'),
+    'the name is set automatically if no ident'
+  );
+  assert.ok(
+    this.$('input').attr('id'),
+    'the id is set automatically if no ident'
+  );
+  assert.ok(
+    this.$('label').attr('for'),
+    'the for is set automatically if no ident'
+  );
   assert.equal(this.$('label').attr('for'), this.$('input').attr('name'));
 });
 
 test(`it sends 'valueChanged' on change`, function(assert) {
   assert.expect(1);
+  //create sendback data
+  let owner = make('custom-card-task');
+  let tc = createCheckWithSendback()[0];
+
+  this.set('owner', owner);
+  this.set('content', tc);
   this.set('actionStub', function(newVal) {
     assert.equal(newVal, true, 'it calls the action with the new value');
   });
   this.render(template);
   this.$('.card-content-toggle-switch input').click();
 });
+
+test(`toggling to 'Pass' will clear any existing sendbacks`, function(assert) {
+  let owner = make('custom-card-task');
+  let [tc, sendbackCheck] = createCheckWithSendback();
+
+  // check the sendback
+  make('answer', {owner: owner, value: true, parent: sendbackCheck});
+  this.set('owner', owner);
+  this.set('content', tc);
+  this.render(template);
+
+  assert.elementFound(`.sendback-reason-row input[type="checkbox"]`);
+});
+
+test(`checking a sendback will set the toggle to 'Fail' and send 'valueChanged'`, function() {});

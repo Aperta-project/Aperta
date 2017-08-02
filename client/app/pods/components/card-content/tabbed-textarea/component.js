5524cd5e13e5a46a62d205b7e31c5d46b719af45
John Fesenko <jfesenko@plos.org>
2017-08-02 08:32:30 -0700

    APERTA-10244: use autosize ember addon for tabbed-textarea

--- client/app/pods/components/card-content/tabbed-textarea/component.js
+++ client/app/pods/components/card-content/tabbed-textarea/component.js
@@ -11,6 +11,10 @@ export default Ember.Component.extend({
     this.$('.nav-tabs > li:first-child').addClass('active');
     this.$('.tab-content > .tab-pane:first-child').addClass('active');
     this.$('.nav-tabs .active').tab('show');
+
+    // resize textarea on initial display and on tab click
+    autosize.update(this.$('.tabbed-textarea'));
+    $(document).on('shown.bs.tab', function () {autosize.update($('.tabbed-textarea'));});
   }
 
 });

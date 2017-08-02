32599bc829c71c17226802f193a6c08e2cee4012
Andrew Max <anm898989@gmail.com>
2017-08-03 02:26:07 +0800

    APERTA-10253 preprint doi validation

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -607,7 +607,6 @@ class Paper < ActiveRecord::Base
   end
 
   def verify_or_assign_preprint_doi!
-    binding.pry
     return unless preprint_doi_short_id == nil
     raise "Invalid paper Journals are required for papers urls." unless journal
     update!(preprint_doi_short_id: journal.next_preprint_short_doi!)

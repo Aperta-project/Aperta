f878203e6793fb9e12703d3cbcf710a653b8d809
Andrew Max <anm898989@gmail.com>
2017-08-04 16:48:56 +0800

    APERTA-10253 cleanup on preprint doi logic

--- engines/tahi_standard_tasks/app/services/tahi_standard_tasks/export_service.rb
+++ engines/tahi_standard_tasks/app/services/tahi_standard_tasks/export_service.rb
@@ -40,7 +40,7 @@ module TahiStandardTasks
 
         paper.ensure_preprint_doi! if needs_preprint_doi?
 
-        paper.verify_or_assign_preprint_doi! if needs_preprint_doi?
+        paper.ensure_preprint_doi! if needs_preprint_doi?
 
         if destination == 'apex'
           upload_to_ftp(packager.zip_file, package_filename)

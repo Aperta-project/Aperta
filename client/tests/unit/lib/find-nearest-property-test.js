a49234b53936437f04f326272ef0f44d4214ec30
Jim Haungs <jhaungs@plos.org>
2017-08-09 11:41:33 -0700

    Add unit test for findNearestProperty

--- client/tests/unit/lib/find-nearest-property-test.js
+++ client/tests/unit/lib/find-nearest-property-test.js
@@ -6,24 +6,35 @@ module('FindNearestProperty');
 
 let scenario = Ember.Object.create({isEditable: true});
 
-test('immediate case', function(assert) {
-  let parent = Ember.Object.create({scenario: scenario});
+function createScenario(scenario, level=1) {
+  var parent = Ember.Object.create(scenario);
+  for (var i = 1; i < level; i++) {
+    parent = Ember.Object.create({parent: parent});
+  }
+  return parent;
+}
+
+test('immediate success', function(assert) {
+  let parent = createScenario({scenario: scenario});
   let result = findNearestProperty(parent, 'scenario');
   assert.equal(result && result.isEditable, true, 'scenario on immediate object is found');
 });
 
-test('inherited case', function(assert) {
-  var parent = Ember.Object.create({scenario: scenario});
-  for (var i = 0; i < 3; i++) {
-    parent = Ember.Object.create({parent: parent});
-  }
+test('nested success', function(assert) {
+  let parent = createScenario({scenario: scenario}, 3);
   let result = findNearestProperty(parent, 'scenario', 'parent');
-  assert.equal(result && result.isEditable, true, 'scenario on inherited object is found');
+  assert.equal(result && result.isEditable, true, 'scenario on nested object is found');
 });
 
-test('failure case', function(assert) {
-  var parent = Ember.Object.create({nonScenario: scenario});
+test('immediate failure', function(assert) {
+  let parent = createScenario({nonScenario: scenario});
   let result = findNearestProperty(parent, 'scenario', 'parent');
-  assert.equal(result, null, 'scenario on inherited object not found');
+  assert.equal(result, null, 'scenario on immediate object not found');
+});
+
+test('nested failure', function(assert) {
+  let parent = createScenario({nonScenario: scenario}, 3);
+  let result = findNearestProperty(parent, 'scenario', 'parent');
+  assert.equal(result, null, 'scenario on nested object not found');
 });
 

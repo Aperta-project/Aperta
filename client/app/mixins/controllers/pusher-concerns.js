82efeef9c892c16143d7b358da44e24bd9cff72a
Shawn Gatchell <shawng03@gmail.com>
2017-08-11 10:13:34 -0700

    Do not duplicate pusher flash messages

--- client/app/mixins/controllers/pusher-concerns.js
+++ client/app/mixins/controllers/pusher-concerns.js
@@ -10,53 +10,56 @@ export default Ember.Mixin.create({
   // this alias works, but is not observable
   pusherConnectionState: Ember.computed.alias('pusher.connection.connection.state'),
 
-  handlePusherConnectionStatusChange(){
+  handlePusherConnectionStatusChange() {
     if (this.get('pusherConnectionState') === 'connecting') {
       this.get('handlePusherConnecting').perform();
     } else {
       this.cleanupPusherConnecting();
     }
     if (this.get('pusherNotConnected')) {
-      this.handlePusherNotConnected();
+      this.updatePusherConnectionMessage();
     }
   },
-  
+
   cleanupPusherConnecting() {
     // remove the connecting message on connecting -> connected transition
     let messages = this.get('flash').get('systemLevelMessages');
-    let connectionMessage = messages.findBy('text', this._pusherFailureMessage('connecting'));
+    let connectionMessage = messages.findBy('text', this.pusherFailureMessage('connecting'));
     this.get('flash').removeSystemLevelMessage(connectionMessage);
   },
-  
+
   handlePusherConnecting: concurrencyTask(function*() {
-    if(!Ember.isTesting){
+    if (!Ember.isTesting) {
       // Pusher tries to connect for 10s. Handle the resulting connection state.
       yield timeout(10000);
     }
     this.handlePusherConnectionStatusChange();
   }).drop(),
 
-  handlePusherNotConnected() {
-    let message = this._pusherFailureMessage(this.get('pusherConnectionState'));
-    this.get('flash').displaySystemLevelMessage('error', message);
+  updatePusherConnectionMessage() {
+    let message = this.pusherFailureMessage(this.get('pusherConnectionState'));
+    let messages = this.get('flash').get('systemLevelMessages').filterBy('text', message);
+    if (!messages.length) {
+      this.get('flash').displaySystemLevelMessage('error', message);
+    }
   },
 
-  _pusherFailureMessage(failureState) {
+  pusherFailureMessage(failureState) {
     switch (failureState) {
     case 'unavailable':
       return `Aperta is currently having trouble maintaining a live connection with your browser.
-            This could impact updates to the interface, and we recommend you 
-            <a href="#" onclick="window.location.reload(false)"> reload this page</a>
-            to attempt to re-establish the connection`;
+          This could impact updates to the interface, and we recommend you 
+          <a href="#" onclick="window.location.reload(false)"> reload this page</a>
+          to attempt to re-establish the connection`;
     case 'failed':
       return `Aperta is having trouble establishing a live connection with your browser
-              due to lack of browser support for required software.
-              <a href="http://browsehappy.com/">Please update your browser to the current version</a>`;
+            due to lack of browser support for required software.
+            <a href="http://browsehappy.com/">Please update your browser to the current version</a>`;
     case 'disconnected':
       return `Aperta\'s live connection with your browser has been dropped. 
-            This could impact updates to the interface,
-            and we recommend you <a href="#" onclick="window.location.reload(false)">reload this page</a> 
-            to attempt to re-establish the connection.`;
+          This could impact updates to the interface,
+          and we recommend you <a href="#" onclick="window.location.reload(false)">reload this page</a> 
+          to attempt to re-establish the connection.`;
     case 'connecting':
       return `Aperta is attempting to acquire a live connection. Please wait until connection is established.`;
     default:

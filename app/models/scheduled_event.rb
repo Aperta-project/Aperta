7d75530f70a296e5b61f60a8a723187940c2ddf5
Owajigbanam Ogbuluijah <owajigbanam.ogbuluijah@andela.com>
2017-08-11 20:14:49 +0100

    APERTA-9687 Scheduled Event States
    For the scope of this ticket, the states needed for the ScheduledEvent
    are `complete`, `active`, and `inactive`.
    
    When an event is scheduled, it is most likely that we want this event to
    be triggered at some point. No event is scheduled in a deactivated
    state. So `active` becomes the initial state.
    
    An active event can be `trigger`ed. After the event id triggered, it is
    assumed that the event is complete. This is only for the scope of this
    ticket. There may be error states in processing. There may also be a
    processing transient state, if this is to be elaborate.
    
    Rescheduling is the focus of this ticket. Depending on the direction of
    the due date, the event may transition from an active state to an
    inactive state, and vice versa. A completed event may also transition to
    an active (yet to be triggered) event. But for history purposes, this
    event would be duplicated as an active one.

--- app/models/scheduled_event.rb
+++ app/models/scheduled_event.rb
@@ -5,4 +5,24 @@
 # on which "Eventamatron" would work off to maintain the states of chasing events
 class ScheduledEvent < ActiveRecord::Base
   belongs_to :due_datetime
+
+  include AASM
+
+  aasm column: :state do
+    state :active, initial: true
+    state :inactive
+    state :complete
+
+    event(:activate) do
+      transitions from: :inactive, to: :active
+    end
+
+    event(:deactivate) do
+      transitions from: :active, to: :inactive
+    end
+
+    event(:trigger) do
+      transitions from: :active, to: :complete
+    end
+  end
 end
---
+++ spec/models/scheduled_event_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe ScheduledEvent do
+  subject { FactoryGirl.create :scheduled_event }
+
+  describe '.state' do
+    it 'defaults to active' do
+      expect(subject.active?).to be true
+    end
+
+    it 'can move from active to inactive' do
+      subject.deactivate
+      expect(subject.inactive?).to be true
+    end
+
+    it 'can move from inactive to active' do
+      subject.deactivate
+      subject.activate
+      expect(subject.active?).to be true
+    end
+
+    it 'can move from active to complete' do
+      subject.trigger
+      expect(subject.complete?).to be true
+    end
+  end
+end

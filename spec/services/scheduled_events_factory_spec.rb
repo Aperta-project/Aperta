5771b9fef05fe31da0fd4fa94b9be51e0e75f52b
Owajigbanam Ogbuluijah <owajigbanam.ogbuluijah@andela.com>
2017-08-14 19:40:50 +0100

    APERTA-9687 decoupled tests
    Prior to this, tests were tightly coupled to ReviewerReport. But
    scheduling events has to be task agnostic. Therefore, the tests should
    also be agnostic. These tests now properly test the scheduling of events
    around some random task class

--- spec/services/scheduled_events_factory_spec.rb
+++ spec/services/scheduled_events_factory_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
+class ScheduledEventTestTask < Task
+  DEFAULT_TITLE = 'Mock Metadata Task'.freeze
+  SCHEDULED_EVENTS_TEMPLATE = [
+    { name: 'Pre-due Reminder', dispatch_offset: -2 },
+    { name: 'First Late Reminder', dispatch_offset: 2 },
+    { name: 'Second Late Reminder', dispatch_offset: 4 }
+  ].freeze
+  has_one :due_datetime, as: :due
+end
+
 shared_examples 'templated scheduled events' do |template|
   let(:owned_active_events) { ScheduledEvent.owned_by(reviewer_report.class.name, reviewer_report.id) }
   it 'should update times on active events correctly' do
@@ -17,7 +27,7 @@ describe ScheduledEventFactory do
     let(:reviewer_report) do
       FactoryGirl.create :reviewer_report, due_datetime: due_date
     end
-    let(:template) { ReviewerReport::SCHEDULED_EVENTS_TEMPLATE }
+    let(:template) { ScheduledEventTestTask::SCHEDULED_EVENTS_TEMPLATE }
     let(:owned_active_events) { ScheduledEvent.owned_by(reviewer_report.class.name, reviewer_report.id) }
 
     subject { described_class.new(reviewer_report, template).schedule_events }
@@ -56,7 +66,7 @@ describe ScheduledEventFactory do
           expect(owned_active_events.complete.count).to be 1
         end
 
-        it_behaves_like 'templated scheduled events', ReviewerReport::SCHEDULED_EVENTS_TEMPLATE
+        it_behaves_like 'templated scheduled events', ScheduledEventTestTask::SCHEDULED_EVENTS_TEMPLATE
       end
 
       context 'with one event triggered and review due date moved backward' do
@@ -74,7 +84,7 @@ describe ScheduledEventFactory do
           expect(owned_active_events.active.count).to be 2
         end
 
-        it_behaves_like 'templated scheduled events', ReviewerReport::SCHEDULED_EVENTS_TEMPLATE
+        it_behaves_like 'templated scheduled events', ScheduledEventTestTask::SCHEDULED_EVENTS_TEMPLATE
       end
 
       context 'when a scheduled event moves into the past without trigger' do
@@ -90,7 +100,7 @@ describe ScheduledEventFactory do
           expect(owned_active_events.active.count).to be 2
         end
 
-        it_behaves_like 'templated scheduled events', ReviewerReport::SCHEDULED_EVENTS_TEMPLATE
+        it_behaves_like 'templated scheduled events', ScheduledEventTestTask::SCHEDULED_EVENTS_TEMPLATE
       end
     end
   end

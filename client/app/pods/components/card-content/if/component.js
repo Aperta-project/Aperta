5dde96a781c0a938f34085ba24db667caf6962bf
Chris Westra <cwestra@gmail.com>
2017-08-14 13:46:35 -0400

    APERTA-10900 PR feedback

--- client/app/pods/components/card-content/if/component.js
+++ client/app/pods/components/card-content/if/component.js
@@ -14,14 +14,21 @@ export default Ember.Component.extend({
 
   scenario: null,
   bivalent: Ember.computed.equal('content.children.length', 2),
-  computedScenario: Ember.computed(function () {
+  computedScenario: Ember.computed(function() {
     return findNearestProperty(this, 'scenario');
   }),
 
+  /**
+   * The 'if' component will look up a given key on the 'scenario' that is passed to it.
+   * We can't statically know the value of the that key, we can't use a standard computed property.
+   * One option is to use 'this.defineProperty' to define a computed property in init() with a dynamic key.
+   * The other option is to use an observer. In this case I thought that the observer-based implementation would be
+   * easier to reason about and require less ember knowledge than using 'defineProperty'
+   */
   init() {
     this._super(...arguments);
     let preview = this.get('preview');
-    if (! preview) {
+    if (!preview) {
       let conditionName = this.get('conditionName');
       let conditionKey = `computedScenario.${conditionName}`;
       this.getConditionValue();

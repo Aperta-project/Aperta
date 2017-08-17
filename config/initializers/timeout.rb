d5e16ca9e4631f160ee5e5fb4727e8545aedb620
Jim Haungs <jhaungs@plos.org>
2017-08-17 15:11:46 -0700

    Revert timout removal

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,6 +1,6 @@
 # Configuration for Rack::Timeout
 class Application < Rails::Application
-  # config.middleware.use Rack::Timeout
+  config.middleware.use Rack::Timeout
 end
 
-# Rack::Timeout.timeout = Integer(ENV["RACK_TIMEOUT"] || 30) # seconds
+Rack::Timeout.timeout = Integer(ENV["RACK_TIMEOUT"] || 30) # seconds

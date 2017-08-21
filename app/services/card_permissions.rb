18c1d8d6eceeb48a1438a7a6c1f7ccb40bde161a
Andrew Max <anm898989@gmail.com>
2017-08-21 22:06:05 +0800

    APERTA-10951 tracks permission for role removals

--- app/services/card_permissions.rb
+++ app/services/card_permissions.rb
@@ -37,7 +37,7 @@ class CardPermissions
   # can only edit in the "editable" states, reviewers can only edit in the
   # "reviewable" states, etc. This means that these roles use a different
   # permission.
-  def self.set_roles(card, action, roles)
+  def self.set_roles(card, action, roles, permission = nil)
     if STATELESS_ACTIONS.include?(action)
       replace_roles_and_save(get_view_card_permission(card, action), roles)
       # Return an array, although there is only one permission to return, in
@@ -51,9 +51,10 @@ class CardPermissions
       # "reviewable" states, etc. This means that these roles use a different
       # permission.
       grouped_roles = group_roles(card, roles)
+
       STATES.keys.map do |key|
-        replace_roles_and_save(get_task_permission(card, action, STATES[key]),
-                               grouped_roles[key])
+        permission ||= get_task_permission(card, action, STATES[key])
+        replace_roles_and_save(permission, grouped_roles[key])
       end
     end
   end

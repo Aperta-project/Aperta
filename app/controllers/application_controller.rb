a375ab36580260669b7f97245d3e3156ee05c78b
Erik Hetzner <ehetzner@plos.org>
2017-08-22 14:29:06 -0700

    remove OldAuthorizationsControllerHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  include OldAuthorizationsControllerHelper
   include AuthorizationsControllerHelper
   include TahiPusher::SocketTracker
   include TahiPusher::CurrentUserTracker

863e34bdcdd97cc643c7f22a77d3ac236c84da83
Shawn Gatchell <shawng03@gmail.com>
2017-08-21 22:30:35 -0700

    Serialize merge_fields

--- app/models/template_scenario.rb
+++ app/models/template_scenario.rb
@@ -2,7 +2,10 @@
 class TemplateScenario < TemplateContext
   def self.merge_fields(defs = merge_field_definitions)
     defs.map do |hash|
-      hash[:children] = merge_fields(hash[:context].merge_field_definitions) if hash[:context]
+      if hash[:context]
+        hash[:children] = merge_fields(hash[:context].merge_field_definitions)
+        hash.delete(:context)
+      end
       hash
     end
   end
--- app/serializers/letter_template_serializer.rb
+++ app/serializers/letter_template_serializer.rb
@@ -1,5 +1,9 @@
 # This is used to serialize letter templates which are used to draft emails
 # 'name' is used by select-2 dropdowns later
 class LetterTemplateSerializer < ActiveModel::Serializer
-  attributes :id, :name, :category, :to, :subject, :body, :journal_id
+  attributes :id, :name, :category, :to, :subject, :body, :journal_id, :merge_fields
+end
+
+def merge_fields
+  TahiStandardTasks::RegisterDecisionScenario.merge_fields
 end

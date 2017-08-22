001520770775f9ce628fff9a7783cc927424bb18
Aaron Christy <aaron@mutuallyhuman.com>
2017-08-22 14:47:37 -0400

    Delete old answers after they are deselected
    We currently have two components which can variably display a tree of
    card content with answers.
    
    With the `if-component`, there are two alternate branches that will be
    displayed. When the user selects one of these branches, the inactive
    tree of answers should be deleted. With the `display-with-value`
    component there is only one branch, but the answers should be similarly
    deleted when the children are hidden.
    
    In either case, these stale answer records were creating misleading
    snapshots. Since the answer was never actually removed, the snapshot
    never noticed any changes. With these changes, the answers will be
    removed and the snapshots will now pick up the differences in answers.

--- client/tests/components/card-content/if/component-test.js
+++ client/tests/components/card-content/if/component-test.js
@@ -2,6 +2,7 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import FactoryGuy from 'ember-data-factory-guy';
 import { manualSetup } from 'ember-data-factory-guy';
+import sinon from 'sinon';
 import Ember from 'ember';
 
 moduleForComponent(
@@ -134,3 +135,68 @@ test(`ignores the scenario when preview is true, and shows a toggle`, function(a
 
   assert.elementFound('.if-preview', 'found preview');
 });
+
+test(
+  `destroy child answers when an if-branch is hidden`,
+  function(assert) {
+    let template = hbs`
+    {{card-content/if
+      class="removing-answers-test"
+      owner=owner
+      scenario=scenario
+      preview=false
+      disabled=disabled
+      content=content}}`;
+
+    let owner = FactoryGuy.make('custom-card-task');
+
+    let content = parent.create({
+      children: [
+        FactoryGuy.make('card-content', {
+          contentType: 'short-input',
+          text: 'question then',
+          answers: [
+            FactoryGuy.make('answer', { owner: owner, value: 'answer then' })
+          ]
+        }),
+        FactoryGuy.make('card-content', {
+          contentType: 'paragraph-input',
+          text: 'question else',
+          answers: [
+            FactoryGuy.make('answer', { owner: owner, value: 'answer else' })
+          ]
+        })
+      ]
+    });
+
+    let thenAnswer = content.get('children.firstObject.answers.firstObject');
+    let elseAnswer = content.get('children.lastObject.answers.firstObject');
+
+    const thenDestroyRecord = sinon.spy();
+    thenAnswer.set('destroyRecord', thenDestroyRecord);
+
+    const elseDestroyRecord = sinon.spy();
+    elseAnswer.set('destroyRecord', elseDestroyRecord);
+
+    this.set('owner', owner);
+    this.set('content', content);
+    this.set('scenario', { isEditable: true });
+    this.render(template);
+
+    // ensure that "then branch" is shown
+    assert.textPresent('.removing-answers-test', 'question then');
+    assert.textNotPresent('.removing-answers-test', 'question else');
+
+    // ensure that "else branch" is shown
+    this.set('scenario.isEditable', false);
+    assert.textNotPresent('.removing-answers-test', 'question then');
+    assert.textPresent('.removing-answers-test', 'question else');
+    assert.ok(thenDestroyRecord.called, 'destroyRecord was never called on the thenAnswer');
+
+    // ensure that "then branch" is shown
+    this.set('scenario.isEditable', true);
+    assert.textPresent('.removing-answers-test', 'question then');
+    assert.textNotPresent('.removing-answers-test', 'question else');
+    assert.ok(elseDestroyRecord.called, 'destroyRecord was never called on the elseAnswer');
+  }
+);

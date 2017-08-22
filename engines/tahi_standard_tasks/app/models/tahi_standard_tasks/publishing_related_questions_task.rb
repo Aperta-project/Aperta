7dccb0579fa57b8a24e35c9024e45b0e2279495d
Andres Visbal <andresvisbal@gmail.com>
2017-08-22 15:43:45 -0500

    Migration Additional Information Card

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/publishing_related_questions_task.rb
+++
@@ -1,8 +0,0 @@
-module TahiStandardTasks
-  class PublishingRelatedQuestionsTask < Task
-    include MetadataTask
-
-    DEFAULT_TITLE = 'Additional Information'.freeze
-    DEFAULT_ROLE_HINT = 'author'.freeze
-  end
-end
---
+++ lib/custom_card/configurations/publishing_related_questions.rb
@@ -0,0 +1,32 @@
+# rubocop:disable Metrics/MethodLength
+module CustomCard
+  module Configurations
+    #
+    # This class defines the specific attributes of a particular
+    # Card and it can be used to create a new valid Card into the
+    # system via the CustomCard::Loader.
+    #
+    class PublishingRelatedQuestions < Base
+      def self.name
+        "Additional Information"
+      end
+
+      def self.view_role_names
+        ["Academic Editor", "Billing Staff", "Collaborator", "Cover Editor", "Creator", "Handling Editor", "Internal Editor", "Production Staff", "Publishing Services", "Reviewer", "Staff Admin"]
+      end
+
+      def self.edit_role_names
+        ["Collaborator", "Cover Editor", "Creator", "Handling Editor", "Internal Editor", "Production Staff", "Publishing Services", "Staff Admin"]
+      end
+
+      def self.publish
+        true
+      end
+
+      def self.do_not_create_in_production_environment
+        false
+      end
+    end
+  end
+end
+# rubocop:enable Metrics/MethodLength

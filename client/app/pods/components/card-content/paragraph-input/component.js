75ed2eda0a35b2b9da8aebb21004b217abed59eb
Carlos Guisao Franco <titofranco@gmail.com>
2017-08-22 14:47:23 -0500

    APERTA-10790: Clean up format-input component

--- client/app/pods/components/card-content/paragraph-input/component.js
+++ client/app/pods/components/card-content/paragraph-input/component.js
@@ -6,11 +6,11 @@ export default Ember.Component.extend(ValidateTextInput, {
   classNames: ['card-content-paragraph-input'],
   classNameBindings: ['answer.hasErrors:has-error', 'disabled:read-only'],
 
+
   propTypes: {
     answer: PropTypes.EmberObject.isRequired,
     content: PropTypes.EmberObject.isRequired,
-    disabled: PropTypes.bool,
+    disabled: PropTypes.bool
   },
-
   isRichText: Ember.computed.equal('content.valueType', 'html')
 });
--- client/tests/components/format-input-test.js
+++ client/tests/components/format-input-test.js
@@ -39,29 +39,5 @@ test('value is displayed', function(assert) {
   this.render(hbs`
     {{format-input value=title}}
   `);
-
-  assert.equal(this.$('.format-input-field').html().trim(), 'Title');
-});
-
-test('value is formatted with bold', function(assert) {
-  assert.expect(2);
-
-  this.render(hbs`
-    {{format-input value=title}}
-  `);
-
-  assert.equal(
-    this.$('.format-input-field').html().trim(),
-    'Title',
-    'Value is unformatted'
-  );
-
-  selectElementText(this.$('.format-input-field').get(0));
-  this.$('.format-input-button:last').click();
-
-  assert.equal(
-    this.$('.format-input-field').html().trim(),
-    '<b>Title</b>',
-    'Value is formatted'
-  );
+  assert.equal(this.$('textarea').val().trim(), 'Title');
 });

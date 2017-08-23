2bf5482202920d91e76935f2a8f5a769e8e3195f
Shawn Gatchell <shawng03@gmail.com>
2017-08-23 12:33:15 -0700

    Use LetterTemplate#scenario to serialize merge fields

--- app/serializers/letter_template_serializer.rb
+++ app/serializers/letter_template_serializer.rb
@@ -3,7 +3,3 @@
 class LetterTemplateSerializer < ActiveModel::Serializer
   attributes :id, :name, :category, :to, :subject, :body, :journal_id, :merge_fields
 end
-
-def merge_fields
-  TahiStandardTasks::RegisterDecisionScenario.merge_fields
-end

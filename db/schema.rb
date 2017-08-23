0bd2e4ce153950eabf1f4f9aa83e1c5d0c6eb666
Jim Haungs <jhaungs@plos.org>
2017-08-23 14:07:11 -0700

    Revert schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -245,7 +245,7 @@ ActiveRecord::Schema.define(version: 20170817151226) do
     t.string   "instruction_text"
     t.string   "editor_style"
     t.string   "condition"
-    t.boolean  "required_field",             default: false
+    t.boolean  "required_field"
   end
 
   add_index "card_contents", ["ident"], name: "index_card_contents_on_ident", using: :btree

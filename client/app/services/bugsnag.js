a6a208714141c04b1737775a59e7e2970f024004
Ben Beckwith <ben@mutuallyhuman.com>
2017-08-24 16:16:57 -0400

    Add notify and clean up notifyException for Bugsnag
    For `notifyException`, the parameters have been reduced to just the
    exception object and optional metadata. The `name` and `severity`
    arguments have been removed.
    
    A `notify` method is added to the service. It is used in places where
    we want a warning, but where there may not be an exception
    
    * Pusher failures call `notify` instead
    * File upload errors use `notifyException`
    * Remove 'Uncaught Ember Error'; rely on exception data
    * Add file upload metadata in case of failure
    
    APERTA-9819

--- client/app/services/bugsnag.js
+++ client/app/services/bugsnag.js
@@ -1,26 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-  notifyException: function(error, name, metadata = {}, severity = 'error') {
+
+  notifyException: function(error, metadata = {}) {
     if (typeof Bugsnag !== 'undefined' && Bugsnag && Bugsnag.notifyException) {
       if (window.currentUserData && !Bugsnag.user) {
         Bugsnag.user = window.currentUserData.user;
       }
-      Bugsnag.notifyException(error, name, metadata, severity);
+      Bugsnag.notifyException(error, metadata);
     } else {
-      console.error(
+      console.error( // eslint-disable-line no-console
         'Bugsnag not available, notifyException called with: ',
         'error:',
         error,
-        'name: ',
-        name,
-        'metadata: ',
+        ', metadata: ',
         metadata
       );
     }
   },
 
-  notifyUploadError: function(error, metadata={}) {
-    this.notifyException(error, 'File Upload Error', metadata);
+  notify: function(name, message, metadata={}, severity='warning') {
+    if (typeof Bugsnag !== 'undefined' && Bugsnag && Bugsnag.notifyException) {
+      if (window.currentUserData && !Bugsnag.user) {
+        Bugsnag.user = window.currentUserData.user;
+      }
+      Bugsnag.notify(name, message, metadata, severity);
+    } else {
+      console.error( // eslint-disable-line no-console
+        'Bugsnag not available, notify called with: ',
+        'name: ',
+        name,
+        ', message:',
+        message,
+        ', metadata: ',
+        metadata,
+        ', severity: ',
+        severity
+      );
+    }
   }
 });

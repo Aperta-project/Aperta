780ab8a7c0b2ab747d26098f7968a7ea85042456
Owajigbanam Ogbuluijah <owajigbanam.ogbuluijah@andela.com>
2017-08-24 21:42:58 +0100

    APERTA-9688 log reminders to workflow activities
    This puts the sending of the reminder in the list of workflow
    activities.

--- app/workers/scheduled_events_worker.rb
+++ app/workers/scheduled_events_worker.rb
@@ -5,6 +5,9 @@ class ScheduledEventsWorker
   def perform
     due_events = ScheduledEvent.active.where('dispatch_at < ?', DateTime.now.in_time_zone)
     return unless due_events.exists?
-    due_events.each(&:trigger!)
+    due_events.each do |event|
+      event.trigger!
+      Activity.reminder_sent!(event)
+    end
   end
 end
--- spec/workers/scheduled_events_worker_spec.rb
+++ spec/workers/scheduled_events_worker_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
+shared_examples 'logged workflow activities' do
+  it 'should log sending the reminder to the workflow activity' do
+    allow_any_instance_of(TahiStandardTasks::ReviewerMailer).to receive(:remind_before_due)
+    allow_any_instance_of(TahiStandardTasks::ReviewerMailer).to receive(:first_late_notice)
+    allow_any_instance_of(TahiStandardTasks::ReviewerMailer).to receive(:second_late_notice)
+    expect(Activity).to receive(:reminder_sent!)
+    worker.perform
+  end
+end
+
 describe ScheduledEventsWorker do
   let(:due_date) { FactoryGirl.create :due_datetime, :in_5_days }
   let(:reviewer_report) do
     FactoryGirl.create :reviewer_report, due_datetime: due_date
   end
   let(:template) { ReviewerReport::SCHEDULED_EVENTS_TEMPLATE }
+
+  subject(:worker) { described_class.new }
 
   context 'after the pre due dispatch is past' do
     before do
@@ -14,10 +26,11 @@ describe ScheduledEventsWorker do
     end
 
     it 'should send the pre due email' do
-      worker = ScheduledEventsWorker.new
       expect_any_instance_of(TahiStandardTasks::ReviewerMailer).to receive(:remind_before_due)
       worker.perform
     end
+
+    it_behaves_like 'logged workflow activities'
 
     after do
       Timecop.return

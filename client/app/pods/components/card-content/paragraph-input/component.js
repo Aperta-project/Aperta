227f399f71d8dd3158ebc7fa0dacf6000181985f
Carlos Guisao Franco <titofranco@gmail.com>
2017-08-25 12:06:30 -0500

    APERTA-10790: Fix SI rich text editor not saving data

--- client/app/pods/components/card-content/paragraph-input/component.js
+++ client/app/pods/components/card-content/paragraph-input/component.js
@@ -13,6 +13,13 @@ export default Ember.Component.extend(ValidateTextInput, {
   },
   isRichText: Ember.computed.equal('content.valueType', 'html'),
 
+  answerValue: Ember.computed(function () {
+    // Since some parent components don't have an answer object as a property
+    // then passing the parent's component 'value' as the workingValue
+    // will help keeping actions working as usual.
+    return this.get('answer.value') || this.get('workingValue');
+  }),
+
   actions: {
     valueChanged(e) {
       // this._super will be the valueChanged action from the ValidateTextInput mixin.

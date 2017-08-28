d075e7b98222ac817a650e43ba457504f4412cb1
Yetunde Sanni <yetunde.sanni@andela.com>
2017-08-28 18:04:33 +0100

    APERTA-10773-prevent-unauthorized-users-from-downloading-attachment

--- client/app/helpers/file-type.js
+++ client/app/helpers/file-type.js
@@ -4,6 +4,9 @@ export default Ember.Helper.helper(function(params) {
   if ( /\.(doc|docx)$/i.test(params) ) {
     return 'fa-file-word-o';
   }
+  else if ( /\.(jpe?g|png|gif|bmp)$/i.test(params) ) {
+    return 'fa-file-image-o';
+  }
   else {
     return 'fa-file-pdf-o';
   }

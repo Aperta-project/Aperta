6b535dca2e25e4e7172dfc2af5106a1f56067c08
Michael Walsh <noxiousthing@gmail.com>
2017-08-29 14:11:06 -0700

    Implement new XML for Reporting Guidelines migration. Remove field-set card content

--- lib/custom_card/configurations/reporting_guidelines.rb
+++ lib/custom_card/configurations/reporting_guidelines.rb
@@ -28,25 +28,28 @@ module CustomCard
 
       def self.xml_content
         <<-XML.strip_heredoc.strip_heredoc
-          <?xml version="1.0" encoding="UTF-8"?>
-          <card required-for-submission="false" workflow-display-only="false">
-            <content content-type="display-children">
+        <?xml version="1.0" encoding="UTF-8"?>
+        <card required-for-submission="false" workflow-display-only="false">
+          <content content-type="display-children">
+            <content content-type="display-children" child-tag="li" custom-class="question-list" custom-child-class="question" wrapper-tag="ol">
               <content content-type="text">
                 <text>
-                  <![CDATA[<ol class="question-list"><li class="question"><div class="question-text"><div>Authors should check the <a target="_blank" href="http://www.equator-network.org">EQUATOR Network</a> site for any reporting guidelines that apply to their study design, and ensure that any required Supporting Information (checklists, protocols, flowcharts, etc.) be included in the article submission.</div></div></li>]]>
+                  <![CDATA[<div class="question-text">Authors should check the <a target="_blank" href="http://www.equator-network.org">EQUATOR Network</a> site for any reporting guidelines that apply to their study design, and ensure that any required Supporting Information (checklists, protocols, flowcharts, etc.) be included in the article submission.</div>]]>
                 </text>
               </content>
-              <content content-type="plain-list">
-                <content content-type="text">
-                  <text>Select all that apply</text>
-                </content>
+            </content>
+            <content content-type="display-children" custom-class="question-list">
+              <content content-type="text">
+                <text>Select all that apply</text>
+              </content>
+              <content content-type="display-children" custom-class="left-indent">
                 <content ident="reporting_guidelines--clinical_trial" content-type="check-box" value-type="boolean">
                   <label>Clinical Trial</label>
                 </content>
                 <content ident="reporting_guidelines--systematic_reviews" content-type="check-box" value-type="boolean">
                   <label>Systematic Reviews</label>
                   <content content-type="display-with-value" visible-with-parent-answer="true">
-                    <content content-type="field-set">
+                    <content content-type="display-children" custom-class="card-content-field-set">
                       <content ident="reporting_guidelines--systematic_reviews--checklist" content-type="file-uploader" value-type="attachment">
                         <text>
                           <![CDATA[<div class="question-text">Provide a completed PRISMA checklist as supporting information. You can <a target="_blank" href="http://www.prisma-statement.org">download it here.</a></div>]]>
@@ -80,7 +83,8 @@ module CustomCard
                 </content>
               </content>
             </content>
-          </card>
+          </content>
+        </card>
         XML
       end
     end

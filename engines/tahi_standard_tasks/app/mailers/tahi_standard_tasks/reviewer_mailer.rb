1f487814af6c16a9cdce9990949a066a7ff5e797
Michael Ogala <michael.ogala@andela.com>
2017-08-30 17:48:22 +0100

    Update thank you mailer

--- engines/tahi_standard_tasks/app/mailers/tahi_standard_tasks/reviewer_mailer.rb
+++ engines/tahi_standard_tasks/app/mailers/tahi_standard_tasks/reviewer_mailer.rb
@@ -123,7 +123,11 @@ module TahiStandardTasks
       @paper = reviewer_report.paper
       @journal = @paper.journal
       @user = @reviewer_report.user
-      mail(to: @user.email, subject: "Thank you for reviewing for #{@journal.name}")
+      @letter_template = @journal.letter_templates.find_by(name: 'template_name')
+      @scenario = ReviewerReportScenario.new(@reviewer_report)
+      @subject = Liquid::Template.parse(@letter_template.subject).render(@scenario)
+      @body = Liquid::Template.parse(@letter_template.body).render(@scenario)
+      mail(to: @user.email, subject: @subject)
     end
   end
 end

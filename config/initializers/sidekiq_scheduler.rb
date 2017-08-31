b38b445f99eb0f97b7c3fd52a9f46a829fd0f61e
Shawn Gatchell <shawng03@gmail.com>
2017-08-31 16:47:45 -0700

    Revert "Try using the scheduler"
    This reverts commit 9516fac7a83d8ebeaac3b433eb54ec5cca9521bd.

--- config/initializers/sidekiq_scheduler.rb
+++ config/initializers/sidekiq_scheduler.rb
@@ -1,24 +1,14 @@
 require 'sidekiq'
 require 'sidekiq/scheduler'
 
-# if TahiEnv.ithenticate_enabled?
-#   Sidekiq.configure_server do |config|
-#     config.on(:startup) do
-#       Sidekiq.set_schedule(
-#         'SimilarityCheckUpdate',
-#         'every' => ['10s'],
-#         'class' => 'SimilarityCheckUpdateWorker'
-#       )
-#     end
-#   end
-# end
-
-Sidekiq.configure_server do |config|
-  config.on(:startup) do
-    Sidekiq.set_schedule(
-      'ScheduledEvents',
-      'every' => ['1m'],
-      'class' => 'ScheduledEventsWorker'
-    )
+if TahiEnv.ithenticate_enabled?
+  Sidekiq.configure_server do |config|
+    config.on(:startup) do
+      Sidekiq.set_schedule(
+        'SimilarityCheckUpdate',
+        'every' => ['10s'],
+        'class' => 'SimilarityCheckUpdateWorker'
+      )
+    end
   end
 end

4098ddab1e11f9aef2935f4aeb6a3f3993dd96a9
Michael Ogala <michael.ogala@andela.com>
2017-08-31 19:43:58 +0100

    Change mailer to send the thank you email later

--- app/controllers/reviewer_reports_controller.rb
+++ app/controllers/reviewer_reports_controller.rb
@@ -4,6 +4,7 @@
 # A reviewer report owns the nested question answers for a given review
 #
 class ReviewerReportsController < ApplicationController
+  include TahiStandardTasks
   before_action :authenticate_user!
   respond_to :json
 
@@ -20,7 +21,9 @@ class ReviewerReportsController < ApplicationController
       reviewer_report.schedule_events if FeatureFlag[:REVIEW_DUE_AT]
     end
     reviewer_report.submit! if reviewer_report_params[:submitted].present?
-    TahiStandardTasks::ReviewerMailer.thank_reviewer(reviewer_report: @reviewer_report).deliver_now! if @reviewer_report.state == 'submitted'
+    if @reviewer_report.state == 'submitted'
+      ReviewerMailer.thank_reviewer(reviewer_report: @reviewer_report).deliver_later
+    end
 
     # return the updated report if the due date changed
     if reviewer_report_params.slice(:due_at).empty?

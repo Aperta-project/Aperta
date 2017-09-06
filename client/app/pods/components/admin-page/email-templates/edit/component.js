d543a463a3fd2c11cc8daaeff8ba1c4f931381a9
Michael Ogala <michael.ogala@andela.com>
2017-09-06 18:22:48 +0100

    Retrieve error from rails validators

--- client/app/pods/components/admin-page/email-templates/edit/component.js
+++ client/app/pods/components/admin-page/email-templates/edit/component.js
@@ -11,15 +11,29 @@ export default Ember.Component.extend({
     return !this.get('template.subject') || !this.get('template.body');
   }),
   unsaved: true,
+  subjectError: '',
+  bodyError: '',
   actions: {
     save: function() {
       if (this.get('disabled') || this.get('template.isSaving')) {
         this.set('unsaved', false);
       } else {
-        this.get('template').save().then(() => {
-          this.get('routing').transitionTo('admin.journals.emailtemplates', this.get('template.journal.id'));
-        });
+        this.get('template').save()
+          .then(() => {
+            this.get('routing').transitionTo('admin.journals.emailtemplates', this.get('template.journal.id'));
+          })
+          .catch(error => {
+            let subjectError = error.errors.filter((e) => e.source.pointer.includes('subject'));
+            let bodyError = error.errors.filter((e) => e.source.pointer.includes('body'));
+            if (subjectError) {
+              this.set('subjectError', subjectError[0].detail);
+            }
+            if (!bodyError.length === 0) {
+              this.set('bodyError', bodyError[0].detail);
+            }
+          });
       }
     }
   }
 });
+

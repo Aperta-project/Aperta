2810e987b616d6330479b9b71e133ee4a59bdd79
Jim Haungs <jhaungs@plos.org>
2017-09-06 08:25:43 -0700

    Convert blanks to nil; use type from attributes hash

--- app/models/concerns/attributable.rb
+++ app/models/concerns/attributable.rb
@@ -3,15 +3,11 @@ module Attributable
 
   CONTENT_ATTRIBUTES = {
     boolean: %w[allow_annotations allow_file_captions allow_multiple_uploads required_field],
+    json:    %w[possible_values],
     string:  %w[child_tag condition custom_class custom_child_class default_answer_value
                 editor_style error_message instruction_text label text value_type
-                visible_with_parent_answer wrapper_tag],
-    json:    %w[possible_values]
+                visible_with_parent_answer wrapper_tag]
   }.freeze
-
-  ATTRIBUTE_TYPES = CONTENT_ATTRIBUTES.each_with_object({}) do |(type, names), hash|
-    names.each { |name| hash[name] = type }
-  end.freeze
 
   included do
     has_many :content_attributes, dependent: :destroy, inverse_of: :card_content
@@ -20,7 +16,7 @@ module Attributable
       content_attributes.each_with_object({}) { |each, hash| hash[each.name] = each.value }.compact
     end
 
-    CONTENT_ATTRIBUTES.each do |_type, names|
+    CONTENT_ATTRIBUTES.each do |type, names|
       names.each do |name|
         getter = "#{name}_attribute".to_sym
         setter = "#{name}_attribute=".to_sym
@@ -32,8 +28,8 @@ module Attributable
         end
 
         define_method("#{name}=") do |new_value|
-          content_attribute = content_attributes.where(name: name, value_type: ATTRIBUTE_TYPES[name]).first_or_initialize
-          content_attribute.value = new_value
+          content_attribute = content_attributes.where(name: name, value_type: type).first_or_initialize
+          content_attribute.value = new_value.presence
           send(setter, content_attribute)
         end
       end

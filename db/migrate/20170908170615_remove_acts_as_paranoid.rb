7ed34931568e825748521ccd1b01eef94d25f86c
Jim Haungs <jhaungs@plos.org>
2017-09-08 11:05:50 -0700

    Remove faux-deleted records for real

--- db/migrate/20170908170615_remove_acts_as_paranoid.rb
+++ db/migrate/20170908170615_remove_acts_as_paranoid.rb
@@ -1,9 +1,16 @@
 class RemoveActsAsParanoid < ActiveRecord::Migration
-  def change
-    remove_column :answers, :deleted_at
-    remove_column :card_content_validations, :deleted_at
-    remove_column :card_contents, :deleted_at
-    remove_column :card_versions, :deleted_at
-    remove_column :cards, :deleted_at
+  TABLES = %i[answers card_content_validations card_contents card_versions cards].freeze
+
+  def up
+    TABLES.each do |table|
+      execute "delete from #{table} where deleted_at is not null"
+      remove_column table, :deleted_at
+    end
+  end
+
+  def down
+    TABLES.each do |table|
+      add_column table, :deleted_at, :timestamp
+    end
   end
 end

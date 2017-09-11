22183ba54fe94eb273fffb246bc8b8bb5e800e54
Andrew Max <anm898989@gmail.com>
2017-09-12 04:00:58 +0800

    APERTA-11067 updates admin all journal logic

--- client/app/pods/components/admin-page/tab-bar/component.js
+++ client/app/pods/components/admin-page/tab-bar/component.js
@@ -1,12 +1,35 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Component.extend({
   classNames: ['admin-tab-bar'],
   can: Ember.inject.service(),
 
-  canAdminJournal: Ember.computed(function() {
+  canAdminJournal: Ember.computed('journal', function() {
     let journal = this.get('journal');
-    let journalTerm = journal ? journal : 'Journal';
-    return this.get('can').can('administer', journalTerm);
+    if (journal) {
+      return this.get('can').can('administer', journal);
+    } else {
+      let promises = this._journalPermissionPromises(this.get('journals'));
+      return this._canAdministerAllJournals(promises);
+    }
   }),
+
+  _journalPermissionPromises(journals) {
+    let promises = [];
+    journals.forEach((journal) => {
+      promises.push(new RSVP.Promise((resolve) => {
+        resolve(this.get('can').can('administer', journal));
+      }));
+    });
+
+    return promises;
+  },
+
+  _canAdministerAllJournals(permissionsPromises) {
+    return RSVP.Promise.all(permissionsPromises).then((values) => {
+      let falses = values.filter((value) => { value === false; });
+      return Ember.isEmpty(falses);
+    });
+  }
 });

f1718a0738991632d14651558af9404e8f4e6c9d
Shawn Gatchell <shawng03@gmail.com>
2017-09-11 13:58:08 -0700

    Ember warns on nov away from dirty template

--- client/app/pods/admin/edit_email/route.js
+++ client/app/pods/admin/edit_email/route.js
@@ -1,16 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  showDirtyOverlay: false,
   model(params) {
-    var id = params.email_id;
-    return this.store.findRecord('letter-template', id, {reload: true});
+    return this.store.findRecord('letter-template', params.email_id, {reload: true});
   },
+
   actions: {
-    willTransition: function () {
+    willTransition(transition) {
       let model = this.currentModel;
-      if (model.get('hasDirtyAttributes')) {
-        model.rollbackAttributes();
+      let hasDirtyBody = !!(model.get('hasDirtyAttributes') && model.changedAttributes()['body']);
+      if(!hasDirtyBody) {
+        return true;
       }
+
+      this.set('previousTransition', transition);
+      transition.abort();
+      this.set('controller.showDirtyOverlay', true);
+    },
+
+    allowStoppedTransition() {
+      this.set('controller.showDirtyOverlay', false);
+      let transition = this.get('previousTransition');
+      this.transitionTo(transition.targetName);
     }
   }
 });
--- client/app/pods/components/admin-page/email-templates/edit/component.js
+++ client/app/pods/components/admin-page/email-templates/edit/component.js
@@ -5,12 +5,14 @@ import Ember from 'ember';
 // new templates.
 
 export default Ember.Component.extend({
+  showDirtyOverlay: false,
   store: Ember.inject.service(),
   routing: Ember.inject.service('-routing'),
   disabled: Ember.computed('template.subject', 'template.body', function() {
     return !this.get('template.subject') || !this.get('template.body');
   }),
   unsaved: true,
+  allowStoppedTransition: 'allowStoppedTransition',
   actions: {
     save: function() {
       if (this.get('disabled') || this.get('template.isSaving')) {
@@ -20,6 +22,13 @@ export default Ember.Component.extend({
           this.get('routing').transitionTo('admin.journals.emailtemplates', this.get('template.journal.id'));
         });
       }
+    },
+    cleanEmailTemplate: function() {
+      let model = this.get('template');
+      if (model.get('hasDirtyAttributes')) {
+        model.rollbackAttributes();
+      }
+      this.sendAction('allowStoppedTransition');
     }
   }
 });

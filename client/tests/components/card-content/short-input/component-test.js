fcb469bb27fd4719e2dfe6a5b80ce68bbe468874
Andres Visbal <andresvisbal@gmail.com>
2017-09-12 15:30:50 -0500

    APERTA-10450 Makes some inputs html
    - Use paragraph input with the inline editor style
    - Fix paragraph input so it passes ident to the rich text editor
    - Format short-input test using prettier, and make sure the tests pass propTypes
    checks

--- client/tests/components/card-content/short-input/component-test.js
+++ client/tests/components/card-content/short-input/component-test.js
@@ -11,6 +11,8 @@ moduleForComponent(
     beforeEach() {
       registerCustomAssertions();
       this.set('actionStub', function() {});
+      this.set('answer', Ember.Object.create());
+      this.set('content', Ember.Object.create());
     }
   }
 );
@@ -22,12 +24,12 @@ disabled=disabled
 valueChanged=(action actionStub)
 }}`;
 test(`it displays the text from content.text in a <label>`, function(assert) {
-  this.set('content', {text: 'Foo'});
+  this.set('content', Ember.Object.create({ text: 'Foo' }));
   this.render(template);
   assert.textPresent('.content-text', 'Foo');
 });
 test(`it displays unescaped html text`, function(assert) {
-  this.set('content', {text: '<b class="foo">Foo</b>'});
+  this.set('content', Ember.Object.create({ text: '<b class="foo">Foo</b>' }));
   this.render(template);
   assert.elementFound('b.foo');
 });
@@ -37,13 +39,13 @@ test(`it disables the input if disabled=true`, function(assert) {
   assert.elementFound('input[disabled]');
 });
 test(`it displays the value from answer.value`, function(assert) {
-  this.set('answer', {value: 'Bar'});
+  this.set('answer', Ember.Object.create({ value: 'Bar' }));
   this.render(template);
   assert.equal(this.$('input').val(), 'Bar');
 });
 test(`it sends 'valueChanged' on change`, function(assert) {
   assert.expect(1);
-  this.set('answer', {value: 'Old'});
+  this.set('answer', Ember.Object.create({ value: 'Old' }));
   this.set('actionStub', function(newVal) {
     assert.equal(newVal, 'New', 'it calls the action with the new value');
   });
@@ -52,19 +54,33 @@ test(`it sends 'valueChanged' on change`, function(assert) {
 });
 test(`it sends 'valueChanged' on input`, function(assert) {
   assert.expect(1);
-  this.set('answer', {value: 'Old'});
+  this.set('answer', Ember.Object.create({ value: 'Old' }));
   this.set('actionStub', function(newVal) {
     assert.equal(newVal, 'New', 'it calls the action with the new value');
   });
   this.render(template);
   this.$('input').val('New').trigger('input').trigger('blur');
 });
-test('it displays error messages if present', function(assert){
+test('it displays error messages if present', function(assert) {
   let errorsArr = ['Oh Noes', 'You fool!'];
-  this.set('answer', Ember.Object.create({readyIssuesArray: errorsArr, hasErrors: true}));
+  this.set(
+    'answer',
+    Ember.Object.create({ readyIssuesArray: errorsArr, hasErrors: true })
+  );
   this.render(template);
   assert.equal(this.$('.validation-error').length, 2, 'Two errors are present');
-  assert.equal(this.$('.validation-error').eq(0).text(), errorsArr[0], 'First error text matches');
-  assert.equal(this.$('.validation-error').eq(1).text(), errorsArr[1], 'Second error text matches');
-  assert.ok(this.$('.card-content-short-input').hasClass('has-error'), 'error class present on parent element');
+  assert.equal(
+    this.$('.validation-error').eq(0).text(),
+    errorsArr[0],
+    'First error text matches'
+  );
+  assert.equal(
+    this.$('.validation-error').eq(1).text(),
+    errorsArr[1],
+    'Second error text matches'
+  );
+  assert.ok(
+    this.$('.card-content-short-input').hasClass('has-error'),
+    'error class present on parent element'
+  );
 });

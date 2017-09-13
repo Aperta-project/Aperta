39fb35fbdbcecf2b716b8e3eba8699c1d6137b1b
Michael Ogala <michael.ogala@andela.com>
2017-09-13 10:38:24 +0100

    Minor correction

--- client/app/pods/components/save-button/component.js
+++ client/app/pods/components/save-button/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'button',
   text: '',
-  displayProgressBar: false,
+  displaySpinner: false,
   size: 'small',
   color: 'blue',
   attributeBindings: ['disabled'],

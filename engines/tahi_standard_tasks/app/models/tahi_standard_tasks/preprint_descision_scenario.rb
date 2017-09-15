8d5b1f6e36fcb48873260e81db0672b2ee03a886
Andrew Max <anm898989@gmail.com>
2017-09-15 18:08:45 +0800

    APERTA-10772 preprint-decision-letters-in-template-list

---
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/preprint_descision_scenario.rb
@@ -0,0 +1,23 @@
+module TahiStandardTasks
+  # Provides a template context for PreprintDecisionTasks
+  class PreprintDecisionScenario < TemplateScenario
+    def self.merge_field_definitions
+      [{ name: :manuscript, context: PaperContext },
+       { name: :journal, context: JournalContext }]
+    end
+
+    def manuscript
+      @manuscript ||= PaperContext.new(paper)
+    end
+
+    def journal
+      @journal ||= JournalContext.new(paper.journal)
+    end
+
+    private
+
+    def paper
+      @object
+    end
+  end
+end

75703d75fc8691c42be14e821a259de83bc5ee2b
Erik Hetzner <ehetzner@plos.org>
2017-09-18 10:41:16 -0700

    Use with_creator_role trait, not with_roles_and_permissions

--- spec/services/paper_factory_spec.rb
+++ spec/services/paper_factory_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe PaperFactory do
-  let(:journal) { FactoryGirl.create(:journal, :with_roles_and_permissions, :with_default_task_types) }
+  let(:journal) { FactoryGirl.create(:journal, :with_creator_role, :with_default_task_types) }
   let(:card) { FactoryGirl.create(:card, :versioned) }
   let(:mmt) do
     FactoryGirl.create(:manuscript_manager_template, paper_type: "Science!").tap do |mmt|
@@ -30,7 +30,6 @@ describe PaperFactory do
     end
   end
 
-  let!(:role) { journal.creator_role }
   let(:user) { FactoryGirl.create :user }
 
   describe ".create" do

c6ba92326ddf64b0d563e2bca113b8ea3aaeebcc
Erik Hetzner <ehetzner@plos.org>
2017-09-22 13:35:16 -0700

    Add ident, order to card content serializer

--- app/serializers/card_content_serializer.rb
+++ app/serializers/card_content_serializer.rb
@@ -1,21 +1,25 @@
 class CardContentSerializer < ActiveModel::Serializer
   attributes :id,
              :content_type,
-             :unsorted_child_ids
+             :unsorted_child_ids,
+             :ident,
+             :order
+
   def order
     object.lft
   end
 
-  # Doing it this way is faster than hitting each attribute
+  # Doing it this way is much faster than hitting each attribute
   def attributes
     hash = super
     object.content_attributes.each do |attr|
-      hash[attr.name] = attr.value
+      hash[attr.name.to_sym] = attr.value
     end
     hash
   end
 
   def unsorted_child_ids
+    # Memoize this because it is called multiple times.
     @unsorted_child_ids ||= object.quick_unsorted_child_ids
   end
 end

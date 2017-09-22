99182fb568c216fea5b44186be75795c99f03744
Erik Hetzner <ehetzner@plos.org>
2017-09-22 12:43:41 -0700

    Serialize contents with card_version

--- app/serializers/card_version_serializer.rb
+++ app/serializers/card_version_serializer.rb
@@ -2,8 +2,13 @@ class CardVersionSerializer < ActiveModel::Serializer
   attributes :id, :version, :card_id,
              :history_entry, :published_by, :published_at
   has_one :content_root, embed: :id, include: true, root: :card_contents
+  has_many :contents, embed: :ids, include: true, root: :card_contents
 
   def published_by
     object.published_by.try(:full_name)
+  end
+
+  def contents
+    @contents ||= object.content_root.quick_load_descendants
   end
 end

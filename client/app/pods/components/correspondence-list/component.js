2e0e4f0e1a77c3f9b06bad11d23923d26e18bfb1
Yetunde Sanni <yetunde.sanni@andela.com>
2017-10-03 16:44:26 +0100

    APERTA-11153-updated-code-to-reflect-AC

--- client/app/pods/components/correspondence-list/component.js
+++ client/app/pods/components/correspondence-list/component.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
-
+  isRecordLost: Ember.computed('firstSubmittedAt', function() {
+    let dateSubmitted = moment(this.get('paper').get('firstSubmittedAt'));
+    let dateChanged = moment('February 1, 2017');
+    return dateSubmitted < dateChanged;
+  })
 });

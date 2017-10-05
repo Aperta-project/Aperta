12cafa8b507e2b1909232ab433e36ee0d2dda620
John Fesenko <jfesenko@plos.org>
2017-10-05 16:31:06 -0700

    APERTA-11251: replace SQL migration with rake task

--- db/migrate/20171005144241_change_text_content_type_to_description.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTextContentTypeToDescription < ActiveRecord::Migration
-  def up
-    execute "UPDATE card_contents SET content_type='description' WHERE content_type='text'"
-  end
-
-  def down
-    execute "UPDATE card_contents SET content_type='text' WHERE content_type='description'"
-  end
-end

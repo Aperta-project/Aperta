5c1295feb1cd57d4d0e183286f46a3682b3863d1
John Fesenko <jfesenko@plos.org>
2017-10-05 16:49:54 -0700

    APERTA-11251: update migration to trigger rake task

--- lib/tasks/data-migrations/APERTA-11251_change_text_content_type_to_description.rb
+++ lib/tasks/data-migrations/APERTA-11251_change_text_content_type_to_description.rb
@@ -3,7 +3,7 @@ namespace :data do
     desc <<-DESC
       Rename card content "text" content type to "description"
     DESC
-      task change_text_content_type_to_description: :environment do
+      task aperta_11251_change_text_content_type_to_description: :environment do
         CardContents.where(content_type: "text").update_all(content_type: "description")
       end
   end

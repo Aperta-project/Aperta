47232264106d41e080dd0e2106cf0492f3e97046
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2017-10-24 16:22:26 -0400

    Additional spec for APERTA-11151

--- spec/controllers/repetitions_controller_spec.rb
+++ spec/controllers/repetitions_controller_spec.rb
@@ -51,7 +51,8 @@ describe RepetitionsController do
             repetition: {
               card_content_id: card_content.id,
               task_id: task.id,
-              parent_id: parent_repetition.id
+              parent_id: parent_repetition.id,
+              position: 0
             }
     end
 
@@ -67,6 +68,54 @@ describe RepetitionsController do
 
       it "creates a repetition" do
         expect { do_request }.to change(Repetition, :count).by(1)
+      end
+    end
+
+    context "when the user does not have access" do
+      before do
+        stub_sign_in(user)
+        allow(user).to receive(:can?)
+          .with(:edit, task)
+          .and_return(false)
+      end
+
+      it { is_expected.to responds_with(403) }
+    end
+  end
+
+  describe "#update" do
+    let(:task) { FactoryGirl.create(:custom_card_task, :with_card) }
+    let(:card_content) { task.card_version.card_contents.first }
+    let!(:repetition_a) { FactoryGirl.create(:repetition, task: task, card_content: card_content, position: 0) }
+    let!(:repetition_b) { FactoryGirl.create(:repetition, task: task, card_content: card_content, position: 1) }
+    let!(:repetition_c) { FactoryGirl.create(:repetition, task: task, card_content: card_content, position: 2) }
+
+    subject(:do_request) do
+      put :update,
+            format: 'json',
+            id: repetition_c.id,
+            repetition: {
+              card_content_id: card_content.id,
+              task_id: task.id,
+              position: 0
+            }
+    end
+
+    it_behaves_like "an unauthenticated json request"
+
+    context "when the user has access" do
+      before do
+        stub_sign_in(user)
+        allow(user).to receive(:can?)
+          .with(:edit, task)
+          .and_return(true)
+      end
+
+      it "updates a repetition position (along with siblings)" do
+        do_request
+        expect(repetition_a.reload.position).to be(1)
+        expect(repetition_b.reload.position).to be(2)
+        expect(repetition_c.reload.position).to be(0)
       end
     end
 

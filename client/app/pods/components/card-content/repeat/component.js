fb1efe0660f7f9d9ed77b4fc88d37b5a38fd4c30
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2017-10-25 17:33:59 -0400

    Add repeat component

---
+++ client/app/pods/components/card-content/repeat/component.js
@@ -0,0 +1,116 @@
+import Ember from 'ember';
+import { PropTypes } from 'ember-prop-types';
+
+export default Ember.Component.extend({
+  classNames: ['card-content-repeat'],
+  store: Ember.inject.service(),
+  min: Ember.computed.readOnly('content.min'),
+  max: Ember.computed.readOnly('content.max'),
+  itemName: Ember.computed.readOnly('content.itemName'),
+
+  propTypes: {
+    content: PropTypes.EmberObject.isRequired,
+    disabled: PropTypes.bool,
+    owner: PropTypes.EmberObject.isRequired,
+    repetition: PropTypes.oneOfType([PropTypes.null, PropTypes.EmberObject]).isRequired,
+    preview: PropTypes.bool
+  },
+
+  init() {
+    this._super(...arguments);
+
+    Ember.run.once(this, 'addMinimumRepetitions');
+  },
+
+  addMinimumRepetitions() {
+    let toAdd =  this.get('min') - this.get('repetitions.length');
+    for(let i = 0; i < toAdd; i++) {
+      this.buildRepetition();
+    }
+  },
+
+  unsortedRepetitions: Ember.computed('content.repetitions.@each.isDeleted', 'repetition', 'owner', function() {
+    let parent = this.get('repetition');
+    return this.ownedRepetitions().rejectBy('isDeleted').filter(function(rep) {
+      if(parent) {
+        return rep.get('parent') === parent;
+      } else {
+        return !rep.get('parent');
+      }
+    });
+  }),
+  ownedRepetitions() {
+    let contentRepetitions = this.get('content.repetitions');
+
+    if(this.get('preview')){
+      // scope repetitions to the card-preview
+      return contentRepetitions.filterBy('task.id', undefined);
+    } else {
+      // scope repetitions to the resolved task
+      return contentRepetitions.filterBy('task.id', this.get('owner.id'));
+    }
+  },
+  repetitionSort: ['position:asc'],
+  repetitions: Ember.computed.sort('unsortedRepetitions', 'repetitionSort'),
+
+  addLabelText: Ember.computed('itemName', function() {
+    return `Add ${this.get('itemName')}`;
+  }),
+
+  deleteLabelText: Ember.computed('itemName', function() {
+    return `Delete ${this.get('itemName')}`;
+  }),
+
+  canDeleteRepetition: Ember.computed('disabled', 'repetitions.[]', function() {
+    if(this.get('disabled')) {
+      return false;
+    }
+    if(this.get('min')) {
+      return this.get('repetitions.length') > this.get('min');
+    } else {
+      return true;
+    }
+  }),
+
+  canAddRepetition: Ember.computed('disabled', 'repetitions.[]', function() {
+    if(this.get('disabled')) {
+      return false;
+    }
+    if(this.get('max')){
+      return this.get('repetitions.length') < this.get('max');
+    } else {
+      return true;
+    }
+  }),
+
+  buildRepetition(previousRepetition) {
+    let position = previousRepetition ? previousRepetition.get('position') + 1 : this.get('repetitions.length');
+    let repetition = this.get('store').createRecord('repetition', {
+      cardContent: this.get('content'),
+      parent: this.get('repetition'),
+      position: position,
+    });
+
+    if(!this.get('preview')) {
+      // a task relationship is only applicable in non-previews (card-editor/previews aren't tasks)
+      repetition.set('task', this.get('owner'));
+      repetition.save();
+    }
+
+    return repetition;
+  },
+
+  actions: {
+    addRepetition(repetition) {
+      if(this.get('canAddRepetition')) {
+        this.buildRepetition(repetition);
+      }
+    },
+
+    deleteRepetition(repetition) {
+      if(this.get('canDeleteRepetition')) {
+        repetition.cascadingDestroy();
+      }
+    },
+  }
+});
---
+++ client/tests/components/card-content/repeat/component-basic-test.js
@@ -0,0 +1,217 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import registerCustomAssertions from 'tahi/tests/helpers/custom-assertions';
+import { manualSetup, make } from 'ember-data-factory-guy';
+import hbs from 'htmlbars-inline-precompile';
+import wait from 'ember-test-helpers/wait';
+
+moduleForComponent(
+  'card-content/repeat',
+  'Integration | Component | card content | repeat (basic)',
+  {
+    integration: true,
+    beforeEach() {
+      registerCustomAssertions();
+      manualSetup(this.container);
+      this.registry.register('pusher:main', Ember.Object.extend({socketId: 'foo'}));
+    },
+
+    afterEach() {
+      $.mockjax.clear();
+    }
+  }
+);
+
+let template = hbs`{{card-content/repeat
+  content=content
+  disabled=disabled
+  owner=owner
+  repetition=repetition
+  preview=false
+}}`;
+
+test(`it builds the minimum number of repetitions that were specified`, function(assert) {
+  let repetitionIdSeed = 110;
+  $.mockjax({url: '/api/repetitions', type: 'POST', status: 201, response: function() {
+    this.responseText = {
+      repetition: { id: repetitionIdSeed++ },
+    };
+  }});
+
+  let content = make('card-content', {
+    contentType: 'repeat',
+    min: 2,
+    itemName: 'thing',
+    repetitions: [],
+    unsortedChildren: [
+      make('card-content', {
+        contentType: 'short-input',
+        text: 'Can you answer my simple question?'
+      })
+    ]
+  });
+
+  this.set('content', content);
+  this.set('disabled', false);
+  this.set('owner', Ember.Object.create());
+  this.set('repetition', null);
+  this.render(template);
+
+  return wait().then(() => {
+    assert.nElementsFound('.card-content-repeat input', 2, 'found input');
+  });
+});
+
+test(`it builds no repetitions when no minimum number of repetitions are specified`, function(assert) {
+  let content = make('card-content', {
+    contentType: 'repeat',
+    min: null,
+    itemName: 'thing',
+    repetitions: [],
+    unsortedChildren: [
+      make('card-content', {
+        contentType: 'short-input',
+        text: 'Can you answer my simple question?'
+      })
+    ]
+  });
+
+  this.set('content', content);
+  this.set('disabled', false);
+  this.set('owner', Ember.Object.create());
+  this.set('repetition', null);
+  this.render(template);
+
+  return wait().then(() => {
+    assert.elementNotFound('.card-content-repeat input', 'did not find input');
+  });
+});
+
+
+test(`it does not allow more repetitions to be created when max number is reached`, function(assert) {
+  let repetitionIdSeed = 110;
+  $.mockjax({url: '/api/repetitions', type: 'POST', status: 201, response: function() {
+    this.responseText = {
+      repetition: { id: repetitionIdSeed++ },
+    };
+  }});
+
+  let content = make('card-content', {
+    contentType: 'repeat',
+    min: null,
+    max: 1,
+    itemName: 'thing',
+    repetitions: [],
+    unsortedChildren: [
+      make('card-content', {
+        contentType: 'short-input',
+        text: 'Can you answer my simple question?'
+      })
+    ]
+  });
+
+  this.set('content', content);
+  this.set('disabled', false);
+  this.set('owner', Ember.Object.create());
+  this.set('repetition', null);
+  this.render(template);
+
+  // ensure add button is enabled and can be clicked
+  assert.elementNotFound('.card-content-repeat input', 'did not find input');
+  assert.elementFound('.add-repetition:not(.disabled)', 'found enabled add button');
+  this.$('.add-repetition').click();
+
+  return wait().then(() => {
+    // ensure that repetition was added and add button is now disabled
+    // since max number of repetitions was reached
+    assert.nElementsFound('.card-content-repeat input', 1, 'found input');
+    assert.elementFound('.add-repetition.disabled', 'found disabled add button');
+  });
+});
+
+test(`it only allows repetitions to be deleted when min number is reached`, function(assert) {
+  let repetitionIdSeed = 110;
+  $.mockjax({url: '/api/repetitions', type: 'POST', status: 201, response: function() {
+    this.responseText = {
+      repetition: { id: repetitionIdSeed++ },
+    };
+  }});
+
+
+  let content = make('card-content', {
+    contentType: 'repeat',
+    min: 1,
+    max: null,
+    itemName: 'thing',
+    repetitions: [],
+    unsortedChildren: [
+      make('card-content', {
+        contentType: 'short-input',
+        text: 'Can you answer my simple question?'
+      })
+    ]
+  });
+
+  this.set('content', content);
+  this.set('disabled', false);
+  this.set('owner', Ember.Object.create());
+  this.set('repetition', null);
+  this.render(template);
+
+
+  return wait().then(() => {
+    // ensure delete button is not enabled
+    assert.elementNotFound('.remove-repetition', 'remove button is not found');
+    this.$('.add-repetition').click();
+
+    return wait().then(() => {
+      assert.nElementsFound('.remove-repetition', 2, 'remove buttons found');
+    });
+  });
+});
+
+test(`it only shows repetitions that are owned by a specific task `, function(assert) {
+  $.mockjax({url: '/api/repetitions', type: 'POST', status: 201, responseText: {repetition: { id: 1 }}});
+  $.mockjax({url: '/api/answers', type: 'POST', status: 201, responseText: {answer: {id: 1}}});
+
+  let store = this.container.lookup('service:store');
+
+  let content = make('card-content', {
+    contentType: 'repeat',
+    min: 1,
+    itemName: 'thing',
+    repetitions: [],
+    unsortedChildren: [
+      make('card-content', {
+        contentType: 'short-input',
+        text: 'Can you answer my simple question?',
+      })
+    ]
+  });
+  let inputCC = content.get('unsortedChildren.firstObject');
+
+  // create two different tasks
+  let taskOne = make('custom-card-task');
+  let taskTwo = make('custom-card-task');
+
+  // create two repetitions where each is associated to different tasks, but same card content
+  let repOne = make('repetition', { task: taskOne, cardContent: content });
+  let repTwo = make('repetition', { task: taskTwo, cardContent: content });
+
+  // assign a different answer to each repetition's card content
+  make('answer', { value: 'answer one', owner: taskOne, repetition: repOne, cardContent: inputCC });
+  make('answer', { value: 'answer two', owner: taskTwo, repetition: repTwo, cardContent: inputCC });
+
+  // render template with taskOne as the owner
+  this.set('content', content);
+  this.set('disabled', false);
+  this.set('owner', taskOne);
+  this.set('repetition', null);
+  this.render(template);
+
+  return wait().then(() => {
+    assert.equal(store.peekAll('repetition').get('length'), 2, 'two repetitions for card content already exist');
+    assert.nElementsFound('.repeated-block', 1, 'should only a single repetition');
+    assert.inputPresent('.repeated-block input', 'answer one', 'only the answer for the owner is being shown');
+  });
+});

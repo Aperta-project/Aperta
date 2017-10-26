feeac5df638ff42b24924fc85cad9af98b8afe82
Edem Kumodzi <ekumodzi@plos.org>
2017-10-26 15:25:44 +0100

    fixing tests

--- app/controllers/reviewer_reports_controller.rb
+++ app/controllers/reviewer_reports_controller.rb
@@ -14,9 +14,15 @@ class ReviewerReportsController < ApplicationController
 
   def update
     requires_user_can :edit, reviewer_report.task
-    update_review_date
+    if FeatureFlag[:REVIEW_DUE_DATE]
+      due_at_date = reviewer_report_params.slice(:due_at)
+      if due_at_date.present? && due_at_date['due_at'].to_datetime != reviewer_report.due_at
+        requires_user_can :edit_due_date, reviewer_report.task
+        reviewer_report.due_datetime.update_attributes due_at_date
+      end
+      reviewer_report.schedule_events if FeatureFlag[:REVIEW_DUE_AT]
+    end
     reviewer_report.submit! if reviewer_report_params[:submitted].present?
-
     # return the updated report if the due date changed
     render_report
   end
@@ -28,17 +34,6 @@ class ReviewerReportsController < ApplicationController
       respond_with reviewer_report
     else
       render json: reviewer_report
-    end
-  end
-
-  def update_review_date
-    if FeatureFlag[:REVIEW_DUE_DATE]
-      due_at_date = reviewer_report_params.slice(:due_at)
-      if due_at_date.present? && due_at_date['due_at'].to_datetime != reviewer_report.due_at
-        requires_user_can :edit_due_date, reviewer_report.task
-        reviewer_report.due_datetime.update_attributes due_at_date
-      end
-      reviewer_report.schedule_events if FeatureFlag[:REVIEW_DUE_AT]
     end
   end
 

671c878e142cc9a68f93f9d382e0946098adba60
Jim Haungs <jhaungs@gmail.com>
2017-10-26 12:59:46 -0700

    Adding required-field tests

--- client/tests/components/card-content/component-test.js
+++ client/tests/components/card-content/component-test.js
@@ -40,45 +40,46 @@ test('it creates an answer for card-content', function(assert) {
 });
 
 test('it does not create an answer for non answerables', function(assert) {
-  // add a single piece of non-answerable card content to work with
   let cardContent = FactoryGuy.make('card-content', 'description');
   this.set('task.cardVersion.contentRoot', cardContent);
 
   this.render(hbs`
     {{custom-card-task task=task preview=false}}
   `);
+
   assert.equal(this.get('task.cardVersion.contentRoot.answers.length'), 0, 'there are no answers for a paragraph tag');
 });
 
 test('it renders text for card-content', function(assert) {
-  // add a single piece of non-answerable card content to work with
   let cardContent = FactoryGuy.make('card-content', 'shortInput');
   this.set('task.cardVersion.contentRoot', cardContent);
 
   this.render(hbs`
     {{custom-card-task task=task preview=false}}
   `);
+
   assert.textPresent('.content-text', 'A short input question');
 });
 
 test('it renders a label for card-content', function(assert) {
-  // add a single piece of non-answerable card content to work with
   let cardContent = FactoryGuy.make('card-content', 'shortInput');
   this.set('task.cardVersion.contentRoot', cardContent);
 
   this.render(hbs`
     {{custom-card-task task=task preview=false}}
   `);
+
   assert.elementFound('.content-label');
 });
 
-test(`it displays an indicator if 'isRequired set to true`, function(assert) {
-  let cardContent = FactoryGuy.make('card-content', 'shortInput', { requiredField: true, label: 'Label1', text: 'Text' });
+test('it displays an indicator for required fields', function(assert) {
+  let cardContent = FactoryGuy.make('card-content', 'shortInput', { requiredField: true, label: 'Label', text: 'Text' });
   let root = 'task.cardVersion.contentRoot';
   this.set(root, cardContent);
 
   let text = `${root}.text`;
   let label = `${root}.label`;
+
   let template = hbs`
     {{custom-card-task task=task preview=false}}
   `;
@@ -101,7 +102,7 @@ test(`it displays an indicator if 'isRequired set to true`, function(assert) {
   assert.elementFound('.content-label .required-field', 'shows the required field indicator when neither label nor text are present');
 });
 
-test(`it does not display an field indicator if 'isRequired set to false`, function(assert) {
+test('it does not display an indicator for non-required fields', function(assert) {
   let cardContent = FactoryGuy.make('card-content', 'shortInput', { requiredField: false });
   this.set('task.cardVersion.contentRoot', cardContent);
 

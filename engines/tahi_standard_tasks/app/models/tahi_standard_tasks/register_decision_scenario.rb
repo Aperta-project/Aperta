a8dfa1bb376d0c6131a1793ebd26c2123525add2
Shawn Gatchell <shawng03@gmail.com>
2017-10-25 21:24:15 -0700

    Test review order and rendering separately

--- engines/tahi_standard_tasks/app/models/tahi_standard_tasks/register_decision_scenario.rb
+++ engines/tahi_standard_tasks/app/models/tahi_standard_tasks/register_decision_scenario.rb
@@ -4,8 +4,8 @@ module TahiStandardTasks
       @reviews ||= [].tap do |reviews|
         return [] unless manuscript_object.draft_decision
         reports = manuscript_object.draft_decision.reviewer_reports.submitted
-        reports_with_num, reports_without_num = reports.partition(&:reviewer_number)
-        reports = reports_with_num.sort_by(&:reviewer_number) + reports_without_num.sort_by(&:submitted_at)
+        reports_with_num, reports_without_num = reports.partition { |r| r.task.reviewer_number }
+        reports = reports_with_num.sort_by { |r| r.task.reviewer_number } + reports_without_num.sort_by(&:submitted_at)
         reports.each { |rr| reviews << ReviewerReportContext.new(rr) }
       end
     end

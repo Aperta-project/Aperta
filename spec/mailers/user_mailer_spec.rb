24578a156b4a64b11592fb44b8ab52aab6195d32
Andres Visbal <andresvisbal@gmail.com>
2017-10-31 09:23:06 -0500

    APERTA-10719 Updating tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -236,12 +236,12 @@ describe UserMailer, redis: true do
   end
 
   describe '#notify_creator_of_initial_submission' do
-    let!(:paper) do
+    let!(:letter_template) { FactoryGirl.create(:letter_template, :notify_initial_submission, journal: paper.journal) }
+    let(:paper) do
       FactoryGirl.create(:paper, :with_creator, :initially_submitted)
     end
-    let!(:author) { paper.creator }
-    let!(:letter_template) { FactoryGirl.create(:letter_template, :notify_initial_submission, journal: paper.journal) }
-    let!(:email) { UserMailer.notify_creator_of_initial_submission(paper.id) }
+    let(:author) { paper.creator }
+    let(:email) { UserMailer.notify_creator_of_initial_submission(paper.id) }
 
     it "sends the email to the paper's creator with the correct subject" do
       expect(email.to).to contain_exactly(author.email)

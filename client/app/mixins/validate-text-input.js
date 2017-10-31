e76c2f3dcfae881cfe9ecfaa7e72e2c3f54c536b
Andrew Max <anm898989@gmail.com>
2017-11-01 04:27:13 +0900

    APERTA-10362 cleanup validate input mixin

--- client/app/mixins/validate-text-input.js
+++ client/app/mixins/validate-text-input.js
@@ -2,42 +2,31 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   workingValue: null,
+
   init() {
     this._super(...arguments);
     let value = this.get('answer.value');
-    // If workingValue is passed in to the component then don't
-    // overwrite it with the value of answer.value
+    // If there is a value, make it the workingValue
     if (value && value.length > 0) {
       this.set('workingValue', value);
     }
   },
+
   actions: {
     valueChanged(newValue) {
       this.set('workingValue', newValue);
-      // Be nice to a user who is starting his answer over. This will hide the actual error
-      // message, but will leave the input field's red border while there are still errors.
+      // Hide error messages if field is blank
       if (Ember.isBlank(newValue) || newValue === '<p></p>') this.set('hideError', true);
-
-      // If there were no previous errors, don't save to rails while typing.
-      if (!this.get('answer.hasErrors')) return;
-
-      let action = this.get('valueChanged');
-      if (action) {
-        action(newValue);
-      }
+      // If there were no previous errors, don't hit rails on change
+      if (this.get('answer.hasErrors')) { this.send('validate'); }
     },
 
     validate() {
-      // This gets triggered on blur, which won't pass us the input field's payload, but
-      // fortunately we've been setting it to Ember on each value change.
-      let workingValue = this.get('workingValue');
-      // Show any errors we might have been hiding while the user typed.
+      // Triggered on blur. This doesn't pass the field's current value so we use workingValue
       this.set('hideError', false);
 
       let action = this.get('valueChanged');
-      if (action) {
-        action(workingValue);
-      }
+      if (action) { action(this.get('workingValue')); }
     }
   }
 });

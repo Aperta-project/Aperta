7e7e7d699ca57314d58000a3025e009038db44cb
Andrew Max <anm898989@gmail.com>
2017-11-01 22:59:30 +0900

    APERTA-10362 use answerProxy for input values

--- client/app/mixins/validate-text-input.js
+++ client/app/mixins/validate-text-input.js
@@ -1,26 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
+  answerProxy: null,
 
   init() {
     // in case this input has no answer, a workingValue needs to be manually passed to the component
     this._super(...arguments);
-    let value = this.get('answer.value') || this.get('workingValue');
+    let value = this.get('answer.value');
     this.set('answerProxy', value);
   },
 
   actions: {
     valueChanged(newValue) {
-      //this is essentially functioning as setting `answerProxy` as a computed on asnwer.value
+      //in effect, this makes `answerProxy` a computed on answer.value
       this.set('answerProxy', newValue);
       // Hide error messages if field is blank
       if (Ember.isBlank(newValue) || newValue === '<p></p>') this.set('hideError', true);
-      // If there were no previous errors, don't hit rails on change
-      if (this.get('answer.hasErrors')) { this.send('validate'); }
+
+      // If there were no previous errors, don't save to rails while typing
+      if (this.get('answer.hasErrors')) {
+        let action = this.get('valueChanged');
+        if (action) { action(this.get('answerProxy')); }
+      }
     },
 
     validate() {
-      // Triggered on blur. AnswerProxy is needed becasue blur does not pass the field's value
+      // Triggered on input blur. AnswerProxy is needed becasue blur does not pass the field's value
       this.set('hideError', false);
       let action = this.get('valueChanged');
       if (action) { action(this.get('answerProxy')); }

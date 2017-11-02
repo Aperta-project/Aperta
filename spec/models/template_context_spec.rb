b90c5fdeede6b539cf85f491ff11902a1cb476d3
Shawn Gatchell <shawng03@gmail.com>
2017-11-01 23:27:34 -0700

    Stop "constant already defined" warnings

--- spec/models/template_context_spec.rb
+++ spec/models/template_context_spec.rb
@@ -2,65 +2,54 @@ require 'rails_helper'
 
 describe TemplateContext do
   describe '.context' do
-    context 'without options' do
-      let(:paper) { Paper.new }
-      let(:fake_model) { double("blah") }
-      before do
-        allow(fake_model).to receive(:paper).and_return(paper)
+    let(:fake_model) { double("blah") }
+    let(:context_class) { Class.new(TemplateContext) }
+    let(:context_instance) { context_class.new(fake_model) }
 
-        ExampleContext = Class.new(TemplateContext) do
-          context :paper
-        end
+    context 'without options' do
+      before do
+        context_class.send(:context, :paper)
       end
 
       it 'uses the argument to determine the method name and the context class to use' do
-        example_context = ExampleContext.new(fake_model)
-        expect(example_context.paper.class).to eq PaperContext
+        allow(fake_model).to receive(:paper).and_return(Paper.new)
+
+        expect(context_instance.paper.class).to eq PaperContext
       end
 
       it 'uses the argument as the method to call to get the source object for the context' do
-        example_context = ExampleContext.new(fake_model)
-        expect(example_context.paper.send(:object)).to eq paper
+        paper = Paper.new
+        allow(fake_model).to receive(:paper).and_return(paper)
+
+        expect(context_instance.paper.send(:object)).to eq paper
       end
     end
 
     context 'with a :type option' do
       it 'uses the :type option to determine the context class' do
-        fake_model = double("blah")
         allow(fake_model).to receive(:foo) { Paper.new }
+        context_class.send(:context, :foo, type: :paper)
 
-        ExampleContext = Class.new(TemplateContext) do
-          context :foo, type: :paper
-        end
-
-        expect(ExampleContext.new(fake_model).foo.class).to eq PaperContext
+        expect(context_instance.foo.class).to eq PaperContext
       end
     end
 
     context 'with a :source option' do
       it 'evaluates the :source option to get the object to pass to the context constructor' do
-        fake_model = double("blah")
         allow(fake_model).to receive(:foo) { Paper.new(title: 'foobars in space') }
+        context_class.send(:context, :paper, source: '@object.foo')
 
-        ExampleContext = Class.new(TemplateContext) do
-          context :paper, source: '@object.foo'
-        end
-
-        expect(ExampleContext.new(fake_model).paper.title).to eq 'foobars in space'
+        expect(context_instance.paper.title).to eq 'foobars in space'
       end
     end
 
     context 'with a :many option' do
       it 'returns a collection of contexts' do
-        fake_model = double("blah")
         allow(fake_model).to receive(:papers) { [Paper.new(title: 'first foobar')] }
+        context_class.send(:context, :papers, type: :paper, many: true)
 
-        ExampleContext = Class.new(TemplateContext) do
-          context :papers, type: :paper, many: true
-        end
-
-        expect(ExampleContext.new(fake_model).papers.map(&:class)).to eq [PaperContext]
-        expect(ExampleContext.new(fake_model).papers.first.title).to eq 'first foobar'
+        expect(context_instance.papers.map(&:class)).to eq [PaperContext]
+        expect(context_instance.papers.first.title).to eq 'first foobar'
       end
     end
   end

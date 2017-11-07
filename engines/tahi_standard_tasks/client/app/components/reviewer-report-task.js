acea7c46851294acfa1007bec9e1fdc7eb5feb50
Curtis Autery <curtis@mutuallyhuman.com>
2017-11-07 13:07:32 -0500

    WIP - Edit submitted reviewer reports
    * Introduces ‘delaySave’ field for answer owners, so nested-question’s
    debounced saveAnswer function will no-op
    * Swap “model” with “report” in reviewer-report-questions to make
    meaning clearer
    * Add edit/save/cancel actions to reviewer-report component to allow
    touching an existing report record without auto-saving answers as you
    type
    - Note to self: don’t forget front-matter!
    
    APERTA-6281

--- engines/tahi_standard_tasks/client/app/components/reviewer-report-task.js
+++ engines/tahi_standard_tasks/client/app/components/reviewer-report-task.js
@@ -14,6 +14,13 @@ export default TaskComponent.extend({
   // this property is responsible for displaying (or not) the 'Make changes to this Task' button.
   // It can be modified later to depend on permissions
   taskStateToggleable: false,
+  editing: false,
+  notEditing: Ember.computed.not('editing'),
+
+  changedAnswers: Ember.computed('currentReviewerReport.nestedQuestionAnswers.[]', function() {
+    return this.get('currentReviewerReport.nestedQuestionAnswers')
+      .filter(answer => answer && answer.changedAttributes().value);
+  }),
 
   actions: {
     confirmSubmission() {
@@ -32,6 +39,24 @@ export default TaskComponent.extend({
         this.get('task').save();
         this.get('flash').displayRouteLevelMessage('success', 'Thank you for submitting your review.');
       });
+    },
+
+    // Admin functions to modify a submitted report
+    editReport() {
+      this.set('currentReviewerReport.delaySave', true);
+      this.set('editing', true);
+    },
+
+    saveEdits() {
+      this.set('editing', false);
+      this.get('changedAnswers').forEach(answer => answer.save());
+      this.set('currentReviewerReport.delaySave', false);
+    },
+
+    cancelEdits() {
+      this.set('editing', false);
+      this.get('changedAnswers').forEach(answer => answer.rollbackAttributes());
+      this.set('currentReviewerReport.delaySave', false);
     }
   }
 });

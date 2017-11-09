ed8e88435b0844fd245af92757f26b2ecc13ebeb
Shawn Gatchell <shawng03@gmail.com>
2017-11-09 09:49:39 -0800

    Remove identical scenarios

--- app/models/paper_reviewer_scenario.rb
+++
@@ -1,6 +0,0 @@
-# Provides a template context for PaperReviewerTask
-class PaperReviewerScenario < TemplateContext
-  subcontext :journal,                  source: [:object, :paper, :journal]
-  subcontext :manuscript, type: :paper, source: [:object, :paper]
-  subcontext :invitation,               source: :object
-end

79326258a8a10c40270ddb8802fa45991542e166
samV-plos <svijghen@plos.org>
2017-11-13 11:03:43 -0800

    APERTA-11249 Remove extraneous class

--- client/app/pods/components/card-content/label/component.js
+++ client/app/pods/components/card-content/label/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import { PropTypes } from 'ember-prop-types';
 
 export default Ember.Component.extend({
-  classNames: ['card-content', 'card-form-label'],
+  classNames: ['card-content'],
   propTypes: {
     content: PropTypes.EmberObject.isRequired,
   },

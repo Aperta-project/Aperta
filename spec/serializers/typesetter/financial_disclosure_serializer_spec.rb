a855f20bbf5a5d3faa62122826b056e79051345b
Alex Burkhart + Aaron Christy <aaron+ab+ac@mutuallyhuman.com>
2017-11-13 11:47:14 -0500

    Update Typesetter Exports
    The typesetters are concerned only with the financial summary statement
    that is now found on the FinancialDisclosureStatement.

--- spec/serializers/typesetter/financial_disclosure_serializer_spec.rb
+++ spec/serializers/typesetter/financial_disclosure_serializer_spec.rb
@@ -2,67 +2,64 @@ require 'rails_helper'
 
 describe Typesetter::FinancialDisclosureSerializer do
   subject(:serializer) { described_class.new(task) }
-  let(:author_received_funding) { true }
-  let(:funders) { [] }
-  let(:task) do
-    FactoryGirl.create(:financial_disclosure_task, funders: funders)
-  end
-
+  let(:task) { FactoryGirl.create(:custom_card_task) }
   let(:output) { serializer.serializable_hash }
+  let(:aaron_funder_repetition) { FactoryGirl.create(:repetition, task: task) }
+  let(:alex_funder_repetition) { FactoryGirl.create(:repetition, task: task) }
+  let(:author_received_funding) { true }
 
   let!(:apex_html_flag) { FactoryGirl.create :feature_flag, name: "KEEP_APEX_HTML", active: false }
 
+  let(:received_funding_question) do
+    [{
+      ident: 'financial_disclosures--author_received_funding',
+      answer: author_received_funding,
+      value_type: 'boolean'
+    }]
+  end
+
+  let(:funders) { [Funder.new([], nil), Funder.new([], nil)] }
+  let(:stubbed_financial_disclosure_statement) { double(funding_statement: "here is my statement", funders: funders) }
+
   before do
-    AnswerableFactory.create(
-      task,
-      questions: [
-        {
-          ident: 'financial_disclosures--author_received_funding',
-          answer: author_received_funding,
-          value_type: 'boolean'
-        }
-      ]
-    )
+    allow_any_instance_of(described_class).to receive(:financial_disclosure_statement).and_return(stubbed_financial_disclosure_statement)
   end
 
   it 'has competing interests fields' do
     expect(output.keys).to contain_exactly(
       :author_received_funding,
       :funding_statement,
-      :funders)
+      :funders
+    )
   end
 
-  it 'works without values' do
-    allow(task).to receive(:answer_for).and_return(nil)
-    output = serializer.serializable_hash
+  describe "without any funder answers" do
+    let(:funders) { [] }
 
-    expect(output[:author_received_funding]).to eq(nil)
+    it 'works' do
+      expect(output[:author_received_funding]).to eq(nil)
+      expect(output[:funding_statement]).to eq("here is my statement")
+      expect(output[:funders]).to eq([])
+    end
   end
 
-  describe 'author_recieved_funding' do
+  describe 'author_received_funding' do
+    before { AnswerableFactory.create(task, questions: received_funding_question) }
+
     it 'marks whether the author received funding' do
-      expect(output[:author_received_funding]).to eq(author_received_funding)
+      expect(output[:author_received_funding]).to eq(true)
     end
   end
 
   describe 'funding_statement' do
-    it 'includes the funding statement from the task' do
-      expect(output[:funding_statement]).to eq(task.funding_statement)
+    it 'collects the funding statements from the funders' do
+      expect(output[:funding_statement]).to eq("here is my statement")
     end
   end
 
   describe 'funders' do
-    let(:funders) { [FactoryGirl.create(:funder)] }
-    let(:fake_serialized_funder) { 'Fake funder' }
-    before do
-      expect(Typesetter::FunderSerializer)
-        .to receive(:new).and_return(
-          instance_double('TypeSetter::FunderSerialiser',
-                          serializable_hash: fake_serialized_funder))
-    end
-
     it 'serializes the funders using the typesetter serializer' do
-      expect(output[:funders]).to eq([fake_serialized_funder])
+      expect(output[:funders].length).to eq(2)
     end
   end
 end

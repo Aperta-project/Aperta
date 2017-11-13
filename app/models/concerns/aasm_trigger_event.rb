9a3d535ad6e95d99d9bd05d950faa3849fd780ea
Erik Hetzner <ehetzner@plos.org>
2017-11-13 10:04:53 -0800

    APERTA-11765 Fix activity feed message

--- app/models/concerns/aasm_trigger_event.rb
+++ app/models/concerns/aasm_trigger_event.rb
@@ -5,7 +5,7 @@ module AASMTriggerEvent
     def trigger_aasm_event(aasm, **args)
       Event.trigger(
         self.class.make_event_name(aasm.to_state),
-        activity_message: "Paper state changed to #{aasm.to_state}",
+        event_message: "#{self.class.name} state changed to #{aasm.to_state}",
         from_state: aasm.from_state,
         to_state: aasm.to_state,
         **args

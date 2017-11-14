06f2e7164a0504a2bcad897f3c5c602f86abc4c5
Carlos Guisao Franco <titofranco@gmail.com>
2017-11-14 11:06:46 -0500

    APERTA-11691 Address PR comments

--- client/tests/models/author-test.js
+++ client/tests/models/author-test.js
@@ -19,3 +19,10 @@ test('#Affiliations', function(assert) {
   let author_two = FactoryGuy.make('author', {affiliation: 'QAZ', secondaryAffiliation: 'WSX'});
   assert.equal(author_two.get('affiliations'), 'QAZ, WSX', 'returns both affiliations joined by a comma');
 });
+
+test('#fullNameWithAffiliations', function(assert) {
+  let author = FactoryGuy.make('author', {affiliation: 'ABC', secondAffiliation: null});
+  assert.equal(author.get('fullNameWithAffiliations'), `${author.get('displayName')}, ABC`, 'returns full name with affiliations when the latter exist');
+  let author_two = FactoryGuy.make('author');
+  assert.equal(author_two.get('fullNameWithAffiliations'), `${author_two.get('displayName')}`, 'returns only full name if author does not have affiliations');
+});

18022f9959808e919130aa5036979d17002019c3
Michael Ogala <michael.ogala@andela.com>
2017-11-15 20:44:52 +0100

    Test that last change made displays

--- client/tests/components/correspondence-details-test.js
+++ client/tests/components/correspondence-details-test.js
@@ -27,3 +27,22 @@ test('shows the history if there are activities', function(assert) {
   assert.textPresent('.history-heading', 'History');
   assert.textPresent('.history-entry', 'Added by Jim');
 });
+
+test('shows the last activity', function(assert) {
+  const paper = make('paper');
+  const correspondence = make('correspondence', 'externalCorrespondence', {
+    activities: [
+      { key: 'correspondence.edited', full_name: 'John Doe', created_at: '1999-10-11' },
+      { key: 'correspondence.created', full_name: 'Jim', created_at: '1989-8-19' }
+    ],
+    paper: paper
+  });
+  const can = FakeCanService.create().allowPermission('manage_workflow', paper);
+  this.register('service:can', can.asService());
+
+  this.set('correspondence', correspondence);
+  this.render(hbs`{{correspondence-details message=correspondence}}`);
+
+  assert.textPresent('.history-text', 'Edited by John Doe');
+});
+

a06a4103f003318e460eed2a5573d934f6225453
Jack LaBarba <brakit@gmail.com>
2017-11-16 10:28:22 -0800

    APERTA-11959 remove unused serializer attributes

--- app/serializers/reviewer_report_serializer.rb
+++ app/serializers/reviewer_report_serializer.rb
@@ -9,19 +9,9 @@ class ReviewerReportSerializer < ActiveModel::Serializer
     :created_at,
     :status,
     :status_datetime,
-    :due_at,
-    :due_at_id,
     :originally_due_at,
     :revision
   has_one :due_datetime, embed: :ids, include: true
-
-  def due_at
-    object.due_at if FeatureFlag[:REVIEW_DUE_DATE]
-  end
-
-  def due_at_id
-    object.due_datetime.id if object.due_datetime.present? && FeatureFlag[:REVIEW_DUE_DATE]
-  end
 
   def status
     object.status

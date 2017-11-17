7798253f44a4c472e6d49dc685aefc0d4067081c
Andrew Max <anm898989@gmail.com>
2017-11-18 01:30:35 +0900

    APERTA-11823 create task behavior prototype

--- app/models/create_task_behavior.rb
+++ app/models/create_task_behavior.rb
@@ -1,8 +1,11 @@
 class CreateTaskBehavior < Behavior
+  # has_attributes string: %w[task_name], integer: %w[phase_id]
+  # validates :task_name, presence: true
+
   def call(event)
     task_name = entity_attributes.find_by(name: 'task_name').string_value
-    phase_id = entity_attributes.find_by(name: 'phase_id').integer_value
     card_version = event.paper.journal.cards.find_by(name: task_name).latest_card_version
+    phase_id = event.paper.phases.first.id
 
     task_opts = {
       "completed" => false,
@@ -17,14 +20,10 @@ class CreateTaskBehavior < Behavior
   end
 end
 
-# Event.register('create_task')
-
-# behavior = CreateTaskBehavior.create(event_name: 'create_task', journal_id: 1)
+# this should be done by rake task
+# behavior = CreateTaskBehavior.create(event_name: "paper.state_changed.submitted", journal_id: Journal.first.id )
 
 # task_ent = behavior.entity_attributes.create(name: 'task_name', value_type:'string', string_value: 'Ethics Statement')
 
 # phase_ent = behavior.entity_attributes.create(name: 'phase_id', value_type:'integer', integer_value: 51)
 
-# event = Event.new(name: 'create_task', paper: Paper.first, task: Paper.first.tasks.first, user: nil)
-
-# event.trigger

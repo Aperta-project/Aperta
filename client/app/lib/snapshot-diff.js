f74aaf6438e682965afa17acd8cda388fba549fd
Edem Kumodzi <ekumodzi@plos.org>
2017-11-21 16:45:05 +0000

    snapshot diff

--- client/app/lib/snapshot-diff.js
+++ client/app/lib/snapshot-diff.js
@@ -1,3 +1,60 @@
-export default function(snapshot1, snapshot2) {
-  return snapshot1 + ' ' + snapshot2;
+export function compareJson(snapshot1, snapshot2) {
+  if (snapshot1.constructor !== snapshot2.constructor || (snapshot1.constructor !== String && snapshot1.constructor !== Object && snapshot1.constructor !== Array)) {
+    return false;
+  }
+
+  let result = false;
+
+  if (snapshot1.constructor === String) {
+    return snapshot1 === snapshot2;
+  }
+  else if (snapshot1.constructor === Array) {
+    if (snapshot1.length === 0 && snapshot2.length === 0) {
+      return true;
+    }
+    //let checkFields = ['owner_type', 'owner_id', 'grant_number', 'website', 'additional_comments'];
+    //let ignoreFields = ['name', 'type', 'value'];
+
+    //snapshot1 = cleanHash(snapshot1, checkFields, ignoreFields);
+    //snapshot2 = cleanHash(snapshot2, checkFields, ignoreFields);
+
+    snapshot1.forEach(function (current, index) {
+      result = compareJson(current, snapshot2[index]);
+      if (!result) {
+        return;
+      }
+    });
+
+  }
+  else if (snapshot1.constructor === Object) {
+    deleteKeys(snapshot1, ['id', 'answer_type']);
+    deleteKeys(snapshot2, ['id', 'answer_type']);
+
+    Object.keys(snapshot1).forEach(function (current) {
+      let value1 = snapshot1[current];
+      let value2 = snapshot2[current];
+
+      if (Object.keys(snapshot2).indexOf(current) === -1) {
+        return false;
+      }
+
+      if (snapshot1.constructor === Array || snapshot1.constructor === Object) {
+        result = compareJson(value1, value2);
+      }
+      else {
+        result = (value1 === value2);
+      }
+    });
+  }
+  return result;
 }
+
+let deleteKeys = function(snapshot, keys){
+  keys.forEach(function(key){
+    delete snapshot[key];
+  });
+};
+
+// let cleanHash = function (snapshot, checkFields, ignoreFields) {
+//   return snapshot;
+// };
--- client/tests/unit/lib/snapshot-diff-test.js
+++ client/tests/unit/lib/snapshot-diff-test.js
@@ -1,9 +1,81 @@
-import { module, test } from 'ember-qunit';
-import { snapshotDiff } from 'tahi/lib/snapshot-diff';
+import { module, test } from 'qunit';
+import { compareJson } from 'tahi/lib/snapshot-diff';
 
 module('Snapshot Diff');
 
-test('compareSnapshots', function (assert) {
-  let result = snapshotDiff('hello', 'world');
-  assert.equal(result, 'hello world');
+test('comparison - competing interests', function (assert) {
+  let json1 = {
+    'name': 'competing-interests-task',
+    'type': 'properties',
+    'children': [
+      {
+        'name': 'competing_interests--has_competing_interests',
+        'type': 'question',
+        'value': {
+          'id': 249,
+          'title': "Do any authors of this manuscript have competing interests (as described in the \u003ca target='_blank' href='http://journals.plos.org/plosbiology/s/competing-interests'\u003ePLOS Policy on Declaration and Evaluation of Competing Interests\u003c/a\u003e)?",
+          'answer_type': 'boolean',
+          'answer': false,
+          'attachments': []
+        },
+        'children': [
+          {
+            'name': 'competing_interests--statement',
+            'type': 'question',
+            'value': {
+              'id': 250,
+              'title': "Please provide details about any and all competing interests in the box below. Your response should begin with this statement: \"I have read the journal's policy and the authors of this manuscript have the following competing interests.\"",
+              'answer_type': 'text',
+              'answer': 'The authors have declared that no competing interests exist.',
+              'attachments': []
+            },
+            'children': []
+          }
+        ]
+      },
+      {
+        'name': 'id',
+        'type': 'integer',
+        'value': 79012
+      }
+    ]
+  };
+  let json2 = {
+    'name': 'competing-interests-task',
+    'type': 'properties',
+    'children': [
+      {
+        'name': 'competing_interests--has_competing_interests',
+        'type': 'question',
+        'value': {
+          'id': 249,
+          'title': "Do any authors of this manuscript have competing interests (as described in the \u003ca target='_blank' href='http://journals.plos.org/plosbiology/s/competing-interests'\u003ePLOS Policy on Declaration and Evaluation of Competing Interests\u003c/a\u003e)?",
+          'answer_type': 'boolean',
+          'answer': false,
+          'attachments': []
+        },
+        'children': [
+          {
+            'name': 'competing_interests--statement',
+            'type': 'question',
+            'value': {
+              'id': 250,
+              'title': "Please provide details about any and all competing interests in the box below. Your response should begin with this statement: \"I have read the journal's policy and the authors of this manuscript have the following competing interests.\"",
+              'answer_type': 'html',
+              'answer': 'The authors have declared that no competing interests exist.',
+              'attachments': []
+            },
+            'children': []
+          }
+        ]
+      },
+      {
+        'name': 'id',
+        'type': 'integer',
+        'value': 79012
+      }
+    ]
+  };
+  let result = compareJson(json1, json2);
+  assert.equal(result, true);
 });

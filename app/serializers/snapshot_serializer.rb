f7d2070b653d18fad12e09cca72e91c2a5afe558
Edem Kumodzi <ekumodzi@plos.org>
2017-11-24 16:38:19 +0000

    snapshot sanitizer library

--- app/serializers/snapshot_serializer.rb
+++ app/serializers/snapshot_serializer.rb
@@ -1,3 +1,7 @@
 class SnapshotSerializer < ActiveModel::Serializer
-  attributes :id, :source_id, :source_type, :major_version, :minor_version, :contents, :created_at
+  attributes :id, :source_id, :source_type, :major_version, :minor_version, :contents, :created_at, :sanitized_contents
+
+  def sanitized_contents
+    SnapshotSanitizer.sanitize(object.contents)
+  end
 end
---
+++ lib/snapshot_sanitizer.rb
@@ -0,0 +1,37 @@
+# Used to clean up snapshots from nodes that are irrelevant for comparisons
+require 'json'
+
+module SnapshotSanitizer
+  def self.sanitize(snapshot)
+    return snapshot unless snapshot.is_a?(Hash) || snapshot.is_a?(Array)
+
+    if snapshot.is_a?(Array)
+      return snapshot if snapshot.empty?
+
+      check_fields = ['id', 'owner_type', 'owner_id', 'grant_number', 'website', 'additional_comments']
+      ignore_fields = ['name', 'type', 'value']
+
+      snapshot = clean(snapshot, check_fields, ignore_fields)
+
+      snapshot.each_with_index do |obj, _index|
+        obj = sanitize(obj)
+      end
+    elsif snapshot.is_a?(Hash)
+      snapshot.delete_if { |key, _value| ['id', 'answer_type'].include?(key) }
+
+      snapshot.each do |key, value|
+        snapshot[key] = sanitize(value)
+      end
+    end
+  end
+
+  def self.clean(json, check_fields, ignore_fields)
+    temp = []
+    json.each_with_index do |obj, _index|
+      if obj.keys == ignore_fields
+        temp << obj if check_fields.include?(obj['name'])
+      end
+    end
+    json - temp
+  end
+end

6f50ea756401a91152c9d2a1a6a5e501770210cf
Ben Beckwith <ben@mutuallyhuman.com>
2017-11-27 20:50:20 -0500

    Move `subcontexts_for` method
    Move the non-production `subcontexts_for` method to the end of the
    class.
    
    APERTA-11887

--- app/services/merge_field.rb
+++ app/services/merge_field.rb
@@ -1,10 +1,6 @@
 # Merge fields are the subset of a context's attributes
 # that we want to publicly advertise to email template admins.
 class MergeField
-  def self.subcontexts_for(context_class)
-    @subcontexts_by_parent[context_class] || {}
-  end
-
   # Gets the list of merge fields that we want to show to letter template admins.
   def self.list_for(context_class)
     fields = context_class.public_instance_methods - TemplateContext.public_instance_methods
@@ -40,5 +36,9 @@ class MergeField
     @subcontexts_by_parent ||= {}
     @subcontexts_by_parent[parent_context] ||= {}
     @subcontexts_by_parent[parent_context][subcontext_name] = props
+  end
+
+  def self.subcontexts_for(context_class)
+    @subcontexts_by_parent[context_class] || {}
   end
 end

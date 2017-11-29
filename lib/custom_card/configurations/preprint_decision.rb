3e24a2a7433e9a319827da5fe0cc7b05ab14e4c8
Ashley Allen <aallen@surgeforward.com>
2017-11-29 15:17:40 -0800

    APERTA-10345 new error message and feature flag

--- lib/custom_card/configurations/preprint_decision.rb
+++ lib/custom_card/configurations/preprint_decision.rb
@@ -5,25 +5,27 @@ module CustomCard
     # Card and it can be used to create a new valid Card into the
     # system via the CustomCard::Loader.
     #
-    class PreprintDecision < Base
-      def self.name
-        "Preprint Decision"
-      end
+    if FeatureFlag[:PREPRINT]
+      class PreprintDecision < Base
+        def self.name
+          "Preprint Decision"
+        end
 
-      def self.view_role_names
-        ["Internal Editor", "Publishing Services", "Production Staff", "Staff Admin"]
-      end
+        def self.view_role_names
+          ["Internal Editor", "Publishing Services", "Production Staff", "Staff Admin"]
+        end
 
-      def self.edit_role_names
-        ["Internal Editor", "Publishing Services", "Production Staff", "Staff Admin"]
-      end
+        def self.edit_role_names
+          ["Internal Editor", "Publishing Services", "Production Staff", "Staff Admin"]
+        end
 
-      def self.publish
-        true
-      end
+        def self.publish
+          true
+        end
 
-      def self.do_not_create_in_production_environment
-        false
+        def self.do_not_create_in_production_environment
+          false
+        end
       end
     end
   end

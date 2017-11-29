a1eb68633b419642bca8431b8e3432e56bd0fb68
Curtis Autery <curtis@mutuallyhuman.com>
2017-11-29 09:57:43 -0500

    Add AdminEdit to client
    * Add controller to rails for client to query
    * Add model and serializer to client
    * Update reviewer report components to allow
    admins to create AdminEdit records on submitted
    reports
    * Have answer_as_nested_question_answer serializer
    return pending edits as answer “value” if the
    reviewer report is in edit mode.
    * Styling
    
    APERTA-6281

--- app/serializers/answer_as_nested_question_answer_serializer.rb
+++ app/serializers/answer_as_nested_question_answer_serializer.rb
@@ -11,4 +11,21 @@ class AnswerAsNestedQuestionAnswerSerializer < ActiveModel::Serializer
   def nested_question_id
     object.card_content_id
   end
+
+  def value
+    if reviewer_report? && can_see_active_edit?
+      additional = object.additional_data || {}
+      additional["pending_edit"]
+    else
+      object.value
+    end
+  end
+
+  def reviewer_report?
+    object.owner.class == ReviewerReport
+  end
+
+  def can_see_active_edit?
+    object.owner.active_admin_edit? && current_user.can?(:edit_answers, object.owner.paper)
+  end
 end

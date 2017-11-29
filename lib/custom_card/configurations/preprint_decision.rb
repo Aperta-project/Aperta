64e382d5797569f46f7df4f01fa264b3c355e828
Ashley Allen <aallen@surgeforward.com>
2017-11-29 15:50:24 -0800

    APERTA-10345 removed preprint flag from card

--- lib/custom_card/configurations/preprint_decision.rb
+++ lib/custom_card/configurations/preprint_decision.rb
@@ -5,27 +5,25 @@ module CustomCard
     # Card and it can be used to create a new valid Card into the
     # system via the CustomCard::Loader.
     #
-    if FeatureFlag[:PREPRINT]
-      class PreprintDecision < Base
-        def self.name
-          "Preprint Decision"
-        end
+    class PreprintDecision < Base
+      def self.name
+        "Preprint Decision"
+      end
 
-        def self.view_role_names
-          ["Internal Editor", "Publishing Services", "Production Staff", "Staff Admin"]
-        end
+      def self.view_role_names
+        ["Internal Editor", "Publishing Services", "Production Staff", "Staff Admin"]
+      end
 
-        def self.edit_role_names
-          ["Internal Editor", "Publishing Services", "Production Staff", "Staff Admin"]
-        end
+      def self.edit_role_names
+        ["Internal Editor", "Publishing Services", "Production Staff", "Staff Admin"]
+      end
 
-        def self.publish
-          true
-        end
+      def self.publish
+        true
+      end
 
-        def self.do_not_create_in_production_environment
-          false
-        end
+      def self.do_not_create_in_production_environment
+        false
       end
     end
   end

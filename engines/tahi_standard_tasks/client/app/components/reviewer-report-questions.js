d8cecaf783ffab496b4b7fd8c9db82d4fef5fb6f
Curtis Autery <curtis@mutuallyhuman.com>
2017-11-29 09:46:58 -0500

    Pre-work for APERTA-6281
    * Replace “model” with “report” in reviewer report
    questions and tasks
    * Make front matter javascript component file
    simply extend reviewer report’s instead of cloning
    its contents.

--- engines/tahi_standard_tasks/client/app/components/reviewer-report-questions.js
+++ engines/tahi_standard_tasks/client/app/components/reviewer-report-questions.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   readOnly: false,
-  hasReport: Ember.computed('model.status', function() {
-    const reportStatus = this.get('model.status');
+  hasReport: Ember.computed('report.status', function() {
+    const reportStatus = this.get('report.status');
     const hasReportStates = ['pending', 'completed', 'invitation_accepted'];
     return hasReportStates.includes(reportStatus);
   }),
 
-  competingInterestsLink: Ember.computed('model.task.paper.journal.name', function() {
-    const name = this.get('model.task.paper.journal.name');
+  competingInterestsLink: Ember.computed('report.task.paper.journal.name', function() {
+    const name = this.get('report.task.paper.journal.name');
     if (name) {
       return `http://journals.plos.org/${name.toLowerCase().replace(' ', '')}/s/reviewer-guidelines#loc-competing-interests`;
     } else {

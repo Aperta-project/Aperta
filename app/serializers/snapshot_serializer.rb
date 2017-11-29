3f905d090ec588092be4016b60208da0ad6c1fc5
Edem Kumodzi <ekumodzi@plos.org>
2017-11-29 18:21:27 +0000

    cleaned up snapshot serializer

--- app/serializers/snapshot_serializer.rb
+++ app/serializers/snapshot_serializer.rb
@@ -1,7 +1,3 @@
 class SnapshotSerializer < ActiveModel::Serializer
   attributes :id, :source_id, :source_type, :major_version, :minor_version, :contents, :sanitized_contents, :created_at
-
-  def sanitized_contents
-    object.sanitized_contents
-  end
 end

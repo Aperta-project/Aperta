82d08c7a668ada5a977fb5e5e36d3c18e48de306
Curtis Autery <curtis@mutuallyhuman.com>
2017-11-29 09:51:49 -0500

    Add AdminEdit to server
    * Define admin_edit rails model
    * Add routes, associations to reviewer report,
    permissions
    * Sideload admin edit rows with reviewer report
    serializer
    
    APERTA-6281

--- config/routes.rb
+++ config/routes.rb
@@ -196,6 +196,7 @@ Tahi::Application.routes.draw do
     resources :related_articles, only: [:show, :create, :update, :destroy]
     resources :reviewer_reports, only: [:show, :update]
     resources :due_datetimes, only: [:update]
+    resources :admin_edits, only: [:show, :update, :create, :destroy]
     resources :tasks, only: [:update, :create, :show, :destroy] do
       get :nested_questions
       get :nested_question_answers

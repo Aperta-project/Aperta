991ab0e95d9aee0dfa92547aeb2e2ca551473890
Ashley Allen <aallen@surgeforward.com>
2017-11-29 11:14:04 -0800

    APERTA-12079 added trigger paper.email_sent event

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -114,6 +114,7 @@ class TasksController < ApplicationController
         task: task
       )
     end
+    trigger_email_sent_event(task)
     d = Time.now.getlocal
     initiator = current_user.email
     render json:  {
@@ -169,6 +170,13 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def trigger_email_sent_event(task_obj)
+    Event.register('paper.email_sent')
+    paper = task_obj.paper
+    event = Event.new(name: 'paper.email_sent', paper: paper, task: task_obj, user: current_user)
+    event.trigger
+  end
 
   def render_sendback_template(task_obj)
     paper = task_obj.paper

d068d6e05ecdccfbefc52b76f4f332fc2cea286b
Ashley Allen <aallen@surgeforward.com>
2017-12-04 13:06:34 -0800

    APERTA-10345 added scope to card

--- app/models/card.rb
+++ app/models/card.rb
@@ -29,6 +29,7 @@ class Card < ActiveRecord::Base
   after_destroy :clean_permissions
 
   scope :archived, -> { where.not(archived_at: nil) }
+  scope :active_cards, -> { where.not(name: Card.feature_inactive_cards) }
 
   # temporarly added for https://jira.plos.org/jira/browse/APERTA-10345
   # we should remove this once the preprint feature flag is removed

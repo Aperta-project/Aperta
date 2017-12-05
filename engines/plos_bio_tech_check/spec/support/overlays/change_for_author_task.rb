08a0802e6577bf1c232e02cc268e19d6b60f8ab7
Michael Walsh <noxiousthing@gmail.com>
2017-12-05 15:37:20 -0800

    Fix feature specs related to Changes for Author task

--- engines/plos_bio_tech_check/spec/support/overlays/change_for_author_task.rb
+++ engines/plos_bio_tech_check/spec/support/overlays/change_for_author_task.rb
@@ -4,8 +4,4 @@ class ChangesForAuthorOverlay < CardOverlay
   def expect_to_see_change_list
     expect(page).to have_content('First round author changes')
   end
-
-  def click_task_completed
-    click_button 'I AM DONE WITH THIS TASK'
-  end
 end

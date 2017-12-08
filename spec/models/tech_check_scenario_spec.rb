5960e28f8b99a2758217bebc3cc5b0326ad5037b
Andrew Max <anm898989@gmail.com>
2017-12-08 21:02:10 +0900

    APERTA-11978 update tech check scenario tests

--- spec/models/tech_check_scenario_spec.rb
+++ spec/models/tech_check_scenario_spec.rb
@@ -5,6 +5,8 @@ describe TechCheckScenario do
   let(:paper) { FactoryGirl.create(:paper, journal: FactoryGirl.create(:journal)) }
   let(:task) { FactoryGirl.create(:initial_tech_check_task, paper: paper) }
   let(:task2) { FactoryGirl.create(:final_tech_check_task, paper: paper) }
+  let(:tech_check_parent) { FactoryGirl.create(:card_content, content_type: 'tech-check', value_type: 'boolean') }
+  let(:tech_check_parent2) { FactoryGirl.create(:card_content, content_type: 'tech-check', value_type: 'boolean') }
 
   describe "rendering a template" do
     it "renders the intro" do
@@ -34,17 +36,48 @@ describe TechCheckScenario do
         expect(LetterTemplate.new(body: template).render(context).body)
           .to eq('it is ugly, it is wrong, ')
       end
+    end
 
-      context 'when rendering paper_sendback_reasons' do
-        it "renders all sendback reasons for the paper" do
-          task.answers.create!(card_content: reasons, value: 'it is ugly', paper: paper)
-          task.answers.create!(card_content: reasons, value: 'it is wrong', paper: paper)
-          task2.answers.create!(card_content: reasons, value: 'it is the worst', paper: paper)
-          task2.answers.create!(card_content: reasons, value: 'I hate it', paper: paper)
-          template = "{% for reason in paper_sendback_reasons %}{{ reason.value }}, {% endfor %}"
-          expect(LetterTemplate.new(body: template).render(context).body)
-            .to eq('it is ugly, it is wrong, it is the worst, I hate it, ')
-        end
+    context 'when rendering sendback reasons' do
+      let(:sendback_parent) { FactoryGirl.create(:card_content, content_type: 'sendback-reason', value_type: 'boolean', parent: tech_check_parent) }
+      let(:reasons) { FactoryGirl.create(:card_content, content_type: 'paragraph-input', parent: sendback_parent) }
+
+      it "renders all sendback reasons if all tech checks are unpassed" do
+        task.card_version.content_root.children << tech_check_parent
+        task.card_version.card_contents << tech_check_parent
+
+        task2.card_version.content_root.children << tech_check_parent2
+        task2.card_version.card_contents << tech_check_parent2
+
+        task.answers.create!(card_content: tech_check_parent, value: 'f', paper: paper)
+        task2.answers.create!(card_content: tech_check_parent2, value: 'f', paper: paper)
+
+        task.answers.create!(card_content: reasons, value: 'it is ugly', paper: paper)
+        task.answers.create!(card_content: reasons, value: 'it is wrong', paper: paper)
+        task2.answers.create!(card_content: reasons, value: 'it is the worst', paper: paper)
+        task2.answers.create!(card_content: reasons, value: 'I hate it', paper: paper)
+        template = "{% for reason in paperwide_sendback_reasons %}{{ reason.value }}, {% endfor %}"
+        expect(LetterTemplate.new(body: template).render(context).body)
+          .to eq('it is ugly, it is wrong, it is the worst, I hate it, ')
+      end
+
+      it "only renders reasons from unpassed tech checks" do
+        task.card_version.content_root.children << tech_check_parent
+        task.card_version.card_contents << tech_check_parent
+
+        task2.card_version.content_root.children << tech_check_parent2
+        task2.card_version.card_contents << tech_check_parent2
+
+        task.answers.create!(card_content: tech_check_parent, value: 'f', paper: paper)
+        task2.answers.create!(card_content: tech_check_parent2, value: 't', paper: paper)
+
+        task.answers.create!(card_content: reasons, value: 'it is ugly', paper: paper)
+        task.answers.create!(card_content: reasons, value: 'it is wrong', paper: paper)
+        task2.answers.create!(card_content: reasons, value: 'it is the worst', paper: paper)
+        task2.answers.create!(card_content: reasons, value: 'I hate it', paper: paper)
+        template = "{% for reason in paperwide_sendback_reasons %}{{ reason.value }}, {% endfor %}"
+        expect(LetterTemplate.new(body: template).render(context).body)
+          .to eq('it is ugly, it is wrong, ')
       end
     end
 

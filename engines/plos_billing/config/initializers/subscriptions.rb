33b91869d0a1b7750365d21906266ee768c58ff1
Chris Westra <cwestra@gmail.com>
2017-12-13 10:39:28 -0500

    APERTA-9867 Merge subscription initializers

--- engines/plos_billing/config/initializers/subscriptions.rb
+++
@@ -1,6 +0,0 @@
-Subscriptions.configure do
-  add 'paper:submitted', PlosBilling::Paper::Salesforce
-  add 'paper:accepted', PlosBilling::Paper::Salesforce
-  add 'paper:rejected', PlosBilling::Paper::Salesforce
-  add 'paper:withdrawn', PlosBilling::Paper::Salesforce
-end

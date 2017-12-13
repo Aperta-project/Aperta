1fb93b41f5107cbeaded38ca21cce3713824c50c
Carlos Guisao Franco <titofranco@gmail.com>
2017-12-13 11:28:22 -0500

    APERTA-9390 Initial implementation of email-templates/preview component

---
+++ client/app/pods/components/admin-page/email-templates/preview/component.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  restless: Ember.inject.service(),
+  classNameBindings: ['preview-email'],
+  visible: false,
+  letterTemplateId: null,
+  letterTemplate: null,
+  actions: {
+    loadPreviewData() {
+      this.get('restless')
+        .get(`/api/admin/letter_templates/${this.get('letterTemplateId')}/preview`).
+        then((data) => {
+          this.set('letterTemplate', data.letter_template);
+        });
+      this.set('visible', true);
+    },
+    close() {
+      this.set('visible', false);
+    }
+  }
+});

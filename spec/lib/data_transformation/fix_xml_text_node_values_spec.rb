7ffade689bc556be1ccf8e6169bfa29318ed7eff
Michael Walsh <noxiousthing@gmail.com>
2017-12-12 16:23:08 -0800

    Update fix_xml_text_node_values_spec

--- spec/lib/data_transformation/fix_xml_text_node_values_spec.rb
+++ spec/lib/data_transformation/fix_xml_text_node_values_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
+require 'data_transformation/base'
+require 'data_transformation/fix_xml_text_node_values'
 
-describe 'FixXmlTextNodeValues' do
-
-  describe 'remove_cdata_nodes' do
-    subject { FixXmlTextNodeValues.new }
+describe 'DataTransformation::FixXmlTextNodeValues' do
+  describe '#remove_cdata_nodes' do
+    subject { DataTransformation::FixXmlTextNodeValues.new }
     let(:text) do
       <<-TEXT
           <![CDATA[Thank you very much for submitting your manuscript for consideration at PLOS Biology.
@@ -21,10 +22,13 @@ describe 'FixXmlTextNodeValues' do
       TEXT
     end
 
-    let(:card_content) { FactoryGirl.create(:card_content, text: text) }
+    let(:card_content) { FactoryGirl.create(:card_content) }
 
     it 'removes cdata nodes' do
-      subject.call
+      # We want to put the text in an invalid state to make it mirror prod
+      # rubocop:disable Rails/SkipsModelValidations:
+      card_content.update_attribute('text', text)
+      subject.remove_cdata_nodes
       expect(card_content.reload.text).to eq expected_text
     end
   end

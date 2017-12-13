3a31a98663372ebd13c67c25b5ae900f5e92274b
Chris Westra <cwestra@gmail.com>
2017-12-13 17:54:24 -0500

    APERTA-9867 remove engine table prefixes
    In order to let the `CustomCard::MigrateFinancialDisclosureMigrator` run without
    too much trouble this migration does not remove the prefix from the Funders
    table.

--- app/models/tahi_standard_tasks/funder.rb
+++ app/models/tahi_standard_tasks/funder.rb
@@ -1,5 +1,12 @@
 module TahiStandardTasks
   class Funder < ActiveRecord::Base
+    # This has been moved from the tahi_standard_tasks engine into the main
+    # app as part of APERTA-9867 but due to complications migrations accompanying the
+    # release it easier to avoid renaming the table at the time.
+    #
+    # After the 'MigrateFinancialDisclosureToCustomCard' migration is in
+    # production this line can be removed
+    self.table_name = 'tahi_standard_tasks_funders'
     include Answerable
 
     belongs_to :task, foreign_key: :task_id

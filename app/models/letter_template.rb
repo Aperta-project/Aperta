a329d91cfa8724f93f7cc731b95503efca4e1f05
Carlos Guisao Franco <titofranco@gmail.com>
2017-12-14 09:20:00 -0500

    APERTA-9390 Add LetterTemplate#render_dummy_data spec

--- app/models/letter_template.rb
+++ app/models/letter_template.rb
@@ -61,10 +61,6 @@ class LetterTemplate < ActiveRecord::Base
     errors.add(:base, 'Template not renderable')
   end
 
-  def dummy_data_file
-    @dummy_data_file ||= YAML.load_file(Rails.root.join('config', 'letter_templates', 'dummy_data.yml'))
-  end
-
   def render_dummy_data
     render(dummy_data_file)
   end
@@ -123,5 +119,9 @@ class LetterTemplate < ActiveRecord::Base
       .map(&:strip)                            # Git rid of surrounding whitespace
       .map { |a| a.gsub(/.*<(.+)>$/, '\1') }   # Strip angle brackets and any leading friendly names that weren't quoted
       .join(',')                               # ...and join them back together with the Tahi standard email separator
+  end
+
+  def dummy_data_file
+    @dummy_data_file ||= YAML.load_file(Rails.root.join('config', 'letter_templates', 'dummy_data.yml'))
   end
 end

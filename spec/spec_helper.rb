c6b16f8899b0aa2b1d8e4e1ecc96b74e46dff3e4
Jim Haungs <jhaungs@plos.org>
2017-12-20 15:47:37 -0800

    Add simplecov configuration to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,17 @@
 # require 'spec_helper' in your specs when you don't want to load dependencies
+
+require 'simplecov'
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start do
+  add_filter %r{^/test/}
+  add_filter %r{^/spec/}
+end
+
 require 'rspec/instafail'
 require 'rspec/retry'
 require 'aasm/rspec'

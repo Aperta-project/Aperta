672b9c65d45f321a5570ae5dd21f36e2953bb36e
Carlos Guisao Franco <titofranco@gmail.com>
2017-12-21 15:06:34 -0500

    APERTA-10969 Get save-button specs green

--- client/tests/integration/pods/components/save-button/component-test.js
+++ client/tests/integration/pods/components/save-button/component-test.js
@@ -2,23 +2,27 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('save-button', 'Integration | Component | save button', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    this.set('displaySpinner', false);
+    this.set('disabled', false);
+  }
 });
 
 test('it renders with text set', function(assert) {
-  this.render(hbs`{{#save-button}}SAVE{{/save-button}}`);
+  this.render(hbs`{{#save-button displaySpinner=displaySpinner disabled=disabled}}SAVE{{/save-button}}`);
 
   assert.equal(this.$().text().trim(), 'SAVE');
 });
 
 test('it is disabled by default', function(assert) {
-  this.render(hbs`{{#save-button}}SAVE{{/save-button}}`);
+  this.render(hbs`{{#save-button displaySpinner=displaySpinner disabled=disabled}}SAVE{{/save-button}}`);
   assert.ok(this.$('button[disabled]'));
 });
 
 test('it displays a spinner when loading', function(assert) {
   this.set('loading', true);
-  this.render(hbs`{{#save-button displaySpinner=loading}}SAVE{{/save-button}}`);
+  this.render(hbs`{{#save-button displaySpinner=loading disabled=disabled}}SAVE{{/save-button}}`);
 
   assert.ok(this.$('.progress-spinner--blue'), 'Displays progress spinner');
 });

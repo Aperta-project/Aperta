f4de69649fa808c8b97125803ff807fc7f324598
Shawn Gatchell <shawng03@gmail.com>
2017-12-22 13:46:44 -0600

    Separate component for "not-authenticated" layout

--- client/app/pods/components/co-authors/component.js
+++ client/app/pods/components/co-authors/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['co-author-confirmaion'],
   date_created: Ember.computed('created_at', function() {
     return moment(this.get('author.created_at')).format('ll');
   }),

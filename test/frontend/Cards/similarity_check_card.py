35206c324c32b2598818868f39df1e67f62b455e
Galina Timonina <gtimonina@US-MC-01243.local>
2017-12-24 17:05:09 -0800

    added docstrings

--- test/frontend/Cards/similarity_check_card.py
+++ test/frontend/Cards/similarity_check_card.py
@@ -22,7 +22,6 @@ class SimilarityCheckCard(BaseCard):
         super(SimilarityCheckCard, self).__init__(driver)
 
         # Locators - Instance members
-        # mmt card locators
         self._sim_check_task_overlay = (By.CSS_SELECTOR, '.task.similarity-check-task')
         self._instruction = (By.CSS_SELECTOR, 'div.ember-view.task>div>p')
         self._automation_disabled_info = (By.CSS_SELECTOR, 'div.auto-report-off p')
@@ -44,14 +43,23 @@ class SimilarityCheckCard(BaseCard):
         self._sim_check_report_score = (By.CLASS_NAME, 'score')
         self._sim_check_report_history = (By.CSS_SELECTOR, '.task.similarity-check-task>div>h3')
         self._sim_check_report_revision_number = (By.CSS_SELECTOR,
-                                                  'similarity-check-bar-revision-number')
+                                                  '.similarity-check-bar-revision-number')
+        self._version_report = (By.CSS_SELECTOR, '.similarity-check-bar>.similarity-check')
         self._btn_done = (By.CSS_SELECTOR, 'span.task-completed-section button')
         self._author = (By.CLASS_NAME, 'paper-creator')
 
     # POM Actions
-    def validate_styles_and_components(self, ithenticate_automation):
+    def validate_styles_and_components(self, ithenticate_automation, triggered=False,
+                                       auto_report_done=False):
         """
-        Validate styles in the Similarity Check Card
+        Validate styles and components in the Similarity Check Card
+        :param ithenticate_automation: string: one of the options: 'off',
+        'at_first_full_submission', 'after_major_revise_decision', 'after_minor_revise_decision',
+        'after_any_first_revise_decision'
+        :param triggered: boolean, True if the generating Report is expected to be triggered,
+        False by default, optional
+        :param auto_report_done: boolean, True if automated Report was triggered on the previous
+        steps, default is False, optional
         :return: void function
         """
         # AC#4
@@ -69,17 +77,15 @@ class SimilarityCheckCard(BaseCard):
         assert card_instruction.text.strip() == expected_instruction, card_instruction.text.strip()
         self.validate_application_body_text(card_instruction)
 
-        # AC 4.2  Button to send for manual report
-        # AC 4.2.1  The button is only enabled if:
-        # AC 4.2.1.1 ... the card is marked incomplete
+        # Button to send for manual report The button is only enabled if:
+        # the card is marked incomplete
 
-        # APERTA-9958
+        # auto options are defined in APERTA-9958
         auto_report_options = {'at_first_full_submission': 'first full submission',
                                'after_major_revise_decision': 'major revision',
                                'after_minor_revise_decision': 'minor revision',
                                'after_any_first_revise_decision': 'any first revision'}
 
-        # ithenticate_automation = 'off'
         if ithenticate_automation == 'off':
             auto_info = self._get(self._automation_disabled_info)
             assert auto_info.text.strip() == 'Automated similarity check is disabled:', \
@@ -94,62 +100,106 @@ class SimilarityCheckCard(BaseCard):
                                                 'the automated similarity check for this manuscript.', \
                 auto_info[0].text.strip()
 
-        if ithenticate_automation == 'at_first_full_submission':
+        if triggered:
             # check it is pending and "generate button is not enabled
             send_for_manual_report_button = self._iget(self._generate_report_button)
             assert not send_for_manual_report_button.is_enabled()
-            try:
-                report_pending_spinner_message = self._get(self._report_pending_spinner_message)
-                assert "Pending" in report_pending_spinner_message.text
-            except ElementDoesNotExistAssertionError:
-                # that means the report is already ready
-                # check if Report is ready
-                report_title = self._get(self._sim_check_report_title)
-                assert 'Similarity Check Report' in report_title.text.strip()
-
+            self.validate_pending()
         else:
-            # check if the button is enable when the card is incomplete
-            self.validate_generate_report_button()
-            self.click_completion_button()  # mark task as complete
-            # check if the button is disable if the card is complete
-            self.validate_generate_report_button()
-            self.click_completion_button()  # mark task as incomplete
-            # AC 4.2.1.2 ... a manuscript file is present - skipped as not valid test case (it should not be possible
-            # to submit manuscript without manuscript file
+            if ithenticate_automation == 'off':
+                # check if the button is enable when the card is incomplete
+                if not self.completed_state():
+                    self.validate_generate_report_button()
+                    self.click_completion_button()  # mark task as complete
+                # check if the button is disable if the card is complete
+                self.validate_generate_report_button()
+                self.click_completion_button()  # mark task as incomplete
+                # The button triggers a confirm/cancel step before generation a report
+                self.validate_manual_report_confirmation()
+            else:
+                # auto option is on, so we are here if:
+                # 1) report sending is not triggered yet, for example, auto option is
+                # 'after_major_revise_decision', and it's first submission or minor revision
+                # 2) it is one of the next submissions after the automated report was done
+                if not auto_report_done: # 1)
+                    assert not self.completed_state(), 'The card is expected to be editable'
+                    self.validate_manual_report_confirmation()
+                else: # 2)
+                    if not self.completed_state():
+                        # it's still in pending status
+                        self.validate_pending()
 
-            # AC 4.2.2 .The button triggers a confirm/cancel step before generation a report
-            send_for_manual_report_button = self._get(self._generate_report_button)
-            self.validate_primary_big_green_button_style(send_for_manual_report_button)
-            send_for_manual_report_button.click()
-            # confirm/cancel container
-            self._wait_for_element(self._get(self._confirm_container))
-            # assert self._get(self._confirm_container)
-            confirm_container = self._get(self._confirm_container)
-            self.validate_generate_confirmation_style(confirm_container)
+    def validate_pending(self):
+        """
+        Validates card components - message and report title -  when the Report is pending
+        :return: void function
+        """
+        try:
+            report_pending_spinner_message = self._get(self._report_pending_spinner_message)
+            assert "Pending" in report_pending_spinner_message.text
+        except ElementDoesNotExistAssertionError:
+            # we were waiting for Similarity Check completion,
+            # and the report is ready
+            report_title = self._get(self._sim_check_report_title)
+            assert 'Similarity Check Report' in report_title.text.strip()
+        return True
 
-            confirm_text = self._get(self._confirm_text)
-            expected_confirm_text = 'Manually generating the report will disable the automated ' \
-                                    'similarity check for this manuscript'
-            assert confirm_text.text.strip() == expected_confirm_text, confirm_text.text.strip()
-            confirm_cancel = self._get(self._manually_generate_cancel_link)
-            self.validate_cancel_confirmation_style(confirm_cancel)
+    def validate_manual_report_confirmation(self):
+        """
+        Validates confirmation step befor manual Report generation
+        :return: void function
+        """
+        # AC 4.2.2 .The button triggers a confirm/cancel step before generation a report
+        send_for_manual_report_button = self._get(self._generate_report_button)
+        self.validate_primary_big_green_button_style(send_for_manual_report_button)
+        send_for_manual_report_button.click()
+        # confirm/cancel container
+        self._wait_for_element(self._get(self._confirm_container))
+        # assert self._get(self._confirm_container)
+        confirm_container = self._get(self._confirm_container)
+        self.validate_generate_confirmation_style(confirm_container)
 
-            confirm_generate_button = self._get(self._manually_generate_button)
-            assert confirm_generate_button.text == 'GENERATE REPORT'  # confirm button
-            self.validate_secondary_big_green_button_style(confirm_generate_button)
-            # click on 'cancel' - confirm container should not be displayed
-            confirm_cancel.click()
-            gen_report = self._get(self._sim_check_task_overlay)
-            gen_report.find_element(*self._confirm_form)
-            # gen_report.find_element_by_css_selector('div div')
-            assert 'confirm-container' not in gen_report.get_attribute('class')
-            # click "GENERATE REPORT' again
-            self.validate_generate_report_button()
+        confirm_text = self._get(self._confirm_text)
+        expected_confirm_text = 'Manually generating the report will disable the automated ' \
+                                'similarity check for this manuscript'
+        assert confirm_text.text.strip() == expected_confirm_text, confirm_text.text.strip()
+        confirm_cancel = self._get(self._manually_generate_cancel_link)
+        self.validate_cancel_confirmation_style(confirm_cancel)
+
+        confirm_generate_button = self._get(self._manually_generate_button)
+        assert confirm_generate_button.text == 'GENERATE REPORT'  # confirm button
+        self.validate_secondary_big_green_button_style(confirm_generate_button)
+        # click on 'cancel' - confirm container should not be displayed
+        confirm_cancel.click()
+        gen_report = self._get(self._sim_check_task_overlay)
+        gen_report.find_element(*self._confirm_form)
+        # gen_report.find_element_by_css_selector('div div')
+        assert 'confirm-container' not in gen_report.get_attribute('class')
+        # click "GENERATE REPORT' again
+        self.validate_generate_report_button()
+
+    def get_report_history(self):
+        """
+        Gets Report History for result validation
+        :return: versions: list of strings, last_version_report: string
+        """
+        report_history = self._get(self._sim_check_report_history)
+        assert 'Report History' in report_history.text.strip(), report_history.text.strip()
+        version_numbers = self._gets(self._sim_check_report_revision_number)
+        versions = [version.text.strip() for version in version_numbers]
+        self._get(self._sim_check_report_revision_number).click()
+        self._wait_for_element(self._get(self._version_report))
+        last_version_report = self._get(self._version_report).text.strip()
+
+        return versions, last_version_report
+
 
     def generate_manual_report(self):
         """
         Generate report manually by clicking on 'Generate Report' button
-        :return: url to check results, time when the generate report call has started
+        :return: task_url: url to get back and check results,
+            start_time: time when the generate report call has started, to track and calculate
+            remaining time to wait for the Report from iThenicate
         """
         send_for_manual_report_button = self._get(self._generate_report_button)
         send_for_manual_report_button.click()
@@ -174,26 +224,38 @@ class SimilarityCheckCard(BaseCard):
 
         return task_url, start_time
 
-    def validate_report_result(self):
+    def get_report_result(self, start_time = None):
         """
-        Wait for the report result and validate
-        :return: "" if validation is ok, if not - assertion will fail
-        if generating report fails with TimeoutException,
-        returns error message or 'No error message'
+        Waits for the Report from iThenticate, fetch the results from the link and gets the results
+        from Similar Card page and iThenticate page to validate
+        :param start_time: time when generation has started to decrease maximum time to wait as
+        there is a 10 minutes limit to wait for the result from iThenticate
+        :return: empty string if validation is ok, error message or 'No error message',
+        if generating report fails with TimeoutException;
+        elapsed time in seconds to track time, data dictionaries with title,
+        value (score) and author from card page and iThenticate page
         """
+        validation_start_time = datetime.now()
         html_header_title = self._get(self._header_title_link)
         paper_title = html_header_title.text
         try:
+            if start_time==None:
+                seconds_to_wait=500
+            else:
+                diff_time = datetime.now() - start_time
+                seconds_to_wait = max(10, 500 - diff_time.seconds)
+
             self._wait_on_lambda(lambda: self.completed_state() == True,
-                                 max_wait=600)  # score and style
+                                 max_wait=seconds_to_wait)  # score and style
         except TimeoutException:
             # after 10 minutes the 'Report not available' error message is expected to be displayed
+            validation_seconds = (datetime.now() - validation_start_time).seconds
             try:
                 error_message = self._get(self._flash_error_msg)
-                return error_message.text.strip()
+                return error_message.text.strip(), validation_seconds, None, None
             except:
-                return 'No error message'
-                # ? raise ElementDoesNotExistAssertionError(locator)
+                return 'No error message', validation_seconds, None, None
+
         score = self._get(self._sim_check_report_score)
         score = score.text
         paper_author = (self._get(self._author)).text
@@ -201,19 +263,19 @@ class SimilarityCheckCard(BaseCard):
         ithenticate_page = IthenticatePage(self._driver)
         ithenticate_page.page_ready()
         title, value, author = ithenticate_page.get_title_score()
-        assert paper_title.strip() == title.strip(), title
-        assert score.strip() in value.strip(), 'Score {0} is expected in {1}' \
-            .format(score.strip(), value.strip())
-        assert paper_author.lower().strip() in author.lower().strip(), \
-            'Paper author {0} is expected in {1}' \
-                .format(paper_author.strip(), author.strip())
-        return ""
+        report_data = {'title': title.strip(), 'value': value.strip(),
+                       'author': author.lower().strip()}
+        paper_data = {'title': paper_title.strip(), 'value': score.strip(),
+                      'author': paper_author.lower().strip()}
+        validation_seconds = (datetime.now() - validation_start_time).seconds
+        self._driver.close()
+        self._wait_for_number_of_windows_to_be(1)
+        self.traverse_from_window()
+
+        return "", validation_seconds, paper_data, report_data
 
     def launch_ithenticate_page(self):
-        """
-        Click on iThenticate report link and validate results
-        :return: void function
-        """
+        """Click on iThenticate report link to go the Report page"""
         report_link = self._get(self._sim_check_report_link)
         assert self._is_link_valid(report_link), 'Report link {0} is invalid' \
             .report_link.get_attribute('href')
@@ -223,9 +285,9 @@ class SimilarityCheckCard(BaseCard):
 
     def validate_generate_report_button(self):
         """
-        Validate 'Generate Report' button visibility depending on completion:
+        Validate 'Generate Report' button visibility depending on card completion:
         it should be not visible if the card completed
-        and visible if the card is uncompleted
+        and visible if the card is editable
         :return: void function
         """
         card_completed_state = self.completed_state()

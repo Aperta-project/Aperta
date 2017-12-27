fc05bdede48ff29480fb2009415f94d6e8b6d3a0
Shawn Gatchell <shawng03@gmail.com>
2017-12-27 15:33:49 -0600

    Use format-date helper

--- client/app/pods/components/co-authors/component.js
+++ client/app/pods/components/co-authors/component.js
@@ -1,11 +1,7 @@
 import Ember from 'ember';
-import moment from 'moment';
 
 export default Ember.Component.extend({
   classNames: ['co-author-confirmation'],
-  dateCreated: Ember.computed('author.createdAt', function() {
-    return moment(this.get('author.createdAt')).format('ll');
-  }),
   isConfirmed: Ember.computed.equal('author.confirmationState', 'confirmed'),
   isConfirmable: Ember.computed.equal('author.confirmationState', 'unconfirmed'),
 

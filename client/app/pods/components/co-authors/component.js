1a9b0827c3fc76e5f7fff9b9f032a2baa88cc178
Shawn Gatchell <shawng03@gmail.com>
2017-12-27 23:12:22 -0600

    Component requires author

--- client/app/pods/components/co-authors/component.js
+++ client/app/pods/components/co-authors/component.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
+import { PropTypes } from 'ember-prop-types';
 
 export default Ember.Component.extend({
   classNames: ['co-author-confirmation'],
   isConfirmed: Ember.computed.equal('author.confirmationState', 'confirmed'),
   isConfirmable: Ember.computed.equal('author.confirmationState', 'unconfirmed'),
+
+  PropTypes: {
+    author: PropTypes.EmberObject.isRequired
+  },
 
   actions: {
     save() {

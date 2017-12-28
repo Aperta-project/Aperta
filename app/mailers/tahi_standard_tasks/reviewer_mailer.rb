6944aa033901055a959057eda96b828851677fdb
Andres Visbal <andresvisbal@gmail.com>
2017-12-28 17:19:54 -0500

    APERTA-10841 Removing unused method

--- app/mailers/tahi_standard_tasks/reviewer_mailer.rb
+++ app/mailers/tahi_standard_tasks/reviewer_mailer.rb
@@ -106,18 +106,6 @@ module TahiStandardTasks
 
     private
 
-    def send_mail_with_letter_template(scenario:)
-      @letter_template.render(scenario)
-      @subject = @letter_template.subject
-      @body = @letter_template.body
-      @to = @letter_template.to
-      @cc = @letter_template.cc
-      @bcc = @letter_template.bcc
-      mail(to: @to, cc: @cc, bcc: @bcc, subject: @subject)
-    rescue BlankRenderFieldsError => e
-      Bugsnag.notify(e)
-    end
-
     def reminder_notice(letter_template_ident:, reviewer_report_id:)
       @reviewer_report = ReviewerReport.find(reviewer_report_id)
       @paper = @reviewer_report.paper

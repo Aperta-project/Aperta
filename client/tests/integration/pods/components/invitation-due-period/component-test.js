ba7713a875f8f355262cf96f9997f65ce4045065
Ben Miller <bmiller@plos.org>
2018-01-02 16:48:08 -0800

    APERTA-9693 fix and add tests

--- client/tests/integration/pods/components/invitation-due-period/component-test.js
+++ client/tests/integration/pods/components/invitation-due-period/component-test.js
@@ -6,10 +6,6 @@ moduleForComponent('invitation-due-period', 'Integration | Component | invitatio
 });
 
 test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
   this.render(hbs`{{invitation-due-period}}`);
 
   assert.equal(this.$().text().trim(), '');
@@ -22,4 +18,28 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
+});
+
+test('only displays the input if display is true', function(assert) {
+  this.set('display', true);
+  this.render(hbs`{{invitation-due-period display=display}}`);
+  assert.ok(this.$('input').length === 1, 'should have one input element');
+  this.set('display', false);
+  this.render(hbs`{{invitation-due-period display=display}}`);
+  assert.ok(!this.$('input').length, 'should have no input element');
+});
+
+test('should have error class when input is invalid', function(assert) {
+  this.set('display', true);
+  this.render(hbs`{{invitation-due-period display=display}}`);
+  this.$('input').val('wat').trigger('input');
+  assert.elementFound('input.error');
+});
+
+test('change of input should trigger callback', function(assert) {
+  assert.expect(1);
+  this.set('display', true);
+  this.set('callback', function() { assert.ok(true, 'callback called'); });
+  this.render(hbs`{{invitation-due-period display=display onchange=callback}}`);
+  this.$('input').val('10').trigger('input');
 });

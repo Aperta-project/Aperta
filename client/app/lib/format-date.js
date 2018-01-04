043ea3ecaf4ae673d980e4472a9da7b25b753614
Shawn Gatchell <shawng03@gmail.com>
2018-01-03 16:42:12 -0800

    Use date constants in templates

--- client/app/lib/format-date.js
+++ client/app/lib/format-date.js
@@ -3,5 +3,14 @@ import moment from 'moment';
 export default function(date, options) {
   let dateObj = moment(date);
   if (!dateObj.isValid()) { return date; }
-  return dateObj.format(options.format || 'MMMM D, YYYY HH:mm');
+
+  let formats = {
+    'long-date-short-time': 'MMMM D, YYYY h:mm A',  //(aka LLL) "September 4, 1986 8:30 PM"
+    'long-date': 'MMMM D, YYYY', // "September 4, 1986"
+    'short-date': 'MMM D, YYYY', //(aka ll) "Sep 4, 1986"
+  };
+
+  let format = formats[options.format] || options.format || 'MMMM D, YYYY HH:mm';
+
+  return dateObj.format(format);
 }

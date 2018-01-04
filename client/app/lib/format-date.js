dce56161faa9e5a4c2f1a9e70e6a4a9f82af0d37
Shawn Gatchell <shawng03@gmail.com>
2018-01-04 11:24:22 -0800

    Replace {{moment-format with {{format-date

--- client/app/lib/format-date.js
+++ client/app/lib/format-date.js
@@ -4,15 +4,16 @@ export default function(date, options) {
   let dateObj = moment(date);
   if (!dateObj.isValid()) { return date; }
 
-  let formats = {
-    'long-date-short-time': 'MMMM D, YYYY h:mm A',  //(aka LLL) "September 4, 1986 8:30 PM"
-    'long-date': 'MMMM D, YYYY', // "September 4, 1986"
-    'short-date': 'MMM D, YYYY', //(aka ll) "Sep 4, 1986"
-    'long-month-day': 'MMMM DD', // "September 04"
-    'long-date-military-time': 'MMMM DD, YYYY H:mm', // "September 04, 1986 20:30"
-  };
-
   let format = formats[options.format] || options.format || 'MMMM D, YYYY HH:mm';
-
   return dateObj.format(format);
 }
+
+export const formats = {
+  'long-date-short-time': 'MMMM D, YYYY h:mm A',  // (alias: LLL) "September 4, 1986 8:30 PM"
+  'long-date': 'MMMM D, YYYY', // "September 4, 1986"
+  'long-month-day': 'MMMM DD', // "September 04"
+  'long-date-military-time': 'MMMM DD, YYYY H:mm', // "September 04, 1986 20:30"
+  'long-date-hour': 'MMMM D, ha z', // "September 4, 8pm PDT"
+  'short-date': 'MMM D, YYYY', // (alias: ll) "Sep 4, 1986"
+  'short-date-2': 'MMM DD, YYYY', // "Sep 04, 1986"
+};

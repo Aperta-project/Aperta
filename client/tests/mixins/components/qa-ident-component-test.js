5233d6a55d2e8b81d2287b164909d401af341ac1
Erik Hetzner <ehetzner@plos.org>
2018-01-08 11:01:30 -0800

    APERTA-12033 Improve QAIdent tests

--- client/tests/mixins/components/qa-ident-component-test.js
+++ client/tests/mixins/components/qa-ident-component-test.js
@@ -1,17 +1,29 @@
-import { module, test } from 'qunit';
+import { moduleFor, test } from 'ember-qunit';
 import Ember from 'ember';
 import QAIdent from 'tahi/mixins/components/qa-ident';
 
-module('mixins | components | QA Ident', {
-  beforeEach() {
-    const ExtendedObject = Ember.Object.extend(QAIdent, {});
-    this.cntrl = ExtendedObject.create({
-      content: {ident: 'identifier'}
-    });
+let content = Ember.Object.create({ 'ident' : 'my-ident' });
+
+moduleFor('mixin:qa-ident', 'Unit | Mixin | qa-ident', {
+  integration: true,
+
+  subject() {
+    const component = Ember.Object.extend(QAIdent, {}).create();
+    component.set('content', content);
+    return component;
   }
 });
 
-test('with data key delays progress bar', function(assert) {
-  const expected = 'qa--' + this.cntrl.content.ident;
-  assert.equal(this.cntrl.get('QAIdent'), expected);
+test('classNameBindings includes QAIdent', function(assert) {
+  assert.deepEqual(this.subject().get('classNameBindings'), ['QAIdent']);
+});
+
+test('QAIdent returns the right value', function(assert) {
+  assert.equal(this.subject().get('QAIdent'), 'qa-ident-my-ident');
+});
+
+
+test('QAIdent squashes disallowed characters', function(assert) {
+  content = Ember.Object.create({ 'ident' : 'my ident 1234!@#$%^&' });
+  assert.equal(this.subject().get('QAIdent'), 'qa-ident-my_ident____________');
 });

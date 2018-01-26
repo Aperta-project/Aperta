d3c4f9defcc3a7036fc3bb1c9ca4fe85ed4b91e0
Erik Hetzner <ehetzner@plos.org>
2018-01-26 15:18:24 -0800

    APERTA-11411 Use create, not build_stubbed

--- spec/support/shared_examples/reviewer_report_task_serializer_shared_examples.rb
+++ spec/support/shared_examples/reviewer_report_task_serializer_shared_examples.rb
@@ -1,13 +1,26 @@
 RSpec.shared_examples_for :reviewer_report_task_serializer do
   let(:task_class) { described_class.name.gsub(/Serializer$/, '').constantize }
-  let(:reviewer_report_task) { FactoryGirl.build_stubbed task_class.name.demodulize.underscore.to_sym }
+  let(:paper) { FactoryGirl.create(:paper) }
+  let!(:reviewer_report) do
+    FactoryGirl.create(
+      :reviewer_report,
+      paper: paper,
+      decision: decision,
+      task: reviewer_report_task
+    )
+  end
+  let(:reviewer_report_task) do
+    FactoryGirl.create(
+      task_class.name.demodulize.underscore.to_sym,
+      paper: paper
+    )
+  end
   let(:object_for_serializer) { reviewer_report_task }
-  let(:decision) { FactoryGirl.build_stubbed(:decision) }
-  let(:user) { FactoryGirl.build_stubbed(:user) }
+  let(:decision) { FactoryGirl.create(:decision, paper: paper) }
+  let(:user) { FactoryGirl.create(:user) }
   let(:serializer) { described_class.new(reviewer_report_task, scope: user) }
 
   before do
-    allow(reviewer_report_task.paper).to receive('decisions').and_return [decision]
     allow(reviewer_report_task).to receive(:submitted?).and_return true
     allow(reviewer_report_task).to receive(:display_status).and_return :submitted
 
@@ -21,7 +34,6 @@ RSpec.shared_examples_for :reviewer_report_task_serializer do
 
   it "serializes successfully" do
     expect(deserialized_content).to be_kind_of Hash
-
     expect(task_content).to match(
       hash_including(
         is_submitted: true,

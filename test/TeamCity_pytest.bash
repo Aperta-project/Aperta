2c1d0abeba3da0eb9eb16e921f84acbf9dbf7e90
Mr. Jeffrey Gray <jgray@plos.org>
2018-01-26 09:01:44 -0800

    Noted issue with pytest run script and resolved.

--- test/TeamCity_pytest.bash
+++ test/TeamCity_pytest.bash
@@ -14,7 +14,6 @@ ASSETS_DIR=$SCRIPT_DIR/frontend/assets
 
 pyenv activate tahi-int3
 pyenv version
-wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux32.tar.gz
 cd $ASSETS_DIR
 wget http://bighector.plos.org/aperta/testing_assets.tar.gz
 TESTING_ASSETS="testing_assets.tar.gz"

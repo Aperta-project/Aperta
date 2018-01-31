03a530e8517261b62831b8056cbe17c9a0080274
Shawn Gatchell <shawng03@gmail.com>
2018-01-30 17:12:04 -0800

    validation no longer needed

--- client/app/pods/components/invitation-due-period/component.js
+++ client/app/pods/components/invitation-due-period/component.js
@@ -1,16 +1,11 @@
 import Ember from 'ember';
-import ValidationErrorsMixin from 'tahi/mixins/validation-errors';
 
-export default Ember.Component.extend(ValidationErrorsMixin, {
+export default Ember.Component.extend({
   classNames: ['invitation-item-due'],
-  validations: { dueIn: ['number'] },
-  errored: Ember.computed.notEmpty('validationErrors.dueIn'),
   actions: {
     noop(){},
     onInputChange: function (event) {
-      this.clearAllValidationErrors();
       if (this.get('value') < 1) { this.set('value', 1); }
-      this.validate('dueIn', this.get('value'));
       if (this.get('onchange')) { this.get('onchange')(event); }
     }
   }

5675f85c3703dd7f760c646cf1be0089aa274644
Jim Haungs <jhaungs@plos.org>
2018-02-21 16:49:52 -0800

    Recursive JSON traversal for keys and data types

--- app/models/security_audit.rb
+++ app/models/security_audit.rb
@@ -7,19 +7,31 @@ class SecurityAudit < ActiveRecord::Base
   def payload=(json)
     super
     hash = JSON.parse(json)
-    self.key_names  = find_keys(hash)
-    self.data_types = find_types(hash)
+    self.key_names  = format_names(find_keys(hash))
+    self.data_types = format_names(find_types(hash))
   end
 
-  def find_keys(hash)
-    format_names(hash.keys)
+  def find_keys(data)
+    case data
+    when Hash
+      data.keys + data.flat_map {|k, v| find_keys(v)}
+    when Array
+      data.flat_map {|each| find_keys(each)}
+    else
+      []
+    end
   end
 
-  def find_types(hash)
-    format_names(hash.values.map(&:class))
+  def find_types(data)
+    case data
+    when Hash
+      data.values.map(&:class) + data.flat_map {|k, v| find_types(v)}
+    when Array
+      data.flat_map {|each| find_types(each)}
+    else
+      [data.class]
+    end
   end
-
-  private
 
   def format_names(list)
     list.map(&:to_s).uniq.sort.join(',')

c3438d55a46176f6735365d6472b26d9d27ee2ca
Erik Hetzner <ehetzner@plos.org>
2018-03-02 15:06:23 -0800

    APERTA-12687 Do not check authentication in first serialization call

--- app/serializers/authz_serializer.rb
+++ app/serializers/authz_serializer.rb
@@ -1,6 +1,12 @@
 class AuthzSerializer < ActiveModel::Serializer
   def attributes
-    if can_view?
+    # Are we at the top level (e.g., not an has_many/has_one included
+    # serialier)? If so, skip this check - it should have happened in the
+    # controller.
+    if !options.fetch(:not_top_level, false)
+      options[:not_top_level] = true
+      super
+    elsif can_view?
       super
     else
       { id: object.try(:id) }.compact
---
+++ spec/serializers/authz_serializer_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+class TestAuthzSerializer < AuthzSerializer
+  attributes :foo
+  has_one :viewable, include: true, serializer: 'TestAuthzSerializerLeaf'
+  has_one :unviewable, include: true, serializer: 'TestAuthzSerializerLeaf'
+end
+
+class TestAuthzSerializerLeaf < AuthzSerializer
+  attributes :foo
+end
+
+class TestAuthzObject
+  include ActiveModel::SerializerSupport
+
+  def foo
+    'foo'
+  end
+
+  def viewable
+    # override
+  end
+
+  def unviewable
+    # override
+  end
+
+  def user_can_view?(_user)
+    # override
+  end
+
+  def id
+    1
+  end
+end
+
+describe AuthzSerializer do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:foo) { { foo: 'foo' } }
+  let(:id) { { id: 1 } }
+  let(:json) { TestAuthzSerializer.new(root_object, scope: user).as_json }
+  let(:root_object) { TestAuthzObject.new }
+  let(:other_root_object) { TestAuthzObject.new }
+  let(:viewable_object) { TestAuthzObject.new }
+  let(:unviewable_object) { TestAuthzObject.new }
+  let(:array) { [root_object, other_root_object] }
+
+  before do
+    expect(root_object).to receive(:viewable).and_return(viewable_object)
+    expect(root_object).to receive(:unviewable).and_return(unviewable_object)
+    expect(viewable_object).to receive(:user_can_view?).with(user).and_return(true).at_least(:once)
+    expect(unviewable_object).to receive(:user_can_view?).with(user).and_return(false).at_least(:once)
+
+    # We expect this method *not* to be called at the root, because the root
+    # object should *not* check authz
+    allow(root_object).to receive(:user_can_view?).and_raise(Exception)
+  end
+
+  it 'should include the attributes at the top level' do
+    expect(json[:test_authz]).to match(hash_including(foo: 'foo'))
+  end
+
+  it 'should include the attributes of the viewable object' do
+    expect(json[:test_authz][:viewable]).to eq(foo)
+  end
+
+  it 'should not include the attributes of the unviewable object' do
+    expect(json[:test_authz][:unviewable]).to eq(id)
+  end
+
+  describe 'when using array serializer' do
+    before do
+      expect(other_root_object).to receive(:viewable).and_return(viewable_object)
+      expect(other_root_object).to receive(:unviewable).and_return(unviewable_object)
+      expect(other_root_object).to receive(:user_can_view?).and_return(true)
+    end
+
+    it 'should serialize both objects and call user_can_view? on the second object' do
+      json = ActiveModel::ArraySerializer.new(array, scope: user, each_serializer: TestAuthzSerializer).as_json
+      expect(json).to contain_exactly(foo.merge(viewable: foo, unviewable: id), foo.merge(viewable: foo, unviewable: id))
+    end
+  end
+end

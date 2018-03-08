8df28406504bba60811b6ab35a2eed01b772148f
Ben Miller <bmiller@plos.org>
2018-03-08 15:36:09 -0800

    APERTA-12458 harden journal attr validations

--- spec/models/journal_spec.rb
+++ spec/models/journal_spec.rb
@@ -51,9 +51,11 @@ describe Journal do
     end
 
     it 'requires a valid doi_publisher_prefix' do
-      journal.doi_publisher_prefix = '@'
-      expect(journal).to_not be_valid
-      expect(journal.errors[:doi_publisher_prefix]).to contain_exactly('The DOI Publisher Prefix is not valid. It can only contain word characters, numbers, -, and .')
+      ["@", "foo\nbar"].each do |input|
+        journal.doi_publisher_prefix = input
+        expect(journal).to_not be_valid
+        expect(journal.errors[:doi_publisher_prefix]).to contain_exactly('The DOI Publisher Prefix is not valid. It can only contain word characters, numbers, -, and .')
+      end
     end
 
     it 'requires a doi_journal_prefix' do
@@ -63,9 +65,11 @@ describe Journal do
     end
 
     it 'requires a valid doi_journal_prefix' do
-      journal.doi_journal_prefix = 'not-valid'
-      expect(journal).to_not be_valid
-      expect(journal.errors[:doi_journal_prefix]).to contain_exactly('The DOI Journal Prefix is not valid. It must begin with \'journal\' and can contain any characters except /')
+      ["not-valid", "foo\nbar"].each do |input|
+        journal.doi_journal_prefix = input
+        expect(journal).to_not be_valid
+        expect(journal.errors[:doi_journal_prefix]).to contain_exactly('The DOI Journal Prefix is not valid. It must begin with \'journal\' and can contain any characters except /')
+      end
     end
 
     it 'requires a last_doi_issued' do

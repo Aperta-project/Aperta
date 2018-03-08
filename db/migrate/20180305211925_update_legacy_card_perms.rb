8897310829505d26902347bd6bea5183bd7d2dbb
Ben Miller <bmiller@plos.org>
2018-03-08 13:54:53 -0800

    APERTA-12446 refactor with DataTransformation and add assertions

--- db/migrate/20180305211925_update_legacy_card_perms.rb
+++ db/migrate/20180305211925_update_legacy_card_perms.rb
@@ -1,15 +1,5 @@
 class UpdateLegacyCardPerms < DataMigration
-  PARTICIPANT_PERMISSIONS = ['view_participants', 'manage_participant'].freeze
   def up
-    Journal.includes(:cards).all.each do |journal|
-      journal.cards.each do |card|
-        default_permissions = CustomCard::DefaultCardPermissions.new(journal)
-        card_key = card.name.delete(' ').underscore # reverse titleize
-        next unless default_permissions.permissions[card_key]
-        default_permissions.apply(card_key) do |action, roles|
-          CardPermissions.set_roles(card, action, roles) if PARTICIPANT_PERMISSIONS.include?(action)
-        end
-      end
-    end
+    DataTransformation::UpdateLegacyCardPerms.new.call
   end
 end

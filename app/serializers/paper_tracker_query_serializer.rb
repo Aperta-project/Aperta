f8f619b42967dc0a3798fe96ec44ccfa74874503
Erik Hetzner <ehetzner@plos.org>
2018-03-07 16:48:31 -0800

    APERTA-12693 Enforce permissions in paper tracker queries

--- app/serializers/paper_tracker_query_serializer.rb
+++ app/serializers/paper_tracker_query_serializer.rb
@@ -2,11 +2,4 @@
 # Searches" on the paper tracker page.
 class PaperTrackerQuerySerializer < AuthzSerializer
   attributes :title, :query, :order_by, :order_dir, :id
-
-  private
-
-  # TODO: APERTA-12693 Stop overriding this
-  def can_view?
-    true
-  end
 end

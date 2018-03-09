e0a99fa386a3cdd92a1e76208a99eb3efb626608
Erik Hetzner <ehetzner@plos.org>
2018-03-09 09:34:54 -0800

    APERTA-12693 Enforce permissions in comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -3,11 +3,4 @@ class CommentSerializer < AuthzSerializer
 
   has_one :task, embed: :id, polymorphic: true
   has_one :commenter, serializer: UserSerializer, include: true, root: :users, embed: :id
-
-  private
-
-  # TODO: APERTA-12693 Stop overriding this
-  def can_view?
-    true
-  end
 end

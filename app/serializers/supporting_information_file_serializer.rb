e51b661137d1884a62c89eaa4098f9c79d5f81a7
Shawn Gatchell <shawng03@gmail.com>
2018-03-09 17:15:57 -0800

    enforce permissions in supporting information files

--- app/serializers/supporting_information_file_serializer.rb
+++ app/serializers/supporting_information_file_serializer.rb
@@ -13,10 +13,4 @@ class SupportingInformationFileSerializer < AuthzSerializer
              :created_at
   has_one :paper, embed: :id, include: false
 
-  private
-
-  # TODO: APERTA-12693 Stop overriding this
-  def can_view?
-    true
-  end
 end

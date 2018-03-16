58abfc7dc29927bb2aa8359187930b3cbbb65c7f
Ben Miller <bmiller@plos.org>
2018-03-16 14:27:00 -0700

    APERTA-12715 fix scheduled events controller spec

--- app/controllers/scheduled_events_controller.rb
+++ app/controllers/scheduled_events_controller.rb
@@ -6,9 +6,8 @@ class ScheduledEventsController < ApplicationController
     permitted = params.require(:scheduled_event).permit(:state)
     scheduled_event = ScheduledEvent.find(params[:id])
     owner = scheduled_event.due_datetime.due
-    # because this is only used by ReviewerReport
-    # but ideally this would be a relationship directly on a task
-    requires_user_can(:edit, (owner.try(:task) || owner))
+    # Ideally this would be a relationship directly on a task, le sigh
+    requires_user_can(:edit, (owner.is_a?(Task) ? owner : owner.task))
     scheduled_event.switch_on! if permitted[:state] == 'active'
     scheduled_event.switch_off! if permitted[:state] == 'passive'
     render json: scheduled_event

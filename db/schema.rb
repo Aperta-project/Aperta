1351e1a930620b5e91a3944f3f93800a0c6506eb
Shawn Gatchell <shawng03@gmail.com>
2018-04-03 12:18:41 -0700

    Remove column journals#pdf_allowed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330020707) do
+ActiveRecord::Schema.define(version: 20180403191254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -535,7 +535,6 @@ ActiveRecord::Schema.define(version: 20180330020707) do
     t.string   "staff_email"
     t.string   "reviewer_email_bcc"
     t.string   "editor_email_bcc"
-    t.boolean  "pdf_allowed",          default: false
     t.boolean  "msword_allowed",       default: false, null: false
   end
 

b7263806bee96e109d8d3915b16b8e86cf887c6e
Ben Miller <bmiller@plos.org>
2018-04-04 14:33:16 -0700

    Bust delegate authorizations too

--- app/services/can_cache.rb
+++ app/services/can_cache.rb
@@ -12,13 +12,23 @@ class CanCache
     end
 
     def permissions_cache_bust(permissions)
-      regex_fragments = permissions.map { |perm| "#{perm.action}_#{perm.applies_to.underscore}" }
-      Rails.cache.delete_matched(/^#{KEY_PREFIX}.*(#{regex_fragments.join('|')})/)
+      perm_fragments = permissions.map { |perm| "#{perm.action}_#{perm.applies_to.underscore}" }
+      fragments = delegated_permission_fragments(permissions).concat(perm_fragments).uniq
+      Rails.cache.delete_matched(/^#{KEY_PREFIX}.*(#{fragments.join('|')})/)
     end
 
     def paper_state_cache_bust
       stateful_permissions = Permission.includes(:states).where.not(permission_states: { name: '*' })
       permissions_cache_bust(stateful_permissions)
+    end
+
+    def delegated_permission_fragments(permissions)
+      permissions.inject([]) do |fragments, perm|
+        delegates = Authorizations::Configuration.authorizations.select do |auth|
+          auth.assignment_to.to_s == perm.applies_to
+        end
+        fragments + delegates.map { |d| "#{perm.action}_#{d.authorizes.to_s.underscore}" }
+      end
     end
   end
 end

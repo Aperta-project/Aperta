e23e6a1f3f9273688673790b6588be8c1ae2df8c
Erik Hetzner <ehetzner@plos.org>
2018-04-06 13:24:52 -0700

    APERTA-12769 Remove whenever cron jobs
    These are no longer needed.

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,6 @@ set :linked_files, %w(env puma.rb)
 set :repo_url, 'git@github.com:aperta-project/aperta.git'
 set :web_service_name, 'tahi-web' # used by puma:{start,stop,restart}
 set :worker_service_name, 'tahi-worker' # used by sidekiq:{start,stop,restart}
-set :whenever_roles, %(cron)
 set :nginx_pidfile, '/run/nginx.pid'
 set :puma_pidfile, '/var/www/tahi/current/tmp/pids/puma.pid'
 set :sidekiq_pidfile, '/var/www/tahi/current/tmp/pids/sidekiq.pid'

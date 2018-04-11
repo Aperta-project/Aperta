48ba4424046eaeed849046e8cf0c594326ea9bd4
Ben Miller <bmiller@plos.org>
2018-04-11 12:28:05 -0700

    APERTA-12773 relax view permission for letter templates

--- app/models/letter_template.rb
+++ app/models/letter_template.rb
@@ -21,7 +21,7 @@ class LetterTemplate < ActiveRecord::Base
   before_validation :canonicalize_email_addresses
 
   def user_can_view?(check_user)
-    check_user.can?(:manage_users, Journal)
+    check_user.can?(:view, Paper)
   end
 
   def render(context, check_blanks: false)

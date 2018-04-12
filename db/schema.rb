bb12e6ce97a4fa3109b0d2e5d0d133a755d0764a
Shawn Gatchell <shawng03@gmail.com>
2018-04-11 21:50:56 -0700

    Update content Upload Manuscript cards and tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403210942) do
+ActiveRecord::Schema.define(version: 20180412043643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

f11196e99ef27b6148f19dd7add4c8eb303a9978
Shawn Gatchell <shawng03@gmail.com>
2018-04-16 15:01:19 -0700

    Set value on column that was created in the same migration chain

--- db/migrate/20180403210942_biology_allows_msword.rb
+++ db/migrate/20180403210942_biology_allows_msword.rb
@@ -3,6 +3,9 @@ class BiologyAllowsMsword < ActiveRecord::Migration
     journal = Journal.find_by(name: "PLOS Biology")
     return unless journal
 
-    journal.update_attributes!(msword_allowed: true)
+    # rubocop:disable Rails/SkipsModelValidations
+    journal.update_column(:msword_allowed, true)
+    # rubocop:enable Rails/SkipsModelValidations
+    raise unless Journal.find_by(name: "PLOS Biology").reload.msword_allowed?
   end
 end

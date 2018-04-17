163e4c163ee25971a1a94480f354a5f982fc07c0
Ben Miller <bmiller@plos.org>
2018-04-17 11:44:54 -0700

    APERTA-12703 styling

--- app/controllers/tahi_devise/sessions_controller.rb
+++ app/controllers/tahi_devise/sessions_controller.rb
@@ -10,12 +10,22 @@ module TahiDevise
       return unless FeatureFlag['DISABLE_SUBMISSIONS']
       <<-HTML
         <div class='disable-submissions-alert'>
-          <p>Please note that we are transitioning to another submission system and are no longer accepting new submissions in Aperta</p>
+          <p class='bold'>
+            Please note that we are transitioning to another submission system and are no longer accepting new submissions in Aperta.
+          </p>
           <ul>
-            <li>To submit your manuscript for consideration at <em>PLOS Biology</em>, please visit Editorial Management</li>
-            <li>You may continue to edit or revise existing existing <em>PLOS Biology</em> submissions that are already under consideration</li>
+            <li>
+              To submit your manuscript for consideration at <em>PLOS Biology</em>, please visit
+              <a href='https://www.editorialmanager.com/pbiology/default.aspx'>Editorial Management</a>.
+            </li>
+            <li>
+              You may continue to edit or revise existing existing <em>PLOS Biology</em> submissions that are already under consideration.
+            </li>
           </ul>
-          <p>Contact us with any concerns at: plosbiology@plos.org. Learn more about submitting to PLOS Biology</p>
+          <p>
+            Contact us with any concerns at: <a href='mailto:plosbiology@plos.org'>plosbiology@plos.org</a>.
+            Learn more about <a href='http://journals.plos.org/plosbiology/s/submit-now'>submitting to <em>PLOS Biology</em></a>.
+          </p>
         </div>
       HTML
     end

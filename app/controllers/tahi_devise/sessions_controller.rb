47cbfb9078885b9862cdd0a90e8e0ea4926649d1
Ben Miller <bmiller@plos.org>
2018-04-17 14:50:59 -0700

    APERTA-12703 use partial

--- app/controllers/tahi_devise/sessions_controller.rb
+++ app/controllers/tahi_devise/sessions_controller.rb
@@ -8,26 +8,7 @@ module TahiDevise
 
     def sign_in_alert
       return unless FeatureFlag['DISABLE_SUBMISSIONS']
-      <<-HTML
-        <div class='disable-submissions-alert'>
-          <p class='bold'>
-            Please note that we are transitioning to another submission system and are no longer accepting new submissions in Aperta.
-          </p>
-          <ul>
-            <li>
-              To submit your manuscript for consideration at <em>PLOS Biology</em>, please visit
-              <a href='https://www.editorialmanager.com/pbiology/default.aspx'>Editorial Management</a>.
-            </li>
-            <li>
-              You may continue to edit or revise existing existing <em>PLOS Biology</em> submissions that are already under consideration.
-            </li>
-          </ul>
-          <p>
-            Contact us with any concerns at: <a href='mailto:plosbiology@plos.org'>plosbiology@plos.org</a>.
-            Learn more about <a href='http://journals.plos.org/plosbiology/s/submit-now'>submitting to <em>PLOS Biology</em></a>.
-          </p>
-        </div>
-      HTML
+      render_to_string(partial: 'shared/disabled_submissions_alert').squish
     end
   end
 end

f2d13ae73ac8ae7d8acb0f83df68fde986bf8d3f
Erik Hetzner <ehetzner@plos.org>
2018-05-07 12:08:53 -0700

    APERTA-12826 Clean up autoload/eager load mess

--- config/application.rb
+++ config/application.rb
@@ -32,16 +32,16 @@ TahiEnv.validate!
 
 module Tahi
   class Application < Rails::Application
-    config.eager_load = true
-
-    config.autoload_paths += %W(#{config.root}/lib)
-    config.autoload_paths += %W(#{config.root}/lib/cli_utilities)
-    config.autoload_paths += %W(#{config.root}/lib/data_transformation)
-    config.autoload_paths += %W(#{config.root}/lib/tahi_reports)
-    config.autoload_paths += %W(#{config.root}/app/workers)
-    config.autoload_paths += %W(#{config.root}/app/subscribers)
-    config.eager_load_paths += %W[#{config.root}/lib/custom_card]
-    config.eager_load_paths += %W[#{config.root}/lib/loofah]
+    paths = %W[
+      #{config.root}/lib
+      #{config.root}/lib/cli_utilities
+      #{config.root}/lib/data_transformation
+      #{config.root}/lib/tahi_reports
+      #{config.root}/lib/custom_card
+      #{config.root}/lib/loofah
+    ]
+    config.eager_load_paths += paths
+    config.autoload_paths += paths
 
     config.s3_bucket = ENV.fetch('S3_BUCKET', :not_set)
     config.carrierwave_storage = :fog
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,7 +30,7 @@ Tahi::Application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  # config.eager_load = true
+  config.eager_load = false
 
   config.session_store :cookie_store, key: '_tahi_session'
 

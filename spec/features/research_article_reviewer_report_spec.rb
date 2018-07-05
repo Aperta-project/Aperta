abb0ee9644aa746b7d1cfafb14ac52d5006683cb
Erik Hetzner <ehetzner@plos.org>
2018-07-05 14:48:34 -0700

    Do not login twice, fix rubocop warnings

--- spec/features/research_article_reviewer_report_spec.rb
+++ spec/features/research_article_reviewer_report_spec.rb
@@ -30,12 +30,13 @@ include RichTextEditorHelpers
 feature 'Reviewer filling out their research article reviewer report', js: true do
   let(:journal) { FactoryGirl.create :journal, :with_roles_and_permissions }
   let(:paper) do
-    FactoryGirl.create \
+    FactoryGirl.create(
       :paper_with_phases,
       :submitted_lite,
       :with_creator,
       journal: journal,
       uses_research_article_reviewer_report: true
+    )
   end
   let(:task) { FactoryGirl.create :paper_reviewer_task, :with_loaded_card, paper: paper }
 
@@ -69,9 +70,6 @@ feature 'Reviewer filling out their research article reviewer report', js: true 
     assign_reviewer_role paper, reviewer
     assign_journal_role(journal, journal_admin, :admin)
     FactoryGirl.create :feature_flag, name: "PREPRINT"
-
-    login_as(reviewer, scope: :user)
-    visit "/"
   end
 
   scenario "A paper's creator cannot access the Reviewer Report" do
@@ -84,184 +82,191 @@ feature 'Reviewer filling out their research article reviewer report', js: true 
     )
   end
 
-  scenario 'A reviewer can fill out their own Reviewer Report, submit it, and see a readonly view of their responses' do
-    create_reviewer_invitation(paper)
-    reviewer_report_task = create_reviewer_report_task
-    report = reviewer_report_task.reviewer_reports.first
-    expect(report.scheduled_events.pluck(:state).uniq).to eq ['active']
+  context 'reviewer is logged in' do
+    before do
+      login_as(reviewer, scope: :user)
+      visit "/"
+    end
 
-    Page.view_paper paper
-    t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
+    scenario 'A reviewer can fill out their own Reviewer Report, submit it, and see a readonly view of their responses' do
+      create_reviewer_invitation(paper)
+      reviewer_report_task = create_reviewer_report_task
+      report = reviewer_report_task.reviewer_reports.first
+      expect(report.scheduled_events.pluck(:state).uniq).to eq ['active']
 
-    wait_for_editors # Wait for rich-text editors to instantiate
+      Page.view_paper paper
+      t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
 
-    t.fill_in_report 'reviewer_report--competing_interests--detail' => 'I have no competing interests'
-    t.submit_report
-    t.confirm_submit_report
+      wait_for_editors # Wait for rich-text editors to instantiate
 
-    expect(page).to have_selector('.answer-text', text: 'I have no competing interests')
-    expect(report.scheduled_events.pluck(:state).uniq).to eq ['canceled']
-  end
+      t.fill_in_report 'reviewer_report--competing_interests--detail' => 'I have no competing interests'
+      t.submit_report
+      t.confirm_submit_report
 
-  scenario 'A review can see their previous rounds of review' do
-    # seed the Upload Manuscript card so that it can be created after a decision has been registered
-    ctt = CardTaskType.find_by(task_class: "TahiStandardTasks::UploadManuscriptTask")
-    FactoryGirl.create(:card, :versioned, card_task_type: ctt)
+      expect(page).to have_selector('.answer-text', text: 'I have no competing interests')
+      expect(report.scheduled_events.pluck(:state).uniq).to eq ['canceled']
+    end
 
-    create_reviewer_invitation(paper)
-    reviewer_report_task = create_reviewer_report_task
+    scenario 'A review can see their previous rounds of review' do
+      # seed the Upload Manuscript card so that it can be created after a decision has been registered
+      ctt = CardTaskType.find_by(task_class: "TahiStandardTasks::UploadManuscriptTask")
+      FactoryGirl.create(:card, :versioned, card_task_type: ctt)
 
-    # Revision 0
-    Page.view_paper paper
+      create_reviewer_invitation(paper)
+      create_reviewer_report_task
 
-    t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
+      # Revision 0
+      Page.view_paper paper
 
-    wait_for_editors # Wait for rich-text editors to instantiate
+      t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
 
-    t.fill_in_report 'reviewer_report--competing_interests--detail' => 'answer for round 0'
+      wait_for_editors # Wait for rich-text editors to instantiate
 
-    t.submit_report
-    t.confirm_submit_report
-    # no history yet, since we only have the current round of review
-    t.ensure_no_review_history
+      t.fill_in_report 'reviewer_report--competing_interests--detail' => 'answer for round 0'
 
-    # Revision 1
-    register_paper_decision(paper, "major_revision")
-    paper.tasks.find_by_title("Upload Manuscript").complete! # a reviewer can't complete this task, so this is a quick workaround
-    paper.submit! paper.creator
+      t.submit_report
+      t.confirm_submit_report
+      # no history yet, since we only have the current round of review
+      t.ensure_no_review_history
 
-    invitation = create_reviewer_invitation(paper)
-    reviewer_report_task = create_reviewer_report_task
-    reviewer_report_task.reviewer_reports
-      .where(state: 'invitation_not_accepted')
-      .first.accept_invitation!
+      # Revision 1
+      register_paper_decision(paper, "major_revision")
+      paper.tasks.find_by(title: "Upload Manuscript").complete! # a reviewer can't complete this task, so this is a quick workaround
+      paper.submit! paper.creator
 
-    Page.view_paper paper
-    t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
+      create_reviewer_invitation(paper)
+      reviewer_report_task = create_reviewer_report_task
+      reviewer_report_task.reviewer_reports
+        .where(state: 'invitation_not_accepted')
+        .first.accept_invitation!
 
-    wait_for_editors # Wait for rich-text editors to instantiate
+      Page.view_paper paper
+      t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
 
-    t.fill_in_report 'reviewer_report--competing_interests--detail' => 'answer for round 1'
+      wait_for_editors # Wait for rich-text editors to instantiate
 
-    t.submit_report
-    t.confirm_submit_report
+      t.fill_in_report 'reviewer_report--competing_interests--detail' => 'answer for round 1'
 
-    t.ensure_review_history(title: 'v0.0', answers: ['answer for round 0'])
+      t.submit_report
+      t.confirm_submit_report
 
-    # Revision 2
-    register_paper_decision(paper, "major_revision")
-    paper.tasks.find_by_title("Upload Manuscript").complete! # a reviewer can't complete this task, so this is a quick workaround
-    paper.submit! paper.creator
+      t.ensure_review_history(title: 'v0.0', answers: ['answer for round 0'])
 
-    create_reviewer_invitation(paper)
-    reviewer_report_task = create_reviewer_report_task
+      # Revision 2
+      register_paper_decision(paper, "major_revision")
+      paper.tasks.find_by(title: "Upload Manuscript").complete! # a reviewer can't complete this task, so this is a quick workaround
+      paper.submit! paper.creator
 
-    Page.view_paper paper
-    t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
+      create_reviewer_invitation(paper)
+      create_reviewer_report_task
 
-    wait_for_editors # Wait for rich-text editors to instantiate
+      Page.view_paper paper
+      t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
 
-    t.fill_in_report 'reviewer_report--competing_interests--detail' => 'answer for round 2'
+      wait_for_editors # Wait for rich-text editors to instantiate
 
-    t.ensure_review_history(
-      { title: 'v0.0', answers: ['answer for round 0'] },
-      title: 'v1.0', answers: ['answer for round 1']
-    )
+      t.fill_in_report 'reviewer_report--competing_interests--detail' => 'answer for round 2'
 
-    # Revision 3 (we won't answer, just look at previous rounds)
-    register_paper_decision(paper, "major_revision")
-    paper.tasks.find_by_title("Upload Manuscript").complete! # a reviewer can't complete this task, so this is a quick workaround
-    paper.submit! paper.creator
+      t.ensure_review_history(
+        { title: 'v0.0', answers: ['answer for round 0'] },
+        title: 'v1.0', answers: ['answer for round 1']
+      )
 
-    create_reviewer_invitation(paper)
-    reviewer_report_task = create_reviewer_report_task
+      # Revision 3 (we won't answer, just look at previous rounds)
+      register_paper_decision(paper, "major_revision")
+      paper.tasks.find_by(title: "Upload Manuscript").complete! # a reviewer can't complete this task, so this is a quick workaround
+      paper.submit! paper.creator
 
-    Page.view_paper paper
-    t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
+      create_reviewer_invitation(paper)
+      create_reviewer_report_task
 
-    t.ensure_review_history(
-      { title: 'v0.0', answers: ['answer for round 0'] },
-      { title: 'v1.0', answers: ['answer for round 1'] },
-      title: 'v2.0', answers: ['answer for round 2']
-    )
-  end
+      Page.view_paper paper
+      t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
 
-  scenario 'Rescinded invitations cancel scheduled events' do
-    create_reviewer_invitation(paper)
-    reviewer_report_task = create_reviewer_report_task
-    report = reviewer_report_task.reviewer_reports.first
-    expect(report.scheduled_events.pluck(:state).uniq).to eq ['active']
-    report.rescind_invitation!
-    expect(report.scheduled_events.pluck(:state).uniq).to eq ['canceled']
-  end
+      t.ensure_review_history(
+        { title: 'v0.0', answers: ['answer for round 0'] },
+        { title: 'v1.0', answers: ['answer for round 1'] },
+        title: 'v2.0', answers: ['answer for round 2']
+      )
+    end
 
-  scenario 'Changing due dates updates active event dispatch times' do
-    create_reviewer_invitation(paper)
-    reviewer_report_task = create_reviewer_report_task
-    report = reviewer_report_task.reviewer_reports.first
-    report.rescind_invitation!
-    report.accept_invitation!
+    scenario 'Rescinded invitations cancel scheduled events' do
+      create_reviewer_invitation(paper)
+      reviewer_report_task = create_reviewer_report_task
+      report = reviewer_report_task.reviewer_reports.first
+      expect(report.scheduled_events.pluck(:state).uniq).to eq ['active']
+      report.rescind_invitation!
+      expect(report.scheduled_events.pluck(:state).uniq).to eq ['canceled']
+    end
 
-    expect(ScheduledEvent.active.count).to eq 3
-    expect(ScheduledEvent.canceled.count).to eq 3
+    scenario 'Changing due dates updates active event dispatch times' do
+      create_reviewer_invitation(paper)
+      reviewer_report_task = create_reviewer_report_task
+      report = reviewer_report_task.reviewer_reports.first
+      report.rescind_invitation!
+      report.accept_invitation!
 
-    canceled_dates = ScheduledEvent.all.canceled.pluck(:dispatch_at)
-    active_dates = ScheduledEvent.all.active.pluck(:dispatch_at)
+      expect(ScheduledEvent.active.count).to eq 3
+      expect(ScheduledEvent.canceled.count).to eq 3
 
-    report.due_datetime.update(due_at: report.due_at + 1.day)
-    report.schedule_events
+      canceled_dates = ScheduledEvent.all.canceled.pluck(:dispatch_at)
+      active_dates = ScheduledEvent.all.active.pluck(:dispatch_at)
 
-    expect(ScheduledEvent.canceled.pluck(:dispatch_at)).to eq canceled_dates
-    expect(ScheduledEvent.active.pluck(:dispatch_at)).to_not eq active_dates
-  end
+      report.due_datetime.update!(due_at: report.due_at + 1.day)
+      report.schedule_events
 
-  scenario 'Reviewer can upload attachments' do
-    create_reviewer_invitation(paper)
-    create_reviewer_report_task
+      expect(ScheduledEvent.canceled.pluck(:dispatch_at)).to eq canceled_dates
+      expect(ScheduledEvent.active.pluck(:dispatch_at)).to_not eq active_dates
+    end
 
-    Page.view_paper paper
-    paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
+    scenario 'Reviewer can upload attachments' do
+      create_reviewer_invitation(paper)
+      create_reviewer_report_task
 
-    expect(page).to have_css('.attachment-manager')
-    expect(page).to have_content('UPLOAD FILE')
+      Page.view_paper paper
+      paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
 
-    expect(DownloadAttachmentWorker).to receive(:perform_async)
-    file_path = Rails.root.join('spec', 'fixtures', 'about_turtles.docx')
-    attach_file 'file', file_path, visible: false
+      expect(page).to have_css('.attachment-manager')
+      expect(page).to have_content('UPLOAD FILE')
 
-    expect(page).to have_css('.attachment-item')
-  end
+      expect(DownloadAttachmentWorker).to receive(:perform_async)
+      file_path = Rails.root.join('spec', 'fixtures', 'about_turtles.docx')
+      attach_file 'file', file_path, visible: false
 
-  scenario 'A journal admin can edit a submitted reviewer report' do
-    create_reviewer_invitation(paper)
-    reviewer_report_task = create_reviewer_report_task
-    report = reviewer_report_task.reviewer_reports.first
-    Page.view_paper paper
-    t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
-    wait_for_editors
-    t.fill_in_report 'reviewer_report--competing_interests--detail' => 'I have no competing interests'
-    t.submit_report
-    t.confirm_submit_report
+      expect(page).to have_css('.attachment-item')
+    end
 
-    logout
-    login_as(journal_admin, scope: :user)
-    workflow_page.view_task(reviewer_report_task)
-    find('#edit-reviewer-report').click
-    wait_for_editors
-    set_rich_text editor: 'reviewer_report--competing_interests--detail', text: 'revert this'
-    wait_for_ajax
-    click_button("cancel")
-    expect(page).to have_selector('.answer-text', text: 'I have no competing interests')
+    scenario 'A journal admin can edit a submitted reviewer report' do
+      create_reviewer_invitation(paper)
+      reviewer_report_task = create_reviewer_report_task
+      reviewer_report_task.reviewer_reports.first
+      Page.view_paper paper
+      t = paper_page.view_task("Review by #{reviewer.full_name}", ReviewerReportTaskOverlay)
+      wait_for_editors
+      t.fill_in_report 'reviewer_report--competing_interests--detail' => 'I have no competing interests'
+      t.submit_report
+      t.confirm_submit_report
 
-    find('#edit-reviewer-report').click
-    wait_for_editors
-    set_rich_text editor: 'reviewer_report--competing_interests--detail', text: 'save this'
-    find('.required-standalone textarea').set 'Edit notes'
-    wait_for_ajax
-    expect(Answer.first.value).to eq '<p>I have no competing interests</p>'
-    page.execute_script("$(\"button:contains('save')\").trigger('click')")
-    wait_for_ajax
-    expect(Answer.first.value).to eq '<p>save this</p>'
-    expect(page).to have_selector('.answer-text', text: 'save this')
+      logout
+      login_as(journal_admin, scope: :user)
+      workflow_page.view_task(reviewer_report_task)
+      find('#edit-reviewer-report').click
+      wait_for_editors
+      set_rich_text editor: 'reviewer_report--competing_interests--detail', text: 'revert this'
+      wait_for_ajax
+      click_button("cancel")
+      expect(page).to have_selector('.answer-text', text: 'I have no competing interests')
+
+      find('#edit-reviewer-report').click
+      wait_for_editors
+      set_rich_text editor: 'reviewer_report--competing_interests--detail', text: 'save this'
+      find('.required-standalone textarea').set 'Edit notes'
+      wait_for_ajax
+      expect(Answer.first.value).to eq '<p>I have no competing interests</p>'
+      page.execute_script("$(\"button:contains('save')\").trigger('click')")
+      wait_for_ajax
+      expect(Answer.first.value).to eq '<p>save this</p>'
+      expect(page).to have_selector('.answer-text', text: 'save this')
+    end
   end
 end

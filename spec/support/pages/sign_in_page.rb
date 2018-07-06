6eddfea2abed08ba06ec1287c3cd8896c5170c5e
Erik Hetzner <ehetzner@plos.org>
2018-07-06 10:27:25 -0700

    Visit / before attempting sign in spec

--- spec/support/pages/sign_in_page.rb
+++ spec/support/pages/sign_in_page.rb
@@ -25,6 +25,7 @@ class SignInPage < Page
   path :new_user_session
 
   def sign_in(user, signin_string=user.email)
+    visit "/"
     fill_in "Login", with: signin_string
     fill_in "Password", with: 'password'
     click_on "Sign in"
